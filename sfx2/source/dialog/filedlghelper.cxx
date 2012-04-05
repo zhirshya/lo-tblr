LibreOffice/core

commit dd2fe95cce75f1157bd1c75d286a0047b2e4175e

fdo#43895 lp#905355: Never let users save in /tmp by default

sfx2/source/dialog/filedlghelper.cxx
@@ -101,6 +101,9 @@
 #include <sfxlocal.hrc>
 #include <rtl/oustringostreaminserter.hxx>
 #include <rtl/strbuf.hxx>
+#ifdef UNX
+#include <sys/stat.h>
+#endif
 
 //-----------------------------------------------------------------------------
 
@@ -1635,6 +1638,24 @@ void FileDialogHelper_Impl::getRealFilter( String& _rFilter ) const
     }
 }
 
+void FileDialogHelper_Impl::verifyPath()
+{
+#ifdef UNX
+    struct stat aFileStat;
+    const OString sFullPath = OUStringToOString( maPath.copy(RTL_CONSTASCII_LENGTH("file://")) + maFileName, osl_getThreadTextEncoding() );
+    stat( sFullPath.getStr(), &aFileStat );
+    // lp#905355, fdo#43895
+    // Check that the file has read only permission and is in /tmp -- this is
+    //  the case if we have opened the file from the web with firefox only.
+    if ( maPath.reverseCompareToAsciiL("file:///tmp",11) == 0 &&
+            ( aFileStat.st_mode & (S_IRWXO + S_IRWXG + S_IRWXU) ) == S_IRUSR )
+    {
+        maPath = SvtPathOptions().GetWorkPath();
+        mxFileDlg->setDisplayDirectory( maPath );
+    }
+#endif
+}
+
 // ------------------------------------------------------------------------
 void FileDialogHelper_Impl::displayFolder( const ::rtl::OUString& _rPath )
 {
@@ -1648,6 +1669,7 @@ void FileDialogHelper_Impl::displayFolder( const ::rtl::OUString& _rPath )
         try
         {
             mxFileDlg->setDisplayDirectory( maPath );
+            verifyPath();
         }
         catch( const IllegalArgumentException& )
         {
@@ -1665,6 +1687,7 @@ void FileDialogHelper_Impl::setFileName( const ::rtl::OUString& _rFile )
         try
         {
             mxFileDlg->setDefaultName( maFileName );
+            verifyPath();
         }
         catch( const IllegalArgumentException& )
         {

sfx2/source/dialog/filedlgimpl.hxx
@@ -152,6 +152,8 @@ namespace sfx2
 
         void                    implInitializeFileName( );
 
+        void                    verifyPath( );
+
         void                    implGetAndCacheFiles( const ::com::sun::star::uno::Reference< XInterface >& xPicker  ,
                                                       std::vector<rtl::OUString>&               rpURLList,
                                                       const SfxFilter*                    pFilter  );

zhirshya/lo-tblr

commit 3c45090aefd2946ac6a2802fbb0137aa443406de

fdo#43895 lp#905355: fix the fix so it doesn't crash
rtl::OUString rtl::OUString::copy(sal_Int32) const: Assertion
`beginIndex >= 0 && beginIndex <= getLength()' failed.
(regression from dd2fe95cce75f1157bd1c75d286a0047b2e4175e)

sfx2/source/dialog/filedlghelper.cxx
@@ -1641,8 +1641,17 @@ void FileDialogHelper_Impl::getRealFilter( String& _rFilter ) const
 void FileDialogHelper_Impl::verifyPath()
 {
 #ifdef UNX
+    static char const s_FileScheme[] = "file://";
+    if (0 != rtl_ustr_ascii_shortenedCompareIgnoreAsciiCase_WithLength(
+                maPath.getStr(), maPath.getLength(),
+                s_FileScheme, RTL_CONSTASCII_LENGTH(s_FileScheme)))
+    {
+        return;
+    }
+    const OString sFullPath = OUStringToOString(
+            maPath.copy(RTL_CONSTASCII_LENGTH(s_FileScheme)) + maFileName,
+            osl_getThreadTextEncoding() );
     struct stat aFileStat;
-    const OString sFullPath = OUStringToOString( maPath.copy(RTL_CONSTASCII_LENGTH("file://")) + maFileName, osl_getThreadTextEncoding() );
     stat( sFullPath.getStr(), &aFileStat );
     // lp#905355, fdo#43895
     // Check that the file has read only permission and is in /tmp -- this is