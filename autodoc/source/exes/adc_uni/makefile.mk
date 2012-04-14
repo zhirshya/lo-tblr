LibreOffice/core

commit 0c80ad06fd96a4fec062a7edfff12bb65ef204b4

fix macosx build

solenv/bin/macosx-change-install-names.pl
@@ -53,7 +53,7 @@ sub action($$$)
         ('app/UREBIN/URELIB' => '@executable_path/../lib',
          'app/OOO/URELIB' => '@executable_path/../ure-link/lib',
          'app/OOO/OOO' => '@executable_path',
-         'app/SDK/URELIB' => '@executable_path/../../ure-link/lib',
+         'app/SDKBIN/URELIB' => '@executable_path/../../ure-link/lib',
          'app/NONE/URELIB' => '@__VIA_LIBRARY_PATH__',
          'app/NONE/OOO' => '@__VIA_LIBRARY_PATH__',
          'app/NONE/NONE' => '@__VIA_LIBRARY_PATH__',

zhirshya/lo-tblr

commit 1069c3ac638ac689fa582452b3bbba6720dee10d

replace APP?RPATH SDK by SDKBIN in old build system
Commit 0c80ad06fd96a4fec062a7edfff12bb65ef204b4 broke MacOS X builds
because of this discrepancy. It would be easy to accept both, but I
think it is better to be consistent with gbuild.

autodoc/source/exes/adc_uni/makefile.mk
@@ -72,7 +72,7 @@ APP1TARGET=	$(TARGET)
 APP1STACK=	1000000
 APP1OBJS=   $(OBJ)$/main.obj
 
-APP1RPATH=SDK
+APP1RPATH=SDKBIN
 
 .IF "$(GUI)"=="WNT"
 APP1STDLIBS= $(COSVLIB) $(UDMLIB)

codemaker/source/cppumaker/makefile.mk
@@ -64,7 +64,7 @@ NOOPTFILES= \
             $(OBJ)$/cpputype.obj
 
 APP1TARGET= $(TARGET)
-APP1RPATH=SDK
+APP1RPATH=SDKBIN
 APP1OBJS=   $(OBJFILES)
 
 APP1DEPN= $(OUT)$/lib$/$(CODEMAKERLIBDEPN) $(OUT)$/lib$/$(COMMONCPPLIBDEPN)

codemaker/source/javamaker/makefile.mk
@@ -56,7 +56,7 @@ OBJFILES = \
     $(OBJ)$/javatype.obj
 
 APP1TARGET= $(TARGET)
-APP1RPATH=SDK
+APP1RPATH=SDKBIN
 APP1OBJS = $(OBJFILES)
 
 APP1DEPN= $(OUT)$/lib$/$(CODEMAKERLIBDEPN) $(OUT)$/lib$/$(COMMONJAVALIBDEPN)

registry/tools/makefile.mk
@@ -62,7 +62,7 @@ APP2STDLIBS=\
 
 APP3TARGET= regcompare
 APP3OBJS=   $(OBJ)$/regcompare.obj $(OBJ)/fileurl.obj $(OBJ)/options.obj
-APP3RPATH=  SDK
+APP3RPATH=  SDKBIN
 
 APP3STDLIBS=\
             $(SALLIB) \

solenv/bin/macosx-change-install-names.pl
@@ -71,7 +71,7 @@ sub action($$$)
     return $act;
 }
 
-@ARGV >= 2 or die 'Usage: app|shl UREBIN|URELIB|OOO|SDK|OXT|NONE <filepath>*';
+@ARGV >= 2 or die 'Usage: app|shl UREBIN|URELIB|OOO|SDKBIN|OXT|NONE <filepath>*';
 $type = shift @ARGV;
 $loc = shift @ARGV;
 if ($type eq "Executable")

unodevtools/source/skeletonmaker/makefile.mk
@@ -44,7 +44,7 @@ all:
 .INCLUDE : $(PRJ)$/unodevtools.pmk
 
 APP1TARGET = $(TARGET)
-APP1RPATH=SDK
+APP1RPATH=SDKBIN
 
 APP1OBJS = $(OBJ)$/skeletonmaker.obj \
     $(OBJ)$/skeletoncommon.obj \