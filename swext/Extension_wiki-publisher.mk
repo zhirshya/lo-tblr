LibreOffice/core

commit e7a1e17c4fdea4103e7f921e8cd9c315c1dbe612

convert swext to gbuild and add to tail_build

swext/Extension_wiki-publisher.mk
@@ -1,0 +1,62 @@
+#
+# Version: MPL 1.1 / GPLv3+ / LGPLv3+
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License or as specified alternatively below. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Initial Developer of the Original Code is
+# 	Peter Foley <pefoley2@verizon.net>
+# Portions created by the Initial Developer are Copyright (C) 2011 the
+# Initial Developer. All Rights Reserved.
+#
+# Major Contributor(s):
+#
+# For minor contributions see the git repository.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 3 or later (the "GPLv3+"), or
+# the GNU Lesser General Public License Version 3 or later (the "LGPLv3+"),
+# in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
+# instead of those above.
+#
+
+WIKISRC := $(SRCDIR)/swext/mediawiki
+
+$(eval $(call gb_Extension_Extension,wiki-publisher,swext/mediawiki/src))
+
+$(eval $(call gb_Extension_add_file,wiki-publisher,commons-codec-1.3.jar,$(OUTDIR)/bin/commons-codec-1.3.jar)) 
+$(eval $(call gb_Extension_add_file,wiki-publisher,commons-httpclient-3.1.jar,$(OUTDIR)/bin/commons-httpclient-3.1.jar)) 
+$(eval $(call gb_Extension_add_file,wiki-publisher,commons-lang-2.3.jar,$(OUTDIR)/bin/commons-lang-2.3.jar)) 
+$(eval $(call gb_Extension_add_file,wiki-publisher,commons-logging-1.1.1.jar,$(OUTDIR)/bin/commons-logging-1.1.1.jar)) 
+$(eval $(call gb_Extension_add_file,wiki-publisher,Addons.xcu,$(WIKISRC)/src/registry/data/org/openoffice/Office/Addons.xcu)) 
+$(eval $(call gb_Extension_add_file,wiki-publisher,Filter.xcu,$(WIKISRC)/src/registry/data/org/openoffice/TypeDetection/Filter.xcu))
+$(eval $(call gb_Extension_add_file,wiki-publisher,OptionsDialog.xcu,$(WIKISRC)/src/registry/data/org/openoffice/Office/OptionsDialog.xcu))
+$(eval $(call gb_Extension_add_file,wiki-publisher,Paths.xcu,$(WIKISRC)/src/registry/data/org/openoffice/Office/Paths.xcu))
+$(eval $(call gb_Extension_add_file,wiki-publisher,ProtocolHandler.xcu,$(WIKISRC)/src/registry/data/org/openoffice/Office/ProtocolHandler.xcu))
+$(eval $(call gb_Extension_add_file,wiki-publisher,Types.xcu,$(WIKISRC)/src/registry/data/org/openoffice/TypeDetection/Types.xcu))
+$(eval $(call gb_Extension_add_file,wiki-publisher,WikiExtension.xcs,$(WIKISRC)/src/registry/schema/org/openoffice/Office/Custom/WikiExtension.xcs))
+$(eval $(call gb_Extension_add_file,wiki-publisher,WikiExtension.xcu,$(WIKISRC)/src/registry/data/org/openoffice/Office/Custom/WikiExtension.xcu))
+$(eval $(call gb_Extension_add_file,wiki-publisher,mediawiki.jar,$(OUTDIR)/bin/mediawiki.jar))
+$(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/EditSetting.xdl,$(WIKISRC)/dialogs/EditSetting.xdl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/Settings.xdl,$(WIKISRC)/dialogs/Settings.xdl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/Module1.xba,$(WIKISRC)/dialogs/Module1.xba))
+$(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/script.xlb,$(WIKISRC)/dialogs/script.xlb))
+$(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/dialog.xlb,$(WIKISRC)/dialogs/dialog.xlb))
+$(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/SendToMediaWiki.xdl,$(WIKISRC)/dialogs/SendToMediaWiki.xdl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,filter/odt2mediawiki.xsl,$(WIKISRC)/src/filter/odt2mediawiki.xsl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,component.txt,$(WIKISRC)/src/component.txt))
+$(eval $(call gb_Extension_add_file,wiki-publisher,help/en-US/com.sun.wiki-publisher/wikisend.xhp,$(WIKISRC)/help/wikisend.xhp))
+$(eval $(call gb_Extension_add_file,wiki-publisher,help/en-US/com.sun.wiki-publisher/wikiformats.xhp,$(WIKISRC)/help/wikiformats.xhp))
+$(eval $(call gb_Extension_add_file,wiki-publisher,help/en-US/com.sun.wiki-publisher/wikiaccount.xhp,$(WIKISRC)/help/wikiaccount.xhp))
+$(eval $(call gb_Extension_add_file,wiki-publisher,help/en-US/com.sun.wiki-publisher/wiki.xhp,$(WIKISRC)/help/wiki.xhp))
+$(eval $(call gb_Extension_add_file,wiki-publisher,help/en-US/com.sun.wiki-publisher/wikisettings.xhp,$(WIKISRC)/help/wikisettings.xhp))
+$(eval $(call gb_Extension_add_file,wiki-publisher,license/THIRDPARTYLICENSEREADME.html,$(WIKISRC)/src/THIRDPARTYLICENSEREADME.html))
+$(eval $(call gb_Extension_add_file,wiki-publisher,templates/MediaWiki/mediawiki.ott,$(WIKISRC)/src/filter/mediawiki.ott))
+

Module_tail_build.mk
@@ -52,6 +52,7 @@ $(eval $(call gb_Module_add_moduledirs,tail_end,\
     slideshow \
     starmath \
     sw \
+    swext \
     unoxml \
     uui \
     vbahelper \

RepositoryModule_ooo.mk
@@ -80,6 +80,7 @@ $(eval $(call gb_Module_add_moduledirs,ooo,\
     svtools \
     svx \
     sw \
+    swext \
     twain \
     toolkit \
     tools \

postprocess/prj/build.lst
@@ -1,4 +1,4 @@
-po      postprocess     ::      accessibility BINFILTER:binfilter configmgr CRASHREP:crashrep CT2N:ct2n dtrans embeddedobj embedserv EPM:epm DESKTOP:extensions extras fpicker HELP:helpcontent2 io LIBRSVG:librsvg lingucomponent ODK:odk officecfg package psprint_config remotebridges scaddins scp2 DESKTOP:setup_native sysui testtools ucb UnoControls ure wizards xmerge MORE_FONTS:more_fonts DICTIONARIES:dictionaries PYUNO:pyuno readlicense_oo DESKTOP:unodevtools JFREEREPORT:jfreereport REPORTBUILDER:reportbuilder LANGUAGETOOL:languagetool SDEXT:sdext SWEXT:swext MYSQLC:mysqlc NLPSOLVER:nlpsolver STLPORT:stlport LIBXSLT:libxslt tail_build NULL
+po      postprocess     ::      accessibility BINFILTER:binfilter configmgr CRASHREP:crashrep CT2N:ct2n dtrans embeddedobj embedserv EPM:epm DESKTOP:extensions extras fpicker HELP:helpcontent2 io LIBRSVG:librsvg lingucomponent ODK:odk officecfg package psprint_config remotebridges scaddins scp2 DESKTOP:setup_native sysui testtools ucb UnoControls ure wizards xmerge MORE_FONTS:more_fonts DICTIONARIES:dictionaries PYUNO:pyuno readlicense_oo DESKTOP:unodevtools JFREEREPORT:jfreereport REPORTBUILDER:reportbuilder LANGUAGETOOL:languagetool SDEXT:sdext MYSQLC:mysqlc NLPSOLVER:nlpsolver STLPORT:stlport LIBXSLT:libxslt tail_build NULL
 po	postprocess			    	usr1	-	all	po_mkout NULL
 po	postprocess\checkxml		nmake	-	all	po_checkxml NULL
 po	postprocess\checkdeliver	nmake	-	all	po_checkdlv NULL

swext/Configuration_mediawiki.mk
@@ -1,0 +1,50 @@
+#
+# Version: MPL 1.1 / GPLv3+ / LGPLv3+
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License or as specified alternatively below. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Initial Developer of the Original Code is
+# 	Peter Foley <pefoley2@verizon.net>
+# Portions created by the Initial Developer are Copyright (C) 2011 the
+# Initial Developer. All Rights Reserved.
+#
+# Major Contributor(s):
+#
+# For minor contributions see the git repository.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 3 or later (the "GPLv3+"), or
+# the GNU Lesser General Public License Version 3 or later (the "LGPLv3+"),
+# in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
+# instead of those above.
+#
+
+$(eval $(call gb_Configuration_Configuration,mediawiki,SRCDIR))
+
+$(eval $(call gb_Configuration_add_datas,mediawiki,swext/mediawiki/src/registry/data,\
+	org/openoffice/Office/Addons.xcu \
+	org/openoffice/Office/OptionsDialog.xcu \
+	org/openoffice/Office/ProtocolHandler.xcu \
+	org/openoffice/Office/Paths.xcu \
+	org/openoffice/Office/Custom/WikiExtension.xcu \
+	org/openoffice/TypeDetection/Filter.xcu \
+	org/openoffice/TypeDetection/Types.xcu \
+))
+
+$(eval $(call gb_Configuration_add_localized_datas,mediawiki,swext/mediawiki/src/registry/data,\
+	org/openoffice/Office/Addons.xcu \
+	org/openoffice/Office/OptionsDialog.xcu \
+	org/openoffice/Office/Custom/WikiExtension.xcu \
+))
+
+$(eval $(call gb_Configuration_add_schemas,mediawiki,swext/mediawiki/src/registry/schema,\
+	org/openoffice/Office/Custom/WikiExtension.xcs \
+))	

swext/Jar_mediawiki.mk
@@ -1,0 +1,72 @@
+#
+# Version: MPL 1.1 / GPLv3+ / LGPLv3+
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License or as specified alternatively below. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Initial Developer of the Original Code is
+# 	Peter Foley <pefoley2@verizon.net>
+# Portions created by the Initial Developer are Copyright (C) 2011 the
+# Initial Developer. All Rights Reserved.
+#
+# Major Contributor(s):
+#
+# For minor contributions see the git repository.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 3 or later (the "GPLv3+"), or
+# the GNU Lesser General Public License Version 3 or later (the "LGPLv3+"),
+# in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
+# instead of those above.
+#
+
+$(eval $(call gb_Jar_Jar,mediawiki,SRCDIR))
+
+$(eval $(call gb_Jar_set_packageroot,mediawiki,com))
+
+$(eval $(call gb_Jar_set_manifest,mediawiki,$(SRCDIR)/swext/mediawiki/src/com/sun/star/wiki/MANIFEST.MF))
+
+$(eval $(call gb_Jar_set_jarclasspath,mediawiki,\
+	commons-codec-1.3.jar \
+	commons-lang-2.3.jar \
+	commons-httpclient-3.1.jar \
+	commons-logging-1.1.1.jar \
+))
+
+$(eval $(call gb_Jar_add_jars,mediawiki,\
+	$(OUTDIR)/bin/juh.jar \
+	$(OUTDIR)/bin/jurt.jar \
+	$(OUTDIR)/bin/ridl.jar \
+	$(OUTDIR)/bin/unoil.jar \
+))
+
+ifneq ($(SYSTEM_APACHE_COMMONS),YES)
+$(eval $(call gb_Jar_add_jars,mediawiki,\
+	$(OUTDIR)/bin/commons-codec-1.3.jar \
+	$(OUTDIR)/bin/commons-lang-2.3.jar \
+	$(OUTDIR)/bin/commons-httpclient-3.1.jar \
+	$(OUTDIR)/bin/commons-logging-1.1.1.jar \
+))
+endif
+
+$(eval $(call gb_Jar_add_sourcefiles,mediawiki,\
+	swext/mediawiki/src/com/sun/star/wiki/EditPageParser \
+	swext/mediawiki/src/com/sun/star/wiki/Helper \
+	swext/mediawiki/src/com/sun/star/wiki/MainThreadDialogExecutor \
+	swext/mediawiki/src/com/sun/star/wiki/Settings \
+	swext/mediawiki/src/com/sun/star/wiki/WikiArticle \
+	swext/mediawiki/src/com/sun/star/wiki/WikiCancelException \
+	swext/mediawiki/src/com/sun/star/wiki/WikiDialog \
+	swext/mediawiki/src/com/sun/star/wiki/WikiEditorImpl \
+	swext/mediawiki/src/com/sun/star/wiki/WikiEditSettingDialog \
+	swext/mediawiki/src/com/sun/star/wiki/WikiOptionsEventHandlerImpl \
+	swext/mediawiki/src/com/sun/star/wiki/WikiPropDialog \
+	swext/mediawiki/src/com/sun/star/wiki/WikiProtocolSocketFactory \
+))

swext/Makefile
@@ -1,8 +1,8 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
-# Copyright 2000, 2010 Oracle and/or its affiliates.
+#
+# Copyright 2000, 2011 Oracle and/or its affiliates.
 #
 # OpenOffice.org - a multi-platform office productivity suite
 #
@@ -25,22 +25,14 @@
 #
 #*************************************************************************
 
-PRJ=..$/..$/..$/..$/..$/..$/..
-PRJNAME=swext
-CUSTID=mediawiki
-TARGET=mediawiki_registry_data_typedetection
-PACKAGE=org.openoffice.TypeDetection
+ifeq ($(strip $(SOLARENV)),)
+$(error No environment set!)
+endif
 
-.INCLUDE : settings.mk
-.INCLUDE : ${PRJ}$/makefile.pmk
+gb_PARTIALBUILD := T
+GBUILDDIR := $(SOLARENV)/gbuild
+include $(GBUILDDIR)/gbuild.mk
 
-.IF "$(ENABLE_MEDIAWIKI)" == "YES"
-LOCALIZEDFILES=
+$(eval $(call gb_Module_make_global_targets,$(shell ls $(dir $(realpath $(firstword $(MAKEFILE_LIST))))/Module*.mk)))
 
-MODULEFILES=
-
-XCUFILES = Filter.xcu Types.xcu
-.ENDIF
-
-.INCLUDE : target.mk
-
+# vim: set noet sw=4 ts=4:

swext/Module_swext.mk
@@ -1,0 +1,39 @@
+#
+# Version: MPL 1.1 / GPLv3+ / LGPLv3+
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License or as specified alternatively below. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Initial Developer of the Original Code is
+# 	Peter Foley <pefoley2@verizon.net>
+# Portions created by the Initial Developer are Copyright (C) 2011 the
+# Initial Developer. All Rights Reserved.
+#
+# Major Contributor(s):
+#
+# For minor contributions see the git repository.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 3 or later (the "GPLv3+"), or
+# the GNU Lesser General Public License Version 3 or later (the "LGPLv3+"),
+# in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
+# instead of those above.
+#
+
+$(eval $(call gb_Module_Module,swext))
+
+ifeq ($(ENABLE_MEDIAWIKI),YES)
+$(eval $(call gb_Module_add_targets,swext,\
+	Jar_mediawiki \
+	Configuration_mediawiki \
+	Zip_mediawiki_develop \
+	Extension_wiki-publisher \
+))
+endif

zhirshya/lo-tblr

commit 02a0173e819af4f1372798a90763f030b453e25e

fdo#46509: xsltml .xsl files missing from wiki-publisher.oxt
A regression introduced by e7a1e17c4fdea4103e7f921e8cd9c315c1dbe612 "convert
swext to gbuild and add to tail_build."

Change-Id: I29efe28ad6353d725afe9cce6f470c5492aa87a8

swext/Extension_wiki-publisher.mk
@@ -53,6 +53,13 @@ $(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/Module1.xba,$(WIKI
 $(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/script.xlb,$(WIKISRC)/dialogs/script.xlb))
 $(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/dialog.xlb,$(WIKISRC)/dialogs/dialog.xlb))
 $(eval $(call gb_Extension_add_file,wiki-publisher,WikiEditor/SendToMediaWiki.xdl,$(WIKISRC)/dialogs/SendToMediaWiki.xdl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,filter/math/cmarkup.xsl,$(OUTDIR)/bin/xslt/export/xsltml/cmarkup.xsl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,filter/math/entities.xsl,$(OUTDIR)/bin/xslt/export/xsltml/entities.xsl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,filter/math/glayout.xsl,$(OUTDIR)/bin/xslt/export/xsltml/glayout.xsl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,filter/math/mmltex.xsl,$(OUTDIR)/bin/xslt/export/xsltml/mmltex.xsl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,filter/math/scripts.xsl,$(OUTDIR)/bin/xslt/export/xsltml/scripts.xsl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,filter/math/tables.xsl,$(OUTDIR)/bin/xslt/export/xsltml/tables.xsl))
+$(eval $(call gb_Extension_add_file,wiki-publisher,filter/math/tokens.xsl,$(OUTDIR)/bin/xslt/export/xsltml/tokens.xsl))
 $(eval $(call gb_Extension_add_file,wiki-publisher,filter/odt2mediawiki.xsl,$(WIKISRC)/src/filter/odt2mediawiki.xsl))
 $(eval $(call gb_Extension_add_file,wiki-publisher,help/en-US/com.sun.wiki-publisher/wikisend.xhp,$(WIKISRC)/help/wikisend.xhp))
 $(eval $(call gb_Extension_add_file,wiki-publisher,help/en-US/com.sun.wiki-publisher/wikiformats.xhp,$(WIKISRC)/help/wikiformats.xhp))