LibreOffice/core

commit 227113bbf16f2feda4d3e41bb9aff7dc100ede17

Slidesorter: Kill more useless cruft.

sd/source/ui/slidesorter/controller/SlsClipboard.cxx
@@ -35,6 +35,7 @@
 #include "model/SlsPageDescriptor.hxx"
 #include "model/SlsPageEnumerationProvider.hxx"
 #include "view/SlideSorterView.hxx"
+#include "view/SlsResource.hrc"
 #include "view/SlsTheme.hxx"
 #include "controller/SlideSorterController.hxx"
 #include "controller/SlsInsertionIndicatorHandler.hxx"
@@ -120,17 +121,16 @@ class Clipboard::UndoContext
 public:
     UndoContext (
         SdDrawDocument* pDocument,
-        const ::boost::shared_ptr<ViewShell>& rpMainViewShell,
-        const ::boost::shared_ptr<view::Theme>& rpTheme)
+        const ::boost::shared_ptr<ViewShell>& rpMainViewShell)
         : mpDocument(pDocument),
           mpMainViewShell(rpMainViewShell)
     {
         if (mpDocument!=NULL && mpDocument->IsUndoEnabled())
         {
             if (mpMainViewShell && mpMainViewShell->GetShellType() == ViewShell::ST_DRAW)
-                mpDocument->BegUndo(rpTheme->GetString(view::Theme::String_DragAndDropPages));
+                mpDocument->BegUndo(String(SdResId(STRING_DRAG_AND_DROP_PAGES)));
             else
-                mpDocument->BegUndo(rpTheme->GetString(view::Theme::String_DragAndDropSlides));
+                mpDocument->BegUndo(String(SdResId(STRING_DRAG_AND_DROP_SLIDES)));
         }
     }
 
@@ -714,8 +714,7 @@ sal_Int8 Clipboard::ExecuteDrop (
                 // Handle a general drop operation.
                 mpUndoContext.reset(new UndoContext (
                     mrSlideSorter.GetModel().GetDocument(),
-                    mrSlideSorter.GetViewShell()->GetViewShellBase().GetMainViewShell(),
-                    mrSlideSorter.GetTheme()));
+                    mrSlideSorter.GetViewShell()->GetViewShellBase().GetMainViewShell()));
                 mpSelectionObserverContext.reset(new SelectionObserver::Context(mrSlideSorter));
 
                 HandlePageDrop(*pDragTransferable);

sd/source/ui/slidesorter/view/SlsButtonBar.cxx
@@ -30,6 +30,7 @@
 #include "view/SlsButtonBar.hxx"
 
 #include "SlideSorter.hxx"
+#include "SlsResource.hxx"
 #include "model/SlsPageDescriptor.hxx"
 #include "model/SlideSorterModel.hxx"
 #include "view/SlsTheme.hxx"
@@ -1070,7 +1071,7 @@ UnhideButton::UnhideButton (SlideSorter& rSlideSorter)
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2BMediumHover),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2BSmall),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2BSmallHover),
-        rSlideSorter.GetTheme()->GetString(Theme::String_Command2B))
+        String(SdResId(STRING_COMMAND2_B)))
 {
 }
 
@@ -1102,7 +1103,7 @@ StartShowButton::StartShowButton (SlideSorter& rSlideSorter)
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command1MediumHover),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command1Small),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command1SmallHover),
-        rSlideSorter.GetTheme()->GetString(Theme::String_Command1))
+        String(SdResId(STRING_COMMAND1)))
 {
 }
 
@@ -1168,7 +1169,7 @@ HideButton::HideButton (SlideSorter& rSlideSorter)
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2MediumHover),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2Small),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2SmallHover),
-        rSlideSorter.GetTheme()->GetString(Theme::String_Command2))
+        String(SdResId(STRING_COMMAND2_A)))
 {
 }
 
@@ -1200,7 +1201,7 @@ DuplicateButton::DuplicateButton (SlideSorter& rSlideSorter)
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command3MediumHover),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command3Small),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command3SmallHover),
-        rSlideSorter.GetTheme()->GetString(Theme::String_Command3))
+        String(SdResId(STRING_COMMAND3)))
 {
 }
 

sd/source/ui/slidesorter/inc/view/SlsPageObjectPainter.hxx
@@ -96,7 +96,6 @@ private:
     Bitmap maMouseOverBackground;
     Bitmap maMouseOverFocusedBackground;
     Bitmap maMouseOverSelectedAndFocusedBackground;
-    ::rtl::OUString msUnhideString;
     ButtonBar& mrButtonBar;
     Size maSize;
 

sd/source/ui/slidesorter/inc/view/SlsTheme.hxx
@@ -164,19 +164,6 @@ public:
     };
     const BitmapEx& GetIcon (const IconType eType);
 
-    enum StringType
-    {
-        String_Unhide,
-        String_DragAndDropPages,
-        String_DragAndDropSlides,
-        String_Command1,
-        String_Command2,
-        String_Command2B,
-        String_Command3,
-        _StringType_Size_
-    };
-    ::rtl::OUString GetString (const StringType eType) const;
-
 private:
     bool mbIsHighContrastMode;
     class GradientDescriptor
@@ -202,7 +189,6 @@ private:
     ::std::vector<GradientDescriptor> maGradients;
     ::std::vector<BitmapEx> maIcons;
     ::std::vector<ColorData> maColor;
-    ::std::vector<rtl::OUString> maStrings;
 
     GradientDescriptor& GetGradient (const GradientColorType eType);
     /** Guarded initialization of the specified icon in the maIcons

sd/source/ui/slidesorter/view/SlsPageObjectPainter.cxx
@@ -67,7 +67,6 @@ PageObjectPainter::PageObjectPainter (
       maFocusedSelectionBackground(),
       maMouseOverBackground(),
       maMouseOverFocusedBackground(),
-      msUnhideString(mpTheme->GetString(Theme::String_Unhide)),
       mrButtonBar(rSlideSorter.GetView().GetButtonBar()),
       maSize()
 {

sd/source/ui/slidesorter/view/SlsTheme.cxx
@@ -83,14 +83,6 @@ Theme::Theme (const ::boost::shared_ptr<controller::Properties>& rpProperties)
     {
         LocalResource aResource (RID_SLIDESORTER_ICONS);
 
-        maStrings.resize(_StringType_Size_);
-        maStrings[String_DragAndDropPages] = String(SdResId(STRING_DRAG_AND_DROP_PAGES));
-        maStrings[String_DragAndDropSlides] = String(SdResId(STRING_DRAG_AND_DROP_SLIDES));
-        maStrings[String_Command1] = String(SdResId(STRING_COMMAND1));
-        maStrings[String_Command2] = String(SdResId(STRING_COMMAND2_A));
-        maStrings[String_Command2B] = String(SdResId(STRING_COMMAND2_B));
-        maStrings[String_Command3] = String(SdResId(STRING_COMMAND3));
-
         maColor.resize(_ColorType_Size_);
         maColor[Color_Background] = maBackgroundColor;
         maColor[Color_PageNumberDefault] = 0x0808080;
@@ -346,20 +338,6 @@ const BitmapEx& Theme::GetIcon (const IconType eType)
     {
         OSL_ASSERT(eType>=0 && size_t(eType)<maIcons.size());
         return maIcons[0];
-    }
-}
-
-
-
-
-::rtl::OUString Theme::GetString (const StringType eType) const
-{
-    if (eType>=0 && size_t(eType)<maStrings.size())
-        return maStrings[eType];
-    else
-    {
-        OSL_ASSERT(eType>=0 && size_t(eType)<maStrings.size());
-        return ::rtl::OUString();
     }
 }
 

zhirshya/lo-tblr

commit 327c6af2340978205cc6f6f1f860414bbd700ce1

fix use of local resource string ids in global context
possibly a regression since 227113bbf16f2feda4d3e41bb9aff7dc100ede17

Take the opportunity to give them more useful names while I'm at it

Change-Id: I175b656ac611b78ebaa21b53778085113632095e

sd/source/ui/app/strings.src
@@ -1269,3 +1269,33 @@ String STR_INSERT_MOVIE
 {
      Text [ en-US ] = "Insert Movie";
 };
+
+String STRING_DRAG_AND_DROP_PAGES
+{
+     Text [ en-US ] = "Drag and Drop Pages" ;
+};
+
+String STRING_DRAG_AND_DROP_SLIDES
+{
+     Text [ en-US ] = "Drag and Drop Slides" ;
+};
+
+String STRING_START_SLIDESHOW
+{
+     Text [ en-US ] = "Start Slide Show" ;
+};
+
+String STRING_HIDE_SLIDE
+{
+     Text [ en-US ] = "Hide Slide" ;
+};
+
+String STRING_SHOW_SLIDE
+{
+     Text [ en-US ] = "Show Slide" ;
+};
+
+String STRING_DUPLICATE_SLIDE
+{
+     Text [ en-US ] = "Duplicate Slide" ;
+};

sd/source/ui/inc/strings.hrc
@@ -500,15 +500,21 @@
 #define STR_NAVIGATOR_SHOW_NAMED_SHAPES                 (RID_APP_START+718)
 #define STR_NAVIGATOR_SHOW_ALL_SHAPES                   (RID_APP_START+719)
 #define STR_NAVIGATOR_SHAPE_BASE_NAME                   (RID_APP_START+720)
-//IAccessibility2 Implementation 2009-----
-#define STR_ACC_DIALOG_DESC             (RID_APP_START + 730)
-//-----IAccessibility2 Implementation 2009
 
 #define STR_RESET_LAYOUT                                (RID_APP_START+721)
 #define STR_INSERT_TABLE                                (RID_APP_START+722)
 #define STR_INSERT_CHART                                (RID_APP_START+723)
 #define STR_INSERT_PICTURE                              (RID_APP_START+724)
 #define STR_INSERT_MOVIE                                (RID_APP_START+725)
+
+#define STRING_DRAG_AND_DROP_PAGES			(RID_APP_START+726)
+#define STRING_DRAG_AND_DROP_SLIDES			(RID_APP_START+727)
+#define STRING_START_SLIDESHOW				(RID_APP_START+728)
+#define STRING_HIDE_SLIDE				(RID_APP_START+729)
+#define STRING_SHOW_SLIDE				(RID_APP_START+730)
+#define STRING_DUPLICATE_SLIDE				(RID_APP_START+731)
+
+#define STR_ACC_DIALOG_DESC             		(RID_APP_START+732)
 
 /******************************************************************************
 * The ids in glob.hrc start at RID_APP_START+750!

sd/source/ui/slidesorter/controller/SlsClipboard.cxx
@@ -35,7 +35,6 @@
 #include "model/SlsPageDescriptor.hxx"
 #include "model/SlsPageEnumerationProvider.hxx"
 #include "view/SlideSorterView.hxx"
-#include "view/SlsResource.hrc"
 #include "view/SlsTheme.hxx"
 #include "controller/SlideSorterController.hxx"
 #include "controller/SlsInsertionIndicatorHandler.hxx"

sd/source/ui/slidesorter/inc/view/SlsResource.hrc
@@ -100,12 +100,4 @@
 #define IMAGE_HIDE_SLIDE_OVERLAY        92
 #define IMAGE_FOCUS_BORDER              93
 
-#define STRING_DRAG_AND_DROP_PAGES      101
-#define STRING_DRAG_AND_DROP_SLIDES     102
-
-#define STRING_COMMAND1                 110
-#define STRING_COMMAND2_A               111
-#define STRING_COMMAND2_B               112
-#define STRING_COMMAND3                 113
-
 #endif

sd/source/ui/slidesorter/view/SlsButtonBar.cxx
@@ -43,6 +43,7 @@
 #include "controller/SlsAnimator.hxx"
 #include "controller/SlsAnimationFunction.hxx"
 #include "app.hrc"
+#include "strings.hrc"
 #include "drawdoc.hxx"
 #include "sdmod.hxx"
 #include "optsitem.hxx"
@@ -1064,7 +1065,7 @@ UnhideButton::UnhideButton (SlideSorter& rSlideSorter)
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2BMediumHover),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2BSmall),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2BSmallHover),
-        String(SdResId(STRING_COMMAND2_B)))
+        String(SdResId(STRING_SHOW_SLIDE)))
 {
 }
 
@@ -1096,7 +1097,7 @@ StartShowButton::StartShowButton (SlideSorter& rSlideSorter)
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command1MediumHover),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command1Small),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command1SmallHover),
-        String(SdResId(STRING_COMMAND1)))
+        String(SdResId(STRING_START_SLIDESHOW)))
 {
 }
 
@@ -1162,7 +1163,7 @@ HideButton::HideButton (SlideSorter& rSlideSorter)
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2MediumHover),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2Small),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command2SmallHover),
-        String(SdResId(STRING_COMMAND2_A)))
+        String(SdResId(STRING_HIDE_SLIDE)))
 {
 }
 
@@ -1194,7 +1195,7 @@ DuplicateButton::DuplicateButton (SlideSorter& rSlideSorter)
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command3MediumHover),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command3Small),
         rSlideSorter.GetTheme()->GetIcon(Theme::Icon_Command3SmallHover),
-        String(SdResId(STRING_COMMAND3)))
+        String(SdResId(STRING_DUPLICATE_SLIDE)))
 {
 }
 

sd/source/ui/slidesorter/view/SlsResource.src
@@ -280,35 +280,4 @@ Resource RID_SLIDESORTER_ICONS
     {
         ImageBitmap = Bitmap { File = "slide_sorter_focus_border.png" ; };
     };
-
-
-    String STRING_DRAG_AND_DROP_PAGES
-    {
-        Text [ en-US ] = "Drag and Drop Pages" ;
-    };
-
-    String STRING_DRAG_AND_DROP_SLIDES
-    {
-        Text [ en-US ] = "Drag and Drop Slides" ;
-    };
-
-    String STRING_COMMAND1
-    {
-        Text [ en-US ] = "Start Slide Show" ;
-    };
-
-    String STRING_COMMAND2_A
-    {
-        Text [ en-US ] = "Hide Slide" ;
-    };
-
-    String STRING_COMMAND2_B
-    {
-        Text [ en-US ] = "Show Slide" ;
-    };
-
-    String STRING_COMMAND3
-    {
-        Text [ en-US ] = "Duplicate Slide" ;
-    };
 };