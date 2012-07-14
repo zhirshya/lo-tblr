LibreOffice/core

commit 0fcd1a73f0e1ec564f3c6da1ccd890183d3c18db

fdo#46808, Adapt UNO services to new style, Part 7, updating ::create
Update code to use factory method URLTransformer::create

Change-Id: I3fd2e838497bcfd8fc949615c0e7d60a6ea47118
Signed-off-by: Stephan Bergmann <sbergman@redhat.com>, added some tweaks.

framework/source/lomenubar/FrameHelper.cxx
@@ -56,6 +56,7 @@
 #include <com/sun/star/lang/SystemDependent.hpp>
 #include <com/sun/star/beans/PropertyValue.hpp>
 #include <com/sun/star/util/URL.hpp>
+#include <com/sun/star/util/URLTransformer.hpp>
 #include <com/sun/star/util/XURLTransformer.hpp>
 #include <com/sun/star/ui/XUIElement.hpp>
 #include <com/sun/star/ui/XUIConfigurationManager.hpp>
@@ -260,7 +261,7 @@ FrameHelper::FrameHelper(const Reference< XMultiServiceFactory >&  rServiceManag
     : m_xStatusListener(new MenuItemStatusListener(this))
     , m_pDispatchRegistry(new framework::lomenubar::DispatchRegistry(m_xStatusListener))
     , m_xMSF(rServiceManager)
-    , m_xTrans(m_xMSF->createInstance( rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.util.URLTransformer" ))), UNO_QUERY)
+    , m_xTrans(util::URLTransformer::create(m_xMSF))
     , m_xMM(m_xMSF->createInstance(OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.frame.ModuleManager"))),UNO_QUERY)
     , m_xPCF(m_xMSF->createInstance(OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.frame.PopupMenuControllerFactory"))), UNO_QUERY)
     , m_xFrame(xFrame)

zhirshya/lo-tblr

commit 8018ca2e4585f27094095980fcdded92c4e7a1b9

fix breaker introduced by 0fcd1a73f0e1ec564f3c6da1ccd890183d3c18db
Change-Id: I5e60c2d157e53831fb8239ca6d833aaca6735994

framework/Library_lomenubar.mk
@@ -42,6 +42,7 @@ $(eval $(call gb_Library_use_libraries,lomenubar,\
 	sal \
 	cppu \
 	cppuhelper \
+    comphelper \
 	X11 \
 	$(gb_STDLIBS) \
 ))

framework/source/lomenubar/FrameHelper.cxx
@@ -64,6 +64,7 @@
 #include <com/sun/star/ui/XAcceleratorConfiguration.hpp>
 #include <com/sun/star/ui/XModuleUIConfigurationManagerSupplier.hpp>
 #include <rtl/process.h>
+#include <comphelper/componentcontext.hxx>
 
 #include <gio/gio.h>
 //#pragma GCC diagnostic push
@@ -261,7 +262,7 @@ FrameHelper::FrameHelper(const Reference< XMultiServiceFactory >&  rServiceManag
     : m_xStatusListener(new MenuItemStatusListener(this))
     , m_pDispatchRegistry(new framework::lomenubar::DispatchRegistry(m_xStatusListener))
     , m_xMSF(rServiceManager)
-    , m_xTrans(util::URLTransformer::create(m_xMSF))
+    , m_xTrans(util::URLTransformer::create(comphelper::ComponentContext(m_xMSF).getUNOContext()))
     , m_xMM(m_xMSF->createInstance(OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.frame.ModuleManager"))),UNO_QUERY)
     , m_xPCF(m_xMSF->createInstance(OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.frame.PopupMenuControllerFactory"))), UNO_QUERY)
     , m_xFrame(xFrame)