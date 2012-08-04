LibreOffice/core

commit 50cf7caee5bc6d8e066580d13c72b40926fcb69a

convert SfxListenerArr_Impl from SvPtrarr to std::vector
Change-Id: I7f46731d880da30dc2dda011b31818437c955f2d

svl/inc/svl/brdcst.hxx
@@ -21,25 +21,22 @@
 
 #include "svl/svldllapi.h"
 #include <tools/rtti.hxx>
-#include <svl/svarray.hxx>
+#include <vector>
 
 class SfxListener;
 class SfxHint;
-
-#ifndef _SFX_BRDCST_CXX
-typedef SvPtrarr SfxListenerArr_Impl;
-#endif
 
 //-------------------------------------------------------------------------
 
 class SVL_DLLPUBLIC SfxBroadcaster
 {
-friend class SfxListener;
+    friend class SfxListener;
+    typedef std::vector<SfxListener*> SfxListenerArr_Impl;
 
     SfxListenerArr_Impl     aListeners;
 
 private:
-    sal_Bool         AddListener( SfxListener& rListener );
+    void                    AddListener( SfxListener& rListener );
     void                    RemoveListener( SfxListener& rListener );
     const SfxBroadcaster&   operator=(const SfxBroadcaster &); // verboten
 
@@ -55,10 +52,18 @@ public:
     virtual                 ~SfxBroadcaster();
 
     void                    Broadcast( const SfxHint &rHint );
-    sal_Bool                    HasListeners() const;
-    sal_uInt16                  GetListenerCount() const { return aListeners.Count(); }
+    bool                    HasListeners() const
+    {
+        return !aListeners.empty();
+    }
+    size_t                  GetListenerCount() const
+    {
+        return aListeners.size();
+    }
     SfxListener*            GetListener( sal_uInt16 nNo ) const
-                            { return (SfxListener*) aListeners[nNo]; }
+    {
+        return aListeners[nNo];
+    }
 };
 
 #endif

svl/source/notify/brdcst.cxx
@@ -25,10 +25,8 @@
 #include <svl/smplhint.hxx>
 #include <svl/lstner.hxx>
 
-SV_DECL_PTRARR( SfxListenerArr_Impl, SfxListener*, 0 )
-
-#define _SFX_BRDCST_CXX
 #include <svl/brdcst.hxx>
+#include <algorithm>
 
 //====================================================================
 DBG_NAME(SfxBroadcaster)
@@ -44,16 +42,11 @@ void SfxBroadcaster::Broadcast( const SfxHint &rHint )
 {
     DBG_CHKTHIS(SfxBroadcaster, 0);
 
-    // is anybody to notify?
-    if ( aListeners.Count() /*! || aGlobListeners.Count() */ )
+    // notify all registered listeners exactly once
+    for (size_t n = 0; n < aListeners.size(); ++n)
     {
-        // notify all registered listeners exactly once
-        for ( sal_uInt16 n = 0; n < aListeners.Count(); ++n )
-        {
-            SfxListener* pListener = aListeners[n];
-            if ( pListener )
-                pListener->Notify( *this, rHint );
-        }
+        SfxListener* pListener = aListeners[n];
+        pListener->Notify( *this, rHint );
     }
 }
 
@@ -66,11 +59,10 @@ SfxBroadcaster::~SfxBroadcaster()
     Broadcast( SfxSimpleHint(SFX_HINT_DYING) );
 
     // remove all still registered listeners
-    for ( sal_uInt16 nPos = 0; nPos < aListeners.Count(); ++nPos )
+    for (size_t nPos = 0; nPos < aListeners.size(); ++nPos)
     {
         SfxListener *pListener = aListeners[nPos];
-        if ( pListener )
-            pListener->RemoveBroadcaster_Impl(*this);
+        pListener->RemoveBroadcaster_Impl(*this);
     }
 }
 
@@ -92,11 +84,10 @@ SfxBroadcaster::SfxBroadcaster( const SfxBroadcaster &rBC )
 {
     DBG_CTOR(SfxBroadcaster, 0);
 
-    for ( sal_uInt16 n = 0; n < rBC.aListeners.Count(); ++n )
+    for (size_t n = 0; n < rBC.aListeners.size(); ++n)
     {
         SfxListener *pListener = rBC.aListeners[n];
-        if ( pListener )
-            pListener->StartListening( *this );
+        pListener->StartListening( *this );
     }
 }
 
@@ -104,25 +95,11 @@ SfxBroadcaster::SfxBroadcaster( const SfxBroadcaster &rBC )
 
 // add a new SfxListener to the list
 
-sal_Bool SfxBroadcaster::AddListener( SfxListener& rListener )
+void SfxBroadcaster::AddListener( SfxListener& rListener )
 {
     DBG_CHKTHIS(SfxBroadcaster, 0);
-    const SfxListener *pListener = &rListener;
-    const SfxListener *pNull = 0;
-    sal_uInt16 nFreePos = aListeners.GetPos( pNull );
-    if ( nFreePos < aListeners.Count() )
-        aListeners.GetData()[nFreePos] = pListener;
-    else if ( aListeners.Count() < (USHRT_MAX-1) )
-        aListeners.Insert( pListener, aListeners.Count() );
-    else
-    {
-        OSL_FAIL( "array overflow" );
-        return sal_False;
-    }
 
-    DBG_ASSERT( USHRT_MAX != aListeners.GetPos(pListener),
-                "AddListener failed" );
-    return sal_True;
+    aListeners.push_back(&rListener);
 }
 
 //--------------------------------------------------------------------
@@ -140,12 +117,10 @@ void SfxBroadcaster::ListenersGone()
 
 void SfxBroadcaster::Forward(SfxBroadcaster& rBC, const SfxHint& rHint)
 {
-    const sal_uInt16 nCount = aListeners.Count();
-    for ( sal_uInt16 i = 0; i < nCount; ++i )
+    for (size_t i = 0; i < aListeners.size(); ++i)
     {
         SfxListener *pListener = aListeners[i];
-        if ( pListener )
-            pListener->Notify( rBC, rHint );
+        pListener->Notify( rBC, rHint );
     }
 }
 
@@ -157,23 +132,13 @@ void SfxBroadcaster::RemoveListener( SfxListener& rListener )
 {
     {DBG_CHKTHIS(SfxBroadcaster, 0);}
     const SfxListener *pListener = &rListener;
-    sal_uInt16 nPos = aListeners.GetPos(pListener);
-    DBG_ASSERT( nPos != USHRT_MAX, "RemoveListener: Listener unknown" );
-    aListeners.GetData()[nPos] = 0;
+
+    SfxListenerArr_Impl::iterator aIter = std::remove(aListeners.begin(), aListeners.end(), pListener);
+    DBG_ASSERT( aIter != aListeners.end(), "RemoveListener: Listener unknown" );
+    aListeners.erase(aIter, aListeners.end());
+
     if ( !HasListeners() )
         ListenersGone();
 }
-
-//--------------------------------------------------------------------
-
-sal_Bool SfxBroadcaster::HasListeners() const
-{
-    for ( sal_uInt16 n = 0; n < aListeners.Count(); ++n )
-        if ( aListeners.GetObject(n) != 0 )
-            return sal_True;
-    return sal_False;
-}
-
-//--------------------------------------------------------------------
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

svl/source/notify/lstner.cxx
@@ -85,15 +85,13 @@ sal_Bool SfxListener::StartListening( SfxBroadcaster& rBroadcaster, sal_Bool bPr
 
     if ( !bPreventDups || !IsListening( rBroadcaster ) )
     {
-        if ( rBroadcaster.AddListener(*this) )
-        {
-            aBCs.push_back( &rBroadcaster );
+        rBroadcaster.AddListener(*this);
+        aBCs.push_back( &rBroadcaster );
 
-            DBG_ASSERT( IsListening(rBroadcaster), "StartListening failed" );
-            return sal_True;
-        }
-
+        DBG_ASSERT( IsListening(rBroadcaster), "StartListening failed" );
+        return sal_True;
     }
+
     return sal_False;
 }
 

zhirshya/lo-tblr

commit 442b615c50c9d9cab7e82d686c69f9fd60b632b6

SfxBroadcaster: fix STL conversion:
50cf7caee5bc6d8e066580d13c72b40926fcb69a introduced a regression that
makes the smoketest fail on Windows (though strangely not on Linux),
because it removed the special handling of null pointers in the
SfxBroadcaster's listener array.  It is evidently possible that
a listener may be either added or removed at the SfxBroadcaster
while that SfxBroadcaster is currently doing a Broadcast, hence
AddListener and RemoveListener must be careful not to make the in
progress iteration through the listener array fail.

svllo.dll!SfxBroadcaster::AddListener(SfxListener & rListener={...}) Line 114 + 0x43 bytes   C++
svllo.dll!SfxListener::StartListening(SfxBroadcaster & rBroadcaster={...}, unsigned char bPreventDups=0)  Line 89  C++
sfxlo.dll!SfxPrintHelper::initialize(const com::sun::star::uno::Sequence<com::sun::star::uno::Any> & aArguments={...})  Line 162 C++
sfxlo.dll!SfxBaseModel::impl_getPrintHelper()  Line 3693    C++
sfxlo.dll!SfxBaseModel::Notify(SfxBroadcaster & rBC={...}, const SfxHint & rHint={...})  Line 2583   C++
svllo.dll!SfxBroadcaster::Broadcast(const SfxHint & rHint={...}) Line 56 + 0xb bytes C++
sfxlo.dll!SfxApplication::NotifyEvent(const SfxEventHint & rEventHint={...}, bool bSynchron=true)  Line 918    C++
sfxlo.dll!SfxObjectShell::SetInitialized_Impl(const bool i_fromInitNew=true)  Line 1146 + 0x37 bytes C++
sfxlo.dll!SfxObjectShell::FinishedLoading(unsigned short nFlags=3) Line 1302   C++

Change-Id: Id27d08faaa2472b57947960a92f19c44df55b72b

svl/inc/svl/brdcst.hxx
@@ -33,7 +33,7 @@ class SVL_DLLPUBLIC SfxBroadcaster
     friend class SfxListener;
     typedef std::vector<SfxListener*> SfxListenerArr_Impl;
 
-    SfxListenerArr_Impl     aListeners;
+    SfxListenerArr_Impl     m_Listeners;
 
 private:
     void                    AddListener( SfxListener& rListener );
@@ -52,17 +52,14 @@ public:
     virtual                 ~SfxBroadcaster();
 
     void                    Broadcast( const SfxHint &rHint );
-    bool                    HasListeners() const
-    {
-        return !aListeners.empty();
-    }
+    bool                    HasListeners() const;
     size_t                  GetListenerCount() const
     {
-        return aListeners.size();
+        return m_Listeners.size();
     }
     SfxListener*            GetListener( sal_uInt16 nNo ) const
     {
-        return aListeners[nNo];
+        return m_Listeners[nNo];
     }
 };
 

svl/source/notify/brdcst.cxx
@@ -17,9 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <assert.h>
 
 #include <tools/debug.hxx>
-#include <osl/diagnose.h>
 
 #include <svl/hint.hxx>
 #include <svl/smplhint.hxx>
@@ -37,16 +37,17 @@ TYPEINIT0(SfxBroadcaster);
 //====================================================================
 // broadcast immediately
 
-
 void SfxBroadcaster::Broadcast( const SfxHint &rHint )
 {
     DBG_CHKTHIS(SfxBroadcaster, 0);
 
     // notify all registered listeners exactly once
-    for (size_t n = 0; n < aListeners.size(); ++n)
+    for (size_t n = 0; n < m_Listeners.size(); ++n)
     {
-        SfxListener* pListener = aListeners[n];
-        pListener->Notify( *this, rHint );
+        SfxListener *const pListener = m_Listeners[n];
+        if (pListener) {
+            pListener->Notify( *this, rHint );
+        }
     }
 }
 
@@ -59,10 +60,12 @@ SfxBroadcaster::~SfxBroadcaster()
     Broadcast( SfxSimpleHint(SFX_HINT_DYING) );
 
     // remove all still registered listeners
-    for (size_t nPos = 0; nPos < aListeners.size(); ++nPos)
+    for (size_t nPos = 0; nPos < m_Listeners.size(); ++nPos)
     {
-        SfxListener *pListener = aListeners[nPos];
-        pListener->RemoveBroadcaster_Impl(*this);
+        SfxListener *const pListener = m_Listeners[nPos];
+        if (pListener) {
+            pListener->RemoveBroadcaster_Impl(*this);
+        }
     }
 }
 
@@ -84,10 +87,12 @@ SfxBroadcaster::SfxBroadcaster( const SfxBroadcaster &rBC )
 {
     DBG_CTOR(SfxBroadcaster, 0);
 
-    for (size_t n = 0; n < rBC.aListeners.size(); ++n)
+    for (size_t n = 0; n < rBC.m_Listeners.size(); ++n)
     {
-        SfxListener *pListener = rBC.aListeners[n];
-        pListener->StartListening( *this );
+        SfxListener *const pListener = rBC.m_Listeners[n];
+        if (pListener) {
+            pListener->StartListening( *this );
+        }
     }
 }
 
@@ -99,7 +104,15 @@ void SfxBroadcaster::AddListener( SfxListener& rListener )
 {
     DBG_CHKTHIS(SfxBroadcaster, 0);
 
-    aListeners.push_back(&rListener);
+    for (size_t i = 0; i < m_Listeners.size(); ++i)
+    {
+        if (!m_Listeners[i]) // removed by RemoveListener?
+        {
+            m_Listeners[i] = &rListener;
+            return;
+        }
+    }
+    m_Listeners.push_back(&rListener);
 }
 
 //--------------------------------------------------------------------
@@ -117,10 +130,12 @@ void SfxBroadcaster::ListenersGone()
 
 void SfxBroadcaster::Forward(SfxBroadcaster& rBC, const SfxHint& rHint)
 {
-    for (size_t i = 0; i < aListeners.size(); ++i)
+    for (size_t i = 0; i < m_Listeners.size(); ++i)
     {
-        SfxListener *pListener = aListeners[i];
-        pListener->Notify( rBC, rHint );
+        SfxListener *const pListener = m_Listeners[i];
+        if (pListener) {
+            pListener->Notify( rBC, rHint );
+        }
     }
 }
 
@@ -131,14 +146,26 @@ void SfxBroadcaster::Forward(SfxBroadcaster& rBC, const SfxHint& rHint)
 void SfxBroadcaster::RemoveListener( SfxListener& rListener )
 {
     {DBG_CHKTHIS(SfxBroadcaster, 0);}
-    const SfxListener *pListener = &rListener;
-
-    SfxListenerArr_Impl::iterator aIter = std::remove(aListeners.begin(), aListeners.end(), pListener);
-    DBG_ASSERT( aIter != aListeners.end(), "RemoveListener: Listener unknown" );
-    aListeners.erase(aIter, aListeners.end());
+    SfxListenerArr_Impl::iterator aIter = std::find(
+            m_Listeners.begin(), m_Listeners.end(), &rListener);
+    assert(aIter != m_Listeners.end()); // "RemoveListener: Listener unknown"
+    // DO NOT erase the listener, set the pointer to 0
+    // because the current continuation may contain this->Broadcast
+    *aIter = 0;
 
     if ( !HasListeners() )
         ListenersGone();
+}
+
+bool SfxBroadcaster::HasListeners() const
+{
+    for (size_t i = 0; i < m_Listeners.size(); ++i)
+    {
+        if (m_Listeners[i]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */