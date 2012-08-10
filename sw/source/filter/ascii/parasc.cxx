LibreOffice/core

commit aac04652fda01b0299e17087b151f07d6115e894

String::AllocBuffer replacements
Change-Id: I278cd66fb4819721bb473796c28598aaf04eb123

sw/source/filter/ascii/parasc.cxx
@@ -26,7 +26,7 @@
  *
  ************************************************************************/
 
-
+#include <boost/scoped_array.hpp>
 #include <tools/stream.hxx>
 #include <hintids.hxx>
 #include <rtl/tencinfo.h>
@@ -303,7 +303,7 @@ sal_uLong SwASCIIParser::ReadChars()
         bSwapUnicode = rInput.IsEndianSwap();
     }
 
-    String sWork;
+    boost::scoped_array<sal_Unicode> aWork;
     sal_uLong nArrOffset = 0;
 
     do {
@@ -331,7 +331,8 @@ sal_uLong SwASCIIParser::ReadChars()
             {
                 sal_uInt32 nInfo;
                 sal_Size nNewLen = lGCount, nCntBytes;
-                sal_Unicode* pBuf = sWork.AllocBuffer( static_cast< xub_StrLen >(nNewLen) );
+                aWork.reset(new sal_Unicode[nNewLen]);
+                sal_Unicode* pBuf = aWork.get();
 
                 nNewLen = rtl_convertTextToUnicode( hConverter, hContext,
                                 pArr, lGCount, pBuf, nNewLen,
@@ -345,9 +346,8 @@ sal_uLong SwASCIIParser::ReadChars()
                                 &nCntBytes );
                 if( 0 != ( nArrOffset = lGCount - nCntBytes ) )
                     memmove( pArr, pArr + nCntBytes, nArrOffset );
-                sWork.ReleaseBufferAccess( static_cast< xub_StrLen >(nNewLen) );
 
-                pStt = pLastStt = sWork.GetBufferAccess();
+                pStt = pLastStt = aWork.get();
                 pEnd = pStt + nNewLen;
             }
             else

sw/source/filter/basflt/iodetect.cxx
@@ -28,7 +28,7 @@
 
 
 #include <iodetect.hxx>
-
+#include <boost/scoped_array.hpp>
 #include <osl/endian.h>
 #include <sot/storage.hxx>
 #include <svtools/parhtml.hxx>
@@ -450,8 +450,8 @@ bool SwIoSystem::IsDetectableText(const sal_Char* pBuf, sal_uLong &rLen,
 
     if (eCharSet != RTL_TEXTENCODING_DONTKNOW)
     {
-        String sWork;
-        sal_Unicode *pNewBuf = sWork.AllocBuffer( static_cast< xub_StrLen >(rLen));
+        boost::scoped_array<sal_Unicode> aWork(new sal_Unicode[rLen]);
+        sal_Unicode *pNewBuf = aWork.get();
         sal_Size nNewLen;
         if (eCharSet != RTL_TEXTENCODING_UCS2)
         {
@@ -494,9 +494,6 @@ bool SwIoSystem::IsDetectableText(const sal_Char* pBuf, sal_uLong &rLen,
                 }
             }
         }
-
-        sWork.ReleaseBufferAccess( static_cast< xub_StrLen >(nNewLen) );
-        pNewBuf = sWork.GetBufferAccess();
 
         for (sal_uLong nCnt = 0; nCnt < nNewLen; ++nCnt, ++pNewBuf)
         {

basic/source/sbx/sbxscan.cxx
@@ -785,9 +785,10 @@ void SbxValue::Format( XubString& rRes, const XubString* pFmt ) const
                 if( nMin < 10 && aFmtStr.EqualsIgnoreCaseAscii( VBAFORMAT_NN ) )
                 {
                     // Minute in two digits
-                     sal_Unicode* p = rRes.AllocBuffer( 2 );
-                     *p++ = '0';
-                     *p = sal_Unicode( '0' + nMin );
+                     sal_Unicode aBuf[2];
+                     aBuf[0] = '0';
+                     aBuf[1] = '0' + nMin;
+                     rRes = rtl::OUString(aBuf, SAL_N_ELEMENTS(aBuf));
                 }
                 else
                 {

editeng/source/editeng/editobj.cxx
@@ -26,7 +26,7 @@
  *
  ************************************************************************/
 
-
+#include <comphelper/string.hxx>
 #include <rtl/strbuf.hxx>
 #include <vcl/wrkwin.hxx>
 #include <vcl/dialog.hxx>
@@ -1444,6 +1444,8 @@ void BinTextObject::CreateData( SvStream& rIStream )
         rIStream >> bUnicodeStrings;
         if ( bUnicodeStrings )
         {
+            using comphelper::string::rtl_uString_alloc;
+
             for ( sal_uInt16 nPara = 0; nPara < nParagraphs; nPara++ )
             {
                 ContentInfo& rC = aContents[nPara];
@@ -1453,18 +1455,18 @@ void BinTextObject::CreateData( SvStream& rIStream )
                 rIStream >> nL;
                 if ( nL )
                 {
-                    rC.GetText().AllocBuffer( nL );
-                    rIStream.Read(rC.GetText().GetBufferAccess(), nL*sizeof(sal_Unicode));
-                    rC.GetText().ReleaseBufferAccess(nL);
+                    rtl_uString *pStr = rtl_uString_alloc(nL);
+                    rIStream.Read(pStr->buffer, nL*sizeof(sal_Unicode));
+                    rC.GetText() = rtl::OUString(pStr, SAL_NO_ACQUIRE);
                 }
 
                 // StyleSheetName
                 rIStream >> nL;
                 if ( nL )
                 {
-                    rC.GetStyle().AllocBuffer(nL);
-                    rIStream.Read(rC.GetStyle().GetBufferAccess(), nL*sizeof(sal_Unicode) );
-                    rC.GetStyle().ReleaseBufferAccess(nL);
+                    rtl_uString *pStr = rtl_uString_alloc(nL);
+                    rIStream.Read(pStr->buffer, nL*sizeof(sal_Unicode) );
+                    rC.GetStyle() = rtl::OUString(pStr, SAL_NO_ACQUIRE);
                 }
             }
         }

editeng/source/items/flditem.cxx
@@ -26,6 +26,7 @@
  *
  ************************************************************************/
 
+#include <comphelper/string.hxx>
 #include <vcl/metaact.hxx>
 #include <svl/zforlist.hxx>
 #include <tools/urlobj.hxx>
@@ -550,25 +551,28 @@ int SvxURLField::operator==( const SvxFieldData& rOther ) const
 
 // -----------------------------------------------------------------------
 
-static void write_unicode( SvPersistStream & rStm, const String& rString )
+static void write_unicode( SvPersistStream & rStm, const rtl::OUString& rString )
 {
-    sal_uInt16 nL = rString.Len();
+    sal_uInt16 nL =  sal::static_int_cast<sal_uInt16>(rString.getLength());
     rStm << nL;
-    rStm.Write( rString.GetBuffer(), nL*sizeof(sal_Unicode) );
+    //endian specific?, yipes!
+    rStm.Write( rString.getStr(), nL*sizeof(sal_Unicode) );
 }
 
-static void read_unicode( SvPersistStream & rStm, rtl::OUString& rString )
+static rtl::OUString read_unicode( SvPersistStream & rStm )
 {
+    rtl_uString *pStr = NULL;
     sal_uInt16 nL = 0;
     rStm >> nL;
-    String aStr;
     if ( nL )
     {
-        aStr.AllocBuffer( nL );
-        rStm.Read( aStr.GetBufferAccess(), nL*sizeof(sal_Unicode) );
-        aStr.ReleaseBufferAccess( nL );
+        using comphelper::string::rtl_uString_alloc;
+        pStr = rtl_uString_alloc(nL);
+        //endian specific?, yipes!
+        rStm.Read(pStr->buffer, nL*sizeof(sal_Unicode));
     }
-    rString = aStr;
+    //take ownership of buffer and return, otherwise return empty string
+    return pStr ? rtl::OUString(pStr, SAL_NO_ACQUIRE) : rtl::OUString();
 }
 
 void SvxURLField::Load( SvPersistStream & rStm )
@@ -578,9 +582,9 @@ void SvxURLField::Load( SvPersistStream & rStm )
     rStm >> nFormat;
     eFormat= (SvxURLFormat)nFormat;
 
-    read_unicode( rStm, aURL );
-    read_unicode( rStm, aRepresentation );
-    read_unicode( rStm, aTargetFrame );
+    aURL = read_unicode( rStm );
+    aRepresentation = read_unicode( rStm );
+    aTargetFrame = read_unicode( rStm );
 }
 
 // -----------------------------------------------------------------------
@@ -1088,9 +1092,9 @@ void SvxAuthorField::Load( SvPersistStream & rStm )
 {
     sal_uInt16 nType = 0, nFormat = 0;
 
-    read_unicode( rStm, aName );
-    read_unicode( rStm, aFirstName );
-    read_unicode( rStm, aShortName );
+    aName = read_unicode( rStm );
+    aFirstName = read_unicode( rStm );
+    aShortName = read_unicode( rStm );
 
     rStm >> nType;
     rStm >> nFormat;

svl/inc/svl/zformat.hxx
@@ -239,7 +239,7 @@ public:
 
     // Load a string which might contain an Euro symbol,
     // in fact that could be any string used in number formats.
-    static void LoadString( SvStream& rStream, String& rStr );
+    static rtl::OUString LoadString( SvStream& rStream );
 
     /**
      * Get output string from a numeric value that fits the number of

svl/source/numbers/zformat.cxx
@@ -170,9 +170,9 @@ void ImpSvNumberformatInfo::Save(SvStream& rStream, sal_uInt16 nAnz) const
 
 void ImpSvNumberformatInfo::Load(SvStream& rStream, sal_uInt16 nAnz)
 {
-    for (sal_uInt16 i = 0; i < nAnz; i++)
+    for (sal_uInt16 i = 0; i < nAnz; ++i)
     {
-        SvNumberformat::LoadString( rStream, sStrArray[i] );
+        sStrArray[i] = SvNumberformat::LoadString( rStream );
         rStream >> nTypeArray[i];
     }
     sal_Bool bStreamThousand;
@@ -1565,7 +1565,7 @@ NfHackConversion SvNumberformat::Load( SvStream& rStream,
 {
     rHdr.StartEntry();
     sal_uInt16 nOp1, nOp2;
-    SvNumberformat::LoadString( rStream, sFormatstring );
+    sFormatstring = SvNumberformat::LoadString( rStream );
     sal_Bool bStreamStandard, bStreamUsed;
     rStream >> eType >> fLimit1 >> fLimit2
             >> nOp1 >> nOp2 >> bStreamStandard >> bStreamUsed;
@@ -1623,7 +1623,7 @@ NfHackConversion SvNumberformat::Load( SvStream& rStream,
     String aComment;        // wird nach dem NewCurrency-Geraffel richtig gesetzt
     if ( rHdr.BytesLeft() )
     {   // ab SV_NUMBERFORMATTER_VERSION_NEWSTANDARD
-        SvNumberformat::LoadString( rStream, aComment );
+        aComment = SvNumberformat::LoadString( rStream );
         rStream >> nNewStandardDefined;
     }
 
@@ -1752,32 +1752,27 @@ void SvNumberformat::ConvertLanguage( SvNumberFormatter& rConverter,
 }
 
 // static
-void SvNumberformat::LoadString( SvStream& rStream, String& rStr )
+rtl::OUString SvNumberformat::LoadString( SvStream& rStream )
 {
     CharSet eStream = rStream.GetStreamCharSet();
     rtl::OString aStr = read_lenPrefixed_uInt8s_ToOString<sal_uInt16>(rStream);
     sal_Char cStream = NfCurrencyEntry::GetEuroSymbol( eStream );
     if (aStr.indexOf(cStream) == -1)
     {   // simple conversion to unicode
-        rStr = rtl::OStringToOUString(aStr, eStream);
+        return rtl::OStringToOUString(aStr, eStream);
     }
-    else
+
+    sal_Unicode cTarget = NfCurrencyEntry::GetEuroSymbol();
+    rtl::OUStringBuffer aBuf(aStr.getLength());
+    for (sal_Int32 i = 0; i < aStr.getLength(); ++i)
     {
-        sal_Unicode cTarget = NfCurrencyEntry::GetEuroSymbol();
-        register const sal_Char* p = aStr.getStr();
-        register const sal_Char* const pEnd = p + aStr.getLength();
-        register sal_Unicode* pUni = rStr.AllocBuffer(aStr.getLength());
-        while ( p < pEnd )
-        {
-            if ( *p == cStream )
-                *pUni = cTarget;
-            else
-                *pUni = rtl::OUString(p, 1, eStream).toChar();
-            ++p;
-            ++pUni;
-        }
-        *pUni = 0;
+        if (aStr[i] == cStream)
+            aBuf.append(cTarget);
+        else
+            aBuf.append(rtl::OUString(aStr.getStr()+i, 1, eStream).toChar());
     }
+
+    return aBuf.makeStringAndClear();
 }
 
 void SvNumberformat::Save( SvStream& rStream, ImpSvNumMultipleWriteHeader& rHdr ) const
@@ -4735,16 +4730,17 @@ String SvNumberformat::ImpGetNatNumString( const SvNumberNatNum& rNum,
         {   // speed up the most common case
             if ( 0 <= nVal && nVal < 10 )
             {
-                sal_Unicode* p = aStr.AllocBuffer( 2 );
-                *p++ = '0';
-                *p = sal_Unicode( '0' + nVal );
+                sal_Unicode aBuf[2];
+                aBuf[0] = '0';
+                aBuf[1] = '0' + nVal;
+                aStr = rtl::OUString(aBuf, SAL_N_ELEMENTS(aBuf));
             }
             else
-                aStr = String::CreateFromInt32( nVal );
+                aStr = rtl::OUString::valueOf( nVal );
         }
         else
         {
-            String aValStr( String::CreateFromInt32( nVal ) );
+            String aValStr( rtl::OUString::valueOf( nVal ) );
             if ( aValStr.Len() >= nMinDigits )
                 aStr = aValStr;
             else
@@ -4755,7 +4751,7 @@ String SvNumberformat::ImpGetNatNumString( const SvNumberNatNum& rNum,
         }
     }
     else
-        aStr = String::CreateFromInt32( nVal );
+        aStr = rtl::OUString::valueOf( nVal );
     ImpTransliterate( aStr, rNum );
     return aStr;
 }

sw/source/core/edit/edglss.cxx
@@ -26,6 +26,7 @@
  *
  ************************************************************************/
 
+#include <comphelper/string.hxx>
 #include <osl/endian.h>
 #include <hintids.hxx>
 #include <svl/urihelper.hxx>
@@ -344,15 +345,16 @@ sal_Bool SwEditShell::GetSelectedText( String &rBuf, int nHndlParaBrk )
 
                 const sal_Unicode *p = (sal_Unicode*)aStream.GetBuffer();
                 if( p )
-                    rBuf = p;
+                    rBuf = rtl::OUString(p);
                 else
                 {
-                    sal_Unicode* pStrBuf = rBuf.AllocBuffer( xub_StrLen(
-                                    ( lLen / sizeof( sal_Unicode ))) );
+                    using comphelper::string::rtl_uString_alloc;
+                    rtl_uString *pStr = rtl_uString_alloc(lLen / sizeof( sal_Unicode ));
                     aStream.Seek( 0 );
                     aStream.ResetError();
-                    aStream.Read( pStrBuf, lLen );
-                    pStrBuf[ lLen / sizeof( sal_Unicode ) ] = '\0';
+                    //endian specific?, yipes!
+                    aStream.Read(pStr->buffer, lLen);
+                    rBuf = rtl::OUString(pStr, SAL_NO_ACQUIRE);
                 }
             }
         }

zhirshya/lo-tblr

commit 0612d183df66a14457e92109a5b017ca09f64b97

fdo#53328: sw: ascii import: fix crash:
The aWork buffer is missing space for the null terminator, which the
previous String::AllocateBuffer automatically added.
(regression from aac04652fda01b0299e17087b151f07d6115e894)

Change-Id: Ia12ad46dfb95dd59965a073debb95b241b2e97d9

sw/source/filter/ascii/parasc.cxx
@@ -331,7 +331,7 @@ sal_uLong SwASCIIParser::ReadChars()
             {
                 sal_uInt32 nInfo;
                 sal_Size nNewLen = lGCount, nCntBytes;
-                aWork.reset(new sal_Unicode[nNewLen]);
+                aWork.reset(new sal_Unicode[nNewLen + 1]); // add 1 for '\0'
                 sal_Unicode* pBuf = aWork.get();
 
                 nNewLen = rtl_convertTextToUnicode( hConverter, hContext,

sw/source/filter/basflt/iodetect.cxx
@@ -450,7 +450,7 @@ bool SwIoSystem::IsDetectableText(const sal_Char* pBuf, sal_uLong &rLen,
 
     if (eCharSet != RTL_TEXTENCODING_DONTKNOW)
     {
-        boost::scoped_array<sal_Unicode> aWork(new sal_Unicode[rLen]);
+        boost::scoped_array<sal_Unicode> aWork(new sal_Unicode[rLen+1]);
         sal_Unicode *pNewBuf = aWork.get();
         sal_Size nNewLen;
         if (eCharSet != RTL_TEXTENCODING_UCS2)