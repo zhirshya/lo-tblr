LibreOffice/core

commit 140281898f21f707582f395918e5234bdb67e435

elide all dependencies on .hdl files, they're ~all duplicated by .hpp
Also - verified that codemaker touches both .hdl and .hpp regardless
of whether the contents changed in anyway under gbuild.

Change-Id: I391e9cf952c61a6c59087cd7cd0ddc8c54a14df3

solenv/bin/concat-deps.c
@@ -710,11 +710,34 @@ static inline void eat_space(char ** token)
     }
 }
 
+/*
+ * Prune LibreOffice specific duplicate dependencies to improve
+ * gnumake startup time, and shrink the disk-space footprint.
+ */
+static inline int elide_dependency(const char* key, int key_len)
+{
+#if 0
+    {
+        int i;
+        fprintf (stderr, "elide?!: '");
+        for (i = 0; i < key_len; i++) {
+            fprintf (stderr, "%c", key[i]);
+        }
+        fprintf (stderr, "'\n");
+    }
+#endif
+
+    /* .hdl files are always matched by .hpp */
+    if (key_len > 4 && !strncmp(key + key_len - 4, ".hdl", 4))
+        return 1;
+    return 0;
+}
+
 /* prefix paths to absolute */
 static inline void print_fullpaths(char* line)
 {
-char* token;
-char* end;
+    char* token;
+    char* end;
 
     token = line;
     eat_space(&token);
@@ -724,15 +747,16 @@ char* end;
         while (*end && (' ' != *end) && ('\t' != *end)) {
             ++end;
         }
-        if(*token == ':' || *token == '\\' || *token == '/' || *token == '$'
-            || ':' == token[1])
+        int token_len = end - token;
+        if(elide_dependency(token, token_len))
+            ; /* don't output it */
+        else if(*token == ':' || *token == '\\' || *token == '/' ||
+                *token == '$' || ':' == token[1])
         {
-            fwrite(token, end - token, 1, stdout);
+            fwrite(token, token_len, 1, stdout);
         }
         else
         {
-            fputs(base_dir_var, stdout);
-            fputc('/', stdout);
             fwrite(token, end - token, 1, stdout);
         }
         fputc(' ', stdout);
@@ -740,6 +764,7 @@ char* end;
         eat_space(&token);
     }
 }
+
 
 static int _process(struct hash* dep_hash, char* fn)
 {
@@ -795,7 +820,9 @@ off_t size;
                              * these are the one for which we want to filter
                              * duplicate out
                              */
-                            if(hash_store(dep_hash, base, (int)(cursor_out - base)))
+                            int key_len = cursor_out - base;
+                            if(!elide_dependency(base,key_len - 1) &&
+                               hash_store(dep_hash, base, key_len))
                             {
                                 /* DO NOT modify base after it has been added
                                    as key by hash_store */

avmedia/source/gstreamer/gstframegrabber.hxx
@@ -21,7 +21,7 @@
 #define _FRAMEGRABBER_HXX
 
 #include "gstplayer.hxx"
-#include "com/sun/star/media/XFrameGrabber.hdl"
+#include <com/sun/star/media/XFrameGrabber.hpp>
 #include <cppuhelper/implbase2.hxx>
 
 namespace avmedia { namespace gstreamer {

zhirshya/lo-tblr

commit 98f9f42a220da1678f9b511384ef4ed6d9bec03f

concat-deps: fix breakage introduced by eliding:
The call to elide_dependency in _process was broken because it checked
the full line, which ends with " : " usually, breaking the build with LO
customised GNU make on cygwin.
(regression from 140281898f21f707582f395918e5234bdb67e435)
Also handle empty lines following '\' better while at it.

Change-Id: Iadc6349e5b6930945e46e244bcbee37a632e0d71

solenv/bin/concat-deps.c
@@ -811,6 +811,17 @@ static inline void print_fullpaths(char* line)
     }
 }
 
+static inline char * eat_space_at_end(char * end)
+{
+    assert('\0' == *end);
+    char * real_end = end - 1;
+    while (' ' == *real_end || '\t' == *real_end || '\n' == *real_end
+                || ':' == *real_end)
+    {    /* eat colon and whitespace at end */
+         --real_end;
+    }
+    return real_end;
+}
 
 static int _process(struct hash* dep_hash, char* fn)
 {
@@ -866,8 +877,8 @@ off_t size;
                              * these are the one for which we want to filter
                              * duplicate out
                              */
-                            int key_len = cursor_out - base;
-                            if(!elide_dependency(base,key_len - 1, NULL) &&
+                            int key_len = eat_space_at_end(cursor_out) - base;
+                            if(!elide_dependency(base,key_len + 1, NULL) &&
                                hash_store(dep_hash, base, key_len))
                             {
                                 /* DO NOT modify base after it has been added
@@ -882,6 +893,7 @@ off_t size;
                             print_fullpaths(base);
                             putc('\n', stdout);
                         }
+                        last_ns = ' '; // cannot hurt to reset it
                     }
                     cursor += 1;
                     base = cursor_out = cursor;
@@ -892,6 +904,7 @@ off_t size;
                      * i.e not a complete rule yet
                      */
                     *cursor_out++ = *cursor++;
+                    continuation = 0; // cancel current one (empty lines!)
                 }
             }
             else
@@ -910,7 +923,9 @@ off_t size;
         {
             if(last_ns == ':')
             {
-                if(hash_store(dep_hash, base, (int)(cursor_out - base)))
+                int key_len = eat_space_at_end(cursor_out) - base;
+                if (!elide_dependency(base,key_len + 1, NULL) &&
+                    hash_store(dep_hash, base, key_len))
                 {
                     puts(base);
                     putc('\n', stdout);