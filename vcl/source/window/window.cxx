LibreOffice/core

commit 2fe852386c9450014f84910b0a282d684f40b56a

fdo#46808, Adapt SystemClipboard UNO service to new style
this is the datatransfer::clipboard::SystemClipboard service

Change-Id: I00227a2d84e2f1eaf9ca1ebb4fbe1f1548a6621d

vcl/source/window/window.cxx
@@ -79,6 +79,7 @@
 #include "com/sun/star/datatransfer/dnd/XDragSource.hpp"
 #include "com/sun/star/datatransfer/dnd/XDropTarget.hpp"
 #include "com/sun/star/datatransfer/clipboard/XClipboard.hpp"
+#include "com/sun/star/datatransfer/clipboard/SystemClipboard.hpp"
 #include "com/sun/star/awt/XTopWindow.hpp"
 #include "com/sun/star/awt/XDisplayConnection.hpp"
 #include "com/sun/star/lang/XInitialization.hpp"
@@ -8429,30 +8430,21 @@ uno::Reference< XClipboard > Window::GetClipboard()
             try
             {
                 uno::Reference< XMultiServiceFactory > xFactory( comphelper::getProcessServiceFactory() );
+                uno::Reference< XComponentContext > xContext( comphelper::getProcessComponentContext() );
 
                 mpWindowImpl->mpFrameData->mxClipboard = uno::Reference< XClipboard >( xFactory->createInstance( OUString("com.sun.star.datatransfer.clipboard.SystemClipboardExt") ), UNO_QUERY );
 
                 if( !mpWindowImpl->mpFrameData->mxClipboard.is() )
-                    mpWindowImpl->mpFrameData->mxClipboard = uno::Reference< XClipboard >( xFactory->createInstance( OUString("com.sun.star.datatransfer.clipboard.SystemClipboard") ), UNO_QUERY );
-
-#if defined(UNX) && !defined(MACOSX)          // unix clipboard needs to be initialized
-                if( mpWindowImpl->mpFrameData->mxClipboard.is() )
                 {
-                    uno::Reference< XInitialization > xInit = uno::Reference< XInitialization >( mpWindowImpl->mpFrameData->mxClipboard, UNO_QUERY );
-
-                    if( xInit.is() )
-                    {
-                        Sequence< Any > aArgumentList( 3 );
-                        aArgumentList[ 0 ] = makeAny( Application::GetDisplayConnection() );
-                        aArgumentList[ 1 ] = makeAny( OUString("CLIPBOARD") );
-                        aArgumentList[ 2 ] = makeAny( vcl::createBmpConverter() );
-
-                        xInit->initialize( aArgumentList );
-                    }
-                }
+                    Reference<XSystemClipboard> xSystemClipboard;
+#if defined(UNX) && !defined(MACOSX)          // unix clipboard needs to be initialized
+                    xSystemClipboard = SystemClipboard::createUnix( xContext, Application::GetDisplayConnection(), "CLIPBOARD", vcl::createBmpConverter() );
+#else
+                    xSystemClipboard = SystemClipboard::createDefault(xContext);
 #endif
+                    mpWindowImpl->mpFrameData->mxClipboard = uno::Reference< XClipboard >( xSystemClipboard, UNO_QUERY );
+                }
             }
-
             // createInstance can throw any exception
             catch (const Exception&)
             {
@@ -8480,15 +8472,11 @@ uno::Reference< XClipboard > Window::GetPrimarySelection()
             try
             {
                 uno::Reference< XMultiServiceFactory > xFactory( comphelper::getProcessServiceFactory() );
+                uno::Reference< XComponentContext > xContext( comphelper::getProcessComponentContext() );
 
 #if defined(UNX) && !defined(MACOSX)
-                Sequence< Any > aArgumentList( 3 );
-                aArgumentList[ 0 ] = makeAny( Application::GetDisplayConnection() );
-                aArgumentList[ 1 ] = makeAny( OUString("PRIMARY") );
-                aArgumentList[ 2 ] = makeAny( vcl::createBmpConverter() );
-
-                mpWindowImpl->mpFrameData->mxSelection = uno::Reference< XClipboard >( xFactory->createInstanceWithArguments(
-                                                                                           OUString("com.sun.star.datatransfer.clipboard.SystemClipboard"), aArgumentList ), UNO_QUERY );
+                Reference<XSystemClipboard> xSystemClipboard = SystemClipboard::createUnix( xContext, Application::GetDisplayConnection(), "CLIPBOARD", vcl::createBmpConverter() );
+                mpWindowImpl->mpFrameData->mxSelection = uno::Reference< XClipboard >( xSystemClipboard, UNO_QUERY );
 #       else
                 static uno::Reference< XClipboard > s_xSelection;
 

dtrans/source/win32/clipb/WinClipboard.cxx
@@ -60,7 +60,7 @@ namespace
 //------------------------------------------------------------------------
 /*XEventListener,*/
 CWinClipboard::CWinClipboard( const Reference< XMultiServiceFactory >& rServiceManager, const OUString& aClipboardName ) :
-    WeakComponentImplHelper4< XClipboardEx, XFlushableClipboard, XClipboardNotifier, XServiceInfo >( m_aCbListenerMutex ),
+    WeakComponentImplHelper3< XSystemClipboard, XFlushableClipboard, XServiceInfo >( m_aCbListenerMutex ),
     m_SrvMgr( rServiceManager )
 {
     m_pImpl.reset( new CWinClipbImpl( aClipboardName, this ) );

dtrans/source/win32/clipb/WinClipboard.hxx
@@ -23,12 +23,13 @@
 
 #include <rtl/ustring.hxx>
 #include <sal/types.h>
-#include <cppuhelper/compbase4.hxx>
+#include <cppuhelper/compbase3.hxx>
 #include <com/sun/star/datatransfer/XTransferable.hpp>
 #include <com/sun/star/datatransfer/clipboard/XClipboardEx.hpp>
 #include <com/sun/star/datatransfer/clipboard/XClipboardOwner.hpp>
 #include <com/sun/star/datatransfer/clipboard/XClipboardListener.hpp>
 #include <com/sun/star/datatransfer/clipboard/XClipboardNotifier.hpp>
+#include <com/sun/star/datatransfer/clipboard/XSystemClipboard.hpp>
 #include <com/sun/star/datatransfer/clipboard/XFlushableClipboard.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <osl/conditn.hxx>
@@ -61,10 +62,9 @@ protected:
 
 class CWinClipboard :
     public CWinClipboardDummy,
-    public cppu::WeakComponentImplHelper4<
-        ::com::sun::star::datatransfer::clipboard::XClipboardEx, \
+    public cppu::WeakComponentImplHelper3<
+        ::com::sun::star::datatransfer::clipboard::XSystemClipboard,
         ::com::sun::star::datatransfer::clipboard::XFlushableClipboard,
-        ::com::sun::star::datatransfer::clipboard::XClipboardNotifier,
         ::com::sun::star::lang::XServiceInfo >
 {
 public:

offapi/UnoApi_offapi.mk
@@ -73,6 +73,9 @@ $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/datatransf
 	DataFormatTranslator \
 	MimeContentTypeFactory \
 ))
+$(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/datatransfer/clipboard,\
+	SystemClipboard \
+))
 $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/deployment,\
 	ExtensionManager \
 	PackageInformationProvider \
@@ -615,7 +618,6 @@ $(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/configu
 $(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/datatransfer/clipboard,\
 	ClipboardManager \
 	GenericClipboard \
-	SystemClipboard \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles_noheader,offapi,offapi/com/sun/star/datatransfer/dnd,\
 	OleDragSource \
@@ -2049,6 +2051,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,offapi/com/sun/star/datatransfer/cli
 	XClipboardNotifier \
 	XClipboardOwner \
 	XFlushableClipboard \
+	XSystemClipboard \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles,offapi,offapi/com/sun/star/datatransfer/dnd,\
 	DNDConstants \

offapi/com/sun/star/datatransfer/clipboard/SystemClipboard.idl
@@ -20,16 +20,13 @@
 #ifndef __com_sun_star_datatransfer_clipboard_SystemClipboard_idl__
 #define __com_sun_star_datatransfer_clipboard_SystemClipboard_idl__
 
-#include <com/sun/star/lang/XComponent.idl>
-#include <com/sun/star/lang/XInitialization.idl>
+#include <com/sun/star/awt/XDisplayConnection.idl>
+#include <com/sun/star/datatransfer/clipboard/XSystemClipboard.idl>
+#include <com/sun/star/script/XInvocation.idl>
 
 
 module com { module sun { module star { module datatransfer { module clipboard {
 
-
- published interface XClipboardEx;
- published interface XClipboardNotifier;
- published interface XFlushableClipboard;
 
 /** The system clipboard service builds a bridge to the OS specific clipboard
     interfaces.
@@ -42,30 +39,14 @@ module com { module sun { module star { module datatransfer { module clipboard {
     </ul>
     It is possible to use clipboards for different selections simultaneously.</p>
 */
-published service SystemClipboard
+published service SystemClipboard : XSystemClipboard
 {
-    /** Provides access to the clipboard content.
-    */
-    interface XClipboardEx;
+    createDefault();
 
-    /** Provides the ability to request notifications on content changes.
-    */
-    interface XClipboardNotifier;
-
-    /** Provides the ability to render the complete clipboard content. This
-        interface is only available if the method
-        <member scope="com::sun::star::datatransfer::clipboard">XClipboardEx::getRenderingCapabilities()</member>
-        returns Persistant
-    */
-    [optional] interface XFlushableClipboard;
-
-    /** Provides the ability to initialize the clipboard
-    */
-    [optional] interface com::sun::star::lang::XInitialization;
-
-    /** For shutdown and listener support.
-    */
-    interface com::sun::star::lang::XComponent;
+    /** This method is only valid for unix systems (excluding MacOS) */
+    createUnix([in] com::sun::star::awt::XDisplayConnection DisplayConnection,
+               [in] string ClipboardName,
+               [in] com::sun::star::script::XInvocation BmpConverter);
 };
 
 

offapi/com/sun/star/datatransfer/clipboard/XSystemClipboard.idl
@@ -1,0 +1,63 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef __com_sun_star_datatransfer_clipboard_XSystemClipboard_idl__
+#define __com_sun_star_datatransfer_clipboard_XSystemClipboard_idl__
+
+#include <com/sun/star/lang/XComponent.idl>
+#include <com/sun/star/datatransfer/clipboard/XClipboardEx.idl>
+#include <com/sun/star/datatransfer/clipboard/XClipboardNotifier.idl>
+#include <com/sun/star/datatransfer/clipboard/XFlushableClipboard.idl>
+
+module com { module sun { module star { module datatransfer { module clipboard {
+
+
+/**
+    Provides a unified interface for new-style service SystemClipboard.
+
+    @since LibreOffice 4.1
+*/
+published interface XSystemClipboard
+{
+    /** Provides access to the clipboard content.
+    */
+    interface XClipboardEx;
+
+    /** Provides the ability to request notifications on content changes.
+    */
+    interface XClipboardNotifier;
+
+    /** Provides the ability to render the complete clipboard content. This
+        interface is only available if the method
+        <member scope="com::sun::star::datatransfer::clipboard">XClipboardEx::getRenderingCapabilities()</member>
+        returns Persistant
+    */
+    [optional] interface XFlushableClipboard;
+
+    /** For shutdown and listener support.
+    */
+    interface com::sun::star::lang::XComponent;
+};
+
+
+}; }; }; }; };
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

toolkit/source/awt/vclxtoolkit.cxx
@@ -42,6 +42,7 @@
 #include <com/sun/star/uno/Sequence.hxx>
 #include <com/sun/star/uno/XInterface.hpp>
 #include <com/sun/star/beans/NamedValue.hpp>
+#include <com/sun/star/datatransfer/clipboard/SystemClipboard.hpp>
 #include <cppuhelper/typeprovider.hxx>
 #include <osl/conditn.hxx>
 #include <rtl/uuid.h>
@@ -1334,13 +1335,10 @@ css::uno::Reference< css::awt::XWindowPeer > VCLXToolkit::ImplCreateWindow(
     {
         if( !mxClipboard.is() )
         {
-            ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > xFactory = ::comphelper::getProcessServiceFactory();
-            if ( xFactory.is() )
-            {
-                // remember clipboard here
-                mxClipboard = ::com::sun::star::uno::Reference< ::com::sun::star::datatransfer::clipboard::XClipboard > (
-                    xFactory->createInstance( ::rtl::OUString("com.sun.star.datatransfer.clipboard.SystemClipboard") ), ::com::sun::star::uno::UNO_QUERY );
-            }
+            ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > xContext = ::comphelper::getProcessComponentContext();
+            // remember clipboard here
+            mxClipboard = ::com::sun::star::uno::Reference< ::com::sun::star::datatransfer::clipboard::XClipboard > (
+                ::com::sun::star::datatransfer::clipboard::SystemClipboard::createDefault(xContext), ::com::sun::star::uno::UNO_QUERY );
         }
 
         return mxClipboard;

vcl/aqua/source/dtrans/aqua_clipboard.cxx
@@ -82,7 +82,7 @@ Sequence<OUString> clipboard_getSupportedServiceNames()
 
 
 AquaClipboard::AquaClipboard(NSPasteboard* pasteboard, bool bUseSystemPasteboard) :
-  WeakComponentImplHelper4<XClipboardEx, XClipboardNotifier, XFlushableClipboard, XServiceInfo>(m_aMutex),
+  WeakComponentImplHelper3<XSystemClipboard, XFlushableClipboard, XServiceInfo>(m_aMutex),
   mIsSystemPasteboard(bUseSystemPasteboard)
 {
     Reference<XMultiServiceFactory> mrServiceMgr = comphelper::getProcessServiceFactory();

zhirshya/lo-tblr

commit 2d07f7b7cf4987ead1128097cffed919426a3a67

fdo#59515: Typo CLIPBOARD -> PRIMARY
...a regression introduced with 2fe852386c9450014f84910b0a282d684f40b56a
"fdo#46808, Adapt SystemClipboard UNO service to new style."

Change-Id: Ia98eadc0fc80762455c1e8cc33797b322b83e458

vcl/source/window/window.cxx
@@ -8452,7 +8452,7 @@ uno::Reference< XClipboard > Window::GetPrimarySelection()
                 uno::Reference< XComponentContext > xContext( comphelper::getProcessComponentContext() );
 
 #if defined(UNX) && !defined(MACOSX)
-                Reference<XSystemClipboard> xSystemClipboard = SystemClipboard::createUnix( xContext, Application::GetDisplayConnection(), "CLIPBOARD", vcl::createBmpConverter() );
+                Reference<XSystemClipboard> xSystemClipboard = SystemClipboard::createUnix( xContext, Application::GetDisplayConnection(), "PRIMARY", vcl::createBmpConverter() );
                 mpWindowImpl->mpFrameData->mxSelection = uno::Reference< XClipboard >( xSystemClipboard, UNO_QUERY );
 #       else
                 static uno::Reference< XClipboard > s_xSelection;