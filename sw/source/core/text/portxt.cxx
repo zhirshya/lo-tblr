LibreOffice/core

commit d9e4c74811855de15f1bf2045c2c9b061a2d4dc6

merge together hand-crafted traditional/simplified chinese tests
merge together a gadzillion hand-crafted
isSimpleChinese/isTraditionalChinese/isKoreanVariants/isCJK
implementations which should fix a goodly amount of them

add a MsLangId::isFamilyNameFirst for locales where
family name appears first while I'm at it.

Change-Id: I65377793be037d16fe7250cd7450b28aec689e83

sw/source/core/text/portxt.cxx
@@ -29,6 +29,7 @@
 #include <ctype.h>
 
 #include <com/sun/star/i18n/ScriptType.hdl>
+#include <i18npool/mslangid.hxx>
 #include <hintids.hxx>     // CH_TXTATR
 #include <SwPortionHandler.hxx>
 #include <porlay.hxx>
@@ -101,7 +102,7 @@ sal_uInt16 lcl_AddSpace( const SwTxtSizeInfo &rInf, const XubString* pStr,
         LanguageType aLang =
             rInf.GetTxtFrm()->GetTxtNode()->GetLang( rInf.GetIdx(), 1, nScript );
 
-        if ( LANGUAGE_KOREAN != aLang && LANGUAGE_KOREAN_JOHAB != aLang )
+        if (MsLangId::isKorean(aLang))
         {
             const SwLinePortion* pPor = rPor.GetPortion();
             if ( pPor && ( pPor->IsKernPortion() ||
@@ -209,7 +210,7 @@ sal_uInt16 lcl_AddSpace( const SwTxtSizeInfo &rInf, const XubString* pStr,
             LanguageType aLang =
                 rInf.GetTxtFrm()->GetTxtNode()->GetLang( nPos, 1, nNextScript );
 
-            if ( LANGUAGE_KOREAN != aLang && LANGUAGE_KOREAN_JOHAB != aLang )
+            if (MsLangId::isKorean(aLang))
                 ++nCnt;
         }
     }

sw/source/core/txtnode/fntcache.cxx
@@ -26,6 +26,7 @@
  *
  ************************************************************************/
 
+#include <i18npool/mslangid.hxx>
 #include <vcl/outdev.hxx>
 #include <vcl/print.hxx>
 #include <vcl/lineinfo.hxx>
@@ -1093,7 +1094,7 @@ void SwFntObj::DrawText( SwDrawTextInfo &rInf )
                     if ( ( SW_CJK == nActual || SW_LATIN == nActual ) && nSpaceAdd )
                     {
                         LanguageType aLang = rInf.GetFont()->GetLanguage( SW_CJK );
-                        if ( LANGUAGE_KOREAN != aLang && LANGUAGE_KOREAN_JOHAB != aLang)
+                        if (MsLangId::isKorean(aLang))
                         {
                             long nSpaceSum = nSpaceAdd;
                             for ( sal_uInt16 nI = 0; nI < rInf.GetLen(); ++nI )
@@ -1269,7 +1270,7 @@ void SwFntObj::DrawText( SwDrawTextInfo &rInf )
                 {
                     LanguageType aLang = rInf.GetFont()->GetLanguage( SW_CJK );
 
-                    if ( LANGUAGE_KOREAN != aLang && LANGUAGE_KOREAN_JOHAB != aLang )
+                    if (MsLangId::isKorean(aLang))
                     {
                         long nSpaceSum = nSpaceAdd;
                         for ( sal_uInt16 nI = 0; nI < rInf.GetLen(); ++nI )
@@ -1480,7 +1481,7 @@ void SwFntObj::DrawText( SwDrawTextInfo &rInf )
             {
                 LanguageType aLang = rInf.GetFont()->GetLanguage( SW_CJK );
 
-                if ( LANGUAGE_KOREAN != aLang && LANGUAGE_KOREAN_JOHAB != aLang )
+                if (MsLangId::isKorean(aLang))
                 {
                     long nSpaceSum = nSpaceAdd;
                     for ( sal_uInt16 nI = 0; nI < rInf.GetLen(); ++nI )
@@ -2050,7 +2051,7 @@ xub_StrLen SwFntObj::GetCrsrOfst( SwDrawTextInfo &rInf )
         {
             LanguageType aLang = rInf.GetFont()->GetLanguage( SW_CJK );
 
-            if ( LANGUAGE_KOREAN != aLang && LANGUAGE_KOREAN_JOHAB != aLang )
+            if (MsLangId::isKorean(aLang))
             {
                 long nSpaceSum = nSpaceAdd;
                 for ( sal_uInt16 nI = 0; nI < rInf.GetLen(); ++nI )

cui/source/options/optasian.cxx
@@ -33,6 +33,7 @@
 #include <optasian.hrc>
 #include <dialmgr.hxx>
 #include <cuires.hrc>
+#include <i18npool/mslangid.hxx>
 #include <tools/shl.hxx>
 #include <svl/asiancfg.hxx>
 #include <com/sun/star/lang/Locale.hpp>
@@ -301,17 +302,10 @@ void SvxAsianLayoutPage::Reset( const SfxItemSet& )
     {
         eLastUsedLanguageTypeForForbiddenCharacters = SvxLocaleToLanguage(
             Application::GetSettings().GetLocale() );
-        switch(eLastUsedLanguageTypeForForbiddenCharacters)
-        {
-            case  LANGUAGE_CHINESE            :
-            case  LANGUAGE_CHINESE_SINGAPORE  :
-                eLastUsedLanguageTypeForForbiddenCharacters = LANGUAGE_CHINESE_SIMPLIFIED;
-            break;
-            case  LANGUAGE_CHINESE_HONGKONG   :
-            case  LANGUAGE_CHINESE_MACAU      :
-                eLastUsedLanguageTypeForForbiddenCharacters = LANGUAGE_CHINESE_TRADITIONAL;
-            break;
-        }
+        if (MsLangId::isSimplifiedChinese(eLastUsedLanguageTypeForForbiddenCharacters))
+            eLastUsedLanguageTypeForForbiddenCharacters = LANGUAGE_CHINESE_SIMPLIFIED;
+        else if (MsLangId::isTraditionalChinese(eLastUsedLanguageTypeForForbiddenCharacters))
+            eLastUsedLanguageTypeForForbiddenCharacters = LANGUAGE_CHINESE_TRADITIONAL;
     }
     aLanguageLB.SelectLanguage( eLastUsedLanguageTypeForForbiddenCharacters );
     LanguageHdl(&aLanguageLB);

cui/source/options/optgenrl.cxx
@@ -27,6 +27,7 @@
  ************************************************************************/
 
 #include <comphelper/string.hxx>
+#include <i18npool/mslangid.hxx>
 #include <tools/shl.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/msgbox.hxx>
@@ -162,11 +163,7 @@ SvxGeneralTabPage::SvxGeneralTabPage( Window* pParent, const SfxItemSet& rCoreSe
         aFirstName.SetZOrder( &aName, WINDOW_ZORDER_BEHIND );
         aFatherName.SetZOrder( &aFirstName, WINDOW_ZORDER_BEHIND );
     }
-    else if ( LANGUAGE_JAPANESE == eLang ||
-          LANGUAGE_KOREAN == eLang ||
-          LANGUAGE_HUNGARIAN == eLang ||
-          LANGUAGE_CHINESE_TRADITIONAL == eLang ||
-          LANGUAGE_CHINESE_SIMPLIFIED == eLang)
+    else if (MsLangId::isFamilyNameFirst(eLang))
     {
         aUsCityEdit.Hide();
         aUsStateEdit.Hide();

editeng/inc/editeng/hangulhanja.hxx
@@ -34,6 +34,7 @@
 #include <com/sun/star/lang/Locale.hpp>
 #include <com/sun/star/uno/Sequence.hxx>
 #include "editeng/editengdllapi.h"
+#include <i18npool/mslangid.hxx>
 
 //.............................................................................
 namespace editeng
@@ -275,20 +276,17 @@ namespace editeng
 
     sal_Bool HangulHanjaConversion::IsSimplified( LanguageType nLang )
     {
-        return  nLang == LANGUAGE_CHINESE_SIMPLIFIED ||
-                nLang == LANGUAGE_CHINESE_SINGAPORE;
+        return MsLangId::isSimplifiedChinese(nLang);
     }
 
     sal_Bool HangulHanjaConversion::IsTraditional( LanguageType nLang )
     {
-        return  nLang == LANGUAGE_CHINESE_TRADITIONAL ||
-                nLang == LANGUAGE_CHINESE_HONGKONG ||
-                nLang == LANGUAGE_CHINESE_MACAU;
+        return MsLangId::isTraditionalChinese(nLang);
     }
 
     sal_Bool HangulHanjaConversion::IsChinese( LanguageType nLang )
     {
-        return IsTraditional( nLang ) || IsSimplified( nLang );
+        return MsLangId::isChinese(nLang);
     }
 //.............................................................................
 }   // namespace svx

i18npool/inc/i18npool/mslangid.hxx
@@ -184,6 +184,29 @@ public:
     /** Whether locale has a Right-To-Left orientation. */
     static bool isRightToLeft( LanguageType nLang );
 
+    /** Whether locale is a CJK locale */
+    static bool isCJK( LanguageType nLang );
+
+    /** Whether locale is a chinese locale */
+    static bool isChinese( LanguageType nLang );
+
+    /** Whether locale is a simplified chinese locale */
+    static bool isSimplifiedChinese( LanguageType nLang );
+
+    /** Whether locale is a traditional chinese locale */
+    static bool isTraditionalChinese( LanguageType nLang );
+
+    /** Whether locale is a korean locale */
+    static bool isKorean( LanguageType nLang );
+
+    /** Whether locale is a simplified chinese locale */
+    static bool isSimplifiedChinese( const ::com::sun::star::lang::Locale & rLocale );
+
+    /** Whether locale is a traditional chinese locale */
+    static bool isTraditionalChinese( const ::com::sun::star::lang::Locale & rLocale );
+
+    /** Whether locale is one where family name comes first, e.g. Japan, Hungary, Samoa */
+    static bool isFamilyNameFirst( LanguageType nLang );
 
     /** Whether there are "forbidden characters at start or end of line" in
         this locale. CJK locales.

i18npool/source/characterclassification/characterclassificationImpl.cxx
@@ -178,7 +178,6 @@ CharacterClassificationImpl::getLocaleSpecificCharacterClassification(const Loca
             }
 
             static sal_Unicode under = (sal_Unicode)'_';
-            static OUString tw("TW");
             sal_Int32 l = rLocale.Language.getLength();
             sal_Int32 c = rLocale.Country.getLength();
             sal_Int32 v = rLocale.Variant.getLength();
@@ -197,7 +196,7 @@ CharacterClassificationImpl::getLocaleSpecificCharacterClassification(const Loca
                                     rLocale.Country.compareToAscii("MO") == 0) &&
                     // if the country code is HK or MO, one more step to try TW.
                     createLocaleSpecificCharacterClassification(aBuf.append(rLocale.Language).append(under).append(
-                                    tw).makeStringAndClear(), rLocale)) ||
+                                    "TW").makeStringAndClear(), rLocale)) ||
                 (l > 0 &&
                     // load service with name <base>_<lang>
                     createLocaleSpecificCharacterClassification(rLocale.Language, rLocale))) {

i18npool/source/collator/collatorImpl.cxx
@@ -195,8 +195,6 @@ CollatorImpl::loadCachedCollator(const lang::Locale& rLocale, const OUString& rS
     }
 
     static sal_Unicode under = (sal_Unicode) '_';
-    static OUString tw("TW");
-    static OUString unicode("Unicode");
 
     sal_Int32 l = rLocale.Language.getLength();
     sal_Int32 c = rLocale.Country.getLength();
@@ -215,7 +213,7 @@ CollatorImpl::loadCachedCollator(const lang::Locale& rLocale, const OUString& rS
                      under).append(rSortAlgorithm).makeStringAndClear(), rSortAlgorithm)) ||
             (l > 0 && c > 0 && a > 0 && rLocale.Language == "zh" && (rLocale.Country == "HK" || rLocale.Country == "MO") &&
              // if the country code is HK or MO, one more step to try TW.
-             createCollator(rLocale, aBuf.append(rLocale.Language).append(under).append(tw).append(under).append(
+             createCollator(rLocale, aBuf.append(rLocale.Language).append(under).append("TW").append(under).append(
                      rSortAlgorithm).makeStringAndClear(), rSortAlgorithm)) ||
             (l > 0 && a > 0 &&
              // load service with name <base>_<lang>_<algorithm>
@@ -225,7 +223,7 @@ CollatorImpl::loadCachedCollator(const lang::Locale& rLocale, const OUString& rS
             (a > 0 &&
              createCollator(rLocale, rSortAlgorithm, rSortAlgorithm)) ||
             // load default service with name <base>_Unicode
-            createCollator(rLocale, unicode, rSortAlgorithm)) {
+            createCollator(rLocale, "Unicode", rSortAlgorithm)) {
                 return;
             } else {
                 cachedItem = NULL;

zhirshya/lo-tblr

commit 0ab37a159f12ba5eaea90bce45981b8aa785f630

Resolves: fdo#59586 Missing comma in particular PDF file
regression from d9e4c74811855de15f1bf2045c2c9b061a2d4dc6

- if ( LANGUAGE_KOREAN != aLang && LANGUAGE_KOREAN_JOHAB != aLang )
+ if (MsLangId::isKorean(aLang))

should obviously have been

- if ( LANGUAGE_KOREAN != aLang && LANGUAGE_KOREAN_JOHAB != aLang )
+ if (!MsLangId::isKorean(aLang))

Change-Id: I5d4407729c46111fc080e24b6a86d6740faa0f81

sw/source/core/text/portxt.cxx
@@ -94,7 +94,7 @@ static sal_uInt16 lcl_AddSpace( const SwTxtSizeInfo &rInf, const XubString* pStr
         LanguageType aLang =
             rInf.GetTxtFrm()->GetTxtNode()->GetLang( rInf.GetIdx(), 1, nScript );
 
-        if (MsLangId::isKorean(aLang))
+        if (!MsLangId::isKorean(aLang))
         {
             const SwLinePortion* pPor = rPor.GetPortion();
             if ( pPor && ( pPor->IsKernPortion() ||
@@ -202,7 +202,7 @@ static sal_uInt16 lcl_AddSpace( const SwTxtSizeInfo &rInf, const XubString* pStr
             LanguageType aLang =
                 rInf.GetTxtFrm()->GetTxtNode()->GetLang( nPos, 1, nNextScript );
 
-            if (MsLangId::isKorean(aLang))
+            if (!MsLangId::isKorean(aLang))
                 ++nCnt;
         }
     }

sw/source/core/txtnode/fntcache.cxx
@@ -1077,7 +1077,7 @@ void SwFntObj::DrawText( SwDrawTextInfo &rInf )
                     if ( ( SW_CJK == nActual || SW_LATIN == nActual ) && nSpaceAdd )
                     {
                         LanguageType aLang = rInf.GetFont()->GetLanguage( SW_CJK );
-                        if (MsLangId::isKorean(aLang))
+                        if (!MsLangId::isKorean(aLang))
                         {
                             long nSpaceSum = nSpaceAdd;
                             for ( sal_uInt16 nI = 0; nI < rInf.GetLen(); ++nI )
@@ -1253,7 +1253,7 @@ void SwFntObj::DrawText( SwDrawTextInfo &rInf )
                 {
                     LanguageType aLang = rInf.GetFont()->GetLanguage( SW_CJK );
 
-                    if (MsLangId::isKorean(aLang))
+                    if (!MsLangId::isKorean(aLang))
                     {
                         long nSpaceSum = nSpaceAdd;
                         for ( sal_uInt16 nI = 0; nI < rInf.GetLen(); ++nI )
@@ -1464,7 +1464,7 @@ void SwFntObj::DrawText( SwDrawTextInfo &rInf )
             {
                 LanguageType aLang = rInf.GetFont()->GetLanguage( SW_CJK );
 
-                if (MsLangId::isKorean(aLang))
+                if (!MsLangId::isKorean(aLang))
                 {
                     long nSpaceSum = nSpaceAdd;
                     for ( sal_uInt16 nI = 0; nI < rInf.GetLen(); ++nI )
@@ -2028,7 +2028,7 @@ xub_StrLen SwFntObj::GetCrsrOfst( SwDrawTextInfo &rInf )
         {
             LanguageType aLang = rInf.GetFont()->GetLanguage( SW_CJK );
 
-            if (MsLangId::isKorean(aLang))
+            if (!MsLangId::isKorean(aLang))
             {
                 long nSpaceSum = nSpaceAdd;
                 for ( sal_uInt16 nI = 0; nI < rInf.GetLen(); ++nI )