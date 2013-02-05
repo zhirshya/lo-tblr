LibreOffice/core

commit f593a2e4179b05ae1019372cde612cb242d1d27f

fdo#59419 fix RTF import of hex form of \r and \n
Change-Id: Ic700cdc67f756cafc454c326b73f680a8a47a6e8

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -945,6 +945,14 @@ void RTFDocumentImpl::singleChar(sal_uInt8 nValue, bool bRunProps)
 
 void RTFDocumentImpl::text(OUString& rString)
 {
+    if (rString.getLength() == 1)
+    {
+        // No cheating! Tokenizer ignores bare \r and \n, their hex \'0d / \'0a form doesn't count, either.
+        char ch = rString.getStr()[0];
+        if (ch == 0x0d || ch == 0x0a)
+            return;
+    }
+
     bool bRet = true;
     switch (m_aStates.top().nDestinationState)
     {

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -139,6 +139,7 @@ public:
     void testFdo44053();
     void testFdo48440();
     void testFdo58646();
+    void testFdo59419();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -249,6 +250,7 @@ void Test::run()
         {"fdo44053.rtf", &Test::testFdo44053},
         {"fdo48440.rtf", &Test::testFdo48440},
         {"fdo58646.rtf", &Test::testFdo58646},
+        {"fdo59419.rtf", &Test::testFdo59419},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1076,6 +1078,14 @@ void Test::testFdo58646()
 {
     // Page break was ignored inside a continous section, on title page.
     CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
+void Test::testFdo59419()
+{
+    // Junk to be ignored broke import of the table.
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

sw/qa/extras/rtfimport/data/fdo59419.rtf
@@ -1,0 +1,5 @@
+{\rtf1
+\trowd\cellx3000\pard\plain\intbl the next cell will be empty\cell\row
+\pard\trowd\cellx3000\pard\plain\intbl\'0d\cell\row
+\pard\par
+}

zhirshya/lo-tblr

commit 9c0803e49644633a411a84ebe5c0eab15d35814a

RTF import: fix import of \line symbol
"Required line break" was eaten by the newly added code in text() to
ignore \'0a.  (regression from f593a2e4179b05ae1019372cde612cb242d1d27f)

Change-Id: Id32687c40a17ddb99196635cedfc850182705799

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1605,12 +1605,16 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
         checkUnicode();
     setNeedSect();
     RTFSkipDestination aSkip(*this);
-    sal_uInt8 cCh = 0;
 
+    if (RTF_LINE == nKeyword)
+    {   // very special handling since text() will eat lone '\n'
+        singleChar('\n');
+        return 0;
+    }
     // Trivial symbols
+    sal_uInt8 cCh = 0;
     switch (nKeyword)
     {
-        case RTF_LINE: cCh = '\n'; break;
         case RTF_TAB: cCh = '\t'; break;
         case RTF_BACKSLASH: cCh = '\\'; break;
         case RTF_LBRACE: cCh = '{'; break;

sw/qa/extras/rtfimport/data/fdo58646line.rtf
@@ -1,0 +1,1 @@
+{\rtf1 foo\line bar}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -138,6 +138,7 @@ public:
     void testFdo58933();
     void testFdo44053();
     void testFdo48440();
+    void testFdo58646line();
     void testFdo58646();
     void testFdo59419();
     void testFdo58076_2();
@@ -250,6 +251,7 @@ void Test::run()
         {"fdo58933.rtf", &Test::testFdo58933},
         {"fdo44053.rtf", &Test::testFdo44053},
         {"fdo48440.rtf", &Test::testFdo48440},
+        {"fdo58646line.rtf", &Test::testFdo58646line},
         {"fdo58646.rtf", &Test::testFdo58646},
         {"fdo59419.rtf", &Test::testFdo59419},
         {"fdo58076-2.rtf", &Test::testFdo58076_2},
@@ -1074,6 +1076,12 @@ void Test::testFdo48440()
 {
     // Page break was ignored.
     CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
+void Test::testFdo58646line()
+{
+    // \line symbol was ignored
+    getParagraph(1, "foo\nbar");
 }
 
 void Test::testFdo58646()