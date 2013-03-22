LibreOffice/core

commit f0cd6fe9075cd0aa00162474784ad804a07ed138

use document settings / embed fonts when exporting odp
Change-Id: I9e8db1ae08b570f093b13d50bfde823c85cdba95

xmloff/source/draw/sdxmlexp.cxx
@@ -2901,4 +2901,17 @@ OUString SAL_CALL SdXMLExport::getImplementationName() throw( uno::RuntimeExcept
     }
 }
 
+XMLFontAutoStylePool* SdXMLExport::CreateFontAutoStylePool()
+{
+    bool bEmbedFonts = false;
+    Reference< lang::XMultiServiceFactory > xFac( GetModel(), UNO_QUERY );
+    if( xFac.is() )
+    {
+        Reference< beans::XPropertySet > xProps( xFac->createInstance( OUString( RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.document.Settings" ) ) ), UNO_QUERY );
+        if( xProps.is() )
+            xProps->getPropertyValue("EmbedFonts") >>= bEmbedFonts;
+    }
+    return new XMLFontAutoStylePool( *this, bEmbedFonts );
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

xmloff/source/draw/sdxmlexp_impl.hxx
@@ -174,6 +174,7 @@ class SdXMLExport : public SvXMLExport
 protected:
     virtual void GetViewSettings(com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aProps);
     virtual void GetConfigurationSettings(com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& aProps);
+    virtual XMLFontAutoStylePool* CreateFontAutoStylePool();
 
 public:
     SdXMLExport(

zhirshya/lo-tblr

commit a31b8ae01ec925f01901bcc80168430bc3567858

SdXMLExport::CreateFontAutoStylePool: "EmbedFonts": ignore exceptions
(regression from f0cd6fe9075cd0aa00162474784ad804a07ed138)

Change-Id: Ia86c0d168e9dd24555e7431666c0d783daef3b6a

xmloff/source/draw/sdxmlexp.cxx
@@ -2912,15 +2912,22 @@ OUString SAL_CALL SdXMLExport::getImplementationName() throw( uno::RuntimeExcept
 XMLFontAutoStylePool* SdXMLExport::CreateFontAutoStylePool()
 {
     bool bEmbedFonts = false;
-    if( getExportFlags() & EXPORT_CONTENT ) {
+    if (getExportFlags() & EXPORT_CONTENT)
+    {
         Reference< lang::XMultiServiceFactory > xFac( GetModel(), UNO_QUERY );
         if( xFac.is() )
+        {
+            Reference<beans::XPropertySet> const xProps( xFac->createInstance(
+                         "com.sun.star.document.Settings"), UNO_QUERY );
+            if (xProps.is())
             {
-                Reference< beans::XPropertySet > xProps( xFac->createInstance( OUString( "com.sun.star.document.Settings" ) ), UNO_QUERY );
-                if( xProps.is() )
+                try // clipboard document doesn't have shell so throws here
+                {
                     xProps->getPropertyValue("EmbedFonts") >>= bEmbedFonts;
-
+                }
+                catch (uno::Exception const&) { }
             }
+        }
     }
 
     XMLFontAutoStylePool *pPool = new XMLFontAutoStylePool( *this, bEmbedFonts );