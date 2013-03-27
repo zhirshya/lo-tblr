LibreOffice/core

commit eb69f807c4709a908222cb57b45a8733be663c56

coverity#440839 Resource leak in object
Change-Id: Icf65fdaac8c5ed852a85dce18fc49d401c12074e

sfx2/source/dialog/tabdlg.cxx
@@ -175,6 +175,10 @@ struct TabDlg_Impl
         pData           ( new SfxTabDlgData_Impl( nCnt ) ),
         pController     ( NULL )
     {}
+    ~TabDlg_Impl()
+    {
+        delete(pData);
+    }
 };
 
 Data_Impl* Find( SfxTabDlgData_Impl& rArr, sal_uInt16 nId, sal_uInt16* pPos = 0 );

zhirshya/lo-tblr

commit 15b22c6be61c67280f4a956d954f419886bd8fef

sfx2: TabDlg_Impl::pData is deleted twice
(regression from eb69f807c4709a908222cb57b45a8733be663c56)

Change-Id: I1752d60496a572152b571e871db818cf8c9264ed

sfx2/source/dialog/tabdlg.cxx
@@ -177,7 +177,8 @@ struct TabDlg_Impl
     {}
     ~TabDlg_Impl()
     {
-        delete(pData);
+        delete pController;
+        delete pData;
     }
 };
 
@@ -534,8 +535,6 @@ SfxTabDialog::~SfxTabDialog()
         delete pDataObject;
     }
 
-    delete pImpl->pController;
-    delete pImpl->pData;
     delete pImpl;
     delete pOutSet;
     delete pExampleSet;