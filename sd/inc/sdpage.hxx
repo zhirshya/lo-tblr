LibreOffice/core

fdo#59071 Slideshow: allow automatic transitions with sub-second
Enable slide transition each 0.25 second instead of whole second.

Change-Id: I1907a1baf30cede91a0438d021e003204ea06651

sd/inc/sdpage.hxx
@@ -115,7 +115,7 @@ protected:
     sd::ScopeLock maLockAutoLayoutArrangement;
     sal_Bool    mbSelected;               ///< selection identifier
     PresChange  mePresChange;             ///< manual / automatic / semi automatic
-    sal_uInt32  mnTime;                   ///< Display time in seconds
+    double      mnTime;                   ///< Display time in seconds
     sal_Bool    mbSoundOn;                ///< with / without sound.
     sal_Bool    mbExcluded;               ///< will (not) be displayed during show.
     OUString    maLayoutName;             ///< Name of the layout
@@ -223,8 +223,8 @@ public:
     void        SetPresChange(PresChange eChange)    { mePresChange = eChange; }
     PresChange  GetPresChange() const                { return mePresChange; }
 
-    void        SetTime(sal_uInt32 nNewTime)             { mnTime = nNewTime; }
-    sal_uInt32      GetTime() const                      { return mnTime; }
+    void        SetTime(double nNewTime)             { mnTime = nNewTime; }
+    double      GetTime() const                      { return mnTime; }
 
     void        SetSound(sal_Bool bNewSoundOn)           { mbSoundOn = bNewSoundOn; }
     sal_Bool        IsSoundOn() const                    { return mbSoundOn; }

sd/source/filter/html/htmlex.hxx
@@ -114,7 +114,7 @@ class HtmlExport
     bool mbDownload;
 
     bool mbAutoSlide;
-    sal_uInt32  mnSlideDuration;
+    double  mnSlideDuration;
     bool mbSlideSound;
     bool mbHiddenSlides;
     bool mbEndless;

zhirshya/lo-tblr

sd/inc/sdpage.hxx
@@ -115,7 +115,7 @@ protected:
     sd::ScopeLock maLockAutoLayoutArrangement;
     sal_Bool    mbSelected;               ///< selection identifier
     PresChange  mePresChange;             ///< manual / automatic / semi automatic
-    sal_uInt32  mnTime;                   ///< Display time in seconds
+    double      mfTime;                   ///< Display time in seconds
     sal_Bool    mbSoundOn;                ///< with / without sound.
     sal_Bool    mbExcluded;               ///< will (not) be displayed during show.
     OUString    maLayoutName;             ///< Name of the layout
@@ -223,8 +223,8 @@ public:
     void        SetPresChange(PresChange eChange)    { mePresChange = eChange; }
     PresChange  GetPresChange() const                { return mePresChange; }
 
-    void        SetTime(sal_uInt32 nNewTime)             { mnTime = nNewTime; }
-    sal_uInt32      GetTime() const                      { return mnTime; }
+    void        SetTime(double fNewTime)             { mfTime = fNewTime; }
+    double      GetTime() const                      { return mfTime; }
 
     void        SetSound(sal_Bool bNewSoundOn)           { mbSoundOn = bNewSoundOn; }
     sal_Bool        IsSoundOn() const                    { return mbSoundOn; }

sd/source/filter/html/htmlex.hxx
@@ -114,7 +114,7 @@ class HtmlExport
     bool mbDownload;
 
     bool mbAutoSlide;
-    sal_uInt32  mnSlideDuration;
+    double  mfSlideDuration;
     bool mbSlideSound;
     bool mbHiddenSlides;
     bool mbEndless;