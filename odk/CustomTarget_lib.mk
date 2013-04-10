LibreOffice/core

commit 613221bde1744265c336c2b5f2ade0bfb9b22d16

convert odk to gbuild and add to tail_build
Change-Id: I8e8de7f2bb87cce7916c7c2df24c1b0ddaea55c0
Reviewed-on: https://gerrit.libreoffice.org/2288
Reviewed-by: Peter Foley <pefoley2@verizon.net>
Tested-by: Peter Foley <pefoley2@verizon.net>

odk/CustomTarget_lib.mk
@@ -1,0 +1,40 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,odk/odkcommon/lib))
+
+define odk_lib
+odkcommon_ZIPLIST += lib/$(1)
+$(call gb_CustomTarget_get_target,odk/odkcommon/lib): $(odk_WORKDIR)/lib/$(1)
+$(odk_WORKDIR)/lib/$(1): $(call gb_Library_get_target,$(1))
+	mkdir -p $$(dir $$@)
+	$$(call gb_Output_announce,$$(subst $$(WORKDIR)/,,$$@),build,CPY,1)
+	cp $$< $$@
+endef
+
+ifeq ($(COM),MSC)
+odk_LIBLIST := store \
+	reg \
+	sal \
+	salhelper \
+	cppu \
+	cppuhelper \
+	purpenvhelper
+
+$(foreach lib,$(odk_LIBLIST),$(eval $(call odk_lib,$(lib))))
+else ifeq ($(OS),LINUX)
+odkcommon_ZIPLIST += lib/libsalcpprt.a
+$(eval $(call gb_CustomTarget_register_target,odk/odkcommon/lib,libsalcpprt.a))
+$(odk_WORKDIR)/lib/libsalcpprt.a: $(call gb_StaticLibrary_get_target,salcpprt)
+	mkdir -p $(dir $@)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,CPY,1)
+	cp $< $@
+endif
+
+# vim: set noet sw=4 ts=4:

Module_tail_build.mk
@@ -132,6 +132,7 @@ $(eval $(call gb_Module_add_moduledirs,tail_build,\
 	$(call gb_Helper_optional,NLPSOLVER,nlpsolver) \
 	np_sdk \
 	o3tl \
+	$(call gb_Helper_optional,ODK,odk) \
 	offapi \
 	officecfg \
 	oovbaapi \

Repository.mk
@@ -73,6 +73,7 @@ $(eval $(call gb_Helper_register_executables,NONE, \
     uiex \
     ulfconv \
     ulfex \
+    $(call gb_Helper_optional,ODK,unoapploader) \
     xpdfimport \
     xrmex \
 ))
@@ -542,6 +543,7 @@ $(eval $(call gb_Helper_register_libraries,PLAINLIBS_OOO, \
     so_activex_x64 \
     sysdtrans \
 	thidxmsi \
+	unowinreg \
 ))
 endif
 

RepositoryModule_ooo.mk
@@ -140,6 +140,7 @@ $(eval $(call gb_Module_add_moduledirs,ooo,\
 	$(call gb_Helper_optional,NLPSOLVER,nlpsolver) \
 	np_sdk \
 	o3tl \
+	$(call gb_Helper_optional,ODK,odk) \
 	offapi \
 	officecfg \
 	oovbaapi \

odk/CustomTarget_autodoc.mk
@@ -1,0 +1,25 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,odk/odkcommon/docs/common/ref))
+
+odkcommon_ZIPLIST += docs/common/ref
+
+$(eval $(call gb_CustomTarget_register_target,odk/odkcommon/docs/common/ref,module-ix.html))
+$(odk_WORKDIR)/docs/common/ref/module-ix.html: $(SRCDIR)/odk/pack/copying/idl_chapter_refs.txt \
+	$(SRCDIR)/odk/docs/common/ref/idl.css $(call gb_UnoApi_get_target,offapi) \
+	$(call gb_Executable_get_target,autodoc)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),AUD,1)
+	$(call gb_Executable_get_target,autodoc) -html $(dir $@) \
+		-dvgroot "http://wiki.services.openoffice.org/wiki" \
+		-name "LibreOffice $(PRODUCTVERSION) API" \
+		-lg idl -dvgfile $< -t $(OUTDIR)/idl
+
+
+# vim: set noet sw=4 ts=4:

odk/CustomTarget_bin.mk
@@ -1,0 +1,42 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,odk/odkcommon/bin))
+
+odk_EXELIST := cppumaker \
+	regcompare \
+	idlc \
+	javamaker \
+	autodoc \
+	unoapploader \
+	uno-skeletonmaker \
+	$(if $(SYSTEM_UCPP),,ucpp) \
+	$(if $(filter WNT,$(OS)),climaker)
+
+define odk_exe
+odkcommon_ZIPLIST += bin/$(1)$(gb_Executable_EXT)
+$(call gb_CustomTarget_get_target,odk/odkcommon/bin): $(odk_WORKDIR)/bin/$(1)$(gb_Executable_EXT)
+$(odk_WORKDIR)/bin/$(1)$(gb_Executable_EXT): $(call gb_Executable_get_target,$(1))
+	mkdir -p $$(dir $$@)
+	$$(call gb_Output_announce,$$(subst $$(WORKDIR)/,,$$@),build,CPY,1)
+	cp $$< $$@
+endef
+
+$(foreach exe,$(odk_EXELIST),$(eval $(call odk_exe,$(exe))))
+
+ifeq ($(OS),MACOSX)
+odkcommon_ZIPLIST += bin/addsym-macosx.sh
+$(eval $(call gb_CustomTarget_register_target,odk/odkcommon/bin,addsym-macosx.sh))
+$(odk_WORKDIR)/bin/addsym-macosx.sh: $(SRCDIR)/odk/pack/copying/addsym-macosx.sh
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,CPY,1)
+	cp $< $@
+	chmod 755 $@
+endif
+
+# vim: set noet sw=4 ts=4:

odk/CustomTarget_check.mk
@@ -1,0 +1,59 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,odk/check))
+
+$(eval $(call gb_CustomTarget_register_targets,odk/check,\
+	checkbin \
+	checkinc \
+	checkidl \
+))
+
+odk_PLATFORM := $(if $(filter WNT,$(OS)),\
+	$(if $(filter GCC,$(COM)),mingw,windows),\
+	$(if $(filter SOLARIS,$(OS)),\
+	$(if $(filter SPARC,$(CPUNAME)),solsparc,\
+	$(if $(filter U,$(CPU)),solsparc64,solintel)),\
+	$(if $(filter LINUX,$(OS)),linux,\
+	$(if $(filter NETBSD,$(OS)),netbsd,\
+	$(if $(filter FREEBSD,$(OS)),freebsd,\
+	$(if $(filter DRAGONFLY,$(OS)),dragonfly,\
+	$(if $(filter MACOSX,$(OS)),macosx,\
+	$(if $(filter AIX,$(OS)),aix))))))))
+
+$(call gb_CustomTarget_get_workdir,odk/check)/checkbin: $(SRCDIR)/odk/util/check.pl $(call gb_CustomTarget_get_target,odk/odkcommon)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),CHK,1)
+	touch $@
+	$(PERL) $< $(call gb_CustomTarget_get_workdir,odk/odkcommon) \
+		$(odk_PLATFORM) "$(gb_Exectable_EXT)" $@
+	cat $@
+
+$(call gb_CustomTarget_get_workdir,odk/check)/checkidl: $(SRCDIR)/odk/util/checkdiff.pl $(call gb_CustomTarget_get_target,odk/odkcommon)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),CHK,1)
+	touch $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon) $(OUTDIR)/idl \
+		2>&1 | $(PERL) $< $@
+	cat $@
+
+$(call gb_CustomTarget_get_workdir,odk/check)/checkinc: $(SRCDIR)/odk/util/checkdiff.pl $(call gb_CustomTarget_get_target,odk/odkcommon)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),CHK,1)
+	touch $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/osl \	$(OUTDIR)/inc/osl 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/sal \	$(OUTDIR)/inc/sal 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/store \	$(OUTDIR)/inc/store 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/typelib \	$(OUTDIR)/inc/typelib 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/uno \	$(OUTDIR)/inc/uno 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/bridges \	$(OUTDIR)/inc/bridges 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/cppu \	$(OUTDIR)/inc/cppu 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/com/sun/star/uno \	$(OUTDIR)/inc/com/sun/star/uno 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/cppuhelper \	$(OUTDIR)/inc/cppuhelper 2>&1 | $(PERL) $< $@
+	diff -br $(call gb_CustomTarget_get_workdir,odk/odkcommon)/include/osl \	$(OUTDIR)/inc/osl 2>&1 | $(PERL) $< $@
+	cat $@
+
+# vim: set noet sw=4 ts=4:

odk/CustomTarget_classes.mk
@@ -1,0 +1,36 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,odk/odkcommon/classes))
+
+$(eval $(call gb_JavaClassSet_JavaClassSet,loader))
+
+$(eval $(call gb_JavaClassSet_add_sourcefiles,loader,\
+	odk/source/com/sun/star/lib/loader/Loader \
+	odk/source/com/sun/star/lib/loader/InstallationFinder \
+	odk/source/com/sun/star/lib/loader/WinRegKey \
+	odk/source/com/sun/star/lib/loader/WinRegKeyException \
+))
+
+odkcommon_ZIPLIST += classes/com/sun/star/lib/loader
+$(call gb_CustomTarget_get_target,odk/odkcommon/classes): $(odk_WORKDIR)/classes/java.done
+$(odk_WORKDIR)/classes/java.done: $(call gb_JavaClassSet_get_target,loader)
+	mkdir -p $(dir $@)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,CPY,1)
+	cp -a $(call gb_JavaClassSet_get_classdir,loader)/com $(dir $@)
+	touch $@
+
+odkcommon_ZIPLIST += classes/win/unowinreg.dll
+$(eval $(call gb_CustomTarget_register_target,odk/odkcommon/classes,win/unowinreg.dll))
+$(odk_WORKDIR)/classes/win/unowinreg.dll: $(call gb_CustomTarget_get_workdir,odk/unowinreg)/unowinreg.dll
+	mkdir -p $(dir $@)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,CPY,1)
+	cp $< $@
+
+# vim: set noet sw=4 ts=4:

zhirshya/lo-tblr

commit 1fa7bc7371576395514bc7d7ee40f05f85bb23c3

odk: fix packaging of WNT import libraries
(regression from 613221bde1744265c336c2b5f2ade0bfb9b22d16)

Change-Id: I32775c27e7d17bfc78562f231a4061630c5b7f1a

odk/CustomTarget_lib.mk
@@ -10,9 +10,10 @@
 $(eval $(call gb_CustomTarget_CustomTarget,odk/odkcommon/lib))
 
 define odk_lib
-odkcommon_ZIPLIST += lib/$(1)
-$(call gb_CustomTarget_get_target,odk/odkcommon/lib): $(odk_WORKDIR)/lib/$(1)
-$(odk_WORKDIR)/lib/$(1): $(call gb_Library_get_target,$(1))
+odkcommon_ZIPLIST += lib/$(notdir $(1))
+$(call gb_CustomTarget_get_target,odk/odkcommon/lib) : \
+	$(odk_WORKDIR)/lib/$(notdir $(1))
+$(odk_WORKDIR)/lib/$(notdir $(1)) : $(1)
 	mkdir -p $$(dir $$@)
 	$$(call gb_Output_announce,$$(subst $$(WORKDIR)/,,$$@),build,CPY,1)
 	cp $$< $$@
@@ -27,7 +28,7 @@ odk_LIBLIST := store \
 	cppuhelper \
 	purpenvhelper
 
-$(foreach lib,$(odk_LIBLIST),$(eval $(call odk_lib,$(lib))))
+$(foreach lib,$(odk_LIBLIST),$(eval $(call odk_lib,$(call gb_Library_get_target,$(lib)))))
 else ifeq ($(OS),LINUX)
 odkcommon_ZIPLIST += lib/libsalcpprt.a
 $(eval $(call gb_CustomTarget_register_target,odk/odkcommon/lib,libsalcpprt.a))