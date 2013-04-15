LibreOffice/core

commit 4811c2dc9f94929b60e7e2e0077871aad14fdc3a

adapt all externals to build against MSVC debug runtime
Add patches and/or tweaks to the following modules:
curl, cppunit, icu, lcms2, libxml2, libxslt, libxmlsec,
lpsolve, nss, openssl, python3

lcms2 has an inconsistency where the .lib and the .dll don't agree on
the .dll name.

openssl gets a honorable mention because apparently it's undocumented
custom build system can build with /MDd if one picks the right
configuration but i couldn't figure out how to do that in an hour of
trying, and just patched the release config instead.

Change-Id: I7854a0fc85247e398d561b4f513d09fe2d1ebb3c

RepositoryFixes.mk
@@ -107,11 +107,13 @@ gb_Library_FILENAMES := $(patsubst xmlsec1-nss:ixmlsec1-nss%,xmlsec1-nss:libxmls
 gb_Library_FILENAMES := $(patsubst z:z%,z:zlib%,$(gb_Library_FILENAMES))
 
 
+gb_Library_NOILIBFILENAMES_D := \
+    icudt icuin icule icuuc \
+    lcms2 \
+
 # change the names of all import libraries that don't have an "i" prefix as in our standard naming schema
 gb_Library_NOILIBFILENAMES := \
-    icudt icuin icule icuuc \
     langtag \
-    lcms2 \
     lpsolve55 \
     xpcom \
     xpcom_core \
@@ -129,10 +131,12 @@ gb_Library_LIBLIBFILENAMES := \
 
 gb_Library_FILENAMES := \
 	$(filter-out $(foreach lib,$(gb_Library_NOILIBFILENAMES),$(lib):%) \
+				 $(foreach lib,$(gb_Library_NOILIBFILENAMES_D),$(lib):%) \
 				 $(foreach lib,$(gb_Library_LIBLIBFILENAMES),$(lib):%) \
 		,$(gb_Library_FILENAMES))
 gb_Library_FILENAMES += \
 	$(foreach lib,$(gb_Library_NOILIBFILENAMES),$(lib):$(lib)$(gb_Library_PLAINEXT)) \
+	$(foreach lib,$(gb_Library_NOILIBFILENAMES_D),$(lib):$(lib)d$(gb_Library_PLAINEXT)) \
 	$(foreach lib,$(gb_Library_LIBLIBFILENAMES),$(lib):lib$(lib)$(gb_Library_PLAINEXT))
 
 endif # ifeq ($(COM),GCC)

RepositoryExternal.mk
@@ -2299,7 +2299,7 @@ $(call gb_LinkTarget__use_python_headers,$(1))
 
 ifeq ($(OS),WNT)
 $(call gb_LinkTarget_add_libs,$(1),\
-	python$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR).lib \
+	python$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR)$(if $(MSVC_USE_DEBUG_RUNTIME),_d).lib \
 )
 else ifeq ($(OS),MACOSX)
 $(call gb_LinkTarget_add_libs,$(1),\

cppunit/ExternalPackage_cppunit.mk
@@ -12,9 +12,9 @@ $(eval $(call gb_ExternalPackage_use_external_project,cppunit,cppunit))
 
 ifeq ($(OS),WNT)
 ifeq ($(COM),MSC)
-$(eval $(call gb_ExternalPackage_add_file,cppunit,bin/cppunit_dll.dll,src/cppunit/ReleaseDll/cppunit_dll.dll))
-$(eval $(call gb_ExternalPackage_add_file,cppunit,lib/icppunit_dll.lib,src/cppunit/ReleaseDll/cppunit_dll.lib))
-$(eval $(call gb_ExternalPackage_add_file,cppunit,bin/DllPlugInTester_dll.exe,src/DllPlugInTester/ReleaseDll/DllPlugInTester_dll.exe))
+$(eval $(call gb_ExternalPackage_add_file,cppunit,bin/$(if $(MSVC_USE_DEBUG_RUNTIME),cppunitd_dll.dll,cppunit_dll.dll),src/cppunit/$(if $(MSVC_USE_DEBUG_RUNTIME),DebugDll/cppunitd_dll.dll,ReleaseDll/cppunit_dll.dll)))
+$(eval $(call gb_ExternalPackage_add_file,cppunit,lib/icppunit_dll.lib,src/cppunit/$(if $(MSVC_USE_DEBUG_RUNTIME),DebugDll/cppunitd_dll.lib,ReleaseDll/cppunit_dll.lib)))
+$(eval $(call gb_ExternalPackage_add_file,cppunit,bin/DllPlugInTester_dll.exe,src/DllPlugInTester/$(if $(MSVC_USE_DEBUG_RUNTIME),DebugDll/DllPlugInTesterd_dll.exe,ReleaseDll/DllPlugInTester_dll.exe)))
 else
 $(eval $(call gb_ExternalPackage_add_file,cppunit,bin/libcppunit-1-13-0.dll,src/cppunit/.libs/libcppunit-1-13-0.dll))
 $(eval $(call gb_ExternalPackage_add_file,cppunit,lib/libcppunit.dll.a,src/cppunit/.libs/libcppunit.dll.a))

cppunit/ExternalProject_cppunit.mk
@@ -20,17 +20,17 @@ ifeq ($(OS)$(COM),WNTMSC)
 ifeq ($(VCVER),90)
 $(call gb_ExternalProject_get_state_target,cppunit,build) :
 	$(call gb_ExternalProject_run,build,\
-		$(COMPATH)/vcpackages/vcbuild.exe cppunit_dll.vcproj "Release|Win32" \
+		$(COMPATH)/vcpackages/vcbuild.exe cppunit_dll.vcproj "$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release)|Win32" \
 		&& cd ../DllPlugInTester \
-		&& $(COMPATH)/vcpackages/vcbuild.exe DllPlugInTester.vcproj "Release|Win32" \
+		&& $(COMPATH)/vcpackages/vcbuild.exe DllPlugInTester.vcproj "$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release)|Win32" \
 	,src/cppunit)
 else
 $(call gb_ExternalProject_get_state_target,cppunit,build) :
 	$(call gb_ExternalProject_run,build,\
-		msbuild.exe cppunit_dll.vcxproj /p:Configuration=Release \
+		msbuild.exe cppunit_dll.vcxproj /p:Configuration=$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release) \
 		$(if $(filter 110,$(VCVER)),/p:PlatformToolset=v110 /p:VisualStudioVersion=11.0) \
 		&& cd ../DllPlugInTester \
-		&& msbuild.exe DllPlugInTester.vcxproj /p:Configuration=Release \
+		&& msbuild.exe DllPlugInTester.vcxproj /p:Configuration=$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release) \
 		$(if $(filter 110,$(VCVER)),/p:PlatformToolset=v110 /p:VisualStudioVersion=11.0) \
 	,src/cppunit)
 endif

curl/ExternalProject_curl.mk
@@ -49,7 +49,8 @@ else ifeq ($(COM),MSC)
 
 $(call gb_ExternalProject_get_state_target,curl,build):
 	$(call gb_ExternalProject_run,build,\
-		MAKEFLAGS= LIB="$(ILIB)" nmake -f Makefile.vc9 cfg=release-dll \
+		MAKEFLAGS= LIB="$(ILIB)" nmake -f Makefile.vc9 \
+			cfg=$(if $(MSVC_USE_DEBUG_RUNTIME),debug-dll,release-dll) \
 			EXCFLAGS="/EHa /Zc:wchar_t- /D_CRT_SECURE_NO_DEPRECATE /DUSE_WINDOWS_SSPI $(SOLARINC)" $(if $(filter X86_64,$(CPUNAME)),MACHINE=X64) \
 	,lib)
 

curl/curl-7.26.0_win.patch
@@ -43,7 +43,14 @@
  
  CFGSET     = FALSE
  
-@@ -620,5 +620,5 @@
+@@ -620,11 +620,11 @@
+ debug-dll-ssl-dll\libcurl.res \
+ debug-dll-zlib-dll\libcurl.res \
+ debug-dll-ssl-dll-zlib-dll\libcurl.res: libcurl.rc
+-	rc /dDEBUGBUILD=1 /Fo $@ libcurl.rc
++	rc $(SOLARINC) /dDEBUGBUILD=1 /Fo $@ libcurl.rc
+
+ release-dll\libcurl.res \
  release-dll-ssl-dll\libcurl.res \
  release-dll-zlib-dll\libcurl.res \
  release-dll-ssl-dll-zlib-dll\libcurl.res: libcurl.rc

icu/ExternalPackage_icu.mk
@@ -33,18 +33,18 @@ $(eval $(call gb_ExternalPackage_add_files,icu,bin,\
 
 else
 $(eval $(call gb_ExternalPackage_add_files,icu,lib,\
-	source/lib/icudt.lib \
-	source/lib/icuuc.lib \
-	source/lib/icuin.lib \
-	source/lib/icule.lib \
-	source/lib/icutu.lib\
+	source/lib/icudt$(if $(MSVC_USE_DEBUG_RUNTIME),d).lib \
+	source/lib/icuuc$(if $(MSVC_USE_DEBUG_RUNTIME),d).lib \
+	source/lib/icuin$(if $(MSVC_USE_DEBUG_RUNTIME),d).lib \
+	source/lib/icule$(if $(MSVC_USE_DEBUG_RUNTIME),d).lib \
+	source/lib/icutu$(if $(MSVC_USE_DEBUG_RUNTIME),d).lib\
 ))
 $(eval $(call gb_ExternalPackage_add_files,icu,bin,\
-	source/lib/icudt$(ICU_MAJOR).dll \
-	source/lib/icuuc$(ICU_MAJOR).dll \
-	source/lib/icuin$(ICU_MAJOR).dll \
-	source/lib/icule$(ICU_MAJOR).dll \
-	source/lib/icutu$(ICU_MAJOR).dll \
+	source/lib/icudt$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
+	source/lib/icuuc$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
+	source/lib/icuin$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
+	source/lib/icule$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
+	source/lib/icutu$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(ICU_MAJOR).dll \
 	source/bin/genccode.exe \
 	source/bin/genbrk.exe \
 	source/bin/gencmn.exe \

icu/ExternalProject_icu.mk
@@ -19,11 +19,15 @@ icu_CPPFLAGS:="-DHAVE_GCC_ATOMICS=$(if $(filter TRUE,$(GCC_HAVE_BUILTIN_ATOMIC))
 
 ifeq ($(OS),WNT)
 
+# Note: runConfigureICU ignores everything following the platform name!
 ifeq ($(COM),MSC)
 $(call gb_ExternalProject_get_state_target,icu,build) :
 	$(call gb_ExternalProject_run,build,\
 		export LIB="$(ILIB)" \
-		&& CFLAGS="$(SOLARINC)" CPPFLAGS="$(SOLARINC)" CXXFLAGS="$(SOLARINC)" ./runConfigureICU Cygwin/MSVC \
+		&& CFLAGS="$(SOLARINC)" CPPFLAGS="$(SOLARINC)" CXXFLAGS="$(SOLARINC)" \
+			./runConfigureICU \
+			$(if $(MSVC_USE_DEBUG_RUNTIME),--enable-debug --disable-release) \
+			Cygwin/MSVC \
 		&& $(MAKE) \
 	,source)
 else

zhirshya/lo-tblr

commit 209c2f2d82d220dbfefc142b66de0958c0980735

RepositoryFixes: add missing check for MSVC_USE_DEBUG_RUNTIME
(regression from 4811c2dc9f94929b60e7e2e0077871aad14fdc3a)

Change-Id: Ide12da645332e789fe7742c34b5af219b94316ab

RepositoryFixes.mk
@@ -136,7 +136,7 @@ gb_Library_FILENAMES := \
 		,$(gb_Library_FILENAMES))
 gb_Library_FILENAMES += \
 	$(foreach lib,$(gb_Library_NOILIBFILENAMES),$(lib):$(lib)$(gb_Library_PLAINEXT)) \
-	$(foreach lib,$(gb_Library_NOILIBFILENAMES_D),$(lib):$(lib)d$(gb_Library_PLAINEXT)) \
+	$(foreach lib,$(gb_Library_NOILIBFILENAMES_D),$(lib):$(lib)$(if $(MSVC_USE_DEBUG_RUNTIME),d)$(gb_Library_PLAINEXT)) \
 	$(foreach lib,$(gb_Library_LIBLIBFILENAMES),$(lib):lib$(lib)$(gb_Library_PLAINEXT))
 
 endif # ifeq ($(COM),GCC)