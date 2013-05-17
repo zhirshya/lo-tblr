LibreOffice/core

commit 115054fef08998c56cba8f14472df1d15007f635

fdo#34548 don't crash on undoing text frame removal
Change-Id: I48b95c6c0cbfb2f4c4b09cc90b254b96de40b3e9

svx/source/svdraw/svdundo.cxx
@@ -18,6 +18,8 @@
  */
 
 
+#include <svl/lstner.hxx>
+
 #include <svx/svdundo.hxx>
 #include "svx/svditext.hxx"
 #include <svx/svdotext.hxx>
@@ -655,11 +657,55 @@ OUString SdrUndoGeoObj::GetComment() const
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+class SdrUndoObjList::ObjListListener : public SfxListener
+{
+public:
+    ObjListListener(SdrUndoObjList& rThat, SdrObject& rObject, SfxBroadcaster& rBroadcaster);
+    ~ObjListListener();
+
+private:
+    virtual void Notify(SfxBroadcaster& rBroadcaster, const SfxHint& rHint);
+
+private:
+    SdrUndoObjList& m_rThat;
+    SdrObject& m_rObject;
+    SfxBroadcaster* m_pBroadcaster;
+};
+
+SdrUndoObjList::ObjListListener::ObjListListener(SdrUndoObjList& rThat, SdrObject& rObject, SfxBroadcaster& rBroadcaster)
+    : m_rThat(rThat)
+    , m_rObject(rObject)
+    , m_pBroadcaster(&rBroadcaster)
+{
+    StartListening(*m_pBroadcaster);
+}
+
+SdrUndoObjList::ObjListListener::~ObjListListener()
+{
+    if (m_pBroadcaster)
+        EndListening(*m_pBroadcaster);
+}
+
+void SdrUndoObjList::ObjListListener::Notify(SfxBroadcaster&, const SfxHint& rHint)
+{
+    const SdrHint* const pSdrHint(dynamic_cast<const SdrHint*>(&rHint));
+    if (pSdrHint)
+    {
+        if ((pSdrHint->GetObject() == &m_rObject) && (pSdrHint->GetKind() == HINT_OBJCHG))
+        {
+            const sal_uInt32 nNewOrdNum(m_rObject.GetOrdNum());
+            if (nNewOrdNum != m_rThat.GetOrdNum())
+                m_rThat.SetOrdNum(nNewOrdNum);
+        }
+    }
+}
+
 SdrUndoObjList::SdrUndoObjList(SdrObject& rNewObj, bool bOrdNumDirect)
 :   SdrUndoObj(rNewObj),
     bOwner(sal_False),
     pView(NULL),
-    pPageView(NULL)
+    pPageView(NULL),
+    m_pListener(NULL)
 {
     pObjList=pObj->GetObjList();
     if (bOrdNumDirect) {
@@ -667,10 +713,14 @@ SdrUndoObjList::SdrUndoObjList(SdrObject& rNewObj, bool bOrdNumDirect)
     } else {
         nOrdNum=pObj->GetOrdNum();
     }
+
+    m_pListener = new ObjListListener(*this, *pObj, *pObjList->GetModel());
 }
 
 SdrUndoObjList::~SdrUndoObjList()
 {
+    delete m_pListener;
+
     if (pObj!=NULL && IsOwner())
     {
         // Attribute have to go back to the regular Pool
@@ -684,6 +734,16 @@ SdrUndoObjList::~SdrUndoObjList()
 void SdrUndoObjList::SetOwner(bool bNew)
 {
     bOwner = bNew;
+}
+
+sal_uInt32 SdrUndoObjList::GetOrdNum() const
+{
+    return nOrdNum;
+}
+
+void SdrUndoObjList::SetOrdNum(sal_uInt32 nOrdNum_)
+{
+    nOrdNum = nOrdNum_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////

svx/inc/svx/svdundo.hxx
@@ -253,6 +253,10 @@ public:
 //************************************************************
 
 class SVX_DLLPUBLIC SdrUndoObjList : public SdrUndoObj {
+    class ObjListListener;
+    friend class ObjListListener;
+
+private:
     bool                        bOwner;
 
 protected:
@@ -262,6 +266,7 @@ protected:
     sal_uInt32                      nOrdNum;
     // Bei einem Undo/Redo findet moeglicherweise Uebereignung des Objektes
     // statt. Im Dtor wird das Obj deleted, wenn bOwner==TRUE
+    ObjListListener*            m_pListener;
 
 protected:
     SdrUndoObjList(SdrObject& rNewObj, bool bOrdNumDirect = false);
@@ -270,6 +275,10 @@ protected:
     void SetView(SdrView* pView1, SdrPageView* pPageView1) { pView=pView1; pPageView=pPageView1; }
     bool IsOwner() { return bOwner; }
     void SetOwner(bool bNew);
+
+private:
+    sal_uInt32 GetOrdNum() const;
+    void SetOrdNum(sal_uInt32 nOrdNum_);
 };
 
 //************************************************************

zhirshya/lo-tblr

commit 427b7e07ab103da67a787d98a9a79a2b3bb46b06

fdo#62965: fix crash in Edit Contour dialog
The pObjList is 0, so use pObj's GetModel() which should be the same.
(regression from 115054fef08998c56cba8f14472df1d15007f635)

Change-Id: Ib20e1806518f7b3b33d3fb4472d79d531ea9f1de

svx/source/svdraw/svdundo.cxx
@@ -738,7 +738,7 @@ SdrUndoObjList::SdrUndoObjList(SdrObject& rNewObj, bool bOrdNumDirect)
         nOrdNum=pObj->GetOrdNum();
     }
 
-    m_pListener = new ObjListListener(*this, *pObj, *pObjList->GetModel());
+    m_pListener = new ObjListListener(*this, *pObj, *pObj->GetModel());
 }
 
 SdrUndoObjList::~SdrUndoObjList()