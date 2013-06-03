LibreOffice/core

commit a11be8a87a749f56d5c5514bbd9ffd90b3f75392

use size_t where possible and fix some more places
Change-Id: I43c315aa9867cf871657064fd9455700b31879ab

sc/source/core/tool/interpr2.cxx
@@ -2223,15 +2223,15 @@ void ScInterpreter::ScStyle()
 }
 
 static ScDdeLink* lcl_GetDdeLink( sfx2::LinkManager* pLinkMgr,
-                                const String& rA, const String& rT, const String& rI, sal_uInt8 nM )
+                                const OUString& rA, const OUString& rT, const OUString& rI, sal_uInt8 nM )
 {
-    sal_uInt16 nCount = pLinkMgr->GetLinks().size();
-    for (sal_uInt16 i=0; i<nCount; i++ )
+    size_t nCount = pLinkMgr->GetLinks().size();
+    for (size_t i=0; i<nCount; i++ )
     {
         ::sfx2::SvBaseLink* pBase = *pLinkMgr->GetLinks()[i];
         if (pBase->ISA(ScDdeLink))
         {
-            ScDdeLink* pLink = (ScDdeLink*)pBase;
+            ScDdeLink* pLink = static_cast<ScDdeLink*>(pBase);
             if ( pLink->GetAppl() == rA &&
                  pLink->GetTopic() == rT &&
                  pLink->GetItem() == rI &&
@@ -2255,9 +2255,9 @@ void ScInterpreter::ScDde()
         sal_uInt8 nMode = SC_DDE_DEFAULT;
         if (nParamCount == 4)
             nMode = (sal_uInt8) ::rtl::math::approxFloor(GetDouble());
-        String aItem  = GetString();
-        String aTopic = GetString();
-        String aAppl  = GetString();
+        const OUString& aItem  = GetString();
+        const OUString& aTopic = GetString();
+        const OUString& aAppl  = GetString();
 
         if (nMode > SC_DDE_TEXT)
             nMode = SC_DDE_DEFAULT;

sc/inc/document.hxx
@@ -668,13 +668,14 @@ public:
     void            DisconnectDdeLinks();
 
                     // for StarOne Api:
-    sal_uInt16          GetDdeLinkCount() const;
+    size_t          GetDdeLinkCount() const;
     bool            UpdateDdeLink( const OUString& rAppl, const OUString& rTopic, const OUString& rItem );
 
     /** Tries to find a DDE link with the specified connection data.
         @param rnDdePos  (out-param) Returns the index of the DDE link (does not include other links from link manager).
         @return  true = DDE link found, rnDdePos valid. */
-    SC_DLLPUBLIC bool            FindDdeLink( const OUString& rAppl, const OUString& rTopic, const OUString& rItem, sal_uInt8 nMode, sal_uInt16& rnDdePos );
+    SC_DLLPUBLIC bool            FindDdeLink( const OUString& rAppl, const OUString& rTopic,
+            const OUString& rItem, sal_uInt8 nMode, size_t& rnDdePos );
 
     /** Returns the connection data of the specified DDE link.
         @param nDdePos  Index of the DDE link (does not include other links from link manager).
@@ -682,16 +683,16 @@ public:
         @param rTopic  (out-param) The DDE topic.
         @param rItem  (out-param) The DDE item.
         @return  true = DDE link found, out-parameters valid. */
-    bool            GetDdeLinkData( sal_uInt16 nDdePos, OUString& rAppl, OUString& rTopic, OUString& rItem ) const;
+    bool            GetDdeLinkData( size_t nDdePos, OUString& rAppl, OUString& rTopic, OUString& rItem ) const;
     /** Returns the link mode of the specified DDE link.
         @param nDdePos  Index of the DDE link (does not include other links from link manager).
         @param rnMode  (out-param) The link mode of the specified DDE link.
         @return  true = DDE link found, rnMode valid. */
-    bool            GetDdeLinkMode( sal_uInt16 nDdePos, sal_uInt8& rnMode ) const;
+    bool            GetDdeLinkMode( size_t nDdePos, sal_uInt8& rnMode ) const;
     /** Returns the result matrix of the specified DDE link.
         @param nDdePos  Index of the DDE link (does not include other links from link manager).
         @return  The result matrix, if the DDE link has been found, 0 otherwise. */
-    SC_DLLPUBLIC const ScMatrix* GetDdeLinkResultMatrix( sal_uInt16 nDdePos ) const;
+    SC_DLLPUBLIC const ScMatrix* GetDdeLinkResultMatrix( sal_uInt16 size_t ) const;
 
     /** Tries to find a DDE link or creates a new, if not extant.
         @param pResults  If not 0, sets the matrix as as DDE link result matrix (also for existing links).
@@ -701,7 +702,7 @@ public:
         @param nDdePos  Index of the DDE link (does not include other links from link manager).
         @param pResults  The array containing all results of the DDE link (intrusive-ref-counted, do not delete).
         @return  true = DDE link found and matrix set. */
-    bool            SetDdeLinkResultMatrix( sal_uInt16 nDdePos, ScMatrixRef pResults );
+    bool            SetDdeLinkResultMatrix( size_t nDdePos, ScMatrixRef pResults );
 
 
     SfxBindings*    GetViewBindings();

sc/inc/linkuno.hxx
@@ -219,7 +219,7 @@ class ScAreaLinkObj : public cppu::WeakImplHelper4<
 private:
     SfxItemPropertySet      aPropSet;
     ScDocShell*             pDocShell;
-    sal_uInt16                  nPos;
+    size_t                  nPos;
     XRefreshListenerArr_Impl aRefreshListeners;
 
     void    Modify_Impl( const OUString* pNewFile, const OUString* pNewFilter,
@@ -229,7 +229,7 @@ private:
     void    Refreshed_Impl();
 
 public:
-                            ScAreaLinkObj(ScDocShell* pDocSh, sal_uInt16 nP);
+                            ScAreaLinkObj(ScDocShell* pDocSh, size_t nP);
     virtual                 ~ScAreaLinkObj();
 
     virtual void            Notify( SfxBroadcaster& rBC, const SfxHint& rHint );

sc/source/core/data/documen8.cxx
@@ -1257,8 +1257,8 @@ void ScDocument::CopyDdeLinks( ScDocument* pDestDoc ) const
     else if (GetLinkManager())              // Links direkt kopieren
     {
         const ::sfx2::SvBaseLinks& rLinks = pLinkManager->GetLinks();
-        sal_uInt16 nCount = rLinks.size();
-        for (sal_uInt16 i=0; i<nCount; i++)
+        size_t nCount = rLinks.size();
+        for (size_t i=0; i<nCount; i++)
         {
             ::sfx2::SvBaseLink* pBase = *rLinks[i];
             if (pBase->ISA(ScDdeLink))
@@ -1272,14 +1272,14 @@ void ScDocument::CopyDdeLinks( ScDocument* pDestDoc ) const
     }
 }
 
-sal_uInt16 ScDocument::GetDdeLinkCount() const
+size_t ScDocument::GetDdeLinkCount() const
 {
-    sal_uInt16 nDdeCount = 0;
+    size_t nDdeCount = 0;
     if (GetLinkManager())
     {
         const ::sfx2::SvBaseLinks& rLinks = pLinkManager->GetLinks();
-        sal_uInt16 nCount = rLinks.size();
-        for (sal_uInt16 i=0; i<nCount; i++)
+        size_t nCount = rLinks.size();
+        for (size_t i=0; i<nCount; i++)
             if ((*rLinks[i])->ISA(ScDdeLink))
                 ++nDdeCount;
     }
@@ -1297,14 +1297,14 @@ namespace {
 ScDdeLink* lclGetDdeLink(
         const sfx2::LinkManager* pLinkManager,
         const OUString& rAppl, const OUString& rTopic, const OUString& rItem, sal_uInt8 nMode,
-        sal_uInt16* pnDdePos = NULL )
+        size_t* pnDdePos = NULL )
 {
     if( pLinkManager )
     {
         const ::sfx2::SvBaseLinks& rLinks = pLinkManager->GetLinks();
-        sal_uInt16 nCount = rLinks.size();
+        size_t nCount = rLinks.size();
         if( pnDdePos ) *pnDdePos = 0;
-        for( sal_uInt16 nIndex = 0; nIndex < nCount; ++nIndex )
+        for( size_t nIndex = 0; nIndex < nCount; ++nIndex )
         {
             ::sfx2::SvBaseLink* pLink = *rLinks[ nIndex ];
             if( ScDdeLink* pDdeLink = PTR_CAST( ScDdeLink, pLink ) )
@@ -1324,14 +1324,14 @@ ScDdeLink* lclGetDdeLink(
 /** Returns a pointer to the specified DDE link.
     @param nDdePos  Index of the DDE link (does not include other links from link manager).
     @return  The DDE link, if it exists, otherwise 0. */
-ScDdeLink* lclGetDdeLink( const sfx2::LinkManager* pLinkManager, sal_uInt16 nDdePos )
+ScDdeLink* lclGetDdeLink( const sfx2::LinkManager* pLinkManager, size_t nDdePos )
 {
     if( pLinkManager )
     {
         const ::sfx2::SvBaseLinks& rLinks = pLinkManager->GetLinks();
-        sal_uInt16 nCount = rLinks.size();
-        sal_uInt16 nDdeIndex = 0;       // counts only the DDE links
-        for( sal_uInt16 nIndex = 0; nIndex < nCount; ++nIndex )
+        size_t nCount = rLinks.size();
+        size_t nDdeIndex = 0;       // counts only the DDE links
+        for( size_t nIndex = 0; nIndex < nCount; ++nIndex )
         {
             ::sfx2::SvBaseLink* pLink = *rLinks[ nIndex ];
             if( ScDdeLink* pDdeLink = PTR_CAST( ScDdeLink, pLink ) )
@@ -1349,12 +1349,13 @@ ScDdeLink* lclGetDdeLink( const sfx2::LinkManager* pLinkManager, sal_uInt16 nDde
 
 // ----------------------------------------------------------------------------
 
-bool ScDocument::FindDdeLink( const OUString& rAppl, const OUString& rTopic, const OUString& rItem, sal_uInt8 nMode, sal_uInt16& rnDdePos )
+bool ScDocument::FindDdeLink( const OUString& rAppl, const OUString& rTopic, const OUString& rItem,
+        sal_uInt8 nMode, size_t& rnDdePos )
 {
     return lclGetDdeLink( GetLinkManager(), rAppl, rTopic, rItem, nMode, &rnDdePos ) != NULL;
 }
 
-bool ScDocument::GetDdeLinkData( sal_uInt16 nDdePos, OUString& rAppl, OUString& rTopic, OUString& rItem ) const
+bool ScDocument::GetDdeLinkData( size_t nDdePos, OUString& rAppl, OUString& rTopic, OUString& rItem ) const
 {
     if( const ScDdeLink* pDdeLink = lclGetDdeLink( GetLinkManager(), nDdePos ) )
     {
@@ -1366,7 +1367,7 @@ bool ScDocument::GetDdeLinkData( sal_uInt16 nDdePos, OUString& rAppl, OUString& 
     return false;
 }
 
-bool ScDocument::GetDdeLinkMode( sal_uInt16 nDdePos, sal_uInt8& rnMode ) const
+bool ScDocument::GetDdeLinkMode( size_t nDdePos, sal_uInt8& rnMode ) const
 {
     if( const ScDdeLink* pDdeLink = lclGetDdeLink( GetLinkManager(), nDdePos ) )
     {
@@ -1408,7 +1409,7 @@ bool ScDocument::CreateDdeLink( const OUString& rAppl, const OUString& rTopic, c
     return false;
 }
 
-bool ScDocument::SetDdeLinkResultMatrix( sal_uInt16 nDdePos, ScMatrixRef pResults )
+bool ScDocument::SetDdeLinkResultMatrix( size_t nDdePos, ScMatrixRef pResults )
 {
     if( ScDdeLink* pDdeLink = lclGetDdeLink( GetLinkManager(), nDdePos ) )
     {

sc/source/filter/excel/xelink.cxx
@@ -1052,7 +1052,7 @@ sal_uInt16 XclExpExtNameBuffer::InsertDde(
     sal_uInt16 nIndex = GetIndex( rItem );
     if( nIndex == 0 )
     {
-        sal_uInt16 nPos;
+        size_t nPos;
         if( GetDoc().FindDdeLink( rApplic, rTopic, rItem, SC_DDE_IGNOREMODE, nPos ) )
         {
             // create the leading 'StdDocumentName' EXTERNNAME record

sc/source/filter/excel/xestyle.cxx
@@ -1552,7 +1552,7 @@ void lclGetBorderLine(
     rnXclLine = EXC_LINE_NONE;
     if( pLine )
     {
-        sal_uInt16 nOuterWidth = pLine->GetOutWidth();
+        sal_uInt16 nOuterWidth = pLine->GetWidth();
         sal_uInt16 nDistance = pLine->GetDistance();
         if( nDistance > 0 )
             rnXclLine = EXC_LINE_DOUBLE;

sc/source/filter/xml/XMLDDELinksContext.cxx
@@ -116,16 +116,15 @@ void ScXMLDDELinkContext::CreateDDELink()
         !sTopic.isEmpty() &&
         !sItem.isEmpty())
     {
-        String sAppl(sApplication);
-        String sTop(sTopic);
-        String sIt(sItem);
-        GetScImport().GetDocument()->CreateDdeLink(sAppl, sTop, sIt, nMode, ScMatrixRef());
-        sal_uInt16 nPos;
-        if(GetScImport().GetDocument()->FindDdeLink(sAppl, sTop, sIt, nMode, nPos))
+        GetScImport().GetDocument()->CreateDdeLink(sApplication, sTopic, sItem, nMode, ScMatrixRef());
+        size_t nPos;
+        if(GetScImport().GetDocument()->FindDdeLink(sApplication, sTopic, sItem, nMode, nPos))
             nPosition = nPos;
         else
+        {
             nPosition = -1;
-        OSL_ENSURE(nPosition > -1, "DDE Link not inserted");
+            SAL_WARN("sc" , "DDE Link not inserted");
+        }
     }
 }
 

sc/source/ui/docshell/docsh6.cxx
@@ -370,14 +370,14 @@ void ScDocShell::UpdateLinks()
 
     // nicht mehr benutzte Links raus
 
-    sal_uInt16 nCount = pLinkManager->GetLinks().size();
-    for (sal_uInt16 k=nCount; k>0; )
+    size_t nCount = pLinkManager->GetLinks().size();
+    for (size_t k=nCount; k>0; )
     {
         --k;
         ::sfx2::SvBaseLink* pBase = *pLinkManager->GetLinks()[k];
         if (pBase->ISA(ScTableLink))
         {
-            ScTableLink* pTabLink = (ScTableLink*)pBase;
+            ScTableLink* pTabLink = static_cast<ScTableLink*>(pBase);
             if (pTabLink->IsUsed())
                 aNames.insert(pTabLink->GetFileName());
             else        // nicht mehr benutzt -> loeschen
@@ -434,14 +434,14 @@ sal_Bool ScDocShell::ReloadTabLinks()
 {
     sfx2::LinkManager* pLinkManager = aDocument.GetLinkManager();
 
-    sal_Bool bAny = false;
-    sal_uInt16 nCount = pLinkManager->GetLinks().size();
-    for (sal_uInt16 i=0; i<nCount; i++ )
+    bool bAny = false;
+    size_t nCount = pLinkManager->GetLinks().size();
+    for (size_t i=0; i<nCount; i++ )
     {
         ::sfx2::SvBaseLink* pBase = *pLinkManager->GetLinks()[i];
         if (pBase->ISA(ScTableLink))
         {
-            ScTableLink* pTabLink = (ScTableLink*)pBase;
+            ScTableLink* pTabLink = static_cast<ScTableLink*>(pBase);
 //			pTabLink->SetAddUndo(sal_False);		//! Undo's zusammenfassen
 
 			// Painting only after Update() makes no sense:

zhirshya/lo-tblr

commit 895dc28ed3c8e21afd15b25815a894541a4f32ab

ScInterpreter::GetString can return reference to shared string instance
...ScInterpreter::aTempStr member.  Regression introduced with
a11be8a87a749f56d5c5514bbd9ffd90b3f75392 "use size_t where possible and fix some
more places" causing JunitTest_sc_unoapi's
sc.ScDDELinkObj::com::sun::star::util::XRefreshable to fail.

Change-Id: I50227b004e37f68c5040d4ace90a79300f711be8

sc/source/core/tool/interpr2.cxx
@@ -2255,9 +2255,9 @@ void ScInterpreter::ScDde()
         sal_uInt8 nMode = SC_DDE_DEFAULT;
         if (nParamCount == 4)
             nMode = (sal_uInt8) ::rtl::math::approxFloor(GetDouble());
-        const OUString& aItem  = GetString();
-        const OUString& aTopic = GetString();
-        const OUString& aAppl  = GetString();
+        OUString aItem  = GetString();
+        OUString aTopic = GetString();
+        OUString aAppl  = GetString();
 
         if (nMode > SC_DDE_TEXT)
             nMode = SC_DDE_DEFAULT;