LibreOffice/core

commit a8c2cb0e6d0b9b301887be176694592f8ecda0c4

SvtURLBox derives from ComboBox not Edit
so should use ComboBox in .src file once that's done we can remove the dummy
Edit's which seem to exist only to get the right size/position onto the
SvtURLBox. All of this removes the resource assert spew on loading
the built-in filepicker

Change-Id: I9e6249bc34ee563fc73784c46bb733901d4d55d6

fpicker/source/office/iodlg.src
@@ -57,7 +57,7 @@ ModalDialog DLG_FPICKER_EXPLORERFILE
     Sizeable = TRUE;
     HelpId = HID_EXPLORERDLG_FILE ;
     Size = MAP_APPFONT ( 280 , 174 ) ;
-    Edit ED_EXPLORERFILE_CURRENTPATH
+    ComboBox ED_EXPLORERFILE_CURRENTPATH
     {
         Pos = MAP_APPFONT ( 6 , 6 ) ;
         Size = MAP_APPFONT ( 90 , 12 ) ;
@@ -113,7 +113,7 @@ ModalDialog DLG_FPICKER_EXPLORERFILE
         Size = MAP_APPFONT ( 50 , 10 ) ;
         Text [ en-US ] = "File ~name:" ;
     };
-    Edit ED_EXPLORERFILE_FILENAME
+    ComboBox ED_EXPLORERFILE_FILENAME
     {
         HelpID = "fpicker:Edit:DLG_SVT_EXPLORERFILE:ED_EXPLORERFILE_FILENAME";
         Pos = MAP_APPFONT ( 59 , 117 ) ;

fpicker/source/office/iodlg.cxx
@@ -507,12 +507,9 @@ void SvtFileDialog::Init_Impl
     // Create control element, the order defines the tab control.
     _pImp->_pFtFileName = new FixedText( this, SvtResId( FT_EXPLORERFILE_FILENAME ) );
 
-    SvtURLBox* pURLBox = new SvtURLBox( this );
+    SvtURLBox* pURLBox = new SvtURLBox( this, SvtResId( ED_EXPLORERFILE_FILENAME ) );
     pURLBox->SetUrlFilter( &m_aURLFilter );
     _pImp->_pEdFileName = pURLBox;
-
-    Edit aDummy( this, SvtResId( ED_EXPLORERFILE_FILENAME ) );
-    _pImp->_pEdFileName->SetPosSizePixel( aDummy.GetPosPixel(), aDummy.GetSizePixel() );
     _pImp->_pEdFileName->Show();
     pURLBox->SetSelectHdl( LINK( this, SvtFileDialog, EntrySelectHdl_Impl ) );
     pURLBox->SetOpenHdl( STATIC_LINK( this, SvtFileDialog, OpenHdl_Impl ) );
@@ -547,10 +544,8 @@ void SvtFileDialog::Init_Impl
         }
     }
 
-    Edit anOtherDummy( this, SvtResId( ED_EXPLORERFILE_CURRENTPATH ) );
     _pImp->_pEdCurrentPath = new SvtURLBox( this, SvtResId(ED_EXPLORERFILE_CURRENTPATH) );
     _pImp->_pEdCurrentPath->SetUrlFilter( &m_aURLFilter );
-    _pImp->_pEdCurrentPath->SetPosSizePixel( anOtherDummy.GetPosPixel(), anOtherDummy.GetSizePixel() );
     _pImp->_pEdCurrentPath->Show();
 
     _pImp->_pBtnFileOpen = new PushButton( this, SvtResId( BTN_EXPLORERFILE_OPEN ) );

zhirshya/lo-tblr

commit 33eca5a8758cad6603762b6ab48f424cf26b2a85

Resolves: fdo#65569 dropdown list missing from built-in fpicker
regression since a8c2cb0e6d0b9b301887be176694592f8ecda0c4
the different ctors have different defaults around whether
they show the dropdown or not.

Change-Id: I3d1bb423c8a25b81ffb26981b63af7498f99e70a

fpicker/source/office/iodlg.src
@@ -120,6 +120,9 @@ ModalDialog DLG_FPICKER_EXPLORERFILE
         Pos = MAP_APPFONT ( 59 , 117 ) ;
         Size = MAP_APPFONT ( 159 , 12 ) ;
         Border = TRUE ;
+        DropDown = TRUE ;
+        AutoSize = TRUE ;
+        AutoHScroll = TRUE ;
     };
     FixedText FT_EXPLORERFILE_SHARED_LISTBOX
     {

svtools/source/control/inettbc.cxx
@@ -899,10 +899,12 @@ void SvtURLBox::Init(bool bSetDefaultHelpID)
         SetHelpId( ".uno:OpenURL" );
     EnableAutocomplete( sal_False );
 
-    SetText( String() );
+    SetText( OUString() );
 
     GetSubEdit()->SetAutocompleteHdl( LINK( this, SvtURLBox, AutoCompleteHdl_Impl ) );
     UpdatePicklistForSmartProtocol_Impl();
+
+    EnableAutoSize(GetStyle() & WB_AUTOSIZE);
 }
 
 SvtURLBox::~SvtURLBox()