LibreOffice/core

commit 9555b5bdb9ce2251bb8c4f967f20ed1b13eecf5e

Add gengal to RepositoryFixes and autoinstall it.
Change-Id: I935fb9fd2620447f9aba9142b7db896e6fe31133

RepositoryExternal.mk
@@ -2805,7 +2805,7 @@ endef
 
 # Better duplication with gb_Gallery__UNO_COMPONENTS than nothing.
 # This is used to determine what we need for 'build' platform.
-define gb_Executable__register_gengal_impl
+define gb_Executable__register_gengal
 $(call gb_Executable_add_runtime_dependencies,$(1),\
 	$(foreach component, \
 		comphelper/util/comphelp \
@@ -2835,12 +2835,6 @@ $(call gb_Executable_add_runtime_dependencies,$(1),\
 	$(call gb_UnoApi_get_target,udkapi) \
 )
 endef
-
-ifeq ($(OS),WNT)
-gb_Executable__register_gengal = $(call gb_Executable__register_gengal_impl,gengal)
-else
-gb_Executable__register_gengal.bin = $(call gb_Executable__register_gengal_impl,gengal.bin)
-endif
 
 ifneq ($(SYSTEM_ICU),YES)
 

Repository.mk
@@ -73,8 +73,11 @@ $(eval $(call gb_Helper_register_executables,SDK, \
 	) \
 ))
 
+$(eval $(call gb_Helper_register_executables_for_install,OOO,ooo, \
+	$(if $(filter DESKTOP,$(BUILD_TYPE)),gengal) \
+))
+
 $(eval $(call gb_Helper_register_executables,OOO, \
-	$(if $(filter DESKTOP,$(BUILD_TYPE)),gengal.bin) \
 	gnome-open-url.bin \
 	$(if $(filter YES,$(ENABLE_NPAPI_INTO_BROWSER)),nsplugin) \
 	$(if $(filter MACOSX,$(OS)),officeloader) \
@@ -93,7 +96,6 @@ $(eval $(call gb_Helper_register_executables,OOO, \
 	$(if $(filter DESKTOP,$(BUILD_TYPE)),unopkg.bin) \
 	$(if $(filter WNT,$(OS)), \
 		crashrep_com \
-		gengal \
 		gcc-wrapper \
 		g++-wrapper \
 		guiloader \

RepositoryFixes.mk
@@ -23,6 +23,7 @@ gb_Executable_FILENAMES := $(foreach group,$(gb_Executable_VALIDGROUPS),\
 	$(foreach exe,$(gb_Executable_$(group)),$(exe):$(exe)$(gb_Executable_EXT)))
 
 gb_Executable_FILENAMES := $(patsubst uno:uno,uno:uno.bin,$(gb_Executable_FILENAMES))
+gb_Executable_FILENAMES := $(patsubst gengal:gengal,gengal:gengal.bin,$(gb_Executable_FILENAMES))
 
 gb_Executable_FILENAMES_FOR_BUILD := $(subst $(gb_Executable_EXT),$(gb_Executable_EXT_for_build),$(gb_Executable_FILENAMES))
 

scp2/AutoInstall.mk
@@ -17,7 +17,7 @@ $(eval $(call gb_AutoInstall_add_module,impress,LIBO_LIB_FILE))
 $(eval $(call gb_AutoInstall_add_module,kde,LIBO_LIB_FILE))
 $(eval $(call gb_AutoInstall_add_module,math,LIBO_LIB_FILE))
 $(eval $(call gb_AutoInstall_add_module,onlineupdate,LIBO_LIB_FILE))
-$(eval $(call gb_AutoInstall_add_module,ooo,LIBO_LIB_FILE))
+$(eval $(call gb_AutoInstall_add_module,ooo,LIBO_LIB_FILE,LIBO_EXECUTABLE))
 $(eval $(call gb_AutoInstall_add_module,tde,LIBO_LIB_FILE))
 $(eval $(call gb_AutoInstall_add_module,ure,URE_PRIVATE_LIB,URE_EXECUTABLE))
 $(eval $(call gb_AutoInstall_add_module,writer,LIBO_LIB_FILE))

scp2/inc/macros.inc
@@ -453,6 +453,14 @@ End
         Styles = (PACKED, VERSION_INDEPENDENT_COMP_ID); \
     End
 
+#define LIBO_EXECUTABLE(id,name) \
+    File id \
+        BIN_FILE_BODY; \
+        Dir = gid_Brand_Dir_Program; \
+        Name = name; \
+        Styles = (PACKED); \
+    End
+
 #define GALLERY_NAME(id) \
 
 #define GALLERY_FILELIST(id) \

scp2/source/ooo/file_ooo.scp
@@ -128,17 +128,6 @@ File gid_File_Exe_Nsplugin
 End
 #endif
 
-File gid_File_Bin_Gengal
-    BIN_FILE_BODY;
-    Dir = gid_Brand_Dir_Program;
-    Styles = (PACKED);
-  #ifdef UNX
-    Name = "gengal.bin";
-  #else
-    Name = "gengal.exe";
-  #endif
-End
-
 File gid_File_Bin_UIPreviewer
     BIN_FILE_BODY;
     Dir = gid_Brand_Dir_Program;

scp2/source/ooo/module_hidden_ooo.scp
@@ -84,7 +84,6 @@ Module gid_Module_Root_Files_2
 	Files = (gid_File_shell_scripts,
 	gid_File_Bin_Pluginapp,
 	gid_File_Bin_Senddoc,
-	gid_File_Bin_Gengal,
 	gid_File_Script_Gengal,
 	gid_File_Bin_Spadmin_Bin,
 	gid_File_Bin_Uri_Encode,

solenv/gbuild/Gallery.mk
@@ -65,7 +65,7 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(if $(filter-out MACOSX WNT,$(OS_FOR_BUILD)),$(if $(ENABLE_HEADLESS),, \
 			SAL_USE_VCLPLUGIN=svp \
 		)) \
-		$(call gb_Executable_get_command,$(gb_GENGAL),$(ICECREAM_RUN)) \
+		$(call gb_Executable_get_command,gengal,$(ICECREAM_RUN)) \
 			$(call gb_Gallery__make_env_args) \
 			--build-tree \
 			--destdir $(GALLERY_BASEDIR) \
@@ -105,7 +105,7 @@ $(dir $(call gb_Gallery_get_target,$(1)))%/.dir :
 	$(if $(wildcard $(dir $@)),,mkdir -p $(dir $@))
 
 $(call gb_Gallery_get_target,%) : \
-		$(call gb_Executable_get_runtime_dependencies,$(gb_GENGAL)) \
+		$(call gb_Executable_get_runtime_dependencies,gengal) \
 		$(gb_Gallery_EXTRA_DEPENCENCIES)
 	$(call gb_Gallery__command,$@,$*)
 

zhirshya/lo-tblr

commit 3d47f7cc4771e551a1f15c741c8e8eb3c5755c5d

fix gengal dependencies
Regression from 9555b5bdb9ce2251bb8c4f967f20ed1b13eecf5e

Change-Id: I91a7efb207209020acef751ad4f766328dc92c57

RepositoryExternal.mk
@@ -2794,7 +2794,7 @@ endef
 # Better duplication with gb_Gallery__UNO_COMPONENTS than nothing.
 # This is used to determine what we need for 'build' platform.
 define gb_Executable__register_gengal
-$(call gb_Executable_add_runtime_dependencies,$(1),\
+$(call gb_Executable_add_runtime_dependencies,gengal,\
 	$(foreach component, \
 		comphelper/util/comphelp \
 		configmgr/source/configmgr \