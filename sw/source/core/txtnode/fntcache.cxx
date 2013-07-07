LibreOffice/core

commit a2f6402b1fe769a430019042e14e63c9414715dc

Use OUString and sal_Int32 in GetTextBreak()
Change-Id: I66b85365d1c59f802253b8abdb1e04e25950a09b
Reviewed-on: https://gerrit.libreoffice.org/3098
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

sw/source/core/txtnode/fntcache.cxx
@@ -2437,14 +2437,23 @@ xub_StrLen SwFont::GetTxtBreak( SwDrawTextInfo& rInf, long nTextWidth )
             bTextReplaced = true;
         }
 
-           if( rInf.GetHyphPos() )
-            nTxtBreak = rInf.GetOut().GetTextBreak( *pTmpText, nTextWidth,
-                                                    '-', *rInf.GetHyphPos(),
-                                                     nTmpIdx, nTmpLen, nKern );
+        OUString sTmpText(*pTmpText); // only needed until *pTmpText is OUString
+        sal_Int32 nTmpIdx2 = nTmpIdx;  // ditto
+        sal_Int32 nTmpLen2 = nTmpLen;  // ditto
+        if( rInf.GetHyphPos() ) {
+            sal_Int32 nHyphPos = *rInf.GetHyphPos();
+            nTxtBreak = rInf.GetOut().GetTextBreak( sTmpText, nTextWidth,
+                             static_cast<sal_Unicode>('-'), nHyphPos,
+                             nTmpIdx2, nTmpLen2, nKern );
+            xub_StrLen nTmpHyphPos = static_cast<xub_StrLen>(nHyphPos);
+            rInf.SetHyphPos(&nTmpHyphPos);
+        }
         else
-            nTxtBreak = rInf.GetOut().GetTextBreak( *pTmpText, nTextWidth,
-                                                    nTmpIdx, nTmpLen, nKern );
+            nTxtBreak = rInf.GetOut().GetTextBreak( sTmpText, nTextWidth,
+                                                    nTmpIdx2, nTmpLen2, nKern );
 
+        nTmpIdx = nTmpIdx2;     // ditto
+        nTmpLen = nTmpLen2;     // ditto
         if ( bTextReplaced && STRING_LEN != nTxtBreak )
         {
             if ( nTmpLen != nLn )

sw/source/core/txtnode/fntcap.cxx
@@ -208,16 +208,27 @@ void SwDoGetCapitalBreak::Do()
         else
         {
             xub_StrLen nEnd = rInf.GetEnd();
+            OUString sText(rInf.GetText()); // only needed until rInf.GetText() returns OUString
+            long nTxtWidth2 = nTxtWidth;    // only needed until variables are migrated to sal_Int32
+            sal_Int32 nIdx2 = rInf.GetIdx(); // ditto
+            sal_Int32 nLen2 = rInf.GetLen(); // ditto
             if( pExtraPos )
             {
-                nBreak = GetOut().GetTextBreak( rInf.GetText(), nTxtWidth, '-',
-                     *pExtraPos, rInf.GetIdx(), rInf.GetLen(), rInf.GetKern() );
-                if( *pExtraPos > nEnd )
-                    *pExtraPos = nEnd;
+                sal_Int32 nExtraPos = *pExtraPos; // ditto
+                nBreak = GetOut().GetTextBreak( sText, nTxtWidth2, static_cast<sal_Unicode>('-'),
+                     nExtraPos, nIdx2, nLen2, rInf.GetKern() );
+                if( nExtraPos > nEnd )
+                    nExtraPos = nEnd;
+                *pExtraPos = nExtraPos;
             }
             else
-                nBreak = GetOut().GetTextBreak( rInf.GetText(), nTxtWidth,
-                               rInf.GetIdx(), rInf.GetLen(), rInf.GetKern() );
+                nBreak = GetOut().GetTextBreak( sText, nTxtWidth2,
+                               nIdx2, nLen2, rInf.GetKern() );
+
+            rInf.SetText(sText); // ditto
+            rInf.SetIdx(nIdx2);  // ditto
+            rInf.SetLen(nLen2);  // ditto
+            nTxtWidth = nTxtWidth2; // ditto
 
             if( nBreak > nEnd )
                 nBreak = nEnd;

vcl/source/gdi/outdev3.cxx
@@ -5795,7 +5795,7 @@ SalLayout* OutputDevice::ImplLayout( const OUString& rOrigStr, sal_Int32 nMinInd
         ImplInitFont();
 
     // check string index and length
-    if( nMinIndex + nLen > rOrigStr.getLength() )
+    if( -1 == nLen || nMinIndex + nLen > rOrigStr.getLength() )
     {
         const sal_Int32 nNewLen = rOrigStr.getLength() - nMinIndex;
         if( nNewLen <= 0 )
@@ -6077,8 +6077,8 @@ sal_Bool OutputDevice::GetTextIsRTL( const OUString& rString, sal_Int32 nIndex, 
     return (nCharPos != nIndex) ? sal_True : sal_False;
 }
 
-xub_StrLen OutputDevice::GetTextBreak( const String& rStr, long nTextWidth,
-                                       xub_StrLen nIndex, xub_StrLen nLen,
+xub_StrLen OutputDevice::GetTextBreak( const OUString& rStr, long nTextWidth,
+                                       sal_Int32 nIndex, sal_Int32 nLen,
                                        long nCharExtra, sal_Bool /*TODO: bCellBreaking*/ ) const
 {
     DBG_CHKTHIS( OutputDevice, ImplDbgCheckOutputDevice );
@@ -6109,9 +6109,9 @@ xub_StrLen OutputDevice::GetTextBreak( const String& rStr, long nTextWidth,
     return nRetVal;
 }
 
-xub_StrLen OutputDevice::GetTextBreak( const String& rStr, long nTextWidth,
-                                       sal_Unicode nHyphenatorChar, xub_StrLen& rHyphenatorPos,
-                                       xub_StrLen nIndex, xub_StrLen nLen,
+xub_StrLen OutputDevice::GetTextBreak( const OUString& rStr, long nTextWidth,
+                                       sal_Unicode nHyphenatorChar, sal_Int32& rHyphenatorPos,
+                                       sal_Int32 nIndex, sal_Int32 nLen,
                                        long nCharExtra ) const
 {
     DBG_CHKTHIS( OutputDevice, ImplDbgCheckOutputDevice );
@@ -6143,7 +6143,7 @@ xub_StrLen OutputDevice::GetTextBreak( const String& rStr, long nTextWidth,
 
     // calculate hyphenated break position
     rtl::OUString aHyphenatorStr(nHyphenatorChar);
-    xub_StrLen nTempLen = 1;
+    sal_Int32 nTempLen = 1;
     SalLayout* pHyphenatorLayout = ImplLayout( aHyphenatorStr, 0, nTempLen );
     if( pHyphenatorLayout )
     {

vcl/inc/vcl/outdev.hxx
@@ -590,12 +590,12 @@ public:
     void                DrawStretchText( const Point& rStartPt, sal_uLong nWidth,
                                          const XubString& rStr,
                                          xub_StrLen nIndex = 0, xub_StrLen nLen = STRING_LEN );
-    xub_StrLen          GetTextBreak( const XubString& rStr, long nTextWidth,
-                                      xub_StrLen nIndex = 0, xub_StrLen nLen = STRING_LEN,
+    xub_StrLen          GetTextBreak( const OUString& rStr, long nTextWidth,
+                                      sal_Int32 nIndex = 0, sal_Int32 nLen = -1,
                                       long nCharExtra = 0, sal_Bool bCellBreaking = sal_True ) const;
-    xub_StrLen          GetTextBreak( const XubString& rStr, long nTextWidth,
-                                      sal_uInt16 nExtraChar, xub_StrLen& rExtraCharPos,
-                                      xub_StrLen nIndex, xub_StrLen nLen,
+    xub_StrLen          GetTextBreak( const OUString& rStr, long nTextWidth,
+                                      sal_Unicode nExtraChar, sal_Int32& rExtraCharPos,
+                                      sal_Int32 nIndex, sal_Int32 nLen,
                                       long nCharExtra = 0 ) const;
     /** Generate MetaTextActions for the text rect
 

zhirshya/lo-tblr

commit 2400ad6ce3b159c68d3eea767c260b3f2e157c14

fdo#66478: sw: un-break hyphenation
The main problem is calling SetHyphPos with a stack pointer.
Converting STRING_LEN <-> -1 can't hurt though.

(regression from a2f6402b1fe769a430019042e14e63c9414715dc)

Change-Id: I59a4e3b16b418082de468c7d7f7f38a9b0bccd01

sw/source/core/txtnode/fntcache.cxx
@@ -2482,8 +2482,8 @@ xub_StrLen SwFont::GetTxtBreak( SwDrawTextInfo& rInf, long nTextWidth )
             nTxtBreak = rInf.GetOut().GetTextBreak( sTmpText, nTextWidth,
                              static_cast<sal_Unicode>('-'), nHyphPos,
                              nTmpIdx2, nTmpLen2, nKern );
-            xub_StrLen nTmpHyphPos = static_cast<xub_StrLen>(nHyphPos);
-            rInf.SetHyphPos(&nTmpHyphPos);
+            *rInf.GetHyphPos() = (nHyphPos == -1)
+                ? STRING_LEN : static_cast<xub_StrLen>(nHyphPos);
         }
         else
             nTxtBreak = rInf.GetOut().GetTextBreak( sTmpText, nTextWidth,

sw/source/core/txtnode/fntcap.cxx
@@ -209,26 +209,25 @@ void SwDoGetCapitalBreak::Do()
         {
             xub_StrLen nEnd = rInf.GetEnd();
             OUString sText(rInf.GetText()); // only needed until rInf.GetText() returns OUString
-            long nTxtWidth2 = nTxtWidth;    // only needed until variables are migrated to sal_Int32
             sal_Int32 nIdx2 = rInf.GetIdx(); // ditto
             sal_Int32 nLen2 = rInf.GetLen(); // ditto
             if( pExtraPos )
             {
                 sal_Int32 nExtraPos = *pExtraPos; // ditto
-                nBreak = GetOut().GetTextBreak( sText, nTxtWidth2, static_cast<sal_Unicode>('-'),
+                nBreak = GetOut().GetTextBreak( sText, nTxtWidth,
+                            static_cast<sal_Unicode>('-'),
                      nExtraPos, nIdx2, nLen2, rInf.GetKern() );
                 if( nExtraPos > nEnd )
                     nExtraPos = nEnd;
-                *pExtraPos = nExtraPos;
+                *pExtraPos = (nExtraPos == -1) ? STRING_LEN : nExtraPos;
             }
             else
-                nBreak = GetOut().GetTextBreak( sText, nTxtWidth2,
+                nBreak = GetOut().GetTextBreak( sText, nTxtWidth,
                                nIdx2, nLen2, rInf.GetKern() );
 
             rInf.SetText(sText); // ditto
             rInf.SetIdx(nIdx2);  // ditto
             rInf.SetLen(nLen2);  // ditto
-            nTxtWidth = nTxtWidth2; // ditto
 
             if( nBreak > nEnd )
                 nBreak = nEnd;

vcl/source/gdi/outdev3.cxx
@@ -6074,10 +6074,14 @@ xub_StrLen OutputDevice::GetTextBreak( const OUString& rStr, long nTextWidth,
         if( nExtraPixelWidth > 0 )
             nTextPixelWidth -= nExtraPixelWidth;
 
-        rHyphenatorPos = sal::static_int_cast<sal_Int32>(pSalLayout->GetTextBreak( nTextPixelWidth, nExtraPixelWidth, nSubPixelFactor ));
+        // why does this return "int" and use STRING_LEN for errors???
+        xub_StrLen nTmp = sal::static_int_cast<xub_StrLen>(
+            pSalLayout->GetTextBreak(nTextPixelWidth, nExtraPixelWidth, nSubPixelFactor));
 
-        if( rHyphenatorPos > nRetVal )
-            rHyphenatorPos = nRetVal;
+        nTmp = std::min(nTmp, nRetVal);
+
+        // TODO: remove nTmp when GetTextBreak sal_Int32
+        rHyphenatorPos = (nTmp == STRING_LEN) ? -1 : nTmp;
     }
 
     pSalLayout->Release();