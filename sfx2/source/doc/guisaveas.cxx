LibreOffice/core

commit e2799d253b1dc62967693aa5bccd7360a9520967

re-base on ALv2 code. Includes:
    Patches contributed by Mathias Bauer:
    cws mba34issues01: #i117713#: remove superfluous assertions about field unit fallbacks
    http://svn.apache.org/viewvc?view=revision&revision=1172341
    cws mba34issues01: #i117718#: provide filter name in case
    storage of medium does not allow to detect one
    cws mba34issues01: #i117712#: fix several resource errors introduced by IAccessible2 implementation
    http://svn.apache.org/viewvc?view=revision&revision=1172343
    http://svn.apache.org/viewvc?view=revision&revision=1172350
    cws mba34issues01: #i117708#: status indicator for inactive documents
    http://svn.apache.org/viewvc?view=revision&revision=1172358
    cws mba34issues01: #i116025#: fix typo and add missing treatment of new parameter
    http://svn.apache.org/viewvc?view=revision&revision=1172359
    cws mba34issues01: #i117024#: rework parameter conversion for load/save slots
    http://svn.apache.org/viewvc?view=revision&revision=1172360
    cws mba34issues01: #i116803#: possible crash on Linux when
    dispatch.cxx is compiled with optimization
    http://svn.apache.org/viewvc?view=revision&revision=1172367
    cws mba34issues01: #i114600#: fix compiler warning for name clash found on Solaris
    http://svn.apache.org/viewvc?view=revision&revision=1172371

sfx2/source/doc/guisaveas.cxx
@@ -1,30 +1,21 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*************************************************************************
+/*
+ * This file is part of the LibreOffice project.
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 2000, 2010 Oracle and/or its affiliates.
+ * This file incorporates work covered by the following license notice:
  *
- * OpenOffice.org - a multi-platform office productivity suite
- *
- * This file is part of OpenOffice.org.
- *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * only, as published by the Free Software Foundation.
- *
- * OpenOffice.org is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
- * (a copy is included in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
- *
- ************************************************************************/
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
 
 #include <com/sun/star/ui/dialogs/XExecutableDialog.hpp>
 #include <com/sun/star/ui/dialogs/XFilePicker.hpp>
@@ -675,10 +666,11 @@ sal_Int8 ModelData_Impl::CheckStateForSave()
     sal_Bool bVersInfoNeedsStore = sal_False;
     ::comphelper::SequenceAsHashMap aAcceptedArgs;
 
-    ::rtl::OUString aVersionCommentString(RTL_CONSTASCII_USTRINGPARAM("VersionComment"));
-    ::rtl::OUString aAuthorString(RTL_CONSTASCII_USTRINGPARAM("Author"));
-    ::rtl::OUString aInteractionHandlerString(RTL_CONSTASCII_USTRINGPARAM("InteractionHandler"));
-    ::rtl::OUString aStatusIndicatorString(RTL_CONSTASCII_USTRINGPARAM("StatusIndicator"));
+    ::rtl::OUString aVersionCommentString("VersionComment");
+    ::rtl::OUString aAuthorString("Author");
+    ::rtl::OUString aInteractionHandlerString("InteractionHandler");
+    ::rtl::OUString aStatusIndicatorString("StatusIndicator");
+	::rtl::OUString aFailOnWarningString("FailOnWarning");
 
     if ( GetMediaDescr().find( aVersionCommentString ) != GetMediaDescr().end() )
     {
@@ -691,6 +683,8 @@ sal_Int8 ModelData_Impl::CheckStateForSave()
         aAcceptedArgs[ aInteractionHandlerString ] = GetMediaDescr()[ aInteractionHandlerString ];
     if ( GetMediaDescr().find( aStatusIndicatorString ) != GetMediaDescr().end() )
         aAcceptedArgs[ aStatusIndicatorString ] = GetMediaDescr()[ aStatusIndicatorString ];
+	if ( GetMediaDescr().find( aFailOnWarningString ) != GetMediaDescr().end() )
+		aAcceptedArgs[ aFailOnWarningString ] = GetMediaDescr()[ aFailOnWarningString ];
 
     // remove unacceptable entry if there is any
     DBG_ASSERT( GetMediaDescr().size() == aAcceptedArgs.size(),

sfx2/source/doc/sfxbasemodel.cxx
@@ -1,30 +1,21 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*************************************************************************
+/*
+ * This file is part of the LibreOffice project.
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 2000, 2010 Oracle and/or its affiliates.
+ * This file incorporates work covered by the following license notice:
  *
- * OpenOffice.org - a multi-platform office productivity suite
- *
- * This file is part of OpenOffice.org.
- *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * only, as published by the Free Software Foundation.
- *
- * OpenOffice.org is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
- * (a copy is included in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
- *
- ************************************************************************/
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
 
 
 //________________________________________________________________________________________________________

zhirshya/lo-tblr

commit 5c4e588f64eb1d38cb108d30f43e1bd21355ec50

fdo#65055: sfx: fix automatic version on close
The parameter "FailOnWarning" is passed around better now, so it arrives
in SfxBaseModel::storeSelf() which considers it illegal.

(regression from e2799d253b1dc62967693aa5bccd7360a9520967)

Change-Id: I2a51a2ecc24e6de506e1840bd28cbe157a8ad6a5

sfx2/source/doc/guisaveas.cxx
@@ -1456,9 +1456,10 @@ sal_Bool SfxStoringHelper::GUIStoreModel( uno::Reference< frame::XModel > xModel
             {
                 aModelData.GetStorable2()->storeSelf( aModelData.GetMediaDescr().getAsConstPropertyValueList() );
             }
-            catch( const lang::IllegalArgumentException& )
+            catch (const lang::IllegalArgumentException& e)
             {
-                OSL_FAIL( "ModelData didn't handle illegal parameters, all the parameters are ignored!\n" );
+                SAL_WARN("sfx", "Ignoring parameters! "
+                    "ModelData considers this illegal:  " << e.Message);
                 aModelData.GetStorable()->store();
             }
         }

sfx2/source/doc/sfxbasemodel.cxx
@@ -1571,7 +1571,9 @@ void SAL_CALL SfxBaseModel::storeSelf( const    Sequence< beans::PropertyValue >
             // check that only acceptable parameters are provided here
             if ( aSeqArgs[nInd].Name != "VersionComment" && aSeqArgs[nInd].Name != "Author"
               && aSeqArgs[nInd].Name != "InteractionHandler" && aSeqArgs[nInd].Name != "StatusIndicator"
-              && aSeqArgs[nInd].Name != "VersionMajor" && aSeqArgs[nInd].Name != "CheckIn" )
+              && aSeqArgs[nInd].Name != "VersionMajor"
+              && aSeqArgs[nInd].Name != "FailOnWarning"
+              && aSeqArgs[nInd].Name != "CheckIn" )
             {
                 m_pData->m_pObjectShell->AddLog( OUString( OSL_LOG_PREFIX "unexpected parameter for storeSelf, might be no problem if SaveAs is executed."  ) );
                 m_pData->m_pObjectShell->StoreLog();