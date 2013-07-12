LibreOffice/core

commit 9e310cc32923ceb4b18d97ce68d54a339b935f01

fdo#38838 Some removal/replacement of the String/UniString with OUString
Change-Id: I6daea312198fae3a9717bd8d4fea6371aa4cd275
Reviewed-on: https://gerrit.libreoffice.org/1962
Reviewed-by: Tor Lillqvist <tml@iki.fi>
Tested-by: Tor Lillqvist <tml@iki.fi>

vcl/inc/win/saldata.hxx
@@ -232,7 +232,7 @@ void ImplSalLogFontToFontW( HDC hDC, const LOGFONTW& rLogFont, Font& rFont );
 
 // \WIN\SOURCE\APP\SALDATA.CXX
 rtl_TextEncoding ImplSalGetSystemEncoding();
-UniString ImplSalGetUniString( const sal_Char* pStr, xub_StrLen nLen = STRING_LEN );
+OUString ImplSalGetUniString( const sal_Char* pStr, xub_StrLen nLen = STRING_LEN );
 int ImplSalWICompareAscii( const wchar_t* pStr1, const char* pStr2 );
 
 // -----------

vcl/win/source/app/saldata.cxx
@@ -44,9 +44,9 @@ rtl_TextEncoding ImplSalGetSystemEncoding()
 
 // -----------------------------------------------------------------------
 
-UniString ImplSalGetUniString( const sal_Char* pStr, xub_StrLen nLen )
+OUString ImplSalGetUniString( const sal_Char* pStr, xub_StrLen nLen )
 {
-    return UniString( pStr, nLen, ImplSalGetSystemEncoding(),
+    return OUString( pStr, nLen, ImplSalGetSystemEncoding(),
                       RTL_TEXTTOUNICODE_FLAGS_UNDEFINED_DEFAULT |
                       RTL_TEXTTOUNICODE_FLAGS_MBUNDEFINED_DEFAULT |
                       RTL_TEXTTOUNICODE_FLAGS_INVALID_DEFAULT );

cui/source/dialogs/cuigaldlg.cxx
@@ -179,7 +179,7 @@ void SearchThread::ImplSearch( const INetURLObject& rStartURL,
                             SolarMutexGuard aGuard;
 
                             mpBrowser->aFoundList.push_back(
-                                new String( aFoundURL.GetMainURL( INetURLObject::NO_DECODE ) )
+                                aFoundURL.GetMainURL( INetURLObject::NO_DECODE )
                             );
                             mpBrowser->aLbxFound.InsertEntry(
                                 GetReducedString( aFoundURL, 50 ),
@@ -309,9 +309,9 @@ void TakeThread::execute()
     for( sal_uInt16 i = 0; i < nEntries && schedule(); i++ )
     {
         if( mpBrowser->bTakeAll )
-            aURL = INetURLObject( *mpBrowser->aFoundList[ nPos = i ] );
+            aURL = INetURLObject( mpBrowser->aFoundList[ nPos = i ] );
         else
-            aURL = INetURLObject(*mpBrowser->aFoundList[ nPos = mpBrowser->aLbxFound.GetSelectEntryPos( i ) ]);
+            aURL = INetURLObject( mpBrowser->aFoundList[ nPos = mpBrowser->aLbxFound.GetSelectEntryPos( i ) ]);
 
         mrTakenList.push_back( (sal_uLong)nPos );
 
@@ -390,14 +390,12 @@ IMPL_LINK_NOARG(TakeProgress, CleanUpHdl)
     // refill found list
     for( i = 0, nCount = aRemoveEntries.size(); i < nCount; ++i )
         if( !aRemoveEntries[ i ] )
-            aRemainingVector.push_back( *mpBrowser->aFoundList[ i ] );
+            aRemainingVector.push_back( mpBrowser->aFoundList[i] );
 
-    for ( i = 0, nCount = mpBrowser->aFoundList.size(); i < nCount; ++i )
-        delete mpBrowser->aFoundList[ i ];
     mpBrowser->aFoundList.clear();
 
     for( i = 0, nCount = aRemainingVector.size(); i < nCount; ++i )
-        mpBrowser->aFoundList.push_back( new String( aRemainingVector[ i ] ) );
+        mpBrowser->aFoundList.push_back( aRemainingVector[ i ] );
 
     aRemainingVector.clear();
 
@@ -812,9 +810,6 @@ TPGalleryThemeProperties::~TPGalleryThemeProperties()
     xMediaPlayer.clear();
     xDialogListener.clear();
 
-    for ( size_t i = 0, n = aFoundList.size(); i < n; ++i )
-        delete aFoundList[ i ];
-
     for ( size_t i = 0, n = aFilterEntryList.size(); i < n; ++i ) {
         delete aFilterEntryList[ i ];
     }
@@ -1011,10 +1006,7 @@ void TPGalleryThemeProperties::SearchFiles()
 {
     SearchProgress* pProgress = new SearchProgress( this, aURL );
 
-    for ( size_t i = 0, n = aFoundList.size(); i < n; ++i )
-        delete aFoundList[ i ];
     aFoundList.clear();
-
     aLbxFound.Clear();
 
     pProgress->SetFileType( aCbbFileType.GetText() );
@@ -1110,7 +1102,7 @@ void TPGalleryThemeProperties::DoPreview()
 
     if( aString != aPreviewString )
     {
-        INetURLObject   _aURL( *aFoundList[ aLbxFound.GetEntryPos( aString ) ] );
+        INetURLObject   _aURL( aFoundList[ aLbxFound.GetEntryPos( aString ) ] );
         bInputAllowed = sal_False;
 
         if ( !aWndPreview.SetGraphic( _aURL ) )

cui/source/inc/cuigaldlg.hxx
@@ -51,7 +51,7 @@ class SearchProgress;
 class TakeProgress;
 class TPGalleryThemeProperties;
 
-typedef ::std::vector< UniString* > StringList;
+typedef ::std::vector< OUString > StringList;
 typedef ::std::vector< sal_uLong > TokenList_impl;
 
 // ---------------

sfx2/source/dialog/mgetempl.cxx
@@ -584,7 +584,7 @@ int SfxManageStyleSheetPage::DeactivatePage( SfxItemSet* pItemSet )
 
     if ( pStyle->HasFollowSupport() && aFollowLb.IsEnabled() )
     {
-        const String aFollowEntry( aFollowLb.GetSelectEntry() );
+        const OUString aFollowEntry( aFollowLb.GetSelectEntry() );
 
         if ( pStyle->GetFollow() != aFollowEntry )
         {
@@ -601,10 +601,10 @@ int SfxManageStyleSheetPage::DeactivatePage( SfxItemSet* pItemSet )
 
     if ( aBaseLb.IsEnabled() )
     {
-        String aParentEntry( aBaseLb.GetSelectEntry() );
+        OUString aParentEntry( aBaseLb.GetSelectEntry() );
 
         if ( SfxResId(STR_NONE).toString().equals(aParentEntry) || aParentEntry == pStyle->GetName() )
-            aParentEntry.Erase();
+            aParentEntry = OUString();
 
         if ( pStyle->GetParent() != aParentEntry )
         {

sfx2/source/dialog/styledlg.cxx
@@ -64,9 +64,7 @@ SfxStyleDialog::SfxStyleDialog
         SetCurPageId( ID_TABPAGE_MANAGESTYLES );
     else
     {
-        String sTxt( GetText() );
-        sTxt += DEFINE_CONST_UNICODE(": ") ;
-        sTxt += rStyle.GetName();
+        OUString sTxt = OUString(GetText()) + ": " + rStyle.GetName();
         SetText( sTxt );
     }
     delete pExampleSet; // in SfxTabDialog::Ctor() already created

sfx2/source/doc/objcont.cxx
@@ -487,7 +487,7 @@ sal_Bool SfxObjectShell::Insert(SfxObjectShell &rSource,
             }
 
             // if such a template already exists: delete!
-            String aOldName(pHisSheet->GetName());
+            OUString aOldName(pHisSheet->GetName());
             SfxStyleFamily eOldFamily = pHisSheet->GetFamily();
 
             SfxStyleSheetBase* pExist = pMyPool->Find(aOldName, eOldFamily);
@@ -600,8 +600,8 @@ sal_Bool SfxObjectShell::Remove
         SetOrganizerSearchMask(pMyPool);
 
         SfxStyleSheetBase* pMySheet =  (*pMyPool)[nIdx2];
-        String aName(pMySheet->GetName());
-        String aEmpty;
+        OUString aName(pMySheet->GetName());
+        OUString aEmpty;
         SfxStyleFamily  eFamily = pMySheet->GetFamily();
         pMyPool->Remove(pMySheet);
         bRet = sal_True;

svtools/source/dialogs/filedlg2.cxx
@@ -37,7 +37,7 @@
 using namespace com::sun::star;
 using namespace com::sun::star::uno;
 
-typedef ::std::vector< UniString* > UniStringList;
+typedef ::std::vector< OUString > OUStringList;
 
 #define INITCONTROL( p, ControlClass, nBits, aPos, aSize, aTitel, rHelpId ) \
     p = new ControlClass( GetPathDialog(), WinBits( nBits ) ); \
@@ -413,27 +413,27 @@ void ImpPathDialog::UpdateEntries( const sal_Bool )
     sal_uInt16 nEntries = aDir.Count();
     if( nEntries )
     {
-        UniStringList aSortDirList;
+        OUStringList aSortDirList;
         const comphelper::string::NaturalStringSorter& rSorter =
             ::vcl::unohelper::getNaturalStringSorterForAppLocale();
         for ( sal_uInt16 n = 0; n < nEntries; n++ )
         {
             DirEntry& rEntry = aDir[n];
-            UniString aName( rEntry.GetName() );
-            if( aName.Len() && ( aName.GetChar(0) != '.' ) && rEntry.Exists() )
+            OUString aName( rEntry.GetName() );
+            if( aName.getLength() && ( aName[0] != '.' ) && rEntry.Exists() )
             {
                 if( FileStat( rEntry ).GetKind() & FSYS_KIND_DIR )
                 {
                     size_t l = 0;
                     for( l = 0; l < aSortDirList.size(); l++ )
-                        if( rSorter.compare( *aSortDirList[ l ], aName ) > 0 )
+                        if( rSorter.compare( aSortDirList[l], aName ) > 0 )
                             break;
                     if ( l < aSortDirList.size() ) {
-                        UniStringList::iterator it = aSortDirList.begin();
+                        OUStringList::iterator it = aSortDirList.begin();
                         ::std::advance( it, l );
-                        aSortDirList.insert( it, new UniString( aName ) );
+                        aSortDirList.insert( it, aName );
                     } else {
-                        aSortDirList.push_back( new UniString( aName ) );
+                        aSortDirList.push_back( aName );
                     }
                 }
             }
@@ -441,10 +441,8 @@ void ImpPathDialog::UpdateEntries( const sal_Bool )
 
         for( size_t l = 0; l < aSortDirList.size(); l++ )
         {
-            UniString aEntryStr( aTabString );
-            aEntryStr += *aSortDirList[ l ];
+            OUString aEntryStr = aTabString + aSortDirList[l];
             pDirList->InsertEntry( aEntryStr );
-            delete aSortDirList[ l ];
         }
     }
 
@@ -624,7 +622,7 @@ void ImpPathDialog::PostExecute()
 {
 }
 
-void ImpPathDialog::SetPath( UniString const & rPath )
+void ImpPathDialog::SetPath( OUString const & rPath )
 {
     aPath = DirEntry( rPath );
 
@@ -644,7 +642,7 @@ void ImpPathDialog::SetPath( UniString const & rPath )
     pSvPathDialog->LeaveWait();
 }
 
-UniString ImpPathDialog::GetPath() const
+OUString ImpPathDialog::GetPath() const
 {
     DirEntry aFile( pEdit->GetText() );
     aFile.ToAbs();

zhirshya/lo-tblr

commit 1234fc0da850cb0b9590be6006d95bfe198e1218

fdo#66811: vcl: fix broken OUString with length STRING_LEN
ImplSalGetUniString was wrongly converted and constructs OUString with
invalid length in WinSalGraphics::CreateFontSubset; this is then
implicitly converted to an empty UniString so the font names are
missing in the PDF files generated on Windows.

(regression from 9e310cc32923ceb4b18d97ce68d54a339b935f01)

Change-Id: I1603e62cf18f353f3d7de322b9111a173dc6b225

vcl/inc/win/saldata.hxx
@@ -196,7 +196,7 @@ void ImplSalPostDispatchMsg( MSG* pMsg, LRESULT nDispatchResult );
 void ImplSalLogFontToFontW( HDC hDC, const LOGFONTW& rLogFont, Font& rFont );
 
 rtl_TextEncoding ImplSalGetSystemEncoding();
-OUString ImplSalGetUniString( const sal_Char* pStr, xub_StrLen nLen = STRING_LEN );
+OUString ImplSalGetUniString(const sal_Char* pStr, sal_Int32 nLen = -1);
 int ImplSalWICompareAscii( const wchar_t* pStr1, const char* pStr2 );
 
 #define SAL_FRAME_WNDEXTRA          sizeof( DWORD )

vcl/win/source/app/saldata.cxx
@@ -44,9 +44,10 @@ rtl_TextEncoding ImplSalGetSystemEncoding()
 
 // -----------------------------------------------------------------------
 
-OUString ImplSalGetUniString( const sal_Char* pStr, xub_StrLen nLen )
+OUString ImplSalGetUniString(const sal_Char* pStr, sal_Int32 const nLen)
 {
-    return OUString( pStr, nLen, ImplSalGetSystemEncoding(),
+    return OUString( pStr, (-1 == nLen) ? strlen(pStr) : nLen,
+                      ImplSalGetSystemEncoding(),
                       RTL_TEXTTOUNICODE_FLAGS_UNDEFINED_DEFAULT |
                       RTL_TEXTTOUNICODE_FLAGS_MBUNDEFINED_DEFAULT |
                       RTL_TEXTTOUNICODE_FLAGS_INVALID_DEFAULT );

vcl/win/source/gdi/salprn.cxx
@@ -266,7 +266,7 @@ OUString WinSalInstance::GetDefaultPrinter()
         char* pTmp = pBuf;
         while ( *pTmp && (*pTmp != ',') )
             pTmp++;
-        return ImplSalGetUniString( pBuf, (xub_StrLen)(pTmp-pBuf) );
+        return ImplSalGetUniString( pBuf, static_cast<sal_Int32>(pTmp-pBuf) );
     }
     else
         return OUString();