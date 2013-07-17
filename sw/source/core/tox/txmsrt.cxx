LibreOffice/core

fdo#39904, n#825976: implement hyperlinks for Illustrations index
Change-Id: I6f682c382e7ab0e06259b335247fdcebd7412942

sw/source/core/tox/txmsrt.cxx
@@ -572,12 +572,13 @@ sal_uInt16 SwTOXContent::GetLevel() const
    The position must not come from the document, but from the "anchor"!
  --------------------------------------------------------------------*/
 
-SwTOXPara::SwTOXPara( const SwCntntNode& rNd, SwTOXElement eT, sal_uInt16 nLevel )
+SwTOXPara::SwTOXPara( const SwCntntNode& rNd, SwTOXElement eT, sal_uInt16 nLevel, OUString sSeqName )
     : SwTOXSortTabBase( TOX_SORT_PARA, &rNd, 0, 0 ),
     eType( eT ),
     m_nLevel(nLevel),
     nStartIndex(0),
-    nEndIndex(STRING_LEN)
+    nEndIndex(STRING_LEN),
+    m_sSequenceName( sSeqName )
 {
 }
 
@@ -698,6 +699,14 @@ String SwTOXPara::GetURL() const
                 if( pStr )
                     aTxt.AppendAscii( pStr );
             }
+        }
+        break;
+    case nsSwTOXElement::TOX_SEQUENCE:
+        {
+            aTxt = '#';
+            aTxt += m_sSequenceName;
+            aTxt += cMarkSeperator;
+            aTxt.AppendAscii( pMarkToSequence );
         }
         break;
     default: break;

sw/source/ui/uiview/view2.cxx
@@ -115,6 +115,7 @@
 #include <svtools/templdlg.hxx>
 #include <dbconfig.hxx>
 #include <dbmgr.hxx>
+#include <reffld.hxx>
 
 #include <PostItMgr.hxx>
 
@@ -1867,7 +1868,7 @@ bool SwView::JumpToSwMark( const String& rMark )
 
         if( sCmp.Len() )
         {
-            String sName( sMark.Copy( 0, nPos ) );
+            rtl::OUString sName( sMark.Copy( 0, nPos ) );
             sCmp.ToLowerAscii();
             FlyCntType eFlyType = FLYCNTTYPE_ALL;
 
@@ -1891,6 +1892,17 @@ bool SwView::JumpToSwMark( const String& rMark )
             {
                 pWrtShell->EnterStdMode();
                 bRet = pWrtShell->GotoTable( sName );
+            }
+            else if( COMPARE_EQUAL == sCmp.CompareToAscii( pMarkToSequence ) )
+            {
+                pWrtShell->EnterStdMode();
+                sal_Int32 nNoPos = sName.indexOf( cSequenceMarkSeparator );
+                if ( nNoPos != -1 )
+                {
+                    sal_uInt16 nSeqNo = sName.copy( nNoPos + 1 ).toInt32();
+                    sName = sName.copy( 0, nNoPos );
+                    pWrtShell->GotoRefMark( sName, REF_SEQUENCEFLD, nSeqNo );
+                }
             }
             else if( COMPARE_EQUAL == sCmp.CompareToAscii( pMarkToText ) )
             {

zhirshya/lo-tblr

(cherry picked from commit 8bb2c7f1556c10508c8ac451c579539ccf5dacfb)
Conflicts:
	sw/inc/swtypes.hxx

sw/source/core/tox/txmsrt.cxx
@@ -571,12 +571,13 @@ sal_uInt16 SwTOXContent::GetLevel() const
    The position must not come from the document, but from the "anchor"!
  --------------------------------------------------------------------*/
 
-SwTOXPara::SwTOXPara( const SwCntntNode& rNd, SwTOXElement eT, sal_uInt16 nLevel )
+SwTOXPara::SwTOXPara( const SwCntntNode& rNd, SwTOXElement eT, sal_uInt16 nLevel, OUString sSeqName )
     : SwTOXSortTabBase( TOX_SORT_PARA, &rNd, 0, 0 ),
     eType( eT ),
     m_nLevel(nLevel),
     nStartIndex(0),
-    nEndIndex(STRING_LEN)
+    nEndIndex(STRING_LEN),
+    m_sSequenceName( sSeqName )
 {
 }
 
@@ -697,6 +698,14 @@ String SwTOXPara::GetURL() const
                 if( pStr )
                     aTxt.AppendAscii( pStr );
             }
+        }
+        break;
+    case nsSwTOXElement::TOX_SEQUENCE:
+        {
+            aTxt = '#';
+            aTxt += m_sSequenceName;
+            aTxt += cMarkSeparator;
+            aTxt.AppendAscii( pMarkToSequence );
         }
         break;
     default: break;

sw/source/ui/uiview/view2.cxx
@@ -115,6 +115,7 @@
 #include <svtools/templdlg.hxx>
 #include <dbconfig.hxx>
 #include <dbmgr.hxx>
+#include <reffld.hxx>
 
 #include <PostItMgr.hxx>
 
@@ -1880,7 +1881,7 @@ bool SwView::JumpToSwMark( const String& rMark )
 
         if( sCmp.Len() )
         {
-            String sName( sMark.Copy( 0, nPos ) );
+            rtl::OUString sName( sMark.Copy( 0, nPos ) );
             sCmp.ToLowerAscii();
             FlyCntType eFlyType = FLYCNTTYPE_ALL;
 
@@ -1904,6 +1905,17 @@ bool SwView::JumpToSwMark( const String& rMark )
             {
                 m_pWrtShell->EnterStdMode();
                 bRet = m_pWrtShell->GotoTable( sName );
+            }
+            else if( COMPARE_EQUAL == sCmp.CompareToAscii( pMarkToSequence ) )
+            {
+                m_pWrtShell->EnterStdMode();
+                sal_Int32 nNoPos = sName.indexOf( cSequenceMarkSeparator );
+                if ( nNoPos != -1 )
+                {
+                    sal_uInt16 nSeqNo = sName.copy( nNoPos + 1 ).toInt32();
+                    sName = sName.copy( 0, nNoPos );
+                    m_pWrtShell->GotoRefMark( sName, REF_SEQUENCEFLD, nSeqNo );
+                }
             }
             else if( COMPARE_EQUAL == sCmp.CompareToAscii( pMarkToText ) )
             {