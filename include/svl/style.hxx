LibreOffice/core

commit a0dcf961879ab644a52f801f65466756cb144b72

Hidden styles: ODF import/export
Change-Id: I1138314eba33dc8cf8d1f60e77ac419b4550bbcc

sw/source/ui/app/docstyle.cxx
@@ -523,39 +523,36 @@ sal_Bool SwDocStyleSheet::IsHidden( ) const
 {
     sal_Bool bRet = sal_False;
 
-    if(!bPhysical)
+    SwFmt* pFmt = 0;
+    switch(nFamily)
     {
-        SwFmt* pFmt = 0;
-        switch(nFamily)
-        {
-            case SFX_STYLE_FAMILY_CHAR:
-                pFmt = rDoc.FindCharFmtByName( aName );
-                bRet = pFmt && pFmt->IsHidden( );
-                break;
+        case SFX_STYLE_FAMILY_CHAR:
+            pFmt = rDoc.FindCharFmtByName( aName );
+            bRet = pFmt && pFmt->IsHidden( );
+            break;
 
-            case SFX_STYLE_FAMILY_PARA:
-                pFmt = rDoc.FindTxtFmtCollByName( aName );
-                bRet = pFmt && pFmt->IsHidden( );
-                break;
+        case SFX_STYLE_FAMILY_PARA:
+            pFmt = rDoc.FindTxtFmtCollByName( aName );
+            bRet = pFmt && pFmt->IsHidden( );
+            break;
 
-            case SFX_STYLE_FAMILY_FRAME:
-                pFmt = rDoc.FindFrmFmtByName( aName );
-                bRet = pFmt && pFmt->IsHidden( );
-                break;
+        case SFX_STYLE_FAMILY_FRAME:
+            pFmt = rDoc.FindFrmFmtByName( aName );
+            bRet = pFmt && pFmt->IsHidden( );
+            break;
 
-            case SFX_STYLE_FAMILY_PAGE:
-                {
-                    SwPageDesc* pPgDesc = rDoc.FindPageDescByName( aName );
-                    bRet = pPgDesc && pPgDesc->IsHidden( );
-                }
-                break;
-            case SFX_STYLE_FAMILY_PSEUDO:
-                {
-                    SwNumRule* pRule = rDoc.FindNumRulePtr( aName );
-                    bRet = pRule && pRule->IsHidden( );
-                }
-            default:;
-        }
+        case SFX_STYLE_FAMILY_PAGE:
+            {
+                SwPageDesc* pPgDesc = rDoc.FindPageDescByName( aName );
+                bRet = pPgDesc && pPgDesc->IsHidden( );
+            }
+            break;
+        case SFX_STYLE_FAMILY_PSEUDO:
+            {
+                SwNumRule* pRule = rDoc.FindNumRulePtr( aName );
+                bRet = pRule && pRule->IsHidden( );
+            }
+        default:;
     }
 
     return bRet;

xmloff/inc/xmloff/xmlnume.hxx
@@ -86,7 +86,7 @@ public:
                        XMLTextListAutoStylePool *pPool,
                        sal_Bool bExportChapterNumbering = sal_True );
     void exportNumberingRule(
-            const ::rtl::OUString& rName,
+            const ::rtl::OUString& rName, sal_Bool bIsHidden,
             const ::com::sun::star::uno::Reference<
                 ::com::sun::star::container::XIndexReplace > & xNumRule );
 };

xmloff/inc/xmloff/xmlstyle.hxx
@@ -65,6 +65,7 @@ class XMLOFF_DLLPUBLIC SvXMLStyleContext : public SvXMLImportContext
     ::rtl::OUString     maAutoName;
     ::rtl::OUString     maParentName;// Will be moved to XMLPropStyle soon!!!!
     ::rtl::OUString     maFollow;   // Will be moved to XMLPropStyle soon!!!!
+    sal_Bool            mbHidden;
 
     ::rtl::OUString     maHelpFile; // Will be removed very soon!!!!
 
@@ -124,6 +125,8 @@ public:
 
     sal_Bool IsNew() const { return mbNew; }
     void SetNew( sal_Bool b ) { mbNew = b; }
+
+    sal_Bool IsHidden() const { return mbHidden; }
 
     // This method is called for every default style
     virtual void SetDefaults();

xmloff/source/draw/sdpropls.cxx
@@ -1567,7 +1567,7 @@ void XMLShapeExportPropertyMapper::handleElementItem(
                 {
                     uno::Reference< container::XIndexReplace > xNumRule( rProperty.maValue, uno::UNO_QUERY );
                     if( xNumRule.is() )
-                        const_cast<XMLShapeExportPropertyMapper*>(this)->maNumRuleExp.exportNumberingRule( maStyleName, xNumRule );
+                        const_cast<XMLShapeExportPropertyMapper*>(this)->maNumRuleExp.exportNumberingRule( maStyleName, sal_False, xNumRule );
                 }
             }
             break;

xmloff/source/style/XMLPageExport.cxx
@@ -117,6 +117,9 @@ sal_Bool XMLPageExport::exportStyle(
         GetExport().AddAttribute( XML_NAMESPACE_STYLE, XML_NAME,
                           GetExport().EncodeStyleName( sName, &bEncoded ) );
 
+        if ( rStyle->isHidden( ) && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
+            GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
+
         if( bEncoded )
             GetExport().AddAttribute( XML_NAMESPACE_STYLE, XML_DISPLAY_NAME,
                                    sName);

xmloff/source/style/prstylei.cxx
@@ -341,6 +341,8 @@ void XMLPropStyleContext::Finish( sal_Bool bOverwrite )
         if( !xFamilies.is() )
             return;
 
+        mxStyle->setHidden( IsHidden( ) );
+
         // connect parent
         OUString sParent( GetParentName() );
         if( !sParent.isEmpty() )

xmloff/source/style/styleexp.cxx
@@ -121,6 +121,9 @@ sal_Bool XMLStyleExport::exportStyle(
     if( !rXMLFamily.isEmpty() )
         GetExport().AddAttribute( XML_NAMESPACE_STYLE, XML_FAMILY, rXMLFamily);
 
+    if ( rStyle->isHidden( ) && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
+        GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
+
     // style:parent-style-name="..."
     OUString sParentString(rStyle->getParentStyle());
     OUString sParent;

xmloff/source/style/xmlnume.cxx
@@ -682,7 +682,7 @@ SvxXMLNumRuleExport::~SvxXMLNumRuleExport()
 }
 
 void SvxXMLNumRuleExport::exportNumberingRule(
-        const OUString& rName,
+        const OUString& rName, sal_Bool bIsHidden,
         const Reference< XIndexReplace >& rNumRule )
 {
     Reference< XPropertySet > xPropSet( rNumRule, UNO_QUERY );
@@ -702,6 +702,10 @@ void SvxXMLNumRuleExport::exportNumberingRule(
             GetExport().AddAttribute( XML_NAMESPACE_STYLE, XML_DISPLAY_NAME,
                                  rName);
     }
+
+    // style:hidden="..."
+    if ( bIsHidden && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
+        GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
 
     // text:consecutive-numbering="..."
     sal_Bool bContNumbering = sal_False;
@@ -747,7 +751,7 @@ sal_Bool SvxXMLNumRuleExport::exportStyle( const Reference< XStyle >& rStyle )
 
     OUString sName = rStyle->getName();
 
-    exportNumberingRule( sName, xNumRule );
+    exportNumberingRule( sName, rStyle->isHidden(), xNumRule );
 
     return sal_True;
 }
@@ -784,7 +788,7 @@ void SvxXMLNumRuleExport::exportOutline()
                    nODFVersion == SvtSaveOptions::ODFVER_011 ) &&
                  GetExport().writeOutlineStyleAsNormalListStyle() )
             {
-                exportNumberingRule( sOutlineStyleName, xNumRule );
+                exportNumberingRule( sOutlineStyleName, sal_False, xNumRule );
             }
             else
             {

zhirshya/lo-tblr

commit 8cef8b6621b14dc3dbf0dd3c64063ce4b63bf057

fdo#67303: sw: speed up setting IsHidden property on styles
Importing an ODF document with 4089 styles is ridiculously slow,
spending most of the time in SwStyleSheetIterator::First() to reset
an existing iterator.

Since the SwStyleSheetIterator can handle the case when First() has not
been called anyway, just add a new method that resets the bFirstCalled
flag, which takes next to no time.

(regression from a0dcf961879ab644a52f801f65466756cb144b72)

Change-Id: I8741b4c0cd51f8c244462ce28a754abc427d0df3

include/svl/style.hxx
@@ -203,9 +203,9 @@ friend class SfxStyleSheetBase;
 
     SfxStyleSheetBasePool_Impl *pImp;
 
-private:
-    SVL_DLLPRIVATE SfxStyleSheetIterator&      GetIterator_Impl();
 protected:
+    SfxStyleSheetIterator&      GetIterator_Impl();
+
     OUString                    aAppName;
     SfxItemPool&                rPool;
     SfxStyles                   aStyles;

sw/inc/docstyle.hxx
@@ -175,6 +175,8 @@ public:
     virtual SfxStyleSheetBase* Find(const OUString& rStr);
 
     virtual void Notify( SfxBroadcaster&, const SfxHint& );
+
+    void InvalidateIterator();
 };
 
 
@@ -216,6 +218,8 @@ public:
 
     virtual void SAL_CALL acquire(  ) throw ();
     virtual void SAL_CALL release(  ) throw ();
+
+    void InvalidateIterator();
 
 protected:
     virtual ~SwDocStyleSheetPool();

sw/source/ui/app/docstyle.cxx
@@ -476,7 +476,8 @@ void SwDocStyleSheet::SetHidden( sal_Bool bValue )
 
     if( bChg )
     {
-        pPool->First();  // internal list has to be updated
+        // calling pPool->First() here would be quite slow...
+        dynamic_cast<SwDocStyleSheetPool*>(pPool)->InvalidateIterator(); // internal list has to be updated
         pPool->Broadcast( SfxStyleSheetHint( SFX_STYLESHEET_MODIFIED, *this ) );
         SwEditShell* pSh = rDoc.GetEditShell();
         if( pSh )
@@ -2772,6 +2773,7 @@ SfxStyleSheetBase*  SwStyleSheetIterator::First()
 
 SfxStyleSheetBase*  SwStyleSheetIterator::Next()
 {
+    assert(bFirstCalled);
     ++nLastPos;
     if(!aLst.empty() && nLastPos < aLst.size())
     {
@@ -2864,6 +2866,21 @@ void SwStyleSheetIterator::AppendStyleList(const boost::ptr_vector<String>& rLis
         if ( ( !bTestUsed && bMatchHidden ) || ( bTestUsed && bUsed ) )
             aLst.Append( cType, rList[i] );
     }
+}
+
+void SwDocStyleSheetPool::InvalidateIterator()
+{
+    dynamic_cast<SwStyleSheetIterator&>(GetIterator_Impl()).InvalidateIterator();
+}
+
+void  SwStyleSheetIterator::InvalidateIterator()
+{
+    // potentially we could send an SfxHint to Notify but currently it's
+    // iterating over the vector anyway so would still be slow - why does
+    // this iterator not use a map?
+    bFirstCalled = false;
+    nLastPos = 0;
+    aLst.Erase();
 }
 
 void  SwStyleSheetIterator::Notify( SfxBroadcaster&, const SfxHint& rHint )