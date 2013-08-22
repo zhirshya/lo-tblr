LibreOffice/core

commit 60c17b2873506ed8b068b34840aecda0c1d9bd65

fdo#61909 fix RTF import of default font without RTF_PLAIN
Change-Id: Ia86548bddb7b87ec9653192c2e5989ce0dc50057

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -394,6 +394,11 @@ void RTFDocumentImpl::checkFirstRun()
         if (!m_pSuperstream)
             Mapper().startSectionGroup();
         Mapper().startParagraphGroup();
+
+        // set the requested default font
+        RTFValue::Pointer_t pFont = m_aDefaultState.aCharacterSprms.find(NS_sprm::LN_CRgFtc0);
+        if (pFont.get())
+            dispatchValue(RTF_F, pFont->getInt());
         m_bFirstRun = false;
     }
 }

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -145,6 +145,7 @@ public:
     void testFdo59953();
     void testFdo59638();
     void testFdo60722();
+    void testFdo61909();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -262,6 +263,7 @@ void Test::run()
         {"fdo59953.rtf", &Test::testFdo59953},
         {"fdo59638.rtf", &Test::testFdo59638},
         {"fdo60722.rtf", &Test::testFdo60722},
+        {"fdo61909.rtf", &Test::testFdo61909},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1164,6 +1166,12 @@ void Test::testFdo60722()
     xShape.set(xDraws->getByIndex(2), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(sal_uInt32(26), getProperty<sal_uInt32>(xShape, "LineWidth"));
     CPPUNIT_ASSERT_EQUAL(sal_uInt32(0), getProperty<sal_uInt32>(xShape, "LineColor"));
+}
+
+void Test::testFdo61909()
+{
+    uno::Reference<text::XTextRange> xTextRange = getRun(getParagraph(1), 1);
+    CPPUNIT_ASSERT_EQUAL(OUString("Courier New"), getProperty<OUString>(xTextRange, "CharFontName"));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

sw/qa/extras/rtfimport/data/fdo61909.rtf
@@ -1,0 +1,6 @@
+{\rtf1\deff0
+{\fonttbl
+{\f000 Courier New;}
+}
+#include<iostream.h>\par
+}

zhirshya/lo-tblr

commit c05334b394cff71fcc0b246265ea866775252705

fdo#68076 RTF import: default font had priority over the specified one
Regression from 60c17b2873506ed8b068b34840aecda0c1d9bd65.

Change-Id: Ie16d611622e6a814d5cbea00c3f992cd9ecba027

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -382,9 +382,10 @@ void RTFDocumentImpl::checkFirstRun()
             Mapper().startSectionGroup();
         Mapper().startParagraphGroup();
 
-        // set the requested default font
+        // set the requested default font, if there are none
         RTFValue::Pointer_t pFont = m_aDefaultState.aCharacterSprms.find(NS_sprm::LN_CRgFtc0);
-        if (pFont.get())
+        RTFValue::Pointer_t pCurrentFont = m_aStates.top().aCharacterSprms.find(NS_sprm::LN_CRgFtc0);
+        if (pFont && !pCurrentFont)
             dispatchValue(RTF_F, pFont->getInt());
         m_bFirstRun = false;
     }

sw/qa/extras/rtfimport/data/fdo68076.rtf
@@ -1,0 +1,18 @@
+{\rtf1\ansi\ansicpg1251\uc1 \deff0\deflang1033\deflangfe1049
+{\fonttbl
+{\f0\froman\fcharset0\fprq2
+{\*\panose 02020603050405020304}
+Times New Roman;}
+{\f31\froman\fcharset238\fprq2 Times New Roman CE;}
+{\f32\froman\fcharset204\fprq2 Times New Roman Cyr;}
+{\f34\froman\fcharset161\fprq2 Times New Roman Greek;}
+{\f35\froman\fcharset162\fprq2 Times New Roman Tur;}
+{\f36\froman\fcharset186\fprq2 Times New Roman Baltic;}
+}
+\paperw11906\paperh16838\margl1701\margr1134\margt1276\margb992 \widowctrl\ftnbj\aenddoc\hyphhotz142\hyphcaps0\hyphauto1\formshade\viewkind1\viewscale90\viewzk2\pgbrdrhead\pgbrdrfoot \fet0\sectd 
+\psz9\linex0\headery709\footery709\colsx709\endnhere\titlepg\sectdefaultcl 
+\pard\plain \li5812\nowidctlpar\widctlpar\adjustright \fs28\lang1049\cgrid 
+{\f32 \'ce\'e1\'fa\'e5\'ea\'f2 \endash  \'f3}
+{
+\par }
+}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -165,6 +165,7 @@ public:
     void testFdo63428();
     void testGroupshapeRotation();
     void testFdo44715();
+    void testFdo68076();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -314,6 +315,7 @@ void Test::run()
         {"hello.rtf", &Test::testFdo63428},
         {"groupshape-rotation.rtf", &Test::testGroupshapeRotation},
         {"fdo44715.rtf", &Test::testFdo44715},
+        {"fdo68076.rtf", &Test::testFdo68076},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1538,6 +1540,13 @@ void Test::testFdo44715()
     uno::Reference<text::XTextRange> xCell(xTable->getCellByName("A1"), uno::UNO_QUERY);
     // Style information wasn't reset, which caused character height to be 16.
     CPPUNIT_ASSERT_EQUAL(12.f, getProperty<float>(getParagraphOfText(2, xCell->getText()), "CharHeight"));
+}
+
+void Test::testFdo68076()
+{
+    // Encoding of the last char was wrong (more 'o' than 'y').
+    OUString aExpected("\xD0\x9E\xD0\xB1\xD1\x8A\xD0\xB5\xD0\xBA\xD1\x82 \xE2\x80\x93 \xD1\x83", 19, RTL_TEXTENCODING_UTF8);
+    getParagraph(1, aExpected);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);