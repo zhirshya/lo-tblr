LibreOffice/core

commit 5089bdb234a93bcb62b597c18b8ab66048246b13

Related: fdo#38838 remove UniString::EqualsAscii
Change-Id: I55c85c1c44452c7fb6ac40591aea7177d054affe

sw/source/core/unocore/unofield.cxx
@@ -2590,44 +2590,40 @@ SwXTextFieldMasters::~SwXTextFieldMasters()
     Falls wir grosszuegig werden wollen, dann koennte man com.sun.star.text
     auch optional weglassen
   -----------------------------------------------------------------------*/
-static sal_uInt16 lcl_GetIdByName( String& rName, String& rTypeName )
+static sal_uInt16 lcl_GetIdByName( OUString& rName, OUString& rTypeName )
 {
-    if( rName.EqualsAscii( COM_TEXT_FLDMASTER, 0, RTL_CONSTASCII_LENGTH(COM_TEXT_FLDMASTER ))
-        ||  rName.EqualsAscii( COM_TEXT_FLDMASTER_CC, 0, RTL_CONSTASCII_LENGTH(COM_TEXT_FLDMASTER_CC )))
-        rName.Erase(0, 30);
+    if (rName.startsWith(COM_TEXT_FLDMASTER) || rName.startsWith(COM_TEXT_FLDMASTER_CC))
+        rName = rName.copy(30);
 
     sal_uInt16 nResId = USHRT_MAX;
     sal_Int32 nFound = 0;
-    rTypeName = rName.GetToken( 0, '.', nFound );
-    if(rTypeName.EqualsAscii("User"))
+    rTypeName = rName.getToken( 0, '.', nFound );
+    if (rTypeName == "User")
         nResId = RES_USERFLD;
-    else if(rTypeName.EqualsAscii("DDE"))
+    else if (rTypeName == "DDE")
         nResId = RES_DDEFLD;
-    else if(rTypeName.EqualsAscii("SetExpression"))
+    else if (rTypeName == "SetExpression")
     {
         nResId = RES_SETEXPFLD;
 
-        String sFldTypName( rName.GetToken( 1, '.' ));
+        String sFldTypName( rName.getToken( 1, '.' ));
         String sUIName( SwStyleNameMapper::GetSpecialExtraUIName( sFldTypName ) );
 
         if( sUIName != sFldTypName )
-            rName.SetToken( 1, '.', sUIName );
+            rName = comphelper::string::setToken(rName, 1, '.', sUIName);
     }
-    else if(rTypeName.EqualsAscii("DataBase"))
+    else if (rTypeName == "DataBase")
     {
-        rName.Erase( 0, RTL_CONSTASCII_LENGTH( "DataBase." ));
+        rName = rName.copy(RTL_CONSTASCII_LENGTH("DataBase."));
         sal_uInt16 nDotCount = comphelper::string::getTokenCount(rName, '.');
         if( 2 <= nDotCount )
         {
             // #i51815#
-            //rName.SearchAndReplace('.', DB_DELIM);
-            //rName.SetChar( rName.SearchBackward( '.' ), DB_DELIM );
-
             rName = OUString("DataBase.") + rName;
             nResId = RES_DBFLD;
         }
     }
-    else if( rTypeName.EqualsAscii("Bibliography"))
+    else if (rTypeName == "Bibliography")
         nResId = RES_AUTHORITY;
     return nResId;
 }
@@ -2639,12 +2635,12 @@ uno::Any SwXTextFieldMasters::getByName(const OUString& rName)
     if(!GetDoc())
         throw uno::RuntimeException();
 
-    String sName(rName), sTypeName;
+    OUString sName(rName), sTypeName;
     sal_uInt16 nResId = lcl_GetIdByName( sName, sTypeName );
     if( USHRT_MAX == nResId )
         throw container::NoSuchElementException();
 
-    sName.Erase(0, sTypeName.Len()+1);
+    sName = sName.copy(sTypeName.getLength()+1);
     SwFieldType* pType = GetDoc()->GetFldType(nResId, sName, sal_True);
     if(!pType)
         throw container::NoSuchElementException();
@@ -2726,12 +2722,12 @@ sal_Bool SwXTextFieldMasters::hasByName(const OUString& rName) throw( uno::Runti
     if(!GetDoc())
         throw uno::RuntimeException();
 
-    String sName(rName), sTypeName;
+    OUString sName(rName), sTypeName;
     sal_uInt16 nResId = lcl_GetIdByName( sName, sTypeName );
     sal_Bool bRet = sal_False;
     if( USHRT_MAX != nResId )
     {
-        sName.Erase(0, sTypeName.Len()+1);
+        sName = sName.copy(sTypeName.getLength()+1);
         bRet = USHRT_MAX != nResId && 0 != GetDoc()->GetFldType(nResId, sName, sal_True);
     }
     return bRet;

comphelper/source/misc/string.cxx
@@ -417,6 +417,38 @@ sal_Int32 indexOfAny(OUString const& rIn,
     return -1;
 }
 
+OUString setToken(const OUString& rIn, sal_Int32 nToken, sal_Unicode cTok,
+    const OUString& rNewToken)
+{
+    const sal_Unicode* pStr = rIn.getStr();
+    sal_Int32 nLen = rIn.getLength();
+    sal_Int32 nTok = 0;
+    sal_Int32 nFirstChar = 0;
+    sal_Int32 i = 0;
+
+    // Determine token position and length
+    while ( i < nLen )
+    {
+        // Increase token count if match
+        if (*pStr == cTok)
+        {
+            ++nTok;
+
+            if (nTok == nToken)
+                nFirstChar = i+1;
+            else if (nTok > nToken)
+                break;
+        }
+
+        ++pStr,
+        ++i;
+    }
+
+    if (nTok >= nToken)
+        return rIn.replaceAt(nFirstChar, i-nFirstChar, rNewToken);
+    return rIn;
+}
+
 } }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/comphelper/string.hxx
@@ -285,6 +285,17 @@ COMPHELPER_DLLPUBLIC inline OUStringBuffer& padToLength(
     return detail::padToLength(rBuffer, nLength, cFill);
 }
 
+/** Replace a token in a string
+    @param rIn       OUString in which the token is to be replaced
+    @param nToken    which nToken to replace
+    @param cTok      token delimiter
+    @param rNewToken replacement token
+
+    @return original string with token nToken replaced by rNewToken
+ */
+COMPHELPER_DLLPUBLIC OUString setToken(const OUString& rIn, sal_Int32 nToken, sal_Unicode cTok,
+    const OUString& rNewToken);
+
 /** Find any of a list of code units in the string.
     @param rIn      OUString to search
     @param pChars   0-terminated array of sal_Unicode code units to search for

include/tools/string.hxx
@@ -230,11 +230,8 @@ public:
     StringCompare       CompareToAscii( const sal_Char* pAsciiStr,
                                         xub_StrLen nLen = STRING_LEN ) const;
     sal_Bool            Equals( const UniString& rStr ) const;
-    sal_Bool            EqualsAscii( const sal_Char* pAsciiStr ) const;
     sal_Bool            Equals( const UniString& rStr,
                                 xub_StrLen nIndex, xub_StrLen nLen ) const;
-    sal_Bool            EqualsAscii( const sal_Char* pAsciiStr,
-                                     xub_StrLen nIndex, xub_StrLen nLen ) const;
 
     xub_StrLen          Match( const UniString& rStr ) const;
 

sw/inc/iodetect.hxx
@@ -46,20 +46,19 @@
 
 struct SwIoDetect
 {
-    const sal_Char* pName;
-    sal_uInt16 nLen;
+    const OUString sName;
 
-    inline SwIoDetect( const sal_Char *pN, sal_uInt16 nL )
-        : pName( pN ), nLen( nL )
-    {}
-
-    inline int IsFilter( const String& rNm ) const
+    SwIoDetect(const OUString &rN)
+        : sName(rN)
     {
-        return pName && rNm.EqualsAscii( pName, 0, nLen );
     }
 
-    const sal_Char* IsReader( const sal_Char* pHeader, sal_uLong nLen_,
-            const String &rFileName, const String& rUserData ) const;
+    bool IsFilter( const OUString& rNm ) const
+    {
+        return rNm.startsWith(sName);
+    }
+
+    OUString IsReader(const sal_Char* pHeader, sal_uLong nLen_) const;
 };
 
 enum ReaderWriterEnum {

sw/inc/shellres.hxx
@@ -57,7 +57,7 @@ struct SW_DLLPUBLIC ShellResource : public Resource
 
     //Names of TOXs.
     String          aTOXIndexName;
-    String          aTOXUserName;
+    OUString        aTOXUserName;
     String          aTOXContentName;
     String          aTOXIllustrationsName;
     String          aTOXObjectsName;

sw/source/core/unocore/unoidx.cxx
@@ -160,11 +160,11 @@ static void
 lcl_ConvertTOUNameToUserName(OUString& rTmp)
 {
     ShellResource* pShellRes = ViewShell::GetShellRes();
-    if(rTmp.equalsAscii(cUserDefined))
+    if (rTmp.equalsAscii(cUserDefined))
     {
         rTmp = pShellRes->aTOXUserName;
     }
-    else if(!pShellRes->aTOXUserName.EqualsAscii(cUserDefined) &&
+    else if (!pShellRes->aTOXUserName.equalsAscii(cUserDefined) &&
         USER_AND_SUFFIXLEN == rTmp.getLength())
     {
         //make sure that in non-English versions the " (user)" suffix is removed

sw/source/filter/basflt/iodetect.cxx
@@ -29,21 +29,20 @@ bool IsDocShellRegistered();
 
 SwIoDetect aFilterDetect[] =
 {
-    SwIoDetect( FILTER_RTF,      STRING_LEN ),
-    SwIoDetect( FILTER_BAS,      STRING_LEN ),
-    SwIoDetect( sWW6,            STRING_LEN ),
-    SwIoDetect( FILTER_WW8,      STRING_LEN ),
-    SwIoDetect( sRtfWH,          STRING_LEN ),
-    SwIoDetect( sHTML,           4 ),
-    SwIoDetect( sWW1,            STRING_LEN ),
-    SwIoDetect( sWW5,            STRING_LEN ),
-    SwIoDetect( FILTER_XML,      4 ),
-    SwIoDetect( FILTER_TEXT_DLG, 8 ),
-    SwIoDetect( FILTER_TEXT,     4 )
+    SwIoDetect( FILTER_RTF ),
+    SwIoDetect( FILTER_BAS ),
+    SwIoDetect( sWW6 ),
+    SwIoDetect( FILTER_WW8 ),
+    SwIoDetect( sRtfWH ),
+    SwIoDetect( sHTML ),
+    SwIoDetect( sWW1 ),
+    SwIoDetect( sWW5 ),
+    SwIoDetect( FILTER_XML ),
+    SwIoDetect( FILTER_TEXT_DLG ),
+    SwIoDetect( FILTER_TEXT )
 };
 
-const sal_Char* SwIoDetect::IsReader(const sal_Char* pHeader, sal_uLong nLen_,
-    const String & /*rFileName*/, const String& /*rUserData*/) const
+OUString SwIoDetect::IsReader(const sal_Char* pHeader, sal_uLong nLen_) const
 {
     // Filter erkennung
     struct W1_FIB
@@ -63,12 +62,11 @@ const sal_Char* SwIoDetect::IsReader(const sal_Char* pHeader, sal_uLong nLen_,
     };
 
     int bRet = sal_False;
-    OString aName( pName );
-    if ( sHTML == aName )
+    if ( sHTML == sName )
         bRet = HTMLParser::IsHTMLFormat( pHeader, sal_True, RTL_TEXTENCODING_DONTKNOW );
-    else if ( FILTER_RTF == aName )
+    else if ( FILTER_RTF == sName )
         bRet = 0 == strncmp( "{\\rtf", pHeader, 5 );
-    else if ( sWW5 == aName )
+    else if ( sWW5 == sName )
     {
         W1_FIB *pW1Header = (W1_FIB*)pHeader;
         if (pW1Header->wIdentGet() == 0xA5DC && pW1Header->nFibGet() == 0x65)
@@ -76,17 +74,17 @@ const sal_Char* SwIoDetect::IsReader(const sal_Char* pHeader, sal_uLong nLen_,
         else if (pW1Header->wIdentGet() == 0xA5DB && pW1Header->nFibGet() == 0x2D)
             bRet = true; /*WW2*/
     }
-    else if ( sWW1 == aName )
+    else if ( sWW1 == sName )
     {
         bRet = (( ((W1_FIB*)pHeader)->wIdentGet() == 0xA59C
                     && ((W1_FIB*)pHeader)->nFibGet() == 0x21)
                 && ((W1_FIB*)pHeader)->fComplexGet() == 0);
     }
-    else if ( FILTER_TEXT == aName )
+    else if ( FILTER_TEXT == sName )
         bRet = SwIoSystem::IsDetectableText(pHeader, nLen_);
-    else if ( FILTER_TEXT_DLG == aName)
+    else if ( FILTER_TEXT_DLG == sName)
         bRet = SwIoSystem::IsDetectableText( pHeader, nLen_, 0, 0, 0, true);
-    return bRet ? pName : 0;
+    return bRet ? sName : OUString();
 }
 
 const String SwIoSystem::GetSubStorageName( const SfxFilter& rFltr )
@@ -244,8 +242,8 @@ sal_Bool SwIoSystem::IsFileFilter(SfxMedium& rMedium, const String& rFmtName)
                     {
                         if (aFilterDetect[i].IsFilter(rFmtName))
                         {
-                            bRet = 0 != aFilterDetect[i].IsReader( aBuffer, nBytesRead,
-                                    rMedium.GetPhysicalName(), rUserData );
+                            OUString sFilter(aFilterDetect[i].IsReader(aBuffer, nBytesRead));
+                            bRet = !sFilter.isEmpty();
                             break;
                         }
                     }
@@ -382,14 +380,12 @@ const SfxFilter* SwIoSystem::GetFileFilter(const String& rFileName,
     /* nie erkannt und es wird auch der ASCII-Filter returnt.             */
     /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
     {
-        const SfxFilter* pFilterTmp = 0;
-        const sal_Char* pNm;
         for( sal_uInt16 n = 0; n < MAXFILTER; ++n )
         {
-            String sEmptyUserData;
-            pNm = aFilterDetect[n].IsReader(aBuffer, nBytesRead, rFileName, sEmptyUserData);
-            pFilterTmp = pNm ? SwIoSystem::GetFilterOfFormat(OUString::createFromAscii(pNm), pFCntnr) : 0;
-            if (pNm && pFilterTmp)
+            OUString sNm(aFilterDetect[n].IsReader(aBuffer, nBytesRead));
+            const SfxFilter* pFilterTmp =
+                sNm.isEmpty() ? 0 : SwIoSystem::GetFilterOfFormat(sNm, pFCntnr);
+            if (pFilterTmp)
             {
                 return pFilterTmp;
             }

zhirshya/lo-tblr

commit 95f24f1c722ca94959516fe9a099681642807213

rtl::OUString::copy needs to check for out-of-bounds
...regression introduced by 5089bdb234a93bcb62b597c18b8ab66048246b13 "Related: fdo#38838 remove UniString::EqualsAscii"

Change-Id: I1d2b78122592a57fe740e36d7811d563edf1e2c4

sw/source/core/unocore/unofield.cxx
@@ -17,6 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include "sal/config.h"
+
+#include <algorithm>
+
 #include <unofield.hxx>
 #include <unofieldcoll.hxx>
 #include <swtypes.hxx>
@@ -2640,7 +2644,7 @@ uno::Any SwXTextFieldMasters::getByName(const OUString& rName)
     if( USHRT_MAX == nResId )
         throw container::NoSuchElementException();
 
-    sName = sName.copy(sTypeName.getLength()+1);
+    sName = sName.copy(std::min(sTypeName.getLength()+1, sName.getLength()));
     SwFieldType* pType = GetDoc()->GetFldType(nResId, sName, sal_True);
     if(!pType)
         throw container::NoSuchElementException();
@@ -2727,7 +2731,7 @@ sal_Bool SwXTextFieldMasters::hasByName(const OUString& rName) throw( uno::Runti
     sal_Bool bRet = sal_False;
     if( USHRT_MAX != nResId )
     {
-        sName = sName.copy(sTypeName.getLength()+1);
+        sName = sName.copy(std::min(sTypeName.getLength()+1, sName.getLength()));
         bRet = USHRT_MAX != nResId && 0 != GetDoc()->GetFldType(nResId, sName, sal_True);
     }
     return bRet;