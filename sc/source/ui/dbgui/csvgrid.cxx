LibreOffice/core

commit 24c079605645cf29ba366ca39b7c1177da8b317f

convert sc/source/ui/dbgui/*.cxx from String to OUString
Change-Id: Iba489d8301f8885dee919e5a433281ce3761988f

sc/source/ui/dbgui/csvgrid.cxx
@@ -1063,9 +1063,9 @@ void ScCsvGrid::ImplDrawColumnHeader( OutputDevice& rOutDev, sal_uInt32 nColInde
 
 void ScCsvGrid::ImplDrawCellText( const Point& rPos, const OUString& rText )
 {
-    String aPlainText( rText );
-    aPlainText.SearchAndReplaceAll( '\t', ' ' );
-    aPlainText.SearchAndReplaceAll( '\n', ' ' );
+    OUString aPlainText( rText );
+    aPlainText = aPlainText.replaceAll( "\t", " " );
+    aPlainText = aPlainText.replaceAll( "\n", " " );
     mpEditEngine->SetPaperSize( maEdEngSize );
 
     /*  #i60296# If string contains mixed script types, the space character
@@ -1076,8 +1076,8 @@ void ScCsvGrid::ImplDrawCellText( const Point& rPos, const OUString& rText )
     for( xub_StrLen nToken = 0; nToken < nTokenCount; ++nToken )
     {
         sal_Int32 nBeginIx = nCharIxInt;
-        String aToken = aPlainText.GetToken( 0, ' ', nCharIxInt );
-        if( aToken.Len() > 0 )
+        OUString aToken = aPlainText.getToken( 0, ' ', nCharIxInt );
+        if( !aToken.isEmpty() )
         {
             sal_Int32 nX = rPos.X() + GetCharWidth() * nBeginIx;
             mpEditEngine->SetText( aToken );
@@ -1160,7 +1160,7 @@ void ScCsvGrid::ImplDrawColumnBackgr( sal_uInt32 nColIndex )
         StringVec& rStrVec = maTexts[ nLine ];
         if( (nColIndex < rStrVec.size()) && (rStrVec[ nColIndex ].getLength() > nStrPos) )
         {
-            String aText( rStrVec[ nColIndex ], nStrPos, nStrLen );
+            OUString aText = rStrVec[ nColIndex ].copy( nStrPos, nStrLen );
             ImplDrawCellText( Point( nStrX, GetY( GetFirstVisLine() + nLine ) ), aText );
         }
     }
@@ -1190,7 +1190,7 @@ void ScCsvGrid::ImplDrawRowHeaders()
     sal_Int32 nLastLine = GetLastVisLine();
     for( sal_Int32 nLine = GetFirstVisLine(); nLine <= nLastLine; ++nLine )
     {
-        String aText( OUString::number( nLine + 1 ) );
+        OUString aText( OUString::number( nLine + 1 ) );
         sal_Int32 nX = GetHdrX() + (GetHdrWidth() - maBackgrDev.GetTextWidth( aText )) / 2;
         maBackgrDev.DrawText( Point( nX, GetY( nLine ) ), aText );
     }

sc/source/ui/dbgui/asciiopt.cxx
@@ -179,20 +179,20 @@ bool ScAsciiOptions::operator==( const ScAsciiOptions& rCmp ) const
     return false;
 }
 
-static OUString lcl_decodeSepString( const String & rSepNums, bool & o_bMergeFieldSeps )
+static OUString lcl_decodeSepString( const OUString & rSepNums, bool & o_bMergeFieldSeps )
 {
-    String aFieldSeps;
+    OUString aFieldSeps;
     xub_StrLen nSub = comphelper::string::getTokenCount( rSepNums, '/');
     for (xub_StrLen i=0; i<nSub; ++i)
     {
-        String aCode = rSepNums.GetToken( i, '/' );
-        if ( aCode.EqualsAscii(pStrMrg) )
+        OUString aCode = rSepNums.getToken( i, '/' );
+        if ( aCode.equalsAscii(pStrMrg) )
             o_bMergeFieldSeps = true;
         else
         {
-            sal_Int32 nVal = aCode.ToInt32();
+            sal_Int32 nVal = aCode.toInt32();
             if ( nVal )
-                aFieldSeps += (sal_Unicode) nVal;
+                aFieldSeps += OUString((sal_Unicode) nVal);
         }
     }
     return aFieldSeps;
@@ -204,7 +204,7 @@ static OUString lcl_decodeSepString( const String & rSepNums, bool & o_bMergeFie
 void ScAsciiOptions::ReadFromString( const OUString& rString )
 {
     xub_StrLen nCount = comphelper::string::getTokenCount(rString, ',');
-    String aToken;
+    OUString aToken;
 
     // Field separator.
     if ( nCount >= 1 )
@@ -212,7 +212,7 @@ void ScAsciiOptions::ReadFromString( const OUString& rString )
         bFixedLen = bMergeFieldSeps = false;
 
         aToken = rString.getToken(0,',');
-        if ( aToken.EqualsAscii(pStrFix) )
+        if ( aToken.equalsAscii(pStrFix) )
             bFixedLen = true;
         aFieldSeps = lcl_decodeSepString( aToken, bMergeFieldSeps);
     }
@@ -221,7 +221,7 @@ void ScAsciiOptions::ReadFromString( const OUString& rString )
     if ( nCount >= 2 )
     {
         aToken = rString.getToken(1,',');
-        sal_Int32 nVal = aToken.ToInt32();
+        sal_Int32 nVal = aToken.toInt32();
         cTextSep = (sal_Unicode) nVal;
     }
 
@@ -236,7 +236,7 @@ void ScAsciiOptions::ReadFromString( const OUString& rString )
     if ( nCount >= 4 )
     {
         aToken = rString.getToken(3,',');
-        nStartRow = aToken.ToInt32();
+        nStartRow = aToken.toInt32();
     }
 
     // Column info.
@@ -254,8 +254,8 @@ void ScAsciiOptions::ReadFromString( const OUString& rString )
             pColFormat = new sal_uInt8[nInfoCount];
             for (sal_uInt16 nInfo=0; nInfo<nInfoCount; nInfo++)
             {
-                pColStart[nInfo]  = (sal_Int32) aToken.GetToken( 2*nInfo, '/' ).ToInt32();
-                pColFormat[nInfo] = (sal_uInt8) aToken.GetToken( 2*nInfo+1, '/' ).ToInt32();
+                pColStart[nInfo]  = (sal_Int32) aToken.getToken( 2*nInfo, '/' ).toInt32();
+                pColFormat[nInfo] = (sal_uInt8) aToken.getToken( 2*nInfo+1, '/' ).toInt32();
             }
         }
         else
@@ -269,21 +269,21 @@ void ScAsciiOptions::ReadFromString( const OUString& rString )
     if (nCount >= 6)
     {
         aToken = rString.getToken(5, ',');
-        eLang = static_cast<LanguageType>(aToken.ToInt32());
+        eLang = static_cast<LanguageType>(aToken.toInt32());
     }
 
     // Import quoted field as text.
     if (nCount >= 7)
     {
         aToken = rString.getToken(6, ',');
-        bQuotedFieldAsText = aToken.EqualsAscii("true") ? true : false;
+        bQuotedFieldAsText = aToken.equalsAscii("true") ? true : false;
     }
 
     // Detect special numbers.
     if (nCount >= 8)
     {
         aToken = rString.getToken(7, ',');
-        bDetectSpecialNumber = aToken.EqualsAscii("true") ? true : false;
+        bDetectSpecialNumber = aToken.equalsAscii("true") ? true : false;
     }
     else
         bDetectSpecialNumber = true;    // default of versions that didn't add the parameter
@@ -362,22 +362,22 @@ OUString ScAsciiOptions::WriteToString() const
 sal_Unicode ScAsciiOptions::GetWeightedFieldSep( const OUString & rFieldSeps, bool bDecodeNumbers )
 {
     bool bMergeFieldSeps = false;
-    String aFieldSeps( bDecodeNumbers ? lcl_decodeSepString( rFieldSeps, bMergeFieldSeps) : rFieldSeps);
-    if (aFieldSeps.Len() <= 1)
-        return aFieldSeps.GetChar(0);
+    OUString aFieldSeps( bDecodeNumbers ? lcl_decodeSepString( rFieldSeps, bMergeFieldSeps) : rFieldSeps);
+    if (aFieldSeps.getLength() <= 1)
+        return aFieldSeps[0];
     else
     {
         // There can be only one separator for output. See also fdo#53449
-        if (aFieldSeps.Search(',') != STRING_NOTFOUND)
+        if (aFieldSeps.indexOf(',') != -1)
             return ',';
-        else if (aFieldSeps.Search('\t') != STRING_NOTFOUND)
+        else if (aFieldSeps.indexOf('\t') != -1)
             return '\t';
-        else if (aFieldSeps.Search(';') != STRING_NOTFOUND)
+        else if (aFieldSeps.indexOf(';') != -1)
             return ';';
-        else if (aFieldSeps.Search(' ') != STRING_NOTFOUND)
+        else if (aFieldSeps.indexOf(' ') != -1)
             return ' ';
         else
-            return aFieldSeps.GetChar(0);
+            return aFieldSeps[0];
     }
 }
 

sc/source/ui/dbgui/consdlg.cxx
@@ -47,16 +47,16 @@ public:
     ScAreaData()  {}
     ~ScAreaData() {}
 
-    void Set( const String& rName, const String& rArea, sal_Bool bDb )
+    void Set( const OUString& rName, const OUString& rArea, sal_Bool bDb )
                 {
                     aStrName  = rName;
                     aStrArea  = rArea;
                     bIsDbArea = bDb;
                 }
 
-    String  aStrName;
-    String  aStrArea;
-    sal_Bool    bIsDbArea;
+    OUString  aStrName;
+    OUString  aStrArea;
+    sal_Bool  bIsDbArea;
 };
 
 
@@ -126,7 +126,7 @@ void ScConsolidateDlg::Init()
 {
     OSL_ENSURE( pViewData && pDoc && pRangeUtil, "Error in Ctor" );
 
-    String aStr;
+    OUString aStr;
     sal_uInt16 i=0;
 
     pRbDataArea->SetReferences(this, pEdDataArea);
@@ -214,7 +214,7 @@ void ScConsolidateDlg::Init()
         ScAreaNameIterator aIter( pDoc );
         while ( aIter.Next( aStrName, aRange ) )
         {
-            String aStrArea(aRange.Format(SCA_ABS_3D, pDoc, eConv));
+            OUString aStrArea(aRange.Format(SCA_ABS_3D, pDoc, eConv));
             pAreaData[nAt++].Set( aStrName, aStrArea, aIter.WasDBName() );
         }
     }
@@ -243,7 +243,7 @@ void ScConsolidateDlg::FillAreaLists()
     if ( pRangeUtil && pAreaData && (nAreaDataCount > 0) )
     {
         for ( size_t i=0;
-              (i<nAreaDataCount) && (pAreaData[i].aStrName.Len()>0);
+              (i<nAreaDataCount) && (!pAreaData[i].aStrName.isEmpty());
               i++ )
         {
             pLbDataArea->InsertEntry( pAreaData[i].aStrName, i+1 );
@@ -267,7 +267,7 @@ void ScConsolidateDlg::SetReference( const ScRange& rRef, ScDocument* pDocP )
         if ( rRef.aStart != rRef.aEnd )
             RefInputStart( pRefInputEdit );
 
-        String      aStr;
+        OUString      aStr;
         sal_uInt16      nFmt = SCR_ABS_3D;       //!!! nCurTab fehlt noch
         const formula::FormulaGrammar::AddressConvention eConv = pDocP->GetAddressConvention();
 
@@ -389,7 +389,7 @@ IMPL_LINK_NOARG(ScConsolidateDlg, OkHdl)
     {
         ScRefAddress aDestAddress;
         SCTAB       nTab = pViewData->GetTabNo();
-        String      aDestPosStr( pEdDestArea->GetText() );
+        OUString    aDestPosStr( pEdDestArea->GetText() );
         const formula::FormulaGrammar::AddressConvention eConv = pDoc->GetAddressConvention();
 
         if ( pRangeUtil->IsAbsPos( aDestPosStr, pDoc, nTab, NULL, &aDestAddress, eConv ) )
@@ -451,7 +451,7 @@ IMPL_LINK( ScConsolidateDlg, ClickHdl, PushButton*, pBtn )
     {
         if ( !pEdDataArea->GetText().isEmpty() )
         {
-            String      aNewEntry( pEdDataArea->GetText() );
+            OUString    aNewEntry( pEdDataArea->GetText() );
             ScArea**    ppAreas = NULL;
             sal_uInt16      nAreaCount = 0;
             const formula::FormulaGrammar::AddressConvention eConv = pDoc->GetAddressConvention();
@@ -464,7 +464,7 @@ IMPL_LINK( ScConsolidateDlg, ClickHdl, PushButton*, pBtn )
 
                 for ( sal_uInt16 i=0; i<nAreaCount; i++ )
                 {
-                    String aNewArea;
+                    OUString aNewArea;
 
                     if ( ppAreas[i] )
                     {
@@ -485,7 +485,7 @@ IMPL_LINK( ScConsolidateDlg, ClickHdl, PushButton*, pBtn )
             }
             else if ( VerifyEdit( pEdDataArea ) )
             {
-                String aNewArea( pEdDataArea->GetText() );
+                OUString aNewArea( pEdDataArea->GetText() );
 
                 if ( pLbConsAreas->GetEntryPos( aNewArea ) == LISTBOX_ENTRY_NOTFOUND )
                     pLbConsAreas->InsertEntry( aNewArea );
@@ -560,8 +560,8 @@ IMPL_LINK( ScConsolidateDlg, ModifyHdl, formula::RefEdit*, pEd )
 {
     if ( pEd == pEdDataArea )
     {
-        String aAreaStr( pEd->GetText() );
-        if ( aAreaStr.Len() > 0 )
+        OUString aAreaStr( pEd->GetText() );
+        if ( !aAreaStr.isEmpty() )
         {
             pBtnAdd->Enable();
         }

sc/source/ui/dbgui/csvruler.cxx
@@ -41,7 +41,7 @@ using namespace com::sun::star::uno;
 
 static void load_FixedWidthList(ScCsvSplits &aSplits)
 {
-    String sSplits;
+    OUString sSplits;
     OUString sFixedWidthLists;
 
     Sequence<Any>aValues;
@@ -59,12 +59,12 @@ static void load_FixedWidthList(ScCsvSplits &aSplits)
         aSplits.Clear();
         pProperties[0] >>= sFixedWidthLists;
 
-        sSplits = String( sFixedWidthLists );
+        sSplits = sFixedWidthLists;
 
         // String ends with a semi-colon so there is no 'int' after the last one.
         xub_StrLen n = comphelper::string::getTokenCount(sSplits, ';') - 1;
         for (xub_StrLen i = 0; i < n; ++i)
-            aSplits.Insert( sSplits.GetToken(i).ToInt32() );
+            aSplits.Insert( sSplits.getToken(i, ';').toInt32() );
     }
 }
 static void save_FixedWidthList(ScCsvSplits aSplits)
@@ -590,7 +590,7 @@ void ScCsvRuler::ImplDrawBackgrDev()
     maBackgrDev.SetTextFillColor();
     for( nPos = ((nFirstPos + 9) / 10) * 10; nPos <= nLastPos; nPos += 10 )
     {
-        String aText( OUString::number( nPos ) );
+        OUString aText( OUString::number( nPos ) );
         sal_Int32 nTextWidth = maBackgrDev.GetTextWidth( aText );
         sal_Int32 nTextX = GetX( nPos ) - nTextWidth / 2;
         ImplDrawArea( nTextX - 1, nTextWidth + 2 );

sc/source/ui/dbgui/dapidata.cxx
@@ -83,7 +83,7 @@ ScDataPilotDatabaseDlg::ScDataPilotDatabaseDlg( Window* pParent ) :
         const OUString* pArray = aNames.getConstArray();
         for (long nPos = 0; nPos < nCount; nPos++)
         {
-            String aName = pArray[nPos];
+            OUString aName = pArray[nPos];
             aLbDatabase.InsertEntry( aName );
         }
     }
@@ -134,8 +134,8 @@ void ScDataPilotDatabaseDlg::FillObjects()
 {
     aCbObject.Clear();
 
-    String aDatabaseName = aLbDatabase.GetSelectEntry();
-    if (!aDatabaseName.Len())
+    OUString aDatabaseName = aLbDatabase.GetSelectEntry();
+    if (aDatabaseName.isEmpty())
         return;
 
     sal_uInt16 nSelect = aLbType.GetSelectEntryPos();
@@ -192,7 +192,7 @@ void ScDataPilotDatabaseDlg::FillObjects()
         const OUString* pArray = aNames.getConstArray();
         for( long nPos=0; nPos<nCount; nPos++ )
         {
-            String aName = pArray[nPos];
+            OUString aName = pArray[nPos];
             aCbObject.InsertEntry( aName );
         }
     }

sc/source/ui/dbgui/dapitype.cxx
@@ -126,7 +126,7 @@ ScDataPilotServiceDlg::ScDataPilotServiceDlg( Window* pParent,
     const OUString* pArray = rServices.getConstArray();
     for (long i=0; i<nCount; i++)
     {
-        String aName = pArray[i];
+        OUString aName = pArray[i];
         aLbService.InsertEntry( aName );
     }
     aLbService.SelectEntryPos( 0 );

sc/source/ui/dbgui/dbnamdlg.cxx
@@ -67,7 +67,7 @@ private:
     CheckBox&   rBtnFormat;
     CheckBox&   rBtnStrip;
     ScRange&    rCurArea;
-    String      aStr;
+    OUString    aStr;
     ScRange     aArea;
     sal_Bool        bHeader:1;
     sal_Bool        bSize:1;
@@ -182,7 +182,7 @@ void ScDbNameDlg::Init()
     m_pEdAssign->SetModifyHdl  ( LINK( this, ScDbNameDlg, AssModifyHdl ) );
     UpdateNames();
 
-    String  theAreaStr;
+    OUString  theAreaStr;
 
     if ( pViewData && pDoc )
     {
@@ -413,16 +413,16 @@ IMPL_LINK_NOARG_INLINE_END(ScDbNameDlg, CancelBtnHdl)
 
 IMPL_LINK_NOARG(ScDbNameDlg, AddBtnHdl)
 {
-    String  aNewName = comphelper::string::strip(m_pEdName->GetText(), ' ');
-    String  aNewArea = m_pEdAssign->GetText();
+    OUString  aNewName = comphelper::string::strip(m_pEdName->GetText(), ' ');
+    OUString  aNewArea = m_pEdAssign->GetText();
 
-    if ( aNewName.Len() > 0 && aNewArea.Len() > 0 )
+    if ( !aNewName.isEmpty() > 0 && !aNewArea.isEmpty() )
     {
-        if ( ScRangeData::IsNameValid( aNewName, pDoc ) && !aNewName.EqualsAscii(STR_DB_LOCAL_NONAME) )
+        if ( ScRangeData::IsNameValid( aNewName, pDoc ) && !aNewName.equalsAscii(STR_DB_LOCAL_NONAME) )
         {
             //  weil jetzt editiert werden kann, muss erst geparst werden
             ScRange aTmpRange;
-            String aText = m_pEdAssign->GetText();
+            OUString aText = m_pEdAssign->GetText();
             if ( aTmpRange.ParseAny( aText, pDoc, aAddrDetails ) & SCA_VALID )
             {
                 theCurArea = aTmpRange;
@@ -516,12 +516,12 @@ IMPL_LINK_NOARG(ScDbNameDlg, RemoveBtnHdl)
 
     if (itr != rDBs.end())
     {
-        String aStrDelMsg = ScGlobal::GetRscString( STR_QUERY_DELENTRY );
+        OUString aStrDelMsg = ScGlobal::GetRscString( STR_QUERY_DELENTRY );
 
         OUStringBuffer aBuf;
-        aBuf.append(aStrDelMsg.GetToken(0, '#'));
+        aBuf.append(aStrDelMsg.getToken(0, '#'));
         aBuf.append(aStrEntry);
-        aBuf.append(aStrDelMsg.GetToken(1, '#'));
+        aBuf.append(aStrDelMsg.getToken(1, '#'));
         QueryBox aBox(this, WinBits(WB_YES_NO|WB_DEF_YES), aBuf.makeStringAndClear());
 
         if (RET_YES == aBox.Execute())
@@ -631,7 +631,7 @@ IMPL_LINK_NOARG(ScDbNameDlg, AssModifyHdl)
     //  hier parsen fuer Save() etc.
 
     ScRange aTmpRange;
-    String aText = m_pEdAssign->GetText();
+    OUString aText = m_pEdAssign->GetText();
     if ( aTmpRange.ParseAny( aText, pDoc, aAddrDetails ) & SCA_VALID )
         theCurArea = aTmpRange;
 

sc/source/ui/dbgui/expftext.cxx
@@ -59,7 +59,7 @@ ScExpandedFixedText::ScExpandedFixedText( Window* pParent,
 
 void ScExpandedFixedText::RequestHelp(const HelpEvent& rEvt)
 {
-    String  aTxtStr = GetText();
+    OUString  aTxtStr = GetText();
     long    nTxtWidth = GetTextWidth(aTxtStr);
     if ( ( rEvt.GetMode() & HELPMODE_QUICK ) == HELPMODE_QUICK &&
          nTxtWidth > GetSizePixel().Width())

zhirshya/lo-tblr

commit 2b8fc32e0a7b19d9ca4a08c2f26eeb92594ec113

fixed !!br0ken CSV import preview
Regression introduced with 24c079605645cf29ba366ca39b7c1177da8b317f
Previous String ctor accepted length > string length, whereas
OUString::copy() does not.

Change-Id: If51e6df4e236a59ab0302d2bb683ca8e6d148bd0

sc/source/ui/dbgui/csvgrid.cxx
@@ -1152,15 +1152,16 @@ void ScCsvGrid::ImplDrawColumnBackgr( sal_uInt32 nColIndex )
     // #i67432# cut string to avoid edit engine performance problems with very large strings
     sal_Int32 nFirstVisPos = ::std::max( GetColumnPos( nColIndex ), GetFirstVisPos() );
     sal_Int32 nLastVisPos = ::std::min( GetColumnPos( nColIndex + 1 ), GetLastVisPos() );
-    xub_StrLen nStrPos = static_cast< xub_StrLen >( nFirstVisPos - GetColumnPos( nColIndex ) );
-    xub_StrLen nStrLen = static_cast< xub_StrLen >( nLastVisPos - nFirstVisPos + 1 );
+    sal_Int32 nStrPos = nFirstVisPos - GetColumnPos( nColIndex );
+    sal_Int32 nStrLen = nLastVisPos - nFirstVisPos + 1;
     sal_Int32 nStrX = GetX( nFirstVisPos );
     for( size_t nLine = 0; nLine < nLineCount; ++nLine )
     {
         StringVec& rStrVec = maTexts[ nLine ];
         if( (nColIndex < rStrVec.size()) && (rStrVec[ nColIndex ].getLength() > nStrPos) )
         {
-            OUString aText = rStrVec[ nColIndex ].copy( nStrPos, nStrLen );
+            const OUString& rStr = rStrVec[ nColIndex ];
+            OUString aText = rStr.copy( nStrPos, ::std::min( nStrLen, rStr.getLength()) - nStrPos );
             ImplDrawCellText( Point( nStrX, GetY( GetFirstVisLine() + nLine ) ), aText );
         }
     }