LibreOffice/core

commit e3d5c3e0746916c4056389dd8c2daa6c451c8f6e

Fix fdo#36868 - doc import list numbering error
This patch appears to be fix fdo#36868. The problem is that the paragraph
style was never being assigned an outline list level. The saved file did
not have an sprmPOutLvl for that paragraph (although it did have one for
the level 0 style), just an sprmPIlvl and an sprmPIlfo. Therefore there
was no call to SwWW8ImplReader::Read_POutLvl to set nOutlineLevel.

sw/source/filter/ww8/ww8par3.cxx
@@ -1705,6 +1705,8 @@ void SwWW8ImplReader::SetStylesList(sal_uInt16 nStyle, sal_uInt16 nActLFO,
             {
                 rStyleInf.nLFOIndex  = nActLFO;
                 rStyleInf.nListLevel = nActLevel;
+                if (nActLevel > 0) // it must be an outline list
+                    rStyleInf.nOutlineLevel = nActLevel;
 
                 if (
                     (USHRT_MAX > nActLFO) &&

zhirshya/lo-tblr

commit 1ae50882e3760a4c10e3a816b7b8d8f07191bf8d

fdo#36868 WW8 import: allow outline numbering and list style in the same ...
.. paragraph style

The original problem (from a user's point of view) was that the second
level of the numbering started from 1.1 instead of 2.1 in the bugdoc.
This was fixed by using outline numbering for level 2 as well, but this
is problematic in many cases: we want to have outline numbering exactly
when outline numbering is enabled for the given paragraph style.

So revert the change in SwWW8ImplReader::SetStylesList() and fix it
differently: SwWW8ImplReader::RegisterNumFmtOnStyle() explicitly ignores
list style if outline numbering is available with no good reason. Both
the WW8 format and Writer core allows to have outline numbering and a
list style at the same time, so set list style even when outline
numbering is available. This fixes the original issue, too -- without
introducing nasty fake outline numbering usage.

Also add a testcase for the original issue.

(regression from e3d5c3e0746916c4056389dd8c2daa6c451c8f6e)

Change-Id: Id7d2d67a96a858aee3230110cb518fea51d19d38

sw/source/filter/ww8/ww8par3.cxx
@@ -1741,8 +1741,6 @@ void SwWW8ImplReader::SetStylesList(sal_uInt16 nStyle, sal_uInt16 nActLFO,
             {
                 rStyleInf.nLFOIndex  = nActLFO;
                 rStyleInf.nListLevel = nActLevel;
-                if (nActLevel > 0) // it must be an outline list
-                    rStyleInf.nOutlineLevel = nActLevel;
 
                 if (
                     (USHRT_MAX > nActLFO) &&
@@ -1791,12 +1789,9 @@ void SwWW8ImplReader::RegisterNumFmtOnStyle(sal_uInt16 nStyle)
             {
                 if( MAXLEVEL > rStyleInf.nOutlineLevel )
                     rStyleInf.pOutlineNumrule = pNmRule;
-                else
-                {
-                    rStyleInf.pFmt->SetFmtAttr(
+                rStyleInf.pFmt->SetFmtAttr(
                         SwNumRuleItem( pNmRule->GetName() ) );
-                    rStyleInf.bHasStyNumRule = true;
-                }
+                rStyleInf.bHasStyNumRule = true;
             }
         }
 

sw/qa/extras/ww8import/ww8import.cxx
@@ -35,6 +35,7 @@ public:
     void testN816593();
     void testPageBorder();
     void testN823651();
+    void testFdo36868();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -62,6 +63,7 @@ void Test::run()
         {"n816593.doc", &Test::testN816593},
         {"page-border.doc", &Test::testPageBorder},
         {"n823651.doc", &Test::testN823651},
+        {"fdo36868.doc", &Test::testFdo36868},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -283,6 +285,13 @@ void Test::testN823651()
     uno::Reference<beans::XPropertySet> xStyle(getStyles("PageStyles")->getByName(DEFAULT_STYLE), uno::UNO_QUERY);
     uno::Reference<text::XText> xText = getProperty< uno::Reference<text::XTextRange> >(xStyle, "HeaderTextFirst")->getText();
     CPPUNIT_ASSERT_EQUAL(7.5f, getProperty<float>(getParagraphOfText(1, xText), "CharHeight"));
+}
+
+void Test::testFdo36868()
+{
+    OUString aText = parseDump("/root/page/body/txt[3]/Special[@nType='POR_NUMBER']", "rText");
+    // This was 1.1.
+    CPPUNIT_ASSERT_EQUAL(OUString("2.1"), aText);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);