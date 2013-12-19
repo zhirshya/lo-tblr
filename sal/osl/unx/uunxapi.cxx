LibreOffice/core

commit 0e6a2601b39cbadaff7f7506ba9e804f108060db

Convert code that calls OUString::getStr()[] to use the [] operator
This also means that this code now gets bounds checked in debug builds.

Change-Id: Id777f85eaee6a737bbcb84625e6e110abe0e0f27

sal/osl/unx/uunxapi.cxx
@@ -128,9 +128,9 @@ int access_u(const rtl_uString* pustrPath, int mode)
 {
     rtl::OString fn = OUStringToOString(pustrPath);
 #ifdef ANDROID
-    if (strncmp(fn.getStr(), "/assets", sizeof("/assets")-1) == 0 &&
-        (fn.getStr()[sizeof("/assets")-1] == '\0' ||
-         fn.getStr()[sizeof("/assets")-1] == '/'))
+    if (fn.startsWith("/assets") &&
+        (fn[sizeof("/assets")-1] == '\0' ||
+         fn[sizeof("/assets")-1] == '/'))
     {
         struct stat stat;
         if (lo_apk_lstat(fn.getStr(), &stat) == -1)
@@ -161,9 +161,9 @@ sal_Bool realpath_u(const rtl_uString* pustrFileName, rtl_uString** ppustrResolv
 {
     rtl::OString fn = OUStringToOString(pustrFileName);
 #ifdef ANDROID
-    if (strncmp(fn.getStr(), "/assets", sizeof("/assets")-1) == 0 &&
-        (fn.getStr()[sizeof("/assets")-1] == '\0' ||
-         fn.getStr()[sizeof("/assets")-1] == '/'))
+    if (fn.startsWith("/assets") &&
+        (fn[sizeof("/assets")-1] == '\0' ||
+         fn[sizeof("/assets")-1] == '/'))
     {
         if (access_u(pustrFileName, F_OK) == -1)
             return sal_False;

zhirshya/lo-tblr

commit 9d5fb7a88de967a75ff2d5459db8cb492dc04ea9

Fix crash on Android
Fallout from 0e6a2601b39cbadaff7f7506ba9e804f108060db ("Convert code that
calls OUString::getStr()[] to use the [] operator"). The code was a bit silly
and "hand-optimized" in the first place so no wonder a bug was introduced
accidentally in that commit.

Make it simpler and more obvious.

Change-Id: Idafb1c0741289e915dfbaa108383a926321764d2

sal/osl/unx/uunxapi.cxx
@@ -128,9 +128,7 @@ int access_u(const rtl_uString* pustrPath, int mode)
 {
     rtl::OString fn = OUStringToOString(pustrPath);
 #ifdef ANDROID
-    if (fn.startsWith("/assets") &&
-        (fn[sizeof("/assets")-1] == '\0' ||
-         fn[sizeof("/assets")-1] == '/'))
+    if (fn == "/assets" || fn.startsWith("/assets/"))
     {
         struct stat stat;
         if (lo_apk_lstat(fn.getStr(), &stat) == -1)
@@ -161,9 +159,7 @@ sal_Bool realpath_u(const rtl_uString* pustrFileName, rtl_uString** ppustrResolv
 {
     rtl::OString fn = OUStringToOString(pustrFileName);
 #ifdef ANDROID
-    if (fn.startsWith("/assets") &&
-        (fn[sizeof("/assets")-1] == '\0' ||
-         fn[sizeof("/assets")-1] == '/'))
+    if (fn == "/assets" || fn.startsWith("/assets/"))
     {
         if (access_u(pustrFileName, F_OK) == -1)
             return sal_False;