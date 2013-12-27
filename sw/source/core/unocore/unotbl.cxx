LibreOffice/core

commit b844f06b36cf9a6c1411861a08701c8f9be2af0d

fdo#48692: fix problems with large number of table cells:
SwSelBoxes containers are filled from SwTableSortBoxes in various
places, so adapt that to use size_t integers as well.

Change-Id: Ia55f020bb9ada94bb22802d9adabb055a22edd2f

sw/source/core/unocore/unotbl.cxx
@@ -1517,10 +1517,12 @@ SwXTextTableCursor::SwXTextTableCursor(SwFrmFmt& rTableFmt, const SwTableCursor*
         pUnoCrsr->SetMark();
         *pUnoCrsr->GetMark() = *pTableSelection->GetMark();
     }
-    const SwSelBoxes& rBoxes = pTableSelection->GetBoxes();
+    const SwSelBoxes& rBoxes = pTableSelection->GetSelectedBoxes();
     SwTableCursor* pTableCrsr = dynamic_cast<SwTableCursor*>(pUnoCrsr);
-    for(sal_uInt16 i = 0; i < rBoxes.size(); i++)
+    for (size_t i = 0; i < rBoxes.size(); i++)
+    {
         pTableCrsr->InsertBox( *rBoxes[i] );
+    }
 
     pUnoCrsr->Add(&aCrsrDepend);
     SwUnoTableCrsr* pTblCrsr = dynamic_cast<SwUnoTableCrsr*>(pUnoCrsr);
@@ -1689,9 +1691,11 @@ sal_Bool SwXTextTableCursor::mergeRange(void) throw( uno::RuntimeException )
             bRet = TBLMERGE_OK == pTblCrsr->GetDoc()->MergeTbl(*pTblCrsr);
             if(bRet)
             {
-                sal_uInt16 nCount = pTblCrsr->GetBoxesCount();
-                while(nCount--)
+                size_t nCount = pTblCrsr->GetSelectedBoxesCount();
+                while (--nCount)
+                {
                     pTblCrsr->DeleteBox(nCount);
+                }
             }
         }
         pTblCrsr->MakeBoxSels();
@@ -1716,7 +1720,8 @@ sal_Bool SwXTextTableCursor::splitRange(sal_Int16 Count, sal_Bool Horizontal) th
         pTblCrsr->MakeBoxSels();
         {
             UnoActionContext aContext(pUnoCrsr->GetDoc());
-            bRet = pTblCrsr->GetDoc()->SplitTbl( pTblCrsr->GetBoxes(), !Horizontal, Count );
+            bRet = pTblCrsr->GetDoc()->SplitTbl(
+                    pTblCrsr->GetSelectedBoxes(), !Horizontal, Count);
         }
         pTblCrsr->MakeBoxSels();
     }
@@ -4595,7 +4600,7 @@ void SAL_CALL SwXCellRange::sort(const uno::Sequence< beans::PropertyValue >& rD
         SwUnoTableCrsr* pTableCrsr = dynamic_cast<SwUnoTableCrsr*>(pTblCrsr);
         pTableCrsr->MakeBoxSels();
         UnoActionContext aContext( pFmt->GetDoc() );
-        pFmt->GetDoc()->SortTbl(pTableCrsr->GetBoxes(), aSortOpt);
+        pFmt->GetDoc()->SortTbl(pTableCrsr->GetSelectedBoxes(), aSortOpt);
     }
 }
 

sw/inc/cellfml.hxx
@@ -152,7 +152,7 @@ public:
             eNmType = EXTRNL_NAME;
         }
 
-    sal_uInt16 GetBoxesOfFormula( const SwTable& rTbl, SwSelBoxes& rBoxes );
+    void GetBoxesOfFormula(const SwTable& rTbl, SwSelBoxes& rBoxes);
     // are all boxes valid which this formula relies on?
     bool HasValidBoxes() const;
 };

sw/inc/crsrsh.hxx
@@ -655,7 +655,7 @@ public:
 
     const SwShellTableCrsr* GetTableCrsr() const { return pTblCrsr; }
     SwShellTableCrsr* GetTableCrsr() { return pTblCrsr; }
-    sal_uInt16 UpdateTblSelBoxes();
+    size_t UpdateTblSelBoxes();
 
     sal_Bool GotoFtnTxt();      ///< jump from content to footnote
     sal_Bool GotoFtnAnchor();   ///< jump from footnote to anchor

sw/inc/swcrsr.hxx
@@ -270,7 +270,7 @@ class SwTableCursor : public virtual SwCursor
 protected:
     sal_uLong nTblPtNd, nTblMkNd;
     xub_StrLen nTblPtCnt, nTblMkCnt;
-    SwSelBoxes aSelBoxes;
+    SwSelBoxes m_SelectedBoxes;
     sal_Bool bChg : 1;
     sal_Bool bParked : 1;       // Table-cursor was parked.
 
@@ -286,9 +286,9 @@ public:
     virtual sal_Bool GotoTable( const String& rName );
 
     void InsertBox( const SwTableBox& rTblBox );
-    void DeleteBox( sal_uInt16 nPos ) { aSelBoxes.erase( aSelBoxes.begin() + nPos ); bChg = sal_True; }
-    sal_uInt16 GetBoxesCount() const { return aSelBoxes.size(); }
-    const SwSelBoxes& GetBoxes() const { return aSelBoxes; }
+    void DeleteBox(size_t nPos);
+    size_t GetSelectedBoxesCount() const { return m_SelectedBoxes.size(); }
+    const SwSelBoxes& GetSelectedBoxes() const { return m_SelectedBoxes; }
 
     // Creates cursor for all boxes.
     SwCursor* MakeBoxSels( SwCursor* pAktCrsr );

sw/source/core/access/acccell.cxx
@@ -72,7 +72,8 @@ sal_Bool SwAccessibleCell::IsSelected()
                 static_cast< const SwCellFrm * >( GetFrm() );
             SwTableBox *pBox =
                 const_cast< SwTableBox *>( pCFrm->GetTabBox() );
-            bRet = pCSh->GetTableCrsr()->GetBoxes().find( pBox ) != pCSh->GetTableCrsr()->GetBoxes().end();
+            SwSelBoxes const& rBoxes(pCSh->GetTableCrsr()->GetSelectedBoxes());
+            bRet = rBoxes.find(pBox) != rBoxes.end();
         }
     }
 

sw/source/core/access/acctable.cxx
@@ -603,7 +603,7 @@ const SwSelBoxes *SwAccessibleTable::GetSelBoxes() const
     const SwCrsrShell *pCSh = GetCrsrShell();
     if( (pCSh != NULL) && pCSh->IsTableMode() )
     {
-        pSelBoxes = &pCSh->GetTableCrsr()->GetBoxes();
+        pSelBoxes = &pCSh->GetTableCrsr()->GetSelectedBoxes();
     }
 
     return pSelBoxes;

sw/source/core/crsr/crsrsh.cxx
@@ -2658,12 +2658,13 @@ sal_Bool SwCrsrShell::ShouldWait() const
             pPam->End()->nNode.GetIndex();
 }
 
-
-sal_uInt16 SwCrsrShell::UpdateTblSelBoxes()
+size_t SwCrsrShell::UpdateTblSelBoxes()
 {
-    if( pTblCrsr && ( pTblCrsr->IsChgd() || !pTblCrsr->GetBoxesCount() ))
+    if (pTblCrsr && (pTblCrsr->IsChgd() || !pTblCrsr->GetSelectedBoxesCount()))
+    {
          GetLayout()->MakeTblCrsrs( *pTblCrsr );
-    return pTblCrsr ? pTblCrsr->GetBoxesCount() : 0;
+    }
+    return (pTblCrsr) ? pTblCrsr->GetSelectedBoxesCount() : 0;
 }
 
 /// show the current selected "object"

sw/source/core/crsr/swcrsr.cxx
@@ -2099,21 +2099,23 @@ SwTableCursor::SwTableCursor( const SwPosition &rPos, SwPaM* pRing )
 SwTableCursor::~SwTableCursor() {}
 
 
-sal_Bool lcl_SeekEntry( const SwSelBoxes& rTmp, const SwStartNode* pSrch, sal_uInt16& rFndPos )
+static bool
+lcl_SeekEntry(const SwSelBoxes& rTmp, SwStartNode const*const pSrch,
+        size_t & o_rFndPos)
 {
     sal_uLong nIdx = pSrch->GetIndex();
 
-    sal_uInt16 nO = rTmp.size();
+    size_t nO = rTmp.size();
     if( nO > 0 )
     {
         nO--;
-        sal_uInt16 nU = 0;
+        size_t nU = 0;
         while( nU <= nO )
         {
-            sal_uInt16 nM = nU + ( nO - nU ) / 2;
+            size_t nM = nU + ( nO - nU ) / 2;
             if( rTmp[ nM ]->GetSttNd() == pSrch )
             {
-                rFndPos = nM;
+                o_rFndPos = nM;
                 return sal_True;
             }
             else if( rTmp[ nM ]->GetSttIdx() < nIdx )
@@ -2146,14 +2148,14 @@ SwCursor* SwTableCursor::MakeBoxSels( SwCursor* pAktCrsr )
 
         // create temporary copies so that all boxes that
         // have already cursors can be removed
-        SwSelBoxes aTmp( aSelBoxes );
+        SwSelBoxes aTmp(m_SelectedBoxes);
 
         // compare old and new ones
         SwNodes& rNds = pAktCrsr->GetDoc()->GetNodes();
-        sal_uInt16 nPos;
         const SwStartNode* pSttNd;
         SwPaM* pCur = pAktCrsr;
         do {
+            size_t nPos;
             sal_Bool bDel = sal_False;
             pSttNd = pCur->GetPoint()->nNode.GetNode().FindTableBoxStartNode();
             if( !pCur->HasMark() || !pSttNd ||
@@ -2198,7 +2200,7 @@ SwCursor* SwTableCursor::MakeBoxSels( SwCursor* pAktCrsr )
             }
         } while ( pAktCrsr != pCur );
 
-        for( nPos = 0; nPos < aTmp.size(); ++nPos )
+        for (size_t nPos = 0; nPos < aTmp.size(); ++nPos)
         {
             pSttNd = aTmp[ nPos ]->GetSttNd();
 
@@ -2239,7 +2241,14 @@ SwCursor* SwTableCursor::MakeBoxSels( SwCursor* pAktCrsr )
 void SwTableCursor::InsertBox( const SwTableBox& rTblBox )
 {
     SwTableBox* pBox = (SwTableBox*)&rTblBox;
-    aSelBoxes.insert( pBox );
+    m_SelectedBoxes.insert(pBox);
+    bChg = sal_True;
+}
+
+
+void SwTableCursor::DeleteBox(size_t const nPos)
+{
+    m_SelectedBoxes.erase(m_SelectedBoxes.begin() + nPos);
     bChg = sal_True;
 }
 
@@ -2255,7 +2264,7 @@ bool SwTableCursor::NewTableSelection()
             pTableNode->GetTable().IsNewModel() )
         {
             bRet = true;
-            SwSelBoxes aNew( aSelBoxes );
+            SwSelBoxes aNew(m_SelectedBoxes);
             pTableNode->GetTable().CreateSelection( pStart, pEnd, aNew,
                 SwTable::SEARCH_NONE, false );
             ActualizeSelection( aNew );
@@ -2266,10 +2275,10 @@ bool SwTableCursor::NewTableSelection()
 
 void SwTableCursor::ActualizeSelection( const SwSelBoxes &rNew )
 {
-    sal_uInt16 nOld = 0, nNew = 0;
-    while ( nOld < aSelBoxes.size() && nNew < rNew.size() )
+    size_t nOld = 0, nNew = 0;
+    while (nOld < m_SelectedBoxes.size() && nNew < rNew.size())
     {
-        const SwTableBox* pPOld = aSelBoxes[ nOld ];
+        SwTableBox const*const pPOld = m_SelectedBoxes[ nOld ];
         const SwTableBox* pPNew = rNew[ nNew ];
         if( pPOld == pPNew )
         {   // this box will stay
@@ -2277,7 +2286,9 @@ void SwTableCursor::ActualizeSelection( const SwSelBoxes &rNew )
             ++nNew;
         }
         else if( pPOld->GetSttIdx() < pPNew->GetSttIdx() )
+        {
             DeleteBox( nOld ); // this box has to go
+        }
         else
         {
             InsertBox( *pPNew ); // this is a new one
@@ -2286,11 +2297,15 @@ void SwTableCursor::ActualizeSelection( const SwSelBoxes &rNew )
         }
     }
 
-    while( nOld < aSelBoxes.size() )
+    while (nOld < m_SelectedBoxes.size())
+    {
         DeleteBox( nOld ); // some more to delete
+    }
 
-    for( ; nNew < rNew.size(); ++nNew ) // some more to insert
+    for ( ; nNew < rNew.size(); ++nNew ) // some more to insert
+    {
         InsertBox( *rNew[ nNew ] );
+    }
 }
 
 sal_Bool SwTableCursor::IsCrsrMovedUpdt()
@@ -2329,14 +2344,15 @@ void SwTableCursor::ParkCrsr()
 sal_Bool SwTableCursor::HasReadOnlyBoxSel() const
 {
     sal_Bool bRet = sal_False;
-    for( sal_uInt16 n = aSelBoxes.size(); n;  )
-        if( aSelBoxes[ --n ]->GetFrmFmt()->GetProtect().IsCntntProtected() )
+    for (size_t n = m_SelectedBoxes.size(); n; )
+    {
+        if (m_SelectedBoxes[--n]->GetFrmFmt()->GetProtect().IsCntntProtected())
         {
             bRet = sal_True;
             break;
         }
+    }
     return bRet;
 }
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

zhirshya/lo-tblr

commit 6874178332ce0ef88a177d1e6003c1bd91eed422

Related: fdo#65090 Fix crash in SwXTextTableCursor::mergeRange()
Regression from b844f06b36cf9a6c1411861a08701c8f9be2af0d (fdo#48692: fix
problems with large number of table cells, 2012-09-21), the problem was
that GetSelectedBoxesCount() may return 0, and if that happens, the
prefix decrement operator set nCount to 0xffffffffffffffff, which then
resulted in a call to DeleteBox(), which did not happen before.

Thanks debug STL for pointing out this issue.

Change-Id: I9eee55f642726343c4ce9af479ccd2c4dde03b6a

sw/source/core/unocore/unotbl.cxx
@@ -1720,7 +1720,7 @@ sal_Bool SwXTextTableCursor::mergeRange(void) throw( uno::RuntimeException )
             if(bRet)
             {
                 size_t nCount = pTblCrsr->GetSelectedBoxesCount();
-                while (--nCount)
+                while (nCount--)
                 {
                     pTblCrsr->DeleteBox(nCount);
                 }