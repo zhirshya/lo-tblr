LibreOffice/core

commit 614e04019a672cdd61b86699d99250d80f169f95

codemaker: fix invalid string index access
Change-Id: Icca819484e751864d146a893fe78e8ef2c36363b

codemaker/source/codemaker/global.cxx
@@ -87,8 +87,9 @@ OString createFileNameFromType( const OString& destination,
     length += prefix.getLength() + type.getLength() + postfix.getLength();
 
     sal_Bool withSeparator = sal_False;
-    if (destination[destination.getLength()] != '\\' &&
-        destination[destination.getLength()] != '/' &&
+    if (!destination.isEmpty() &&
+        destination[destination.getLength() - 1] != '\\' &&
+        destination[destination.getLength() - 1] != '/' &&
         type[0] != '\\' &&
         type[0] != '/')
     {

zhirshya/lo-tblr

commit b9514ed27f75f132e0cf1c528452823f01b65881

codemaker: fix^2 invalid string index access
...originally the past-the-end checks in destination where always true, and thus
happend to work as intended for empty destionation, but
614e04019a672cdd61b86699d99250d80f169f95 broke that, so calling cppumaker w/o
-O was broken now.

Change-Id: I8d41dfe8d4c12e4a73a9782d4d5e7c9fa4d9df81

codemaker/source/codemaker/global.cxx
@@ -87,11 +87,8 @@ OString createFileNameFromType( const OString& destination,
     length += prefix.getLength() + type.getLength() + postfix.getLength();
 
     sal_Bool withSeparator = sal_False;
-    if (!destination.isEmpty() &&
-        destination[destination.getLength() - 1] != '\\' &&
-        destination[destination.getLength() - 1] != '/' &&
-        type[0] != '\\' &&
-        type[0] != '/')
+    if (!(destination.endsWith("\\") || destination.endsWith("/"))
+        && !(type.startsWith("\\") || type.startsWith("/")))
     {
         length++;
         withSeparator = sal_True;