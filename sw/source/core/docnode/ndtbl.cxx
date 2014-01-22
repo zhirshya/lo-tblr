LibreOffice/core

commit 772101649cf16233bbaf0900aa9ebbc915151a95

Convert SwDoc::UnProtectCells to use std::vector
Change-Id: Ib1b4756bf26934ded941a7c91c6742e03b942f59

sw/source/core/docnode/ndtbl.cxx
@@ -4466,22 +4466,22 @@ sal_Bool SwDoc::UnProtectCells( const SwSelBoxes& rBoxes )
                 ? new SwUndoAttrTbl( *rBoxes[0]->GetSttNd()->FindTableNode() )
                 : 0;
 
-        SvPtrarr aFmts( 16 ), aNewFmts( 16 );
+        std::vector<SwFrmFmt*> aFmts, aNewFmts;
         for( sal_uInt16 i = rBoxes.Count(); i; )
         {
             SwTableBox* pBox = rBoxes[ --i ];
             SwFrmFmt* pBoxFmt = pBox->GetFrmFmt();
             if( pBoxFmt->GetProtect().IsCntntProtected() )
             {
-                sal_uInt16 nFnd = aFmts.GetPos( pBoxFmt );
-                if( USHRT_MAX != nFnd )
-                    pBox->ChgFrmFmt( (SwTableBoxFmt*)aNewFmts[ nFnd ] );
+                std::vector<SwFrmFmt*>::iterator it = std::find( aFmts.begin(), aFmts.end(), pBoxFmt );
+                if( aFmts.end() != it )
+                    pBox->ChgFrmFmt( (SwTableBoxFmt*)*it );
                 else
                 {
-                    aFmts.Insert( pBoxFmt, aFmts.Count() );
+                    aFmts.push_back( pBoxFmt );
                     pBoxFmt = pBox->ClaimFrmFmt();
                     pBoxFmt->ResetFmtAttr( RES_PROTECT );
-                    aNewFmts.Insert( pBoxFmt, aNewFmts.Count() );
+                    aNewFmts.push_back( pBoxFmt );
                 }
                 bChgd = sal_True;
             }

zhirshya/lo-tblr

commit 2f302d5060a969c7b4bedee8cdaf19305576ac06

fdo#67238: sw: fix table cell Unprotect
SwDoc::UnProtectCells() was setting the old box format (from aFmts)
instead of the new one, and actually these 2 vectors are a map.

(regression from 772101649cf16233bbaf0900aa9ebbc915151a95)

Change-Id: I0881a1c499c51b5f3c257e9def1a2e9a00bcb639

sw/source/core/docnode/ndtbl.cxx
@@ -4424,22 +4424,23 @@ sal_Bool SwDoc::UnProtectCells( const SwSelBoxes& rBoxes )
                 ? new SwUndoAttrTbl( *rBoxes[0]->GetSttNd()->FindTableNode() )
                 : 0;
 
-        std::vector<SwFrmFmt*> aFmts, aNewFmts;
+        std::map<SwFrmFmt*, SwTableBoxFmt*> aFmtsMap;
         for (size_t i = rBoxes.size(); i; )
         {
             SwTableBox* pBox = rBoxes[ --i ];
             SwFrmFmt* pBoxFmt = pBox->GetFrmFmt();
             if( pBoxFmt->GetProtect().IsCntntProtected() )
             {
-                std::vector<SwFrmFmt*>::iterator it = std::find( aFmts.begin(), aFmts.end(), pBoxFmt );
-                if( aFmts.end() != it )
-                    pBox->ChgFrmFmt( (SwTableBoxFmt*)*it );
+                std::map<SwFrmFmt*, SwTableBoxFmt*>::const_iterator const it =
+                    aFmtsMap.find(pBoxFmt);
+                if (aFmtsMap.end() != it)
+                    pBox->ChgFrmFmt(it->second);
                 else
                 {
-                    aFmts.push_back( pBoxFmt );
-                    pBoxFmt = pBox->ClaimFrmFmt();
-                    pBoxFmt->ResetFmtAttr( RES_PROTECT );
-                    aNewFmts.push_back( pBoxFmt );
+                    SwTableBoxFmt *const pNewBoxFmt(
+                        dynamic_cast<SwTableBoxFmt*>(pBox->ClaimFrmFmt()));
+                    pNewBoxFmt->ResetFmtAttr( RES_PROTECT );
+                    aFmtsMap.insert(std::make_pair(pBoxFmt, pNewBoxFmt));
                 }
                 bChgd = sal_True;
             }