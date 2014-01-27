LibreOffice/core

commit e44335abe006d05f0f915279605a03ef084116d6

Related #i123433# PolyPolygonBezier must be drawing::PolyPolygonBezierCoords
Change-Id: Idc7e0a2651f2e0b499bbf8b8443ffb84b54a45b6

xmloff/source/draw/ximpshap.cxx
@@ -1459,6 +1459,8 @@ void SdXMLPathShapeContext::StartElement(const uno::Reference< xml::sax::XAttrib
                                 aPolyPolygon,
                                 aSourcePolyPolygon);
                             aAny <<= aSourcePolyPolygon;
+
+                            xPropSet->setPropertyValue(OUString("PolyPolygonBezier"), aAny);
                         }
                         else
                         {
@@ -1468,9 +1470,9 @@ void SdXMLPathShapeContext::StartElement(const uno::Reference< xml::sax::XAttrib
                                 aPolyPolygon,
                                 aSourcePolyPolygon);
                             aAny <<= aSourcePolyPolygon;
-                        }
 
-                        xPropSet->setPropertyValue(OUString("Geometry"), aAny);
+                            xPropSet->setPropertyValue(OUString("Geometry"), aAny);
+                        }
                     }
 
                     // set pos, size, shear and rotate
@@ -1832,25 +1834,12 @@ void SdXMLConnectorShapeContext::processAttribute( sal_uInt16 nPrefix, const OUS
             {
                 if(aPolyPolygon.count())
                 {
-                    // set polygon data
-                    if(aPolyPolygon.areControlPointsUsed())
-                    {
-                        drawing::PolyPolygonBezierCoords aSourcePolyPolygon;
+                    drawing::PolyPolygonBezierCoords aSourcePolyPolygon;
 
-                        basegfx::tools::B2DPolyPolygonToUnoPolyPolygonBezierCoords(
-                            aPolyPolygon,
-                            aSourcePolyPolygon);
-                        maPath <<= aSourcePolyPolygon;
-                    }
-                    else
-                    {
-                        drawing::PointSequenceSequence aSourcePolyPolygon;
-
-                        basegfx::tools::B2DPolyPolygonToUnoPointSequenceSequence(
-                            aPolyPolygon,
-                            aSourcePolyPolygon);
-                        maPath <<= aSourcePolyPolygon;
-                    }
+                    basegfx::tools::B2DPolyPolygonToUnoPolyPolygonBezierCoords(
+                        aPolyPolygon,
+                        aSourcePolyPolygon);
+                    maPath <<= aSourcePolyPolygon;
                 }
             }
         }
@@ -1967,7 +1956,8 @@ void SdXMLConnectorShapeContext::StartElement(const uno::Reference< xml::sax::XA
 
                 if ( bApplySVGD )
                 {
-                    xProps->setPropertyValue("PolyPolygonBezier", maPath );
+                    assert(maPath.getValueType() == ::getCppuType((const drawing::PolyPolygonBezierCoords*)0));
+                    xProps->setPropertyValue("PolyPolygonBezier", maPath);
                 }
             }
 

zhirshya/lo-tblr

commit 55ea947ddf9420e076fe391935bf4b37f813b88c

Resolves: fdo#73814 wrong name for drawing::PointSequenceSequence property
examining svx/source/unodraw/unoprov.cxx "Geometry" is a
indeed a drawing::PolyPolygonBezierCoords for the BezierShapes
and a drawing::PointSequenceSequence for the PolyShapes

regression since e44335abe006d05f0f915279605a03ef084116d6 because after
223f6b631c1b087754c0f9051fb55f029f2503ce we started getting
drawing::PointSequenceSequences in maPath which is the wrong type for the
argument to property "PolyPolygonBezier" for those shapes.  Which led me to
incorrectly assume that the all PolyPolygonBezier properties were named
"PolyPolygonBezier" which isn't the case.

so reverting the non maPath hunks of e44335abe006d05f0f915279605a03ef084116d6

Change-Id: I013a66778d11a472fc4567e53a9e17e73e2b91ce

xmloff/source/draw/ximpshap.cxx
@@ -1470,8 +1470,6 @@ void SdXMLPathShapeContext::StartElement(const uno::Reference< xml::sax::XAttrib
                                 aPolyPolygon,
                                 aSourcePolyPolygon);
                             aAny <<= aSourcePolyPolygon;
-
-                            xPropSet->setPropertyValue(OUString("PolyPolygonBezier"), aAny);
                         }
                         else
                         {
@@ -1481,9 +1479,9 @@ void SdXMLPathShapeContext::StartElement(const uno::Reference< xml::sax::XAttrib
                                 aPolyPolygon,
                                 aSourcePolyPolygon);
                             aAny <<= aSourcePolyPolygon;
-
-                            xPropSet->setPropertyValue(OUString("Geometry"), aAny);
                         }
+
+                        xPropSet->setPropertyValue(OUString("Geometry"), aAny);
                     }
 
                     // set pos, size, shear and rotate