LibreOffice/core

commit 887bc4dd3e62fe6dd19dc9d1c3ba273a5b21b5ec

cp#1000025: Import casemaps from pptx.
Note: This only imports it and it may not be
even possible to export (even to othe formats).

Change-Id: I8e104f1bc3a95c42ac932b6ab58365dc113fb2e0

svx/source/tbxctrls/tbcontrl.cxx
@@ -73,6 +73,8 @@
 #include <editeng/shdditem.hxx>
 #include <editeng/udlnitem.hxx>
 #include <editeng/wghtitem.hxx>
+#include <editeng/svxfont.hxx>
+#include <editeng/cmapitem.hxx>
 #include "svx/drawitem.hxx"
 #include <svx/tbcontrl.hxx>
 #include "svx/dlgutil.hxx"
@@ -627,6 +629,10 @@ void SvxStyleBox_Impl::UserDraw( const UserDrawEvent& rUDEvt )
                 pItem = aItemSet.GetItem( SID_ATTR_CHAR_STRIKEOUT );
                 if ( pItem )
                     aFont.SetStrikeout( static_cast< const SvxCrossedOutItem* >( pItem )->GetStrikeout() );
+
+                pItem = aItemSet.GetItem( SID_ATTR_CHAR_CASEMAP );
+                if ( pItem )
+                    ((SvxFont &)aFont).SetCaseMap( static_cast< const SvxCaseMapItem* >( pItem )->GetCaseMap() );
 
                 pItem = aItemSet.GetItem( SID_ATTR_CHAR_EMPHASISMARK );
                 if ( pItem )

editeng/source/accessibility/AccessibleEditableTextPara.cxx
@@ -885,6 +885,7 @@ namespace accessibility
             STR("CharPosture");
             STR("CharShadowed");
             STR("CharStrikeout");
+            STR("CharCaseMap");
             STR("CharUnderline");
             STR("CharUnderlineColor");
             STR("CharWeight");

editeng/source/editeng/editattr.cxx
@@ -43,6 +43,7 @@
 #include <editeng/emphasismarkitem.hxx>
 #include <editeng/charscaleitem.hxx>
 #include <editeng/charreliefitem.hxx>
+#include <editeng/cmapitem.hxx>
 
 #include "editattr.hxx"
 
@@ -182,12 +183,26 @@ void EditCharAttribFontWidth::SetFont( SvxFont& /*rFont*/, OutputDevice* )
 EditCharAttribStrikeout::EditCharAttribStrikeout( const SvxCrossedOutItem& rAttr, sal_uInt16 _nStart, sal_uInt16 _nEnd )
     : EditCharAttrib( rAttr, _nStart, _nEnd )
 {
-    DBG_ASSERT( rAttr.Which() == EE_CHAR_STRIKEOUT, "Not a Size attribute!" );
+    DBG_ASSERT( rAttr.Which() == EE_CHAR_STRIKEOUT, "Not a Strikeout attribute!" );
 }
 
 void EditCharAttribStrikeout::SetFont( SvxFont& rFont, OutputDevice* )
 {
     rFont.SetStrikeout( (FontStrikeout)((const SvxCrossedOutItem*)GetItem())->GetValue() );
+}
+
+// -------------------------------------------------------------------------
+// class EditCharAttribCaseMap
+// -------------------------------------------------------------------------
+EditCharAttribCaseMap::EditCharAttribCaseMap( const SvxCaseMapItem& rAttr, sal_uInt16 _nStart, sal_uInt16 _nEnd )
+    : EditCharAttrib( rAttr, _nStart, _nEnd )
+{
+    DBG_ASSERT( rAttr.Which() == EE_CHAR_CASEMAP, "Not a CaseMap Item!" );
+}
+
+void EditCharAttribCaseMap::SetFont( SvxFont& rFont, OutputDevice* )
+{
+    rFont.SetCaseMap( ((const SvxCaseMapItem*)GetItem())->GetCaseMap() );
 }
 
 // -------------------------------------------------------------------------

editeng/source/editeng/editattr.hxx
@@ -47,6 +47,7 @@ class SvxEmphasisMarkItem;
 class SvxCharReliefItem;
 class SfxVoidItem;
 class OutputDevice;
+class SvxCaseMapItem;
 
 #define CH_FEATURE_OLD  (sal_uInt8)         0xFF
 #define CH_FEATURE      (sal_Unicode)   0x01
@@ -214,6 +215,17 @@ class EditCharAttribStrikeout : public EditCharAttrib
 {
 public:
     EditCharAttribStrikeout( const SvxCrossedOutItem& rAttr, sal_uInt16 nStart, sal_uInt16 nEnd );
+
+    virtual void    SetFont( SvxFont& rFont, OutputDevice* pOutDev );
+};
+
+// -------------------------------------------------------------------------
+// class EditCharAttribCaseMap
+// -------------------------------------------------------------------------
+class EditCharAttribCaseMap : public EditCharAttrib
+{
+public:
+    EditCharAttribCaseMap( const SvxCaseMapItem& rAttr, sal_uInt16 nStart, sal_uInt16 nEnd );
 
     virtual void    SetFont( SvxFont& rFont, OutputDevice* pOutDev );
 };

editeng/source/editeng/editdoc.cxx
@@ -33,6 +33,7 @@
 #include <editeng/wrlmitem.hxx>
 #include <editeng/wghtitem.hxx>
 #include <editeng/udlnitem.hxx>
+#include <editeng/cmapitem.hxx>
 #include <editeng/contouritem.hxx>
 #include <editeng/escapementitem.hxx>
 #include <editeng/shdditem.hxx>
@@ -211,6 +212,7 @@ const SfxItemInfo aItemInfos[EDITITEMCOUNT] = {
         { 0, SFX_ITEM_POOLABLE },                           // EE_CHAR_RUBI_DUMMY
         { 0, SFX_ITEM_POOLABLE },                           // EE_CHAR_XMLATTRIBS
         { SID_ATTR_CHAR_OVERLINE, SFX_ITEM_POOLABLE },
+        { SID_ATTR_CHAR_CASEMAP, SFX_ITEM_POOLABLE },       // EE_CHAR_CASEMAP
         { 0, SFX_ITEM_POOLABLE },                           // EE_FEATURE_TAB
         { 0, SFX_ITEM_POOLABLE },                           // EE_FEATURE_LINEBR
         { SID_ATTR_CHAR_CHARSETCOLOR, SFX_ITEM_POOLABLE },  // EE_FEATURE_NOTCONV
@@ -361,6 +363,11 @@ EditCharAttrib* MakeCharAttrib( SfxItemPool& rPool, const SfxPoolItem& rAttr, sa
         case EE_CHAR_XMLATTRIBS:
         {
             pNew = new EditCharAttrib( rNew, nS, nE );  // Attribute is only for holding XML information...
+        }
+        break;
+        case EE_CHAR_CASEMAP:
+        {
+            pNew = new EditCharAttribCaseMap( (const SvxCaseMapItem&)rNew, nS, nE );
         }
         break;
         case EE_FEATURE_TAB:
@@ -1945,6 +1952,8 @@ void CreateFont( SvxFont& rFont, const SfxItemSet& rSet, bool bSearchInParent, s
         rFont.SetOverline( ((const SvxOverlineItem&)rSet.Get( EE_CHAR_OVERLINE )).GetLineStyle() );
     if ( bSearchInParent || ( rSet.GetItemState( EE_CHAR_STRIKEOUT ) == SFX_ITEM_ON ) )
         rFont.SetStrikeout( ((const SvxCrossedOutItem&)rSet.Get( EE_CHAR_STRIKEOUT )).GetStrikeout() );
+    if ( bSearchInParent || ( rSet.GetItemState( EE_CHAR_CASEMAP ) == SFX_ITEM_ON ) )
+        rFont.SetCaseMap( ((const SvxCaseMapItem&)rSet.Get( EE_CHAR_CASEMAP )).GetCaseMap() );
     if ( bSearchInParent || ( rSet.GetItemState( nWhich_Italic ) == SFX_ITEM_ON ) )
         rFont.SetItalic( ((const SvxPostureItem&)rSet.Get( nWhich_Italic )).GetPosture() );
     if ( bSearchInParent || ( rSet.GetItemState( EE_CHAR_OUTLINE ) == SFX_ITEM_ON ) )

editeng/source/editeng/editeng.cxx
@@ -37,6 +37,7 @@
 #include <editeng/acorrcfg.hxx>
 #include <editeng/flditem.hxx>
 #include <editeng/txtrange.hxx>
+#include <editeng/cmapitem.hxx>
 #include <vcl/graph.hxx>
 
 #include <editeng/autokernitem.hxx>
@@ -2607,6 +2608,7 @@ void EditEngine::SetFontInfoInItemSet( SfxItemSet& rSet, const SvxFont& rFont )
     rSet.Put( SvxUnderlineItem( rFont.GetUnderline(), EE_CHAR_UNDERLINE )  );
     rSet.Put( SvxOverlineItem( rFont.GetOverline(), EE_CHAR_OVERLINE )  );
     rSet.Put( SvxCrossedOutItem( rFont.GetStrikeout(), EE_CHAR_STRIKEOUT )  );
+    rSet.Put( SvxCaseMapItem( rFont.GetCaseMap(), EE_CHAR_CASEMAP )  );
     rSet.Put( SvxPostureItem( rFont.GetItalic(), EE_CHAR_ITALIC )  );
     rSet.Put( SvxContourItem( rFont.IsOutline(), EE_CHAR_OUTLINE )  );
     rSet.Put( SvxAutoKernItem( rFont.IsKerning(), EE_CHAR_PAIRKERNING ) );

editeng/source/editeng/eerdll.cxx
@@ -58,6 +58,7 @@
 #include <editeng/wrlmitem.hxx>
 #include <editeng/numitem.hxx>
 #include <editeng/langitem.hxx>
+#include <editeng/cmapitem.hxx>
 #include <editeng/charscaleitem.hxx>
 #include <editeng/charreliefitem.hxx>
 #include <editeng/frmdiritem.hxx>
@@ -151,12 +152,13 @@ SfxPoolItem** GlobalEditData::GetDefItems()
         ppDefItems[45] = new SfxVoidItem( EE_CHAR_RUBI_DUMMY );
         ppDefItems[46] = new SvXMLAttrContainerItem( EE_CHAR_XMLATTRIBS );
         ppDefItems[47] = new SvxOverlineItem( UNDERLINE_NONE, EE_CHAR_OVERLINE );
+        ppDefItems[48] = new SvxCaseMapItem( SVX_CASEMAP_NOT_MAPPED, EE_CHAR_CASEMAP );
 
         // Features
-        ppDefItems[48] = new SfxVoidItem( EE_FEATURE_TAB );
-        ppDefItems[49] = new SfxVoidItem( EE_FEATURE_LINEBR );
-        ppDefItems[50] = new SvxCharSetColorItem( Color( COL_RED ), RTL_TEXTENCODING_DONTKNOW, EE_FEATURE_NOTCONV );
-        ppDefItems[51] = new SvxFieldItem( SvxFieldData(), EE_FEATURE_FIELD );
+        ppDefItems[49] = new SfxVoidItem( EE_FEATURE_TAB );
+        ppDefItems[50] = new SfxVoidItem( EE_FEATURE_LINEBR );
+        ppDefItems[51] = new SvxCharSetColorItem( Color( COL_RED ), RTL_TEXTENCODING_DONTKNOW, EE_FEATURE_NOTCONV );
+        ppDefItems[52] = new SvxFieldItem( SvxFieldData(), EE_FEATURE_FIELD );
 
         DBG_ASSERT( EDITITEMCOUNT == 52, "ITEMCOUNT geaendert, DefItems nicht angepasst!" );
 

editeng/source/uno/unofdesc.cxx
@@ -33,6 +33,7 @@
 #include <svl/itempool.hxx>
 
 #include <editeng/unofdesc.hxx>
+#include <editeng/svxfont.hxx>
 
 using namespace ::rtl;
 using namespace ::com::sun::star;

zhirshya/lo-tblr

commit 4b9b68f1bbcfdda543679765f2300e984a317388

fix bad cast in SvxStyleBox_Impl::StateChanged()
This causes various JunitTest crashes on Windows, regression from
887bc4dd3e62fe6dd19dc9d1c3ba273a5b21b5ec.

Change-Id: Iab24e0d39375ac1e5ac63d32470dca1e54d518c5

svx/source/tbxctrls/tbcontrl.cxx
@@ -596,7 +596,10 @@ void SvxStyleBox_Impl::UserDraw( const UserDrawEvent& rUDEvt )
                 Size aPixelSize( pDevice->LogicToPixel( aFontSize, pShell->GetMapUnit() ) );
 
                 // setup the font properties
-                Font aFont( pFontItem->GetFamilyName(), pFontItem->GetStyleName(), aPixelSize );
+                SvxFont aFont;
+                aFont.SetName(pFontItem->GetFamilyName());
+                aFont.SetStyleName(pFontItem->GetStyleName());
+                aFont.SetSize(aPixelSize);
 
                 const SfxPoolItem *pItem = aItemSet.GetItem( SID_ATTR_CHAR_WEIGHT );
                 if ( pItem )
@@ -632,7 +635,7 @@ void SvxStyleBox_Impl::UserDraw( const UserDrawEvent& rUDEvt )
 
                 pItem = aItemSet.GetItem( SID_ATTR_CHAR_CASEMAP );
                 if ( pItem )
-                    ((SvxFont &)aFont).SetCaseMap( static_cast< const SvxCaseMapItem* >( pItem )->GetCaseMap() );
+                    aFont.SetCaseMap(static_cast<const SvxCaseMapItem*>(pItem)->GetCaseMap());
 
                 pItem = aItemSet.GetItem( SID_ATTR_CHAR_EMPHASISMARK );
                 if ( pItem )