LibreOffice/core

commit 04683f14883f4cd64febadd71b327639f1e7edcc

Window::Notify should return bool
Change-Id: I72081b1022582c8b6f95a611e21d9c78f7581efe

sw/source/ui/ribbar/workctrl.cxx
@@ -623,7 +623,7 @@ public:
 
 protected:
     virtual void    Select();
-    virtual long    Notify( NotifyEvent& rNEvt );
+    virtual bool    Notify( NotifyEvent& rNEvt );
 
     void ReleaseFocus();
 
@@ -680,9 +680,9 @@ void    SwZoomBox_Impl::Select()
     }
 }
 
-long SwZoomBox_Impl::Notify( NotifyEvent& rNEvt )
+bool SwZoomBox_Impl::Notify( NotifyEvent& rNEvt )
 {
-    long nHandled = 0;
+    bool nHandled = true;
 
     if ( rNEvt.GetType() == EVENT_KEYINPUT )
     {
@@ -696,7 +696,7 @@ long SwZoomBox_Impl::Notify( NotifyEvent& rNEvt )
                 if ( KEY_TAB == nCode )
                     bRelease = false;
                 else
-                    nHandled = 1;
+                    nHandled = true;
                 Select();
                 break;
             }
@@ -714,7 +714,7 @@ long SwZoomBox_Impl::Notify( NotifyEvent& rNEvt )
             SetText( GetSavedValue() );
     }
 
-    return nHandled ? nHandled : ComboBox::Notify( rNEvt );
+    return nHandled || ComboBox::Notify( rNEvt );
 }
 
 void SwZoomBox_Impl::ReleaseFocus()

zhirshya/lo-tblr

commit b97a9adb2158043a6432ce6fd62dcd28dd19b2f3

coverity#1169809 regression on long->bool
regression since 04683f14883f4cd64febadd71b327639f1e7edcc
apparently

Change-Id: I35b154dafd9534fb8234a82683cb3e879e136834

sw/source/ui/ribbar/workctrl.cxx
@@ -684,7 +684,7 @@ void    SwZoomBox_Impl::Select()
 
 bool SwZoomBox_Impl::Notify( NotifyEvent& rNEvt )
 {
-    bool nHandled = true;
+    bool bHandled = false;
 
     if ( rNEvt.GetType() == EVENT_KEYINPUT )
     {
@@ -698,7 +698,7 @@ bool SwZoomBox_Impl::Notify( NotifyEvent& rNEvt )
                 if ( KEY_TAB == nCode )
                     bRelease = false;
                 else
-                    nHandled = true;
+                    bHandled = true;
                 Select();
                 break;
             }
@@ -716,7 +716,7 @@ bool SwZoomBox_Impl::Notify( NotifyEvent& rNEvt )
             SetText( GetSavedValue() );
     }
 
-    return nHandled || ComboBox::Notify( rNEvt );
+    return bHandled || ComboBox::Notify( rNEvt );
 }
 
 void SwZoomBox_Impl::ReleaseFocus()