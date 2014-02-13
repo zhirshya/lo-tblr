LibreOffice/core

commit 16428c9600964a4945cf6fd0d938dea047d1248b

sidebar: converted slide transitions panel to .ui
Change-Id: I681908baaf27682f04d26f4430c6198bf35ca5b6

sd/uiconfig/simpress/ui/slidetransitionspanel.ui
@@ -1,0 +1,372 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <!-- interface-requires gtk+ 3.0 -->
+  <object class="GtkBox" id="box1">
+    <property name="visible">True</property>
+    <property name="can_focus">False</property>
+    <property name="orientation">vertical</property>
+    <property name="border_width">6</property>
+    <property name="spacing">12</property>
+    <child>
+      <object class="GtkFrame" id="frame3">
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+        <property name="label_xalign">0</property>
+        <property name="shadow_type">none</property>
+        <child>
+          <object class="GtkAlignment" id="alignment3">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="top_padding">6</property>
+            <property name="left_padding">12</property>
+            <child>
+              <object class="GtkTreeView" id="transitions_list">
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <child internal-child="selection">
+                  <object class="GtkTreeSelection" id="treeview-selection1"/>
+                </child>
+              </object>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="label3">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="label" translatable="yes">Apply to selected slides</property>
+            <attributes>
+              <attribute name="weight" value="bold"/>
+            </attributes>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">0</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkFrame" id="frame1">
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+        <property name="label_xalign">0</property>
+        <property name="shadow_type">none</property>
+        <child>
+          <object class="GtkAlignment" id="alignment1">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="top_padding">6</property>
+            <property name="left_padding">12</property>
+            <child>
+              <object class="GtkBox" id="box2">
+                <property name="visible">True</property>
+                <property name="can_focus">False</property>
+                <property name="orientation">vertical</property>
+                <property name="spacing">12</property>
+                <child>
+                  <object class="GtkBox" id="box4">
+                    <property name="visible">True</property>
+                    <property name="can_focus">False</property>
+                    <property name="spacing">6</property>
+                    <child>
+                      <object class="GtkLabel" id="speed_label">
+                        <property name="visible">True</property>
+                        <property name="can_focus">False</property>
+                        <property name="label" translatable="yes">Speed:</property>
+                      </object>
+                      <packing>
+                        <property name="expand">False</property>
+                        <property name="fill">True</property>
+                        <property name="position">0</property>
+                      </packing>
+                    </child>
+                    <child>
+                      <object class="GtkComboBox" id="speed_list">
+                        <property name="visible">True</property>
+                        <property name="can_focus">False</property>
+                        <property name="hexpand">True</property>
+                        <items>
+                           <item translatable="yes">Slow</item>
+                           <item translatable="yes">Medium</item>
+                           <item translatable="yes">Fast</item>
+                        </items>
+                      </object>
+                      <packing>
+                        <property name="expand">False</property>
+                        <property name="fill">True</property>
+                        <property name="position">1</property>
+                      </packing>
+                    </child>
+                  </object>
+                  <packing>
+                    <property name="expand">False</property>
+                    <property name="fill">True</property>
+                    <property name="position">0</property>
+                  </packing>
+                </child>
+                <child>
+                  <object class="GtkBox" id="box5">
+                    <property name="visible">True</property>
+                    <property name="can_focus">False</property>
+                    <property name="spacing">6</property>
+                    <child>
+                      <object class="GtkLabel" id="sound_label">
+                        <property name="visible">True</property>
+                        <property name="can_focus">False</property>
+                        <property name="label" translatable="yes">Sound:</property>
+                      </object>
+                      <packing>
+                        <property name="expand">False</property>
+                        <property name="fill">True</property>
+                        <property name="position">0</property>
+                      </packing>
+                    </child>
+                    <child>
+                      <object class="GtkComboBoxText" id="sound_list">
+                        <property name="visible">True</property>
+                        <property name="can_focus">False</property>
+                        <property name="hexpand">True</property>
+                        <items>
+                           <item translatable="yes">No Sound</item>
+                           <item translatable="yes">Stop Previous Sound</item>
+                           <item translatable="yes">Other Sound...</item>
+                        </items>
+                      </object>
+                      <packing>
+                        <property name="expand">False</property>
+                        <property name="fill">True</property>
+                        <property name="position">1</property>
+                      </packing>
+                    </child>
+                  </object>
+                  <packing>
+                    <property name="expand">False</property>
+                    <property name="fill">True</property>
+                    <property name="position">1</property>
+                  </packing>
+                </child>
+                <child>
+                  <object class="GtkCheckButton" id="loop_sound">
+                    <property name="label" translatable="yes">Loop until next sound</property>
+                    <property name="use_action_appearance">False</property>
+                    <property name="visible">True</property>
+                    <property name="can_focus">True</property>
+                    <property name="receives_default">False</property>
+                    <property name="margin_left">12</property>
+                    <property name="use_action_appearance">False</property>
+                    <property name="xalign">0</property>
+                    <property name="draw_indicator">True</property>
+                  </object>
+                  <packing>
+                    <property name="expand">False</property>
+                    <property name="fill">True</property>
+                    <property name="position">2</property>
+                  </packing>
+                </child>
+              </object>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="label1">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="label" translatable="yes">Modify transition</property>
+            <attributes>
+              <attribute name="weight" value="bold"/>
+            </attributes>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">1</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkFrame" id="frame2">
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+        <property name="label_xalign">0</property>
+        <property name="shadow_type">none</property>
+        <child>
+          <object class="GtkAlignment" id="alignment2">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="top_padding">6</property>
+            <property name="left_padding">12</property>
+            <child>
+              <object class="GtkGrid" id="grid1">
+                <property name="visible">True</property>
+                <property name="can_focus">False</property>
+                <child>
+                  <object class="GtkRadioButton" id="rb_mouse_click">
+                    <property name="label" translatable="yes">On mouse click</property>
+                    <property name="use_action_appearance">False</property>
+                    <property name="visible">True</property>
+                    <property name="can_focus">True</property>
+                    <property name="receives_default">False</property>
+                    <property name="use_action_appearance">False</property>
+                    <property name="xalign">0</property>
+                    <property name="active">True</property>
+                    <property name="draw_indicator">True</property>
+                    <property name="group">rb_mouse_click</property>
+                  </object>
+                  <packing>
+                    <property name="left_attach">0</property>
+                    <property name="top_attach">0</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
+                  </packing>
+                </child>
+                <child>
+                  <object class="GtkRadioButton" id="rb_auto_after">
+                    <property name="label" translatable="yes">Automatically after</property>
+                    <property name="use_action_appearance">False</property>
+                    <property name="visible">True</property>
+                    <property name="can_focus">True</property>
+                    <property name="receives_default">False</property>
+                    <property name="use_action_appearance">False</property>
+                    <property name="xalign">0</property>
+                    <property name="active">True</property>
+                    <property name="draw_indicator">True</property>
+                    <property name="group">rb_mouse_click</property>
+                  </object>
+                  <packing>
+                    <property name="left_attach">0</property>
+                    <property name="top_attach">1</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
+                  </packing>
+                </child>
+                <child>
+                  <object class="GtkSpinButton" id="auto_after_value:0">
+                    <property name="visible">True</property>
+                    <property name="can_focus">True</property>
+                    <property name="invisible_char">●</property>
+                  </object>
+                  <packing>
+                    <property name="left_attach">1</property>
+                    <property name="top_attach">1</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
+                  </packing>
+                </child>
+                <child>
+                  <placeholder/>
+                </child>
+                <child>
+                  <placeholder/>
+                </child>
+                <child>
+                  <placeholder/>
+                </child>
+                <child>
+                  <placeholder/>
+                </child>
+                <child>
+                  <placeholder/>
+                </child>
+                <child>
+                  <placeholder/>
+                </child>
+              </object>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="label2">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="label" translatable="yes">Advance slide</property>
+            <attributes>
+              <attribute name="weight" value="bold"/>
+            </attributes>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">2</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkButton" id="apply_to_all">
+        <property name="label" translatable="yes">Apply to All Slides</property>
+        <property name="use_action_appearance">False</property>
+        <property name="visible">True</property>
+        <property name="can_focus">True</property>
+        <property name="receives_default">True</property>
+        <property name="use_action_appearance">False</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">3</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkBox" id="box3">
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+        <property name="spacing">12</property>
+        <child>
+          <object class="GtkButton" id="play">
+            <property name="label" translatable="yes">Play</property>
+            <property name="use_action_appearance">False</property>
+            <property name="visible">True</property>
+            <property name="can_focus">True</property>
+            <property name="receives_default">True</property>
+            <property name="use_action_appearance">False</property>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkButton" id="slide_show">
+            <property name="label" translatable="yes">Slide Show</property>
+            <property name="use_action_appearance">False</property>
+            <property name="visible">True</property>
+            <property name="can_focus">True</property>
+            <property name="receives_default">True</property>
+            <property name="use_action_appearance">False</property>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">1</property>
+          </packing>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">4</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkCheckButton" id="auto_preview">
+        <property name="label" translatable="yes">Automatic preview</property>
+        <property name="use_action_appearance">False</property>
+        <property name="visible">True</property>
+        <property name="can_focus">True</property>
+        <property name="receives_default">False</property>
+        <property name="use_action_appearance">False</property>
+        <property name="xalign">0</property>
+        <property name="draw_indicator">True</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">5</property>
+      </packing>
+    </child>
+  </object>
+</interface>

sd/UIConfig_simpress.mk
@@ -86,6 +86,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/simpress,\
 	sd/uiconfig/simpress/ui/printeroptions \
 	sd/uiconfig/simpress/ui/prntopts \
 	sd/uiconfig/simpress/ui/sdviewpage \
+	sd/uiconfig/simpress/ui/slidetransitionspanel \
 	sd/uiconfig/simpress/ui/templatedialog \
 ))
 

sd/source/ui/animations/CustomAnimation.hrc
@@ -84,6 +84,7 @@
 #define STR_CUSTOMANIMATION_TRIGGER                     RID_CUSTOMANIMATION_START+35
 #define STR_CUSTOMANIMATION_LIST_HELPTEXT               RID_CUSTOMANIMATION_START+36
 #define STR_CUSTOMANIMATION_USERPATH                    RID_CUSTOMANIMATION_START+37
+#define STR_SLIDETRANSITION_NONE                        RID_CUSTOMANIMATION_START+38
 
 #define CM_WITH_CLICK               1
 #define CM_WITH_PREVIOUS            2

sd/source/ui/animations/CustomAnimation.src
@@ -389,6 +389,11 @@ String STR_CUSTOMANIMATION_USERPATH
     Text [ en-US ] = "User paths";
 };
 
+String STR_SLIDETRANSITION_NONE
+{
+    Text [ en-US ] = "No transition";
+};
+
 Image IMG_CUSTOMANIMATION_ON_CLICK
 {
     ImageBitmap = Bitmap { File = "click_16.png" ; };

sd/source/ui/animations/SlideTransitionPane.hxx
@@ -28,6 +28,8 @@
 #include <vcl/fixed.hxx>
 #include <vcl/button.hxx>
 #include <vcl/field.hxx>
+#include <svx/sidebar/PanelLayout.hxx>
+#include <sfx2/sidebar/ControlFactory.hxx>
 #include <com/sun/star/drawing/XDrawView.hpp>
 #include <com/sun/star/frame/XModel.hpp>
 
@@ -46,14 +48,15 @@ namespace impl
     struct TransitionEffect;
 }
 
-class SlideTransitionPane : public Control
+class SlideTransitionPane : public PanelLayout
 {
 public:
     explicit SlideTransitionPane(
         ::Window * pParent,
         ViewShellBase & rBase,
         const Size& rMinSize,
-        SdDrawDocument* pDoc );
+        SdDrawDocument* pDoc,
+        const cssu::Reference<css::frame::XFrame>& rxFrame );
     virtual ~SlideTransitionPane();
 
     // Window
@@ -101,24 +104,19 @@ private:
     SdDrawDocument *  mpDrawDoc;
     Size              maMinSize;
 
-    FixedLine    maFL_APPLY_TRANSITION;
-    ListBox      maLB_SLIDE_TRANSITIONS;
-    FixedLine    maFL_MODIFY_TRANSITION;
-    FixedText    maFT_SPEED;
-    ListBox      maLB_SPEED;
-    FixedText    maFT_SOUND;
-    ListBox      maLB_SOUND;
-    CheckBox     maCB_LOOP_SOUND;
-    FixedLine    maFL_ADVANCE_SLIDE;
-    RadioButton  maRB_ADVANCE_ON_MOUSE;
-    RadioButton  maRB_ADVANCE_AUTO;
-    MetricField  maMF_ADVANCE_AUTO_AFTER;
-    FixedLine    maFL_EMPTY1;
-    PushButton   maPB_APPLY_TO_ALL;
-    PushButton   maPB_PLAY;
-    PushButton   maPB_SLIDE_SHOW;
-    FixedLine    maFL_EMPTY2;
-    CheckBox     maCB_AUTO_PREVIEW;
+    ListBox*      mpLB_SLIDE_TRANSITIONS;
+    FixedText*    mpFT_SPEED;
+    ListBox*      mpLB_SPEED;
+    FixedText*    mpFT_SOUND;
+    ListBox*      mpLB_SOUND;
+    CheckBox*     mpCB_LOOP_SOUND;
+    RadioButton*  mpRB_ADVANCE_ON_MOUSE;
+    RadioButton*  mpRB_ADVANCE_AUTO;
+    MetricField*  mpMF_ADVANCE_AUTO_AFTER;
+    PushButton*   mpPB_APPLY_TO_ALL;
+    PushButton*   mpPB_PLAY;
+    PushButton*   mpPB_SLIDE_SHOW;
+    CheckBox*     mpCB_AUTO_PREVIEW;
 
     OUString     maSTR_NO_TRANSITION;
 

sd/source/ui/sidebar/PanelFactory.cxx
@@ -190,7 +190,7 @@ Reference<ui::XUIElement> SAL_CALL PanelFactory::createUIElement (
     else if (EndsWith(rsUIElementResourceURL, gsResourceNameUsedMasterPages))
         pControl = CurrentMasterPagesSelector::Create(pParentWindow, *pBase, xSidebar);
     else if (EndsWith(rsUIElementResourceURL, gsResourceNameSlideTransitions))
-        pControl = new SlideTransitionPanel(pParentWindow, *pBase);
+        pControl = new SlideTransitionPanel(pParentWindow, *pBase, xFrame);
     else if (EndsWith(rsUIElementResourceURL, gsResourceNameTableDesign))
         pControl = createTableDesignPanel(pParentWindow, *pBase);
     else if (EndsWith(rsUIElementResourceURL, gsResourceNameNavigator))

sd/source/ui/sidebar/SlideTransitionPanel.cxx
@@ -22,7 +22,7 @@
 
 
 namespace sd {
-    extern ::Window* createSlideTransitionPanel (::Window* pParent, ViewShellBase& rBase);
+    extern ::Window* createSlideTransitionPanel (::Window* pParent, ViewShellBase& rBase, const cssu::Reference<css::frame::XFrame>& rxFrame);
     extern sal_Int32 getSlideTransitionPanelMinimumHeight (::Window* pParent);
 }
 
@@ -34,8 +34,11 @@ namespace sd { namespace sidebar {
 
 SlideTransitionPanel::SlideTransitionPanel (
     ::Window* pParentWindow,
-    ViewShellBase& rViewShellBase)
-    : PanelBase(pParentWindow, rViewShellBase)
+    ViewShellBase& rViewShellBase,
+    const cssu::Reference<css::frame::XFrame>& rxFrame
+    )
+    : PanelBase(pParentWindow, rViewShellBase),
+      mxFrame( rxFrame )
 {
 #ifdef DEBUG
     SetText(OUString("sd:SlideTransitionPanel"));
@@ -56,7 +59,7 @@ SlideTransitionPanel::~SlideTransitionPanel (void)
     ::Window* pParentWindow,
     ViewShellBase& rViewShellBase)
 {
-    return createSlideTransitionPanel(pParentWindow, rViewShellBase);
+    return createSlideTransitionPanel(pParentWindow, rViewShellBase, mxFrame);
 }
 
 

zhirshya/lo-tblr

commit 482e410f34dfc5529e14ed9fcc839c2997ec8e16

fdo#74468 fix timing of slide transitions
Regression since commit 16428c9600964a4945cf6fd0d938dea047d1248b.

Change-Id: Id274c21e08d10d2e727f3b5a3fd852cd297e4637

sd/uiconfig/simpress/ui/slidetransitionspanel.ui
@@ -255,6 +255,9 @@
                         <property name="can_focus">True</property>
                         <property name="invisible_char">●</property>
                         <property name="invisible_char_set">True</property>
+                        <property name="format">sec</property>
+                        <property name="digits">2</property>
+                        <property name="spin_size">25</property>
                       </object>
                       <packing>
                         <property name="left_attach">1</property>

vcl/source/control/field.cxx
@@ -1596,6 +1596,8 @@ bool MetricField::set_property(const OString &rKey, const OString &rValue)
     }
     else if (rKey == "digits")
         SetDecimalDigits(rValue.toInt32());
+    else if (rKey == "spin-size")
+        SetSpinSize(rValue.toInt32());
     else
         return SpinField::set_property(rKey, rValue);
     return true;