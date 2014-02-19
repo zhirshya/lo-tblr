LibreOffice/core

commit b80d8c695e6b6ded231e67ed9a80bcd1f1250c03

fdo#62096 - replace some O(U)String compareTo with ==
Change-Id: I98d04d7da4c2b7ea0b769df9c2bfa8c1ad86bf2d
Reviewed-on: https://gerrit.libreoffice.org/3422
Reviewed-by: Thomas Arnhold <thomas@arnhold.org>
Tested-by: Thomas Arnhold <thomas@arnhold.org>

swext/mediawiki/src/com/sun/star/wiki/WikiEditorImpl.java
@@ -146,7 +146,7 @@ public final class WikiEditorImpl extends WeakBase
     {
         final com.sun.star.util.URL myURL = aURL;
         //logger.log( Level.INFO, "received dispatch request for: "+aURL.Complete );
-        if ( aURL.Protocol.compareTo( protocolName ) == 0 )
+        if ( aURL.Protocol == protocolName )
         {
             /*
             synchronized( this )
@@ -158,7 +158,7 @@ public final class WikiEditorImpl extends WeakBase
 
             try
             {
-                if ( myURL.Path.compareTo( "send" ) == 0 )
+                if ( myURL.Path == "send" )
                 {
                     sendArticle();
                 }

package/source/xstor/xfactory.cxx
@@ -294,7 +294,7 @@ sal_Bool SAL_CALL OStorageFactory::supportsService( const OUString& ServiceName 
     uno::Sequence< OUString > aSeq = impl_staticGetSupportedServiceNames();
 
     for ( sal_Int32 nInd = 0; nInd < aSeq.getLength(); nInd++ )
-        if ( ServiceName.compareTo( aSeq[nInd] ) == 0 )
+        if ( ServiceName == aSeq[nInd] )
             return sal_True;
 
     return sal_False;

package/source/zippackage/zipfileaccess.cxx
@@ -469,7 +469,7 @@ sal_Bool SAL_CALL OZipFileAccess::supportsService( const OUString& ServiceName )
     uno::Sequence< OUString > aSeq = impl_staticGetSupportedServiceNames();
 
     for ( sal_Int32 nInd = 0; nInd < aSeq.getLength(); nInd++ )
-        if ( ServiceName.compareTo( aSeq[nInd] ) == 0 )
+        if ( ServiceName == aSeq[nInd] )
             return sal_True;
 
     return sal_False;

sal/workben/testfile.cxx
@@ -1760,7 +1760,7 @@ void DirectoryItemTest( void )
         }
 
 
-        if ( str1[0].compareTo(str1[1]) == 0 )
+        if ( str1[0] == str1[1] )
             print_error( rtl::OString( "Reset" ),FileBase::E_None );
         else
             print_error( rtl::OString( "Reset" ),FileBase::E_invalidError );

sd/source/ui/remotecontrol/DiscoveryService.cxx
@@ -93,7 +93,7 @@ void SAL_CALL DiscoveryService::run()
         socklen_t aLen = sizeof( aAddr );
         recvfrom( mSocket, aBuffer, BUFFER_SIZE, 0, (sockaddr*) &aAddr, &aLen );
         OString aString( aBuffer, strlen( "LOREMOTE_SEARCH" ) );
-        if ( aString.compareTo( "LOREMOTE_SEARCH" ) == 0 )
+        if ( aString == "LOREMOTE_SEARCH" )
         {
             OStringBuffer aStringBuffer("LOREMOTE_ADVERTISE\n");
             aStringBuffer.append( OUStringToOString(

sdext/source/pdfimport/wrapper/wrapper.cxx
@@ -297,7 +297,7 @@ uno::Reference<rendering::XPolyPolygon2D> Parser::readPath( double* pArea = NULL
 {
     const OString aSubPathMarker( "subpath" );
 
-    if( 0 != readNextToken().compareTo( aSubPathMarker ) )
+    if( readNextToken() != aSubPathMarker )
         OSL_PRECOND(false, "broken path");
 
     basegfx::B2DPolyPolygon aResult;
@@ -313,7 +313,7 @@ uno::Reference<rendering::XPolyPolygon2D> Parser::readPath( double* pArea = NULL
         sal_Int32 nDummy=m_nCharIndex;
         OString aCurrToken( m_aLine.getToken(m_nNextToken,' ',nDummy) );
 
-        while( m_nCharIndex != -1 && 0 != aCurrToken.compareTo(aSubPathMarker) )
+        while( m_nCharIndex != -1 && aCurrToken != aSubPathMarker )
         {
             sal_Int32 nCurveFlag;
             double    nX, nY;
@@ -634,15 +634,15 @@ uno::Sequence<beans::PropertyValue> Parser::readImageImpl()
     const sal_Int32 nImageSize( readInt32() );
 
     OUString           aFileName;
-    if( aToken.compareTo( aPngMarker ) == 0 )
+    if( aToken == aPngMarker )
         aFileName = aPngFile;
-    else if( aToken.compareTo( aJpegMarker ) == 0 )
+    else if( aToken == aJpegMarker )
         aFileName = aJpegFile;
-    else if( aToken.compareTo( aPbmMarker ) == 0 )
+    else if( aToken == aPbmMarker )
         aFileName = aPbmFile;
     else
     {
-        SAL_WARN_IF(aToken.compareTo( aPpmMarker ),"sdext.pdfimport","Invalid bitmap format");
+        SAL_WARN_IF(aToken != aPpmMarker,"sdext.pdfimport","Invalid bitmap format");
         aFileName = aPpmFile;
     }
 

sfx2/source/appl/xpackcreator.cxx
@@ -178,7 +178,7 @@ sal_Bool SAL_CALL OPackageStructureCreator::supportsService( const OUString& Ser
     uno::Sequence< OUString > aSeq = impl_getStaticSupportedServiceNames();
 
     for ( sal_Int32 nInd = 0; nInd < aSeq.getLength(); nInd++ )
-        if ( ServiceName.compareTo( aSeq[nInd] ) == 0 )
+        if ( ServiceName == aSeq[nInd] )
             return sal_True;
 
     return sal_False;

sfx2/source/doc/ownsubfilterservice.cxx
@@ -138,7 +138,7 @@ sal_Bool SAL_CALL OwnSubFilterService::supportsService( const OUString& ServiceN
     uno::Sequence< OUString > aSeq = impl_getStaticSupportedServiceNames();
 
     for ( sal_Int32 nInd = 0; nInd < aSeq.getLength(); nInd++ )
-        if ( ServiceName.compareTo( aSeq[nInd] ) == 0 )
+        if ( ServiceName == aSeq[nInd] )
             return sal_True;
 
     return sal_False;

zhirshya/lo-tblr

commit 8097d20a7388ea1d16882138ae1334038fc108e6

Resolves: fdo#69925 Wiki Publisher Extension is not working
Regression from http://cgit.freedesktop.org/libreoffice/core/commit/?id=b80d8c695e6b6ded231e67ed9a80bcd1f1250c03
"compareTo" has been replaced by "==", so we compared the equality ot the pointers on the objects instead of the content of the strings
To make it simple and since we compare String objects, let's simply use "equals" method

Change-Id: I65bc1a87751297411fdbe722d3f49bce5e291638

swext/mediawiki/src/com/sun/star/wiki/WikiEditorImpl.java
@@ -134,7 +134,7 @@ public final class WikiEditorImpl extends WeakBase
     {
         final com.sun.star.util.URL myURL = aURL;
         //logger.log( Level.INFO, "received dispatch request for: "+aURL.Complete );
-        if ( aURL.Protocol == protocolName )
+        if ( aURL.Protocol.equals(protocolName) )
         {
             /*
             synchronized( this )
@@ -146,7 +146,7 @@ public final class WikiEditorImpl extends WeakBase
 
             try
             {
-                if ( myURL.Path == "send" )
+                if ( myURL.Path.equals("send") )
                 {
                     sendArticle();
                 }