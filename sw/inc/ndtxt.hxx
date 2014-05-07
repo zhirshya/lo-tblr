LibreOffice/core

commit 6db39dbd7378351f6476f6db25eb7110c9cfb291

fdo#52028: sw: let text formatting ignore RSID in automatic styles
A suprising regression from 062eaeffe7cb986255063bb9b0a5f3fb3fc8e34c:
The RSID text attributes that are inserted for every user-inserted text
cause the text formatting (SwAttrIter) to create a lot more text portions,
and the portion breaks make font kerning impossible.

This is the only way i can think of to fix this problem; alternatives that
don't work are splitting the RSID out of the AUTOFMT hint into a separate
one and combining them in the sw UNO wrappers (fails because
SwXAutoStylesEnumerator actually does need to enumerate every AUTOFMT
including RSID), trying to detect and ignore them just in the text
formatting (the SwAttrIter cannot easily/cheaply detect when it's allowed
to skip), and having an internal subdivision inside the AUTOFMT hint (one
subsection for every RSID change) (which does not work because it cannot
ignore RSID-only AUTOFMTs completely).

Solve the problem with 2 additional flags on AUTOFMT and CHARFMT
attributes: FormatIgnoreStart and FormatIgnoreEnd, which indicate to
SwAttrIter::GetNextAttr() that the start or end of the hint should be
ignored, so that effectively it is merged with the preceding/subsequent
hint.  Of course the UNO API does not respect the flags so we can store
the RSIDs in automatic styles.

The flags are maintained in SwpHints::MergePortions, which detects both
RSID-only AUTOFMT hints (which can be ignored completely), and the
situation of N CHARFMT hints + AUTOFMT hint vs. N CHARFMT hints +
AUTOFMT hint where the AUTOFMT hints differ only in their RSID attribute.

This means that MergePortions needs to be called more often now, in cases
where the ignore flags may have been invalidated, such as:
- insertion of text with possible DontExpand flag set on hints
- deletion of hints
- SETATTR_NOHINTADJUST mode

Change-Id: I1fb95a87c654c67d114f6f7f2c43b847c50b0ffa

sw/source/core/txtnode/ndtxt.cxx
@@ -95,7 +95,7 @@ TYPEINIT1( SwTxtNode, SwCntntNode )
 #ifdef DBG_UTIL
 #define CHECK_SWPHINTS(pNd)  { if( pNd->GetpSwpHints() && \
                                    !pNd->GetDoc()->IsInReading() ) \
-                                  pNd->GetpSwpHints()->Check(); }
+                                  pNd->GetpSwpHints()->Check(true); }
 #else
 #define CHECK_SWPHINTS(pNd)
 #endif
@@ -263,6 +263,12 @@ SwTxtNode::~SwTxtNode()
 
 SwCntntFrm *SwTxtNode::MakeFrm( SwFrm* pSib )
 {
+    // fdo#52028: ODF file import does not result in MergePortions being called
+    // for every attribute, since that would be inefficient.  So call it here.
+    if (m_pSwpHints)
+    {
+        m_pSwpHints->MergePortions(*this);
+    }
     SwCntntFrm *pFrm = new SwTxtFrm( this, pSib );
     return pFrm;
 }
@@ -883,6 +889,7 @@ void SwTxtNode::Update( SwIndex const & rPos, const xub_StrLen nChangeLen,
         {
             bool bNoExp = false;
             bool bResort = false;
+            bool bMergePortionsNeeded = false;
             const sal_uInt16 coArrSz = static_cast<sal_uInt16>(RES_TXTATR_WITHEND_END) -
                                    static_cast<sal_uInt16>(RES_CHRATR_BEGIN);
 
@@ -929,6 +936,11 @@ void SwTxtNode::Update( SwIndex const & rPos, const xub_StrLen nChangeLen,
                         {
                             pHint->SetDontExpand( false );
                             bResort = true;
+                            // could have a continuation with IgnoreStart()...
+                            if (pHint->IsFormatIgnoreEnd())
+                            {
+                                bMergePortionsNeeded = true;
+                            }
                             if ( pHint->IsCharFmtAttr() )
                             {
                                 bNoExp = true;
@@ -969,7 +981,11 @@ void SwTxtNode::Update( SwIndex const & rPos, const xub_StrLen nChangeLen,
                     }
                 }
             }
-            if ( bResort )
+            if (bMergePortionsNeeded)
+            {
+                m_pSwpHints->MergePortions(*this); // does Resort too
+            }
+            else if (bResort)
             {
                 m_pSwpHints->Resort();
             }
@@ -1745,6 +1761,7 @@ OUString SwTxtNode::InsertText( const XubString & rStr, const SwIndex & rIdx,
 
     if ( HasHints() )
     {
+        bool bMergePortionsNeeded(false);
         for ( sal_uInt16 i = 0; i < m_pSwpHints->Count() &&
                 rIdx >= *(*m_pSwpHints)[i]->GetStart(); ++i )
         {
@@ -1764,6 +1781,14 @@ OUString SwTxtNode::InsertText( const XubString & rStr, const SwIndex & rIdx,
                         *pHt->GetStart() = *pHt->GetStart() - nLen;
                     *pEndIdx = *pEndIdx - nLen;
                     m_pSwpHints->DeleteAtPos(i);
+                    // could be that pHt has IsFormatIgnoreEnd set, and it's
+                    // not a RSID-only hint - now we have the inserted text
+                    // between pHt and its continuation... which we don't know.
+                    // punt the job to MergePortions below.
+                    if (pHt->IsFormatIgnoreEnd())
+                    {
+                        bMergePortionsNeeded = true;
+                    }
                     InsertHint( pHt, nsSetAttrMode::SETATTR_NOHINTADJUST );
                 }
                 // empty hints at insert position?
@@ -1792,8 +1817,13 @@ OUString SwTxtNode::InsertText( const XubString & rStr, const SwIndex & rIdx,
                 // Kein Feld, am Absatzanfang, HintExpand
                 m_pSwpHints->DeleteAtPos(i);
                 *pHt->GetStart() = *pHt->GetStart() - nLen;
+                // no effect on format ignore flags here (para start)
                 InsertHint( pHt, nsSetAttrMode::SETATTR_NOHINTADJUST );
             }
+        }
+        if (bMergePortionsNeeded)
+        {
+            m_pSwpHints->MergePortions(*this);
         }
         TryDeleteSwpHints();
     }
@@ -2042,6 +2072,7 @@ void SwTxtNode::CutImpl( SwTxtNode * const pDest, const SwIndex & rDestStart,
         // 2. Attribute verschieben
         // durch das Attribute-Array, bis der Anfang des Geltungsbereiches
         // des Attributs hinter dem zu verschiebenden Bereich liegt
+        bool bMergePortionsNeeded(false);
         sal_uInt16 nAttrCnt = 0;
         while ( m_pSwpHints && (nAttrCnt < m_pSwpHints->Count()) )
         {
@@ -2085,6 +2116,10 @@ void SwTxtNode::CutImpl( SwTxtNode * const pDest, const SwIndex & rDestStart,
                     // Attribut verschieben
                     m_pSwpHints->Delete( pHt );
                     // die Start/End Indicies neu setzen
+                    if (pHt->IsFormatIgnoreStart() || pHt->IsFormatIgnoreEnd())
+                    {
+                        bMergePortionsNeeded = true;
+                    }
                     *pHt->GetStart() =
                             nDestStart + (nAttrStartIdx - nTxtStartIdx);
                     if( pEndIdx )
@@ -2162,6 +2197,11 @@ void SwTxtNode::CutImpl( SwTxtNode * const pDest, const SwIndex & rDestStart,
         else
         {
             Update( rStart, nLen, sal_True, sal_True );
+        }
+
+        if (bMergePortionsNeeded)
+        {
+            m_pSwpHints->MergePortions(*this);
         }
 
         CHECK_SWPHINTS(this);

sw/inc/ndhints.hxx
@@ -116,7 +116,7 @@ public:
     inline sal_uInt16 Count() const { return m_HintStarts.size(); }
 
 #ifdef DBG_UTIL
-    bool Check() const;
+    bool Check(bool) const;
 #endif
 };
 

sw/inc/swtypes.hxx
@@ -192,6 +192,8 @@ namespace nsSetAttrMode
     const SetAttrMode SETATTR_DONTREPLACE    = 0x0002;  // Don't replace another text attribute.
 
     const SetAttrMode SETATTR_NOTXTATRCHR    = 0x0004;  // Don't insert 0xFF at attributes with no end.
+    /// attention: NOHINTADJUST prevents MergePortions!
+    /// when using this need to pay attention to ignore start/end flags of hint
     const SetAttrMode SETATTR_NOHINTADJUST   = 0x0008;  // No merging of ranges.
     const SetAttrMode SETATTR_NOFORMATATTR   = 0x0010;  // Do not change into format attribute.
     const SetAttrMode SETATTR_DONTCHGNUMRULE = 0x0020;  // Do not change NumRule.

sw/inc/txatbase.hxx
@@ -55,6 +55,8 @@ private:
     bool m_bDontExpandStart     : 1;    // don't expand start at paragraph start (ruby)
     bool m_bNesting             : 1;    // SwTxtAttrNesting
     bool m_bHasDummyChar        : 1;    // without end + meta
+    bool m_bFormatIgnoreStart   : 1;    ///< text formatting should ignore start
+    bool m_bFormatIgnoreEnd     : 1;    ///< text formatting should ignore end
 
 protected:
     SwTxtAttr( SfxPoolItem& rAttr, xub_StrLen nStart );
@@ -94,6 +96,10 @@ public:
     bool IsDontExpandStartAttr() const      { return m_bDontExpandStart; }
     bool IsNesting() const                  { return m_bNesting; }
     bool HasDummyChar() const               { return m_bHasDummyChar; }
+    bool IsFormatIgnoreStart() const        { return m_bFormatIgnoreStart; }
+    bool IsFormatIgnoreEnd  () const        { return m_bFormatIgnoreEnd  ; }
+    void SetFormatIgnoreStart(bool bFlag)   { m_bFormatIgnoreStart = bFlag; }
+    void SetFormatIgnoreEnd  (bool bFlag)   { m_bFormatIgnoreEnd   = bFlag; }
 
     inline const SfxPoolItem& GetAttr() const;
     inline       SfxPoolItem& GetAttr();

sw/source/core/inc/rolbck.hxx
@@ -122,6 +122,8 @@ class SwHistorySetTxt : public SwHistoryHint
     const sal_uLong m_nNodeIndex;
     const xub_StrLen m_nStart;
     const xub_StrLen m_nEnd;
+    bool m_bFormatIgnoreStart : 1;
+    bool m_bFormatIgnoreEnd   : 1;
 
 public:
     SwHistorySetTxt( SwTxtAttr* pTxtHt, sal_uLong nNode );

sw/source/core/layout/frmtool.cxx
@@ -1277,8 +1277,7 @@ void _InsertCnt( SwLayoutFrm *pLay, SwDoc *pDoc,
         if ( pNd->IsCntntNode() )
         {
             SwCntntNode* pNode = (SwCntntNode*)pNd;
-            pFrm = pNode->IsTxtNode() ? new SwTxtFrm( (SwTxtNode*)pNode, pLay ) :
-                                        pNode->MakeFrm( pLay );
+            pFrm = pNode->MakeFrm(pLay);
             if( pPageMaker )
                 pPageMaker->CheckInsert( nIndex );
 

sw/source/core/layout/laycache.cxx
@@ -843,8 +843,9 @@ bool SwLayHelper::CheckInsert( sal_uLong nNodeIndex )
                         }
                         else
                         {
-                            SwTxtFrm *pNew = new SwTxtFrm( ((SwTxtFrm*)rpFrm)->
-                                                            GetTxtNode(), rpFrm );
+                            SwTxtFrm *const pNew = static_cast<SwTxtFrm*>(
+                                static_cast<SwTxtFrm*>(rpFrm)
+                                    ->GetTxtNode()->MakeFrm(rpFrm));
                             pNew->ManipOfst( nOfst );
                             pNew->SetFollow( ((SwTxtFrm*)rpFrm)->GetFollow() );
                             ((SwTxtFrm*)rpFrm)->SetFollow( pNew );

sw/source/core/text/itratr.cxx
@@ -311,12 +311,26 @@ xub_StrLen SwAttrIter::GetNextAttr( ) const
     xub_StrLen nNext = STRING_LEN;
     if( pHints )
     {
-        if (pHints->GetStartCount() > nStartIndex) // Gibt es noch Starts?
-           nNext = (*pHints->GetStart(nStartIndex)->GetStart());
-        if (pHints->GetEndCount() > nEndIndex) // Gibt es noch Enden?
+        // are there attribute starts left?
+        for (sal_uInt16 i = nStartIndex; i < pHints->GetStartCount(); ++i)
         {
-            xub_StrLen nNextEnd = (*pHints->GetEnd(nEndIndex)->GetAnyEnd());
-            if ( nNextEnd<nNext ) nNext = nNextEnd; // Wer ist naeher?
+            SwTxtAttr *const pAttr(pHints->GetStart(i));
+            if (!pAttr->IsFormatIgnoreStart())
+            {
+                nNext = *pAttr->GetStart();
+                break;
+            }
+        }
+        // are there attribute ends left?
+        for (sal_uInt16 i = nEndIndex; i < pHints->GetEndCount(); ++i)
+        {
+            SwTxtAttr *const pAttr(pHints->GetEnd(i));
+            if (!pAttr->IsFormatIgnoreEnd())
+            {
+                xub_StrLen const nNextEnd = *pAttr->GetAnyEnd();
+                nNext = std::min(nNext, nNextEnd); // pick nearest one
+                break;
+            }
         }
     }
     if (m_pTxtNode!=NULL) {

zhirshya/lo-tblr

commit 2c574321f68324e9e2ee73f04fe76c4a4501891c

fdo#74790: sw: Compare Document: fix assertions about RSID-only hints
For ordinary documents the SwTxtNode::MakeFrm() would set the
m_bFormatIgnoreStart/End flags of the RSID-only hints, but the document
loaded by Compare Document (and Merge Document) is special because it
is loaded, there are editing operations on it, but it has no layout,
so the assertions about these flags trigger.

(regression from 6db39dbd7378351f6476f6db25eb7110c9cfb291)

Change-Id: I8a6e02b68f22e609640adbe93ff194e4081d9856

sw/inc/ndtxt.hxx
@@ -277,6 +277,9 @@ public:
     // delete all attributes from SwpHintsArray.
     void    ClearSwpHintsArr( bool bDelFields );
 
+    /// initialize the hints after file loading (which takes shortcuts)
+    void    FileLoadedInitHints();
+
     /// Insert pAttr into hints array. @return true iff inserted successfully
     bool    InsertHint( SwTxtAttr * const pAttr,
                   const SetAttrMode nMode = nsSetAttrMode::SETATTR_DEFAULT );

sw/source/core/doc/doccomp.cxx
@@ -1757,11 +1757,22 @@ void SwCompareData::SetRedlinesToDoc( bool bUseDocInfo )
     }
 }
 
+static bool lcl_MergePortions(SwNode *const& pNode, void *)
+{
+    if (pNode->IsTxtNode())
+    {
+        pNode->GetTxtNode()->FileLoadedInitHints();
+    }
+    return true;
+}
+
 // Returns (the difference count?) if something is different
 long SwDoc::CompareDoc( const SwDoc& rDoc )
 {
     if( &rDoc == this )
         return 0;
+
+    const_cast<SwDoc&>(rDoc).GetNodes().ForEach(&lcl_MergePortions);
 
     long nRet = 0;
 
@@ -2003,6 +2014,8 @@ long SwDoc::MergeDoc( const SwDoc& rDoc )
         return 0;
 
     long nRet = 0;
+
+    const_cast<SwDoc&>(rDoc).GetNodes().ForEach(&lcl_MergePortions);
 
     GetIDocumentUndoRedo().StartUndo(UNDO_EMPTY, NULL);
 

sw/source/core/txtnode/ndtxt.cxx
@@ -255,14 +255,19 @@ SwTxtNode::~SwTxtNode()
     InitSwParaStatistics( false );
 }
 
-SwCntntFrm *SwTxtNode::MakeFrm( SwFrm* pSib )
+void SwTxtNode::FileLoadedInitHints()
 {
-    // fdo#52028: ODF file import does not result in MergePortions being called
-    // for every attribute, since that would be inefficient.  So call it here.
     if (m_pSwpHints)
     {
         m_pSwpHints->MergePortions(*this);
     }
+}
+
+SwCntntFrm *SwTxtNode::MakeFrm( SwFrm* pSib )
+{
+    // fdo#52028: ODF file import does not result in MergePortions being called
+    // for every attribute, since that would be inefficient.  So call it here.
+    FileLoadedInitHints();
     SwCntntFrm *pFrm = new SwTxtFrm( this, pSib );
     return pFrm;
 }