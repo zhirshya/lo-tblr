LibreOffice/core

commit 004a29b9ac66f68af5ea12a2303a4b2be77d8145

Fixed a crash: GDIMetafile was deleted twice

svtools/source/graphic/grfcache.cxx
@@ -371,7 +371,7 @@ void GraphicCacheEntry::GraphicObjectWasSwappedOut( const GraphicObject& /*rObj*
     if( mbSwappedAll )
     {
         delete mpBmpEx, mpBmpEx = NULL;
-        delete mpMtf, mpMtf = NULL;
+        mpMtf = NULL; // No need to delete it as it has already been dereferenced
         delete mpAnimation, mpAnimation = NULL;
     }
 }

zhirshya/lo-tblr

commit 1e01813f8fed8febc6c3814da1f77435cc3547f2

fix memory leak on pasting metafiles into writer
copying from mspaint to writer and then deleting the image in a loop will
eventually exhaust all memory.

regression since 004a29b9ac66f68af5ea12a2303a4b2be77d8145

Change-Id: I381285cda3823de7df0c1725a339943caf9536fe

svtools/source/graphic/grfcache.cxx
@@ -367,7 +367,7 @@ void GraphicCacheEntry::GraphicObjectWasSwappedOut( const GraphicObject& /*rObj*
     if( mbSwappedAll )
     {
         delete mpBmpEx, mpBmpEx = NULL;
-        mpMtf = NULL; // No need to delete it as it has already been dereferenced
+        delete mpMtf, mpMtf = NULL;
         delete mpAnimation, mpAnimation = NULL;
 
         // #119176# also reset SvgData