LibreOffice/core

commit 263153842741d7ce21cc0bf1c5296a55a1138024

String to OUString
Change-Id: I64f31d8a0bb02a2ecd8fcc993c90ca76923b35fb
Reviewed-on: https://gerrit.libreoffice.org/4924
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

sw/source/core/fields/ddefld.cxx
@@ -17,7 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <comphelper/string.hxx>
 #include <sfx2/linkmgr.hxx>
 #include <doc.hxx>
 #include <editsh.hxx>
@@ -228,8 +227,8 @@ sal_Bool SwIntrnlRefLink::IsInRange( sal_uLong nSttNd, sal_uLong nEndNd,
     return sal_False;
 }
 
-SwDDEFieldType::SwDDEFieldType(const String& rName,
-                                const String& rCmd, sal_uInt16 nUpdateType )
+SwDDEFieldType::SwDDEFieldType(const OUString& rName,
+                               const OUString& rCmd, sal_uInt16 nUpdateType )
     : SwFieldType( RES_DDEFLD ),
     aName( rName ), pDoc( 0 ), nRefCnt( 0 )
 {
@@ -255,21 +254,22 @@ SwFieldType* SwDDEFieldType::Copy() const
     return pType;
 }
 
-const OUString& SwDDEFieldType::GetName() const
+OUString SwDDEFieldType::GetName() const
 {
     return aName;
 }
 
-void SwDDEFieldType::SetCmd( const String& rStr )
+void SwDDEFieldType::SetCmd( OUString aStr )
 {
-    String sCmd( rStr );
-    xub_StrLen nPos;
-    while( STRING_NOTFOUND != (nPos = sCmd.SearchAscii( "  " )) )
-        sCmd.Erase( nPos, 1 );
-    refLink->SetLinkSourceName( sCmd );
+    sal_Int32 nIndex = 0;
+    do
+    {
+        aStr = aStr.replaceFirst("  ", " ", &nIndex);
+    } while (nIndex>=0);
+    refLink->SetLinkSourceName( aStr );
 }
 
-String SwDDEFieldType::GetCmd() const
+OUString SwDDEFieldType::GetCmd() const
 {
     return refLink->GetLinkSourceName();
 }
@@ -312,12 +312,12 @@ void SwDDEFieldType::_RefCntChgd()
 
 bool SwDDEFieldType::QueryValue( uno::Any& rVal, sal_uInt16 nWhichId ) const
 {
-    sal_uInt8 nPart = 0;
+    sal_Int32 nPart = -1;
     switch( nWhichId )
     {
-    case FIELD_PROP_PAR2:      nPart = 3; break;
-    case FIELD_PROP_PAR4:      nPart = 2; break;
-    case FIELD_PROP_SUBTYPE:   nPart = 1; break;
+    case FIELD_PROP_PAR2:      nPart = 2; break;
+    case FIELD_PROP_PAR4:      nPart = 1; break;
+    case FIELD_PROP_SUBTYPE:   nPart = 0; break;
     case FIELD_PROP_BOOL1:
         {
             sal_Bool bSet = GetType() == sfx2::LINKUPDATE_ALWAYS ? sal_True : sal_False;
@@ -325,46 +325,50 @@ bool SwDDEFieldType::QueryValue( uno::Any& rVal, sal_uInt16 nWhichId ) const
         }
         break;
     case FIELD_PROP_PAR5:
-        rVal <<= OUString(aExpansion);
-    break;
+        rVal <<= aExpansion;
+        break;
     default:
         OSL_FAIL("illegal property");
     }
-    if( nPart )
-        rVal <<= OUString(GetCmd().GetToken(nPart-1, sfx2::cTokenSeparator));
+    if ( nPart>=0 )
+        rVal <<= GetCmd().getToken(nPart, sfx2::cTokenSeparator);
     return true;
 }
 
 bool SwDDEFieldType::PutValue( const uno::Any& rVal, sal_uInt16 nWhichId )
 {
-    sal_uInt8 nPart = 0;
+    sal_Int32 nPart = -1;
     switch( nWhichId )
     {
-    case FIELD_PROP_PAR2:      nPart = 3; break;
-    case FIELD_PROP_PAR4:      nPart = 2; break;
-    case FIELD_PROP_SUBTYPE:   nPart = 1; break;
+    case FIELD_PROP_PAR2:      nPart = 2; break;
+    case FIELD_PROP_PAR4:      nPart = 1; break;
+    case FIELD_PROP_SUBTYPE:   nPart = 0; break;
     case FIELD_PROP_BOOL1:
         SetType( static_cast<sal_uInt16>(*(sal_Bool*)rVal.getValue() ?
                                      sfx2::LINKUPDATE_ALWAYS :
                                      sfx2::LINKUPDATE_ONCALL ) );
         break;
     case FIELD_PROP_PAR5:
-    {
-        OUString sTemp;
-        rVal >>= sTemp;
-        aExpansion = sTemp;
-    }
-    break;
+        rVal >>= aExpansion;
+        break;
     default:
         OSL_FAIL("illegal property");
     }
-    if( nPart )
+    if( nPart>=0 )
     {
-        String sTmp, sCmd( GetCmd() );
-        while(3 > comphelper::string::getTokenCount(sCmd, sfx2::cTokenSeparator))
-            sCmd += sfx2::cTokenSeparator;
-        sCmd.SetToken( nPart-1, sfx2::cTokenSeparator, ::GetString( rVal, sTmp ) );
-        SetCmd( sCmd );
+        const OUString sOldCmd( GetCmd() );
+        OUString sNewCmd;
+        sal_Int32 nIndex = 0;
+        for (sal_Int32 i=0; i<3; ++i)
+        {
+            OUString sToken = sOldCmd.getToken(0, sfx2::cTokenSeparator, nIndex);
+            if (i==nPart)
+            {
+                rVal >>= sToken;
+            }
+            sNewCmd += sToken + OUString(sfx2::cTokenSeparator);
+        }
+        SetCmd( sNewCmd );
     }
     return true;
 }
@@ -380,17 +384,16 @@ SwDDEField::~SwDDEField()
         ((SwDDEFieldType*)GetTyp())->Disconnect();
 }
 
-String SwDDEField::Expand() const
+OUString SwDDEField::Expand() const
 {
-    xub_StrLen nPos;
-    String aStr(comphelper::string::remove(((SwDDEFieldType*)GetTyp())->GetExpansion(), '\r'));
-
-    while( (nPos = aStr.Search( '\t' )) != STRING_NOTFOUND )
-        aStr.SetChar( nPos, ' ' );
-    while( (nPos = aStr.Search( '\n' )) != STRING_NOTFOUND )
-        aStr.SetChar( nPos, '|' );
-    if( aStr.Len() && ( aStr.GetChar( aStr.Len()-1 ) == '|') )
-        aStr.Erase( aStr.Len()-1, 1 );
+    OUString aStr = ((SwDDEFieldType*)GetTyp())->GetExpansion();
+    aStr = aStr.replaceAll("\r", OUString());
+    aStr = aStr.replaceAll("\t", " ");
+    aStr = aStr.replaceAll("\n", "|");
+    if (aStr.endsWith("|"))
+    {
+        return aStr.copy(0, aStr.getLength()-1);
+    }
     return aStr;
 }
 
@@ -400,7 +403,7 @@ SwField* SwDDEField::Copy() const
 }
 
 /// get field type name
-const OUString& SwDDEField::GetPar1() const
+OUString SwDDEField::GetPar1() const
 {
     return ((const SwDDEFieldType*)GetTyp())->GetName();
 }

sw/inc/authfld.hxx
@@ -27,16 +27,16 @@
 
 class SwAuthEntry
 {
-    String      aAuthFields[AUTH_FIELD_END];
+    OUString        aAuthFields[AUTH_FIELD_END];
     sal_uInt16      nRefCount;
 public:
     SwAuthEntry() : nRefCount(0){}
     SwAuthEntry( const SwAuthEntry& rCopy );
     sal_Bool            operator==(const SwAuthEntry& rComp);
 
-    inline const String&    GetAuthorField(ToxAuthorityField ePos)const;
+    inline OUString         GetAuthorField(ToxAuthorityField ePos) const;
     inline void             SetAuthorField(ToxAuthorityField ePos,
-                                            const String& rField);
+                                            const OUString& rField);
 
     void            AddRef()                { ++nRefCount; }
     void            RemoveRef()             { --nRefCount; }
@@ -67,7 +67,7 @@ class SW_DLLPUBLIC SwAuthorityFieldType : public SwFieldType
     sal_Bool                m_bIsSequence :1;
     sal_Bool                m_bSortByDocument :1;
     LanguageType            m_eLanguage;
-    String                  m_sSortAlgorithm;
+    OUString                m_sSortAlgorithm;
 
 protected:
 virtual void Modify( const SfxPoolItem* pOld, const SfxPoolItem *pNew );
@@ -84,7 +84,7 @@ public:
     inline void         SetDoc(SwDoc* pNewDoc)              { m_pDoc = pNewDoc; }
     SwDoc*              GetDoc(){ return m_pDoc; }
     void                RemoveField(sal_IntPtr nHandle);
-    sal_IntPtr          AddField(const String& rFieldContents);
+    sal_IntPtr          AddField(const OUString& rFieldContents);
     sal_Bool            AddField(sal_IntPtr nHandle);
     void                DelSequenceArray()
                         {
@@ -93,8 +93,8 @@ public:
 
     const SwAuthEntry*  GetEntryByHandle(sal_IntPtr nHandle) const;
 
-    void                GetAllEntryIdentifiers( std::vector<String>& rToFill )const;
-    const SwAuthEntry*  GetEntryByIdentifier(const String& rIdentifier)const;
+    void                GetAllEntryIdentifiers( std::vector<OUString>& rToFill ) const;
+    const SwAuthEntry*  GetEntryByIdentifier(const OUString& rIdentifier) const;
 
     bool                ChangeEntryContent(const SwAuthEntry* pNewEntry);
     // import interface
@@ -130,14 +130,14 @@ public:
     void                SetSortKeys(sal_uInt16 nKeyCount, SwTOXSortKey nKeys[]);
 
     //initui.cxx
-    static const String&    GetAuthFieldName(ToxAuthorityField eType);
-    static const String&    GetAuthTypeName(ToxAuthorityType eType);
+    static OUString     GetAuthFieldName(ToxAuthorityField eType);
+    static OUString     GetAuthTypeName(ToxAuthorityType eType);
 
     LanguageType    GetLanguage() const {return m_eLanguage;}
     void            SetLanguage(LanguageType nLang)  {m_eLanguage = nLang;}
 
-    const String&   GetSortAlgorithm()const {return m_sSortAlgorithm;}
-    void            SetSortAlgorithm(const String& rSet) {m_sSortAlgorithm = rSet;}
+    OUString        GetSortAlgorithm() const {return m_sSortAlgorithm;}
+    void            SetSortAlgorithm(const OUString& rSet) {m_sSortAlgorithm = rSet;}
 
 };
 
@@ -146,15 +146,15 @@ class SwAuthorityField : public SwField
     sal_IntPtr          m_nHandle;
     mutable sal_IntPtr  m_nTempSequencePos;
 
-    virtual String      Expand() const;
+    virtual OUString    Expand() const;
     virtual SwField*    Copy() const;
 
 public:
-    SwAuthorityField(SwAuthorityFieldType* pType, const String& rFieldContents);
+    SwAuthorityField(SwAuthorityFieldType* pType, const OUString& rFieldContents);
     SwAuthorityField(SwAuthorityFieldType* pType, sal_IntPtr nHandle);
     ~SwAuthorityField();
 
-    const String&       GetFieldText(ToxAuthorityField eField) const;
+    OUString            GetFieldText(ToxAuthorityField eField) const;
 
     virtual void        SetPar1(const OUString& rStr);
     virtual SwFieldType* ChgTyp( SwFieldType* );
@@ -164,16 +164,16 @@ public:
 
     sal_IntPtr          GetHandle() const       { return m_nHandle; }
 
-    virtual String GetDescription() const;
+    virtual OUString GetDescription() const;
 };
 
 // --- inlines -----------------------------------------------------------
-inline const String&    SwAuthEntry::GetAuthorField(ToxAuthorityField ePos)const
+inline OUString SwAuthEntry::GetAuthorField(ToxAuthorityField ePos) const
 {
     OSL_ENSURE(AUTH_FIELD_END > ePos, "wrong index");
     return aAuthFields[ePos];
 }
-inline void SwAuthEntry::SetAuthorField(ToxAuthorityField ePos, const String& rField)
+inline void SwAuthEntry::SetAuthorField(ToxAuthorityField ePos, const OUString& rField)
 {
     OSL_ENSURE(AUTH_FIELD_END > ePos, "wrong index");
     if(AUTH_FIELD_END > ePos)

sw/inc/chpfld.hxx
@@ -51,9 +51,12 @@ class SW_DLLPUBLIC SwChapterField : public SwField
 {
     friend class SwChapterFieldType;
     sal_uInt8 nLevel;
-    String sTitle, sNumber, sPre, sPost;
+    OUString sTitle;
+    OUString sNumber;
+    OUString sPre;
+    OUString sPost;
 
-    virtual String   Expand() const;
+    virtual OUString Expand() const;
     virtual SwField* Copy() const;
 
 public:
@@ -68,16 +71,16 @@ public:
     inline sal_uInt8 GetLevel() const;
     inline void SetLevel(sal_uInt8);
 
-    inline const String& GetNumber() const;
-    inline const String& GetTitle() const;
+    inline OUString      GetNumber() const;
+    inline OUString      GetTitle() const;
     virtual bool         QueryValue( com::sun::star::uno::Any& rVal, sal_uInt16 nWhich ) const;
     virtual bool         PutValue( const com::sun::star::uno::Any& rVal, sal_uInt16 nWhich );
 };
 
 inline sal_uInt8 SwChapterField::GetLevel() const   { return nLevel; }
 inline void SwChapterField::SetLevel(sal_uInt8 nLev) { nLevel = nLev; }
-inline const String& SwChapterField::GetNumber() const { return sNumber; }
-inline const String& SwChapterField::GetTitle() const { return sTitle; }
+inline OUString SwChapterField::GetNumber() const { return sNumber; }
+inline OUString SwChapterField::GetTitle() const { return sTitle; }
 
 #endif // SW_CHPFLD_HXX
 

sw/inc/dbfld.hxx
@@ -35,21 +35,21 @@ class SW_DLLPUBLIC SwDBFieldType : public SwValueFieldType
 {
     SwDBData    aDBData;        //
     OUString sName;          ///< only used in ::GetName() !
-    String      sColumn;
+    OUString sColumn;
     long        nRefCnt;
 
 public:
 
-    SwDBFieldType(SwDoc* pDocPtr, const String& rColumnName, const SwDBData& rDBData);
+    SwDBFieldType(SwDoc* pDocPtr, const OUString& rColumnName, const SwDBData& rDBData);
     ~SwDBFieldType();
 
-    virtual const OUString& GetName() const;
+    virtual OUString GetName() const;
     virtual SwFieldType*  Copy() const;
 
     inline void     AddRef() { nRefCnt++; }
     void            ReleaseRef();
 
-    const String&   GetColumnName() const {return sColumn;}
+    OUString        GetColumnName() const {return sColumn;}
     const SwDBData& GetDBData() const {return aDBData;}
 
     virtual bool        QueryValue( com::sun::star::uno::Any& rVal, sal_uInt16 nWhich ) const;
@@ -70,7 +70,7 @@ class SW_DLLPUBLIC SwDBField : public SwValueField
     bool    bValidValue     : 1;
     bool    bInitialized    : 1;
 
-    virtual String      Expand() const;
+    virtual OUString    Expand() const;
     virtual SwField*    Copy() const;
 
 public:
@@ -80,12 +80,12 @@ public:
     virtual SwFieldType*    ChgTyp( SwFieldType* );
 
     /// Current text.
-    inline  void        SetExpansion(const String& rStr);
+    inline  void        SetExpansion(const OUString& rStr);
 
     virtual sal_uInt16      GetSubType() const;
     virtual void        SetSubType(sal_uInt16 nType);
 
-    virtual String      GetFieldName() const;
+    virtual OUString    GetFieldName() const;
 
     /// For calculations in expressions.
     void                ChgValue( double d, bool bVal );
@@ -96,7 +96,7 @@ public:
     /// Evaluation for header and footer.
     void                ChangeExpansion( const SwFrm*, const SwTxtFld* );
     void                InitContent();
-    void                InitContent(const String& rExpansion);
+    void                InitContent(const OUString& rExpansion);
 
     inline void         ChgBodyTxtFlag( bool bIsInBody );
 
@@ -105,10 +105,10 @@ public:
     inline void         SetInitialized()        { bInitialized = true; }
 
     /// Get name.
-    virtual const OUString& GetPar1() const;
+    virtual OUString    GetPar1() const;
 
     /// access to the command string
-    const OUString& GetFieldCode() const { return sFieldCode;}
+    OUString            GetFieldCode() const { return sFieldCode;}
     void                SetFieldCode(const OUString& rStr) { sFieldCode = rStr; }
 
     /// DBName
@@ -117,7 +117,7 @@ public:
     virtual bool        PutValue( const com::sun::star::uno::Any& rVal, sal_uInt16 nWhich );
 };
 
-inline  void SwDBField::SetExpansion(const String& rStr)
+inline  void SwDBField::SetExpansion(const OUString& rStr)
     { aContent = rStr; }
 
 /// set from UpdateExpFlds (the Node-Position is known there)
@@ -146,7 +146,7 @@ public:
     SwDBData                GetDBData(SwDoc* pDoc);
     void                    SetDBData(const SwDBData& rDBData);
 
-    virtual String          GetFieldName() const;
+    virtual OUString        GetFieldName() const;
 
     virtual bool            QueryValue( com::sun::star::uno::Any& rVal, sal_uInt16 nWhich ) const;
     virtual bool            PutValue( const com::sun::star::uno::Any& rVal, sal_uInt16 nWhich );
@@ -177,9 +177,9 @@ class SW_DLLPUBLIC SwDBNextSetField : public SwDBNameInfField
 
 public:
     SwDBNextSetField( SwDBNextSetFieldType*,
-                      const String& rCond, const String& rDummy, const SwDBData& rDBData);
+                      const OUString& rCond, const OUString& rDummy, const SwDBData& rDBData);
 
-    virtual String          Expand() const;
+    virtual OUString        Expand() const;
     virtual SwField*        Copy() const;
 
     void                    Evaluate(SwDoc*);
@@ -187,7 +187,7 @@ public:
     inline bool             IsCondValid() const;
 
     // Condition
-    virtual const OUString&   GetPar1() const;
+    virtual OUString    GetPar1() const;
     virtual void        SetPar1(const OUString& rStr);
     virtual bool        QueryValue( com::sun::star::uno::Any& rVal, sal_uInt16 nWhich ) const;
     virtual bool        PutValue( const com::sun::star::uno::Any& rVal, sal_uInt16 nWhich );
@@ -219,13 +219,13 @@ public:
 class SwDBNumSetField : public SwDBNameInfField
 {
     OUString  aCond;
-    String  aPar2;
+    OUString  aPar2;
     bool    bCondValid;
 
 public:
-    SwDBNumSetField(SwDBNumSetFieldType*, const String& rCond, const String& rDBNum, const SwDBData& rDBData);
+    SwDBNumSetField(SwDBNumSetFieldType*, const OUString& rCond, const OUString& rDBNum, const SwDBData& rDBData);
 
-    virtual String          Expand() const;
+    virtual OUString        Expand() const;
     virtual SwField*        Copy() const;
 
     inline bool             IsCondValid() const;
@@ -233,7 +233,7 @@ public:
     void                    Evaluate(SwDoc*);
 
     // Condition
-    virtual const OUString&   GetPar1() const;
+    virtual OUString        GetPar1() const;
     virtual void            SetPar1(const OUString& rStr);
 
     // Number of data record.
@@ -261,7 +261,7 @@ class SwDBNameFieldType : public SwFieldType
 public:
     SwDBNameFieldType(SwDoc*);
 
-    String                  Expand(sal_uLong) const;
+    OUString                Expand(sal_uLong) const;
     virtual SwFieldType*    Copy() const;
 };
 
@@ -274,7 +274,7 @@ class SW_DLLPUBLIC SwDBNameField : public SwDBNameInfField
 public:
     SwDBNameField(SwDBNameFieldType*, const SwDBData& rDBData, sal_uLong nFmt = 0);
 
-    virtual String   Expand() const;
+    virtual OUString Expand() const;
     virtual SwField* Copy() const;
     virtual bool        QueryValue( com::sun::star::uno::Any& rVal, sal_uInt16 nWhich ) const;
     virtual bool        PutValue( const com::sun::star::uno::Any& rVal, sal_uInt16 nWhich );
@@ -303,7 +303,7 @@ class SW_DLLPUBLIC SwDBSetNumberField : public SwDBNameInfField
 public:
     SwDBSetNumberField(SwDBSetNumberFieldType*, const SwDBData& rDBData, sal_uLong nFmt = 0);
 
-    virtual String  Expand() const;
+    virtual OUString Expand() const;
     virtual         SwField* Copy() const;
     void            Evaluate(SwDoc*);
 

sw/inc/ddefld.hxx
@@ -32,7 +32,7 @@ class SwDoc;
 class SW_DLLPUBLIC SwDDEFieldType : public SwFieldType
 {
     OUString aName;
-    String aExpansion;
+    OUString aExpansion;
 
     ::sfx2::SvBaseLinkRef refLink;
     SwDoc* pDoc;
@@ -44,22 +44,22 @@ class SW_DLLPUBLIC SwDDEFieldType : public SwFieldType
     SW_DLLPRIVATE void _RefCntChgd();
 
 public:
-    SwDDEFieldType( const String& rName, const String& rCmd,
+    SwDDEFieldType( const OUString& rName, const OUString& rCmd,
                     sal_uInt16 = sfx2::LINKUPDATE_ONCALL );
     ~SwDDEFieldType();
 
-    const String& GetExpansion() const          { return aExpansion; }
-    void SetExpansion( const String& rStr )     { aExpansion = rStr,
+    OUString GetExpansion() const               { return aExpansion; }
+    void SetExpansion( const OUString& rStr )   { aExpansion = rStr;
                                                   bCRLFFlag = sal_False; }
 
     virtual SwFieldType* Copy() const;
-    virtual const OUString& GetName() const;
+    virtual OUString GetName() const;
 
     virtual bool QueryValue( com::sun::star::uno::Any& rVal, sal_uInt16 nWhich ) const;
     virtual bool PutValue( const com::sun::star::uno::Any& rVal, sal_uInt16 nWhich );
 
-    String GetCmd() const;
-    void SetCmd( const String& rStr );
+    OUString GetCmd() const;
+    void SetCmd( OUString aStr );
 
     sal_uInt16 GetType() const          { return refLink->GetUpdateMode();  }
     void SetType( sal_uInt16 nType )    { refLink->SetUpdateMode( nType );  }
@@ -90,7 +90,7 @@ public:
 class SwDDEField : public SwField
 {
 private:
-    virtual String   Expand() const;
+    virtual OUString Expand() const;
     virtual SwField* Copy() const;
 
 public:
@@ -99,7 +99,7 @@ public:
 
     /** Get parameter via types.
      Name cannot be changed. */
-    virtual const OUString& GetPar1() const;
+    virtual OUString  GetPar1() const;
 
     // Command
     virtual OUString  GetPar2() const;

zhirshya/lo-tblr

commit 4b43463e0399ca7b3e76eb2f621feda8ed25520c

fdo#78332: sw: fix separators in SwDDEFieldType::PutValue()
Setting 3 separators instead of 2 does not work.

(regression from 263153842741d7ce21cc0bf1c5296a55a1138024)

Change-Id: Id0b4649404b8a49a7f89a237c46106bce4e146ff

sw/source/core/fields/ddefld.cxx
@@ -366,7 +366,8 @@ bool SwDDEFieldType::PutValue( const uno::Any& rVal, sal_uInt16 nWhichId )
             {
                 rVal >>= sToken;
             }
-            sNewCmd += sToken + OUString(sfx2::cTokenSeparator);
+            sNewCmd += (i < 2)
+                ? sToken + OUString(sfx2::cTokenSeparator) : sToken;
         }
         SetCmd( sNewCmd );
     }