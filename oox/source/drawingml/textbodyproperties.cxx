LibreOffice/core

commit 53c376d35b7223d53e8c9403390afe53d1f69089

bnc#870237: wrong text position in grouped list
In grouped list text area does not cover the whole
shape but just a part of it at the top.
To get the same visual effect modify text distance
attribute.

Change-Id: I32f30d0afbc1975f940c4562ec65f46596e97060

oox/source/drawingml/textbodyproperties.cxx
@@ -81,8 +81,10 @@ void TextBodyProperties::pushRotationAdjustments( sal_Int32 nRotation )
         // Hack for n#760986
         // TODO: Preferred method would be to have a textbox on top
         // of the shape and the place it according to the (off,ext)
-        if( nOff == 0 && moTextOffX && mbAnchorCtr ) nVal = *moTextOffX;
-        if( nOff == 1 && moTextOffY && mbAnchorCtr ) nVal = *moTextOffY;
+        if( nOff == 0 && moTextOffLeft && mbAnchorCtr ) nVal = *moTextOffLeft;
+        if( nOff == 1 && moTextOffUpper ) nVal = *moTextOffLeft;
+        if( nOff == 2 && moTextOffRight && mbAnchorCtr ) nVal = *moTextOffRight;
+        if( nOff == 3 && moTextOffLower ) nVal = *moTextOffLower;
         if( nVal < 0 ) nVal = 0;
 
         if( moInsets[i] )

oox/source/drawingml/transform2dcontext.cxx
@@ -62,12 +62,29 @@ ContextHandlerRef Transform2DContext::onCreateContext( sal_Int32 aElementToken, 
                     OUString sXValue = rAttribs.getString( XML_x ).get();
                     OUString sYValue = rAttribs.getString( XML_y ).get();
                     if( !sXValue.isEmpty() )
-                        mrShape.getTextBody()->getTextProperties().moTextOffX = GetCoordinate( sXValue.toInt32() - mrShape.getPosition().X );
+                        mrShape.getTextBody()->getTextProperties().moTextOffUpper = GetCoordinate( sXValue.toInt32() - mrShape.getPosition().X );
                     if( !sYValue.isEmpty() )
-                        mrShape.getTextBody()->getTextProperties().moTextOffY = GetCoordinate( sYValue.toInt32() - mrShape.getPosition().Y );
+                        mrShape.getTextBody()->getTextProperties().moTextOffLeft = GetCoordinate( sYValue.toInt32() - mrShape.getPosition().Y );
                 }
                 break;
             case A_TOKEN( ext ):
+                {
+                    const OUString sXValue = rAttribs.getString( XML_cx ).get();
+                    const OUString sYValue = rAttribs.getString( XML_cy ).get();
+                    if( !sXValue.isEmpty() )
+                    {
+                        mrShape.getTextBody()->getTextProperties().moTextOffRight = GetCoordinate(mrShape.getSize().Width - sXValue.toInt32());
+                        if( mrShape.getTextBody()->getTextProperties().moTextOffLeft )
+                           *mrShape.getTextBody()->getTextProperties().moTextOffRight -=  *mrShape.getTextBody()->getTextProperties().moTextOffLeft;
+                    }
+                    if( !sYValue.isEmpty() )
+                    {
+                        mrShape.getTextBody()->getTextProperties().moTextOffLower = GetCoordinate(mrShape.getSize().Height - sYValue.toInt32());
+                        if( mrShape.getTextBody()->getTextProperties().moTextOffUpper )
+                           *mrShape.getTextBody()->getTextProperties().moTextOffLower -=  *mrShape.getTextBody()->getTextProperties().moTextOffUpper;
+
+                    }
+                }
                 break;
         }
         return 0;

sd/qa/unit/import-tests.cxx
@@ -73,6 +73,7 @@ public:
     void testN862510_4();
     void testFdo71961();
     void testMediaEmbedding();
+    void testBnc870237();
 
     CPPUNIT_TEST_SUITE(SdFiltersTest);
     CPPUNIT_TEST(testDocumentLayout);
@@ -97,6 +98,7 @@ public:
     CPPUNIT_TEST(testN862510_4);
     CPPUNIT_TEST(testFdo71961);
     CPPUNIT_TEST(testMediaEmbedding);
+    CPPUNIT_TEST(testBnc870237);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -736,6 +738,26 @@ void SdFiltersTest::testMediaEmbedding()
     CPPUNIT_ASSERT_MESSAGE( "missing media object", pMediaObj != NULL);
     CPPUNIT_ASSERT_EQUAL( OUString( "vnd.sun.star.Package:Media/button-1.wav" ), pMediaObj->getMediaProperties().getURL());
     CPPUNIT_ASSERT_EQUAL( OUString( "application/vnd.sun.star.media" ), pMediaObj->getMediaProperties().getMimeType());
+}
+
+void SdFiltersTest::testBnc870237()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(getURLFromSrc("/sd/qa/unit/data/pptx/bnc870237.pptx"));
+
+    SdDrawDocument *pDoc = xDocShRef->GetDoc();
+    CPPUNIT_ASSERT_MESSAGE( "no document", pDoc != NULL );
+    const SdrPage *pPage = pDoc->GetPage (1);
+    CPPUNIT_ASSERT_MESSAGE( "no page", pPage != NULL );
+
+    // Simulate a:ext inside dsp:txXfrm with changing the lower distance
+    const SdrObjGroup* pObj = dynamic_cast<SdrObjGroup*>( pPage->GetObj( 1 ) );
+    CPPUNIT_ASSERT_MESSAGE( "no object", pObj != NULL);
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(0), (static_cast< const SdrTextUpperDistItem& >(pObj->GetMergedItem(SDRATTR_TEXT_UPPERDIST))).GetValue());
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(9919), (static_cast< const SdrTextLowerDistItem& >(pObj->GetMergedItem(SDRATTR_TEXT_LOWERDIST))).GetValue());
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(0), (static_cast< const SdrTextRightDistItem& >(pObj->GetMergedItem(SDRATTR_TEXT_RIGHTDIST))).GetValue());
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(0), (static_cast< const SdrTextLeftDistItem& >(pObj->GetMergedItem(SDRATTR_TEXT_LEFTDIST))).GetValue());
+
+    xDocShRef->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdFiltersTest);

include/oox/drawingml/textbodyproperties.hxx
@@ -37,8 +37,10 @@ struct TextBodyProperties
     bool                                            mbAnchorCtr;
     OptValue< sal_Int32 >                           moVert;
     boost::optional< sal_Int32 >                    moInsets[4];
-    boost::optional< sal_Int32 >                    moTextOffX;
-    boost::optional< sal_Int32 >                    moTextOffY;
+    boost::optional< sal_Int32 >                    moTextOffUpper;
+    boost::optional< sal_Int32 >                    moTextOffLeft;
+    boost::optional< sal_Int32 >                    moTextOffLower;
+    boost::optional< sal_Int32 >                    moTextOffRight;
     ::com::sun::star::drawing::TextVerticalAdjust   meVA;
 
     explicit            TextBodyProperties();

zhirshya/lo-tblr

commit 70687337a3eca918e87a4902d70b4d0aee89a158

Related bnc#862510: extend handling of txXfrm
We don't actually need to check mbAnchorCtr to change
text spacing. This txXfrm workaround works only with rectangles,
because other shapes' text area can be smaller then the shape
size. So add some condition to avoid using it for
other shapes.

Plus fix typos cause regression introduced in:
53c376d35b7223d53e8c9403390afe53d1f69089

Change-Id: I87917b8e0b2bb97ae1bba773e7dda7f81682736f

oox/source/drawingml/textbodyproperties.cxx
@@ -81,9 +81,9 @@ void TextBodyProperties::pushRotationAdjustments( sal_Int32 nRotation )
         // Hack for n#760986
         // TODO: Preferred method would be to have a textbox on top
         // of the shape and the place it according to the (off,ext)
-        if( nOff == 0 && moTextOffLeft && mbAnchorCtr ) nVal = *moTextOffLeft;
-        if( nOff == 1 && moTextOffUpper ) nVal = *moTextOffLeft;
-        if( nOff == 2 && moTextOffRight && mbAnchorCtr ) nVal = *moTextOffRight;
+        if( nOff == 0 && moTextOffLeft  ) nVal = *moTextOffLeft;
+        if( nOff == 1 && moTextOffUpper ) nVal = *moTextOffUpper;
+        if( nOff == 2 && moTextOffRight ) nVal = *moTextOffRight;
         if( nOff == 3 && moTextOffLower ) nVal = *moTextOffLower;
         if( nVal < 0 ) nVal = 0;
 

oox/source/drawingml/transform2dcontext.cxx
@@ -55,37 +55,43 @@ ContextHandlerRef Transform2DContext::onCreateContext( sal_Int32 aElementToken, 
 {
     if( mbtxXfrm )
     {
-        switch( aElementToken )
+        // Workaround: only for rectangles
+        const sal_Int32 nType = mrShape.getCustomShapeProperties()->getShapePresetType();
+        if( nType == XML_rect || nType == XML_roundRect )
         {
-            case A_TOKEN( off ):
-                {
-                    OUString sXValue = rAttribs.getString( XML_x ).get();
-                    OUString sYValue = rAttribs.getString( XML_y ).get();
-                    if( !sXValue.isEmpty() )
-                        mrShape.getTextBody()->getTextProperties().moTextOffUpper = GetCoordinate( sXValue.toInt32() - mrShape.getPosition().X );
-                    if( !sYValue.isEmpty() )
-                        mrShape.getTextBody()->getTextProperties().moTextOffLeft = GetCoordinate( sYValue.toInt32() - mrShape.getPosition().Y );
-                }
-                break;
-            case A_TOKEN( ext ):
-                {
-                    const OUString sXValue = rAttribs.getString( XML_cx ).get();
-                    const OUString sYValue = rAttribs.getString( XML_cy ).get();
-                    if( !sXValue.isEmpty() )
+            switch( aElementToken )
+            {
+                case A_TOKEN( off ):
                     {
-                        mrShape.getTextBody()->getTextProperties().moTextOffRight = GetCoordinate(mrShape.getSize().Width - sXValue.toInt32());
-                        if( mrShape.getTextBody()->getTextProperties().moTextOffLeft )
-                           *mrShape.getTextBody()->getTextProperties().moTextOffRight -=  *mrShape.getTextBody()->getTextProperties().moTextOffLeft;
+                        const OUString sXValue = rAttribs.getString( XML_x ).get();
+                        const OUString sYValue = rAttribs.getString( XML_y ).get();
+                        if( !sXValue.isEmpty() && nType == XML_rect )
+                            mrShape.getTextBody()->getTextProperties().moTextOffLeft = GetCoordinate( sXValue.toInt32() - mrShape.getPosition().X );
+                        if( !sYValue.isEmpty() )
+                            mrShape.getTextBody()->getTextProperties().moTextOffUpper = GetCoordinate( sYValue.toInt32() - mrShape.getPosition().Y );
                     }
-                    if( !sYValue.isEmpty() )
+                    break;
+                case A_TOKEN( ext ):
                     {
-                        mrShape.getTextBody()->getTextProperties().moTextOffLower = GetCoordinate(mrShape.getSize().Height - sYValue.toInt32());
-                        if( mrShape.getTextBody()->getTextProperties().moTextOffUpper )
-                           *mrShape.getTextBody()->getTextProperties().moTextOffLower -=  *mrShape.getTextBody()->getTextProperties().moTextOffUpper;
+                        const OUString sXValue = rAttribs.getString( XML_cx ).get();
+                        const OUString sYValue = rAttribs.getString( XML_cy ).get();
 
+                        if( !sXValue.isEmpty() && nType == XML_rect )
+                        {
+                            mrShape.getTextBody()->getTextProperties().moTextOffRight = GetCoordinate(mrShape.getSize().Width - sXValue.toInt32());
+                            if( mrShape.getTextBody()->getTextProperties().moTextOffLeft )
+                               *mrShape.getTextBody()->getTextProperties().moTextOffRight -=  *mrShape.getTextBody()->getTextProperties().moTextOffLeft;
+                        }
+                        if( !sYValue.isEmpty() )
+                        {
+                            mrShape.getTextBody()->getTextProperties().moTextOffLower = GetCoordinate(mrShape.getSize().Height - sYValue.toInt32());
+                            if( mrShape.getTextBody()->getTextProperties().moTextOffUpper )
+                               *mrShape.getTextBody()->getTextProperties().moTextOffLower -=  *mrShape.getTextBody()->getTextProperties().moTextOffUpper;
+
+                        }
                     }
-                }
-                break;
+                    break;
+            }
         }
         return 0;
     }

sd/qa/unit/import-tests.cxx
@@ -70,7 +70,6 @@ public:
     void testStrictOOXML();
     void testN862510_1();
     void testN862510_2();
-    void testN862510_3();
     void testN862510_4();
     void testFdo71961();
     void testMediaEmbedding();
@@ -78,6 +77,7 @@ public:
     void testBnc870233_1();
     void testBnc870233_2();
     void testBnc880763();
+    void testBnc862510_5();
 
     CPPUNIT_TEST_SUITE(SdFiltersTest);
     CPPUNIT_TEST(testDocumentLayout);
@@ -98,7 +98,6 @@ public:
     CPPUNIT_TEST(testStrictOOXML);
     CPPUNIT_TEST(testN862510_1);
     CPPUNIT_TEST(testN862510_2);
-    CPPUNIT_TEST(testN862510_3);
     CPPUNIT_TEST(testN862510_4);
     CPPUNIT_TEST(testFdo71961);
     CPPUNIT_TEST(testMediaEmbedding);
@@ -106,6 +105,7 @@ public:
     CPPUNIT_TEST(testBnc870233_1);
     CPPUNIT_TEST(testBnc870233_2);
     CPPUNIT_TEST(testBnc880763);
+    CPPUNIT_TEST(testBnc862510_5);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -300,25 +300,6 @@ void SdFiltersTest::testN862510_2()
         SdrObjCustomShape *pObj = dynamic_cast<SdrObjCustomShape *>( pGrpObj->GetSubList()->GetObj( 0 ) );
         CPPUNIT_ASSERT( pObj );
         CPPUNIT_ASSERT_MESSAGE( "Wrong Text Rotation!", pObj->GetExtraTextRotation( true ) == 90 );
-    }
-
-    xDocShRef->DoClose();
-}
-
-void SdFiltersTest::testN862510_3()
-{
-    ::sd::DrawDocShellRef xDocShRef = loadURL( getURLFromSrc("/sd/qa/unit/data/pptx/n862510_3.pptx") );
-
-    SdDrawDocument *pDoc = xDocShRef->GetDoc();
-    CPPUNIT_ASSERT_MESSAGE( "no document", pDoc != NULL );
-    const SdrPage *pPage = pDoc->GetPage( 1 );
-    CPPUNIT_ASSERT_MESSAGE( "no page", pPage != NULL );
-    {
-        SdrObjGroup *pGrpObj = dynamic_cast<SdrObjGroup *>( pPage->GetObj( 0 ) );
-        CPPUNIT_ASSERT( pGrpObj );
-        SdrObjCustomShape *pObj = dynamic_cast<SdrObjCustomShape *>( pGrpObj->GetSubList()->GetObj( 0 ) );
-        CPPUNIT_ASSERT( pObj );
-        CPPUNIT_ASSERT_MESSAGE( "Left Spacing is wrong! check attribute anchorCtr", pObj->GetTextLeftDistance() < 30);
     }
 
     xDocShRef->DoClose();
@@ -923,6 +904,27 @@ void SdFiltersTest::testBnc880763()
     CPPUNIT_ASSERT_MESSAGE( "no object", pObj != NULL);
     CPPUNIT_ASSERT_EQUAL( sal_uInt32(0x00ff00),(static_cast< const XColorItem& >(pObj->GetMergedItem(XATTR_FILLCOLOR))).GetColorValue().GetColor());
 
+
+    xDocShRef->DoClose();
+}
+
+void SdFiltersTest::testBnc862510_5()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(getURLFromSrc("/sd/qa/unit/data/pptx/bnc862510_5.pptx"));
+    xDocShRef = saveAndReload( xDocShRef, PPTX );
+
+    SdDrawDocument *pDoc = xDocShRef->GetDoc();
+    CPPUNIT_ASSERT_MESSAGE( "no document", pDoc != NULL );
+    const SdrPage *pPage = pDoc->GetPage (1);
+    CPPUNIT_ASSERT_MESSAGE( "no page", pPage != NULL );
+
+    // Same as testBnc870237, but here we check the horizontal spacing
+    const SdrObject* pObj = dynamic_cast<SdrObject*>( pPage->GetObj( 1 ) );
+    CPPUNIT_ASSERT_MESSAGE( "no object", pObj != NULL);
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(0), (static_cast< const SdrTextUpperDistItem& >(pObj->GetMergedItem(SDRATTR_TEXT_UPPERDIST))).GetValue());
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(0), (static_cast< const SdrTextLowerDistItem& >(pObj->GetMergedItem(SDRATTR_TEXT_LOWERDIST))).GetValue());
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(7510), (static_cast< const SdrTextRightDistItem& >(pObj->GetMergedItem(SDRATTR_TEXT_RIGHTDIST))).GetValue());
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(0), (static_cast< const SdrTextLeftDistItem& >(pObj->GetMergedItem(SDRATTR_TEXT_LEFTDIST))).GetValue());
 
     xDocShRef->DoClose();
 }