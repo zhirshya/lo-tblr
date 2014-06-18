LibreOffice/core

commit 57779aa0195d77239b339c6dbe17209b3f0cc4ee

editeng: sal_Bool->bool
Change-Id: Id4174904487fc153d8e80471da7c829c52092f78

sd/source/ui/view/Outliner.cxx
@@ -343,7 +343,7 @@ void Outliner::EndSpelling (void)
             mpImpl->ReleaseOutlinerView();
         }
 
-        SetUpdateMode(sal_True);
+        SetUpdateMode(true);
     }
 
     // Before clearing the modify flag use it as a hint that
@@ -373,7 +373,7 @@ void Outliner::EndSpelling (void)
 
 
 
-sal_Bool Outliner::SpellNextDocument (void)
+bool Outliner::SpellNextDocument (void)
 {
     ::boost::shared_ptr<ViewShell> pViewShell (mpWeakViewShell.lock());
     if (pViewShell->ISA(OutlineViewShell))
@@ -401,7 +401,7 @@ sal_Bool Outliner::SpellNextDocument (void)
         ClearModifyFlag();
     }
 
-    return mbEndOfSearch ? sal_False : sal_True;
+    return mbEndOfSearch;
 
 }
 
@@ -776,7 +776,7 @@ void Outliner::DetectChange (void)
         if (pPageView != NULL)
             mpView->UnmarkAllObj (pPageView);
         mpView->SdrEndTextEdit();
-        SetUpdateMode(sal_False);
+        SetUpdateMode(false);
         OutlinerView* pOutlinerView = mpImpl->GetOutlinerView();
         if (pOutlinerView != NULL)
             pOutlinerView->SetOutputArea( Rectangle( Point(), Size(1, 1) ) );
@@ -988,7 +988,7 @@ void Outliner::ProvideNextTextObject (void)
     {
         DBG_UNHANDLED_EXCEPTION();
     }
-    SetUpdateMode(sal_False);
+    SetUpdateMode(false);
     OutlinerView* pOutlinerView = mpImpl->GetOutlinerView();
     if (pOutlinerView != NULL)
         pOutlinerView->SetOutputArea( Rectangle( Point(), Size(1, 1) ) );
@@ -1379,7 +1379,7 @@ void Outliner::EnterEditMode (sal_Bool bGrabFocus)
         // Turn on the edit mode for the text object.
         mpView->SdrBeginTextEdit(mpTextObj, pPV, mpWindow, sal_True, this, pOutlinerView, sal_True, sal_True, bGrabFocus);
 
-        SetUpdateMode(sal_True);
+        SetUpdateMode(true);
         mbFoundObject = true;
     }
 }
@@ -1581,10 +1581,10 @@ void Outliner::StartConversion( sal_Int16 nSourceLanguage,  sal_Int16 nTargetLan
 */
 void Outliner::PrepareConversion (void)
 {
-    SetUpdateMode(sal_True);
+    SetUpdateMode(true);
     if( HasConvertibleTextPortion( mnConversionLanguage ) )
     {
-        SetUpdateMode(sal_False);
+        SetUpdateMode(false);
         mbStringFound = true;
         mbMatchMayExist = true;
 
@@ -1596,7 +1596,7 @@ void Outliner::PrepareConversion (void)
     }
     else
     {
-        SetUpdateMode(sal_False);
+        SetUpdateMode(false);
     }
 }
 
@@ -1643,7 +1643,7 @@ void Outliner::EndConversion()
 
 
 
-sal_Bool Outliner::ConvertNextDocument()
+bool Outliner::ConvertNextDocument()
 {
     ::boost::shared_ptr<ViewShell> pViewShell (mpWeakViewShell.lock());
     if (pViewShell && pViewShell->ISA(OutlineViewShell) )
@@ -1796,7 +1796,7 @@ void Outliner::Implementation::ProvideOutlinerView (
                 mpOutlineView->SetControlWord(nStat);
                 if (bInsert)
                     rOutliner.InsertView( mpOutlineView );
-                rOutliner.SetUpdateMode(sal_False);
+                rOutliner.SetUpdateMode(false);
                 mpOutlineView->SetOutputArea (Rectangle (Point(), Size(1, 1)));
                 rOutliner.SetPaperSize( Size(1, 1) );
                 rOutliner.SetText(OUString(), rOutliner.GetParagraph(0));

chart2/source/controller/main/ChartController_TextEdit.cxx
@@ -192,7 +192,7 @@ void SAL_CALL ChartController::executeDispatch_InsertSpecialCharacter()
 
         // prevent flicker
         pOutlinerView->HideCursor();
-        pOutliner->SetUpdateMode(sal_False);
+        pOutliner->SetUpdateMode(false);
 
         // delete current selection by inserting empty String, so current
         // attributes become unique (sel. has to be erased anyway)
@@ -206,7 +206,7 @@ void SAL_CALL ChartController::executeDispatch_InsertSpecialCharacter()
         pOutlinerView->SetSelection(aSel);
 
         // show changes
-        pOutliner->SetUpdateMode(sal_True);
+        pOutliner->SetUpdateMode(true);
         pOutlinerView->ShowCursor();
     }
 

editeng/source/outliner/outleeng.cxx
@@ -70,7 +70,7 @@ Rectangle OutlinerEditEng::GetBulletArea( sal_Int32 nPara )
     if ( nPara < pOwner->pParaList->GetParagraphCount() )
     {
         if ( pOwner->ImplHasNumberFormat( nPara ) )
-            aBulletArea = pOwner->ImpCalcBulletArea( nPara, sal_False, sal_False );
+            aBulletArea = pOwner->ImpCalcBulletArea( nPara, false, false );
     }
     return aBulletArea;
 }

editeng/source/outliner/outlin2.cxx
@@ -46,13 +46,13 @@ using namespace ::com::sun::star::linguistic2;
 // ======================   Simple pass-through   =======================
 // ======================================================================
 
-void Outliner::SetUpdateMode( sal_Bool bUpdate )
+void Outliner::SetUpdateMode( bool bUpdate )
 {
     pEditEngine->SetUpdateMode( bUpdate );
 }
 
 
-sal_Bool Outliner::GetUpdateMode() const
+bool Outliner::GetUpdateMode() const
 {
     return pEditEngine->GetUpdateMode();
 }
@@ -62,12 +62,12 @@ const SfxItemSet& Outliner::GetEmptyItemSet() const
     return pEditEngine->GetEmptyItemSet();
 }
 
-void Outliner::EnableUndo( sal_Bool bEnable )
+void Outliner::EnableUndo( bool bEnable )
 {
     pEditEngine->EnableUndo( bEnable );
 }
 
-sal_Bool Outliner::IsUndoEnabled() const
+bool Outliner::IsUndoEnabled() const
 {
     return pEditEngine->IsUndoEnabled();
 }
@@ -98,7 +98,7 @@ void Outliner::ClearModifyFlag()
     pEditEngine->ClearModifyFlag();
 }
 
-sal_Bool Outliner::IsModified() const
+bool Outliner::IsModified() const
 {
     return pEditEngine->IsModified();
 }
@@ -144,17 +144,17 @@ void Outliner::SetDefTab( sal_uInt16 nTab )
     pEditEngine->SetDefTab( nTab );
 }
 
-sal_Bool Outliner::IsFlatMode() const
+bool Outliner::IsFlatMode() const
 {
     return pEditEngine->IsFlatMode();
 }
 
-sal_Bool Outliner::UpdateFields()
+bool Outliner::UpdateFields()
 {
     return pEditEngine->UpdateFields();
 }
 
-void Outliner::RemoveFields( sal_Bool bKeepFieldText, TypeId aType )
+void Outliner::RemoveFields( bool bKeepFieldText, TypeId aType )
 {
     pEditEngine->RemoveFields( bKeepFieldText, aType );
 }
@@ -229,7 +229,7 @@ void Outliner::SetMaxAutoPaperSize( const Size& rSz )
     pEditEngine->SetMaxAutoPaperSize( rSz );
 }
 
-sal_Bool Outliner::IsExpanded( Paragraph* pPara ) const
+bool Outliner::IsExpanded( Paragraph* pPara ) const
 {
     return pParaList->HasVisibleChildren( pPara );
 }
@@ -269,7 +269,7 @@ SfxStyleSheet* Outliner::GetStyleSheet( sal_Int32 nPara )
     return pEditEngine->GetStyleSheet( nPara );
 }
 
-sal_Bool Outliner::IsInSelectionMode() const
+bool Outliner::IsInSelectionMode() const
 {
     return pEditEngine->IsInSelectionMode();
 }
@@ -289,12 +289,12 @@ void Outliner::SetAsianCompressionMode( sal_uInt16 n )
     pEditEngine->SetAsianCompressionMode( n );
 }
 
-void Outliner::SetKernAsianPunctuation( sal_Bool b )
+void Outliner::SetKernAsianPunctuation( bool b )
 {
     pEditEngine->SetKernAsianPunctuation( b );
 }
 
-void Outliner::SetAddExtLeading( sal_Bool bExtLeading )
+void Outliner::SetAddExtLeading( bool bExtLeading )
 {
     pEditEngine->SetAddExtLeading( bExtLeading );
 }
@@ -314,7 +314,7 @@ void Outliner::InsertUndo( EditUndo* pUndo )
     pEditEngine->GetUndoManager().AddUndoAction( pUndo, false );
 }
 
-sal_Bool Outliner::IsInUndo()
+bool Outliner::IsInUndo()
 {
     return pEditEngine->IsInUndo();
 }
@@ -344,14 +344,14 @@ EESpellState Outliner::HasSpellErrors()
     return pEditEngine->HasSpellErrors();
 }
 
-sal_Bool Outliner::HasConvertibleTextPortion( LanguageType nLang )
+bool Outliner::HasConvertibleTextPortion( LanguageType nLang )
 {
     return pEditEngine->HasConvertibleTextPortion( nLang );
 }
 
-sal_Bool Outliner::ConvertNextDocument()
+bool Outliner::ConvertNextDocument()
 {
-    return sal_False;
+    return false;
 }
 
 void Outliner::SetDefaultLanguage( LanguageType eLang )
@@ -369,7 +369,7 @@ void Outliner::CompleteOnlineSpelling()
     pEditEngine->CompleteOnlineSpelling();
 }
 
-sal_Bool Outliner::HasText( const SvxSearchItem& rSearchItem )
+bool Outliner::HasText( const SvxSearchItem& rSearchItem )
 {
     return pEditEngine->HasText( rSearchItem );
 }
@@ -384,9 +384,9 @@ SfxItemPool* Outliner::GetEditTextObjectPool() const
     return pEditEngine->GetEditTextObjectPool();
 }
 
-sal_Bool Outliner::SpellNextDocument()
+bool Outliner::SpellNextDocument()
 {
-    return sal_False;
+    return false;
 }
 
 
@@ -429,28 +429,28 @@ Point Outliner::GetDocPosTopLeft( sal_Int32 nParagraph )
     return pEditEngine->GetDocPosTopLeft( nParagraph );
 }
 
-sal_Bool Outliner::IsTextPos( const Point& rPaperPos, sal_uInt16 nBorder )
+bool Outliner::IsTextPos( const Point& rPaperPos, sal_uInt16 nBorder )
 {
     return IsTextPos( rPaperPos, nBorder, NULL );
 }
 
-sal_Bool Outliner::IsTextPos( const Point& rPaperPos, sal_uInt16 nBorder, sal_Bool* pbBullet )
+bool Outliner::IsTextPos( const Point& rPaperPos, sal_uInt16 nBorder, bool* pbBullet )
 {
     if ( pbBullet)
-        *pbBullet = sal_False;
-    sal_Bool bTextPos = pEditEngine->IsTextPos( rPaperPos, nBorder );
+        *pbBullet = false;
+    bool bTextPos = pEditEngine->IsTextPos( rPaperPos, nBorder );
     if ( !bTextPos )
     {
         Point aDocPos = GetDocPos( rPaperPos );
         sal_Int32 nPara = pEditEngine->FindParagraph( aDocPos.Y() );
         if ( ( nPara != EE_PARA_NOT_FOUND ) && ImplHasNumberFormat( nPara ) )
         {
-            Rectangle aBulArea = ImpCalcBulletArea( nPara, sal_True, sal_True );
+            Rectangle aBulArea = ImpCalcBulletArea( nPara, true, true );
             if ( aBulArea.IsInside( rPaperPos ) )
             {
-                bTextPos = sal_True;
+                bTextPos = true;
                 if ( pbBullet)
-                    *pbBullet = sal_True;
+                    *pbBullet = true;
             }
         }
     }
@@ -465,29 +465,29 @@ void Outliner::QuickSetAttribs( const SfxItemSet& rSet, const ESelection& rSel )
 
 void Outliner::QuickInsertText( const OUString& rText, const ESelection& rSel )
 {
-    bFirstParaIsEmpty = sal_False;
+    bFirstParaIsEmpty = false;
     pEditEngine->QuickInsertText( rText, rSel );
 }
 
 void Outliner::QuickDelete( const ESelection& rSel )
 {
-    bFirstParaIsEmpty = sal_False;
+    bFirstParaIsEmpty = false;
     pEditEngine->QuickDelete( rSel );
 }
 
 void Outliner::QuickInsertField( const SvxFieldItem& rFld, const ESelection& rSel )
 {
-    bFirstParaIsEmpty = sal_False;
+    bFirstParaIsEmpty = false;
     pEditEngine->QuickInsertField( rFld, rSel );
 }
 
 void Outliner::QuickInsertLineBreak( const ESelection& rSel )
 {
-    bFirstParaIsEmpty = sal_False;
+    bFirstParaIsEmpty = false;
     pEditEngine->QuickInsertLineBreak( rSel );
 }
 
-void Outliner::QuickFormatDoc( sal_Bool bFull )
+void Outliner::QuickFormatDoc( bool bFull )
 {
     pEditEngine->QuickFormatDoc( bFull );
 }
@@ -517,12 +517,12 @@ void Outliner::EraseVirtualDevice()
     pEditEngine->EraseVirtualDevice();
 }
 
-sal_Bool Outliner::ShouldCreateBigTextObject() const
+bool Outliner::ShouldCreateBigTextObject() const
 {
     return pEditEngine->ShouldCreateBigTextObject();
 }
 
-void Outliner::SetVertical( sal_Bool b )
+void Outliner::SetVertical( bool b )
 {
     pEditEngine->SetVertical( b );
 }
@@ -532,7 +532,7 @@ bool Outliner::IsVertical() const
     return pEditEngine->IsVertical();
 }
 
-void Outliner::SetFixedCellHeight( sal_Bool bUseFixedCellHeight )
+void Outliner::SetFixedCellHeight( bool bUseFixedCellHeight )
 {
     pEditEngine->SetFixedCellHeight( bUseFixedCellHeight );
 }
@@ -557,27 +557,27 @@ LanguageType Outliner::GetLanguage( sal_Int32 nPara, sal_Int32 nPos ) const
     return pEditEngine->GetLanguage( nPara, nPos );
 }
 
-void Outliner::RemoveAttribs( const ESelection& rSelection, sal_Bool bRemoveParaAttribs, sal_uInt16 nWhich )
+void Outliner::RemoveAttribs( const ESelection& rSelection, bool bRemoveParaAttribs, sal_uInt16 nWhich )
 {
     pEditEngine->RemoveAttribs( rSelection, bRemoveParaAttribs, nWhich );
 }
 
-void Outliner::EnableAutoColor( sal_Bool b )
+void Outliner::EnableAutoColor( bool b )
 {
     pEditEngine->EnableAutoColor( b );
 }
 
-void Outliner::ForceAutoColor( sal_Bool b )
+void Outliner::ForceAutoColor( bool b )
 {
     pEditEngine->ForceAutoColor( b );
 }
 
-sal_Bool Outliner::IsForceAutoColor() const
+bool Outliner::IsForceAutoColor() const
 {
     return pEditEngine->IsForceAutoColor();
 }
 
-void Outliner::StartSpelling(EditView& rEditView, sal_Bool bMultipleDoc)
+void Outliner::StartSpelling(EditView& rEditView, bool bMultipleDoc)
 {
     pEditEngine->StartSpelling(rEditView, bMultipleDoc);
 }

editeng/source/outliner/outlundo.cxx
@@ -110,12 +110,12 @@ OutlinerUndoChangeDepth::OutlinerUndoChangeDepth( Outliner* pOutliner, sal_Int32
 
 void OutlinerUndoChangeDepth::Undo()
 {
-    GetOutliner()->ImplInitDepth( mnPara, mnOldDepth, sal_False );
+    GetOutliner()->ImplInitDepth( mnPara, mnOldDepth, false );
 }
 
 void OutlinerUndoChangeDepth::Redo()
 {
-    GetOutliner()->ImplInitDepth( mnPara, mnNewDepth, sal_False );
+    GetOutliner()->ImplInitDepth( mnPara, mnNewDepth, false );
 }
 
 void OutlinerUndoChangeDepth::Repeat()
@@ -134,14 +134,14 @@ void OutlinerUndoCheckPara::Undo()
 {
     Paragraph* pPara = GetOutliner()->GetParagraph( mnPara );
     pPara->Invalidate();
-    GetOutliner()->ImplCalcBulletText( mnPara, sal_False, sal_False );
+    GetOutliner()->ImplCalcBulletText( mnPara, false, false );
 }
 
 void OutlinerUndoCheckPara::Redo()
 {
     Paragraph* pPara = GetOutliner()->GetParagraph( mnPara );
     pPara->Invalidate();
-    GetOutliner()->ImplCalcBulletText( mnPara, sal_False, sal_False );
+    GetOutliner()->ImplCalcBulletText( mnPara, false, false );
 }
 
 void OutlinerUndoCheckPara::Repeat()

editeng/source/outliner/paralist.cxx
@@ -63,7 +63,7 @@ Paragraph::Paragraph( sal_Int16 nDDepth )
 
     nDepth = nDDepth;
     nFlags = 0;
-    bVisible = sal_True;
+    bVisible = true;
 }
 
 Paragraph::Paragraph( const Paragraph& rPara )
@@ -80,7 +80,7 @@ Paragraph::Paragraph( const Paragraph& rPara )
 Paragraph::Paragraph( const ParagraphData& rData )
 : nFlags( 0 )
 , aBulSize( -1, -1)
-, bVisible( sal_True )
+, bVisible( true )
 {
     nDepth = rData.nDepth;
     mnNumberingStartValue = rData.mnNumberingStartValue;
@@ -98,7 +98,7 @@ void Paragraph::SetNumberingStartValue( sal_Int16 nNumberingStartValue )
         mbParaIsNumberingRestart = true;
 }
 
-void Paragraph::SetParaIsNumberingRestart( sal_Bool bParaIsNumberingRestart )
+void Paragraph::SetParaIsNumberingRestart( bool bParaIsNumberingRestart )
 {
     mbParaIsNumberingRestart = bParaIsNumberingRestart;
     if( !mbParaIsNumberingRestart )
@@ -232,7 +232,7 @@ void ParagraphList::Expand( Paragraph* pParent )
         Paragraph* pPara = GetParagraph( nPos+n );
         if ( !( pPara->IsVisible() ) )
         {
-            pPara->bVisible = sal_True;
+            pPara->bVisible = true;
             aVisibleStateChangedHdl.Call( pPara );
         }
     }
@@ -248,7 +248,7 @@ void ParagraphList::Collapse( Paragraph* pParent )
         Paragraph* pPara = GetParagraph( nPos+n );
         if ( pPara->IsVisible() )
         {
-            pPara->bVisible = sal_False;
+            pPara->bVisible = false;
             aVisibleStateChangedHdl.Call( pPara );
         }
     }

zhirshya/lo-tblr

commit 7529cf5df515f49741975f193ed97eb4b45b130d

Resolves: fdo#80140 inverted spell-check continue logic
regression from 57779aa0195d77239b339c6dbe17209b3f0cc4ee
"editeng: sal_Bool->bool"

Change-Id: I6042830f5ba1822b1ba59326895080f3375cd701

sd/source/ui/view/Outliner.cxx
@@ -370,10 +370,7 @@ void Outliner::EndSpelling (void)
     mnStartPageIndex = (sal_uInt16) -1;
 }
 
-
-
-
-bool Outliner::SpellNextDocument (void)
+bool Outliner::SpellNextDocument()
 {
     ::boost::shared_ptr<ViewShell> pViewShell (mpWeakViewShell.lock());
     if (pViewShell->ISA(OutlineViewShell))
@@ -401,10 +398,8 @@ bool Outliner::SpellNextDocument (void)
         ClearModifyFlag();
     }
 
-    return mbEndOfSearch;
-
+    return !mbEndOfSearch;
 }
-
 
 /**
  * check next text object