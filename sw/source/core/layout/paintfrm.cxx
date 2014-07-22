LibreOffice/core

commit 2cd1673f41cb5bd8502a9a48a5721244660fe3a0

fdo#75260: Apply the same fix from writer table to character bordering.
And mirror the right and bottom borders while I'm at it. I guess it was
originally meant to.

Change-Id: I408b077c1524d19bbadd6f0b284ce204064eb735

sw/source/core/layout/paintfrm.cxx
@@ -4965,23 +4965,25 @@ void PaintCharacterBorder(
 
     // Init borders, after this initialization top, bottom, right and left means the
     // absolute position
-    const boost::optional<editeng::SvxBorderLine> aTopBorder =
+    boost::optional<editeng::SvxBorderLine> aTopBorder =
         (bTop ? rFont.GetAbsTopBorder(bVerticalLayout) : boost::none);
-    const boost::optional<editeng::SvxBorderLine> aBottomBorder =
+    boost::optional<editeng::SvxBorderLine> aBottomBorder =
         (bBottom ? rFont.GetAbsBottomBorder(bVerticalLayout) : boost::none);
-    const boost::optional<editeng::SvxBorderLine> aLeftBorder =
+    boost::optional<editeng::SvxBorderLine> aLeftBorder =
         (bLeft ? rFont.GetAbsLeftBorder(bVerticalLayout) : boost::none);
-    const boost::optional<editeng::SvxBorderLine> aRightBorder =
+    boost::optional<editeng::SvxBorderLine> aRightBorder =
         (bRight ? rFont.GetAbsRightBorder(bVerticalLayout) : boost::none);
 
     if( aTopBorder )
     {
+        sal_uInt16 nOffset = aTopBorder->GetDistance();
+
         Point aLeftTop(
-            aAlignedRect.Left(),
-            aAlignedRect.Top());
+            aAlignedRect.Left() - nOffset,
+            aAlignedRect.Top() - nOffset);
         Point aRightBottom(
-            aAlignedRect.Right(),
-            aAlignedRect.Top() + aTopBorder.get().GetScaledWidth());
+            aAlignedRect.Right() + nOffset,
+            aAlignedRect.Top() - nOffset + aTopBorder->GetScaledWidth());
 
         lcl_MakeBorderLine(
             SwRect(aLeftTop, aRightBottom),
@@ -4993,6 +4995,8 @@ void PaintCharacterBorder(
 
     if( aBottomBorder )
     {
+        aBottomBorder->SetMirrorWidths(true);
+
         Point aLeftTop(
             aAlignedRect.Left(),
             aAlignedRect.Bottom() - aBottomBorder.get().GetScaledWidth());
@@ -5010,12 +5014,14 @@ void PaintCharacterBorder(
 
     if( aLeftBorder )
     {
+        sal_uInt16 nOffset = aLeftBorder->GetDistance();
+
         Point aLeftTop(
-            aAlignedRect.Left(),
-            aAlignedRect.Top());
+            aAlignedRect.Left() - nOffset,
+            aAlignedRect.Top() - nOffset);
         Point aRightBottom(
-            aAlignedRect.Left() + aLeftBorder.get().GetScaledWidth(),
-            aAlignedRect.Bottom());
+            aAlignedRect.Left() - nOffset + aLeftBorder->GetScaledWidth(),
+            aAlignedRect.Bottom() + nOffset);
 
         lcl_MakeBorderLine(
             SwRect(aLeftTop, aRightBottom),
@@ -5027,6 +5033,8 @@ void PaintCharacterBorder(
 
     if( aRightBorder )
     {
+        aRightBorder->SetMirrorWidths(true);
+
         Point aLeftTop(
             aAlignedRect.Right() - aRightBorder.get().GetScaledWidth(),
             aAlignedRect.Top());

zhirshya/lo-tblr

commit 71de6687d2141015adf268dbd5f452512097d129

fdo#79221: Wrong rendering of bottom and right character border
Regression from 2cd1673f41cb5bd8502a9a48a5721244660fe3a0.

SetMirrorWidths() works only when there are two lines
(double border), but breaks one line borders.

Change-Id: I2727afbc071bb4038c6cef18e6b96c796ef3a8b9

sw/source/core/layout/paintfrm.cxx
@@ -5084,7 +5084,8 @@ void PaintCharacterBorder(
 
     if( aBottomBorder )
     {
-        aBottomBorder->SetMirrorWidths(true);
+        if( aBottomBorder->isDouble() )
+            aBottomBorder->SetMirrorWidths(true);
 
         Point aLeftTop(
             aAlignedRect.Left(),
@@ -5122,7 +5123,8 @@ void PaintCharacterBorder(
 
     if( aRightBorder )
     {
-        aRightBorder->SetMirrorWidths(true);
+        if( aRightBorder->isDouble() )
+            aRightBorder->SetMirrorWidths(true);
 
         Point aLeftTop(
             aAlignedRect.Right() - aRightBorder.get().GetScaledWidth(),