LibreOffice/core

commit 8b949134441056a1455d67ddfdd7e0bc5f2ee682

fdo#65403, fdo#65404 DOCX export/import of character highlight
Steps
-Add a new character attribute (RES_CHRATR_HIGHLIGHT)
-Get this character attribute via SwFont class just like
background
-If has highlight, then paint that, otherwise paint background
-Extend UNO API
-Implement DOCX export and import filter
Note: By now character highlight can't be set via UI.
It's a next step to add a highlight option.

Change-Id: I7f81e173744bf256891487f898d06dbf372a2f88

writerfilter/source/dmapper/DomainMapper.cxx
@@ -2011,8 +2011,8 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext, SprmType
     case NS_sprm::LN_CHighlight:
         {
             sal_Int32 nColor = 0;
-            if(true ==( mbIsHighlightSet = getColorFromIndex(nIntValue, nColor)))
-                rContext->Insert(PROP_CHAR_BACK_COLOR, uno::makeAny( nColor ));
+            if( (mbIsHighlightSet = getColorFromIndex(nIntValue, nColor)) )
+                rContext->Insert(PROP_CHAR_HIGHLIGHT, uno::makeAny( nColor ));
             else if (mnBackgroundColor)
                 rContext->Insert(PROP_CHAR_BACK_COLOR, uno::makeAny( mnBackgroundColor ));
         }

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -121,6 +121,7 @@ public:
     void testStyleInheritance();
     void testSmartart();
     void testFdo69636();
+    void testCharHighLight();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -239,6 +240,7 @@ void Test::run()
         {"style-inheritance.docx", &Test::testStyleInheritance},
         {"smartart.docx", &Test::testSmartart},
         {"fdo69636.docx", &Test::testFdo69636},
+        {"char_highlight.docx", &Test::testCharHighLight},
     };
     // Don't test the first import of these, for some reason those tests fail
     const char* aBlacklist[] = {
@@ -1490,6 +1492,53 @@ void Test::testFdo69636()
      */
     xmlDocPtr pXmlDoc = parseExport();
     CPPUNIT_ASSERT(getXPath(pXmlDoc, "/w:document/w:body/w:p/w:r/w:pict/v:rect/v:textbox", "style").match("mso-layout-flow-alt:bottom-to-top"));
+}
+
+void Test::testCharHighLight()
+{
+    const uno::Reference< text::XTextRange > xPara = getParagraph(1);
+    // Both highlight and background
+    const sal_Int32 nBackColor(0x4F81BD);
+    for( int nRun = 1; nRun <= 16; ++nRun )
+    {
+        const uno::Reference<beans::XPropertySet> xRun(getRun(xPara,nRun), uno::UNO_QUERY);
+        sal_Int32 nHighLightColor = 0;
+        switch( nRun )
+        {
+            case 1: nHighLightColor = 0x000000; break; //black
+            case 2: nHighLightColor = 0x0000ff; break; //blue
+            case 3: nHighLightColor = 0x00ffff; break; //cyan
+            case 4: nHighLightColor = 0x00ff00; break; //green
+            case 5: nHighLightColor = 0xff00ff; break; //magenta
+            case 6: nHighLightColor = 0xff0000; break; //red
+            case 7: nHighLightColor = 0xffff00; break; //yellow
+            case 8: nHighLightColor = 0xffffff; break; //white
+            case 9: nHighLightColor = 0x000080;  break;//dark blue
+            case 10: nHighLightColor = 0x008080; break; //dark cyan
+            case 11: nHighLightColor = 0x008000; break; //dark green
+            case 12: nHighLightColor = 0x800080; break; //dark magenta
+            case 13: nHighLightColor = 0x800000; break; //dark red
+            case 14: nHighLightColor = 0x808000; break; //dark yellow
+            case 15: nHighLightColor = 0x808080; break; //dark gray
+            case 16: nHighLightColor = 0xC0C0C0; break; //light gray
+        }
+        CPPUNIT_ASSERT_EQUAL(nHighLightColor, getProperty<sal_Int32>(xRun,"CharHighLight"));
+        CPPUNIT_ASSERT_EQUAL(nBackColor, getProperty<sal_Int32>(xRun,"CharBackColor"));
+    }
+
+    // Only highlight
+    {
+        const uno::Reference<beans::XPropertySet> xRun(getRun(xPara,17), uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0xC0C0C0), getProperty<sal_Int32>(xRun,"CharHighLight"));
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(COL_TRANSPARENT), getProperty<sal_Int32>(xRun,"CharBackColor"));
+    }
+
+    // Only background
+    {
+        const uno::Reference<beans::XPropertySet> xRun(getRun(xPara,18), uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(COL_TRANSPARENT), getProperty<sal_Int32>(xRun,"CharHighLight"));
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0x0000ff), getProperty<sal_Int32>(xRun,"CharBackColor"));
+    }
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

offapi/com/sun/star/style/CharacterProperties.idl
@@ -435,6 +435,12 @@ published service CharacterProperties
     **/
     [optional, property] com::sun::star::table::ShadowFormat CharShadowFormat;
 
+    /** Determines the color of the highlight.
+     *
+     *  @since LibreOffice 4.2
+    **/
+    [optional, property] com::sun::star::util::Color CharHighLight;
+
 };
 
 }; }; }; };

sw/inc/charatr.hxx
@@ -87,6 +87,8 @@ inline const SvxBlinkItem  &SwAttrSet::GetBlink(sal_Bool bInP) const
     {   return (const SvxBlinkItem&)Get( RES_CHRATR_BLINK,bInP); }
 inline const SvxBrushItem &SwAttrSet::GetChrBackground( sal_Bool bInP ) const
     {   return (const SvxBrushItem&)Get( RES_CHRATR_BACKGROUND, bInP ); }
+inline const SvxBrushItem &SwAttrSet::GetChrHighLight( sal_Bool bInP ) const
+    {   return (const SvxBrushItem&)Get( RES_CHRATR_HIGHLIGHT, bInP ); }
 inline const SvxEmphasisMarkItem &SwAttrSet::GetEmphasisMark( sal_Bool bInP ) const
     {   return (const SvxEmphasisMarkItem&)Get( RES_CHRATR_EMPHASIS_MARK, bInP ); }
 inline const SvxTwoLinesItem    &SwAttrSet::Get2Lines( sal_Bool bInP ) const
@@ -152,6 +154,8 @@ inline const SvxBlinkItem           &SwFmt::GetBlink(sal_Bool bInP) const
     {   return aSet.GetBlink(bInP); }
 inline const SvxBrushItem           &SwFmt::GetChrBackground(sal_Bool bInP) const
     {   return aSet.GetChrBackground(bInP); }
+inline const SvxBrushItem &SwFmt::GetChrHighLight(sal_Bool bInP) const
+    {   return aSet.GetChrHighLight(bInP); }
 inline const SvxEmphasisMarkItem   &SwFmt::GetEmphasisMark( sal_Bool bInP ) const
     {   return aSet.GetEmphasisMark(bInP); }
 inline const SvxTwoLinesItem       &SwFmt::Get2Lines( sal_Bool bInP ) const

sw/inc/format.hxx
@@ -190,6 +190,7 @@ public:
     inline const SvxNoHyphenItem     &GetNoHyphenHere( sal_Bool = sal_True ) const;
     inline const SvxBlinkItem        &GetBlink( sal_Bool = sal_True ) const;
     inline const SvxBrushItem        &GetChrBackground( sal_Bool = sal_True ) const;
+    inline const SvxBrushItem        &GetChrHighLight( sal_Bool = sal_True ) const;
 
     inline const SvxFontItem         &GetCJKFont( sal_Bool = sal_True ) const;
     inline const SvxFontHeightItem   &GetCJKSize( sal_Bool = sal_True ) const;

sw/inc/hintids.hxx
@@ -90,7 +90,7 @@ RES_CHRATR_BEGIN = HINT_BEGIN,
     RES_CHRATR_RSID,                        // 39
     RES_CHRATR_BOX,                         // 40
     RES_CHRATR_SHADOW,                      // 41
-    RES_CHRATR_DUMMY1,                      // 42
+    RES_CHRATR_HIGHLIGHT,                   // 42
     RES_CHRATR_DUMMY2,                      // 43
     RES_CHRATR_DUMMY3,                      // 44
 RES_CHRATR_END

sw/inc/swatrset.hxx
@@ -232,6 +232,7 @@ public:
     inline const SvxNoHyphenItem      &GetNoHyphenHere( sal_Bool = sal_True ) const;
     inline const SvxBlinkItem         &GetBlink( sal_Bool = sal_True ) const;
     inline const SvxBrushItem         &GetChrBackground( sal_Bool = sal_True ) const;
+    inline const SvxBrushItem         &GetChrHighLight( sal_Bool = sal_True ) const;
     inline const SvxFontItem          &GetCJKFont( sal_Bool = sal_True ) const;
     inline const SvxFontHeightItem    &GetCJKSize( sal_Bool = sal_True ) const;
     inline const SvxLanguageItem      &GetCJKLanguage( sal_Bool = sal_True ) const;

sw/inc/unoprnms.hxx
@@ -840,6 +840,7 @@ enum SwPropNameIds
 
 /* 0774 */  UNO_NAME_DOC_INTEROP_GRAB_BAG,
 /* 0775 */  UNO_NAME_FRAME_INTEROP_GRAB_BAG,
+/* 0776 */  UNO_NAME_CHAR_HIGHLIGHT,
 
             SW_PROPNAME_END
 

zhirshya/lo-tblr

commit 1ae748ec5de1ff1cacd47e60338b0b1aba2ec57b

fdo#80986: disable highlight DOCX import
Highlight support is not implemented fully, so
it seems better to disable DOCX import of it until
it is finished.
Highlight can't be modified on the UI and is not
saved to ODT/DOC (regression).

Regression from:
8b949134441056a1455d67ddfdd7e0bc5f2ee682

Change-Id: I94891769766ae90017e8afa70e65d080d5270202

writerfilter/source/dmapper/DomainMapper.cxx
@@ -1367,7 +1367,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext )
         {
             sal_Int32 nColor = 0;
             if( (mbIsHighlightSet = getColorFromIndex(nIntValue, nColor)) )
-                rContext->Insert(PROP_CHAR_HIGHLIGHT, uno::makeAny( nColor ));
+                rContext->Insert(PROP_CHAR_BACK_COLOR, uno::makeAny( nColor ));
             else if (mnBackgroundColor)
                 rContext->Insert(PROP_CHAR_BACK_COLOR, uno::makeAny( mnBackgroundColor ));
             else

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -1482,6 +1482,7 @@ DECLARE_OOXMLEXPORT_TEST(testSmartart, "smartart.docx")
     CPPUNIT_ASSERT_EQUAL(OUString("RenderedShapes"), nValue); // Rendered bitmap has the proper name
 }
 
+/*
 DECLARE_OOXMLEXPORT_TEST(testCharHighlight, "char_highlight.docx")
 {
     const uno::Reference< text::XTextRange > xPara = getParagraph(1);
@@ -1528,6 +1529,7 @@ DECLARE_OOXMLEXPORT_TEST(testCharHighlight, "char_highlight.docx")
         CPPUNIT_ASSERT_EQUAL(sal_Int32(0x0000ff), getProperty<sal_Int32>(xRun,"CharBackColor"));
     }
 }
+*/
 
 DECLARE_OOXMLEXPORT_TEST(testFontNameIsEmpty, "font-name-is-empty.docx")
 {