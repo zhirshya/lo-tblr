LibreOffice/core

commit 03a2b4a80c5854bcb8520f2a43e485b98e3eba8f

fdo#82151 when constructing column object, replace m_aCurrentRow by a function
The passed m_aCurrentRow becomes out-of-date as soon as the current row changes.
This also hides an implementation detail of ORowSet to ORowSet(Data)Column.

Change-Id: Ib9188743e5dd6dec240e9f5fd3fd9655c6761abc
Reviewed-on: https://gerrit.libreoffice.org/10792
Reviewed-by: Lionel Elie Mamane <lionel@mamane.lu>
Tested-by: Lionel Elie Mamane <lionel@mamane.lu>

dbaccess/source/core/api/CRowSetDataColumn.cxx
@@ -39,16 +39,16 @@ using namespace cppu;
 using namespace osl;
 
 
-ORowSetDataColumn::ORowSetDataColumn(   const Reference < XResultSetMetaData >& _xMetaData,
+ORowSetDataColumn::ORowSetDataColumn( const Reference < XResultSetMetaData >& _xMetaData,
                                       const Reference < XRow >& _xRow,
                                       const Reference < XRowUpdate >& _xRowUpdate,
                                       sal_Int32 _nPos,
                                       const Reference< XDatabaseMetaData >& _rxDBMeta,
                                       const OUString& _rDescription,
                                       const OUString& i_sLabel,
-                                      const ORowSetCacheIterator& _rColumnValue)
+                                      const boost::function< const ORowSetValue& (sal_Int32)> &_getValue)
     :ODataColumn(_xMetaData,_xRow,_xRowUpdate,_nPos,_rxDBMeta)
-    ,m_aColumnValue(_rColumnValue)
+    ,m_pGetValue(_getValue)
     ,m_sLabel(i_sLabel)
     ,m_aDescription(_rDescription)
 {
@@ -105,16 +105,7 @@ void SAL_CALL ORowSetDataColumn::getFastPropertyValue( Any& rValue, sal_Int32 nH
 {
     if ( PROPERTY_ID_VALUE == nHandle )
     {
-        if ( !m_aColumnValue.isNull() && m_aColumnValue->is() )
-        {
-            ::osl::Mutex* pMutex = m_aColumnValue.getMutex();
-            ::osl::MutexGuard aGuard( *pMutex );
-#if OSL_DEBUG_LEVEL > 0
-            ORowSetRow aRow = *m_aColumnValue;
-#endif
-            OSL_ENSURE((sal_Int32)aRow->get().size() > m_nPos,"Pos is greater than size of vector");
-            rValue = ((*m_aColumnValue)->get())[m_nPos].makeAny();
-        }
+        rValue = m_pGetValue(m_nPos).makeAny();
     }
     else if ( PROPERTY_ID_LABEL == nHandle && !m_sLabel.isEmpty() )
         rValue <<= m_sLabel;
@@ -179,19 +170,12 @@ Sequence< sal_Int8 > ORowSetDataColumn::getImplementationId() throw (RuntimeExce
 
 void ORowSetDataColumn::fireValueChange(const ORowSetValue& _rOldValue)
 {
-    if ( !m_aColumnValue.isNull() && m_aColumnValue->is() && (((*m_aColumnValue)->get())[m_nPos] != _rOldValue) )
+    const ORowSetValue &value(m_pGetValue(m_nPos));
+    if ( value != _rOldValue)
     {
-        sal_Int32 nHandle = PROPERTY_ID_VALUE;
+        sal_Int32 nHandle(PROPERTY_ID_VALUE);
         m_aOldValue = _rOldValue.makeAny();
-        Any aNew = ((*m_aColumnValue)->get())[m_nPos].makeAny();
-
-        fire(&nHandle, &aNew, &m_aOldValue, 1, sal_False );
-    }
-    else if ( !m_aColumnValue.isNull() && !_rOldValue.isNull() )
-    {
-        sal_Int32 nHandle = PROPERTY_ID_VALUE;
-        m_aOldValue = _rOldValue.makeAny();
-        Any aNew;
+        Any aNew = value.makeAny();
 
         fire(&nHandle, &aNew, &m_aOldValue, 1, sal_False );
     }

dbaccess/source/core/api/CRowSetColumn.cxx
@@ -36,9 +36,13 @@ using namespace ::com::sun::star::beans;
 namespace dbaccess
 {
 
-ORowSetColumn::ORowSetColumn(   const Reference < XResultSetMetaData >& _xMetaData, const Reference < XRow >& _xRow, sal_Int32 _nPos,
-                const Reference< XDatabaseMetaData >& _rxDBMeta, const OUString& _rDescription, const OUString& i_sLabel,ORowSetCacheIterator& _rColumnValue )
-    :ORowSetDataColumn( _xMetaData, _xRow, NULL, _nPos, _rxDBMeta, _rDescription, i_sLabel,_rColumnValue )
+ORowSetColumn::ORowSetColumn( const Reference < XResultSetMetaData >& _xMetaData,
+                              const Reference < XRow >& _xRow, sal_Int32 _nPos,
+                              const Reference< XDatabaseMetaData >& _rxDBMeta,
+                              const OUString& _rDescription,
+                              const OUString& i_sLabel,
+                              const boost::function< const ::connectivity::ORowSetValue& ( sal_Int32 ) > &_getValue )
+    :ORowSetDataColumn( _xMetaData, _xRow, NULL, _nPos, _rxDBMeta, _rDescription, i_sLabel, _getValue )
 {
 }
 

dbaccess/source/core/api/CRowSetColumn.hxx
@@ -38,7 +38,7 @@ namespace dbaccess
                         const ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XDatabaseMetaData >& _rxDBMeta,
                         const OUString& _rDescription,
                         const OUString& i_sLabel,
-                        ORowSetCacheIterator& _rColumnValue);
+                        const boost::function< const ::connectivity::ORowSetValue& ( sal_Int32 ) > &_getValue);
 
         virtual ::cppu::IPropertyArrayHelper* createArrayHelper( ) const SAL_OVERRIDE;
         virtual ::cppu::IPropertyArrayHelper& SAL_CALL getInfoHelper() SAL_OVERRIDE;

dbaccess/source/core/api/CRowSetDataColumn.hxx
@@ -21,11 +21,12 @@
 
 #include "datacolumn.hxx"
 #include "RowSetRow.hxx"
-#include "RowSetCacheIterator.hxx"
 #include "columnsettings.hxx"
 
 #include <connectivity/CommonTools.hxx>
 #include <comphelper/proparrhlp.hxx>
+
+#include <boost/function.hpp>
 
 namespace dbaccess
 {
@@ -37,7 +38,7 @@ namespace dbaccess
                                 public ORowSetDataColumn_PROP
     {
     protected:
-        ORowSetCacheIterator        m_aColumnValue;
+        const boost::function< const ::connectivity::ORowSetValue& ( sal_Int32 ) > m_pGetValue;
         ::com::sun::star::uno::Any  m_aOldValue;
 
         OUString             m_sLabel;
@@ -52,7 +53,7 @@ namespace dbaccess
                           const ::com::sun::star::uno::Reference< ::com::sun::star::sdbc::XDatabaseMetaData >& _rxDBMeta,
                           const OUString& _rDescription,
                           const OUString& i_sLabel,
-                          const ORowSetCacheIterator& _rColumnValue);
+                          const boost::function< const ::connectivity::ORowSetValue& ( sal_Int32 ) > &_getValue);
 
 
         // com::sun::star::lang::XTypeProvider

dbaccess/source/core/api/RowSet.cxx
@@ -21,6 +21,7 @@
 
 #include <map>
 #include <utility>
+#include <boost/bind.hpp>
 
 #include <string.h>
 #include "RowSet.hxx"
@@ -1876,7 +1877,7 @@ void ORowSet::execute_NoApprove_NoNewConn(ResettableMutexGuard& _rClearForNotifi
                                                                             m_xActiveConnection->getMetaData(),
                                                                             aDescription,
                                                                             OUString(),
-                                                                            m_aCurrentRow);
+                                                                            boost::bind(&ORowSet::getInsertValue, this, _1));
                         aColumnMap.insert(std::make_pair(sName,0));
                         aColumns->get().push_back(pColumn);
                         pColumn->setName(sName);
@@ -1978,7 +1979,7 @@ void ORowSet::execute_NoApprove_NoNewConn(ResettableMutexGuard& _rClearForNotifi
                                                                         m_xActiveConnection->getMetaData(),
                                                                         aDescription,
                                                                         sParseLabel,
-                                                                        m_aCurrentRow);
+                                                                        boost::bind(&ORowSet::getInsertValue, this, _1));
                     aColumns->get().push_back(pColumn);
 
                     pColumn->setFastPropertyValue_NoBroadcast(PROPERTY_ID_ISREADONLY,makeAny(rKeyColumns.find(i) != rKeyColumns.end()));
@@ -2810,7 +2811,7 @@ ORowSetClone::ORowSetClone( const Reference<XComponentContext>& _rContext, ORowS
                                                                 rParent.m_xActiveConnection->getMetaData(),
                                                                 aDescription,
                                                                 sParseLabel,
-                                                                m_aCurrentRow);
+                                                                boost::bind(&ORowSetClone::getValue, this, _1));
             aColumns->get().push_back(pColumn);
             pColumn->setName(*pIter);
             aNames.push_back(*pIter);

zhirshya/lo-tblr

commit 36a5e3f9f36b2afec6fb2e165904cb1a6a96584c

Tunnel SQLException through getPropertyValue
Regression introduced with 03a2b4a80c5854bcb8520f2a43e485b98e3eba8f "fdo#82151
when constructing column object, replace m_aCurrentRow by a function," where
the other call to m_pGetValue in ORowSetDataColumn::fireValueChange appears OK,
as ORowSetBase::firePropertyChange already wraps the fireValueChange calls in a
try--catch.

Change-Id: I527cc35ae120cf083f7c69a9a23526839a2bbddb

dbaccess/source/core/api/CRowSetDataColumn.cxx
@@ -22,6 +22,7 @@
 #include "dbastrings.hrc"
 #include "apitools.hxx"
 #include <comphelper/types.hxx>
+#include <cppuhelper/exc_hlp.hxx>
 #include <cppuhelper/typeprovider.hxx>
 #include <com/sun/star/beans/PropertyAttribute.hpp>
 #include <tools/debug.hxx>
@@ -105,7 +106,17 @@ void SAL_CALL ORowSetDataColumn::getFastPropertyValue( Any& rValue, sal_Int32 nH
 {
     if ( PROPERTY_ID_VALUE == nHandle )
     {
-        rValue = m_pGetValue(m_nPos).makeAny();
+        try
+        {
+            rValue = m_pGetValue(m_nPos).makeAny();
+        }
+        catch (css::sdbc::SQLException & e)
+        {
+            css::uno::Any a(cppu::getCaughtException());
+            throw css::lang::WrappedTargetException(
+                "wrapped css::sdbc::SQLException: " + e.Message,
+                css::uno::Reference<css::uno::XInterface>(), a);
+        }
     }
     else if ( PROPERTY_ID_LABEL == nHandle && !m_sLabel.isEmpty() )
         rValue <<= m_sLabel;

dbaccess/source/core/api/SingleSelectQueryComposer.cxx
@@ -1506,82 +1506,83 @@ void SAL_CALL OSingleSelectQueryComposer::setStructuredHavingClause( const Seque
 
 void OSingleSelectQueryComposer::setConditionByColumn( const Reference< XPropertySet >& column, bool andCriteria ,::std::mem_fun1_t<bool,OSingleSelectQueryComposer,const OUString& >& _aSetFunctor,sal_Int32 filterOperator)
 {
-    ::connectivity::checkDisposed(OSubComponent::rBHelper.bDisposed);
-
-    if ( !column.is()
-        || !column->getPropertySetInfo()->hasPropertyByName(PROPERTY_VALUE)
-        || !column->getPropertySetInfo()->hasPropertyByName(PROPERTY_NAME)
-        || !column->getPropertySetInfo()->hasPropertyByName(PROPERTY_TYPE))
-        throw SQLException(DBACORE_RESSTRING(RID_STR_COLUMN_NOT_VALID),*this,SQLSTATE_GENERAL,1000,Any() );
-
-    sal_Int32 nType = 0;
-    column->getPropertyValue(PROPERTY_TYPE) >>= nType;
-    sal_Int32 nSearchable = dbtools::getSearchColumnFlag(m_xConnection,nType);
-    if(nSearchable == ColumnSearch::NONE)
-        throw SQLException(DBACORE_RESSTRING(RID_STR_COLUMN_NOT_SEARCHABLE),*this,SQLSTATE_GENERAL,1000,Any() );
-
-    ::osl::MutexGuard aGuard( m_aMutex );
-
-    OUString aName;
-    column->getPropertyValue(PROPERTY_NAME) >>= aName;
-
-    Any aValue;
-    column->getPropertyValue(PROPERTY_VALUE) >>= aValue;
-
-    OUStringBuffer aSQL;
-    const OUString aQuote    = m_xMetaData->getIdentifierQuoteString();
-    getColumns();
-
-    // TODO: if this is called for HAVING, check that the column is a GROUP BY column
-    //       or that it is an aggregate function
-
-    if ( m_aCurrentColumns[SelectColumns] && m_aCurrentColumns[SelectColumns]->hasByName(aName) )
+    try
     {
-        Reference<XPropertySet> xColumn;
-        m_aCurrentColumns[SelectColumns]->getByName(aName) >>= xColumn;
-        OSL_ENSURE(xColumn->getPropertySetInfo()->hasPropertyByName(PROPERTY_REALNAME),"Property REALNAME not available!");
-        OSL_ENSURE(xColumn->getPropertySetInfo()->hasPropertyByName(PROPERTY_TABLENAME),"Property TABLENAME not available!");
-        OSL_ENSURE(xColumn->getPropertySetInfo()->hasPropertyByName("AggregateFunction"),"Property AggregateFunction not available!");
+        ::connectivity::checkDisposed(OSubComponent::rBHelper.bDisposed);
 
-        OUString sRealName,sTableName;
-        xColumn->getPropertyValue(PROPERTY_REALNAME)    >>= sRealName;
-        xColumn->getPropertyValue(PROPERTY_TABLENAME)   >>= sTableName;
-        if(sTableName.indexOf('.',0) != -1)
+        if ( !column.is()
+             || !column->getPropertySetInfo()->hasPropertyByName(PROPERTY_VALUE)
+             || !column->getPropertySetInfo()->hasPropertyByName(PROPERTY_NAME)
+             || !column->getPropertySetInfo()->hasPropertyByName(PROPERTY_TYPE))
+            throw SQLException(DBACORE_RESSTRING(RID_STR_COLUMN_NOT_VALID),*this,SQLSTATE_GENERAL,1000,Any() );
+
+        sal_Int32 nType = 0;
+        column->getPropertyValue(PROPERTY_TYPE) >>= nType;
+        sal_Int32 nSearchable = dbtools::getSearchColumnFlag(m_xConnection,nType);
+        if(nSearchable == ColumnSearch::NONE)
+            throw SQLException(DBACORE_RESSTRING(RID_STR_COLUMN_NOT_SEARCHABLE),*this,SQLSTATE_GENERAL,1000,Any() );
+
+        ::osl::MutexGuard aGuard( m_aMutex );
+
+        OUString aName;
+        column->getPropertyValue(PROPERTY_NAME) >>= aName;
+
+        Any aValue;
+        column->getPropertyValue(PROPERTY_VALUE) >>= aValue;
+
+        OUStringBuffer aSQL;
+        const OUString aQuote    = m_xMetaData->getIdentifierQuoteString();
+        getColumns();
+
+        // TODO: if this is called for HAVING, check that the column is a GROUP BY column
+        //       or that it is an aggregate function
+
+        if ( m_aCurrentColumns[SelectColumns] && m_aCurrentColumns[SelectColumns]->hasByName(aName) )
         {
-            OUString aCatlog,aSchema,aTable;
-            ::dbtools::qualifiedNameComponents(m_xMetaData,sTableName,aCatlog,aSchema,aTable,::dbtools::eInDataManipulation);
-            sTableName = ::dbtools::composeTableName( m_xMetaData, aCatlog, aSchema, aTable, true, ::dbtools::eInDataManipulation );
+            Reference<XPropertySet> xColumn;
+            m_aCurrentColumns[SelectColumns]->getByName(aName) >>= xColumn;
+            OSL_ENSURE(xColumn->getPropertySetInfo()->hasPropertyByName(PROPERTY_REALNAME),"Property REALNAME not available!");
+            OSL_ENSURE(xColumn->getPropertySetInfo()->hasPropertyByName(PROPERTY_TABLENAME),"Property TABLENAME not available!");
+            OSL_ENSURE(xColumn->getPropertySetInfo()->hasPropertyByName("AggregateFunction"),"Property AggregateFunction not available!");
+
+            OUString sRealName,sTableName;
+            xColumn->getPropertyValue(PROPERTY_REALNAME)    >>= sRealName;
+            xColumn->getPropertyValue(PROPERTY_TABLENAME)   >>= sTableName;
+            if(sTableName.indexOf('.',0) != -1)
+            {
+                OUString aCatlog,aSchema,aTable;
+                ::dbtools::qualifiedNameComponents(m_xMetaData,sTableName,aCatlog,aSchema,aTable,::dbtools::eInDataManipulation);
+                sTableName = ::dbtools::composeTableName( m_xMetaData, aCatlog, aSchema, aTable, true, ::dbtools::eInDataManipulation );
+            }
+            else
+                sTableName = ::dbtools::quoteName(aQuote,sTableName);
+
+            if ( !::comphelper::getBOOL(xColumn->getPropertyValue("Function")) )
+            {
+                aSQL =  sTableName + "." + ::dbtools::quoteName( aQuote, sRealName );
+            }
+            else
+                aSQL = sRealName;
         }
         else
-            sTableName = ::dbtools::quoteName(aQuote,sTableName);
-
-        if ( !::comphelper::getBOOL(xColumn->getPropertyValue("Function")) )
         {
-            aSQL =  sTableName + "." + ::dbtools::quoteName( aQuote, sRealName );
-        }
-        else
-            aSQL = sRealName;
-
-    }
-    else
-    {
-        aSQL = getTableAlias( column ) + ::dbtools::quoteName( aQuote, aName );
-    }
-
-    if ( aValue.hasValue() )
-    {
-        if(  !m_xTypeConverter.is() )
-            m_xTypeConverter.set( Converter::create(m_aContext) );
-        OSL_ENSURE(m_xTypeConverter.is(),"NO typeconverter!");
-
-        if ( nType != DataType::BOOLEAN && DataType::BIT != nType )
-        {
-            OUString sEmpty;
-            lcl_addFilterCriteria_throw(filterOperator,sEmpty,aSQL);
+            aSQL = getTableAlias( column ) + ::dbtools::quoteName( aQuote, aName );
         }
 
-        switch(nType)
+        if ( aValue.hasValue() )
         {
+            if(  !m_xTypeConverter.is() )
+                m_xTypeConverter.set( Converter::create(m_aContext) );
+            OSL_ENSURE(m_xTypeConverter.is(),"NO typeconverter!");
+
+            if ( nType != DataType::BOOLEAN && DataType::BIT != nType )
+            {
+                OUString sEmpty;
+                lcl_addFilterCriteria_throw(filterOperator,sEmpty,aSQL);
+            }
+
+            switch(nType)
+            {
             case DataType::VARCHAR:
             case DataType::CHAR:
             case DataType::LONGVARCHAR:
@@ -1642,31 +1643,44 @@ void OSingleSelectQueryComposer::setConditionByColumn( const Reference< XPropert
             default:
                 aSQL.append( DBTypeConversion::toSQLString( nType, aValue, true, m_xTypeConverter ) );
                 break;
+            }
+        }
+        else
+        {
+            sal_Int32 nFilterOp = filterOperator;
+            if ( filterOperator != SQLFilterOperator::SQLNULL && filterOperator != SQLFilterOperator::NOT_SQLNULL )
+                nFilterOp = SQLFilterOperator::SQLNULL;
+            OUString sEmpty;
+            lcl_addFilterCriteria_throw(nFilterOp,sEmpty,aSQL);
+        }
+
+        // Attach filter
+        // Construct SELECT without WHERE and ORDER BY
+        OUString sFilter = getFilter();
+
+        if ( !sFilter.isEmpty() && !aSQL.isEmpty() )
+        {
+            OUString sTemp(L_BRACKET + sFilter + R_BRACKET);
+            sTemp += andCriteria ? OUString(STR_AND) : OUString(STR_OR);
+            sFilter = sTemp;
+        }
+        sFilter += aSQL.makeStringAndClear();
+
+        // add the filter and the sort order
+        _aSetFunctor(this,sFilter);
+    }
+    catch (css::lang::WrappedTargetException & e)
+    {
+        if (e.TargetException.isExtractableTo(
+                cppu::UnoType<css::sdbc::SQLException>::get()))
+        {
+            cppu::throwException(e.TargetException);
+        }
+        else
+        {
+            throw e;
         }
     }
-    else
-    {
-        sal_Int32 nFilterOp = filterOperator;
-        if ( filterOperator != SQLFilterOperator::SQLNULL && filterOperator != SQLFilterOperator::NOT_SQLNULL )
-            nFilterOp = SQLFilterOperator::SQLNULL;
-        OUString sEmpty;
-        lcl_addFilterCriteria_throw(nFilterOp,sEmpty,aSQL);
-    }
-
-    // Attach filter
-    // Construct SELECT without WHERE and ORDER BY
-    OUString sFilter = getFilter();
-
-    if ( !sFilter.isEmpty() && !aSQL.isEmpty() )
-    {
-        OUString sTemp(L_BRACKET + sFilter + R_BRACKET);
-        sTemp += andCriteria ? OUString(STR_AND) : OUString(STR_OR);
-        sFilter = sTemp;
-    }
-    sFilter += aSQL.makeStringAndClear();
-
-    // add the filter and the sort order
-    _aSetFunctor(this,sFilter);
 }
 
 Sequence< Sequence< PropertyValue > > OSingleSelectQueryComposer::getStructuredCondition( TGetParseNode& _aGetFunctor )