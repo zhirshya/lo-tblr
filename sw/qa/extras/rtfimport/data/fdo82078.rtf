LibreOffice/core

commit 41890bf714803969515578b47e63fa9730ea7090

fdo#54473 fdo#57133 fix RTF import of character styles
Regression from commit d2d77fd86e6cb7341e113557e2cfe3d7948759bd

Change-Id: I7accc5c383de3fa53c12527766c3fc155bfa9863

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2673,7 +2673,7 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
                 m_aStates.top().aParagraphAttributes.set(NS_rtf::LN_ISTD, pIntValue);
             break;
         case RTF_CS:
-            if (m_aStates.top().nDestinationState == DESTINATION_STYLESHEET)
+            if (m_aStates.top().nDestinationState == DESTINATION_STYLESHEET || m_aStates.top().nDestinationState == DESTINATION_STYLEENTRY)
             {
                 m_nCurrentStyleIndex = nParam;
                 m_aStates.top().aTableAttributes.set(NS_rtf::LN_ISTD, pIntValue);

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -127,6 +127,7 @@ public:
     void testFdo48442();
     void testFdo55525();
     void testFdo57708();
+    void testFdo54473();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -204,6 +205,7 @@ void Test::run()
         {"fdo48442.rtf", &Test::testFdo48442},
         {"fdo55525.rtf", &Test::testFdo55525},
         {"fdo57708.rtf", &Test::testFdo57708},
+        {"fdo54473.rtf", &Test::testFdo54473},
     };
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
     {
@@ -939,6 +941,13 @@ void Test::testFdo57708()
     uno::Reference<container::XIndexAccess> xDraws(xDrawPageSupplier->getDrawPage(), uno::UNO_QUERY);
     // Two objects: a picture and a textframe.
     CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xDraws->getCount());
+}
+
+void Test::testFdo54473()
+{
+    // The problem was that character styles were not imported due to a typo.
+    CPPUNIT_ASSERT_EQUAL(OUString("Anot"), getProperty<OUString>(getRun(getParagraph(1), 1, "Text "), "CharStyleName"));
+    CPPUNIT_ASSERT_EQUAL(OUString("ForeignTxt"), getProperty<OUString>(getRun(getParagraph(1), 3, "character "), "CharStyleName"));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

sw/qa/extras/rtfimport/data/fdo54473.rtf
@@ -1,0 +1,10 @@
+{\rtf1\ansi
+{\stylesheet
+{\*\cs16\ul Anot;}
+{\*\cs15\i ForeignTxt;}
+}
+{\cs16\ul Text }
+with
+{\cs15\i character }
+formatting
+\par }

zhirshya/lo-tblr

commit 222495dfb057e471eee80c9f4ac154bc9e9b9511

fdo#82078 RTF import: fix bold text spilling over to non-bold text
Regression from commit 41890bf714803969515578b47e63fa9730ea7090
(fdo#54473 fdo#57133 fix RTF import of character styles, 2012-12-04),
the problem was that the style undoing magic introduced in
1be0a3fa9ebb22b607c54b47739d4467acfed259 (n#825305: writerfilter RTF
import: override style properties like Word, 2014-06-17) was not used
for character styles.

Change-Id: Id31cfd5574a83bcf2895f0c2fd496fa60791cbb3

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -445,9 +445,29 @@ writerfilter::Reference<Properties>::Pointer_t RTFDocumentImpl::getProperties(RT
     if (it != m_aStyleTableEntries.end())
     {
         RTFReferenceProperties& rProps = *(RTFReferenceProperties*)it->second.get();
+
+        // cloneAndDeduplicate() wants to know about only a single "style", so
+        // let's merge paragraph and character style properties here.
+        int nCharStyle = m_aStates.top().nCurrentCharacterStyleIndex;
+        RTFReferenceTable::Entries_t::iterator itChar = m_aStyleTableEntries.find(nCharStyle);
+        RTFSprms aStyleSprms = rProps.getSprms();
+        RTFSprms aStyleAttributes = rProps.getAttributes();
+        if (itChar != m_aStyleTableEntries.end())
+        {
+            // Found active character style, then update aStyleSprms/Attributes.
+            RTFReferenceProperties& rCharProps = *(RTFReferenceProperties*)itChar->second.get();
+            RTFSprms& rCharStyleSprms = rCharProps.getSprms();
+            for (RTFSprms::Iterator_t itSprm = rCharStyleSprms.begin(); itSprm != rCharStyleSprms.end(); ++itSprm)
+                aStyleSprms.set(itSprm->first, itSprm->second);
+
+            RTFSprms& rCharStyleAttributes = rCharProps.getAttributes();
+            for (RTFSprms::Iterator_t itAttr = rCharStyleAttributes.begin(); itAttr != rCharStyleAttributes.end(); ++itAttr)
+                aStyleAttributes.set(itAttr->first, itAttr->second);
+        }
+
         // Get rid of direct formatting what is already in the style.
-        RTFSprms const sprms(rSprms.cloneAndDeduplicate(rProps.getSprms()));
-        RTFSprms const attributes(rAttributes.cloneAndDeduplicate(rProps.getAttributes()));
+        RTFSprms const sprms(rSprms.cloneAndDeduplicate(aStyleSprms));
+        RTFSprms const attributes(rAttributes.cloneAndDeduplicate(aStyleAttributes));
         return writerfilter::Reference<Properties>::Pointer_t(new RTFReferenceProperties(attributes, sprms));
     }
     writerfilter::Reference<Properties>::Pointer_t pRet(new RTFReferenceProperties(rAttributes, rSprms));
@@ -2807,6 +2827,7 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
         m_aStates.top().aCharacterSprms = getDefaultState().aCharacterSprms;
         m_aStates.top().nCurrentEncoding = getEncoding(getFontIndex(m_nDefaultFontIndex));
         m_aStates.top().aCharacterAttributes = getDefaultState().aCharacterAttributes;
+        m_aStates.top().nCurrentCharacterStyleIndex = -1;
     }
     break;
     case RTF_PARD:
@@ -3692,6 +3713,7 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
     }
     break;
     case RTF_CS:
+        m_aStates.top().nCurrentCharacterStyleIndex = nParam;
         if (m_aStates.top().nDestinationState == DESTINATION_STYLESHEET || m_aStates.top().nDestinationState == DESTINATION_STYLEENTRY)
         {
             m_nCurrentStyleIndex = nParam;
@@ -5937,6 +5959,7 @@ RTFParserState::RTFParserState(RTFDocumentImpl* pDocumentImpl)
       nMinute(0),
       pDestinationText(0),
       nCurrentStyleIndex(-1),
+      nCurrentCharacterStyleIndex(-1),
       pCurrentBuffer(0),
       bInListpicture(false),
       bInBackground(false),

writerfilter/source/rtftok/rtfdocumentimpl.hxx
@@ -279,6 +279,8 @@ public:
 
     /// Index of the current style.
     int nCurrentStyleIndex;
+    /// Index of the current character style.
+    int nCurrentCharacterStyleIndex;
 
     /// Points to the active buffer, if there is one.
     RTFBuffer_t* pCurrentBuffer;

writerfilter/source/rtftok/rtfsprm.cxx
@@ -143,6 +143,7 @@ static RTFValue::Pointer_t getDefaultSPRM(Id const id)
     {
     case NS_ooxml::LN_CT_Spacing_before:
     case NS_ooxml::LN_CT_Spacing_after:
+    case NS_ooxml::LN_EG_RPrBase_b:
         return RTFValue::Pointer_t(new RTFValue(0));
 
     default:

sw/qa/extras/rtfimport/data/fdo82078.rtf
@@ -1,0 +1,13 @@
+{\rtf1\adeflang1025\ansi\ansicpg1252\uc1\adeff31507\deff0\stshfdbch0\stshfloch0\stshfhich0\stshfbi0\deflang1033\deflangfe1033\themelang1033\themelangfe0\themelangcs0
+\noqfpromote
+{\stylesheet
+{\ql \li0\ri0\sl240\slmult0\widctlpar\wrapdefault\aspalpha\aspnum\faauto\adjustright\rin0\lin0\itap0 \rtlch\fcs1 \af31507\afs22\alang1025 \ltrch\fcs0 \f40\fs18\lang1033\langfe1033\cgrid\langnp1033\langfenp1033
+\snext0 \sqformat \spriority14 \styrsid11343869 Normal;}
+{\*\cs73\b Strong;}
+}
+\pard
+{before }
+{\cs73
+after}
+\par
+}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -9,6 +9,7 @@
 #include <swmodeltestbase.hxx>
 
 #include <com/sun/star/awt/FontDescriptor.hpp>
+#include <com/sun/star/awt/FontWeight.hpp>
 #include <com/sun/star/document/XFilter.hpp>
 #include <com/sun/star/document/XImporter.hpp>
 #include <com/sun/star/drawing/EnhancedCustomShapeParameterPair.hpp>
@@ -1917,6 +1918,12 @@ DECLARE_RTFIMPORT_TEST(testFdo79959, "fdo79959.rtf")
 {
     // This was false, as the style was imported as " Test", i.e. no whitespace stripping.
     CPPUNIT_ASSERT_EQUAL(true, static_cast<bool>(getStyles("ParagraphStyles")->hasByName("Test")));
+}
+
+DECLARE_RTFIMPORT_TEST(testFdo82078, "fdo82078.rtf")
+{
+    // This was awt::FontWeight::BOLD, i.e. the second run was bold, when it should be normal.
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::NORMAL, getProperty<float>(getRun(getParagraph(1), 2), "CharWeight"));
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();