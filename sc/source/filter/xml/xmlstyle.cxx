LibreOffice/core

commit 63ea2d467b50452ae0cab18b4151107a747ae70d

replace while with for
Change-Id: I752376e42bf745b33a7be7478531eebd42893926

sc/source/filter/xml/xmlstyle.cxx
@@ -623,7 +623,7 @@ void ScXMLAutoStylePoolP::exportStyleAttributes(
     {
         ::std::vector< XMLPropertyState >::const_iterator i(rProperties.begin());
         ::std::vector< XMLPropertyState >::const_iterator endi(rProperties.end());
-        while (i != endi)
+        for(; i != endi; ++i)
         {
             rtl::Reference< XMLPropertySetMapper > aPropMapper(rScXMLExport.GetCellStylesPropertySetMapper());
             sal_Int16 nContextID(aPropMapper->GetEntryContextId(i->mnIndex));
@@ -646,14 +646,13 @@ void ScXMLAutoStylePoolP::exportStyleAttributes(
                 }
                 break;
             }
-            ++i;
         }
     }
     else if (nFamily == XML_STYLE_FAMILY_TABLE_TABLE)
     {
         ::std::vector< XMLPropertyState >::const_iterator i(rProperties.begin());
         ::std::vector< XMLPropertyState >::const_iterator endi(rProperties.end());
-        while(i != endi)
+        for(; i != endi; ++i)
         {
             rtl::Reference< XMLPropertySetMapper > aPropMapper(rScXMLExport.GetTableStylesPropertySetMapper());
             sal_Int16 nContextID(aPropMapper->GetEntryContextId(i->mnIndex));
@@ -692,7 +691,7 @@ void ScXMLAutoStylePoolP::exportStyleContent(
         bool bNotFound = true;
         ::std::vector< XMLPropertyState >::const_iterator i(rProperties.begin());
         ::std::vector< XMLPropertyState >::const_iterator endi(rProperties.end());
-        while ((i != endi) && bNotFound)
+        for(; i != endi && bNotFound; ++i)
         {
             if (i->mnIndex != -1)
             {
@@ -793,7 +792,6 @@ void ScXMLAutoStylePoolP::exportStyleContent(
                     break;
                 }
             }
-            ++i;
         }
     }
 }

zhirshya/lo-tblr

commit 0b09d16a72326971391da33efdeb2e6c6474a0b7

Resolves: fdo#83633 remove additional ++i
Regression from 63ea2d467b50452ae0cab18b4151107a747ae70d

Date:   Wed Jul 2 19:10:24 2014 +0200
replace while with for

Change-Id: I151e447446b9793cff6abe7633eca4d11059a390

sc/source/filter/xml/xmlstyle.cxx
@@ -671,7 +671,6 @@ void ScXMLAutoStylePoolP::exportStyleAttributes(
                 }
                 break;
             }
-            ++i;
         }
     }
 }