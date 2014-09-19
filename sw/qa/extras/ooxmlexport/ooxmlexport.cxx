LibreOffice/core

commit 614b31615e69b527e669635245cf59faf9c9e2fc

Use NS_ooxml::LN_Value_ST_Jc_{right,end} constants
Change-Id: Ifee3d97fb9a3037baeb838a676ff753006b7c861

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -3397,7 +3397,7 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
             nValue = 1; // center
             break;
         case 2:
-            nValue = 2; // right
+            nValue = NS_ooxml::LN_Value_ST_Jc_right;
             break;
         }
         pIntValue.reset(new RTFValue(nValue));

writerfilter/source/dmapper/DomainMapper.cxx
@@ -3268,7 +3268,8 @@ void DomainMapper::handleParaJustification(const sal_Int32 nIntValue, const ::bo
         nAdjust = style::ParagraphAdjust_CENTER;
         aStringValue = "center";
         break;
-    case 2:
+    case NS_ooxml::LN_Value_ST_Jc_right:
+    case NS_ooxml::LN_Value_ST_Jc_end:
         nAdjust = static_cast< sal_Int16 > (bExchangeLeftRight ? style::ParagraphAdjust_LEFT : style::ParagraphAdjust_RIGHT);
         aStringValue = "right";
         break;

writerfilter/source/dmapper/NumberingManager.cxx
@@ -1014,7 +1014,8 @@ void ListsManager::lcl_sprm( Sprm& rSprm )
                 case 1:
                     nValue = text::HoriOrientation::CENTER;
                     break;
-                case 2:
+                case NS_ooxml::LN_Value_ST_Jc_right:
+                case NS_ooxml::LN_Value_ST_Jc_end:
                     nValue = text::HoriOrientation::RIGHT;
                     break;
                 }

writerfilter/source/ooxml/model.xml
@@ -17131,10 +17131,10 @@
     </resource>
     <resource name="ST_Jc" resource="List">
       <value tokenid="ooxml:Value_ST_Jc_left">left</value>
-      <value tokenid="2">right</value>
+      <value tokenid="ooxml:Value_ST_Jc_right">right</value>
       <value tokenid="ooxml:Value_ST_Jc_start">start</value>
       <value tokenid="1">center</value>
-      <value tokenid="2">end</value>
+      <value tokenid="ooxml:Value_ST_Jc_end">end</value>
       <value tokenid="3">both</value>
       <value tokenid="ooxml:Value_ST_Jc_mediumKashida">mediumKashida</value>
       <value tokenid="ooxml:Value_ST_Jc_distribute">distribute</value>

zhirshya/lo-tblr

commit be4e47e2f11eca04dcb0c3651d229c270d048c87

fdo#83728 DOCX import: fix table alignment
Regression from 614b31615e69b527e669635245cf59faf9c9e2fc (Use
NS_ooxml::LN_Value_ST_Jc_{right,end} constants, 2014-09-05), one more
remnant from doctok.

Change-Id: Ibab68eada337a3fe1bc919987a09a8b146f86ac9

writerfilter/source/dmapper/ConversionHelper.cxx
@@ -252,9 +252,15 @@ sal_Int16 convertTableJustification( sal_Int32 nIntValue )
     sal_Int16 nOrient = text::HoriOrientation::LEFT_AND_WIDTH;
     switch( nIntValue )
     {
-        case 1 : nOrient = text::HoriOrientation::CENTER; break;
-        case 2 : nOrient = text::HoriOrientation::RIGHT; break;
-        case 0 :
+        case NS_ooxml::LN_Value_ST_Jc_center:
+            nOrient = text::HoriOrientation::CENTER;
+            break;
+        case NS_ooxml::LN_Value_ST_Jc_right:
+        case NS_ooxml::LN_Value_ST_Jc_end:
+            nOrient = text::HoriOrientation::RIGHT;
+            break;
+        case NS_ooxml::LN_Value_ST_Jc_left:
+        case NS_ooxml::LN_Value_ST_Jc_start:
         //no break
         default:;
 

writerfilter/source/dmapper/TablePropertiesHandler.cxx
@@ -72,7 +72,6 @@ namespace dmapper {
             case NS_ooxml::LN_CT_TblPrBase_jc:
             case 0x5400: // sprmTJc
             {
-                //table justification 0: left, 1: center, 2: right
                 sal_Int16 nOrient = ConversionHelper::convertTableJustification( nIntValue );
                 TablePropertyMapPtr pTableMap( new TablePropertyMap );
                 pTableMap->setValue( TablePropertyMap::HORI_ORIENT, nOrient );

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2741,13 +2741,13 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
     switch (nKeyword)
     {
     case RTF_TRQL:
-        nParam = 0;
+        nParam = NS_ooxml::LN_Value_ST_Jc_left;
         break;
     case RTF_TRQC:
-        nParam = 1;
+        nParam = NS_ooxml::LN_Value_ST_Jc_center;
         break;
     case RTF_TRQR:
-        nParam = 2;
+        nParam = NS_ooxml::LN_Value_ST_Jc_right;
         break;
     default:
         break;

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -11,6 +11,7 @@
 
 #include <com/sun/star/awt/XBitmap.hpp>
 #include <com/sun/star/graphic/XGraphic.hpp>
+#include <com/sun/star/text/HoriOrientation.hpp>
 #include <com/sun/star/text/XTextRangeCompare.hpp>
 
 #include <string>
@@ -230,6 +231,15 @@ DECLARE_OOXMLEXPORT_TEST(testDropdownInCell, "dropdown-in-cell.docx")
     uno::Reference<text::XTextRange> xCell(xTable->getCellByName("B1"), uno::UNO_QUERY);
     uno::Reference<text::XTextRangeCompare> xTextRangeCompare(xCell, uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(sal_Int16(0), xTextRangeCompare->compareRegionStarts(xAnchor, xCell));
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTableAlignment, "table-alignment.docx")
+{
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    // This was LEFT_AND_WIDTH, i.e. table alignment wasn't imported correctly.
+    CPPUNIT_ASSERT_EQUAL(text::HoriOrientation::RIGHT, getProperty<sal_Int16>(xTable, "HoriOrient"));
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();