LibreOffice/core

commit fc49c052dbdbb5ab3b0a02a13143705f769b9662

writerfilter: RTF import: fix handling of associated char properties
These are all dependent on the active \lrtch \rtlch \loch \hich \dbch.

There does not appear to be a SPRM Id for CJK bold / italic / fontsize.

Change-Id: I055ac29700ccd3b32b02c3f7685629254a6c3fd6

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -3354,10 +3354,11 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
     switch (nKeyword)
     {
     case RTF_FS:
-        nSprm = NS_ooxml::LN_EG_RPrBase_sz;
-        break;
     case RTF_AFS:
-        nSprm = NS_ooxml::LN_EG_RPrBase_szCs;
+        nSprm = (m_aStates.top().isRightToLeft
+                        || m_aStates.top().eRunType == RTFParserState::HICH)
+                ? NS_ooxml::LN_EG_RPrBase_szCs
+                : NS_ooxml::LN_EG_RPrBase_sz;
         break;
     case RTF_ANIMTEXT:
         nSprm = NS_ooxml::LN_EG_RPrBase_effect;
@@ -3383,13 +3384,23 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
     switch (nKeyword)
     {
     case RTF_LANG:
-        nSprm = NS_ooxml::LN_CT_Language_val;
-        break;
-    case RTF_LANGFE:
-        nSprm = NS_ooxml::LN_CT_Language_eastAsia;
-        break;
     case RTF_ALANG:
-        nSprm = NS_ooxml::LN_CT_Language_bidi;
+        if (m_aStates.top().isRightToLeft || m_aStates.top().eRunType == RTFParserState::HICH)
+        {
+            nSprm = NS_ooxml::LN_CT_Language_bidi;
+        }
+        else if (m_aStates.top().eRunType == RTFParserState::DBCH)
+        {
+            nSprm = NS_ooxml::LN_CT_Language_eastAsia;
+        }
+        else
+        {
+            assert(m_aStates.top().eRunType == RTFParserState::LOCH);
+            nSprm = NS_ooxml::LN_CT_Language_val;
+        }
+        break;
+    case RTF_LANGFE: // this one is always CJK apparently
+        nSprm = NS_ooxml::LN_CT_Language_eastAsia;
         break;
     default:
         break;
@@ -4593,16 +4604,18 @@ int RTFDocumentImpl::dispatchToggle(RTFKeyword nKeyword, bool bParam, int nParam
     switch (nKeyword)
     {
     case RTF_B:
-        nSprm = NS_ooxml::LN_EG_RPrBase_b;
-        break;
     case RTF_AB:
-        nSprm = NS_ooxml::LN_EG_RPrBase_bCs;
+        nSprm = (m_aStates.top().isRightToLeft
+                        || m_aStates.top().eRunType == RTFParserState::HICH)
+                ? NS_ooxml::LN_EG_RPrBase_bCs
+                : NS_ooxml::LN_EG_RPrBase_b;
         break;
     case RTF_I:
-        nSprm = NS_ooxml::LN_EG_RPrBase_i;
-        break;
     case RTF_AI:
-        nSprm = NS_ooxml::LN_EG_RPrBase_iCs;
+        nSprm = (m_aStates.top().isRightToLeft
+                        || m_aStates.top().eRunType == RTFParserState::HICH)
+                ? NS_ooxml::LN_EG_RPrBase_iCs
+                : NS_ooxml::LN_EG_RPrBase_i;
         break;
     case RTF_OUTL:
         nSprm = NS_ooxml::LN_EG_RPrBase_outline;

zhirshya/lo-tblr

commit 32aef2cb1e639c19fbd4ace4fe0e002e4f0fa800

fdo#85812: RTF import: fix run type in new groups
Apparently the run type resets to LTR in a new group.

(regression from fc49c052dbdbb5ab3b0a02a13143705f769b9662)

Change-Id: I61c0fcbe4e44b488a14cc78823063a1a1ad4f4b1

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -4828,6 +4828,8 @@ int RTFDocumentImpl::pushState()
         m_aStates.push(m_aStates.top());
     }
     m_aStates.top().aDestinationText.setLength(0); // was copied: always reset!
+    m_aStates.top().isRightToLeft = false; // fdo#85812 group resets run type
+    m_aStates.top().eRunType = RTFParserState::LOCH;
 
     m_pTokenizer->pushGroup();
 

sw/qa/extras/rtfimport/data/fdo85812.rtf
@@ -1,0 +1,8 @@
+{\rtf1\ansi\deflang1049\deflangfe2052
+{\*\generator Microsoft Word 11.0.5604;}
+
+\pard\plain
+\lang1049\langfe2052\loch\hich\dbch\langnp1049\langfenp2052
+{\lang1033\langfe2052\dbch\langnp1033 \hich\dbch\loch This }
+{\lang1036\langfe2052\langnp1036 \par }
+}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -474,6 +474,16 @@ DECLARE_RTFIMPORT_TEST(testFdo48037, "fdo48037.rtf")
     CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
 }
 
+DECLARE_RTFIMPORT_TEST(testFdo85812, "fdo85812.rtf")
+{
+    lang::Locale locale(getProperty<lang::Locale>(
+                getRun(getParagraph(1), 1, "This "), "CharLocale"));
+    // the \lang inside the group was applied to CJK not Western
+    CPPUNIT_ASSERT_EQUAL(OUString("en"), locale.Language);
+    CPPUNIT_ASSERT_EQUAL(OUString("US"), locale.Country);
+
+}
+
 DECLARE_RTFIMPORT_TEST(testFdo47764, "fdo47764.rtf")
 {
     // \cbpat with zero argument should mean the auto (-1) color, not a default color (black)