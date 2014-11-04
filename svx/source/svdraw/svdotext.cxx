LibreOffice/core

Rename aRect -> maRect for a member variable.
Change-Id: I12dc6c12ee44753c63b15f03276cdaa24e57d395

svx/source/svdraw/svdotext.cxx
@@ -109,7 +109,7 @@ SdrTextObj::SdrTextObj()
 
 SdrTextObj::SdrTextObj(const Rectangle& rNewRect)
 :   SdrAttrObj(),
-    aRect(rNewRect),
+    maRect(rNewRect),
     mpText(NULL),
     pEdtOutl(NULL),
     pFormTextBoundRect(NULL),
@@ -122,7 +122,7 @@ SdrTextObj::SdrTextObj(const Rectangle& rNewRect)
     bNoRotate=false;
     bNoMirror=false;
     bDisableAutoWidthOnDragging=false;
-    ImpJustifyRect(aRect);
+    ImpJustifyRect(maRect);
 
     mbInEditMode = false;
     mbTextHidden = false;
@@ -161,7 +161,7 @@ SdrTextObj::SdrTextObj(SdrObjKind eNewTextKind)
 
 SdrTextObj::SdrTextObj(SdrObjKind eNewTextKind, const Rectangle& rNewRect)
 :   SdrAttrObj(),
-    aRect(rNewRect),
+    maRect(rNewRect),
     mpText(NULL),
     pEdtOutl(NULL),
     pFormTextBoundRect(NULL),
@@ -174,7 +174,7 @@ SdrTextObj::SdrTextObj(SdrObjKind eNewTextKind, const Rectangle& rNewRect)
     bNoRotate=false;
     bNoMirror=true;
     bDisableAutoWidthOnDragging=false;
-    ImpJustifyRect(aRect);
+    ImpJustifyRect(maRect);
 
     mbInEditMode = false;
     mbTextHidden = false;
@@ -205,13 +205,13 @@ SdrTextObj::~SdrTextObj()
 void SdrTextObj::FitFrameToTextSize()
 {
     DBG_ASSERT(pModel!=NULL,"SdrTextObj::FitFrameToTextSize(): pModel=NULL!");
-    ImpJustifyRect(aRect);
+    ImpJustifyRect(maRect);
 
     SdrText* pText = getActiveText();
     if( pText!=NULL && pText->GetOutlinerParaObject() && pModel!=NULL)
     {
         SdrOutliner& rOutliner=ImpGetDrawOutliner();
-        rOutliner.SetPaperSize(Size(aRect.Right()-aRect.Left(),aRect.Bottom()-aRect.Top()));
+        rOutliner.SetPaperSize(Size(maRect.Right()-maRect.Left(),maRect.Bottom()-maRect.Top()));
         rOutliner.SetUpdateMode(true);
         rOutliner.SetText(*pText->GetOutlinerParaObject());
         Size aNewSize(rOutliner.CalcTextSize());
@@ -219,10 +219,10 @@ void SdrTextObj::FitFrameToTextSize()
         aNewSize.Width()++; // because of possible rounding errors
         aNewSize.Width()+=GetTextLeftDistance()+GetTextRightDistance();
         aNewSize.Height()+=GetTextUpperDistance()+GetTextLowerDistance();
-        Rectangle aNewRect(aRect);
+        Rectangle aNewRect(maRect);
         aNewRect.SetSize(aNewSize);
         ImpJustifyRect(aNewRect);
-        if (aNewRect!=aRect) {
+        if (aNewRect!=maRect) {
             SetLogicRect(aNewRect);
         }
     }
@@ -573,7 +573,7 @@ void SdrTextObj::AdaptTextMinSize()
     {
         // Set minimum width.
         const long nDist = GetTextLeftDistance() + GetTextRightDistance();
-        const long nW = std::max<long>(0, aRect.GetWidth() - 1 - nDist); // text width without margins
+        const long nW = std::max<long>(0, maRect.GetWidth() - 1 - nDist); // text width without margins
 
         aSet.Put(makeSdrTextMinFrameWidthItem(nW));
 
@@ -588,7 +588,7 @@ void SdrTextObj::AdaptTextMinSize()
     {
         // Set Minimum height.
         const long nDist = GetTextUpperDistance() + GetTextLowerDistance();
-        const long nH = std::max<long>(0, aRect.GetHeight() - 1 - nDist); // text height without margins
+        const long nH = std::max<long>(0, maRect.GetHeight() - 1 - nDist); // text height without margins
 
         aSet.Put(makeSdrTextMinFrameHeightItem(nH));
 
@@ -662,7 +662,7 @@ void SdrTextObj::ImpSetContourPolygon( SdrOutliner& rOutliner, Rectangle& rAncho
 
 void SdrTextObj::TakeUnrotatedSnapRect(Rectangle& rRect) const
 {
-    rRect=aRect;
+    rRect=maRect;
 }
 
 void SdrTextObj::TakeTextAnchorRect(Rectangle& rAnchorRect) const
@@ -671,7 +671,7 @@ void SdrTextObj::TakeTextAnchorRect(Rectangle& rAnchorRect) const
     long nRightDist=GetTextRightDistance();
     long nUpperDist=GetTextUpperDistance();
     long nLowerDist=GetTextLowerDistance();
-    Rectangle aAnkRect(aRect); // the rectangle in which we anchor
+    Rectangle aAnkRect(maRect); // the rectangle in which we anchor
     bool bFrame=IsTextFrame();
     if (!bFrame) {
         TakeUnrotatedSnapRect(aAnkRect);
@@ -1085,7 +1085,7 @@ SdrTextObj& SdrTextObj::operator=(const SdrTextObj& rObj)
     // call parent
     SdrObject::operator=(rObj);
 
-    aRect     =rObj.aRect;
+    maRect = rObj.maRect;
     aGeo      =rObj.aGeo;
     eTextKind =rObj.eTextKind;
     bTextFrame=rObj.bTextFrame;
@@ -1122,9 +1122,9 @@ SdrTextObj& SdrTextObj::operator=(const SdrTextObj& rObj)
 
 basegfx::B2DPolyPolygon SdrTextObj::TakeXorPoly() const
 {
-    Polygon aPol(aRect);
-    if (aGeo.nShearWink!=0) ShearPoly(aPol,aRect.TopLeft(),aGeo.nTan);
-    if (aGeo.nDrehWink!=0) RotatePoly(aPol,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+    Polygon aPol(maRect);
+    if (aGeo.nShearWink!=0) ShearPoly(aPol,maRect.TopLeft(),aGeo.nTan);
+    if (aGeo.nDrehWink!=0) RotatePoly(aPol,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
 
     basegfx::B2DPolyPolygon aRetval;
     aRetval.append(aPol.getB2DPolygon());
@@ -1161,12 +1161,12 @@ basegfx::B2DPolyPolygon SdrTextObj::TakeContour() const
 void SdrTextObj::RecalcSnapRect()
 {
     if (aGeo.nDrehWink!=0 || aGeo.nShearWink!=0) {
-        Polygon aPol(aRect);
-        if (aGeo.nShearWink!=0) ShearPoly(aPol,aRect.TopLeft(),aGeo.nTan);
-        if (aGeo.nDrehWink!=0) RotatePoly(aPol,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+        Polygon aPol(maRect);
+        if (aGeo.nShearWink!=0) ShearPoly(aPol,maRect.TopLeft(),aGeo.nTan);
+        if (aGeo.nDrehWink!=0) RotatePoly(aPol,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
         maSnapRect=aPol.GetBoundRect();
     } else {
-        maSnapRect=aRect;
+        maSnapRect = maRect;
     }
 }
 
@@ -1179,14 +1179,14 @@ Point SdrTextObj::GetSnapPoint(sal_uInt32 i) const
 {
     Point aP;
     switch (i) {
-        case 0: aP=aRect.TopLeft(); break;
-        case 1: aP=aRect.TopRight(); break;
-        case 2: aP=aRect.BottomLeft(); break;
-        case 3: aP=aRect.BottomRight(); break;
-        default: aP=aRect.Center(); break;
+        case 0: aP=maRect.TopLeft(); break;
+        case 1: aP=maRect.TopRight(); break;
+        case 2: aP=maRect.BottomLeft(); break;
+        case 3: aP=maRect.BottomRight(); break;
+        default: aP=maRect.Center(); break;
     }
-    if (aGeo.nShearWink!=0) ShearPoint(aP,aRect.TopLeft(),aGeo.nTan);
-    if (aGeo.nDrehWink!=0) RotatePoint(aP,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+    if (aGeo.nShearWink!=0) ShearPoint(aP,maRect.TopLeft(),aGeo.nTan);
+    if (aGeo.nDrehWink!=0) RotatePoint(aP,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
     return aP;
 }
 
@@ -1471,7 +1471,7 @@ void SdrTextObj::SaveGeoData(SdrObjGeoData& rGeo) const
 {
     SdrAttrObj::SaveGeoData(rGeo);
     SdrTextObjGeoData& rTGeo=(SdrTextObjGeoData&)rGeo;
-    rTGeo.aRect  =aRect;
+    rTGeo.aRect = maRect;
     rTGeo.aGeo   =aGeo;
 }
 
@@ -1492,6 +1492,11 @@ SdrFitToSizeType SdrTextObj::GetFitToSize() const
         eType = ((SdrTextFitToSizeTypeItem&)(GetObjectItem(SDRATTR_TEXT_FITTOSIZE))).GetValue();
 
     return eType;
+}
+
+const Rectangle& SdrTextObj::GetGeoRect() const
+{
+    return maRect;
 }
 
 void SdrTextObj::ForceOutlinerParaObject()
@@ -1607,7 +1612,7 @@ bool SdrTextObj::TRGetBaseGeometry(basegfx::B2DHomMatrix& rMatrix, basegfx::B2DP
     double fShearX = (aGeo.nShearWink / 100.0) * F_PI180;
 
     // get aRect, this is the unrotated snaprect
-    Rectangle aRectangle(aRect);
+    Rectangle aRectangle(maRect);
 
     // fill other values
     basegfx::B2DTuple aScale(aRectangle.GetWidth(), aRectangle.GetHeight());

svx/source/svdraw/svdotxdr.cxx
@@ -43,17 +43,17 @@ SdrHdl* SdrTextObj::GetHdl(sal_uInt32 nHdlNum) const
     Point aPnt;
     SdrHdlKind eKind=HDL_MOVE;
     switch (nHdlNum) {
-        case 0: aPnt=aRect.TopLeft();      eKind=HDL_UPLFT; break;
-        case 1: aPnt=aRect.TopCenter();    eKind=HDL_UPPER; break;
-        case 2: aPnt=aRect.TopRight();     eKind=HDL_UPRGT; break;
-        case 3: aPnt=aRect.LeftCenter();   eKind=HDL_LEFT ; break;
-        case 4: aPnt=aRect.RightCenter();  eKind=HDL_RIGHT; break;
-        case 5: aPnt=aRect.BottomLeft();   eKind=HDL_LWLFT; break;
-        case 6: aPnt=aRect.BottomCenter(); eKind=HDL_LOWER; break;
-        case 7: aPnt=aRect.BottomRight();  eKind=HDL_LWRGT; break;
+        case 0: aPnt=maRect.TopLeft();      eKind=HDL_UPLFT; break;
+        case 1: aPnt=maRect.TopCenter();    eKind=HDL_UPPER; break;
+        case 2: aPnt=maRect.TopRight();     eKind=HDL_UPRGT; break;
+        case 3: aPnt=maRect.LeftCenter();   eKind=HDL_LEFT ; break;
+        case 4: aPnt=maRect.RightCenter();  eKind=HDL_RIGHT; break;
+        case 5: aPnt=maRect.BottomLeft();   eKind=HDL_LWLFT; break;
+        case 6: aPnt=maRect.BottomCenter(); eKind=HDL_LOWER; break;
+        case 7: aPnt=maRect.BottomRight();  eKind=HDL_LWRGT; break;
     }
-    if (aGeo.nShearWink!=0) ShearPoint(aPnt,aRect.TopLeft(),aGeo.nTan);
-    if (aGeo.nDrehWink!=0) RotatePoint(aPnt,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+    if (aGeo.nShearWink!=0) ShearPoint(aPnt,maRect.TopLeft(),aGeo.nTan);
+    if (aGeo.nDrehWink!=0) RotatePoint(aPnt,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
     if (eKind!=HDL_MOVE) {
         pH=new SdrHdl(aPnt,eKind);
         pH->SetObj((SdrObject*)this);
@@ -71,7 +71,7 @@ bool SdrTextObj::hasSpecialDrag() const
 
 Rectangle SdrTextObj::ImpDragCalcRect(const SdrDragStat& rDrag) const
 {
-    Rectangle aTmpRect(aRect);
+    Rectangle aTmpRect(maRect);
     const SdrHdl* pHdl=rDrag.GetHdl();
     SdrHdlKind eHdl=pHdl==NULL ? HDL_MOVE : pHdl->GetKind();
     bool bEcke=(eHdl==HDL_UPLFT || eHdl==HDL_UPRGT || eHdl==HDL_LWLFT || eHdl==HDL_LWRGT);
@@ -92,8 +92,8 @@ Rectangle SdrTextObj::ImpDragCalcRect(const SdrDragStat& rDrag) const
     if (bTop) aTmpRect.Top()   =aPos.Y();
     if (bBtm) aTmpRect.Bottom()=aPos.Y();
     if (bOrtho) { // Ortho
-        long nWdt0=aRect.Right() -aRect.Left();
-        long nHgt0=aRect.Bottom()-aRect.Top();
+        long nWdt0=maRect.Right() -maRect.Left();
+        long nHgt0=maRect.Bottom()-maRect.Top();
         long nXMul=aTmpRect.Right() -aTmpRect.Left();
         long nYMul=aTmpRect.Bottom()-aTmpRect.Top();
         long nXDiv=nWdt0;
@@ -125,13 +125,13 @@ Rectangle SdrTextObj::ImpDragCalcRect(const SdrDragStat& rDrag) const
             }
         } else { // apex handles
             if ((bLft || bRgt) && nXDiv!=0) {
-                long nHgt0b=aRect.Bottom()-aRect.Top();
+                long nHgt0b=maRect.Bottom()-maRect.Top();
                 long nNeed=long(BigInt(nHgt0b)*BigInt(nXMul)/BigInt(nXDiv));
                 aTmpRect.Top()-=(nNeed-nHgt0b)/2;
                 aTmpRect.Bottom()=aTmpRect.Top()+nNeed;
             }
             if ((bTop || bBtm) && nYDiv!=0) {
-                long nWdt0b=aRect.Right()-aRect.Left();
+                long nWdt0b=maRect.Right()-maRect.Left();
                 long nNeed=long(BigInt(nWdt0b)*BigInt(nYMul)/BigInt(nYDiv));
                 aTmpRect.Left()-=(nNeed-nWdt0b)/2;
                 aTmpRect.Right()=aTmpRect.Left()+nNeed;
@@ -150,22 +150,22 @@ bool SdrTextObj::applySpecialDrag(SdrDragStat& rDrag)
 {
     Rectangle aNewRect(ImpDragCalcRect(rDrag));
 
-    if(aNewRect.TopLeft() != aRect.TopLeft() && (aGeo.nDrehWink || aGeo.nShearWink))
+    if (aNewRect.TopLeft() != maRect.TopLeft() && (aGeo.nDrehWink || aGeo.nShearWink))
     {
         Point aNewPos(aNewRect.TopLeft());
 
         if(aGeo.nShearWink)
-            ShearPoint(aNewPos,aRect.TopLeft(),aGeo.nTan);
+            ShearPoint(aNewPos,maRect.TopLeft(),aGeo.nTan);
 
         if(aGeo.nDrehWink)
-            RotatePoint(aNewPos,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+            RotatePoint(aNewPos,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
 
         aNewRect.SetPos(aNewPos);
     }
 
-    if(aNewRect != aRect)
+    if (aNewRect != maRect)
     {
-          NbcSetLogicRect(aNewRect);
+        NbcSetLogicRect(aNewRect);
     }
 
     return true;
@@ -187,7 +187,7 @@ bool SdrTextObj::BegCreate(SdrDragStat& rStat)
     Rectangle aRect1(rStat.GetStart(), rStat.GetNow());
     aRect1.Justify();
     rStat.SetActionRect(aRect1);
-    aRect = aRect1;
+    maRect = aRect1;
     return true;
 }
 
@@ -197,7 +197,7 @@ bool SdrTextObj::MovCreate(SdrDragStat& rStat)
     rStat.TakeCreateRect(aRect1);
     ImpJustifyRect(aRect1);
     rStat.SetActionRect(aRect1);
-    aRect=aRect1; // for ObjName
+    maRect = aRect1; // for ObjName
     SetBoundRectDirty();
     bSnapRectDirty=true;
     if (HAS_BASE(SdrRectObj,this)) {
@@ -208,8 +208,8 @@ bool SdrTextObj::MovCreate(SdrDragStat& rStat)
 
 bool SdrTextObj::EndCreate(SdrDragStat& rStat, SdrCreateCmd eCmd)
 {
-    rStat.TakeCreateRect(aRect);
-    ImpJustifyRect(aRect);
+    rStat.TakeCreateRect(maRect);
+    ImpJustifyRect(maRect);
 
     // #115391#
     AdaptTextMinSize();

zhirshya/lo-tblr

Conflicts:
	svx/source/svdraw/svdoashp.cxx
	svx/source/svdraw/svdocirc.cxx
	svx/source/svdraw/svdograf.cxx
	svx/source/svdraw/svdoole2.cxx
	svx/source/svdraw/svdorect.cxx
	svx/source/svdraw/svdotext.cxx
	svx/source/svdraw/svdotxdr.cxx
	svx/source/svdraw/svdotxtr.cxx

svx/source/svdraw/svdotext.cxx
@@ -109,7 +109,7 @@ SdrTextObj::SdrTextObj()
 
 SdrTextObj::SdrTextObj(const Rectangle& rNewRect)
 :   SdrAttrObj(),
-    aRect(rNewRect),
+    maRect(rNewRect),
     mpText(NULL),
     pEdtOutl(NULL),
     pFormTextBoundRect(NULL),
@@ -122,7 +122,7 @@ SdrTextObj::SdrTextObj(const Rectangle& rNewRect)
     bNoRotate=false;
     bNoMirror=false;
     bDisableAutoWidthOnDragging=false;
-    ImpJustifyRect(aRect);
+    ImpJustifyRect(maRect);
 
     mbInEditMode = false;
     mbTextHidden = false;
@@ -161,7 +161,7 @@ SdrTextObj::SdrTextObj(SdrObjKind eNewTextKind)
 
 SdrTextObj::SdrTextObj(SdrObjKind eNewTextKind, const Rectangle& rNewRect)
 :   SdrAttrObj(),
-    aRect(rNewRect),
+    maRect(rNewRect),
     mpText(NULL),
     pEdtOutl(NULL),
     pFormTextBoundRect(NULL),
@@ -174,7 +174,7 @@ SdrTextObj::SdrTextObj(SdrObjKind eNewTextKind, const Rectangle& rNewRect)
     bNoRotate=false;
     bNoMirror=true;
     bDisableAutoWidthOnDragging=false;
-    ImpJustifyRect(aRect);
+    ImpJustifyRect(maRect);
 
     mbInEditMode = false;
     mbTextHidden = false;
@@ -205,13 +205,13 @@ SdrTextObj::~SdrTextObj()
 void SdrTextObj::FitFrameToTextSize()
 {
     DBG_ASSERT(pModel!=NULL,"SdrTextObj::FitFrameToTextSize(): pModel=NULL!");
-    ImpJustifyRect(aRect);
+    ImpJustifyRect(maRect);
 
     SdrText* pText = getActiveText();
     if( pText!=NULL && pText->GetOutlinerParaObject() && pModel!=NULL)
     {
         SdrOutliner& rOutliner=ImpGetDrawOutliner();
-        rOutliner.SetPaperSize(Size(aRect.Right()-aRect.Left(),aRect.Bottom()-aRect.Top()));
+        rOutliner.SetPaperSize(Size(maRect.Right()-maRect.Left(),maRect.Bottom()-maRect.Top()));
         rOutliner.SetUpdateMode(true);
         rOutliner.SetText(*pText->GetOutlinerParaObject());
         Size aNewSize(rOutliner.CalcTextSize());
@@ -219,10 +219,10 @@ void SdrTextObj::FitFrameToTextSize()
         aNewSize.Width()++; // because of possible rounding errors
         aNewSize.Width()+=GetTextLeftDistance()+GetTextRightDistance();
         aNewSize.Height()+=GetTextUpperDistance()+GetTextLowerDistance();
-        Rectangle aNewRect(aRect);
+        Rectangle aNewRect(maRect);
         aNewRect.SetSize(aNewSize);
         ImpJustifyRect(aNewRect);
-        if (aNewRect!=aRect) {
+        if (aNewRect!=maRect) {
             SetLogicRect(aNewRect);
         }
     }
@@ -573,7 +573,7 @@ void SdrTextObj::AdaptTextMinSize()
     {
         // Set minimum width.
         const long nDist = GetTextLeftDistance() + GetTextRightDistance();
-        const long nW = std::max<long>(0, aRect.GetWidth() - 1 - nDist); // text width without margins
+        const long nW = std::max<long>(0, maRect.GetWidth() - 1 - nDist); // text width without margins
 
         aSet.Put(makeSdrTextMinFrameWidthItem(nW));
 
@@ -588,7 +588,7 @@ void SdrTextObj::AdaptTextMinSize()
     {
         // Set Minimum height.
         const long nDist = GetTextUpperDistance() + GetTextLowerDistance();
-        const long nH = std::max<long>(0, aRect.GetHeight() - 1 - nDist); // text height without margins
+        const long nH = std::max<long>(0, maRect.GetHeight() - 1 - nDist); // text height without margins
 
         aSet.Put(makeSdrTextMinFrameHeightItem(nH));
 
@@ -662,7 +662,7 @@ void SdrTextObj::ImpSetContourPolygon( SdrOutliner& rOutliner, Rectangle& rAncho
 
 void SdrTextObj::TakeUnrotatedSnapRect(Rectangle& rRect) const
 {
-    rRect=aRect;
+    rRect=maRect;
 }
 
 void SdrTextObj::TakeTextAnchorRect(Rectangle& rAnchorRect) const
@@ -671,7 +671,7 @@ void SdrTextObj::TakeTextAnchorRect(Rectangle& rAnchorRect) const
     long nRightDist=GetTextRightDistance();
     long nUpperDist=GetTextUpperDistance();
     long nLowerDist=GetTextLowerDistance();
-    Rectangle aAnkRect(aRect); // the rectangle in which we anchor
+    Rectangle aAnkRect(maRect); // the rectangle in which we anchor
     bool bFrame=IsTextFrame();
     if (!bFrame) {
         TakeUnrotatedSnapRect(aAnkRect);
@@ -1085,7 +1085,7 @@ SdrTextObj& SdrTextObj::operator=(const SdrTextObj& rObj)
     // call parent
     SdrObject::operator=(rObj);
 
-    aRect     =rObj.aRect;
+    maRect = rObj.maRect;
     aGeo      =rObj.aGeo;
     eTextKind =rObj.eTextKind;
     bTextFrame=rObj.bTextFrame;
@@ -1122,9 +1122,9 @@ SdrTextObj& SdrTextObj::operator=(const SdrTextObj& rObj)
 
 basegfx::B2DPolyPolygon SdrTextObj::TakeXorPoly() const
 {
-    Polygon aPol(aRect);
-    if (aGeo.nShearAngle!=0) ShearPoly(aPol,aRect.TopLeft(),aGeo.nTan);
-    if (aGeo.nRotationAngle!=0) RotatePoly(aPol,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+    Polygon aPol(maRect);
+    if (aGeo.nShearAngle!=0) ShearPoly(aPol,maRect.TopLeft(),aGeo.nTan);
+    if (aGeo.nRotationAngle!=0) RotatePoly(aPol,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
 
     basegfx::B2DPolyPolygon aRetval;
     aRetval.append(aPol.getB2DPolygon());
@@ -1160,13 +1160,14 @@ basegfx::B2DPolyPolygon SdrTextObj::TakeContour() const
 
 void SdrTextObj::RecalcSnapRect()
 {
-    if (aGeo.nRotationAngle!=0 || aGeo.nShearAngle!=0) {
-        Polygon aPol(aRect);
-        if (aGeo.nShearAngle!=0) ShearPoly(aPol,aRect.TopLeft(),aGeo.nTan);
-        if (aGeo.nRotationAngle!=0) RotatePoly(aPol,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+    if (aGeo.nRotationAngle!=0 || aGeo.nShearAngle!=0)
+    {
+        Polygon aPol(maRect);
+        if (aGeo.nShearAngle!=0) ShearPoly(aPol,maRect.TopLeft(),aGeo.nTan);
+        if (aGeo.nRotationAngle!=0) RotatePoly(aPol,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
         maSnapRect=aPol.GetBoundRect();
     } else {
-        maSnapRect=aRect;
+        maSnapRect = maRect;
     }
 }
 
@@ -1179,14 +1180,14 @@ Point SdrTextObj::GetSnapPoint(sal_uInt32 i) const
 {
     Point aP;
     switch (i) {
-        case 0: aP=aRect.TopLeft(); break;
-        case 1: aP=aRect.TopRight(); break;
-        case 2: aP=aRect.BottomLeft(); break;
-        case 3: aP=aRect.BottomRight(); break;
-        default: aP=aRect.Center(); break;
+        case 0: aP=maRect.TopLeft(); break;
+        case 1: aP=maRect.TopRight(); break;
+        case 2: aP=maRect.BottomLeft(); break;
+        case 3: aP=maRect.BottomRight(); break;
+        default: aP=maRect.Center(); break;
     }
-    if (aGeo.nShearAngle!=0) ShearPoint(aP,aRect.TopLeft(),aGeo.nTan);
-    if (aGeo.nRotationAngle!=0) RotatePoint(aP,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+    if (aGeo.nShearAngle!=0) ShearPoint(aP,maRect.TopLeft(),aGeo.nTan);
+    if (aGeo.nRotationAngle!=0) RotatePoint(aP,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
     return aP;
 }
 
@@ -1471,7 +1472,7 @@ void SdrTextObj::SaveGeoData(SdrObjGeoData& rGeo) const
 {
     SdrAttrObj::SaveGeoData(rGeo);
     SdrTextObjGeoData& rTGeo=static_cast<SdrTextObjGeoData&>(rGeo);
-    rTGeo.aRect  =aRect;
+    rTGeo.aRect = maRect;
     rTGeo.aGeo   =aGeo;
 }
 
@@ -1492,6 +1493,11 @@ SdrFitToSizeType SdrTextObj::GetFitToSize() const
         eType = static_cast<const SdrTextFitToSizeTypeItem&>(GetObjectItem(SDRATTR_TEXT_FITTOSIZE)).GetValue();
 
     return eType;
+}
+
+const Rectangle& SdrTextObj::GetGeoRect() const
+{
+    return maRect;
 }
 
 void SdrTextObj::ForceOutlinerParaObject()
@@ -1607,7 +1613,7 @@ bool SdrTextObj::TRGetBaseGeometry(basegfx::B2DHomMatrix& rMatrix, basegfx::B2DP
     double fShearX = (aGeo.nShearAngle / 100.0) * F_PI180;
 
     // get aRect, this is the unrotated snaprect
-    Rectangle aRectangle(aRect);
+    Rectangle aRectangle(maRect);
 
     // fill other values
     basegfx::B2DTuple aScale(aRectangle.GetWidth(), aRectangle.GetHeight());

svx/source/svdraw/svdotxdr.cxx
@@ -43,17 +43,17 @@ SdrHdl* SdrTextObj::GetHdl(sal_uInt32 nHdlNum) const
     Point aPnt;
     SdrHdlKind eKind=HDL_MOVE;
     switch (nHdlNum) {
-        case 0: aPnt=aRect.TopLeft();      eKind=HDL_UPLFT; break;
-        case 1: aPnt=aRect.TopCenter();    eKind=HDL_UPPER; break;
-        case 2: aPnt=aRect.TopRight();     eKind=HDL_UPRGT; break;
-        case 3: aPnt=aRect.LeftCenter();   eKind=HDL_LEFT ; break;
-        case 4: aPnt=aRect.RightCenter();  eKind=HDL_RIGHT; break;
-        case 5: aPnt=aRect.BottomLeft();   eKind=HDL_LWLFT; break;
-        case 6: aPnt=aRect.BottomCenter(); eKind=HDL_LOWER; break;
-        case 7: aPnt=aRect.BottomRight();  eKind=HDL_LWRGT; break;
+        case 0: aPnt=maRect.TopLeft();      eKind=HDL_UPLFT; break;
+        case 1: aPnt=maRect.TopCenter();    eKind=HDL_UPPER; break;
+        case 2: aPnt=maRect.TopRight();     eKind=HDL_UPRGT; break;
+        case 3: aPnt=maRect.LeftCenter();   eKind=HDL_LEFT ; break;
+        case 4: aPnt=maRect.RightCenter();  eKind=HDL_RIGHT; break;
+        case 5: aPnt=maRect.BottomLeft();   eKind=HDL_LWLFT; break;
+        case 6: aPnt=maRect.BottomCenter(); eKind=HDL_LOWER; break;
+        case 7: aPnt=maRect.BottomRight();  eKind=HDL_LWRGT; break;
     }
-    if (aGeo.nShearAngle!=0) ShearPoint(aPnt,aRect.TopLeft(),aGeo.nTan);
-    if (aGeo.nRotationAngle!=0) RotatePoint(aPnt,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+    if (aGeo.nShearAngle!=0) ShearPoint(aPnt,maRect.TopLeft(),aGeo.nTan);
+    if (aGeo.nRotationAngle!=0) RotatePoint(aPnt,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
     if (eKind!=HDL_MOVE) {
         pH=new SdrHdl(aPnt,eKind);
         pH->SetObj((SdrObject*)this);
@@ -71,7 +71,7 @@ bool SdrTextObj::hasSpecialDrag() const
 
 Rectangle SdrTextObj::ImpDragCalcRect(const SdrDragStat& rDrag) const
 {
-    Rectangle aTmpRect(aRect);
+    Rectangle aTmpRect(maRect);
     const SdrHdl* pHdl=rDrag.GetHdl();
     SdrHdlKind eHdl=pHdl==NULL ? HDL_MOVE : pHdl->GetKind();
     bool bEcke=(eHdl==HDL_UPLFT || eHdl==HDL_UPRGT || eHdl==HDL_LWLFT || eHdl==HDL_LWRGT);
@@ -92,8 +92,8 @@ Rectangle SdrTextObj::ImpDragCalcRect(const SdrDragStat& rDrag) const
     if (bTop) aTmpRect.Top()   =aPos.Y();
     if (bBtm) aTmpRect.Bottom()=aPos.Y();
     if (bOrtho) { // Ortho
-        long nWdt0=aRect.Right() -aRect.Left();
-        long nHgt0=aRect.Bottom()-aRect.Top();
+        long nWdt0=maRect.Right() -maRect.Left();
+        long nHgt0=maRect.Bottom()-maRect.Top();
         long nXMul=aTmpRect.Right() -aTmpRect.Left();
         long nYMul=aTmpRect.Bottom()-aTmpRect.Top();
         long nXDiv=nWdt0;
@@ -125,13 +125,13 @@ Rectangle SdrTextObj::ImpDragCalcRect(const SdrDragStat& rDrag) const
             }
         } else { // apex handles
             if ((bLft || bRgt) && nXDiv!=0) {
-                long nHgt0b=aRect.Bottom()-aRect.Top();
+                long nHgt0b=maRect.Bottom()-maRect.Top();
                 long nNeed=long(BigInt(nHgt0b)*BigInt(nXMul)/BigInt(nXDiv));
                 aTmpRect.Top()-=(nNeed-nHgt0b)/2;
                 aTmpRect.Bottom()=aTmpRect.Top()+nNeed;
             }
             if ((bTop || bBtm) && nYDiv!=0) {
-                long nWdt0b=aRect.Right()-aRect.Left();
+                long nWdt0b=maRect.Right()-maRect.Left();
                 long nNeed=long(BigInt(nWdt0b)*BigInt(nYMul)/BigInt(nYDiv));
                 aTmpRect.Left()-=(nNeed-nWdt0b)/2;
                 aTmpRect.Right()=aTmpRect.Left()+nNeed;
@@ -150,22 +150,22 @@ bool SdrTextObj::applySpecialDrag(SdrDragStat& rDrag)
 {
     Rectangle aNewRect(ImpDragCalcRect(rDrag));
 
-    if(aNewRect.TopLeft() != aRect.TopLeft() && (aGeo.nRotationAngle || aGeo.nShearAngle))
+    if(aNewRect.TopLeft() != maRect.TopLeft() && (aGeo.nRotationAngle || aGeo.nShearAngle))
     {
         Point aNewPos(aNewRect.TopLeft());
 
         if(aGeo.nShearAngle)
-            ShearPoint(aNewPos,aRect.TopLeft(),aGeo.nTan);
+            ShearPoint(aNewPos,maRect.TopLeft(),aGeo.nTan);
 
         if(aGeo.nRotationAngle)
-            RotatePoint(aNewPos,aRect.TopLeft(),aGeo.nSin,aGeo.nCos);
+            RotatePoint(aNewPos,maRect.TopLeft(),aGeo.nSin,aGeo.nCos);
 
         aNewRect.SetPos(aNewPos);
     }
 
-    if(aNewRect != aRect)
+    if (aNewRect != maRect)
     {
-          NbcSetLogicRect(aNewRect);
+        NbcSetLogicRect(aNewRect);
     }
 
     return true;
@@ -187,7 +187,7 @@ bool SdrTextObj::BegCreate(SdrDragStat& rStat)
     Rectangle aRect1(rStat.GetStart(), rStat.GetNow());
     aRect1.Justify();
     rStat.SetActionRect(aRect1);
-    aRect = aRect1;
+    maRect = aRect1;
     return true;
 }
 
@@ -197,7 +197,7 @@ bool SdrTextObj::MovCreate(SdrDragStat& rStat)
     rStat.TakeCreateRect(aRect1);
     ImpJustifyRect(aRect1);
     rStat.SetActionRect(aRect1);
-    aRect=aRect1; // for ObjName
+    maRect = aRect1; // for ObjName
     SetBoundRectDirty();
     bSnapRectDirty=true;
     if (HAS_BASE(SdrRectObj,this)) {
@@ -208,8 +208,8 @@ bool SdrTextObj::MovCreate(SdrDragStat& rStat)
 
 bool SdrTextObj::EndCreate(SdrDragStat& rStat, SdrCreateCmd eCmd)
 {
-    rStat.TakeCreateRect(aRect);
-    ImpJustifyRect(aRect);
+    rStat.TakeCreateRect(maRect);
+    ImpJustifyRect(maRect);
 
     // #115391#
     AdaptTextMinSize();