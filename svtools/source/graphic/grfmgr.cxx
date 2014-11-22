LibreOffice/core

commit 286e2f5c6ec829bc0987b1be7016699f7ef03e5e

Related fdo#82953: Forget package URL of image after it is loaded
It causes problems if we handle those imported images differently which
are identified by a package URL, so after the first load remove
this URL and handle images on the same way as inserted images.

Some related bugs:
* #i44367#
* #i124946#
* #i114361#
* fdo#73270

The image in the test document has a special ID which is different
from that one which is generated by LO internally so after ODF export
the new generated image URL is different from the imported one.

Change-Id: I4e7d3490674c5f86bec5c7c6e1c975dcafd7c265

svx/source/svdraw/svdograf.cxx
@@ -1351,7 +1351,7 @@ IMPL_LINK( SdrGrafObj, ImpSwapHdl, GraphicObject*, pO )
                         const OUString aNewUserData( pGraphic->GetUserData() );
 
                         pGraphic->SetGraphic( aGraphic );
-                        pGraphic->SetUserData( aNewUserData );
+                        pGraphic->SetUserData();
 
                         // Graphic successfully swapped in.
                         pRet = GRFMGR_AUTOSWAPSTREAM_LOADED;

sw/CppunitTest_sw_odfexport.mk
@@ -80,6 +80,10 @@ $(eval $(call gb_CppunitTest_use_components,sw_odfexport,\
     xmloff/util/xo \
 ))
 
+$(eval $(call gb_CppunitTest_use_custom_headers,sw_odfexport,\
+    officecfg/registry \
+))
+
 $(eval $(call gb_CppunitTest_use_configuration,sw_odfexport))
 
 $(eval $(call gb_CppunitTest_use_unittest_configuration,sw_odfexport))

sw/qa/extras/odfexport/odfexport.cxx
@@ -17,6 +17,9 @@
 #include <com/sun/star/text/RelOrientation.hpp>
 #include <com/sun/star/text/XDocumentIndex.hpp>
 #include <com/sun/star/drawing/TextVerticalAdjust.hpp>
+#include <com/sun/star/awt/XBitmap.hpp>
+#include <com/sun/star/graphic/XGraphic.hpp>
+#include <officecfg/Office/Common.hxx>
 
 class Test : public SwModelTestBase
 {
@@ -399,6 +402,53 @@ DECLARE_ODFEXPORT_TEST(testTextboxRoundedCorners, "textbox-rounded-corners.odt")
     uno::Reference<text::XTextTable> xTable(getParagraphOrTable(1, xText), uno::UNO_QUERY);
     uno::Reference<text::XTextRange> xCell(xTable->getCellByName("A1"), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("a"), xCell->getString());
+}
+
+DECLARE_ODFEXPORT_TEST(testImageWithSpecialID, "document_with_two_images_with_special_IDs.odt")
+{
+    // Here the problem was that LO did not handle well those image URLs in the ODT file which are
+    // not match with that one which is generated by LO internaly (based on the image's size, type and so on)
+
+    // Trigger swap out mechanism to test swapped state factor too.
+    boost::shared_ptr< comphelper::ConfigurationChanges > batch(comphelper::ConfigurationChanges::create());
+    officecfg::Office::Common::Cache::GraphicManager::TotalCacheSize::set(sal_Int32(1), batch);
+    batch->commit();
+
+    uno::Reference<drawing::XShape> xImage = getShape(1);
+    uno::Reference< beans::XPropertySet > XPropSet( xImage, uno::UNO_QUERY_THROW );
+    // Check URL
+    {
+        OUString sURL;
+        XPropSet->getPropertyValue("GraphicURL") >>= sURL;
+        CPPUNIT_ASSERT(sURL != OUString("vnd.sun.star.GraphicObject:00000000000000000000000000000000"));
+    }
+    // Check size
+    {
+        uno::Reference<graphic::XGraphic> xGraphic;
+        XPropSet->getPropertyValue("Graphic") >>= xGraphic;
+        uno::Reference<awt::XBitmap> xBitmap(xGraphic, uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xBitmap.is());
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(610), xBitmap->getSize().Width );
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(381), xBitmap->getSize().Height );
+    }
+    // Second Image
+    xImage = getShape(2);
+    XPropSet.set( xImage, uno::UNO_QUERY_THROW );
+    // Check URL
+    {
+        OUString sURL;
+        XPropSet->getPropertyValue("GraphicURL") >>= sURL;
+        CPPUNIT_ASSERT(sURL != OUString("vnd.sun.star.GraphicObject:00000000000000000000000000000000"));
+    }
+    // Check size
+    {
+        uno::Reference<graphic::XGraphic> xGraphic;
+        XPropSet->getPropertyValue("Graphic") >>= xGraphic;
+        uno::Reference<awt::XBitmap> xBitmap(xGraphic, uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xBitmap.is());
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(900), xBitmap->getSize().Width );
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(600), xBitmap->getSize().Height );
+    }
 }
 
 #endif

sw/source/core/graphic/ndgrf.cxx
@@ -581,6 +581,10 @@ bool SwGrfNode::SwapIn( bool bWaitForData )
                 {
                     bRet = ImportGraphic( *pStrm );
                     delete pStrm;
+                    if( bRet )
+                    {
+                        maGrfObj.SetUserData();
+                    }
                 }
             }
             catch (const uno::Exception&)

zhirshya/lo-tblr

commit 1d2bf8699a543b4351acd65fce0a609712af794e

fdo#86573: Impress does not show pictures anymore
Regression from:
286e2f5c6ec829bc0987b1be7016699f7ef03e5e

Sometimes SdrGrafObj import the image file just as a
preview (with low resolution) and late when the real
picture is needed it just throws this preview and tries
to load the image again.
This preview works only with package URL so in this
case we should not forget that URL.

Plus two other related potential image loss situation:
- SetUserData() is called with empty image URL
- Image preview is thrown when we actually have no
package URL from we can import the image again.

Change-Id: Ib21f400eb6d28133e8598e6b201209e5dd1a976f

svtools/source/graphic/grfmgr.cxx
@@ -430,7 +430,8 @@ void GraphicObject::SetUserData()
 void GraphicObject::SetUserData( const OUString& rUserData )
 {
     maUserData = rUserData;
-    SetSwapState();
+    if( !rUserData.isEmpty() )
+        SetSwapState();
 }
 
 void GraphicObject::SetSwapStreamHdl()

svx/source/svdraw/svdograf.cxx
@@ -562,7 +562,7 @@ OUString SdrGrafObj::GetGrafStreamURL() const
 
 void SdrGrafObj::ForceSwapIn() const
 {
-    if( mbIsPreview )
+    if( mbIsPreview && pGraphic->HasUserData() )
     {
         // removing preview graphic
         const OUString aUserData( pGraphic->GetUserData() );
@@ -1347,8 +1347,16 @@ IMPL_LINK( SdrGrafObj, ImpSwapHdl, GraphicObject*, pO )
                         aGraphic, aUserData, *pStream,
                         GRFILTER_FORMAT_DONTKNOW, NULL, 0, pFilterData.get()))
                     {
+                        const OUString aNewUserData( pGraphic->GetUserData() );
                         pGraphic->SetGraphic( aGraphic );
-                        pGraphic->SetUserData();
+                        if( mbIsPreview )
+                        {
+                            pGraphic->SetUserData(aNewUserData);
+                        }
+                        else
+                        {
+                            pGraphic->SetUserData();
+                        }
 
                         // Graphic successfully swapped in.
                         pRet = GRFMGR_AUTOSWAPSTREAM_LOADED;