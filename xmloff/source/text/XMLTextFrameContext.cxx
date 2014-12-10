LibreOffice/core

commit bebf8ccfba37f77d6a43c7874249b31736467b17

i#98430: Import at least the shape.
The image that is inside is dropped though, we do not have support for that in
editeng.

Change-Id: I16dc1656e6f71fd80136bc39233decef4e1d4069

xmloff/source/text/txtimp.cxx
@@ -1167,8 +1167,12 @@ void XMLTextImportHelper::InsertTextContent(
     DBG_ASSERT(m_pImpl->m_xCursorAsRange.is(), "no range");
     if (m_pImpl->m_xText.is())
     {
-        m_pImpl->m_xText->insertTextContent(
-            m_pImpl->m_xCursorAsRange, xContent, sal_False);
+        try {
+            m_pImpl->m_xText->insertTextContent( m_pImpl->m_xCursorAsRange, xContent, sal_False);
+        } catch ( const lang::IllegalArgumentException & )
+        {
+            SAL_WARN( "xmloff", "Cannot import part of the text - probably an image in the text frame?" );
+        }
     }
 }
 

svx/source/unodraw/unomod.cxx
@@ -49,6 +49,7 @@
 #include <svx/svdobj.hxx>
 #include <svx/svdpage.hxx>
 #include <svx/unoshape.hxx>
+#include <svx/xmlgrhlp.hxx>
 
 #include <com/sun/star/text/textfield/Type.hpp>
 
@@ -182,6 +183,13 @@ uno::Reference< uno::XInterface > SAL_CALL SvxUnoDrawMSFactory::createInstance( 
 
             return uno::Reference< uno::XInterface >( (drawing::XShape*) SvxDrawPage::CreateShapeByTypeAndInventor( nT, nI ) );
         }
+    }
+    else if ( rServiceSpecifier == "com.sun.star.document.ImportGraphicObjectResolver" )
+    {
+        SvXMLGraphicHelper* pGraphicHelper = SvXMLGraphicHelper::Create( GRAPHICHELPER_MODE_READ );
+        uno::Reference< uno::XInterface> xRet( static_cast< ::cppu::OWeakObject* >( pGraphicHelper ) );
+        pGraphicHelper->release();
+        return xRet;
     }
 
     uno::Reference< uno::XInterface > xRet( createTextField( rServiceSpecifier ) );

zhirshya/lo-tblr

commit 1cbfe1cffed5cd37f4731bff1924cf4d9312d739

fdo#87110: xmloff: don't catch IllegalArgumentException here
There are at least 2 callers of InsertTextContent() that have
non-trivial catch handlers for this exception, which aren't called now.

(regression from bebf8ccfba37f77d6a43c7874249b31736467b17)

Change-Id: I085b710dfd5877e9b7e71610951543eddf6a6e46

xmloff/source/text/XMLTextFrameContext.cxx
@@ -759,7 +759,15 @@ void XMLTextFrameContext_Impl::Create( bool /*bHRefOrBase64*/ )
         XML_TEXT_FRAME_FLOATING_FRAME != nType)
     {
         Reference < XTextContent > xTxtCntnt( xPropSet, UNO_QUERY );
-        xTextImportHelper->InsertTextContent( xTxtCntnt );
+        try
+        {
+            xTextImportHelper->InsertTextContent(xTxtCntnt);
+        }
+        catch (lang::IllegalArgumentException const& e)
+        {
+            SAL_WARN("xmloff.text", "Cannot import part of the text - probably an image in the text frame? " << e.Message);
+            return;
+        }
     }
 
     // #107848#

xmloff/source/text/txtimp.cxx
@@ -1158,12 +1158,8 @@ void XMLTextImportHelper::InsertTextContent(
     assert(m_pImpl->m_xCursorAsRange.is());
     if (m_pImpl->m_xText.is())
     {
-        try {
-            m_pImpl->m_xText->insertTextContent( m_pImpl->m_xCursorAsRange, xContent, sal_False);
-        } catch ( const lang::IllegalArgumentException & )
-        {
-            SAL_WARN( "xmloff", "Cannot import part of the text - probably an image in the text frame?" );
-        }
+        // note: this may throw IllegalArgumentException and callers handle it
+        m_pImpl->m_xText->insertTextContent( m_pImpl->m_xCursorAsRange, xContent, sal_False);
     }
 }
 