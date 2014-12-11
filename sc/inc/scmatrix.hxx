LibreOffice/core

commit 8e8b43a03e77dd251876c1de0ac06eeeb09192cd

Generate boolean comparison result array in one step.
This is faster than doing it in two steps as was previously done.

Change-Id: I1417e9c6add9d20ee8d68f0c1c91c7d24a0f79b3

sc/inc/scmatrix.hxx
@@ -109,6 +109,8 @@ struct ScMatrixValue
  */
 class SC_DLLPUBLIC ScMatrix
 {
+    friend class ScMatrixImpl;
+
     ScMatrixImpl*   pImpl;
     mutable size_t  nRefCnt;    // reference count
 

sc/source/core/tool/scmatrix.cxx
@@ -1217,12 +1217,36 @@ class CompareMatrixFunc : std::unary_function<MatrixImplType::element_block_type
     sc::Compare& mrComp;
     size_t mnMatPos;
     sc::CompareOptions* mpOptions;
-
-    std::vector<double> maResValues;
+    std::vector<bool> maResValues;
 
     void compare()
     {
-        maResValues.push_back(sc::CompareFunc(mrComp, mpOptions));
+        double fVal = sc::CompareFunc(mrComp, mpOptions);
+        bool bRes = false;
+        switch (mrComp.meOp)
+        {
+            case SC_EQUAL:
+                bRes = fVal == 0.0;
+            break;
+            case SC_LESS:
+                bRes = fVal < 0.0;
+            break;
+            case SC_GREATER:
+                bRes = fVal > 0.0;
+            break;
+            case SC_LESS_EQUAL:
+                bRes = fVal <= 0.0;
+            break;
+            case SC_GREATER_EQUAL:
+                bRes = fVal >= 0.0;
+            break;
+            case SC_NOT_EQUAL:
+                bRes = fVal != 0.0;
+            break;
+            default:
+                OSL_TRACE( "CompareMatrixFunc: unhandled comparison operator: %d", (int)mrComp.meOp);
+        }
+        maResValues.push_back(bRes);
     }
 
 public:
@@ -1295,7 +1319,7 @@ public:
         }
     }
 
-    const std::vector<double>& getValues() const
+    const std::vector<bool>& getValues() const
     {
         return maResValues;
     }
@@ -1501,9 +1525,9 @@ void ScMatrixImpl::CompareMatrix(
     maMat.walk(aFunc);
 
     // We assume the result matrix has the same dimension as this matrix.
-    const std::vector<double>& rResVal = aFunc.getValues();
+    const std::vector<bool>& rResVal = aFunc.getValues();
     if (nSize == rResVal.size())
-        rResMat.PutDouble(&rResVal[0], rResVal.size(), 0, 0);
+        rResMat.pImpl->maMat.set(0, 0, rResVal.begin(), rResVal.end());
 }
 
 void ScMatrixImpl::GetDoubleArray( std::vector<double>& rArray, bool bEmptyAsZero ) const

sc/inc/compare.hxx
@@ -35,9 +35,11 @@ struct Compare
     bool bVal[2];
     bool bEmpty[2];
 
+    ScQueryOp meOp;
     bool mbIgnoreCase;
 
     Compare( OUString* p1, OUString* p2 ) :
+        meOp(SC_EQUAL),
         mbIgnoreCase(true)
     {
         pVal[0] = p1;

sc/source/core/tool/interpr1.cxx
@@ -883,6 +883,7 @@ sc::RangeMatrix ScInterpreter::CompareMat( ScQueryOp eOp, sc::CompareOptions* pO
 {
     OUString aVal1, aVal2;
     sc::Compare aComp( &aVal1, &aVal2 );
+    aComp.meOp = eOp;
     aComp.mbIgnoreCase = pDok->GetDocOptions().IsIgnoreCase();
     sc::RangeMatrix aMat[2];
     ScAddress aAdr;
@@ -1033,32 +1034,6 @@ sc::RangeMatrix ScInterpreter::CompareMat( ScQueryOp eOp, sc::CompareOptions* pO
         ScMatrix& rMat = *aMat[i].mpMat;
         ScMatrix& rResMat = *aRes.mpMat;
         rMat.CompareMatrix(rResMat, aComp, i, pOptions);
-
-        switch (eOp)
-        {
-            case SC_EQUAL:
-                aRes.mpMat->CompareEqual();
-                break;
-            case SC_LESS:
-                aRes.mpMat->CompareLess();
-                break;
-            case SC_GREATER:
-                aRes.mpMat->CompareGreater();
-                break;
-            case SC_LESS_EQUAL:
-                aRes.mpMat->CompareLessEqual();
-                break;
-            case SC_GREATER_EQUAL:
-                aRes.mpMat->CompareGreaterEqual();
-                break;
-            case SC_NOT_EQUAL:
-                aRes.mpMat->CompareNotEqual();
-                break;
-            default:
-                OSL_TRACE( "ScInterpreter::QueryMat: unhandled comparison operator: %d", (int)eOp);
-                aRes.mpMat.reset();
-                return aRes;
-        }
     }
 
     nCurFmtType = nFuncFmtType = NUMBERFORMAT_LOGICAL;

zhirshya/lo-tblr

commit 1bfa2e0a63a947e6c1079881b11d521f88ec5fff

resolved fdo#87237 propagate error values through matrix comparisons
Apparently introduced with 8e8b43a03e77dd251876c1de0ac06eeeb09192cd the
comparison results were stored as boolean values, effectively discarding
any infinite double values and error values encoded as NaN values.

Change-Id: I1fb6f46894a0bee02a37e28b7e6cc84f8c051f28

sc/inc/scmatrix.hxx
@@ -202,7 +202,7 @@ public:
     ScMatrix(SCSIZE nC, SCSIZE nR);
     ScMatrix(SCSIZE nC, SCSIZE nR, double fInitVal);
 
-    ScMatrix( size_t nC, size_t nR, const std::vector<bool>& rInitVals );
+    ScMatrix( size_t nC, size_t nR, const std::vector<double>& rInitVals );
 
     /** Clone the matrix. */
     ScMatrix* Clone() const;

sc/source/core/tool/scmatrix.cxx
@@ -70,51 +70,63 @@ typedef mdds::multi_type_matrix<custom_string_trait> MatrixImplType;
 
 namespace {
 
-struct ElemEqualZero : public unary_function<double, bool>
+struct ElemEqualZero : public unary_function<double, double>
 {
-    bool operator() (double val) const
+    double operator() (double val) const
     {
-        return val == 0.0;
+        if (!::rtl::math::isFinite(val))
+            return val;
+        return val == 0.0 ? 1.0 : 0.0;
     }
 };
 
-struct ElemNotEqualZero : public unary_function<double, bool>
+struct ElemNotEqualZero : public unary_function<double, double>
 {
-    bool operator() (double val) const
+    double operator() (double val) const
     {
-        return val != 0.0;
+        if (!::rtl::math::isFinite(val))
+            return val;
+        return val != 0.0 ? 1.0 : 0.0;
     }
 };
 
-struct ElemGreaterZero : public unary_function<double, bool>
+struct ElemGreaterZero : public unary_function<double, double>
 {
-    bool operator() (double val) const
+    double operator() (double val) const
     {
-        return val > 0.0;
+        if (!::rtl::math::isFinite(val))
+            return val;
+        return val > 0.0 ? 1.0 : 0.0;
     }
 };
 
-struct ElemLessZero : public unary_function<double, bool>
+struct ElemLessZero : public unary_function<double, double>
 {
-    bool operator() (double val) const
+    double operator() (double val) const
     {
-        return val < 0.0;
+        if (!::rtl::math::isFinite(val))
+            return val;
+        return val < 0.0 ? 1.0 : 0.0;
     }
 };
 
-struct ElemGreaterEqualZero : public unary_function<double, bool>
+struct ElemGreaterEqualZero : public unary_function<double, double>
 {
-    bool operator() (double val) const
+    double operator() (double val) const
     {
-        return val >= 0.0;
+        if (!::rtl::math::isFinite(val))
+            return val;
+        return val >= 0.0 ? 1.0 : 0.0;
     }
 };
 
-struct ElemLessEqualZero : public unary_function<double, bool>
+struct ElemLessEqualZero : public unary_function<double, double>
 {
-    bool operator() (double val) const
+    double operator() (double val) const
     {
-        return val <= 0.0;
+        if (!::rtl::math::isFinite(val))
+            return val;
+        return val <= 0.0 ? 1.0 : 0.0;
     }
 };
 
@@ -123,7 +135,7 @@ class CompareMatrixElemFunc : std::unary_function<MatrixImplType::element_block_
 {
     static _Comp maComp;
 
-    std::vector<bool> maNewMatValues;
+    std::vector<double> maNewMatValues;     // double instead of bool to transport error values
     size_t mnRow;
     size_t mnCol;
 public:
@@ -145,13 +157,6 @@ public:
                 for (; it != itEnd; ++it)
                 {
                     double fVal = *it;
-                    if (!rtl::math::isFinite(fVal))
-                    {
-                        /* FIXME: this silently skips an error instead of propagating it! */
-                        maNewMatValues.push_back(false);
-                        continue;
-                    }
-
                     maNewMatValues.push_back(maComp(fVal));
                 }
             }
@@ -173,7 +178,7 @@ public:
             case mdds::mtm::element_empty:
             default:
                 // Fill it with false.
-                maNewMatValues.resize(maNewMatValues.size() + node.size, false);
+                maNewMatValues.resize(maNewMatValues.size() + node.size, 0.0);
         }
     }
 
@@ -200,7 +205,7 @@ public:
     ScMatrixImpl(SCSIZE nC, SCSIZE nR);
     ScMatrixImpl(SCSIZE nC, SCSIZE nR, double fInitVal);
 
-    ScMatrixImpl( size_t nC, size_t nR, const std::vector<bool>& rInitVals );
+    ScMatrixImpl( size_t nC, size_t nR, const std::vector<double>& rInitVals );
 
     ~ScMatrixImpl();
 
@@ -295,7 +300,7 @@ ScMatrixImpl::ScMatrixImpl(SCSIZE nC, SCSIZE nR) :
 ScMatrixImpl::ScMatrixImpl(SCSIZE nC, SCSIZE nR, double fInitVal) :
     maMat(nR, nC, fInitVal), maMatFlag(nR, nC), pErrorInterpreter(NULL), mbCloneIfConst(true) {}
 
-ScMatrixImpl::ScMatrixImpl( size_t nC, size_t nR, const std::vector<bool>& rInitVals ) :
+ScMatrixImpl::ScMatrixImpl( size_t nC, size_t nR, const std::vector<double>& rInitVals ) :
     maMat(nR, nC, rInitVals.begin(), rInitVals.end()), maMatFlag(nR, nC), pErrorInterpreter(NULL), mbCloneIfConst(true) {}
 
 ScMatrixImpl::~ScMatrixImpl()
@@ -1284,32 +1289,35 @@ public:
     }
 };
 
-inline bool evaluate( double fVal, ScQueryOp eOp )
+inline double evaluate( double fVal, ScQueryOp eOp )
 {
+    if (!rtl::math::isFinite(fVal))
+        return fVal;
+
     switch (eOp)
     {
         case SC_EQUAL:
-            return fVal == 0.0;
+            return fVal == 0.0 ? 1.0 : 0.0;
         case SC_LESS:
-            return fVal < 0.0;
+            return fVal < 0.0 ? 1.0 : 0.0;
         case SC_GREATER:
-            return fVal > 0.0;
+            return fVal > 0.0 ? 1.0 : 0.0;
         break;
         case SC_LESS_EQUAL:
-            return fVal <= 0.0;
+            return fVal <= 0.0 ? 1.0 : 0.0;
         break;
         case SC_GREATER_EQUAL:
-            return fVal >= 0.0;
+            return fVal >= 0.0 ? 1.0 : 0.0;
         break;
         case SC_NOT_EQUAL:
-            return fVal != 0.0;
+            return fVal != 0.0 ? 1.0 : 0.0;
         break;
         default:
             ;
     }
 
     OSL_TRACE( "evaluate: unhandled comparison operator: %d", (int)eOp);
-    return false;
+    return CreateDoubleError( errUnknownState);
 }
 
 class CompareMatrixFunc : std::unary_function<MatrixImplType::element_block_type, void>
@@ -1317,7 +1325,7 @@ class CompareMatrixFunc : std::unary_function<MatrixImplType::element_block_type
     sc::Compare& mrComp;
     size_t mnMatPos;
     sc::CompareOptions* mpOptions;
-    std::vector<bool> maResValues;
+    std::vector<double> maResValues;    // double instead of bool to transport error values
 
     void compare()
     {
@@ -1397,7 +1405,7 @@ public:
         }
     }
 
-    const std::vector<bool>& getValues() const
+    const std::vector<double>& getValues() const
     {
         return maResValues;
     }
@@ -1411,7 +1419,7 @@ class CompareMatrixToNumericFunc : std::unary_function<MatrixImplType::element_b
     sc::Compare& mrComp;
     double mfRightValue;
     sc::CompareOptions* mpOptions;
-    std::vector<bool> maResValues;
+    std::vector<double> maResValues;    // double instead of bool to transport error values
 
     void compare()
     {
@@ -1489,7 +1497,7 @@ public:
         }
     }
 
-    const std::vector<bool>& getValues() const
+    const std::vector<double>& getValues() const
     {
         return maResValues;
     }
@@ -1711,7 +1719,7 @@ ScMatrixRef ScMatrixImpl::CompareMatrix(
             maMat.walk(aFunc);
 
             // We assume the result matrix has the same dimension as this matrix.
-            const std::vector<bool>& rResVal = aFunc.getValues();
+            const std::vector<double>& rResVal = aFunc.getValues();
             if (nSize != rResVal.size())
                 ScMatrixRef();
 
@@ -1723,7 +1731,7 @@ ScMatrixRef ScMatrixImpl::CompareMatrix(
     maMat.walk(aFunc);
 
     // We assume the result matrix has the same dimension as this matrix.
-    const std::vector<bool>& rResVal = aFunc.getValues();
+    const std::vector<double>& rResVal = aFunc.getValues();
     if (nSize != rResVal.size())
         ScMatrixRef();
 
@@ -1882,7 +1890,7 @@ ScMatrix::ScMatrix(SCSIZE nC, SCSIZE nR, double fInitVal) :
     SAL_WARN_IF( !nR, "sc", "ScMatrix with 0 rows!");
 }
 
-ScMatrix::ScMatrix( size_t nC, size_t nR, const std::vector<bool>& rInitVals ) :
+ScMatrix::ScMatrix( size_t nC, size_t nR, const std::vector<double>& rInitVals ) :
     pImpl(new ScMatrixImpl(nC, nR, rInitVals)), nRefCnt(0)
 {
     SAL_WARN_IF( !nC, "sc", "ScMatrix with 0 columns!");