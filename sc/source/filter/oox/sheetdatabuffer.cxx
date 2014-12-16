LibreOffice/core

commit 30a20743ae17e6e02183a65603d38968253b3ffb

Avoid using UNO API formula tokens.
Change-Id: Ide89b06a64405d006ac4b88cd3ae88b696f981f2

sc/source/filter/oox/sheetdatabuffer.cxx
@@ -53,6 +53,7 @@
 #include "docpool.hxx"
 #include "paramisc.hxx"
 #include "documentimport.hxx"
+#include "formulabuffer.hxx"
 
 namespace oox {
 namespace xls {
@@ -199,7 +200,9 @@ void SheetDataBuffer::setDateTimeCell( const CellModel& rModel, const ::com::sun
 
 void SheetDataBuffer::setBooleanCell( const CellModel& rModel, bool bValue )
 {
-    setCellFormula( rModel.maCellAddr, getFormulaParser().convertBoolToFormula( bValue ) );
+    getFormulaBuffer().setCellFormula(
+        rModel.maCellAddr, bValue ? OUString("TRUE()") : OUString("FALSE()"));
+
     // #108770# set 'Standard' number format for all Boolean cells
     setCellFormat( rModel, 0 );
 }
@@ -211,7 +214,12 @@ void SheetDataBuffer::setErrorCell( const CellModel& rModel, const OUString& rEr
 
 void SheetDataBuffer::setErrorCell( const CellModel& rModel, sal_uInt8 nErrorCode )
 {
-    setCellFormula( rModel.maCellAddr, getFormulaParser().convertErrorToFormula( nErrorCode ) );
+    OUStringBuffer aBuf;
+    aBuf.append(sal_Unicode('{'));
+    aBuf.append(BiffHelper::calcDoubleFromError(nErrorCode));
+    aBuf.append(sal_Unicode('}'));
+
+    getFormulaBuffer().setCellFormula(rModel.maCellAddr, aBuf.makeStringAndClear());
     setCellFormat( rModel );
 }
 

zhirshya/lo-tblr

commit 4fabb953bc9fb4a5b1500ea9baf7462ca2c49642

fdo#79249 call formula compiler with error string
... instead of attempting to stringize a NaN coded error value.

Regression introduced with 30a20743ae17e6e02183a65603d38968253b3ffb

Change-Id: Ia7a8ca39938820ac75db169404446fa696c6ee1b

sc/source/filter/oox/sheetdatabuffer.cxx
@@ -197,11 +197,16 @@ void SheetDataBuffer::setBooleanCell( const CellModel& rModel, bool bValue )
 
 void SheetDataBuffer::setErrorCell( const CellModel& rModel, const OUString& rErrorCode )
 {
-    setErrorCell( rModel, getUnitConverter().calcBiffErrorCode( rErrorCode ) );
+    // Using the formula compiler now we can simply pass on the error string.
+    getFormulaBuffer().setCellFormula( rModel.maCellAddr, rErrorCode);
+    setCellFormat( rModel );
 }
 
 void SheetDataBuffer::setErrorCell( const CellModel& rModel, sal_uInt8 nErrorCode )
 {
+    assert(!"stringizing any NaN will only give 'nan'");
+    /* FIXME: map nErrorCode to error string and call setErrorCell() above. */
+
     OUStringBuffer aBuf;
     aBuf.append('{');
     aBuf.append(BiffHelper::calcDoubleFromError(nErrorCode));