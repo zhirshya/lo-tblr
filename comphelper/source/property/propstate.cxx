LibreOffice/core

commit 60d60caf99a40ca0c3891bf230c5a1fdbae5f49c

Renamed XPropertySet2 to XPropertySetOption.
To avoid making it appear to be a derived interface of XPropertySet
when it's not.

cppuhelper/source/propshlp.cxx
@@ -218,7 +218,7 @@ Any OPropertySetHelper::queryInterface( const ::com::sun::star::uno::Type & rTyp
     return ::cppu::queryInterface(
         rType,
         static_cast< XPropertySet * >( this ),
-        static_cast< XPropertySet2 * >( this ),
+        static_cast< XPropertySetOption * >( this ),
         static_cast< XMultiPropertySet * >( this ),
         static_cast< XFastPropertySet * >( this ) );
 }
@@ -231,7 +231,7 @@ Any OPropertySetHelper::queryInterface( const ::com::sun::star::uno::Type & rTyp
 {
     Sequence< ::com::sun::star::uno::Type > aTypes( 4 );
     aTypes[ 0 ] = XPropertySet::static_type();
-    aTypes[ 1 ] = XPropertySet2::static_type();
+    aTypes[ 1 ] = XPropertySetOption::static_type();
     aTypes[ 2 ] = XMultiPropertySet::static_type();
     aTypes[ 3 ] = XFastPropertySet::static_type();
     return aTypes;

cppuhelper/inc/cppuhelper/propshlp.hxx
@@ -34,7 +34,7 @@
 #include <cppuhelper/interfacecontainer.hxx>
 
 #include <com/sun/star/beans/XPropertySet.hpp>
-#include <com/sun/star/beans/XPropertySet2.hpp>
+#include <com/sun/star/beans/XPropertySetOption.hpp>
 #include <com/sun/star/beans/XMultiPropertySet.hpp>
 #include <com/sun/star/beans/XFastPropertySet.hpp>
 
@@ -352,7 +352,7 @@ public:
 class OPropertySetHelper : public ::com::sun::star::beans::XMultiPropertySet,
                            public ::com::sun::star::beans::XFastPropertySet,
                            public ::com::sun::star::beans::XPropertySet,
-                           public ::com::sun::star::beans::XPropertySet2
+                           public ::com::sun::star::beans::XPropertySetOption
 {
 public:
     /**
@@ -506,7 +506,7 @@ public:
         const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertiesChangeListener > & Listener )
         throw(::com::sun::star::uno::RuntimeException);
 
-    // XPropertySet2
+    // XPropertySetOption
     virtual void SAL_CALL enableChangeListenerNotification( sal_Bool bEnable )
         throw(::com::sun::star::uno::RuntimeException);
 

cppuhelper/source/makefile.mk
@@ -61,7 +61,7 @@ UNOTYPES= \
         com.sun.star.beans.XMultiPropertySet \
         com.sun.star.beans.XPropertyAccess \
         com.sun.star.beans.XPropertySet \
-        com.sun.star.beans.XPropertySet2 \
+        com.sun.star.beans.XPropertySetOption \
         com.sun.star.bridge.UnoUrlResolver \
         com.sun.star.bridge.XUnoUrlResolver \
         com.sun.star.connection.SocketPermission \

cppuhelper/source/msvc_win32_intel.map
@@ -288,5 +288,5 @@ UDK_3.7 { # OOo 3.4
 UDK_3.8 { # LibO 3.5
     global:
         ?enableChangeListenerNotification@OPropertySetHelper@cppu@@UAAXE@Z;
-        ??_7OPropertySetHelper@cppu@@6BXPropertySet2@beans@star@sun@com@@@;
+        ??_7OPropertySetHelper@cppu@@6BXPropertySetOption@beans@star@sun@com@@@;
 } UDK_3.7;

sc/source/filter/ftools/fapihelper.cxx
@@ -34,7 +34,7 @@
 #include <com/sun/star/lang/XServiceName.hpp>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #include <com/sun/star/beans/XPropertyState.hpp>
-#include <com/sun/star/beans/XPropertySet2.hpp>
+#include <com/sun/star/beans/XPropertySetOption.hpp>
 #include <comphelper/docpasswordhelper.hxx>
 #include <comphelper/processfactory.hxx>
 #include <tools/urlobj.hxx>
@@ -169,18 +169,18 @@ uno::Sequence< beans::NamedValue > ScfApiHelper::QueryEncryptionDataForMedium( S
 
 ScfPropertySet::~ScfPropertySet()
 {
-    Reference<beans::XPropertySet2> xPropSet2(mxPropSet, UNO_QUERY);
-    if (xPropSet2.is())
-        xPropSet2->enableChangeListenerNotification(true);
+    Reference<beans::XPropertySetOption> xPropSetOpt(mxPropSet, UNO_QUERY);
+    if (xPropSetOpt.is())
+        xPropSetOpt->enableChangeListenerNotification(true);
 }
 
 void ScfPropertySet::Set( Reference< XPropertySet > xPropSet )
 {
     mxPropSet = xPropSet;
     mxMultiPropSet.set( mxPropSet, UNO_QUERY );
-    Reference<beans::XPropertySet2> xPropSet2(mxPropSet, UNO_QUERY);
-    if (xPropSet2.is())
-        xPropSet2->enableChangeListenerNotification(false);
+    Reference<beans::XPropertySetOption> xPropSetOpt(mxPropSet, UNO_QUERY);
+    if (xPropSetOpt.is())
+        xPropSetOpt->enableChangeListenerNotification(false);
 }
 
 OUString ScfPropertySet::GetServiceName() const

udkapi/UnoApi_udkapi.mk
@@ -159,10 +159,10 @@ $(eval $(call gb_UnoApiTarget_add_idlfiles,udkapi,\
 	udkapi/com/sun/star/beans/XPropertyChangeListener \
 	udkapi/com/sun/star/beans/XPropertyContainer \
 	udkapi/com/sun/star/beans/XPropertySet \
-	udkapi/com/sun/star/beans/XPropertySet2 \
 	udkapi/com/sun/star/beans/XPropertySetInfo \
 	udkapi/com/sun/star/beans/XPropertySetInfoChangeListener \
 	udkapi/com/sun/star/beans/XPropertySetInfoChangeNotifier \
+	udkapi/com/sun/star/beans/XPropertySetOption \
 	udkapi/com/sun/star/beans/XPropertyState \
 	udkapi/com/sun/star/beans/XPropertyStateChangeListener \
 	udkapi/com/sun/star/beans/XPropertyWithState \

udkapi/com/sun/star/beans/XPropertySetOption.idl
@@ -2,17 +2,11 @@
 #ifndef __com_sun_star_beans_XPropertySet2_idl__
 #define __com_sun_star_beans_XPropertySet2_idl__
 
-#include <com/sun/star/beans/XPropertySet.idl>
+#include <com/sun/star/uno/XInterface.idl>
 
 module com {  module sun {  module star {  module beans {
 
-/**
- * This interface should be conceptually derived from XPropertySet but isn't
- * due to ABI compatibility on Windows; cppu::OPrpertySetHelper exports
- * XPropertySet's vtable symbol in the UDK_3_0_0 section which must stay
- * there.  Once we hit LibreOffice 4 we should clean this mess up.
- */
-interface XPropertySet2: com::sun::star::uno::XInterface
+interface XPropertySetOption: com::sun::star::uno::XInterface
 {
     /**
      * Turn on or off notifying change listeners on property value change.  It

zhirshya/lo-tblr

commit aa17c9eaacc53e87eef002032ad206375794d1b1

Fix getTypes() of OPropertySetHelper and derived
...broken with 60d60caf99a40ca0c3891bf230c5a1fdbae5f49c "Renamed XPropertySet2
to XPropertySetOption" et al

Change-Id: I684736ffafc4642548b7c24171cc52c1acb32252

comphelper/source/property/propstate.cxx
@@ -55,15 +55,12 @@ namespace comphelper
 
     css::uno::Sequence<css::uno::Type> OPropertyStateHelper::getTypes()
     {
-        ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Type> aTypes(4);
-        ::com::sun::star::uno::Type* pTypes = aTypes.getArray();
-        // base class types
-        pTypes[0] = cppu::UnoType<com::sun::star::beans::XPropertySet>::get();
-        pTypes[1] = cppu::UnoType<com::sun::star::beans::XMultiPropertySet>::get();
-        pTypes[2] = cppu::UnoType<com::sun::star::beans::XFastPropertySet>::get();
-        // my own type
-        pTypes[3] = cppu::UnoType<com::sun::star::beans::XPropertyState>::get();
-        return aTypes;
+        return css::uno::Sequence<css::uno::Type>({
+            cppu::UnoType<css::beans::XPropertySet>::get(),
+            cppu::UnoType<css::beans::XMultiPropertySet>::get(),
+            cppu::UnoType<css::beans::XFastPropertySet>::get(),
+            cppu::UnoType<css::beans::XPropertySetOption>::get(),
+            cppu::UnoType<css::beans::XPropertyState>::get()});
     }
 
     OPropertyStateHelper::OPropertyStateHelper(

cppuhelper/source/propshlp.cxx
@@ -231,12 +231,10 @@ Any OPropertySetHelper2::queryInterface( const ::com::sun::star::uno::Type & rTy
 ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Type > OPropertySetHelper::getTypes()
     throw (RuntimeException)
 {
-    Sequence< ::com::sun::star::uno::Type > aTypes( 4 );
-    aTypes[ 0 ] = XPropertySet::static_type();
-    aTypes[ 1 ] = XPropertySetOption::static_type();
-    aTypes[ 2 ] = XMultiPropertySet::static_type();
-    aTypes[ 3 ] = XFastPropertySet::static_type();
-    return aTypes;
+    return css::uno::Sequence<css::uno::Type>({
+        UnoType<css::beans::XPropertySet>::get(),
+        UnoType<css::beans::XMultiPropertySet>::get(),
+        UnoType<css::beans::XFastPropertySet>::get()});
 }
 
 // ComponentHelper

include/cppuhelper/propshlp.hxx
@@ -686,7 +686,11 @@ protected:
 
 /**
    OPropertySetHelper plus XPropertySetOption
- */
+
+   @attention
+   The getTypes() inherited from OPropertysetHelper does not cover
+   XPropertySetOption.
+*/
 class CPPUHELPER_DLLPUBLIC OPropertySetHelper2 : public OPropertySetHelper,
                             public ::com::sun::star::beans::XPropertySetOption
 {