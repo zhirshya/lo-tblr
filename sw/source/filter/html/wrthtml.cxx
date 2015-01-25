LibreOffice/core

commit 832e5aadbff006ec24959162c29756fe2b1982be

Related: fdo#38838 remove UniString::SearchAndReplaceAll
Change-Id: I093c95b8700b628375d69293022f7d4b8da2af9c

sw/source/filter/html/wrthtml.cxx
@@ -1059,9 +1059,8 @@ void SwHTMLWriter::OutBookmarks()
 
     while( nPos < aOutlineMarkPoss.size() && aOutlineMarkPoss[nPos] == nNode )
     {
-        String sMark( aOutlineMarks[nPos] );
-        sMark.SearchAndReplaceAll( '?', '_' );  // '?' causes problems in IE/Netscape 5
-        OutAnchor( sMark );
+        OUString sMark( aOutlineMarks[nPos] );
+        OutAnchor( sMark.replace('?', '_') ); // '?' causes problems in IE/Netscape 5
         aOutlineMarkPoss.erase( aOutlineMarkPoss.begin()+nPos );
         aOutlineMarks.erase( aOutlineMarks.begin() + nPos );
     }
@@ -1072,13 +1071,11 @@ void SwHTMLWriter::OutImplicitMark( const String& rMark,
 {
     if( rMark.Len() && !aImplicitMarks.empty() )
     {
-        String sMark( rMark );
-        sMark.Append( cMarkSeparator );
-        sMark.AppendAscii( pMarkType );
+        OUString sMark( rMark );
+        sMark + OUString(cMarkSeparator) + OUString::createFromAscii(pMarkType);
         if( 0 != aImplicitMarks.erase( sMark ) )
         {
-            sMark.SearchAndReplaceAll( '?', '_' );  // '?' causes problems in IE/Netscape 5
-            OutAnchor( sMark );
+            OutAnchor(sMark.replace('?', '_')); // '?' causes problems in IE/Netscape 5
         }
     }
 }

include/tools/string.hxx
@@ -245,8 +245,6 @@ public:
                                           xub_StrLen nIndex = 0 );
     xub_StrLen          SearchAndReplace( const UniString& rStr, const UniString& rRepStr,
                                           xub_StrLen nIndex = 0 );
-    void                SearchAndReplaceAll( sal_Unicode c, sal_Unicode cRep );
-    void                SearchAndReplaceAll( const UniString& rStr, const UniString& rRepStr );
 
     void                SetToken( xub_StrLen nToken, sal_Unicode cTok, const UniString& rStr,
                                   xub_StrLen nIndex = 0 );

reportdesign/source/ui/inspection/GeometryHandler.cxx
@@ -2114,9 +2114,9 @@ void GeometryHandler::impl_createFunction(const OUString& _sFunctionName,const O
 
     const String sPlaceHolder1(RTL_CONSTASCII_USTRINGPARAM("%Column"));
     const String sPlaceHolder2(RTL_CONSTASCII_USTRINGPARAM("%FunctionName"));
-    String sFormula(_aFunction.m_sFormula);
-    sFormula.SearchAndReplaceAll(sPlaceHolder1,_sDataField);
-    sFormula.SearchAndReplaceAll(sPlaceHolder2,_sFunctionName);
+    OUString sFormula(_aFunction.m_sFormula);
+    sFormula = sFormula.replaceAll(sPlaceHolder1,_sDataField);
+    sFormula = sFormula.replaceAll(sPlaceHolder2,_sFunctionName);
 
     m_xFunction->setFormula(sFormula);
     m_xFunction->setPreEvaluated(_aFunction.m_bPreEvaluated);
@@ -2124,9 +2124,9 @@ void GeometryHandler::impl_createFunction(const OUString& _sFunctionName,const O
     if ( _aFunction.m_sInitialFormula.IsPresent )
     {
         beans::Optional< OUString> aInitialFormula = _aFunction.m_sInitialFormula;
-        String sInitialFormula = aInitialFormula.Value;
-        sInitialFormula.SearchAndReplaceAll(sPlaceHolder1,_sDataField);
-        sInitialFormula.SearchAndReplaceAll(sPlaceHolder2,_sFunctionName);
+        OUString sInitialFormula = aInitialFormula.Value;
+        sInitialFormula = sInitialFormula.replaceAll(sPlaceHolder1,_sDataField);
+        sInitialFormula = sInitialFormula.replaceAll(sPlaceHolder2,_sFunctionName);
         aInitialFormula.Value = sInitialFormula;
         m_xFunction->setInitialFormula( aInitialFormula );
     }

sw/source/core/doc/docfld.cxx
@@ -284,9 +284,9 @@ SwFieldType* SwDoc::GetFldType( sal_uInt16 nResId, const String& rName,
     {
         SwFieldType* pFldType = (*mpFldTypes)[i];
 
-        String aFldName( pFldType->GetName() );
+        OUString aFldName( pFldType->GetName() );
         if (bDbFieldMatching && nResId == RES_DBFLD)    // #i51815#
-            aFldName.SearchAndReplaceAll(DB_DELIM, '.');
+            aFldName = aFldName.replace(DB_DELIM, '.');
 
         if( nResId == pFldType->Which() &&
             rSCmp.isEqual( rName, aFldName ))

sw/source/core/doc/doctxm.cxx
@@ -1539,28 +1539,28 @@ static String lcl_GetNumString( const SwTOXSortTabBase& rBase, bool bUsePrefix, 
 
 /// Generate String with newlines changed to spaces, consecutive spaces changed
 /// to a single space, and trailing space removed.
-String lcl_RemoveLineBreaks( String sRet )
+OUString lcl_RemoveLineBreaks(const OUString &rRet)
 {
-    if (!sRet.Len())
-        return sRet;
-    xub_StrLen nOffset = 0;
-    sRet.SearchAndReplaceAll('\n', ' ');
-    for (xub_StrLen i = 1; i < sRet.Len(); i++)
+    if (rRet.isEmpty())
+        return rRet;
+    sal_Int32 nOffset = 0;
+    OUStringBuffer sRet(rRet.replace('\n', ' '));
+    for (sal_Int32 i = 1; i < sRet.getLength(); ++i)
     {
-        if ( sRet.GetChar(i - 1) == ' ' && sRet.GetChar(i) == ' ' )
+        if ( sRet[i - 1] == ' ' && sRet[i] == ' ' )
         {
             nOffset += 1;
         }
         else
         {
-            sRet.SetChar(i - nOffset, sRet.GetChar(i));
+            sRet[i - nOffset] = sRet[i];
         }
     }
-    if (sRet.GetChar(sRet.Len() - 1) == ' ')
+    if (sRet[sRet.getLength() - 1] == ' ')
     {
         nOffset += 1;
     }
-    return sRet.Copy(0, sRet.Len() - nOffset);
+    return sRet.copy(0, sRet.getLength() - nOffset).toString();
 }
 
 // Add parameter <_TOXSectNdIdx> and <_pDefaultPageDesc> in order to control,

sw/source/filter/ascii/ascatr.cxx
@@ -165,9 +165,9 @@ static Writer& OutASC_SwTxtNode( Writer& rWrt, SwCntntNode& rNode )
         }
     }
 
-    String aStr( rNd.GetTxt() );
+    OUString aStr( rNd.GetTxt() );
     if( rWrt.bASCII_ParaAsBlanc )
-        aStr.SearchAndReplaceAll( 0x0A, ' ' );
+        aStr = aStr.replace(0x0A, ' ');
 
     const bool bExportSoftHyphens = RTL_TEXTENCODING_UCS2 == rWrt.GetAsciiOptions().GetCharSet() ||
                                     RTL_TEXTENCODING_UTF8 == rWrt.GetAsciiOptions().GetCharSet();
@@ -180,7 +180,7 @@ static Writer& OutASC_SwTxtNode( Writer& rWrt, SwCntntNode& rNode )
 
         if( !aAttrIter.OutAttr( nStrPos ))
         {
-            String aOutStr( aStr.Copy( nStrPos, nNextAttr - nStrPos ) );
+            String aOutStr( aStr.copy( nStrPos, nNextAttr - nStrPos ) );
             if ( !bExportSoftHyphens )
                 aOutStr = comphelper::string::remove(aOutStr, CHAR_SOFTHYPHEN);
 

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -890,9 +890,9 @@ void DocxAttributeOutput::StartField_Impl( FieldInfos& rInfos, bool bWriteRun )
     }
 }
 
-void DocxAttributeOutput::DoWriteCmd( String& rCmd )
+void DocxAttributeOutput::DoWriteCmd( const OUString& rCmd )
 {
-    OUString sCmd = OUString(rCmd).trim();
+    OUString sCmd = rCmd.trim();
     if (sCmd.startsWith("SEQ"))
     {
         OUString sSeqName = msfilter::util::findQuotedText(sCmd, "SEQ ", '\\').trim();
@@ -900,7 +900,7 @@ void DocxAttributeOutput::DoWriteCmd( String& rCmd )
     }
     // Write the Field command
     m_pSerializer->startElementNS( XML_w, XML_instrText, FSEND );
-    m_pSerializer->writeEscaped( OUString( rCmd ) );
+    m_pSerializer->writeEscaped( rCmd );
     m_pSerializer->endElementNS( XML_w, XML_instrText );
 
 }
@@ -912,15 +912,15 @@ void DocxAttributeOutput::CmdField_Impl( FieldInfos& rInfos )
 
     for ( xub_StrLen i = 0; i < nNbToken; i++ )
     {
-        String sToken = rInfos.sCmd.GetToken( i, '\t' );
+        OUString sToken = rInfos.sCmd.GetToken( i, '\t' );
         if ( rInfos.eType ==  ww::eCREATEDATE
           || rInfos.eType ==  ww::eSAVEDATE
           || rInfos.eType ==  ww::ePRINTDATE
           || rInfos.eType ==  ww::eDATE
           || rInfos.eType ==  ww::eTIME )
         {
-           sToken.SearchAndReplaceAll( String( "NNNN" ), String( "dddd"  ) );
-           sToken.SearchAndReplaceAll( String( "NN" ), String( "ddd"  ) );
+           sToken = sToken.replaceAll("NNNN", "dddd");
+           sToken = sToken.replaceAll("NN", "ddd");
         }
         // Write the Field command
         DoWriteCmd( sToken );
@@ -963,10 +963,9 @@ void DocxAttributeOutput::EndField_Impl( FieldInfos& rInfos )
         // Write the Field latest value
         m_pSerializer->startElementNS( XML_w, XML_r, FSEND );
 
-        String sExpand( rInfos.pField->ExpandField( true ) );
+        OUString sExpand( rInfos.pField->ExpandField( true ) );
         // newlines embedded in fields are 0x0B in MSO and 0x0A for us
-        sExpand.SearchAndReplaceAll( 0x0A, 0x0B );
-        RunText( sExpand );
+        RunText(sExpand.replace(0x0A, 0x0B));
 
         m_pSerializer->endElementNS( XML_w, XML_r );
     }

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -631,7 +631,7 @@ private:
     void WriteCommentRanges();
 
     void StartField_Impl( FieldInfos& rInfos, bool bWriteRun = sal_False );
-    void DoWriteCmd( String& rCmd );
+    void DoWriteCmd( const OUString& rCmd );
     void CmdField_Impl( FieldInfos& rInfos );
     void EndField_Impl( FieldInfos& rInfos );
 

zhirshya/lo-tblr

commit 1ae00e5d72e270affe0936d4d407e3f138549c28

coverity#1266508 Useless call (gold)
regression from

commit 832e5aadbff006ec24959162c29756fe2b1982be
Author: Caolán McNamara <caolanm@redhat.com>
Date:   Tue Oct 8 10:06:59 2013 +0100
    Related: fdo#38838 remove UniString::SearchAndReplaceAll

Change-Id: If792925eddc9c640584a2e8fa313a4297a32c74c

sw/source/filter/html/wrthtml.cxx
@@ -1170,8 +1170,7 @@ void SwHTMLWriter::OutImplicitMark( const OUString& rMark,
 {
     if( !rMark.isEmpty() && !aImplicitMarks.empty() )
     {
-        OUString sMark( rMark );
-        sMark + OUStringLiteral1<cMarkSeparator>() + OUString::createFromAscii(pMarkType);
+        OUString sMark(rMark + OUStringLiteral1<cMarkSeparator>() + OUString::createFromAscii(pMarkType));
         if( 0 != aImplicitMarks.erase( sMark ) )
         {
             OutAnchor(sMark.replace('?', '_')); // '?' causes problems in IE/Netscape 5