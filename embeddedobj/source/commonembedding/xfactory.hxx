LibreOffice/core

commit 251f1b0b38be7d1267339898e44e320f05849e22

fdo#46808, Adapt embed::*EmbeddedObjectCreator UNO services to new style
The services already existed, it just did not have an IDL file.
Adapts
  com.sun.star.embed.OOoEmbeddedObjectFactory
  com.sun.star.embed.OLEEmbeddedObjectFactory
  com.sun.star.embed.EmbeddedObjectCreator

Change-Id: I8ba01c7fd956a46c87d02dc7d61bdd5fcdf21ab8

embeddedobj/source/commonembedding/xfactory.hxx
@@ -20,20 +20,13 @@
 #ifndef __XFACTORY_HXX_
 #define __XFACTORY_HXX_
 
-#include <com/sun/star/embed/XEmbedObjectCreator.hpp>
-#include <com/sun/star/embed/XEmbedObjectFactory.hpp>
-#include <com/sun/star/embed/XLinkCreator.hpp>
-#include <com/sun/star/embed/XLinkFactory.hpp>
+#include <com/sun/star/embed/XEmbeddedObjectCreator.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <cppuhelper/implbase2.hxx>
-#include <cppuhelper/implbase5.hxx>
 #include <comphelper/mimeconfighelper.hxx>
 
-class OOoEmbeddedObjectFactory : public ::cppu::WeakImplHelper5<
-                                                ::com::sun::star::embed::XEmbedObjectCreator,
-                                                ::com::sun::star::embed::XEmbedObjectFactory,
-                                                ::com::sun::star::embed::XLinkCreator,
-                                                ::com::sun::star::embed::XLinkFactory,
+class OOoEmbeddedObjectFactory : public ::cppu::WeakImplHelper2<
+                                                ::com::sun::star::embed::XEmbeddedObjectCreator,
                                                 ::com::sun::star::lang::XServiceInfo >
 {
     ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > m_xFactory;

comphelper/source/container/embeddedobjectcontainer.cxx
@@ -19,7 +19,7 @@
 
 #include <com/sun/star/container/XChild.hpp>
 #include <com/sun/star/container/XNameAccess.hpp>
-#include <com/sun/star/embed/XEmbedObjectCreator.hpp>
+#include <com/sun/star/embed/EmbeddedObjectCreator.hpp>
 #include <com/sun/star/embed/XLinkCreator.hpp>
 #include <com/sun/star/embed/XEmbedPersist.hpp>
 #include <com/sun/star/embed/XLinkageSupport.hpp>
@@ -360,8 +360,7 @@ uno::Reference < embed::XEmbeddedObject > EmbeddedObjectContainer::Get_Impl( con
 
         // object was not added until now - should happen only by calling this method from "inside"
         //TODO/LATER: it would be good to detect an error when an object should be created already, but isn't (not an "inside" call)
-        uno::Reference < embed::XEmbedObjectCreator > xFactory( ::comphelper::getProcessServiceFactory()->createInstance(
-                OUString("com.sun.star.embed.EmbeddedObjectCreator") ), uno::UNO_QUERY_THROW );
+        uno::Reference < embed::XEmbeddedObjectCreator > xFactory = embed::EmbeddedObjectCreator::create( ::comphelper::getProcessComponentContext() );
         uno::Sequence< beans::PropertyValue > aObjDescr( xCopy.is() ? 2 : 1 );
         aObjDescr[0].Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "Parent" ) );
         aObjDescr[0].Value <<= pImpl->m_xModel.get();
@@ -402,8 +401,7 @@ uno::Reference < embed::XEmbeddedObject > EmbeddedObjectContainer::CreateEmbedde
     uno::Reference < embed::XEmbeddedObject > xObj;
     try
     {
-        uno::Reference < embed::XEmbedObjectCreator > xFactory( ::comphelper::getProcessServiceFactory()->createInstance(
-                ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.embed.EmbeddedObjectCreator")) ), uno::UNO_QUERY );
+        uno::Reference < embed::XEmbeddedObjectCreator > xFactory = embed::EmbeddedObjectCreator::create( ::comphelper::getProcessComponentContext() );
 
         uno::Sequence< beans::PropertyValue > aObjDescr( rArgs.getLength() + 1 );
         aObjDescr[0].Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "Parent" ) );
@@ -620,8 +618,7 @@ uno::Reference < embed::XEmbeddedObject > EmbeddedObjectContainer::InsertEmbedde
     uno::Reference < embed::XEmbeddedObject > xObj;
     try
     {
-        uno::Reference < embed::XEmbedObjectCreator > xFactory( ::comphelper::getProcessServiceFactory()->createInstance(
-                ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.embed.EmbeddedObjectCreator")) ), uno::UNO_QUERY );
+        uno::Reference < embed::XEmbeddedObjectCreator > xFactory = embed::EmbeddedObjectCreator::create( ::comphelper::getProcessComponentContext() );
         uno::Sequence< beans::PropertyValue > aObjDescr( 1 );
         aObjDescr[0].Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "Parent" ) );
         aObjDescr[0].Value <<= pImpl->m_xModel.get();
@@ -655,8 +652,7 @@ uno::Reference < embed::XEmbeddedObject > EmbeddedObjectContainer::InsertEmbedde
     uno::Reference < embed::XEmbeddedObject > xObj;
     try
     {
-        uno::Reference < embed::XLinkCreator > xFactory( ::comphelper::getProcessServiceFactory()->createInstance(
-                ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.embed.EmbeddedObjectCreator")) ), uno::UNO_QUERY );
+        uno::Reference < embed::XEmbeddedObjectCreator > xFactory = embed::EmbeddedObjectCreator::create(::comphelper::getProcessComponentContext());
         uno::Sequence< beans::PropertyValue > aObjDescr( 1 );
         aObjDescr[0].Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "Parent" ) );
         aObjDescr[0].Value <<= pImpl->m_xModel.get();
@@ -740,10 +736,8 @@ uno::Reference < embed::XEmbeddedObject > EmbeddedObjectContainer::CopyAndGetEmb
                         throw uno::RuntimeException();
 
                     // create new linked object from the URL the link is based on
-                    uno::Reference < embed::XLinkCreator > xCreator(
-                        ::comphelper::getProcessServiceFactory()->createInstance(
-                            ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.embed.EmbeddedObjectCreator") ) ),
-                        uno::UNO_QUERY_THROW );
+                    uno::Reference < embed::XEmbeddedObjectCreator > xCreator =
+                        embed::EmbeddedObjectCreator::create( ::comphelper::getProcessComponentContext() );
 
                     uno::Sequence< beans::PropertyValue > aMediaDescr( 1 );
                     aMediaDescr[0].Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "URL" ) );
@@ -769,10 +763,8 @@ uno::Reference < embed::XEmbeddedObject > EmbeddedObjectContainer::CopyAndGetEmb
                     uno::Reference< beans::XPropertySet > xOrigProps( xObj->getComponent(), uno::UNO_QUERY_THROW );
 
                     // use object class ID to create a new one and tranfer all the properties
-                    uno::Reference < embed::XEmbedObjectCreator > xCreator(
-                        ::comphelper::getProcessServiceFactory()->createInstance(
-                            ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.embed.EmbeddedObjectCreator") ) ),
-                        uno::UNO_QUERY_THROW );
+                    uno::Reference < embed::XEmbeddedObjectCreator > xCreator =
+                        embed::EmbeddedObjectCreator::create( ::comphelper::getProcessComponentContext() );
 
                     uno::Sequence< beans::PropertyValue > aObjDescr( 1 );
                     aObjDescr[0].Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "Parent" ) );

dbaccess/source/core/dataaccess/documentdefinition.cxx
@@ -46,8 +46,9 @@
 #include <com/sun/star/report/XReportEngine.hpp>
 #include <com/sun/star/ucb/OpenMode.hpp>
 #include <com/sun/star/embed/XEmbedObjectFactory.hpp>
-#include <com/sun/star/embed/XEmbedObjectCreator.hpp>
+#include <com/sun/star/embed/EmbeddedObjectCreator.hpp>
 #include <com/sun/star/embed/Aspects.hpp>
+#include <com/sun/star/embed/OOoEmbeddedObjectFactory.hpp>
 #include <ucbhelper/cancelcommandexecution.hxx>
 #include <com/sun/star/ucb/UnsupportedDataSinkException.hpp>
 #include <com/sun/star/ucb/UnsupportedOpenModeException.hpp>
@@ -1215,36 +1216,33 @@ void ODocumentDefinition::onCommandInsert( const ::rtl::OUString& _sURL, const R
         Reference< XStorage> xStorage = getContainerStorage();
         if ( xStorage.is() )
         {
-            Reference< XEmbedObjectCreator> xEmbedFactory( m_aContext.createComponent( "com.sun.star.embed.EmbeddedObjectCreator" ), UNO_QUERY );
-            if ( xEmbedFactory.is() )
+            Reference< XEmbeddedObjectCreator> xEmbedFactory = EmbeddedObjectCreator::create(m_aContext.getUNOContext());
+            Sequence<PropertyValue> aEmpty,aMediaDesc(1);
+            aMediaDesc[0].Name = PROPERTY_URL;
+            aMediaDesc[0].Value <<= _sURL;
+            m_xEmbeddedObject.set(xEmbedFactory->createInstanceInitFromMediaDescriptor( xStorage
+                                                                            ,m_pImpl->m_aProps.sPersistentName
+                                                                            ,aMediaDesc
+                                                                            ,aEmpty),UNO_QUERY);
+
+            lcl_resetFormsToEmptyDataSource( m_xEmbeddedObject );
+            // #i57669#
+
+            Reference<XEmbedPersist> xPersist(m_xEmbeddedObject,UNO_QUERY);
+            if ( xPersist.is() )
             {
-                Sequence<PropertyValue> aEmpty,aMediaDesc(1);
-                aMediaDesc[0].Name = PROPERTY_URL;
-                aMediaDesc[0].Value <<= _sURL;
-                m_xEmbeddedObject.set(xEmbedFactory->createInstanceInitFromMediaDescriptor( xStorage
-                                                                                ,m_pImpl->m_aProps.sPersistentName
-                                                                                ,aMediaDesc
-                                                                                ,aEmpty),UNO_QUERY);
-
-                lcl_resetFormsToEmptyDataSource( m_xEmbeddedObject );
-                // #i57669#
-
-                Reference<XEmbedPersist> xPersist(m_xEmbeddedObject,UNO_QUERY);
-                if ( xPersist.is() )
-                {
-                    xPersist->storeOwn();
-                }
-                try
-                {
-                    Reference< com::sun::star::util::XCloseable> xCloseable(m_xEmbeddedObject,UNO_QUERY);
-                    if ( xCloseable.is() )
-                        xCloseable->close(sal_True);
-                }
-                catch(const Exception&)
-                {
-                }
-                m_xEmbeddedObject = NULL;
-              }
+                xPersist->storeOwn();
+            }
+            try
+            {
+                Reference< com::sun::star::util::XCloseable> xCloseable(m_xEmbeddedObject,UNO_QUERY);
+                if ( xCloseable.is() )
+                    xCloseable->close(sal_True);
+            }
+            catch(const Exception&)
+            {
+            }
+            m_xEmbeddedObject = NULL;
         }
     }
 
@@ -1617,80 +1615,77 @@ void ODocumentDefinition::loadEmbeddedObject( const Reference< XConnection >& i_
         Reference< XStorage> xStorage = getContainerStorage();
         if ( xStorage.is() )
         {
-            Reference< XEmbedObjectFactory> xEmbedFactory( m_aContext.createComponent( "com.sun.star.embed.OOoEmbeddedObjectFactory" ), UNO_QUERY );
-            if ( xEmbedFactory.is() )
+            Reference< XEmbeddedObjectCreator> xEmbedFactory = OOoEmbeddedObjectFactory::create(m_aContext.getUNOContext());
+            ::rtl::OUString sDocumentService;
+            sal_Bool bSetSize = sal_False;
+            sal_Int32 nEntryConnectionMode = EntryInitModes::DEFAULT_INIT;
+            Sequence< sal_Int8 > aClassID = _aClassID;
+            if ( aClassID.getLength() )
             {
-                ::rtl::OUString sDocumentService;
-                sal_Bool bSetSize = sal_False;
-                sal_Int32 nEntryConnectionMode = EntryInitModes::DEFAULT_INIT;
-                Sequence< sal_Int8 > aClassID = _aClassID;
-                if ( aClassID.getLength() )
+                nEntryConnectionMode = EntryInitModes::TRUNCATE_INIT;
+                bSetSize = sal_True;
+            }
+            else
+            {
+                sDocumentService = GetDocumentServiceFromMediaType( getContentType(), m_aContext, aClassID );
+                // check if we are not a form and
+                // the com.sun.star.report.pentaho.SOReportJobFactory is not present.
+                if ( !m_bForm && !sDocumentService.equalsAsciiL(RTL_CONSTASCII_STRINGPARAM("com.sun.star.text.TextDocument")))
                 {
-                    nEntryConnectionMode = EntryInitModes::TRUNCATE_INIT;
-                    bSetSize = sal_True;
-                }
-                else
-                {
-                    sDocumentService = GetDocumentServiceFromMediaType( getContentType(), m_aContext, aClassID );
-                    // check if we are not a form and
-                    // the com.sun.star.report.pentaho.SOReportJobFactory is not present.
-                    if ( !m_bForm && !sDocumentService.equalsAsciiL(RTL_CONSTASCII_STRINGPARAM("com.sun.star.text.TextDocument")))
+                    // we seem to be a "new style" report, check if report extension is present.
+                    Reference< XContentEnumerationAccess > xEnumAccess( m_aContext.getLegacyServiceFactory(), UNO_QUERY );
+                    const ::rtl::OUString sReportEngineServiceName = ::dbtools::getDefaultReportEngineServiceName(m_aContext.getUNOContext());
+                    Reference< XEnumeration > xEnumDrivers = xEnumAccess->createContentEnumeration(sReportEngineServiceName);
+                    if ( !xEnumDrivers.is() || !xEnumDrivers->hasMoreElements() )
                     {
-                        // we seem to be a "new style" report, check if report extension is present.
-                        Reference< XContentEnumerationAccess > xEnumAccess( m_aContext.getLegacyServiceFactory(), UNO_QUERY );
-                        const ::rtl::OUString sReportEngineServiceName = ::dbtools::getDefaultReportEngineServiceName(m_aContext.getUNOContext());
-                        Reference< XEnumeration > xEnumDrivers = xEnumAccess->createContentEnumeration(sReportEngineServiceName);
-                        if ( !xEnumDrivers.is() || !xEnumDrivers->hasMoreElements() )
-                        {
-                            com::sun::star::io::WrongFormatException aWFE;
-                            aWFE.Message = DBACORE_RESSTRING( RID_STR_MISSING_EXTENSION );
-                            throw aWFE;
-                        }
-                    }
-                    if ( !aClassID.getLength() )
-                    {
-                        if ( m_bForm )
-                            aClassID = MimeConfigurationHelper::GetSequenceClassID(SO3_SW_CLASSID);
-                        else
-                        {
-                            aClassID = MimeConfigurationHelper::GetSequenceClassID(SO3_RPT_CLASSID_90);
-                        }
+                        com::sun::star::io::WrongFormatException aWFE;
+                        aWFE.Message = DBACORE_RESSTRING( RID_STR_MISSING_EXTENSION );
+                        throw aWFE;
                     }
                 }
-
-                OSL_ENSURE( aClassID.getLength(),"No Class ID" );
-
-                Sequence< PropertyValue > aEmbeddedObjectDescriptor;
-                Sequence< PropertyValue > aLoadArgs( fillLoadArgs(
-                    i_rConnection, _bSuppressMacros, _bReadOnly, i_rOpenCommandArguments, aEmbeddedObjectDescriptor ) );
-
-                m_xEmbeddedObject.set(xEmbedFactory->createInstanceUserInit(aClassID
-                                                                            ,sDocumentService
-                                                                            ,xStorage
-                                                                            ,m_pImpl->m_aProps.sPersistentName
-                                                                            ,nEntryConnectionMode
-                                                                            ,aLoadArgs
-                                                                            ,aEmbeddedObjectDescriptor
-                                                                            ),UNO_QUERY);
-                if ( m_xEmbeddedObject.is() )
+                if ( !aClassID.getLength() )
                 {
-                    if ( !m_pClientHelper )
+                    if ( m_bForm )
+                        aClassID = MimeConfigurationHelper::GetSequenceClassID(SO3_SW_CLASSID);
+                    else
                     {
-                        m_pClientHelper = new OEmbeddedClientHelper(this);
-                        m_pClientHelper->acquire();
-                    }
-                    Reference<XEmbeddedClient> xClient = m_pClientHelper;
-                    m_xEmbeddedObject->setClientSite(xClient);
-                    m_xEmbeddedObject->changeState(EmbedStates::RUNNING);
-                    if ( bSetSize )
-                    {
-                        LockModifiable aLockModify( impl_getComponent_throw( false ) );
-
-                        awt::Size aSize( DEFAULT_WIDTH, DEFAULT_HEIGHT );
-                        m_xEmbeddedObject->setVisualAreaSize(Aspects::MSOLE_CONTENT,aSize);
+                        aClassID = MimeConfigurationHelper::GetSequenceClassID(SO3_RPT_CLASSID_90);
                     }
                 }
-              }
+            }
+
+            OSL_ENSURE( aClassID.getLength(),"No Class ID" );
+
+            Sequence< PropertyValue > aEmbeddedObjectDescriptor;
+            Sequence< PropertyValue > aLoadArgs( fillLoadArgs(
+                i_rConnection, _bSuppressMacros, _bReadOnly, i_rOpenCommandArguments, aEmbeddedObjectDescriptor ) );
+
+            m_xEmbeddedObject.set(xEmbedFactory->createInstanceUserInit(aClassID
+                                                                        ,sDocumentService
+                                                                        ,xStorage
+                                                                        ,m_pImpl->m_aProps.sPersistentName
+                                                                        ,nEntryConnectionMode
+                                                                        ,aLoadArgs
+                                                                        ,aEmbeddedObjectDescriptor
+                                                                        ),UNO_QUERY);
+            if ( m_xEmbeddedObject.is() )
+            {
+                if ( !m_pClientHelper )
+                {
+                    m_pClientHelper = new OEmbeddedClientHelper(this);
+                    m_pClientHelper->acquire();
+                }
+                Reference<XEmbeddedClient> xClient = m_pClientHelper;
+                m_xEmbeddedObject->setClientSite(xClient);
+                m_xEmbeddedObject->changeState(EmbedStates::RUNNING);
+                if ( bSetSize )
+                {
+                    LockModifiable aLockModify( impl_getComponent_throw( false ) );
+
+                    awt::Size aSize( DEFAULT_WIDTH, DEFAULT_HEIGHT );
+                    m_xEmbeddedObject->setVisualAreaSize(Aspects::MSOLE_CONTENT,aSize);
+                }
+            }
         }
     }
     else

embeddedobj/source/general/xcreator.cxx
@@ -20,6 +20,8 @@
 #include <com/sun/star/embed/ElementModes.hpp>
 #include <com/sun/star/embed/EntryInitModes.hpp>
 #include <com/sun/star/embed/XEmbedObjectFactory.hpp>
+#include <com/sun/star/embed/OOoEmbeddedObjectFactory.hpp>
+#include <com/sun/star/embed/OLEEmbeddedObjectFactory.hpp>
 #include <com/sun/star/embed/XLinkFactory.hpp>
 #include <com/sun/star/document/XTypeDetection.hpp>
 #include <com/sun/star/beans/PropertyValue.hpp>
@@ -28,6 +30,7 @@
 #include <com/sun/star/lang/XComponent.hpp>
 
 #include <rtl/logfile.hxx>
+#include <comphelper/processfactory.hxx>
 
 
 #include <xcreator.hxx>
@@ -258,12 +261,8 @@ uno::Reference< uno::XInterface > SAL_CALL UNOEmbeddedObjectCreator::createInsta
     if ( !aFilterName.isEmpty() )
     {
         // the object can be loaded by one of the office application
-        uno::Reference< embed::XEmbedObjectCreator > xOOoEmbCreator(
-                            m_xFactory->createInstance(
-                                    ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.embed.OOoEmbeddedObjectFactory" ) )),
-                            uno::UNO_QUERY );
-        if ( !xOOoEmbCreator.is() )
-            throw uno::RuntimeException(); // TODO:
+        uno::Reference< embed::XEmbeddedObjectCreator > xOOoEmbCreator =
+                            embed::OOoEmbeddedObjectFactory::create( comphelper::getComponentContext(m_xFactory) );
 
         xResult = xOOoEmbCreator->createInstanceInitFromMediaDescriptor( xStorage,
                                                                          sEntName,
@@ -281,12 +280,8 @@ uno::Reference< uno::XInterface > SAL_CALL UNOEmbeddedObjectCreator::createInsta
         // Or for example the typename can be used to detect object type if typedetection
         // was also extended.
 
-        uno::Reference< embed::XEmbedObjectCreator > xOleEmbCreator(
-                            m_xFactory->createInstance(
-                                    ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.embed.OLEEmbeddedObjectFactory" ) )),
-                            uno::UNO_QUERY );
-        if ( !xOleEmbCreator.is() )
-            throw uno::RuntimeException(); // TODO:
+        uno::Reference< embed::XEmbeddedObjectCreator > xOleEmbCreator =
+                            embed::OLEEmbeddedObjectFactory::create( comphelper::getComponentContext(m_xFactory) );
 
         xResult = xOleEmbCreator->createInstanceInitFromMediaDescriptor( xStorage, sEntName, aTempMedDescr, lObjArgs );
     }
@@ -371,12 +366,8 @@ uno::Reference< uno::XInterface > SAL_CALL UNOEmbeddedObjectCreator::createInsta
     if ( !aFilterName.isEmpty() )
     {
         // the object can be loaded by one of the office application
-        uno::Reference< embed::XLinkCreator > xOOoLinkCreator(
-                            m_xFactory->createInstance(
-                                    ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.embed.OOoEmbeddedObjectFactory" ) )),
-                            uno::UNO_QUERY );
-        if ( !xOOoLinkCreator.is() )
-            throw uno::RuntimeException(); // TODO:
+        uno::Reference< embed::XEmbeddedObjectCreator > xOOoLinkCreator =
+                            embed::OOoEmbeddedObjectFactory::create( comphelper::getComponentContext(m_xFactory) );
 
         xResult = xOOoLinkCreator->createInstanceLink( xStorage,
                                                         sEntName,
@@ -406,12 +397,8 @@ uno::Reference< uno::XInterface > SAL_CALL UNOEmbeddedObjectCreator::createInsta
                                                     static_cast< ::cppu::OWeakObject* >(this) ),
                                                 4 );
 
-        uno::Reference< embed::XLinkCreator > xLinkCreator(
-                            m_xFactory->createInstance(
-                                ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM( "com.sun.star.embed.OLEEmbeddedObjectFactory" ) )),
-                            uno::UNO_QUERY );
-        if ( !xLinkCreator.is() )
-            throw uno::RuntimeException(); // TODO:
+        uno::Reference< embed::XEmbeddedObjectCreator > xLinkCreator =
+                            embed::OLEEmbeddedObjectFactory::create( comphelper::getComponentContext(m_xFactory));
 
         xResult = xLinkCreator->createInstanceLink( xStorage, sEntName, aTempMedDescr, lObjArgs );
     }

embeddedobj/source/inc/xcreator.hxx
@@ -20,21 +20,15 @@
 #ifndef __XCREATOR_HXX_
 #define __XCREATOR_HXX_
 
-#include <com/sun/star/embed/XEmbedObjectCreator.hpp>
-#include <com/sun/star/embed/XEmbedObjectFactory.hpp>
-#include <com/sun/star/embed/XLinkCreator.hpp>
-#include <com/sun/star/embed/XLinkFactory.hpp>
+#include <com/sun/star/embed/XEmbeddedObjectCreator.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 
 
-#include <cppuhelper/implbase5.hxx>
+#include <cppuhelper/implbase2.hxx>
 #include <comphelper/mimeconfighelper.hxx>
 
-class UNOEmbeddedObjectCreator : public ::cppu::WeakImplHelper5<
-                                                ::com::sun::star::embed::XEmbedObjectCreator,
-                                                ::com::sun::star::embed::XEmbedObjectFactory,
-                                                ::com::sun::star::embed::XLinkCreator,
-                                                ::com::sun::star::embed::XLinkFactory,
+class UNOEmbeddedObjectCreator : public ::cppu::WeakImplHelper2<
+                                                ::com::sun::star::embed::XEmbeddedObjectCreator,
                                                 ::com::sun::star::lang::XServiceInfo >
 {
     ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > m_xFactory;

zhirshya/lo-tblr

commit 25ad70bf689b9ef99cd3750daecac4c83552a68d

OOoEmbeddedObjectFactory does implement optional XLinkFactory
regression from 251f1b0b38be7d1267339898e44e320f05849e22 "fdo#46808, Adapt
embed::*EmbeddedObjectCreator UNO services to new style"

Change-Id: Id8b49c04183d3bc98f22398a8e43c80909c1ea11

embeddedobj/source/commonembedding/xfactory.hxx
@@ -21,13 +21,15 @@
 #define INCLUDED_EMBEDDEDOBJ_SOURCE_COMMONEMBEDDING_XFACTORY_HXX
 
 #include <com/sun/star/embed/XEmbeddedObjectCreator.hpp>
+#include <com/sun/star/embed/XLinkFactory.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <osl/diagnose.h>
-#include <cppuhelper/implbase2.hxx>
+#include <cppuhelper/implbase.hxx>
 #include <comphelper/mimeconfighelper.hxx>
 
-class OOoEmbeddedObjectFactory : public ::cppu::WeakImplHelper2<
+class OOoEmbeddedObjectFactory : public ::cppu::WeakImplHelper<
                                                 ::com::sun::star::embed::XEmbeddedObjectCreator,
+                                                css::embed::XLinkFactory,
                                                 ::com::sun::star::lang::XServiceInfo >
 {
     ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > m_xContext;
@@ -64,7 +66,7 @@ public:
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL createInstanceLink( const ::com::sun::star::uno::Reference< ::com::sun::star::embed::XStorage >& xStorage, const OUString& sEntName, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aMediaDescr, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& lObjArgs ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::io::IOException, ::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;
 
     // XLinkFactory
-    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL createInstanceLinkUserInit( const ::com::sun::star::uno::Sequence< sal_Int8 >& aClassID, const OUString& aClassName, const ::com::sun::star::uno::Reference< ::com::sun::star::embed::XStorage >& xStorage, const OUString& sEntName, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& lArguments, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& lObjArgs ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::io::IOException, ::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+    ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > SAL_CALL createInstanceLinkUserInit( const ::com::sun::star::uno::Sequence< sal_Int8 >& aClassID, const OUString& aClassName, const ::com::sun::star::uno::Reference< ::com::sun::star::embed::XStorage >& xStorage, const OUString& sEntName, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& lArguments, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& lObjArgs ) throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::io::IOException, ::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
     // XServiceInfo
     virtual OUString SAL_CALL getImplementationName() throw (::com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;
@@ -73,7 +75,7 @@ public:
 
 };
 
-class OOoSpecialEmbeddedObjectFactory : public ::cppu::WeakImplHelper2<
+class OOoSpecialEmbeddedObjectFactory : public ::cppu::WeakImplHelper<
                                                 ::com::sun::star::embed::XEmbedObjectFactory,
                                                 ::com::sun::star::lang::XServiceInfo >
 {