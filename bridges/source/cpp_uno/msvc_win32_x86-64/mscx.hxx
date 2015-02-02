LibreOffice/core

commit fe480d8136b204c8dc6c68916cce7e816f8b9c48

remove unused typedefs
found with some minor modifications to find/find-unused-defines.sh

Change-Id: I18cc479adedc7a0dada68a4aeef08300e62631dd
Reviewed-on: https://gerrit.libreoffice.org/14194
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

bridges/source/cpp_uno/msvc_win32_x86-64/mscx.hxx
@@ -36,7 +36,6 @@ namespace CPPU_CURRENT_NAMESPACE
 const DWORD MSVC_ExceptionCode = 0xe06d7363;
 const long MSVC_magic_number = 0x19930520L;
 
-typedef enum { REGPARAM_INT, REGPARAM_FLT } RegParamKind;
 
 
 type_info * mscx_getRTTI( OUString const & rUNOname );

basic/source/inc/sbjsmeth.hxx
@@ -37,7 +37,6 @@ public:
     TYPEINFO_OVERRIDE();
 };
 
-typedef tools::SvRef<SbJScriptMethod> SbJScriptMethodRef;
 
 #endif
 

basic/source/inc/sbunoobj.hxx
@@ -99,7 +99,6 @@ public:
     ::com::sun::star::uno::Any getUnoAny();
     void SFX_NOTIFY( SfxBroadcaster&, const TypeId&, const SfxHint& rHint, const TypeId& ) SAL_OVERRIDE;
 };
-typedef tools::SvRef<SbUnoStructRefObject> SbUnoStructRefObjectRef;
 
 class SbUnoObject: public SbxObject
 {
@@ -242,7 +241,6 @@ public:
     const ::com::sun::star::uno::Reference< ::com::sun::star::reflection::XIdlClass >& getUnoClass( void ) { return m_xClass; }
 
 };
-typedef tools::SvRef<SbUnoClass> SbUnoClassRef;
 
 
 // function to find a global identifier in
@@ -269,7 +267,6 @@ public:
 
     void SFX_NOTIFY( SfxBroadcaster&, const TypeId&, const SfxHint& rHint, const TypeId& ) SAL_OVERRIDE;
 };
-typedef tools::SvRef<SbUnoService> SbUnoServiceRef;
 
 SbUnoService* findUnoService( const OUString& rName );
 
@@ -309,7 +306,6 @@ public:
 
     void SFX_NOTIFY( SfxBroadcaster&, const TypeId&, const SfxHint& rHint, const TypeId& ) SAL_OVERRIDE;
 };
-typedef tools::SvRef<SbUnoSingleton> SbUnoSingletonRef;
 
 SbUnoSingleton* findUnoSingleton( const OUString& rName );
 

desktop/source/deployment/gui/dp_gui_extlistbox.hxx
@@ -48,7 +48,6 @@ namespace dp_gui {
 
 class TheExtensionManager;
 
-typedef ::boost::shared_ptr< FixedHyperlink > TFixedHyperlink;
 
 
 //                          struct Entry_Impl

filter/source/flash/swfexporter.hxx
@@ -111,7 +111,6 @@ struct ShapeAnimationInfo
     ShapeAnimationInfo( ShapeInfo* pShapeInfo, sal_uInt16 nDepth ) : mpShapeInfo( pShapeInfo ), mnDepth( nDepth ) {}
 };
 
-typedef std::vector<ShapeAnimationInfo> ShapeAnimationInfoVector;
 
 
 

framework/inc/classes/filtercachedata.hxx
@@ -418,9 +418,7 @@ typedef OUStringList                                                OrderList;
 
 typedef CheckedIterator< OUStringList >                             CheckedStringListIterator;
 typedef CheckedIterator< FileTypeHash >                             CheckedTypeIterator;
-typedef CheckedIterator< PerformanceHash >                          CheckedPerformanceIterator;
 
-typedef ::std::vector< FilterHash::const_iterator >                 FilterQuery;
 
 // Use private static data container to hold all values of configuration!
 

framework/inc/stdtypes.h
@@ -101,13 +101,6 @@ void free(std::vector<T>& vec)
 }
 
 /**
-    Basic string queue based on a std::queue()
-    It implements some additional funtionality which can be useful but
-    is missing at the normal std implementation.
-*/
-typedef ::std::queue< OUString > OUStringQueue;
-
-/**
     Basic hash based on a std::unordered_map() which provides key=[OUString] and value=[template type] pairs
     It implements some additional funtionality which can be useful but
     is missing at the normal hash implementation.
@@ -138,7 +131,6 @@ typedef BaseHash< OUString > OUStringHashMap;
     Our helper class OPropertySetHelper works optimized with handles but sometimes we have only a property name.
     Mapping between these two parts of a property should be done in the fastest way :-)
 */
-typedef BaseHash< sal_Int32 > NameToHandleHash;
 
 typedef cppu::OMultiTypeInterfaceContainerHelperVar<OUString> ListenerHash;
 

framework/source/inc/accelerators/acceleratorconfiguration.hxx
@@ -59,10 +59,6 @@ const char CFG_ENTRY_PRIMARY[] = "PrimaryKeys";
 const char CFG_ENTRY_GLOBAL[] = "Global";
 const char CFG_ENTRY_MODULES[] = "Modules";
 
-/** "global" type to make accelerator presets unique, so they can be used
-    in combination with the salhelper::SingletonRef mechanism! */
-typedef PresetHandler AcceleratorPresets;
-
 /**
     implements a read/write access to the accelerator configuration.
  */

zhirshya/lo-tblr

commit 197742e589a59982b7cb054889d0fa37dec492a0

add back typedef (regression from fe480d8136b204c8dc6c68916cce7e816f8b9c48)
Change-Id: Ia367fcd3b204b9dd96f5fa6d3a52d0895bd9c0c9

bridges/source/cpp_uno/msvc_win32_x86-64/mscx.hxx
@@ -36,7 +36,7 @@ namespace CPPU_CURRENT_NAMESPACE
 const DWORD MSVC_ExceptionCode = 0xe06d7363;
 const long MSVC_magic_number = 0x19930520L;
 
-
+typedef enum { REGPARAM_INT, REGPARAM_FLT } RegParamKind;
 
 type_info * mscx_getRTTI( OUString const & rUNOname );
 