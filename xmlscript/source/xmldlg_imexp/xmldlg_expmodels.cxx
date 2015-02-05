LibreOffice/core

commit 2d4b87f0c1bfd97185a89c18d5b7680d11a958d6

ODF export: don't write invalid "group-name" attribute
Radio buttons are grouped via their "form:name" attribute already.

Change-Id: I9f8b27a2904d947c3d4665495d36961e3e41d2c6

xmlscript/source/xmldlg_imexp/xmldlg_expmodels.cxx
@@ -419,7 +419,6 @@ void ElementDescriptor::readRadioButtonModel( StyleBag * all_styles  )
     readImageURLAttr( "ImageURL", XMLNS_DIALOGS_PREFIX ":image-src" );
     readImagePositionAttr( "ImagePosition", XMLNS_DIALOGS_PREFIX ":image-position" );
     readBoolAttr( "MultiLine", XMLNS_DIALOGS_PREFIX ":multiline" );
-    readStringAttr( "GroupName", XMLNS_DIALOGS_PREFIX ":group-name" );
 
     sal_Int16 nState = 0;
     if (readProp( "State" ) >>= nState)

xmlscript/source/xmldlg_imexp/xmldlg_impmodels.cxx
@@ -1174,7 +1174,9 @@ void TitledBoxElement::endElement()
         ctx.importImageURLProperty( "ImageURL" ,  "image-src" , _xAttributes );
         ctx.importImagePositionProperty( "ImagePosition", "image-position", xAttributes );
         ctx.importBooleanProperty( "MultiLine", "multiline", xAttributes );
-        ctx.importStringProperty( "GroupName", "group-name", xAttributes );
+        // map invalid "group-name" attribute to "name"
+        // (since radio buttons are grouped by name)
+        ctx.importStringProperty( "Name", "group-name", xAttributes );
 
         sal_Int16 nVal = 0;
         sal_Bool bChecked = sal_False;
@@ -1269,7 +1271,9 @@ void RadioGroupElement::endElement()
         ctx.importImageURLProperty( "ImageURL" , "image-src" , xAttributes );
         ctx.importImagePositionProperty( "ImagePosition", "image-position", xAttributes );
         ctx.importBooleanProperty( "MultiLine", "multiline", xAttributes );
-        ctx.importStringProperty( "GroupName", "group-name", xAttributes );
+        // map invalid "group-name" attribute to "name"
+        // (since radio buttons are grouped by name)
+        ctx.importStringProperty( "Name", "group-name", xAttributes );
         sal_Int16 nVal = 0;
         sal_Bool bChecked = sal_False;
         if (getBoolAttr( &bChecked, "checked", xAttributes, _pImport->XMLNS_DIALOGS_UID ) && bChecked)

sc/source/filter/excel/xiescher.cxx
@@ -2288,7 +2288,8 @@ void XclImpOptionButtonObj::DoProcessControl( ScfPropertySet& rPropSet ) const
                 ScfPropertySet aProps( xCtrlModel );
                 OUString sGroupName = OUString::number( pLeader->GetDffShapeId() );
 
-                aProps.SetStringProperty( "GroupName", sGroupName );
+                // for radio buttons, "Name" is the group name
+                aProps.SetStringProperty( "Name", sGroupName );
                 aProps.SetStringProperty( "RefValue", OUString::number( nRefVal++ ) );
                 if ( pLeader->HasCellLink() && !pTbxObj->HasCellLink() )
                 {

xmloff/source/forms/elementexport.cxx
@@ -1159,11 +1159,9 @@ namespace xmloff
         {
             static const sal_Int32 nStringPropertyAttributeIds[] =
             {   // attribute flags
-                SCA_GROUP_NAME
             };
             static const OUString pStringPropertyNames[] =
             {   // property names
-                OUString(PROPERTY_GROUP_NAME)
             };
 
             static const sal_Int32 nIdCount = sizeof( nStringPropertyAttributeIds ) / sizeof( nStringPropertyAttributeIds[0] );
@@ -1659,8 +1657,6 @@ namespace xmloff
                 }
                 if ( m_xPropertyInfo->hasPropertyByName( PROPERTY_IMAGE_POSITION ) )
                     m_nIncludeSpecial |= SCA_IMAGE_POSITION;
-                if ( m_xPropertyInfo->hasPropertyByName( PROPERTY_GROUP_NAME ) )
-                    m_nIncludeSpecial |= SCA_GROUP_NAME;
                 m_nIncludeDatabase = DA_DATA_FIELD | DA_INPUT_REQUIRED;
                 m_nIncludeEvents = EA_CONTROL_EVENTS | EA_ON_CHANGE;
                 break;

xmloff/source/forms/layerimport.cxx
@@ -76,9 +76,11 @@ OFormLayerXMLImport_Impl::OFormLayerXMLImport_Impl(SvXMLImport& _rImporter)
     // string properties which are exported as attributes
     m_aAttributeMetaData.addStringProperty(
         OAttributeMetaData::getCommonControlAttributeName(CCA_NAME), PROPERTY_NAME);
+    // map invalid "group-name" attribute to "name"
+    // (since radio buttons are grouped by name)
     m_aAttributeMetaData.addStringProperty(
-            OAttributeMetaData::getSpecialAttributeName(SCA_GROUP_NAME), PROPERTY_GROUP_NAME);
-        m_aAttributeMetaData.addStringProperty(
+            OAttributeMetaData::getSpecialAttributeName(SCA_GROUP_NAME), PROPERTY_NAME);
+    m_aAttributeMetaData.addStringProperty(
         OAttributeMetaData::getCommonControlAttributeName(CCA_IMAGE_DATA), PROPERTY_IMAGEURL);
     m_aAttributeMetaData.addStringProperty(
         OAttributeMetaData::getCommonControlAttributeName(CCA_LABEL), PROPERTY_LABEL);

zhirshya/lo-tblr

commit d27ba12e5addca8a41d12a8f7d2fceaf24435bc0

xmlscript: fix import/export of dialog radio-buttons
The BASIC dialog format is not specified by ODF anyway, so the change
was particularly stupid.

(regression from 2d4b87f0c1bfd97185a89c18d5b7680d11a958d6)

Change-Id: I5b20d690093b0d2b898f3e45cc0292fb72fbb353

xmlscript/source/xmldlg_imexp/xmldlg_expmodels.cxx
@@ -419,6 +419,7 @@ void ElementDescriptor::readRadioButtonModel( StyleBag * all_styles  )
     readImageURLAttr( "ImageURL", XMLNS_DIALOGS_PREFIX ":image-src" );
     readImagePositionAttr( "ImagePosition", XMLNS_DIALOGS_PREFIX ":image-position" );
     readBoolAttr( "MultiLine", XMLNS_DIALOGS_PREFIX ":multiline" );
+    readStringAttr( "GroupName", XMLNS_DIALOGS_PREFIX ":group-name" );
 
     sal_Int16 nState = 0;
     if (readProp( "State" ) >>= nState)

xmlscript/source/xmldlg_imexp/xmldlg_impmodels.cxx
@@ -1178,9 +1178,7 @@ void TitledBoxElement::endElement()
         ctx.importImageURLProperty( "ImageURL" ,  "image-src" , _xAttributes );
         ctx.importImagePositionProperty( "ImagePosition", "image-position", xAttributes );
         ctx.importBooleanProperty( "MultiLine", "multiline", xAttributes );
-        // map invalid "group-name" attribute to "name"
-        // (since radio buttons are grouped by name)
-        ctx.importStringProperty( "Name", "group-name", xAttributes );
+        ctx.importStringProperty( "GroupName", "group-name", xAttributes );
 
         sal_Int16 nVal = 0;
         sal_Bool bChecked = sal_False;
@@ -1275,9 +1273,7 @@ void RadioGroupElement::endElement()
         ctx.importImageURLProperty( "ImageURL" , "image-src" , xAttributes );
         ctx.importImagePositionProperty( "ImagePosition", "image-position", xAttributes );
         ctx.importBooleanProperty( "MultiLine", "multiline", xAttributes );
-        // map invalid "group-name" attribute to "name"
-        // (since radio buttons are grouped by name)
-        ctx.importStringProperty( "Name", "group-name", xAttributes );
+        ctx.importStringProperty( "GroupName", "group-name", xAttributes );
         sal_Int16 nVal = 0;
         sal_Bool bChecked = sal_False;
         if (getBoolAttr( &bChecked, "checked", xAttributes, _pImport->XMLNS_DIALOGS_UID ) && bChecked)