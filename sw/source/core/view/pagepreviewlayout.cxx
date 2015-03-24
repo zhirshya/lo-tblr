LibreOffice/core

commit 5d37fa2a710e3bd76d3f1e18d1d66b8a4ab15030

convert SvxZoomType to enum class
Change-Id: I7308e848d3f9ac391dc656a145139dabbc792df3

sw/source/core/view/pagepreviewlayout.cxx
@@ -254,7 +254,7 @@ void SwPagePreviewLayout::_ApplyNewZoomAtViewShell( sal_uInt8 _aNewZoom )
     {
         aNewViewOptions.SetZoom( _aNewZoom );
         //#i19975# - consider zoom type.
-        enum SvxZoomType eZoomType = SVX_ZOOM_PERCENT;
+        enum SvxZoomType eZoomType = SvxZoomType::PERCENT;
         aNewViewOptions.SetZoomType( eZoomType );
         mrParentViewShell.ApplyViewOptions( aNewViewOptions );
     }

cui/source/dialogs/zoom.cxx
@@ -212,18 +212,18 @@ SvxZoomDialog::SvxZoomDialog( vcl::Window* pParent, const SfxItemSet& rCoreSet )
 
         switch (eType)
         {
-            case SVX_ZOOM_OPTIMAL:
+            case SvxZoomType::OPTIMAL:
                 nButtonId = ZOOMBTN_OPTIMAL;
                 break;
-            case SVX_ZOOM_PAGEWIDTH:
+            case SvxZoomType::PAGEWIDTH:
                 nButtonId = ZOOMBTN_PAGEWIDTH;
                 break;
-            case SVX_ZOOM_WHOLEPAGE:
+            case SvxZoomType::WHOLEPAGE:
                 nButtonId = ZOOMBTN_WHOLEPAGE;
                 break;
-            case SVX_ZOOM_PERCENT:
+            case SvxZoomType::PERCENT:
                 break;
-            case SVX_ZOOM_PAGEWIDTH_NOBORDER:
+            case SvxZoomType::PAGEWIDTH_NOBORDER:
                 break;
         }
 
@@ -380,7 +380,7 @@ IMPL_LINK(SvxZoomDialog, OKHdl, Button*, pButton)
 {
     if (mbModified || m_pOKBtn != pButton)
     {
-        SvxZoomItem aZoomItem(SVX_ZOOM_PERCENT, 0, mrSet.GetPool()->GetWhich(SID_ATTR_ZOOM));
+        SvxZoomItem aZoomItem(SvxZoomType::PERCENT, 0, mrSet.GetPool()->GetWhich(SID_ATTR_ZOOM));
         SvxViewLayoutItem aViewLayoutItem(0, false, mrSet.GetPool()->GetWhich(SID_ATTR_VIEWLAYOUT));
 
         if (m_pOKBtn == pButton)
@@ -390,11 +390,11 @@ IMPL_LINK(SvxZoomDialog, OKHdl, Button*, pButton)
             if (SPECIAL_FACTOR == nFactor)
             {
                 if (m_pOptimalBtn->IsChecked())
-                    aZoomItem.SetType(SVX_ZOOM_OPTIMAL);
+                    aZoomItem.SetType(SvxZoomType::OPTIMAL);
                 else if (m_pPageWidthBtn->IsChecked())
-                    aZoomItem.SetType(SVX_ZOOM_PAGEWIDTH);
+                    aZoomItem.SetType(SvxZoomType::PAGEWIDTH);
                 else if (m_pWholePageBtn->IsChecked())
-                    aZoomItem.SetType(SVX_ZOOM_WHOLEPAGE);
+                    aZoomItem.SetType(SvxZoomType::WHOLEPAGE);
             }
             else
             {

include/sfx2/zoomitem.hxx
@@ -25,32 +25,32 @@
 
 
 
-enum SvxZoomType
+enum class SvxZoomType
 {
-    SVX_ZOOM_PERCENT,       // GetValue() is no special percentage value
-    SVX_ZOOM_OPTIMAL,       // GetValue() corresponds to the optimal size
-    SVX_ZOOM_WHOLEPAGE,     // GetValue() corresponds to the whole page
-    SVX_ZOOM_PAGEWIDTH,      // GetValue() corresponds to the page width
-    SVX_ZOOM_PAGEWIDTH_NOBORDER  // GetValue() pagewidth without border
+    PERCENT,       // GetValue() is no special percentage value
+    OPTIMAL,       // GetValue() corresponds to the optimal size
+    WHOLEPAGE,     // GetValue() corresponds to the whole page
+    PAGEWIDTH,      // GetValue() corresponds to the page width
+    PAGEWIDTH_NOBORDER  // GetValue() pagewidth without border
 };
 
 
 
 class SFX2_DLLPUBLIC SvxZoomItem: public SfxUInt16Item
 {
-    sal_uInt16                  nValueSet;  // allowed values (see #defines below)
+    sal_uInt16              nValueSet;  // allowed values (see #defines below)
     SvxZoomType             eType;
 
 public:
     TYPEINFO_OVERRIDE();
 
-    SvxZoomItem( SvxZoomType eZoomType = SVX_ZOOM_PERCENT,
+    SvxZoomItem( SvxZoomType eZoomType = SvxZoomType::PERCENT,
                  sal_uInt16 nVal = 0, sal_uInt16 nWhich = SID_ATTR_ZOOM );
     SvxZoomItem( const SvxZoomItem& );
     virtual ~SvxZoomItem();
 
     void                    SetValueSet( sal_uInt16 nValues ) { nValueSet = nValues; }
-    sal_uInt16                  GetValueSet() const { return nValueSet; }
+    sal_uInt16              GetValueSet() const { return nValueSet; }
     bool                    IsValueAllowed( sal_uInt16 nValue ) const
                             { return nValue == ( nValue & nValueSet ); }
 

reportdesign/source/ui/inc/ReportController.hxx
@@ -445,7 +445,7 @@ namespace rptui
 
         inline ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >  getContext() const { return m_xContext; }
         inline sal_Int16   getZoomValue() const     { return m_nZoomValue; }
-        inline void         resetZoomType()         { m_eZoomType = SVX_ZOOM_PERCENT; }
+        inline void         resetZoomType()         { m_eZoomType = SvxZoomType::PERCENT; }
 
         // com::sun::star::beans::XPropertySet
         virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo > SAL_CALL getPropertySetInfo(  ) throw(::com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE

reportdesign/source/ui/report/ReportController.cxx
@@ -298,7 +298,7 @@ OReportController::OReportController(Reference< XComponentContext > const & xCon
     ,m_nSelectionCount(0)
     ,m_nAspect(0)
     ,m_nZoomValue(100)
-    ,m_eZoomType(SVX_ZOOM_PERCENT)
+    ,m_eZoomType(SvxZoomType::PERCENT)
     ,m_bShowRuler(true)
     ,m_bGridVisible(true)
     ,m_bGridUse(true)
@@ -1645,7 +1645,7 @@ void OReportController::Execute(sal_uInt16 _nId, const Sequence< PropertyValue >
                 SvxZoomSliderItem aZoomSlider;
                 aZoomSlider.PutValue(aArgs[0].Value);
                 m_nZoomValue = aZoomSlider.GetValue();
-                m_eZoomType = SVX_ZOOM_PERCENT;
+                m_eZoomType = SvxZoomType::PERCENT;
                 impl_zoom_nothrow();
             }
             break;
@@ -4287,7 +4287,7 @@ void OReportController::openZoomDialog()
                 const SvxZoomItem&  rZoomItem = static_cast<const SvxZoomItem&>(pDlg->GetOutputItemSet()->Get( SID_ATTR_ZOOM ));
                 m_eZoomType = rZoomItem.GetType();
                 m_nZoomValue = rZoomItem.GetValue();
-                if ( m_eZoomType != SVX_ZOOM_PERCENT )
+                if ( m_eZoomType != SvxZoomType::PERCENT )
                     m_nZoomValue = getDesignView()->getZoomFactor( m_eZoomType );
 
                 impl_zoom_nothrow();

reportdesign/source/ui/report/ReportWindow.cxx
@@ -407,12 +407,12 @@ sal_uInt16 OReportWindow::getZoomFactor(SvxZoomType _eType) const
     const Size aSize( GetSizePixel() );
     switch( _eType)
     {
-        case SVX_ZOOM_PERCENT:
+        case SvxZoomType::PERCENT:
             nZoom = m_pView->getController().getZoomValue();
             break;
-        case SVX_ZOOM_OPTIMAL:
+        case SvxZoomType::OPTIMAL:
             break;
-        case SVX_ZOOM_WHOLEPAGE:
+        case SvxZoomType::WHOLEPAGE:
             {
                 nZoom = (sal_uInt16)(long)Fraction(aSize.Width()*100,impl_getRealPixelWidth());
                 MapMode aMap( MAP_100TH_MM );
@@ -420,7 +420,7 @@ sal_uInt16 OReportWindow::getZoomFactor(SvxZoomType _eType) const
                 nZoom = ::std::min(nZoom,(sal_uInt16)(long)Fraction(aSize.Height()*100,aHeight.Height()));
             }
             break;
-        case SVX_ZOOM_PAGEWIDTH:
+        case SvxZoomType::PAGEWIDTH:
             nZoom = (sal_uInt16)(long)Fraction(aSize.Width()*100,impl_getRealPixelWidth());
             break;
         default:

sc/source/core/tool/appoptio.cxx
@@ -60,7 +60,7 @@ void ScAppOptions::SetDefaults()
         eMetric     = FUNIT_INCH;           // default for others
 
     nZoom           = 100;
-    eZoomType       = SVX_ZOOM_PERCENT;
+    eZoomType       = SvxZoomType::PERCENT;
     bSynchronizeZoom = true;
     nStatusFunc     = SUBTOTAL_FUNC_SUM;
     bAutoComplete   = true;

sc/source/ui/unoobj/appluno.cxx
@@ -455,19 +455,19 @@ void SAL_CALL ScSpreadsheetSettings::setPropertyValue(
         short nVal = ScUnoHelpFunctions::GetInt16FromAny( aValue );
         if ( nVal < 0 )
         {
-            SvxZoomType eType = SVX_ZOOM_PERCENT;
+            SvxZoomType eType = SvxZoomType::PERCENT;
             switch (nVal)
             {
-                case SC_ZOOMVAL_OPTIMAL:    eType = SVX_ZOOM_OPTIMAL;   break;
-                case SC_ZOOMVAL_WHOLEPAGE:  eType = SVX_ZOOM_WHOLEPAGE; break;
-                case SC_ZOOMVAL_PAGEWIDTH:  eType = SVX_ZOOM_PAGEWIDTH; break;
+                case SC_ZOOMVAL_OPTIMAL:    eType = SvxZoomType::OPTIMAL;   break;
+                case SC_ZOOMVAL_WHOLEPAGE:  eType = SvxZoomType::WHOLEPAGE; break;
+                case SC_ZOOMVAL_PAGEWIDTH:  eType = SvxZoomType::PAGEWIDTH; break;
             }
             aAppOpt.SetZoomType( eType );
         }
         else if ( nVal >= MINZOOM && nVal <= MAXZOOM )
         {
             aAppOpt.SetZoom( nVal );
-            aAppOpt.SetZoomType( SVX_ZOOM_PERCENT );
+            aAppOpt.SetZoomType( SvxZoomType::PERCENT );
         }
         bSaveApp = true;
     }
@@ -549,10 +549,10 @@ uno::Any SAL_CALL ScSpreadsheetSettings::getPropertyValue( const OUString& aProp
         sal_Int16 nZoomVal = 0;
         switch ( aAppOpt.GetZoomType() )
         {
-            case SVX_ZOOM_PERCENT:   nZoomVal = aAppOpt.GetZoom();    break;
-            case SVX_ZOOM_OPTIMAL:   nZoomVal = SC_ZOOMVAL_OPTIMAL;   break;
-            case SVX_ZOOM_WHOLEPAGE: nZoomVal = SC_ZOOMVAL_WHOLEPAGE; break;
-            case SVX_ZOOM_PAGEWIDTH: nZoomVal = SC_ZOOMVAL_PAGEWIDTH; break;
+            case SvxZoomType::PERCENT:   nZoomVal = aAppOpt.GetZoom();    break;
+            case SvxZoomType::OPTIMAL:   nZoomVal = SC_ZOOMVAL_OPTIMAL;   break;
+            case SvxZoomType::WHOLEPAGE: nZoomVal = SC_ZOOMVAL_WHOLEPAGE; break;
+            case SvxZoomType::PAGEWIDTH: nZoomVal = SC_ZOOMVAL_PAGEWIDTH; break;
             default:
             {
                 // added to avoid warnings

zhirshya/lo-tblr

commit a181a4bc3717be3b8c963dc7a4c417c1d4281a30

fix windows build
"error: a scoped enumeration cannot be redeclared as an unscoped
enumeration"

caused by my commit
5d37fa2a710e3bd76d3f1e18d1d66b8a4ab1503
"convert SvxZoomType to enum class"

Change-Id: I26e6f9e7c608aaf1a1fbfe370d8a1bafc57c2983

sw/source/core/view/pagepreviewlayout.cxx
@@ -254,7 +254,7 @@ void SwPagePreviewLayout::_ApplyNewZoomAtViewShell( sal_uInt8 _aNewZoom )
     {
         aNewViewOptions.SetZoom( _aNewZoom );
         //#i19975# - consider zoom type.
-        enum SvxZoomType eZoomType = SvxZoomType::PERCENT;
+        SvxZoomType eZoomType = SvxZoomType::PERCENT;
         aNewViewOptions.SetZoomType( eZoomType );
         mrParentViewShell.ApplyViewOptions( aNewViewOptions );
     }