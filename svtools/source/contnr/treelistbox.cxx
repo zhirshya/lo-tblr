LibreOffice/core

commit 6bf3b6d420873e236b2805a42a95ce33e614ad98

loplugin:constantfunction: svtools
Change-Id: I42479fb10878a0a1c2071f7b35ae375061781d40

svtools/source/contnr/treelistbox.cxx
@@ -566,10 +566,6 @@ bool SvTreeListBox::CheckDragAndDropMode( SvTreeListBox* pSource, sal_Int8 nActi
 
 
 
-void SvTreeListBox::NotifyRemoving( SvTreeListEntry* )
-{
-}
-
 /*
     NotifyMoving/Copying
     ====================
@@ -808,13 +804,8 @@ void SvTreeListBox::RecalcViewData()
             pItem->InitViewData( this, pEntry );
             nCurPos++;
         }
-        ViewDataInitialized( pEntry );
         pEntry = Next( pEntry );
     }
-}
-
-void SvTreeListBox::ViewDataInitialized( SvTreeListEntry* )
-{
 }
 
 void SvTreeListBox::ImplShowTargetEmphasis( SvTreeListEntry* pEntry, bool bShow)
@@ -1191,14 +1182,6 @@ bool SvTreeListBox::HandleKeyInput( const KeyEvent& _rKEvt )
     return false;
 }
 
-void SvTreeListBox::WriteDragServerInfo( const Point&, SvLBoxDDInfo* )
-{
-}
-
-void SvTreeListBox::ReadDragServerInfo(const Point&, SvLBoxDDInfo* )
-{
-}
-
 bool SvTreeListBox::EditingCanceled() const
 {
     if( pEdCtrl && pEdCtrl->EditingCanceled() )
@@ -1279,9 +1262,9 @@ sal_Int8 SvTreeListBox::ExecuteDrop( const ExecuteDropEvent& rEvt, SvTreeListBox
     {
         nRet = DND_ACTION_NONE;
 
-        ReadDragServerInfo( rEvt.maPosPixel, &aDDInfo );
-
         SvTreeListEntry* pTarget = pTargetEntry; // may be 0!
+
+        memset( &aDDInfo, 0, sizeof(SvLBoxDDInfo) );
 
         if( DND_ACTION_COPY == rEvt.mnAction )
         {
@@ -1344,8 +1327,6 @@ void SvTreeListBox::StartDrag( sal_Int8, const Point& rPosPixel )
     aDDInfo.pApp = GetpApp();
     aDDInfo.pSource = this;
     aDDInfo.pDDStartEntry = pEntry;
-    // let derived views do their thing
-    WriteDragServerInfo( rPosPixel, &aDDInfo );
 
     pContainer->CopyAnyData( SotClipboardFormatId::TREELISTBOX,
                         reinterpret_cast<char*>(&aDDInfo), sizeof(SvLBoxDDInfo) );
@@ -2263,7 +2244,6 @@ void SvTreeListBox::ScrollOutputArea( short nDeltaEntries )
     long nThumb = pImp->aVerSBar.GetThumbPos();
     long nMax = pImp->aVerSBar.GetRange().Max();
 
-    NotifyBeginScroll();
     if( nDeltaEntries < 0 )
     {
         // move window up
@@ -2526,7 +2506,6 @@ void SvTreeListBox::ModelIsRemoving( SvTreeListEntry* pEntry )
         pEdEntry = NULL;
 
     pImp->RemovingEntry( (SvTreeListEntry*)pEntry );
-    NotifyRemoving( (SvTreeListEntry*)pEntry );
 }
 
 void SvTreeListBox::ModelHasRemoved( SvTreeListEntry* pEntry  )
@@ -3510,15 +3489,7 @@ Size SvTreeListBox::GetOutputSizePixel() const
     return aSize;
 }
 
-void SvTreeListBox::NotifyBeginScroll()
-{
-}
-
 void SvTreeListBox::NotifyEndScroll()
-{
-}
-
-void SvTreeListBox::NotifyScrolling( long )
 {
 }
 
@@ -3527,17 +3498,12 @@ void SvTreeListBox::NotifyScrolled()
     aScrolledHdl.Call( this );
 }
 
-void SvTreeListBox::NotifyInvalidating()
-{
-}
-
 void SvTreeListBox::Invalidate( sal_uInt16 nInvalidateFlags )
 {
     if( nFocusWidth == -1 )
         // to make sure that the control doesn't show the wrong focus rectangle
         // after painting
         pImp->RecalcFocusRect();
-    NotifyInvalidating();
     Control::Invalidate( nInvalidateFlags );
     pImp->Invalidate();
 }
@@ -3548,7 +3514,6 @@ void SvTreeListBox::Invalidate( const Rectangle& rRect, sal_uInt16 nInvalidateFl
         // to make sure that the control doesn't show the wrong focus rectangle
         // after painting
         pImp->RecalcFocusRect();
-    NotifyInvalidating();
     Control::Invalidate( rRect, nInvalidateFlags );
 }
 
@@ -3702,11 +3667,6 @@ void SvTreeListBox::ModelNotification( SvListAction nActionId, SvTreeListEntry* 
 void SvTreeListBox::EndSelection()
 {
     pImp->EndSelection();
-}
-
-void SvTreeListBox::RepaintScrollBars() const
-{
-    ((SvTreeListBox*)this)->pImp->RepaintScrollBars();
 }
 
 ScrollBar *SvTreeListBox::GetVScroll()

cui/source/options/optaccessibility.cxx
@@ -79,9 +79,6 @@ bool SvxAccessibilityOptionsTabPage::FillItemSet( SfxItemSet* )
     m_pImpl->m_aConfig.SetSelectionInReadonly( m_pTextSelectionInReadonly->IsChecked());
     m_pImpl->m_aConfig.SetAutoDetectSystemHC( m_pAutoDetectHC->IsChecked());
 
-    if(m_pImpl->m_aConfig.IsModified())
-        m_pImpl->m_aConfig.Commit();
-
     AllSettings aAllSettings = Application::GetSettings();
     MiscSettings aMiscSettings = aAllSettings.GetMiscSettings();
 #ifndef UNX

filter/source/xsltdialog/xmlfiltersettingsdialog.cxx
@@ -1421,7 +1421,6 @@ bool SvxPathControl::Notify(NotifyEvent& rNEvt)
 
 XMLFilterListBox::XMLFilterListBox(Window* pParent, SvxPathControl* pPathControl)
     : SvTabListBox(pParent, WB_SORT | WB_HSCROLL | WB_CLIPCHILDREN | WB_TABSTOP)
-    , mbFirstPaint(true)
     , m_pHeaderBar(pPathControl->getHeaderBar())
 {
     Size aBoxSize( pParent->GetOutputSizePixel() );
@@ -1452,12 +1451,6 @@ XMLFilterListBox::XMLFilterListBox(Window* pParent, SvxPathControl* pPathControl
 
 void XMLFilterListBox::Paint( const Rectangle& rRect )
 {
-    if( mbFirstPaint )
-    {
-        mbFirstPaint = false;
-        RepaintScrollBars();
-    }
-
     SvTabListBox::Paint( rRect );
 }
 

filter/source/xsltdialog/xmlfiltersettingsdialog.hxx
@@ -59,7 +59,6 @@ class HeaderBar;
 class XMLFilterListBox : public SvTabListBox
 {
 private:
-    bool        mbFirstPaint;
     HeaderBar*  m_pHeaderBar;
 
     DECL_LINK( TabBoxScrollHdl_Impl, SvTabListBox* );

include/svtools/accessibilityoptions.hxx
@@ -57,7 +57,6 @@ public:
     void        SetAutoDetectSystemHC(bool bSet);
 
     bool        IsModified() const;
-    void        Commit();
 
     //SfxListener:
     virtual void        Notify( SfxBroadcaster& rBC, const SfxHint& rHint ) SAL_OVERRIDE;

include/svtools/accessibleruler.hxx
@@ -68,8 +68,6 @@ public:
 protected:
     virtual ~SvtRulerAccessible();
 public:
-    bool SAL_CALL
-        isShowing() throw( ::com::sun::star::uno::RuntimeException );
 
     bool SAL_CALL
         isVisible() throw( ::com::sun::star::uno::RuntimeException );

include/svtools/brwbox.hxx
@@ -363,7 +363,6 @@ protected:
     */
     virtual bool    SeekRow( long nRow ) = 0;
     void            DrawCursor();
-    void            PaintRow( OutputDevice &rDev, const Rectangle &rRect );
     void            PaintData( vcl::Window& rWin, const Rectangle& rRect );
     virtual void    PaintField( OutputDevice& rDev, const Rectangle& rRect,
                                 sal_uInt16 nColumnId ) const = 0;
@@ -450,7 +449,6 @@ public:
 
     // neue Handler
     virtual void    MouseButtonDown( const BrowserMouseEvent& rEvt );
-    void            MouseMove( const BrowserMouseEvent& rEvt );
     virtual void    MouseButtonUp( const BrowserMouseEvent& rEvt );
     void            StartScroll();
     virtual void    EndScroll();

include/svtools/ctrltool.hxx
@@ -134,6 +134,8 @@ FontList.
 class SVT_DLLPUBLIC FontList
 {
 private:
+    static const sal_IntPtr aStdSizeAry[];
+
     OUString                maMapBoth;
     OUString                maMapPrinterOnly;
     OUString                maMapScreenOnly;
@@ -193,7 +195,7 @@ public:
     const vcl::FontInfo&    GetFontInfo( sal_Handle hFontInfo ) const;
 
     const sal_IntPtr*       GetSizeAry( const vcl::FontInfo& rInfo ) const;
-    static const sal_IntPtr* GetStdSizeAry();
+    static const sal_IntPtr* GetStdSizeAry() { return aStdSizeAry; }
 
 private:
                             FontList( const FontList& ) SAL_DELETED_FUNCTION;

zhirshya/lo-tblr

commit 2685deb46e6958b4a7d9c6cc30ff4882082b43f8

tdf#90292: Crash in pivot table when dragging field into Rows fields
Regression from 6bf3b6d420873e236b2805a42a95ce33e614ad98

Change-Id: I422958e5979abeee1b07a4d0e81328d72d2c61c9

svtools/source/contnr/treelistbox.cxx
@@ -1246,6 +1246,7 @@ sal_Int8 SvTreeListBox::ExecuteDrop( const ExecuteDropEvent& rEvt, SvTreeListBox
     pDDTarget = this;
 
     SvLBoxDDInfo aDDInfo;
+    memset( &aDDInfo, 0, sizeof(SvLBoxDDInfo) );
 
     TransferableDataHelper aData( rEvt.maDropEvent.Transferable );
     if( aData.HasFormat( SotClipboardFormatId::TREELISTBOX ))
@@ -1264,7 +1265,6 @@ sal_Int8 SvTreeListBox::ExecuteDrop( const ExecuteDropEvent& rEvt, SvTreeListBox
 
         SvTreeListEntry* pTarget = pTargetEntry; // may be 0!
 
-        memset( &aDDInfo, 0, sizeof(SvLBoxDDInfo) );
 
         if( DND_ACTION_COPY == rEvt.mnAction )
         {