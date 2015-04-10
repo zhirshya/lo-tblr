chidelmun/core

vclwidget: improve detection of vcl::Window fields
that need to be wrapped in VclPtr, and convert several sites

Change-Id: I1fb1ed164a99642a0c1ccbf433eb14df688fa476

include/svtools/valueset.hxx
@@ -201,7 +201,7 @@ private:
     Timer           maTimer;
     ValueItemList   mItemList;
     ValueSetItemPtr mpNoneItem;
-    boost::scoped_ptr<ScrollBar> mpScrollBar;
+    VclPtr<ScrollBar> mpScrollBar;
     Rectangle       maNoneItemRect;
     Rectangle       maItemListRect;
     long            mnItemWidth;

vcl/source/window/openglwin.cxx
@@ -19,7 +19,7 @@ public:
     OpenGLContext& getContext() { return maContext;}
 private:
     OpenGLContext maContext;
-    boost::scoped_ptr<SystemChildWindow> mpChildWindow;
+    VclPtr<SystemChildWindow> mpChildWindow;
 };
 
 OpenGLWindowImpl::OpenGLWindowImpl(vcl::Window* pWindow)

zhirshya/lo-tblr

include/svtools/valueset.hxx
@@ -201,7 +201,7 @@ private:
     Timer           maTimer;
     ValueItemList   mItemList;
     ValueSetItemPtr mpNoneItem;
-    std::unique_ptr<ScrollBar> mxScrollBar;
+    VclPtr<ScrollBar> mxScrollBar;
     Rectangle       maNoneItemRect;
     Rectangle       maItemListRect;
     long            mnItemWidth;

vcl/source/window/openglwin.cxx
@@ -19,7 +19,7 @@ public:
     OpenGLContext& getContext() { return maContext;}
 private:
     OpenGLContext maContext;
-    std::unique_ptr<SystemChildWindow> mxChildWindow;
+    VclPtr<SystemChildWindow> mxChildWindow;
 };
 
 OpenGLWindowImpl::OpenGLWindowImpl(vcl::Window* pWindow)