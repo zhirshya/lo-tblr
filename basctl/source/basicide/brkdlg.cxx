chidelmun/core

vclwidget: convert lots more sites to use VclPtr
seems there was a lot of shared_ptr usage floating around...

Change-Id: Icd05243170eb8493709275fc36bf986fc194b781

basctl/source/basicide/brkdlg.cxx
@@ -118,6 +118,7 @@ void BreakPointDialog::dispose()
     m_pNewButton.disposeAndClear();
     m_pDelButton.disposeAndClear();
     m_pNumericField.disposeAndClear();
+    m_pCheckBox.disposeAndClear();
     ModalDialog::dispose();
 }
 

zhirshya/lo-tblr

basctl/source/basicide/brkdlg.cxx
@@ -118,6 +118,7 @@ void BreakPointDialog::dispose()
     m_pNewButton.clear();
     m_pDelButton.clear();
     m_pNumericField.clear();
+    m_pCheckBox.clear();
     ModalDialog::dispose();
 }
 