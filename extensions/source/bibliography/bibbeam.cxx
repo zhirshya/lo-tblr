chidelmun/core

vclwidget: check for calling delete on subclasses of vcl::Window
Change-Id: I7fb7cf919e3f46dd03a18b1cb95fa881915f9642

extensions/source/bibliography/bibbeam.cxx
@@ -221,10 +221,10 @@ namespace bib
 
         if( pGridWin )
         {
-            BibGridwin* pDel = pGridWin;
+            VclPtr<BibGridwin> pDel = pGridWin;
             pGridWin.disposeAndClear();
             pDel->disposeGridWin();
-            delete pDel;
+            pDel.disposeAndClear();
         }
         pToolBar.disposeAndClear();
         pGridWin.disposeAndClear();

zhirshya/lo-tblr

extensions/source/bibliography/bibbeam.cxx
@@ -215,11 +215,9 @@ namespace bib
             m_xToolBarRef->dispose();
 
         if ( pToolBar )
-        {
             pDatMan->SetToolbar(0);
-            pToolBar.disposeAndClear();
-        }
 
+        pToolBar.disposeAndClear();
         pGridWin.disposeAndClear();
         BibSplitWindow::dispose();
     }