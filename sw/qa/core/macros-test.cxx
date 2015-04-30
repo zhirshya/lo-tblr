LibreOffice/core

commit 370febbf19a5f362394d1c9e69b12dcb218f6501

tdf#75996: sw: correct bookmark positions when deleting text
The problem is that SwUndoDelete will move the fully selected nodes to
the UndoNodes but it leaves bookmarks with their SwIndex pointing to the
deleted nodes.  The SwNodeIndex are corrected by SwNodes::_MoveNodes()
so they point to a different node than the SwIndex.

This only happens if only one position of the bookmark is inside the
deletion range; if both are, the bookmark will be deleted by
SwUndoSaveCntnt::DelCntntIndex().

Also joining the 2 start/end nodes of the selection will accidentally
correct the bookmarks but only if it happens to delete the end node.
(and apparently there is also a DeleteRange method that doesn't join)

Change-Id: I91ec362bb833328f8d681fd9458cb915c4efb267

sw/source/core/undo/undel.cxx
@@ -153,7 +153,11 @@ SwUndoDelete::SwUndoDelete(
         _DelBookmarks(pStt->nNode, pEnd->nNode);
     }
     else
+    {
         DelCntntIndex( *rPam.GetMark(), *rPam.GetPoint() );
+        ::sw::UndoGuard const undoGuard(pDoc->GetIDocumentUndoRedo());
+        _DelBookmarks(pStt->nNode, pEnd->nNode, nullptr, &pStt->nContent, &pEnd->nContent);
+    }
 
     nSetPos = pHistory ? pHistory->Count() : 0;
 

sw/qa/core/macros-test.cxx
@@ -43,6 +43,8 @@
 
 #include <doc.hxx>
 #include <IDocumentLayoutAccess.hxx>
+#include <IDocumentUndoRedo.hxx>
+#include <IDocumentContentOperations.hxx>
 #include "docsh.hxx"
 
 typedef tools::SvRef<SwDocShell> SwDocShellRef;
@@ -66,6 +68,7 @@ public:
 #if !defined MACOSX && !defined WNT
     void testVba();
 #endif
+    void testBookmarkDeleteAndJoin();
     void testFdo55289();
     void testFdo68983();
     CPPUNIT_TEST_SUITE(SwMacrosTest);
@@ -75,6 +78,7 @@ public:
     //CPPUNIT_TEST(testStarBasic);
     CPPUNIT_TEST(testVba);
 #endif
+    CPPUNIT_TEST(testBookmarkDeleteAndJoin);
     CPPUNIT_TEST(testFdo55289);
     CPPUNIT_TEST(testFdo68983);
 
@@ -160,6 +164,45 @@ void SwMacrosTest::testVba()
     }
 }
 #endif
+
+void SwMacrosTest::testBookmarkDeleteAndJoin()
+{
+    SwDoc *const pDoc = new SwDoc;
+    pDoc->GetIDocumentUndoRedo().DoUndo(true); // bug is in SwUndoDelete
+    SwNodeIndex aIdx(pDoc->GetNodes().GetEndOfContent(), -1);
+    SwPaM aPaM(aIdx);
+
+    IDocumentContentOperations & rIDCO(pDoc->getIDocumentContentOperations());
+    rIDCO.AppendTxtNode(*aPaM.GetPoint());
+    rIDCO.InsertString(aPaM, OUString("A"));
+    rIDCO.AppendTxtNode(*aPaM.GetPoint());
+    rIDCO.InsertString(aPaM, OUString("A"));
+    rIDCO.AppendTxtNode(*aPaM.GetPoint());
+    aPaM.Move(fnMoveBackward, fnGoNode);
+    aPaM.Move(fnMoveBackward, fnGoNode);
+    aPaM.Move(fnMoveBackward, fnGoCntnt);
+    aPaM.SetMark();
+    aPaM.Move(fnMoveForward, fnGoDoc);
+    IDocumentMarkAccess & rIDMA = *pDoc->getIDocumentMarkAccess();
+    sw::mark::IMark *pMark =
+            rIDMA.makeMark(aPaM, "test", IDocumentMarkAccess::BOOKMARK);
+    CPPUNIT_ASSERT(pMark);
+    // select so pMark start position is on a node that is fully deleted
+    aPaM.Move(fnMoveBackward, fnGoNode);
+    // must leave un-selected content in last node to get the bJoinPrev flag!
+    aPaM.Move(fnMoveBackward, fnGoCntnt);
+    aPaM.Exchange();
+    aPaM.Move(fnMoveBackward, fnGoDoc);
+    // delete
+    rIDCO.DeleteAndJoin(aPaM, false);
+
+    for (IDocumentMarkAccess::const_iterator_t i = rIDMA.getAllMarksBegin(); i != rIDMA.getAllMarksEnd(); ++i)
+    {
+        // problem was that the nContent was pointing at deleted node
+        CPPUNIT_ASSERT((*i)->GetMarkStart().nNode.GetNode().GetCntntNode() ==
+            static_cast<const SwCntntNode*>((*i)->GetMarkStart().nContent.GetIdxReg()));
+    }
+}
 
 void SwMacrosTest::testFdo55289()
 {

zhirshya/lo-tblr

commit a11330eb617f148eeba04f67733b41e5fcfe5c36

tdf#90816: sw: fix bookmark loss in SwUndoDelete
_DelBookmarks() will actually delete marks that exactly match both start
and end position of the range, so restrict the call to only the
fully-deleted nodes that will be moved to the Undo-array.

(regression from 370febbf19a5f362394d1c9e69b12dcb218f6501)

Change-Id: Icf5097774aa55ee152a1e20c0c7264330955c615

sw/source/core/undo/undel.cxx
@@ -154,7 +154,11 @@ SwUndoDelete::SwUndoDelete(
     {
         DelCntntIndex( *rPam.GetMark(), *rPam.GetPoint() );
         ::sw::UndoGuard const undoGuard(pDoc->GetIDocumentUndoRedo());
-        _DelBookmarks(pStt->nNode, pEnd->nNode, nullptr, &pStt->nContent, &pEnd->nContent);
+        if (nEndNode - nSttNode > 1) // check for fully selected nodes
+        {
+            SwNodeIndex const start(pStt->nNode, +1);
+            _DelBookmarks(start, pEnd->nNode);
+        }
     }
 
     nSetPos = pHistory ? pHistory->Count() : 0;

sw/qa/core/macros-test.cxx
@@ -79,6 +79,7 @@ public:
     void testVba();
 #endif
     void testBookmarkDeleteAndJoin();
+    void testBookmarkDeleteTdf90816();
 #if 0
     void testControlShapeGrouping();
 #endif
@@ -93,6 +94,7 @@ public:
     CPPUNIT_TEST(testVba);
 #endif
     CPPUNIT_TEST(testBookmarkDeleteAndJoin);
+    CPPUNIT_TEST(testBookmarkDeleteTdf90816);
 #if 0
     CPPUNIT_TEST(testControlShapeGrouping);
 #endif
@@ -220,6 +222,35 @@ void SwMacrosTest::testBookmarkDeleteAndJoin()
         CPPUNIT_ASSERT((*i)->GetMarkStart().nNode.GetNode().GetCntntNode() ==
             static_cast<const SwCntntNode*>((*i)->GetMarkStart().nContent.GetIdxReg()));
     }
+}
+
+void SwMacrosTest::testBookmarkDeleteTdf90816()
+{
+    SwDoc *const pDoc = new SwDoc;
+    pDoc->GetIDocumentUndoRedo().DoUndo(true); // bug is in SwUndoDelete
+    SwNodeIndex aIdx(pDoc->GetNodes().GetEndOfContent(), -1);
+    SwPaM aPaM(aIdx);
+
+    IDocumentContentOperations & rIDCO(pDoc->getIDocumentContentOperations());
+    rIDCO.AppendTxtNode(*aPaM.GetPoint());
+    rIDCO.InsertString(aPaM, OUString("ABC"));
+    aPaM.Move(fnMoveBackward, fnGoCntnt);
+    aPaM.SetMark();
+    aPaM.Move(fnMoveBackward, fnGoCntnt);
+    IDocumentMarkAccess & rIDMA = *pDoc->getIDocumentMarkAccess();
+    sw::mark::IMark *pMark =
+        rIDMA.makeMark(aPaM, "test", IDocumentMarkAccess::MarkType::BOOKMARK);
+    CPPUNIT_ASSERT(pMark);
+
+    // delete the same selection as the bookmark
+    rIDCO.DeleteAndJoin(aPaM, false);
+
+    // bookmark still there?
+    auto iter = rIDMA.getAllMarksBegin();
+    CPPUNIT_ASSERT_MESSAGE("the bookmark was deleted",
+            iter != rIDMA.getAllMarksEnd());
+    CPPUNIT_ASSERT(*aPaM.Start() == (*iter)->GetMarkPos());
+    CPPUNIT_ASSERT(*aPaM.End() == (*iter)->GetOtherMarkPos());
 }
 
 #if 0