LibreOffice/core

commit 738fb2ad77e5a1a4d6e2dc540886a17f4527e4db

faster mapping from nodes to SwFrmFmt's anchored at them
The SwFrmFmtAnchorMap class provides reverse mapping
to SwFrmFmt::GetAnchor().GetCntntAnchor(), so that when code somewhere needs
to update SwFrmFmt's anchored at a position, it's not necessary to iterate
over all of them (which can be a large number e.g. with mail merge).

One special catch with the multimap of SwNodeIndex keys is that
the values of those keys change (whenever the node structure of the document
changes, indexes of nodes change as a result). This makes it impossible to use
any hashing container, as the hashes would change without the container
noticing, but multimap should work fine, as it just requires that the keys
remain sorted, and that is the case. Nevertheless, the old code in the two
converted places is intentionally left there in debug mode to verify the reverse
mapping is updated correctly.

I intentionally went with SwNodeIndex rather than SwPosition, as SwIndex
(the other component of SwPosition) was causing some trouble (see e.g. the
SwPosition comparison operator< , where two same positions are different if one
is registered and the other not) and it doesn't appear to be actually necessary.

Change-Id: I7f1768558f60155d4ba83c84aa7f9e34dc65ebf9

sw/inc/doc.hxx
@@ -99,6 +99,7 @@ class SwFmt;
 class SwFmtINetFmt;
 class SwFmtRefMark;
 class SwFrmFmt;
+class SwFrmFmtAnchorMap;
 class SwFrmFmts;
 class SwFtnIdxs;
 class SwFtnInfo;
@@ -302,6 +303,7 @@ class SW_DLLPUBLIC SwDoc :
     SwGrfFmtColl    *mpDfltGrfFmtColl;
 
     SwFrmFmts       *mpFrmFmtTbl;        //< Format table
+    SwFrmFmtAnchorMap *mpFrmFmtAnchorMap;
     SwCharFmts      *mpCharFmtTbl;
     SwFrmFmts       *mpSpzFrmFmtTbl;
     SwSectionFmts   *mpSectionFmtTbl;
@@ -810,6 +812,9 @@ public:
           SwFrmFmt *GetColumnContFmt()      { return mpColumnContFmt; }
     const SwCharFmt *GetDfltCharFmt() const { return mpDfltCharFmt;}
           SwCharFmt *GetDfltCharFmt()       { return mpDfltCharFmt;}
+
+    const SwFrmFmtAnchorMap* GetFrmFmtAnchorMap() const { return mpFrmFmtAnchorMap; }
+    SwFrmFmtAnchorMap* GetFrmFmtAnchorMap() { return mpFrmFmtAnchorMap; }
 
     // @return the interface of the management of (auto)styles
     IStyleAccess& GetIStyleAccess() { return *mpStyleAccess; }

sw/inc/frmfmt.hxx
@@ -23,6 +23,8 @@
 #include <cppuhelper/weakref.hxx>
 #include <tools/gen.hxx>
 #include <format.hxx>
+#include <map>
+#include <ndindex.hxx>
 #include "swdllapi.h"
 
 class SwFlyFrm;
@@ -67,6 +69,7 @@ protected:
 
 public:
     TYPEINFO_OVERRIDE();     ///< Already in base class Client.
+    virtual ~SwFrmFmt();
 
     /// Destroys all Frms in aDepend (Frms are identified via PTR_CAST).
     virtual void DelFrms();
@@ -299,6 +302,30 @@ public:
 };
 
 SW_DLLPUBLIC bool IsFlyFrmFmtInHeader(const SwFrmFmt& rFmt);
+
+/**
+ Fast mapping from node positions to SwFrmFmt objects anchored at them.
+
+ SwFrmFmt::GetAnchor().GetCntntAnchor() provides the position where the object is anchored.
+ This class provides the reverse mapping. It intentionally uses SwNodeIndex instead of SwPosition
+ to allow simpler implementation, do SwIndex checking explicitly if needed.
+*/
+class SwFrmFmtAnchorMap
+{
+public:
+    SwFrmFmtAnchorMap( const SwDoc* doc );
+    void Add( SwFrmFmt* fmt, const SwNodeIndex& index );
+    void Remove( SwFrmFmt* fmt, const SwNodeIndex& index );
+    typedef std::multimap< SwNodeIndex, SwFrmFmt* >::const_iterator const_iterator;
+    typedef std::pair< const_iterator, const_iterator > const_iterator_pair;
+    const_iterator_pair equal_range( const SwNodeIndex& pos ) const;
+    const_iterator lower_bound( const SwNodeIndex& pos ) const;
+    const_iterator upper_bound( const SwNodeIndex& pos ) const;
+    const_iterator end() const;
+private:
+    std::multimap< SwNodeIndex, SwFrmFmt* > items;
+    const SwDoc* doc;
+};
 
 #endif
 

sw/source/core/doc/docnew.cxx
@@ -231,6 +231,7 @@ SwDoc::SwDoc()
     mpDfltTxtFmtColl( new SwTxtFmtColl( GetAttrPool(), sTxtCollStr ) ),
     mpDfltGrfFmtColl( new SwGrfFmtColl( GetAttrPool(), sGrfCollStr ) ),
     mpFrmFmtTbl( new SwFrmFmts() ),
+    mpFrmFmtAnchorMap( new SwFrmFmtAnchorMap( this ) ),
     mpCharFmtTbl( new SwCharFmts() ),
     mpSpzFrmFmtTbl( new SwFrmFmts() ),
     mpSectionFmtTbl( new SwSectionFmts() ),
@@ -609,6 +610,7 @@ SwDoc::~SwDoc()
     delete mpDfltCharFmt;
     delete mpDfltFrmFmt;
     delete mpLayoutCache;
+    delete mpFrmFmtAnchorMap;
 
     SfxItemPool::Free(mpAttrPool);
 }

sw/source/core/layout/atrfrm.cxx
@@ -2423,6 +2423,16 @@ SwFrmFmt::SwFrmFmt(
 {
 }
 
+SwFrmFmt::~SwFrmFmt()
+{
+    if( !GetDoc()->IsInDtor())
+    {
+        const SwFmtAnchor& anchor = GetAnchor();
+        if( anchor.GetCntntAnchor() != NULL )
+            GetDoc()->GetFrmFmtAnchorMap()->Remove( this, anchor.GetCntntAnchor()->nNode );
+    }
+}
+
 bool SwFrmFmt::supportsFullDrawingLayerFillAttributeSet() const
 {
     return true;
@@ -2490,6 +2500,31 @@ void SwFrmFmt::Modify( const SfxPoolItem* pOld, const SfxPoolItem* pNew )
     {   // invalidate cached uno object
         SetXObject(uno::Reference<uno::XInterface>(0));
     }
+
+    const SwPosition* oldAnchorPosition = NULL;
+    const SwPosition* newAnchorPosition = NULL;
+    if( pNew && pNew->Which() == RES_ATTRSET_CHG )
+    {
+        const SfxPoolItem* tmp = NULL;
+        static_cast< const SwAttrSetChg* >(pNew)->GetChgSet()->GetItemState( RES_ANCHOR, false, &tmp );
+        if( tmp )
+            newAnchorPosition = static_cast< const SwFmtAnchor* >( tmp )->GetCntntAnchor();
+    }
+    if( pNew && pNew->Which() == RES_ANCHOR )
+        newAnchorPosition = static_cast< const SwFmtAnchor* >( pNew )->GetCntntAnchor();
+    if( pOld && pOld->Which() == RES_ATTRSET_CHG )
+    {
+        const SfxPoolItem* tmp = NULL;
+        static_cast< const SwAttrSetChg* >(pOld)->GetChgSet()->GetItemState( RES_ANCHOR, false, &tmp );
+        if( tmp )
+            oldAnchorPosition = static_cast< const SwFmtAnchor* >( tmp )->GetCntntAnchor();
+    }
+    if( pOld && pOld->Which() == RES_ANCHOR )
+        oldAnchorPosition = static_cast< const SwFmtAnchor* >( pOld )->GetCntntAnchor();
+    if( oldAnchorPosition != NULL && ( newAnchorPosition == NULL || oldAnchorPosition->nNode.GetIndex() != newAnchorPosition->nNode.GetIndex()))
+        GetDoc()->GetFrmFmtAnchorMap()->Remove( this, oldAnchorPosition->nNode );
+    if( newAnchorPosition != NULL && ( oldAnchorPosition == NULL || oldAnchorPosition->nNode.GetIndex() != newAnchorPosition->nNode.GetIndex()))
+        GetDoc()->GetFrmFmtAnchorMap()->Add( this, newAnchorPosition->nNode );
 }
 
 void SwFrmFmt::RegisterToFormat( SwFmt& rFmt )
@@ -3297,6 +3332,54 @@ bool IsFlyFrmFmtInHeader(const SwFrmFmt& rFmt)
         }
     }
     return false;
+}
+
+
+SwFrmFmtAnchorMap::SwFrmFmtAnchorMap( const SwDoc* _doc )
+: doc( _doc )
+{
+}
+
+void SwFrmFmtAnchorMap::Add( SwFrmFmt* fmt, const SwNodeIndex& pos )
+{
+    assert( pos.GetNode().GetDoc() == doc );
+    items.insert( std::make_pair( pos, fmt ));
+}
+
+void SwFrmFmtAnchorMap::Remove( SwFrmFmt* fmt, const SwNodeIndex& pos )
+{
+    assert( pos.GetNode().GetDoc() == doc );
+    typedef std::multimap< SwNodeIndex, SwFrmFmt* >::iterator iterator;
+    std::pair< iterator, iterator > range = items.equal_range( pos );
+    for( iterator it = range.first; it != range.second; ++it )
+    {
+        if( it->second == fmt )
+        {
+            items.erase( it );
+            return;
+        }
+    }
+    assert( false );
+}
+
+SwFrmFmtAnchorMap::const_iterator_pair SwFrmFmtAnchorMap::equal_range( const SwNodeIndex& pos ) const
+{
+    return items.equal_range( pos );
+}
+
+SwFrmFmtAnchorMap::const_iterator SwFrmFmtAnchorMap::lower_bound( const SwNodeIndex& pos ) const
+{
+    return items.lower_bound( pos );
+}
+
+SwFrmFmtAnchorMap::const_iterator SwFrmFmtAnchorMap::upper_bound( const SwNodeIndex& pos ) const
+{
+    return items.upper_bound( pos );
+}
+
+SwFrmFmtAnchorMap::const_iterator SwFrmFmtAnchorMap::end() const
+{
+    return items.end();
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/layout/frmtool.cxx
@@ -999,15 +999,36 @@ SwCntntNotify::~SwCntntNotify()
 }
 
 void AppendObjs( const SwFrmFmts *pTbl, sal_uLong nIndex,
-                        SwFrm *pFrm, SwPageFrm *pPage )
+                        SwFrm *pFrm, SwPageFrm *pPage, SwDoc* doc )
 {
+#if OSL_DEBUG_LEVEL > 0
+    std::list<SwFrmFmt*> checkFmts;
     for ( sal_uInt16 i = 0; i < pTbl->size(); ++i )
     {
-        SwFrmFmt *pFmt = (SwFrmFmt*)(*pTbl)[i];
+        SwFrmFmt *pFmt = (*pTbl)[i];
         const SwFmtAnchor &rAnch = pFmt->GetAnchor();
         if ( rAnch.GetCntntAnchor() &&
              (rAnch.GetCntntAnchor()->nNode.GetIndex() == nIndex) )
         {
+            checkFmts.push_back( pFmt );
+        }
+    }
+#endif
+    SwFrmFmtAnchorMap::const_iterator_pair range = doc->GetFrmFmtAnchorMap()->equal_range( SwNodeIndex( doc->GetNodes(), nIndex ));
+    for( std::multimap< SwNodeIndex, SwFrmFmt* >::const_iterator it = range.first;
+         it != range.second;
+         )
+    {
+        SwFrmFmt *pFmt = it->second;
+        const SwFmtAnchor &rAnch = pFmt->GetAnchor();
+        if ( rAnch.GetCntntAnchor() &&
+             (rAnch.GetCntntAnchor()->nNode.GetIndex() == nIndex) )
+        {
+#if OSL_DEBUG_LEVEL > 0
+            std::list<SwFrmFmt*>::iterator checkPos = std::find( checkFmts.begin(), checkFmts.end(), pFmt );
+            assert( checkPos != checkFmts.end());
+            checkFmts.erase( checkPos );
+#endif
             const bool bFlyAtFly = rAnch.GetAnchorId() == FLY_AT_FLY; // LAYER_IMPL
             //Is a frame or a SdrObject described?
             const bool bSdrObj = RES_DRAWFRMFMT == pFmt->Which();
@@ -1025,8 +1046,8 @@ void AppendObjs( const SwFrmFmts *pTbl, sal_uLong nIndex,
                 if ( bSdrObj && 0 == (pSdrObj = pFmt->FindSdrObject()) )
                 {
                     OSL_ENSURE( !bSdrObj, "DrawObject not found." );
+                    ++it;
                     pFmt->GetDoc()->DelFrmFmt( pFmt );
-                    --i;
                     continue;
                 }
                 if ( pSdrObj )
@@ -1071,7 +1092,11 @@ void AppendObjs( const SwFrmFmts *pTbl, sal_uLong nIndex,
                 }
             }
         }
+        ++it;
     }
+#if OSL_DEBUG_LEVEL > 0
+    assert( checkFmts.empty());
+#endif
 }
 
 static bool lcl_ObjConnected( const SwFrmFmt *pFmt, const SwFrm* pSib )
@@ -1311,7 +1336,7 @@ void _InsertCnt( SwLayoutFrm *pLay, SwDoc *pDoc,
             pPrv = pFrm;
 
             if ( !pTbl->empty() && bObjsDirect && !bDontCreateObjects )
-                AppendObjs( pTbl, nIndex, pFrm, pPage );
+                AppendObjs( pTbl, nIndex, pFrm, pPage, pDoc );
         }
         else if ( pNd->IsTableNode() )
         {   //Should we have encountered a table?
@@ -1531,7 +1556,7 @@ void _InsertCnt( SwLayoutFrm *pLay, SwDoc *pDoc,
             {
                 SwFlyFrm* pFly = pLay->FindFlyFrm();
                 if( pFly )
-                    AppendObjs( pTbl, nIndex, pFly, pPage );
+                    AppendObjs( pTbl, nIndex, pFly, pPage, pDoc );
             }
         }
         else

sw/source/core/txtnode/ndtxt.cxx
@@ -1106,6 +1106,8 @@ void SwTxtNode::Update(
         }
 
         // at-char anchored flys shouldn't be moved, either.
+#if OSL_DEBUG_LEVEL > 0
+        std::list<SwFrmFmt*> checkFmts;
         const SwFrmFmts& rFmts = *GetDoc()->GetSpzFrmFmts();
         for (SwFrmFmts::const_iterator pFmt = rFmts.begin(); pFmt != rFmts.end(); ++pFmt)
         {
@@ -1116,10 +1118,43 @@ void SwTxtNode::Update(
                 // The fly is at-char anchored and has an anchor position.
                 SwIndex& rEndIdx = const_cast<SwIndex&>(pCntntAnchor->nContent);
                 if (&pCntntAnchor->nNode.GetNode() == this && rEndIdx.GetIndex() == rPos.GetIndex())
+                {
                     // The anchor position is exactly our insert position.
+                    #if 0
                     rEndIdx.Assign(&aTmpIdxReg, rEndIdx.GetIndex());
+                    #endif
+                    checkFmts.push_back( *pFmt );
+                }
             }
         }
+#endif
+        SwFrmFmtAnchorMap::const_iterator_pair range = GetDoc()->GetFrmFmtAnchorMap()->equal_range( SwNodeIndex( *this ));
+        for( SwFrmFmtAnchorMap::const_iterator it = range.first;
+             it != range.second;
+             ++it )
+        {
+            SwFrmFmt *pFmt = it->second;
+            const SwFmtAnchor& rAnchor = pFmt->GetAnchor();
+            const SwPosition* pCntntAnchor = rAnchor.GetCntntAnchor();
+            if (rAnchor.GetAnchorId() == FLY_AT_CHAR && pCntntAnchor)
+            {
+                // The fly is at-char anchored and has an anchor position.
+                SwIndex& rEndIdx = const_cast<SwIndex&>(pCntntAnchor->nContent);
+                if (&pCntntAnchor->nNode.GetNode() == this && rEndIdx.GetIndex() == rPos.GetIndex())
+                {
+                    // The anchor position is exactly our insert position.
+                    rEndIdx.Assign(&aTmpIdxReg, rEndIdx.GetIndex());
+#if OSL_DEBUG_LEVEL > 0
+                    std::list<SwFrmFmt*>::iterator checkPos = std::find( checkFmts.begin(), checkFmts.end(), pFmt );
+                    assert( checkPos != checkFmts.end());
+                    checkFmts.erase( checkPos );
+#endif
+                }
+            }
+        }
+#if OSL_DEBUG_LEVEL > 0
+        assert( checkFmts.empty());
+#endif
     }
 
     // base class

sw/source/core/attr/swatrset.cxx
@@ -27,6 +27,7 @@
 #include <editeng/lineitem.hxx>
 #include <editeng/boxitem.hxx>
 #include <editeng/editeng.hxx>
+#include <fmtanchr.hxx>
 #include <fmtpdsc.hxx>
 #include <hintids.hxx>
 #include <istyleaccess.hxx>
@@ -380,12 +381,15 @@ void SwAttrSet::CopyToModify( SwModify& rMod ) const
                 }
             }
 
+            boost::scoped_ptr< SfxItemSet > tmpSet;
+
             const SwPageDesc* pPgDesc;
             if( pSrcDoc != pDstDoc && SfxItemState::SET == GetItemState(
                                             RES_PAGEDESC, false, &pItem ) &&
                 0 != ( pPgDesc = ((SwFmtPageDesc*)pItem)->GetPageDesc()) )
             {
-                SfxItemSet aTmpSet( *this );
+                if( !tmpSet )
+                    tmpSet.reset( new SfxItemSet( *this ));
 
                 SwPageDesc* pDstPgDesc = pDstDoc->FindPageDesc(pPgDesc->GetName());
                 if( !pDstPgDesc )
@@ -395,20 +399,33 @@ void SwAttrSet::CopyToModify( SwModify& rMod ) const
                 }
                 SwFmtPageDesc aDesc( pDstPgDesc );
                 aDesc.SetNumOffset( ((SwFmtPageDesc*)pItem)->GetNumOffset() );
-                aTmpSet.Put( aDesc );
+                tmpSet->Put( aDesc );
+            }
 
+            if( pSrcDoc != pDstDoc && SfxItemState::SET == GetItemState( RES_ANCHOR, false, &pItem )
+                && static_cast< const SwFmtAnchor* >( pItem )->GetCntntAnchor() != NULL )
+            {
+                if( !tmpSet )
+                    tmpSet.reset( new SfxItemSet( *this ));
+                // Anchors at any node position cannot be copied to another document, because the SwPosition
+                // would still point to the old document. It needs to be fixed up explicitly.
+                tmpSet->ClearItem( RES_ANCHOR );
+            }
+
+            if( tmpSet )
+            {
                 if( pCNd )
                 {
                     // #i92811#
                     if ( pNewListIdItem != 0 )
                     {
-                        aTmpSet.Put( *pNewListIdItem );
+                        tmpSet->Put( *pNewListIdItem );
                     }
-                    pCNd->SetAttr( aTmpSet );
+                    pCNd->SetAttr( *tmpSet );
                 }
                 else
                 {
-                    pFmt->SetFmtAttr( aTmpSet );
+                    pFmt->SetFmtAttr( *tmpSet );
                 }
             }
             else if( pCNd )

sw/source/core/inc/flyfrm.hxx
@@ -57,7 +57,7 @@ bool CalcClipRect( const SdrObject *pSdrObj, SwRect &rRect, bool bMove = true );
 class SwFlyFrm : public SwLayoutFrm, public SwAnchoredObject
 {
     // is allowed to lock, implemented in frmtool.cxx
-    friend void AppendObjs   ( const SwFrmFmts *, sal_uLong, SwFrm *, SwPageFrm * );
+    friend void AppendObjs   ( const SwFrmFmts *, sal_uLong, SwFrm *, SwPageFrm *, SwDoc* );
     friend void Notify( SwFlyFrm *, SwPageFrm *pOld, const SwRect &rOld,
                         const SwRect* pOldPrt );
 

zhirshya/lo-tblr

commit af9af06c34d13630e6f164d1ed9e969d4c73fbcb

sw: fix assert with frames anchored in redlines on rhbz490395-1.odt
SwRangeRedline::Show() will move nodes around in the nodes-array, which
means that using SwNodeIndex as a key in a map that has a lifetime
not limited by the stack is a bad idea, as the map will become unsorted.

Remove SwFrmFmtAnchorMap from SwDoc and replace it with new
SwNode::m_pAnchoredFlys to do the same mapping.

(regression from 738fb2ad77e5a1a4d6e2dc540886a17f4527e4db)

Change-Id: I396d92b9d0b2045e98bad6d0b374303cd4e62b59

sw/inc/doc.hxx
@@ -103,7 +103,6 @@ class SwFmt;
 class SwFmtINetFmt;
 class SwFmtRefMark;
 class SwFrmFmt;
-class SwFrmFmtAnchorMap;
 class SwFrmFmts;
 class SwFtnIdxs;
 class SwFtnInfo;
@@ -300,7 +299,6 @@ class SW_DLLPUBLIC SwDoc :
     SwGrfFmtColl    *mpDfltGrfFmtColl;
 
     SwFrmFmts       *mpFrmFmtTbl;        //< Format table
-    SwFrmFmtAnchorMap *mpFrmFmtAnchorMap;
     SwCharFmts      *mpCharFmtTbl;
     SwFrmFmts       *mpSpzFrmFmtTbl;
     SwSectionFmts   *mpSectionFmtTbl;
@@ -807,9 +805,6 @@ public:
           SwFrmFmt *GetColumnContFmt()      { return mpColumnContFmt; }
     const SwCharFmt *GetDfltCharFmt() const { return mpDfltCharFmt;}
           SwCharFmt *GetDfltCharFmt()       { return mpDfltCharFmt;}
-
-    const SwFrmFmtAnchorMap* GetFrmFmtAnchorMap() const { return mpFrmFmtAnchorMap; }
-    SwFrmFmtAnchorMap* GetFrmFmtAnchorMap() { return mpFrmFmtAnchorMap; }
 
     // @return the interface of the management of (auto)styles
     IStyleAccess& GetIStyleAccess() { return *mpStyleAccess; }

sw/inc/frmfmt.hxx
@@ -23,8 +23,6 @@
 #include <cppuhelper/weakref.hxx>
 #include <tools/gen.hxx>
 #include <format.hxx>
-#include <map>
-#include <ndindex.hxx>
 #include "swdllapi.h"
 
 class SwFlyFrm;
@@ -303,27 +301,6 @@ public:
 };
 
 SW_DLLPUBLIC bool IsFlyFrmFmtInHeader(const SwFrmFmt& rFmt);
-
-/**
- Fast mapping from node positions to SwFrmFmt objects anchored at them.
-
- SwFrmFmt::GetAnchor().GetCntntAnchor() provides the position where the object is anchored.
- This class provides the reverse mapping. It intentionally uses SwNodeIndex instead of SwPosition
- to allow simpler implementation, do SwIndex checking explicitly if needed.
-*/
-class SwFrmFmtAnchorMap
-{
-public:
-    SwFrmFmtAnchorMap( const SwDoc* doc );
-    void Add( SwFrmFmt* fmt, const SwNodeIndex& index );
-    void Remove( SwFrmFmt* fmt, const SwNodeIndex& index );
-    typedef std::multimap< SwNodeIndex, SwFrmFmt* >::const_iterator const_iterator;
-    typedef std::pair< const_iterator, const_iterator > const_iterator_pair;
-    const_iterator_pair equal_range( const SwNodeIndex& pos ) const;
-private:
-    std::multimap< SwNodeIndex, SwFrmFmt* > items;
-    const SwDoc* doc;
-};
 
 #endif
 

sw/inc/node.hxx
@@ -94,6 +94,11 @@ class SW_DLLPUBLIC SwNode
     long m_nSerial;
 #endif
 
+    /// all SwFrmFmt that are anchored at the node
+    /// invariant: SwFrmFmt is in the list iff
+    /// SwFrmFmt::GetAnchor().GetCntntAnchor() points to this node
+    std::unique_ptr<std::vector<SwFrmFmt*>> m_pAnchoredFlys;
+
 protected:
     SwStartNode* pStartOfSection;
 
@@ -276,6 +281,10 @@ public:
     const SwTxtNode* FindOutlineNodeOfLevel( sal_uInt8 nLvl ) const;
 
     sal_uInt8 HasPrevNextLayNode() const;
+
+    std::vector<SwFrmFmt *> const* GetAnchoredFlys() const { return m_pAnchoredFlys.get(); }
+    void AddAnchoredFly(SwFrmFmt *);
+    void RemoveAnchoredFly(SwFrmFmt *);
 
     /**
      * Dumps the node structure to the given destination (file nodes.xml in the current directory by default)

sw/source/core/doc/doclay.cxx
@@ -1521,18 +1521,17 @@ bool SwDoc::IsInHeaderFooter( const SwNodeIndex& rIdx ) const
                 checkFmts.push_back( pFmt );
         }
 #endif
-        SwFrmFmtAnchorMap::const_iterator_pair range = GetFrmFmtAnchorMap()->equal_range( SwNodeIndex( *pFlyNd ));
-        SwFrmFmtAnchorMap::const_iterator it;
-        for( it = range.first;
-             it != range.second;
-             ++it )
+        std::vector<SwFrmFmt*> const*const pFlys(pFlyNd->GetAnchoredFlys());
+        bool bFound(false);
+        for (size_t i = 0; pFlys && i < pFlys->size(); ++i)
         {
-            const SwFrmFmt* pFmt = it->second;
+            const SwFrmFmt *const pFmt = (*pFlys)[i];
             const SwNodeIndex* pIdx = pFmt->GetCntnt().GetCntntIdx();
             if( pIdx && pFlyNd == &pIdx->GetNode() )
             {
 #if OSL_DEBUG_LEVEL > 0
-                std::list<const SwFrmFmt*>::iterator checkPos = std::find( checkFmts.begin(), checkFmts.end(), pFmt );
+                std::list<const SwFrmFmt*>::iterator checkPos = std::find(
+                        checkFmts.begin(), checkFmts.end(), pFmt );
                 assert( checkPos != checkFmts.end());
                 checkFmts.erase( checkPos );
 #endif
@@ -1545,12 +1544,13 @@ bool SwDoc::IsInHeaderFooter( const SwNodeIndex& rIdx ) const
 
                 pNd = &rAnchor.GetCntntAnchor()->nNode.GetNode();
                 pFlyNd = pNd->FindFlyStartNode();
+                bFound = true;
                 break;
             }
         }
-        if( it == range.second )
+        if (!bFound)
         {
-            OSL_ENSURE( mbInReading, "Found a FlySection but not a Format!" );
+            OSL_ENSURE(mbInReading, "Found a FlySection but not a Format!");
             return false;
         }
 #if OSL_DEBUG_LEVEL > 0

sw/source/core/doc/docnew.cxx
@@ -230,7 +230,6 @@ SwDoc::SwDoc()
     mpDfltTxtFmtColl( new SwTxtFmtColl( GetAttrPool(), sTxtCollStr ) ),
     mpDfltGrfFmtColl( new SwGrfFmtColl( GetAttrPool(), sGrfCollStr ) ),
     mpFrmFmtTbl( new SwFrmFmts() ),
-    mpFrmFmtAnchorMap( new SwFrmFmtAnchorMap( this ) ),
     mpCharFmtTbl( new SwCharFmts() ),
     mpSpzFrmFmtTbl( new SwFrmFmts() ),
     mpSectionFmtTbl( new SwSectionFmts() ),
@@ -577,7 +576,6 @@ SwDoc::~SwDoc()
     delete mpDfltCharFmt;
     delete mpDfltFrmFmt;
     delete mpLayoutCache;
-    delete mpFrmFmtAnchorMap;
 
     SfxItemPool::Free(mpAttrPool);
 }

sw/source/core/docnode/node.cxx
@@ -342,6 +342,7 @@ SwNode::SwNode( SwNodes& rNodes, sal_uLong nPos, const sal_uInt8 nNdType )
 
 SwNode::~SwNode()
 {
+    assert(!m_pAnchoredFlys || GetDoc()->IsInDtor()); // must all be deleted
 }
 
 /// Find the TableNode in which it is located.
@@ -2026,6 +2027,52 @@ bool SwNode::IsInRedlines() const
         bResult = pDoc->getIDocumentRedlineAccess().IsInRedlines(*this);
 
     return bResult;
+}
+
+void SwNode::AddAnchoredFly(SwFrmFmt *const pFlyFmt)
+{
+    assert(pFlyFmt);
+    assert(&pFlyFmt->GetAnchor(false).GetCntntAnchor()->nNode.GetNode() == this);
+    // check node type, cf. SwFmtAnchor::SetAnchor()
+    assert(IsTxtNode() || IsStartNode() || IsTableNode());
+    if (!m_pAnchoredFlys)
+    {
+        m_pAnchoredFlys.reset(new std::vector<SwFrmFmt*>);
+    }
+    m_pAnchoredFlys->push_back(pFlyFmt);
+}
+
+void SwNode::RemoveAnchoredFly(SwFrmFmt *const pFlyFmt)
+{
+    assert(pFlyFmt);
+    // cannot assert this in Remove because it is called when new anchor is already set
+//    assert(&pFlyFmt->GetAnchor(false).GetCntntAnchor()->nNode.GetNode() == this);
+    assert(IsTxtNode() || IsStartNode() || IsTableNode());
+    if (!m_pAnchoredFlys)
+    {
+        SwNodeIndex idx(GetNodes());
+        while (true)
+        {
+            SwNode & rNode(idx.GetNode());
+            if (rNode.m_pAnchoredFlys)
+            {
+                auto it(std::find(rNode.m_pAnchoredFlys->begin(), rNode.m_pAnchoredFlys->end(), pFlyFmt));
+                if (it != rNode.m_pAnchoredFlys->end())
+                {
+                    //XXX bug
+                }
+            }
+            ++idx;
+        }
+    }
+    assert(m_pAnchoredFlys);
+    auto it(std::find(m_pAnchoredFlys->begin(), m_pAnchoredFlys->end(), pFlyFmt));
+    assert(it != m_pAnchoredFlys->end());
+    m_pAnchoredFlys->erase(it);
+    if (m_pAnchoredFlys->empty())
+    {
+        m_pAnchoredFlys.reset();
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/layout/atrfrm.cxx
@@ -2532,9 +2532,11 @@ SwFrmFmt::~SwFrmFmt()
 {
     if( !GetDoc()->IsInDtor())
     {
-        const SwFmtAnchor& anchor = GetAnchor();
-        if( anchor.GetCntntAnchor() != NULL )
-            GetDoc()->GetFrmFmtAnchorMap()->Remove( this, anchor.GetCntntAnchor()->nNode );
+        const SwFmtAnchor& rAnchor = GetAnchor();
+        if (rAnchor.GetCntntAnchor() != nullptr)
+        {
+            rAnchor.GetCntntAnchor()->nNode.GetNode().RemoveAnchoredFly(this);
+        }
     }
 }
 
@@ -2627,9 +2629,13 @@ void SwFrmFmt::Modify( const SfxPoolItem* pOld, const SfxPoolItem* pNew )
     if( pOld && pOld->Which() == RES_ANCHOR )
         oldAnchorPosition = static_cast< const SwFmtAnchor* >( pOld )->GetCntntAnchor();
     if( oldAnchorPosition != NULL && ( newAnchorPosition == NULL || oldAnchorPosition->nNode.GetIndex() != newAnchorPosition->nNode.GetIndex()))
-        GetDoc()->GetFrmFmtAnchorMap()->Remove( this, oldAnchorPosition->nNode );
+    {
+        oldAnchorPosition->nNode.GetNode().RemoveAnchoredFly(this);
+    }
     if( newAnchorPosition != NULL && ( oldAnchorPosition == NULL || oldAnchorPosition->nNode.GetIndex() != newAnchorPosition->nNode.GetIndex()))
-        GetDoc()->GetFrmFmtAnchorMap()->Add( this, newAnchorPosition->nNode );
+    {
+        newAnchorPosition->nNode.GetNode().AddAnchoredFly(this);
+    }
 }
 
 void SwFrmFmt::RegisterToFormat( SwFmt& rFmt )
@@ -3479,41 +3485,6 @@ bool IsFlyFrmFmtInHeader(const SwFrmFmt& rFmt)
         }
     }
     return false;
-}
-
-
-SwFrmFmtAnchorMap::SwFrmFmtAnchorMap( const SwDoc* _doc )
-: doc( _doc )
-{
-}
-
-void SwFrmFmtAnchorMap::Add( SwFrmFmt* fmt, const SwNodeIndex& pos )
-{
-    (void) doc;
-    assert( pos.GetNode().GetDoc() == doc );
-    items.insert( std::make_pair( pos, fmt ));
-}
-
-void SwFrmFmtAnchorMap::Remove( SwFrmFmt* fmt, const SwNodeIndex& pos )
-{
-    (void) doc;
-    assert( pos.GetNode().GetDoc() == doc );
-    typedef std::multimap< SwNodeIndex, SwFrmFmt* >::iterator iterator;
-    std::pair< iterator, iterator > range = items.equal_range( pos );
-    for( iterator it = range.first; it != range.second; ++it )
-    {
-        if( it->second == fmt )
-        {
-            items.erase( it );
-            return;
-        }
-    }
-    assert( false );
-}
-
-SwFrmFmtAnchorMap::const_iterator_pair SwFrmFmtAnchorMap::equal_range( const SwNodeIndex& pos ) const
-{
-    return items.equal_range( pos );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/layout/frmtool.cxx
@@ -1019,12 +1019,11 @@ void AppendObjs( const SwFrmFmts *pTbl, sal_uLong nIndex,
 #else
     (void)pTbl;
 #endif
-    SwFrmFmtAnchorMap::const_iterator_pair range = doc->GetFrmFmtAnchorMap()->equal_range( SwNodeIndex( doc->GetNodes(), nIndex ));
-    for( std::multimap< SwNodeIndex, SwFrmFmt* >::const_iterator it = range.first;
-         it != range.second;
-         )
+    SwNode const& rNode(*doc->GetNodes()[nIndex]);
+    std::vector<SwFrmFmt*> const*const pFlys(rNode.GetAnchoredFlys());
+    for (size_t it = 0; pFlys && it != pFlys->size(); )
     {
-        SwFrmFmt *pFmt = it->second;
+        SwFrmFmt *const pFmt = (*pFlys)[it];
         const SwFmtAnchor &rAnch = pFmt->GetAnchor();
         if ( rAnch.GetCntntAnchor() &&
              (rAnch.GetCntntAnchor()->nNode.GetIndex() == nIndex) )

sw/source/core/txtnode/ndtxt.cxx
@@ -1133,12 +1133,10 @@ void SwTxtNode::Update(
             }
         }
 #endif
-        SwFrmFmtAnchorMap::const_iterator_pair range = GetDoc()->GetFrmFmtAnchorMap()->equal_range( SwNodeIndex( *this ));
-        for( SwFrmFmtAnchorMap::const_iterator it = range.first;
-             it != range.second;
-             ++it )
+        std::vector<SwFrmFmt*> const*const pFlys(GetAnchoredFlys());
+        for (size_t i = 0; pFlys && i != pFlys->size(); ++i)
         {
-            SwFrmFmt *pFmt = it->second;
+            SwFrmFmt const*const pFmt = (*pFlys)[i];
             const SwFmtAnchor& rAnchor = pFmt->GetAnchor();
             const SwPosition* pCntntAnchor = rAnchor.GetCntntAnchor();
             if (rAnchor.GetAnchorId() == FLY_AT_CHAR && pCntntAnchor)