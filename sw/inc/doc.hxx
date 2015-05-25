zzz99977/core

introduce CreateUnoCrsr2 to migrate to sane memory mngmt
Change-Id: Ia530827daa354555983c3c28c5aeec38bc3dbf11

sw/inc/doc.hxx
@@ -337,6 +337,7 @@ class SW_DLLPUBLIC SwDoc :
     mutable std::unordered_map<OUString, SwNumRule *, OUStringHash> maNumRuleMap;
 
     SwUnoCrsrTbl    *mpUnoCrsrTbl;
+    std::list< std::weak_ptr<SwUnoCrsr> > mvUnoCrsrTbl2;
 
     SwPagePreviewPrtData *mpPgPViewPrtData;  //< Indenting / spacing for printing of page view.
     SwPaM           *mpExtInputRing;
@@ -1501,6 +1502,7 @@ public:
     // Uno - Interfaces
     const SwUnoCrsrTbl& GetUnoCrsrTbl() const       { return *mpUnoCrsrTbl; }
     SwUnoCrsr* CreateUnoCrsr( const SwPosition& rPos, bool bTblCrsr = false );
+    std::shared_ptr<SwUnoCrsr> CreateUnoCrsr2( const SwPosition& rPos, bool bTblCrsr = false );
 
     // FeShell - Interfaces
     // !!! These assume always an existing layout !!!

zhirshya/lo-tblr

sw/inc/doc.hxx
@@ -334,6 +334,7 @@ class SW_DLLPUBLIC SwDoc :
     mutable std::unordered_map<OUString, SwNumRule *, OUStringHash> maNumRuleMap;
 
     SwUnoCrsrTable    *mpUnoCrsrTable;
+    std::list< std::weak_ptr<SwUnoCrsr> > mvUnoCrsrTbl2;
 
     SwPagePreviewPrtData *mpPgPViewPrtData;  //< Indenting / spacing for printing of page view.
     SwPaM           *mpExtInputRing;
@@ -1495,6 +1496,7 @@ public:
     // Uno - Interfaces
     const SwUnoCrsrTable& GetUnoCrsrTable() const       { return *mpUnoCrsrTable; }
     SwUnoCrsr* CreateUnoCrsr( const SwPosition& rPos, bool bTableCrsr = false );
+    std::shared_ptr<SwUnoCrsr> CreateUnoCrsr2( const SwPosition& rPos, bool bTableCrsr = false );
 
     // FeShell - Interfaces
     // !!! These assume always an existing layout !!!