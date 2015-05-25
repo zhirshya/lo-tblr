zzz99977/core

use new unocrsrs in SwXParagraphEnumeration
Change-Id: Ia57a04e617b1d7301d098524ebb55acd1d0e60b3

sw/source/core/unocore/unoframe.cxx
@@ -3258,17 +3258,13 @@ uno::Reference< text::XTextCursor >  SwXTextFrame::createTextCursorByRange(const
 uno::Reference< container::XEnumeration >  SwXTextFrame::createEnumeration(void) throw( uno::RuntimeException, std::exception )
 {
     SolarMutexGuard aGuard;
-    uno::Reference< container::XEnumeration >  aRef;
     SwFrmFmt* pFmt = GetFrmFmt();
-    if(pFmt)
-    {
-        SwPosition aPos(pFmt->GetCntnt().GetCntntIdx()->GetNode());
-        ::std::unique_ptr<SwUnoCrsr> pUnoCursor(
-                GetDoc()->CreateUnoCrsr(aPos, false));
-        pUnoCursor->Move(fnMoveForward, fnGoNode);
-        aRef = new SwXParagraphEnumeration(this, std::move(pUnoCursor), CURSOR_FRAME);
-    }
-    return aRef;
+    if(!pFmt)
+        return nullptr;
+    SwPosition aPos(pFmt->GetCntnt().GetCntntIdx()->GetNode());
+    auto pUnoCursor(GetDoc()->CreateUnoCrsr2(aPos, false));
+    pUnoCursor->Move(fnMoveForward, fnGoNode);
+    return new SwXParagraphEnumeration(this, pUnoCursor, CURSOR_FRAME);
 }
 
 uno::Type  SwXTextFrame::getElementType(void) throw( uno::RuntimeException, std::exception )

zhirshya/lo-tblr

sw/source/core/unocore/unoframe.cxx
@@ -3285,17 +3285,13 @@ uno::Reference< text::XTextCursor >  SwXTextFrame::createTextCursorByRange(const
 uno::Reference< container::XEnumeration >  SwXTextFrame::createEnumeration() throw( uno::RuntimeException, std::exception )
 {
     SolarMutexGuard aGuard;
-    uno::Reference< container::XEnumeration >  aRef;
     SwFrameFormat* pFormat = GetFrameFormat();
-    if(pFormat)
-    {
-        SwPosition aPos(pFormat->GetContent().GetContentIdx()->GetNode());
-        ::std::unique_ptr<SwUnoCrsr> pUnoCursor(
-                GetDoc()->CreateUnoCrsr(aPos, false));
-        pUnoCursor->Move(fnMoveForward, fnGoNode);
-        aRef = new SwXParagraphEnumeration(this, std::move(pUnoCursor), CURSOR_FRAME);
-    }
-    return aRef;
+    if(!pFormat)
+        return nullptr;
+    SwPosition aPos(pFormat->GetContent().GetContentIdx()->GetNode());
+    auto pUnoCursor(GetDoc()->CreateUnoCrsr2(aPos, false));
+    pUnoCursor->Move(fnMoveForward, fnGoNode);
+    return new SwXParagraphEnumeration(this, pUnoCursor, CURSOR_FRAME);
 }
 
 uno::Type  SwXTextFrame::getElementType() throw( uno::RuntimeException, std::exception )