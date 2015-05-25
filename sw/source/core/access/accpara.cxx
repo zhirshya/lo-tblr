zzz99977/core

use new unocrsrs in access
Change-Id: Ice2bb4fae03efba1f516859c2f129c3442f3b85f

sw/source/core/access/accpara.cxx
@@ -640,15 +640,14 @@ SwXTextPortion* SwAccessibleParagraph::CreateUnoPortion(
     SwTxtNode* pTxtNode = const_cast<SwTxtNode*>( GetTxtNode() );
     SwIndex aIndex( pTxtNode, nStart );
     SwPosition aStartPos( *pTxtNode, aIndex );
-    SwUnoCrsr* pUnoCursor = pTxtNode->GetDoc()->CreateUnoCrsr( aStartPos );
+    auto pUnoCursor(pTxtNode->GetDoc()->CreateUnoCrsr2( aStartPos ));
     pUnoCursor->SetMark();
     pUnoCursor->GetMark()->nContent = nEnd;
 
     // create a (dummy) text portion to be returned
     uno::Reference<text::XText> aEmpty;
     SwXTextPortion* pPortion =
-        new SwXTextPortion ( pUnoCursor, aEmpty, PORTION_TEXT);
-    delete pUnoCursor;
+        new SwXTextPortion ( pUnoCursor.get(), aEmpty, PORTION_TEXT);
 
     return pPortion;
 }

zhirshya/lo-tblr

sw/source/core/access/accpara.cxx
@@ -640,15 +640,14 @@ SwXTextPortion* SwAccessibleParagraph::CreateUnoPortion(
     SwTextNode* pTextNode = const_cast<SwTextNode*>( GetTextNode() );
     SwIndex aIndex( pTextNode, nStart );
     SwPosition aStartPos( *pTextNode, aIndex );
-    SwUnoCrsr* pUnoCursor = pTextNode->GetDoc()->CreateUnoCrsr( aStartPos );
+    auto pUnoCursor(pTextNode->GetDoc()->CreateUnoCrsr2( aStartPos ));
     pUnoCursor->SetMark();
     pUnoCursor->GetMark()->nContent = nEnd;
 
     // create a (dummy) text portion to be returned
     uno::Reference<text::XText> aEmpty;
     SwXTextPortion* pPortion =
-        new SwXTextPortion ( pUnoCursor, aEmpty, PORTION_TEXT);
-    delete pUnoCursor;
+        new SwXTextPortion ( pUnoCursor.get(), aEmpty, PORTION_TEXT);
 
     return pPortion;
 }