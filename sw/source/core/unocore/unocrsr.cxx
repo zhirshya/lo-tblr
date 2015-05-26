LibreOffice/core

commit 692c886f937c525d6bfcb541917a5114b085efa9

remove the weak_ptrs on destruction too
Change-Id: I65f5867c41417539a70eef15754988d9931394a4

sw/source/core/unocore/unocrsr.cxx
@@ -23,6 +23,7 @@
 #include <swtable.hxx>
 #include <docary.hxx>
 #include <rootfrm.hxx>
+#include <calbck.hxx>
 
 IMPL_FIXEDMEMPOOL_NEWDEL( SwUnoCrsr )
 
@@ -39,7 +40,13 @@ SwUnoCrsr::~SwUnoCrsr()
     SwDoc* pDoc = GetDoc();
     if( !pDoc->IsInDtor() )
     {
-        //assert(!SwIterator<SwClient,SwUnoCrsr>(this).First());
+#ifdef DBG_UTIL
+        SwIterator<SwClient, SwUnoCrsr> pClient(*this);
+        assert(!pClient.First());
+#endif
+        // remove the weak_ptr the document keeps to notify about document death
+        pDoc->mvUnoCrsrTbl.remove_if(
+            [this](const std::weak_ptr<SwUnoCrsr>& pWeakPtr) -> bool { return pWeakPtr.lock().get() == this; });
     }
 
     // delete the whole ring

zhirshya/lo-tblr

commit 3c53d3528812fe1ee60f332f29c7c9373f42a163

Silence failing assert for now
...recently introduced with 692c886f937c525d6bfcb541917a5114b085efa9 "remove the
weak_ptrs on destruction too," but causing lots of CppunitTests to fail.

Change-Id: I9a958531473ba338499bac37282d934a51801e12

sw/source/core/unocore/unocrsr.cxx
@@ -40,7 +40,7 @@ SwUnoCrsr::~SwUnoCrsr()
     SwDoc* pDoc = GetDoc();
     if( !pDoc->IsInDtor() )
     {
-#ifdef DBG_UTIL
+#ifdef DBG_UTIL_TODO
         SwIterator<SwClient, SwUnoCrsr> pClient(*this);
         assert(!pClient.First());
 #endif