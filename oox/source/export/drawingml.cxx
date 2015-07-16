LibreOffice/core

commit ccfc02f7691848284403d339ee17054169fbff1d

fdo#77476:FILE_SAVE : 'TIF' is changed to 'TIFF' in RT
1. Open the attached file in LO which has images [.TIF] format
2. Save the document as DOCX
3. Open the saved document in MS-Office or extract the content of the saved docx file.
 In [Content_Types].xml , .tif extension is changed to .tiff.

Change-Id: I73baea9ad6cd991b04d47e23865b77432127a510
Reviewed-on: https://gerrit.libreoffice.org/9010
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

oox/source/export/drawingml.cxx
@@ -749,8 +749,8 @@ OUString DrawingML::WriteImage( const Graphic& rGraphic , bool bRelPathToMedia )
             pExtension = ".png";
             break;
         case GFX_LINK_TYPE_NATIVE_TIF:
-            sMediaType = "image/tiff";
-            pExtension = ".tiff";
+            sMediaType = "image/tif";
+            pExtension = ".tif";
             break;
         case GFX_LINK_TYPE_NATIVE_WMF:
             sMediaType = "image/x-wmf";

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -3130,6 +3130,17 @@ DECLARE_OOXMLEXPORT_TEST(testFDO76597, "fdo76597.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[4]/w:pPr/w:spacing", "before", "96");
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[4]/w:pPr/w:spacing", "after", "120");
 }
+
+DECLARE_OOXMLEXPORT_TEST(testContentTypeTIF, "fdo77476.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("[Content_Types].xml");
+
+    if (!pXmlDoc)
+       return;
+
+    assertXPath(pXmlDoc, "/ContentType:Types/ContentType:Override[@ContentType='image/tif']", "PartName", "/word/media/image1.tif");
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

zhirshya/lo-tblr

commit 21a97e5db8af115b8ef58c763b666f4b412f77e4

oox: fix invalid mime type "image/tif"
(regression from ccfc02f7691848284403d339ee17054169fbff1d)

Change-Id: I29a366228bf08f29edfa4a8e963889e34aa3d2ea

oox/source/export/drawingml.cxx
@@ -849,7 +849,7 @@ OUString DrawingML::WriteImage( const Graphic& rGraphic , bool bRelPathToMedia )
             pExtension = ".png";
             break;
         case GFX_LINK_TYPE_NATIVE_TIF:
-            sMediaType = "image/tif";
+            sMediaType = "image/tiff";
             pExtension = ".tif";
             break;
         case GFX_LINK_TYPE_NATIVE_WMF:

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -209,7 +209,7 @@ DECLARE_OOXMLEXPORT_TEST(testContentTypeTIF, "fdo77476.docx")
     if (!pXmlDoc)
        return;
 
-    assertXPath(pXmlDoc, "/ContentType:Types/ContentType:Override[@ContentType='image/tif']", "PartName", "/word/media/image1.tif");
+    assertXPath(pXmlDoc, "/ContentType:Types/ContentType:Override[@ContentType='image/tiff']", "PartName", "/word/media/image1.tif");
 }
 
 DECLARE_OOXMLEXPORT_TEST(testFDO77117, "fdo77117.docx")