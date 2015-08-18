LibreOffice/core

commit c9175a1bd3249ad573ae6827bf19963a3ebe2fbc

SwViewShell::ImplEndAction: avoid direct PaintDesktop()
With double-buffering enabled and typing quickly, one saw blinks in the
text sometimes. The reason was that PaintDesktop() painted directly
outside SwViewShell::Paint().

The problem is more visible with VCL_DOUBLEBUFFERING_AVOID_PAINT=1,
loading a simple document + pressing a key draw an unexpected white
rectangle.

Given that InvalidateWindows() already calls PaintDesktop() inside
Paint() implicitly (via SwViewShell::Paint()), fix the problem by just
avoiding the DLPrePaint2() + PaintDesktop() + DLPostPaint2() calls.

Change-Id: Ib8ce26a0bdd526bf85d362f0e865dd61d29f6d11

sw/source/core/view/viewsh.cxx
@@ -405,20 +405,7 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
                     }
                     if ( bPaint )
                     {
-                        // #i75172# begin DrawingLayer paint
-                        // need to do begin/end DrawingLayer preparation for each single rectangle of the
-                        // repaint region. I already tried to prepare only once for the whole Region. This
-                        // seems to work (and does technically) but fails with transparent objects. Since the
-                        // region given to BeginDarwLayers() defines the clip region for DrawingLayer paint,
-                        // transparent objects in the single rectangles will indeed be painted multiple times.
-                        DLPrePaint2(vcl::Region(aRect.SVRect()));
-
-                        if ( bPaintsFromSystem )
-                            PaintDesktop(*GetOut(), aRect);
-                        pCurrentLayout->GetCurrShell()->InvalidateWindows(aRect.SVRect());
-
-                        // #i75172# end DrawingLayer paint
-                        DLPostPaint2(true);
+                        InvalidateWindows(aRect.SVRect());
                     }
 
                     lcl_PaintTransparentFormControls(*this, aRect); // i#107365

zhirshya/lo-tblr

commit 16009a6f902d2a810bc2bec44b99e17e55c5c899

tdf#93096 sw: fix selection with keyboard outside current view
Regression from commit c9175a1bd3249ad573ae6827bf19963a3ebe2fbc
(SwViewShell::ImplEndAction: avoid direct PaintDesktop(), 2015-07-03),
the problem is that while going via InvalidateWindows() is fine for the
double-buffering case, it has side effects when painting directly, so
revert back to the old code in that case.

Change-Id: Ib1e3b143f5cfe2c6ab8b102a1a2064900282f136

sw/source/core/view/viewsh.cxx
@@ -405,7 +405,25 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
                     }
                     if ( bPaint )
                     {
-                        InvalidateWindows(aRect.SVRect());
+                        if (GetWin() && GetWin()->SupportsDoubleBuffering())
+                            InvalidateWindows(aRect.SVRect());
+                        else
+                        {
+                            // #i75172# begin DrawingLayer paint
+                            // need to do begin/end DrawingLayer preparation for each single rectangle of the
+                            // repaint region. I already tried to prepare only once for the whole Region. This
+                            // seems to work (and does technically) but fails with transparent objects. Since the
+                            // region given to BeginDarwLayers() defines the clip region for DrawingLayer paint,
+                            // transparent objects in the single rectangles will indeed be painted multiple times.
+                            DLPrePaint2(vcl::Region(aRect.SVRect()));
+
+                            if ( bPaintsFromSystem )
+                                PaintDesktop(*GetOut(), aRect);
+                            pCurrentLayout->GetCurrShell()->InvalidateWindows(aRect.SVRect());
+
+                            // #i75172# end DrawingLayer paint
+                            DLPostPaint2(true);
+                        }
                     }
                     else
                         lcl_PaintTransparentFormControls(*this, aRect); // i#107365