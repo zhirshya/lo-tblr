LibreOffice/core

commit 765f6211b1371c1e40de386e715de6b3d1a8df86

utl::ConfigItem::Commit() should call ClearModified()
Rename the virtual function, and add a new non-virtual Commit() to do
that.

Change-Id: I09421df781ba965d6ff638b46cd8214fb3a00022

svtools/source/config/menuoptions.cxx
@@ -107,17 +107,6 @@ class SvtMenuOptions_Impl : public ConfigItem
 
         virtual void Notify( const Sequence< OUString >& seqPropertyNames ) SAL_OVERRIDE;
 
-        /*-****************************************************************************************************
-            @short      write changes to configuration
-            @descr      These method writes the changed values into the sub tree
-                        and should always called in our destructor to guarantee consistency of config data.
-
-            @seealso    baseclass ConfigItem
-        *//*-*****************************************************************************************************/
-
-        virtual void Commit() SAL_OVERRIDE;
-
-
         //  public interface
 
 
@@ -147,6 +136,8 @@ class SvtMenuOptions_Impl : public ConfigItem
 
 
     private:
+
+        virtual void ImplCommit() SAL_OVERRIDE;
 
         /*-****************************************************************************************************
             @short      return list of fix key names of our configuration management which represent our module tree
@@ -306,7 +297,7 @@ void SvtMenuOptions_Impl::Notify( const Sequence< OUString >& seqPropertyNames )
 
 //  public method
 
-void SvtMenuOptions_Impl::Commit()
+void SvtMenuOptions_Impl::ImplCommit()
 {
     // Get names of supported properties, create a list for values and copy current values to it.
     Sequence< OUString >    seqNames    = impl_GetPropertyNames();

zhirshya/lo-tblr

commit 2dbb03ead621a2dbd2825e9c45b2f048749c7aa5

tdf#93451: svtools: don't commit SvtMenuOptions too early
Surprisingly SvtMenuOptions_Impl::ImplCommit() does not actually write
m_eMenuIcons for "ShowIconsInMenues" but something read out of
Application::Settings(), which appears to be deliberate (i#95318).
Whatever sets that does it after SetMenuIconsState() is called,
so keep the SvtMenuOptions modified by not calling Commit().

(regression from 765f6211b1371c1e40de386e715de6b3d1a8df86)

Change-Id: I368b65589d16753d80a1b9e9cc7d7df59c0f53da

svtools/source/config/menuoptions.cxx
@@ -128,7 +128,7 @@ class SvtMenuOptions_Impl : public ConfigItem
                         SetModified();
                         for ( ::std::list<Link<>>::const_iterator iter = aList.begin(); iter != aList.end(); ++iter )
                             iter->Call( this );
-                        Commit();
+                        // tdf#93451: don't Commit() here, it's too early
                     }
 
 