LibreOffice/core

commit a50cd18768289c65debeed5ec507cf37095365b8

make Control::ImplCallEventListenersAndHandler take a lambda
so that we make the Link<> calls type-safe, without having to make this
a template method

Change-Id: I2e36bd6aa7c63440f72d266b593e101965b5ebce

vcl/source/control/ctrl.cxx
@@ -258,7 +258,7 @@ bool Control::Notify( NotifyEvent& rNEvt )
             {
                 mbHasControlFocus = true;
                 CompatStateChanged( StateChangedType::ControlFocus );
-                if ( ImplCallEventListenersAndHandler( VCLEVENT_CONTROL_GETFOCUS, maGetFocusHdl, this ) )
+                if ( ImplCallEventListenersAndHandler( VCLEVENT_CONTROL_GETFOCUS, [this] () { maGetFocusHdl.Call(this); } ) )
                     // been destroyed within the handler
                     return true;
             }
@@ -272,7 +272,7 @@ bool Control::Notify( NotifyEvent& rNEvt )
                 {
                     mbHasControlFocus = false;
                     CompatStateChanged( StateChangedType::ControlFocus );
-                    if ( ImplCallEventListenersAndHandler( VCLEVENT_CONTROL_LOSEFOCUS, maLoseFocusHdl, this ) )
+                    if ( ImplCallEventListenersAndHandler( VCLEVENT_CONTROL_LOSEFOCUS, [this] () { maLoseFocusHdl.Call(this); } ) )
                         // been destroyed within the handler
                         return true;
                 }
@@ -320,7 +320,7 @@ void Control::AppendLayoutData( const Control& rSubControl ) const
     }
 }
 
-bool Control::ImplCallEventListenersAndHandler( sal_uLong nEvent, const Link<>& rHandler, void* pCaller )
+bool Control::ImplCallEventListenersAndHandler( sal_uLong nEvent, std::function<void()> callHandler )
 {
     VclPtr<Control> xThis(this);
 
@@ -328,7 +328,7 @@ bool Control::ImplCallEventListenersAndHandler( sal_uLong nEvent, const Link<>& 
 
     if ( !xThis->IsDisposed() )
     {
-        rHandler.Call( pCaller );
+        callHandler();
 
         if ( !xThis->IsDisposed() )
             return false;

include/vcl/ctrl.hxx
@@ -69,15 +69,13 @@ protected:
 
         @param nEvent
             the event to notify to our event listeners
-        @param rHandler
-            the handler to call
-        @param pCaller
-            the parameter to pass to the handler call
+        @param callHandler
+            the lambda function that calls the handler
         @return
             if the Control instance has been destroyed in any of the call
     */
     bool        ImplCallEventListenersAndHandler(
-                    sal_uLong nEvent, const Link<>& rHandler, void* pCaller
+                    sal_uLong nEvent, std::function<void()> callHandler
                 );
 
     /** draws the given text onto the given device

sc/source/ui/condformat/condformatdlgentry.cxx
@@ -78,7 +78,7 @@ bool ScCondFrmtEntry::Notify( NotifyEvent& rNEvt )
 {
     if( rNEvt.GetType() == MouseNotifyEvent::MOUSEBUTTONDOWN )
     {
-        ImplCallEventListenersAndHandler( VCLEVENT_WINDOW_MOUSEBUTTONDOWN, maClickHdl, this );
+        ImplCallEventListenersAndHandler( VCLEVENT_WINDOW_MOUSEBUTTONDOWN, [this] () { maClickHdl.Call(this); } );
     }
     return Control::Notify(rNEvt);
 }

svtools/source/table/tablecontrol.cxx
@@ -652,7 +652,7 @@ namespace svt { namespace table
 
     void TableControl::Select()
     {
-        ImplCallEventListenersAndHandler( VCLEVENT_TABLEROW_SELECT, m_pImpl->getSelectHandler(), this );
+        ImplCallEventListenersAndHandler( VCLEVENT_TABLEROW_SELECT, [this] () { m_pImpl->getSelectHandler().Call(this); } );
 
         if ( m_pImpl->isAccessibleAlive() )
         {

vcl/source/control/button.cxx
@@ -114,7 +114,7 @@ void Button::SetCommandHandler(const OUString& aCommand)
 
 void Button::Click()
 {
-    ImplCallEventListenersAndHandler( VCLEVENT_BUTTON_CLICK, maClickHdl, this );
+    ImplCallEventListenersAndHandler( VCLEVENT_BUTTON_CLICK, [this] () { maClickHdl.Call(this); } );
 }
 
 OUString Button::GetStandardText( StandardButtonType eButton )
@@ -1548,7 +1548,7 @@ bool PushButton::PreNotify( NotifyEvent& rNEvt )
 
 void PushButton::Toggle()
 {
-    ImplCallEventListenersAndHandler( VCLEVENT_PUSHBUTTON_TOGGLE, maToggleHdl, this );
+    ImplCallEventListenersAndHandler( VCLEVENT_PUSHBUTTON_TOGGLE, [this] () { maToggleHdl.Call(this); } );
 }
 
 void PushButton::SetSymbol( SymbolType eSymbol )
@@ -2640,7 +2640,7 @@ bool RadioButton::PreNotify( NotifyEvent& rNEvt )
 
 void RadioButton::Toggle()
 {
-    ImplCallEventListenersAndHandler( VCLEVENT_RADIOBUTTON_TOGGLE, maToggleHdl, this );
+    ImplCallEventListenersAndHandler( VCLEVENT_RADIOBUTTON_TOGGLE, [this] () { maToggleHdl.Call(this); } );
 }
 
 bool RadioButton::SetModeRadioImage( const Image& rImage )
@@ -3595,7 +3595,7 @@ bool CheckBox::PreNotify( NotifyEvent& rNEvt )
 
 void CheckBox::Toggle()
 {
-    ImplCallEventListenersAndHandler( VCLEVENT_CHECKBOX_TOGGLE, maToggleHdl, this );
+    ImplCallEventListenersAndHandler( VCLEVENT_CHECKBOX_TOGGLE, [this] () { maToggleHdl.Call(this); } );
 }
 
 void CheckBox::SetState( TriState eState )

vcl/source/control/combobox.cxx
@@ -556,12 +556,12 @@ void ComboBox::ToggleDropDown()
 
 void ComboBox::Select()
 {
-    ImplCallEventListenersAndHandler( VCLEVENT_COMBOBOX_SELECT, m_pImpl->m_SelectHdl, this );
+    ImplCallEventListenersAndHandler( VCLEVENT_COMBOBOX_SELECT, [this] () { m_pImpl->m_SelectHdl.Call(this); } );
 }
 
 void ComboBox::DoubleClick()
 {
-    ImplCallEventListenersAndHandler( VCLEVENT_COMBOBOX_DOUBLECLICK, m_pImpl->m_DoubleClickHdl, this );
+    ImplCallEventListenersAndHandler( VCLEVENT_COMBOBOX_DOUBLECLICK, [this] () { m_pImpl->m_DoubleClickHdl.Call(this); } );
 }
 
 bool ComboBox::IsAutoSizeEnabled() const { return m_pImpl->m_isDDAutoSize; }

vcl/source/control/edit.cxx
@@ -2416,7 +2416,7 @@ void Edit::Modify()
         if ( mpUpdateDataTimer )
             mpUpdateDataTimer->Start();
 
-        if ( ImplCallEventListenersAndHandler( VCLEVENT_EDIT_MODIFY, maModifyHdl, this ) )
+        if ( ImplCallEventListenersAndHandler( VCLEVENT_EDIT_MODIFY, [this] () { maModifyHdl.Call(this); } ) )
             // have been destroyed while calling into the handlers
             return;
 

vcl/source/control/fixedhyper.cxx
@@ -77,7 +77,7 @@ void FixedHyperlink::MouseButtonUp( const MouseEvent& )
 {
     // calls the link if the control is enabled and the mouse is over the text.
     if ( IsEnabled() && ImplIsOverText(GetPointerPosPixel()) )
-        ImplCallEventListenersAndHandler( VCLEVENT_BUTTON_CLICK, m_aClickHdl, this );
+        ImplCallEventListenersAndHandler( VCLEVENT_BUTTON_CLICK, [this] () { m_aClickHdl.Call(this); } );
 }
 
 void FixedHyperlink::RequestHelp( const HelpEvent& rHEvt )

zhirshya/lo-tblr

commit 41448082aa486e27d0b679fb34528a70f1a1a3db

Calling empty std::function behaves different than calling empty Link
...regression introduced with a50cd18768289c65debeed5ec507cf37095365b8 "make
Control::ImplCallEventListenersAndHandler take a lambda"

Change-Id: I62e66d71cd36e55d68cf15c70dc9e06214f7e312

vcl/source/control/ctrl.cxx
@@ -328,7 +328,10 @@ bool Control::ImplCallEventListenersAndHandler( sal_uLong nEvent, std::function<
 
     if ( !xThis->IsDisposed() )
     {
-        callHandler();
+        if (callHandler)
+        {
+            callHandler();
+        }
 
         if ( !xThis->IsDisposed() )
             return false;