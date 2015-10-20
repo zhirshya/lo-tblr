LibreOffice/core

commit c72fac269626cd4c2c3f62a6eed69e47b59256a4

vclwidget: convert lots more sites to use VclPtr
seems there was a lot of shared_ptr usage floating around...

Change-Id: Icd05243170eb8493709275fc36bf986fc194b781

sd/source/ui/annotations/annotationtag.hxx
@@ -79,7 +79,7 @@ protected:
 private:
     AnnotationManagerImpl& mrManager;
     css::uno::Reference< css::office::XAnnotation > mxAnnotation;
-    std::unique_ptr<AnnotationWindow>               mpAnnotationWindow;
+    VclPtr<AnnotationWindow>                        mpAnnotationWindow;
     Color                                           maColor;
     int                                             mnIndex;
     const ::vcl::Font&                              mrFont;

zhirshya/lo-tblr

commit 097173bcc9f3b9a44249c533ac30c8efceae28c5

afl-eventtesting + VclPtr: missing dispose on impress annotation window
regression from

commit c72fac269626cd4c2c3f62a6eed69e47b59256a4
Date:   Mon Mar 16 12:17:44 2015 +0200

    vclwidget: convert lots more sites to use VclPtr

Change-Id: Iac6a83003d9c395589acdc23fe511d2bb47e91ee

sd/source/ui/annotations/annotationtag.cxx
@@ -566,9 +566,9 @@ void AnnotationTag::OpenPopup( bool bEdit )
 
     if( !mpAnnotationWindow.get() )
     {
-           vcl::Window* pWindow = dynamic_cast< vcl::Window* >( getView().GetFirstOutputDevice() );
-           if( pWindow )
-           {
+        vcl::Window* pWindow = dynamic_cast< vcl::Window* >( getView().GetFirstOutputDevice() );
+        if( pWindow )
+        {
             RealPoint2D aPosition( mxAnnotation->getPosition() );
             Point aPos( pWindow->OutputToScreenPixel( pWindow->LogicToPixel( Point( static_cast<long>(aPosition.X * 100.0), static_cast<long>(aPosition.Y * 100.0) ) ) ) );
 
@@ -604,7 +604,7 @@ void AnnotationTag::ClosePopup()
     {
         mpAnnotationWindow->RemoveEventListener( LINK(this, AnnotationTag, WindowEventHandler));
         mpAnnotationWindow->Deactivate();
-        mpAnnotationWindow.reset();
+        mpAnnotationWindow.disposeAndClear();
     }
 }
 

sd/source/ui/annotations/annotationtag.hxx
@@ -82,7 +82,7 @@ private:
     VclPtr<AnnotationWindow>                        mpAnnotationWindow;
     Color                                           maColor;
     int                                             mnIndex;
-    const vcl::Font&                              mrFont;
+    const vcl::Font&                                mrFont;
     Size                                            maSize;
     ImplSVEvent *                                   mnClosePopupEvent;
     VclPtr<vcl::Window>                             mpListenWindow;