LibreOffice/core

commit 53f16c39f5c4c69ab603bf9098daec71f7a0acb8

remove unused typedefs and inline use-once typedefs
and improve the script a little

Change-Id: I2792ea4dd5df3a50736fbe209225c3f16fb86b84
Reviewed-on: https://gerrit.libreoffice.org/20033
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

bridges/source/cpp_uno/msvc_win32_x86-64/mscx.hxx
@@ -36,7 +36,6 @@ namespace CPPU_CURRENT_NAMESPACE
 const DWORD MSVC_ExceptionCode = 0xe06d7363;
 const long MSVC_magic_number = 0x19930520L;
 
-typedef enum { REGPARAM_INT, REGPARAM_FLT } RegParamKind;
 
 type_info * mscx_getRTTI( OUString const & rUNOname );
 

basic/inc/sbxbase.hxx
@@ -33,15 +33,14 @@ class SbxFactory;
 class SbxVariable;
 class SbxBasicFormater;
 
-typedef std::vector<std::unique_ptr<SbxFactory>> SbxFactories;
-
 // AppData structure for SBX:
 struct SbxAppData
     : private ::boost::noncopyable
 {
     SbxError            eSbxError;  // Error code
-    SbxFactories        m_Factories;
-    SbxBasicFormater    *pBasicFormater;    // Pointer to Format()-Command helper class
+    std::vector<std::unique_ptr<SbxFactory>>
+                        m_Factories;
+    SbxBasicFormater   *pBasicFormater;    // Pointer to Format()-Command helper class
 
     LanguageType        eBasicFormaterLangType;
     // It might be useful to store this class 'global' because some string reosurces are saved here

bin/find-unused-defines.awk
@@ -14,7 +14,6 @@ END {
 
 
 # | xargs -P 4 -Ixxx sh -c "git grep -w 'xxx' | awk '{ x++; y=\$0 } END { if (x==1) print y }' && echo \"xxx\" 1>&2"
-
 # 	sed -i '' '/pattern/d'
 # | awk  'arr[$0]++ END { for (i in arr) { if(arr[i]==1) print i } }' \
 # | awk -f find-unused-defines.awk

bin/find-unused-typedefs.py
@@ -2,12 +2,13 @@
 
 import subprocess
 
-a = subprocess.Popen("git grep -P '^typedef\s+.+\w+;' -- \"[!e][!x][!t]*\"", stdout=subprocess.PIPE, shell=True)
+a = subprocess.Popen("git grep -P '^typedef\s+.+\s+\w+;' -- \"[!e][!x][!t]*\"", stdout=subprocess.PIPE, shell=True)
 
 with a.stdout as txt:
     for line in txt:
-        idx1 = line.rfind(" ")
-        typedefName = line[idx1+1 : len(line)-2]
+        idx2 = line.rfind(";")
+        idx1 = line.rfind(" ", 0, idx2)
+        typedefName = line[idx1+1 : idx2]
         if typedefName.startswith("*"):
            typedefName = typedefName[1:]
         # ignore anything less than 5 characters, it's probably a parsing error

bin/find-unused-typedefs.sh
@@ -18,9 +18,8 @@
 #   (8) if so, generate a sed command to remove the #define
 #
 bin/find-unused-typedefs.py \
-  | sort \
-  | uniq \
+  | sort -u \
   | xargs -Ixxx -n 1 -P 8 sh -c \
-    "( git grep -w 'xxx' | awk -f bin/find-unused-defines.awk -v p1=xxx ) && echo \"xxx\" 1>&2"
+    '( git grep -w xxx | awk -f bin/find-unused-defines.awk -v p1=xxx ) && echo xxx 1>&2'
 
 

chart2/source/view/inc/GL3DRenderer.hxx
@@ -39,7 +39,6 @@ struct PosVecf3
 };
 
 typedef std::vector <glm::vec3> Vertices3D;
-typedef std::vector <glm::vec2> UVs3D;
 typedef std::vector <glm::vec3> Normals3D;
 
 struct MaterialParameters

cui/source/inc/treeopt.hxx
@@ -37,15 +37,14 @@ struct OrderedEntry
         m_nIndex( nIndex ), m_sId( rId ) {}
 };
 
-typedef std::vector< OrderedEntry* > VectorOfOrderedEntries;
 
 // struct Module ---------------------------------------------------------
 
 struct Module
 {
-    OUString           m_sName;
-    bool                    m_bActive;
-    VectorOfOrderedEntries  m_aNodeList;
+    OUString                      m_sName;
+    bool                          m_bActive;
+    std::vector< OrderedEntry* >  m_aNodeList;
 
     Module( const OUString& rName ) : m_sName( rName ), m_bActive( false ) {}
 };
@@ -76,7 +75,6 @@ struct OptionsLeaf
 };
 
 typedef ::std::vector< OptionsLeaf* > VectorOfLeaves;
-typedef ::std::vector< VectorOfLeaves > VectorOfGroupedLeaves;
 
 // struct OptionsNode ----------------------------------------------------
 
@@ -89,7 +87,8 @@ struct OptionsNode
     OUString                m_sGroupId;
     sal_Int32               m_nGroupIndex;
     VectorOfLeaves          m_aLeaves;
-    VectorOfGroupedLeaves   m_aGroupedLeaves;
+    ::std::vector< VectorOfLeaves >
+                            m_aGroupedLeaves;
 
     OptionsNode(    const OUString& rId,
                     const OUString& rLabel,
@@ -133,7 +132,6 @@ struct OptionsPageInfo;
 struct Module;
 class ExtensionsTabPage;
 class SvxColorTabPage;
-typedef std::vector< ExtensionsTabPage* > VectorOfPages;
 
 class OfaTreeOptionsDialog : public SfxModalDialog
 {

cui/source/tabpages/align.cxx
@@ -230,7 +230,7 @@ AlignmentTabPage::AlignmentTabPage( vcl::Window* pParent, const SfxItemSet& rCor
 
     AddItemConnection( new HorJustConnection( SID_ATTR_ALIGN_HOR_JUSTIFY, *m_pLbHorAlign, s_pHorJustMap, sfx::ITEMCONN_HIDE_UNKNOWN ) );
     AddItemConnection( new sfx::DummyItemConnection( SID_ATTR_ALIGN_INDENT, *m_pFtIndent, sfx::ITEMCONN_HIDE_UNKNOWN ) );
-    AddItemConnection( new sfx::UInt16MetricConnection( SID_ATTR_ALIGN_INDENT, *m_pEdIndent, FUNIT_TWIP, sfx::ITEMCONN_HIDE_UNKNOWN ) );
+    AddItemConnection( new sfx::MetricConnection<sfx::UInt16ItemWrapper>( SID_ATTR_ALIGN_INDENT, *m_pEdIndent, FUNIT_TWIP, sfx::ITEMCONN_HIDE_UNKNOWN ) );
     AddItemConnection( new sfx::DummyItemConnection( SID_ATTR_ALIGN_VER_JUSTIFY, *m_pFtVerAlign, sfx::ITEMCONN_HIDE_UNKNOWN ) );
     AddItemConnection( new VerJustConnection( SID_ATTR_ALIGN_VER_JUSTIFY, *m_pLbVerAlign, s_pVerJustMap, sfx::ITEMCONN_HIDE_UNKNOWN ) );
     AddItemConnection( new DialControlConnection( SID_ATTR_ALIGN_DEGREES, *m_pCtrlDial, sfx::ITEMCONN_HIDE_UNKNOWN ) );

zhirshya/lo-tblr

commit 814c07e5b1ca1bfe0fba2588ef34e65ec8377d3d

REGPARAM_INT/FLT /are/ used
...broken with 53f16c39f5c4c69ab603bf9098daec71f7a0acb8 "remove unused typedefs
and inline use-once typedefs"

Change-Id: I4b1343b98733ae998c7f9c594a86d65348402d28

bridges/source/cpp_uno/msvc_win32_x86-64/mscx.hxx
@@ -36,6 +36,7 @@ namespace CPPU_CURRENT_NAMESPACE
 const DWORD MSVC_ExceptionCode = 0xe06d7363;
 const long MSVC_magic_number = 0x19930520L;
 
+typedef enum { REGPARAM_INT, REGPARAM_FLT } RegParamKind;
 
 type_info * mscx_getRTTI( OUString const & rUNOname );
 