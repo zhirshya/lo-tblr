LibreOffice/core

commit d75b9fd582f0aa83bd2fc99028b3f83eb7171d61

tdf#94205: Use o3tl::make_unique insted of new+std::move.
Change-Id: I9ff14760479d2ac882546e2e5b74ab750ba2fa4b
Reviewed-on: https://gerrit.libreoffice.org/20984
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
Tested-by: Stephan Bergmann <sbergman@redhat.com>

sw/source/core/text/blink.cxx
@@ -23,7 +23,7 @@
 #include "blink.hxx"
 #include "porlin.hxx"
 #include "porlay.hxx"
-
+#include <o3tl/make_unique.hxx>
 // Visible time
 #define BLINK_ON_TIME       2400L
 // Invisible time
@@ -147,9 +147,8 @@ void SwBlink::Replace( const SwLinePortion* pOld, const SwLinePortion* pNew )
     SwBlinkSet::iterator it = m_List.find( pBlinkPortion );
     if (it != m_List.end())
     {
-        std::unique_ptr<SwBlinkPortion> pTmp(new SwBlinkPortion(it->get(), pNew));
         m_List.erase( it );
-        m_List.insert(std::move(pTmp));
+        m_List.insert(o3tl::make_unique<SwBlinkPortion>(it->get(), pNew));
     }
 }
 

chart2/source/controller/accessibility/AccessibleTextHelper.cxx
@@ -34,7 +34,7 @@
 #include <vcl/window.hxx>
 
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
-
+#include <o3tl/make_unique.hxx>
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::accessibility;
 
@@ -90,8 +90,7 @@ void SAL_CALL AccessibleTextHelper::initialize( const Sequence< uno::Any >& aArg
             SdrObject * pTextObj = m_pDrawViewWrapper->getNamedSdrObject( aCID );
             if( pTextObj )
             {
-                std::unique_ptr<SvxEditSource> pEditSource(new SvxTextEditSource( *pTextObj, nullptr, *pView, *pWindow ));
-                m_pTextHelper = new ::accessibility::AccessibleTextHelper(std::move(pEditSource));
+                m_pTextHelper = new ::accessibility::AccessibleTextHelper(o3tl::make_unique<SvxTextEditSource>(*pTextObj, nullptr, *pView, *pWindow));
                 m_pTextHelper->SetEventSource( xEventSource );
             }
         }

cui/source/customize/acccfg.cxx
@@ -69,7 +69,7 @@
 #include <vcl/help.hxx>
 #include <rtl/ustrbuf.hxx>
 #include <comphelper/sequenceashashmap.hxx>
-
+#include <o3tl/make_unique.hxx>
 // namespaces
 
 using namespace css;
@@ -911,13 +911,10 @@ void SfxAcceleratorConfigPage::CreateCustomItems(SvTreeListEntry* pEntry,
                                                  const OUString& sCol1 ,
                                                  const OUString& sCol2)
 {
-    std::unique_ptr<SfxAccCfgLBoxString_Impl> pStringItem1(
-            new SfxAccCfgLBoxString_Impl(pEntry, 0, sCol1));
-    pEntry->ReplaceItem(std::move(pStringItem1), 1);
 
-    std::unique_ptr<SfxAccCfgLBoxString_Impl> pStringItem2(
-            new SfxAccCfgLBoxString_Impl(pEntry, 0, sCol2));
-    pEntry->ReplaceItem(std::move(pStringItem2), 2);
+    pEntry->ReplaceItem(o3tl::make_unique<SfxAccCfgLBoxString_Impl>(pEntry, 0, sCol1), 1);
+
+    pEntry->ReplaceItem(o3tl::make_unique<SfxAccCfgLBoxString_Impl>(pEntry, 0, sCol2), 2);
 }
 
 

cui/source/options/optHeaderTabListbox.cxx
@@ -23,7 +23,7 @@
 #include <svtools/headbar.hxx>
 #include <svtools/svlbitm.hxx>
 #include <svtools/treelistentry.hxx>
-
+#include <o3tl/make_unique.hxx>
 namespace svx
 {
 // class OptLBoxString_Impl ----------------------------------------------
@@ -71,9 +71,7 @@ void OptHeaderTabListBox::InitEntry( SvTreeListEntry* pEntry, const OUString& rT
     {
         // initialize all columns with own class (column 0 == Bitmap)
         SvLBoxString& rCol = static_cast<SvLBoxString&>(pEntry->GetItem( nCol ));
-        std::unique_ptr<OptLBoxString_Impl> pStr(
-                new OptLBoxString_Impl(pEntry, 0, rCol.GetText()));
-        pEntry->ReplaceItem(std::move(pStr), nCol);
+        pEntry->ReplaceItem(o3tl::make_unique<OptLBoxString_Impl>(pEntry, 0, rCol.GetText()), nCol);
     }
 }
 

dbaccess/source/ui/control/dbtreelistbox.cxx
@@ -39,7 +39,7 @@
 #include "svtools/treelistentry.hxx"
 
 #include <memory>
-
+#include <o3tl/make_unique.hxx>
 namespace dbaui
 {
 
@@ -139,8 +139,7 @@ void DBTreeListBox::InitEntry(SvTreeListEntry* _pEntry, const OUString& aStr, co
 {
     SvTreeListBox::InitEntry( _pEntry, aStr, _rCollEntryBmp,_rExpEntryBmp, eButtonKind);
     SvLBoxItem* pTextItem(_pEntry->GetFirstItem(SV_ITEM_ID_LBOXSTRING));
-    std::unique_ptr<SvLBoxString> pString(new OBoldListboxString(_pEntry, 0, aStr));
-    _pEntry->ReplaceItem(std::move(pString), _pEntry->GetPos(pTextItem));
+    _pEntry->ReplaceItem(o3tl::make_unique<OBoldListboxString>(_pEntry, 0, aStr), _pEntry->GetPos(pTextItem));
 }
 
 void DBTreeListBox::implStopSelectionTimer()

dbaccess/source/ui/misc/WNameMatch.cxx
@@ -28,7 +28,7 @@
 #include <vcl/builderfactory.hxx>
 #include "svtools/treelistentry.hxx"
 #include <com/sun/star/sdbc/DataType.hpp>
-
+#include <o3tl/make_unique.hxx>
 using namespace ::dbaui;
 // OWizColumnSelect
 OWizNameMatching::OWizNameMatching( vcl::Window* pParent)
@@ -373,8 +373,7 @@ VCL_BUILDER_FACTORY(OColumnTreeBox)
 void OColumnTreeBox::InitEntry(SvTreeListEntry* pEntry, const OUString& rStr, const Image& rImg1, const Image& rImg2, SvLBoxButtonKind eButtonKind)
 {
     DBTreeListBox::InitEntry(pEntry, rStr, rImg1, rImg2, eButtonKind);
-    std::unique_ptr<SvLBoxString> pString(new OColumnString(pEntry, 0, rStr,false));
-    pEntry->ReplaceItem(std::move(pString), pEntry->ItemCount() - 1);
+    pEntry->ReplaceItem(o3tl::make_unique<OColumnString>(pEntry, 0, rStr,false), pEntry->ItemCount() - 1);
 }
 
 bool OColumnTreeBox::Select( SvTreeListEntry* pEntry, bool bSelect )

sc/source/filter/html/htmlpars.cxx
@@ -57,7 +57,7 @@
 #include "rangelst.hxx"
 
 #include <config_orcus.h>
-
+#include <o3tl/make_unique.hxx>
 #if ENABLE_ORCUS
 #include <orcus/css_parser.hpp>
 #endif
@@ -86,9 +86,8 @@ void ScHTMLStyles::add(const char* pElemName, size_t nElemName, const char* pCla
             if (itrElem == m_ElemProps.end())
             {
                 // new element
-                std::unique_ptr<NamePropsType> p(new NamePropsType);
                 std::pair<ElemsType::iterator, bool> r =
-                    m_ElemProps.insert(std::make_pair(aElem, std::move(p)));
+                    m_ElemProps.insert(std::make_pair(aElem, o3tl::make_unique<NamePropsType>()));
                 if (!r.second)
                     // insertion failed.
                     return;
@@ -171,9 +170,8 @@ void ScHTMLStyles::insertProp(
     if (itr == rStore.end())
     {
         // new element
-        std::unique_ptr<PropsType> p(new PropsType);
         std::pair<NamePropsType::iterator, bool> r =
-            rStore.insert(std::make_pair(aName, std::move(p)));
+            rStore.insert(std::make_pair(aName, o3tl::make_unique<PropsType>()));
         if (!r.second)
             // insertion failed.
             return;

sc/source/filter/xml/xmlimprt.cxx
@@ -96,7 +96,7 @@
 
 #include <memory>
 #include <utility>
-
+#include <o3tl/make_unique.hxx>
 #define SC_LOCALE           "Locale"
 #define SC_CURRENCYSYMBOL   "CurrencySymbol"
 #define SC_REPEAT_ROW "repeat-row"
@@ -2401,9 +2401,8 @@ void ScXMLImport::AddNamedExpression(SCTAB nTab, ScMyNamedExpression* pNamedExp)
     if (itr == m_SheetNamedExpressions.end())
     {
         // No chain exists for this sheet.  Create one.
-        ::std::unique_ptr<ScMyNamedExpressions> pNew(new ScMyNamedExpressions);
         ::std::pair<SheetNamedExpMap::iterator, bool> r =
-            m_SheetNamedExpressions.insert(std::make_pair(nTab, std::move(pNew)));
+            m_SheetNamedExpressions.insert(std::make_pair(nTab, o3tl::make_unique<ScMyNamedExpressions>()));
         if (!r.second)
             // insertion failed.
             return;

zhirshya/lo-tblr

commit 9b6d898d61119721e227f53e57c32d77a0f1cb18

sw: revert use of invalid (erased) iterator
(regression from commit d75b9fd582f0aa83bd2fc99028b3f83eb7171d61)

Change-Id: I6e67ebfbac20b42c0ff8580b575793daec46b68d

sw/source/core/text/blink.cxx
@@ -23,7 +23,7 @@
 #include "blink.hxx"
 #include "porlin.hxx"
 #include "porlay.hxx"
-#include <o3tl/make_unique.hxx>
+
 // Visible time
 #define BLINK_ON_TIME       2400L
 // Invisible time
@@ -147,8 +147,9 @@ void SwBlink::Replace( const SwLinePortion* pOld, const SwLinePortion* pNew )
     SwBlinkSet::iterator it = m_List.find( pBlinkPortion );
     if (it != m_List.end())
     {
+        std::unique_ptr<SwBlinkPortion> pTmp(new SwBlinkPortion(it->get(), pNew));
         m_List.erase( it );
-        m_List.insert(o3tl::make_unique<SwBlinkPortion>(it->get(), pNew));
+        m_List.insert(std::move(pTmp));
     }
 }
 