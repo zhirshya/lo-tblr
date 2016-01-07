LibreOffice/core

commit d6a83d3f91336e23b51bfc3b3d58da799760829f

convert UserDrawHdl to boost signals2
Note that ImplListBox::userDrawSignal is connected to
ImplListBoxWindow::userDrawSignal to forward the signal, just like
the Link was previously forwarded.

Change-Id: Ic8fc6f51e309bc34fb4e39c13745302649991d0a

vcl/source/control/combobox.cxx
@@ -180,7 +180,7 @@ void ComboBox::ImplInit( Window* pParent, WinBits nStyle )
     mpImplLB->SetSelectHdl( LINK( this, ComboBox, ImplSelectHdl ) );
     mpImplLB->SetCancelHdl( LINK( this, ComboBox, ImplCancelHdl ) );
     mpImplLB->SetDoubleClickHdl( LINK( this, ComboBox, ImplDoubleClickHdl ) );
-    mpImplLB->SetUserDrawHdl( LINK( this, ComboBox, ImplUserDrawHdl ) );
+    mpImplLB->userDrawSignal.connect( boost::bind( &ComboBox::ImplUserDrawHandler, this, _1 ) );
     mpImplLB->SetSelectionChangedHdl( LINK( this, ComboBox, ImplSelectionChangedHdl ) );
     mpImplLB->SetListItemSelectHdl( LINK( this, ComboBox, ImplListItemSelectHdl ) );
     mpImplLB->Show();
@@ -1221,10 +1221,9 @@ void ComboBox::Draw( OutputDevice* pDev, const Point& rPos, const Size& rSize, s
 
 }
 
-IMPL_LINK( ComboBox, ImplUserDrawHdl, UserDrawEvent*, pEvent )
+void::ComboBox::ImplUserDrawHandler( UserDrawEvent* pEvent )
 {
     UserDraw( *pEvent );
-    return 1;
 }
 
 void ComboBox::UserDraw( const UserDrawEvent& )

include/vcl/combobox.hxx
@@ -71,11 +71,11 @@ private:
     DECL_DLLPRIVATE_LINK(   ImplDoubleClickHdl, void* );
     DECL_DLLPRIVATE_LINK(   ImplPopupModeEndHdl, void* );
     DECL_DLLPRIVATE_LINK(   ImplSelectionChangedHdl, void* );
-    DECL_DLLPRIVATE_LINK(   ImplUserDrawHdl, UserDrawEvent* );
     DECL_DLLPRIVATE_LINK(   ImplAutocompleteHdl, Edit* );
     DECL_DLLPRIVATE_LINK( ImplListItemSelectHdl , void* );
 
     SAL_DLLPRIVATE void ImplClickButtonHandler( ImplBtn* );
+    SAL_DLLPRIVATE void ImplUserDrawHandler( UserDrawEvent* );
 
 protected:
     using Window::ImplInit;

include/vcl/lstbox.hxx
@@ -61,11 +61,12 @@ private:
     DECL_DLLPRIVATE_LINK(  ImplDoubleClickHdl, void* );
     DECL_DLLPRIVATE_LINK(  ImplPopupModeEndHdl, void* );
     DECL_DLLPRIVATE_LINK(  ImplSelectionChangedHdl, void* );
-    DECL_DLLPRIVATE_LINK(  ImplUserDrawHdl, UserDrawEvent* );
     DECL_DLLPRIVATE_LINK(  ImplFocusHdl, void* );
     DECL_DLLPRIVATE_LINK(  ImplListItemSelectHdl, void* );
 
     SAL_DLLPRIVATE void ImplClickButtonHandler( Control* );
+    SAL_DLLPRIVATE void ImplUserDrawHandler( UserDrawEvent* );
+
 protected:
     using Window::ImplInit;
     SAL_DLLPRIVATE void    ImplInit( Window* pParent, WinBits nStyle );

vcl/inc/ilstbox.hxx
@@ -233,7 +233,6 @@ private:
     Link            maSelectHdl;
     Link            maCancelHdl;
     Link            maDoubleClickHdl;
-    Link            maUserDrawHdl;
     Link            maMRUChangedHdl;
     Link            maFocusHdl;
     Link            maListItemSelectHdl;
@@ -342,12 +341,12 @@ public:
     const Link&     GetCancelHdl() const                { return maCancelHdl; }
     void            SetDoubleClickHdl( const Link& rLink )  { maDoubleClickHdl = rLink; }
     const Link&     GetDoubleClickHdl() const               { return maDoubleClickHdl; }
-    void            SetUserDrawHdl( const Link& rLink ) { maUserDrawHdl = rLink; }
-    const Link&     GetUserDrawHdl() const              { return maUserDrawHdl; }
     void            SetMRUChangedHdl( const Link& rLink )   { maMRUChangedHdl = rLink; }
     const Link&     GetMRUChangedHdl() const                { return maMRUChangedHdl; }
     void            SetFocusHdl( const Link& rLink )    { maFocusHdl = rLink ; }
     const Link&     GetFocusHdl() const             { return maFocusHdl; }
+
+    boost::signals2::signal< void ( UserDrawEvent* ) > userDrawSignal;
 
     void            SetListItemSelectHdl( const Link& rLink )   { maListItemSelectHdl = rLink ; }
     const Link&     GetListItemSelectHdl() const                { return maListItemSelectHdl; }
@@ -478,8 +477,8 @@ public:
     const Link&     GetCancelHdl() const                { return maLBWindow.GetCancelHdl(); }
     void            SetDoubleClickHdl( const Link& rLink )  { maLBWindow.SetDoubleClickHdl( rLink ); }
     const Link&     GetDoubleClickHdl() const               { return maLBWindow.GetDoubleClickHdl(); }
-    void            SetUserDrawHdl( const Link& rLink ) { maLBWindow.SetUserDrawHdl( rLink ); }
-    const Link&     GetUserDrawHdl() const              { return maLBWindow.GetUserDrawHdl(); }
+
+    boost::signals2::signal< void ( UserDrawEvent* ) > userDrawSignal;
 
     void            SetFocusHdl( const Link& rLink )    { maLBWindow.SetFocusHdl( rLink ); }
     const Link&     GetFocusHdl() const             { return maLBWindow.GetFocusHdl(); }
@@ -555,8 +554,6 @@ private:
     Rectangle       maFocusRect;
     Size            maUserItemSize;
 
-    Link            maUserDrawHdl;
-
     /// bitfield
     bool            mbUserDrawEnabled : 1;
     bool            mbInUserDraw : 1;
@@ -589,9 +586,7 @@ public:
     virtual void    MBDown();
 
     boost::signals2::signal< void ( ImplWin* ) > buttonDownSignal;
-
-    void            SetUserDrawHdl( const Link& rLink ) { maUserDrawHdl = rLink; }
-    const Link&     GetUserDrawHdl() const              { return maUserDrawHdl; }
+    boost::signals2::signal< void ( UserDrawEvent* ) > userDrawSignal;
 
     void            SetUserItemSize( const Size& rSz )  { maUserItemSize = rSz; }
     const Size&     GetUserItemSize() const             { return maUserItemSize; }

vcl/source/control/ilstbox.cxx
@@ -1739,7 +1739,7 @@ void ImplListBoxWindow::ImplPaint( sal_Int32 nPos, bool bErase, bool bLayout )
         nCurr = sal::static_int_cast<sal_Int32>( nCurr - GetEntryList()->GetMRUCount());
 
         UserDrawEvent aUDEvt( this, aRect, nPos, nCurr );
-        maUserDrawHdl.Call( &aUDEvt );
+        userDrawSignal( &aUDEvt );
         mbInUserDraw = false;
     }
     else
@@ -2132,6 +2132,8 @@ ImplListBox::ImplListBox( Window* pParent, WinBits nWinStyle ) :
     Control( pParent, nWinStyle ),
     maLBWindow( this, nWinStyle&(~WB_BORDER) )
 {
+    maLBWindow.userDrawSignal.connect( userDrawSignal );
+
     // for native widget rendering we must be able to detect this window type
     SetType( WINDOW_LISTBOXWINDOW );
 
@@ -2737,7 +2739,7 @@ void ImplWin::ImplDraw( bool bLayout )
     {
         mbInUserDraw = true;
         UserDrawEvent aUDEvt( this, maFocusRect, mnItemPos, 0 );
-        maUserDrawHdl.Call( &aUDEvt );
+        userDrawSignal( &aUDEvt );
         mbInUserDraw = false;
     }
     else

vcl/source/control/lstbox.cxx
@@ -137,7 +137,7 @@ void ListBox::ImplInit( Window* pParent, WinBits nStyle )
 
         mpImplWin = new ImplWin( this, (nStyle & (WB_LEFT|WB_RIGHT|WB_CENTER))|WB_NOBORDER );
         mpImplWin->buttonDownSignal.connect( boost::bind( &ListBox::ImplClickButtonHandler, this, _1 ));
-        mpImplWin->SetUserDrawHdl( LINK( this, ListBox, ImplUserDrawHdl ) );
+        mpImplWin->userDrawSignal.connect( boost::bind( &ListBox::ImplUserDrawHandler, this, _1 ) );
         mpImplWin->Show();
         mpImplWin->GetDropTarget()->addDropTargetListener(xDrop);
         mpImplWin->SetEdgeBlending(GetEdgeBlending());
@@ -157,7 +157,7 @@ void ListBox::ImplInit( Window* pParent, WinBits nStyle )
     mpImplLB->SetScrollHdl( LINK( this, ListBox, ImplScrollHdl ) );
     mpImplLB->SetCancelHdl( LINK( this, ListBox, ImplCancelHdl ) );
     mpImplLB->SetDoubleClickHdl( LINK( this, ListBox, ImplDoubleClickHdl ) );
-    mpImplLB->SetUserDrawHdl( LINK( this, ListBox, ImplUserDrawHdl ) );
+    mpImplLB->userDrawSignal.connect( boost::bind( &ListBox::ImplUserDrawHandler, this, _1 ) );
     mpImplLB->SetFocusHdl( LINK( this, ListBox, ImplFocusHdl ) );
     mpImplLB->SetListItemSelectHdl( LINK( this, ListBox, ImplListItemSelectHdl ) );
     mpImplLB->SetPosPixel( Point() );
@@ -1360,10 +1360,9 @@ void ListBox::GetMaxVisColumnsAndLines( sal_uInt16& rnCols, sal_uInt16& rnLines 
     }
 }
 
-IMPL_LINK( ListBox, ImplUserDrawHdl, UserDrawEvent*, pEvent )
+void ListBox::ImplUserDrawHandler( UserDrawEvent* pEvent )
 {
     UserDraw( *pEvent );
-    return 1;
 }
 
 void ListBox::UserDraw( const UserDrawEvent& )

zhirshya/lo-tblr

commit 72952793016b526e2c8421b4231fd2c9f2c43ad7

vcl: ComboBox::ImplUserDrawHandler was in the void namespace...
I'm amazed that C++ allows you to do this, but I checked the draft
standard and there appears to be no restrictions around using a
C++ keyword as a namespace identifier!

This hasn't been an issue it appears because we don't have any
user-defined draw signals for combobox's currently.

This was picked up by Doxygen, which spat out the following error:

/home/chris/repos/libreoffice/vcl/source/control/combobox.cxx:1322 warning: no uniquely matching class member found for
  void::ComboBox::Impl::ImplUserDrawHandler(UserDrawEvent *pEvent)

Issue introduced in commit d6a83d3f913, I checked it and it was the
only place this occured in the patch.

Change-Id: Ie4272e5c4e3dba13ed82457bba8579971a5345d0

vcl/source/control/combobox.cxx
@@ -1319,7 +1319,7 @@ void ComboBox::Draw( OutputDevice* pDev, const Point& rPos, const Size& rSize, D
 
 }
 
-void::ComboBox::Impl::ImplUserDrawHandler( UserDrawEvent* pEvent )
+void ComboBox::Impl::ImplUserDrawHandler( UserDrawEvent* pEvent )
 {
     m_rThis.UserDraw(*pEvent);
 }