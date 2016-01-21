LibreOffice/core

commit a73e606b8cd714520285b4e40890db9fd27d7ba5

Quickfix for deterministic SvpSalBitmap checksums
...that include the junk parts of the mpDIB->mpBits array in the calculation.
MALLOC_PERTURB_=153 in gb_CppunitTest_malloc_check
(solenv/gbuild/platform/unxgcc.mk) causes those junk bytes to normally contain
0x66 on Linux, but ASan makes them contain 0xBE instead.

Change-Id: I74429c5e539e667cb936b3a486de31867eef3e50

vcl/headless/svpbmp.cxx
@@ -19,6 +19,10 @@
 
 #ifndef IOS
 
+#include <sal/config.h>
+
+#include <cstring>
+
 #include "headless/svpbmp.hxx"
 #include "headless/svpgdi.hxx"
 #include "headless/svpinst.hxx"
@@ -117,15 +121,17 @@ BitmapBuffer* ImplCreateDIB(
                 pDIB->maPalette.SetEntryCount( nColors );
             }
 
+            auto size = pDIB->mnScanlineSize * pDIB->mnHeight;
             try
             {
-                pDIB->mpBits = new sal_uInt8[ pDIB->mnScanlineSize * pDIB->mnHeight ];
+                pDIB->mpBits = new sal_uInt8[size];
             }
             catch (const std::bad_alloc&)
             {
                 delete pDIB;
                 pDIB = nullptr;
             }
+            std::memset(pDIB->mpBits, 0, size);
         }
     }
     else

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -773,7 +773,7 @@ DECLARE_OOXMLIMPORT_TEST(testN777345, "n777345.docx")
     Graphic aGraphic(xGraphic);
     // If this changes later, feel free to update it, but make sure it's not
     // the checksum of a white/transparent placeholder rectangle.
-    CPPUNIT_ASSERT_EQUAL(BitmapChecksum(SAL_CONST_UINT64(17166884950237449306)), aGraphic.GetChecksum());
+    CPPUNIT_ASSERT_EQUAL(BitmapChecksum(SAL_CONST_UINT64(12149824012634930130)), aGraphic.GetChecksum());
 #endif
 }
 

zhirshya/lo-tblr

commit b54082158d0a2b001bcb458b9812e82f74968de4

new[] failed but memset of requested len called anyway
recent regression from

commit a73e606b8cd714520285b4e40890db9fd27d7ba5
Date:   Thu Jan 21 16:04:28 2016 +0100

    Quickfix for deterministic SvpSalBitmap checksums

move the memset (and similar memcpy) inside try block

Change-Id: I51d76777f91664459deb777a5dfafae80203af57

vcl/headless/svpbmp.cxx
@@ -121,17 +121,17 @@ BitmapBuffer* ImplCreateDIB(
                 pDIB->maPalette.SetEntryCount( nColors );
             }
 
-            auto size = pDIB->mnScanlineSize * pDIB->mnHeight;
             try
             {
+                size_t size = pDIB->mnScanlineSize * pDIB->mnHeight;
                 pDIB->mpBits = new sal_uInt8[size];
+                std::memset(pDIB->mpBits, 0, size);
             }
             catch (const std::bad_alloc&)
             {
                 delete pDIB;
                 pDIB = nullptr;
             }
-            std::memset(pDIB->mpBits, 0, size);
         }
     }
     else
@@ -167,16 +167,15 @@ bool SvpSalBitmap::Create(const SalBitmap& rBmp)
         // TODO: get rid of this when BitmapBuffer gets copy constructor
         try
         {
-            mpDIB->mpBits = new sal_uInt8[ mpDIB->mnScanlineSize * mpDIB->mnHeight ];
+            size_t size = mpDIB->mnScanlineSize * mpDIB->mnHeight;
+            mpDIB->mpBits = new sal_uInt8[size];
+            std::memcpy(mpDIB->mpBits, rSalBmp.mpDIB->mpBits, size);
         }
         catch (const std::bad_alloc&)
         {
             delete mpDIB;
             mpDIB = nullptr;
         }
-
-        if (mpDIB)
-            memcpy(mpDIB->mpBits, rSalBmp.mpDIB->mpBits, mpDIB->mnScanlineSize * mpDIB->mnHeight);
     }
 
     return !rSalBmp.mpDIB || (rSalBmp.mpDIB && mpDIB != nullptr);