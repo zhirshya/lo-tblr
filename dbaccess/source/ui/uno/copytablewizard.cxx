LibreOffice/core

InterfaceContainer2 with vector instead of Sequence
create an InterfaceContainer2 class to replace InterfaceContainer.
It uses a std::vector instead of a Sequence for the mutable listener
list, which provides far better performance.

Switch all our internal use-sites to the new class.

Change-Id: I6b56cfa511ded2395faa22e68fab3b2f16c3cb88

dbaccess/source/ui/uno/copytablewizard.cxx
@@ -64,6 +64,7 @@
 #include <connectivity/dbtools.hxx>
 #include <cppuhelper/exc_hlp.hxx>
 #include <cppuhelper/implbase.hxx>
+#include <cppuhelper/interfacecontainer2.hxx>
 #include <rtl/ustrbuf.hxx>
 #include <svtools/genericunodialog.hxx>
 #include <tools/diagnose_ex.h>
@@ -346,7 +347,7 @@ private:
 
         // other
         Reference< XInteractionHandler > m_xInteractionHandler;
-        ::cppu::OInterfaceContainerHelper
+        ::cppu::OInterfaceContainerHelper2
                                         m_aCopyTableListeners;
         sal_Int16                       m_nOverrideExecutionResult;
     };
@@ -1016,7 +1017,7 @@ bool CopyTableWizard::impl_processCopyError_nothrow( const CopyTableRowEvent& _r
     Reference< XCopyTableListener > xListener;
     try
     {
-        ::cppu::OInterfaceIteratorHelper aIter( m_aCopyTableListeners );
+        ::cppu::OInterfaceIteratorHelper2 aIter( m_aCopyTableListeners );
         while ( aIter.hasMoreElements() )
         {
             xListener.set( aIter.next(), UNO_QUERY_THROW );

embeddedobj/source/msole/olemisc.cxx
@@ -190,7 +190,7 @@ void OleEmbeddedObject::StateChangeNotification_Impl( sal_Bool bBeforeChange, sa
 {
     if ( m_pInterfaceContainer )
     {
-        ::cppu::OInterfaceContainerHelper* pContainer = m_pInterfaceContainer->getContainer(
+        ::cppu::OInterfaceContainerHelper2* pContainer = m_pInterfaceContainer->getContainer(
                             cppu::UnoType<embed::XStateChangeListener>::get());
         if ( pContainer != NULL )
         {

extensions/source/propctrlr/formgeometryhandler.cxx
@@ -194,7 +194,7 @@ namespace pcr
 
     private:
         ::cppu::OWeakObject&                m_rParent;
-        ::cppu::OInterfaceContainerHelper   m_aPropertyChangeListeners;
+        ::cppu::OInterfaceContainerHelper2   m_aPropertyChangeListeners;
         Reference< XShape >                 m_xShape;
     };
 

sc/source/ui/inc/DrawModelBroadcaster.hxx
@@ -21,7 +21,7 @@
 #define INCLUDED_SC_SOURCE_UI_INC_DRAWMODELBROADCASTER_HXX
 
 #include <svl/lstner.hxx>
-#include <cppuhelper/interfacecontainer.h>
+#include <cppuhelper/interfacecontainer2.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <com/sun/star/document/XEventBroadcaster.hpp>
 
@@ -31,7 +31,7 @@ class ScDrawModelBroadcaster : public SfxListener,
     public ::cppu::WeakImplHelper< css::document::XEventBroadcaster >
 {
     mutable ::osl::Mutex maListenerMutex;
-    ::cppu::OInterfaceContainerHelper maEventListeners;
+    ::cppu::OInterfaceContainerHelper2 maEventListeners;
     SdrModel *mpDrawModel;
 
 public:

sw/inc/unochart.hxx
@@ -42,7 +42,7 @@
 #include <com/sun/star/chart/ChartDataRowSource.hpp>
 #include <com/sun/star/table/XCell.hpp>
 
-#include <cppuhelper/interfacecontainer.h>
+#include <cppuhelper/interfacecontainer2.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <cppuhelper/weakref.hxx>
 
@@ -135,9 +135,9 @@ class SwChartDataProvider :
     // all tables of the document.
     mutable Map_Set_DataSequenceRef_t       aDataSequences;
 
-    ::cppu::OInterfaceContainerHelper       aEvtListeners;
+    ::cppu::OInterfaceContainerHelper2      aEvtListeners;
     const SwDoc *                           pDoc;
-    bool                                bDisposed;
+    bool                                    bDisposed;
 
     SwChartDataProvider( const SwChartDataProvider & ) = delete;
     SwChartDataProvider & operator = ( const SwChartDataProvider & ) = delete;
@@ -251,8 +251,8 @@ class SwChartDataSequence :
     public SwChartDataSequenceBaseClass,
     public SwClient
 {
-    ::cppu::OInterfaceContainerHelper          m_aEvtListeners;
-    ::cppu::OInterfaceContainerHelper          m_aModifyListeners;
+    ::cppu::OInterfaceContainerHelper2          m_aEvtListeners;
+    ::cppu::OInterfaceContainerHelper2          m_aModifyListeners;
     css::chart2::data::DataSequenceRole        m_aRole;
 
     OUString  m_aRowLabelText;
@@ -362,8 +362,8 @@ SwChartLabeledDataSequenceBaseClass;
 class SwChartLabeledDataSequence :
     public SwChartLabeledDataSequenceBaseClass
 {
-    ::cppu::OInterfaceContainerHelper                           aEvtListeners;
-    ::cppu::OInterfaceContainerHelper                           aModifyListeners;
+    ::cppu::OInterfaceContainerHelper2                           aEvtListeners;
+    ::cppu::OInterfaceContainerHelper2                           aModifyListeners;
 
     css::uno::Reference< css::chart2::data::XDataSequence >     xData;
     css::uno::Reference< css::chart2::data::XDataSequence >     xLabels;

zhirshya/lo-tblr

dbaccess/source/ui/uno/copytablewizard.cxx
@@ -64,6 +64,7 @@
 #include <connectivity/dbtools.hxx>
 #include <cppuhelper/exc_hlp.hxx>
 #include <cppuhelper/implbase.hxx>
+#include <comphelper/interfacecontainer2.hxx>
 #include <rtl/ustrbuf.hxx>
 #include <svtools/genericunodialog.hxx>
 #include <tools/diagnose_ex.h>
@@ -346,7 +347,7 @@ private:
 
         // other
         Reference< XInteractionHandler > m_xInteractionHandler;
-        ::cppu::OInterfaceContainerHelper
+        ::comphelper::OInterfaceContainerHelper2
                                         m_aCopyTableListeners;
         sal_Int16                       m_nOverrideExecutionResult;
     };
@@ -1016,7 +1017,7 @@ bool CopyTableWizard::impl_processCopyError_nothrow( const CopyTableRowEvent& _r
     Reference< XCopyTableListener > xListener;
     try
     {
-        ::cppu::OInterfaceIteratorHelper aIter( m_aCopyTableListeners );
+        ::comphelper::OInterfaceIteratorHelper2 aIter( m_aCopyTableListeners );
         while ( aIter.hasMoreElements() )
         {
             xListener.set( aIter.next(), UNO_QUERY_THROW );

embeddedobj/source/msole/olemisc.cxx
@@ -190,7 +190,7 @@ void OleEmbeddedObject::StateChangeNotification_Impl( sal_Bool bBeforeChange, sa
 {
     if ( m_pInterfaceContainer )
     {
-        ::cppu::OInterfaceContainerHelper* pContainer = m_pInterfaceContainer->getContainer(
+        ::comphelper::OInterfaceContainerHelper2* pContainer = m_pInterfaceContainer->getContainer(
                             cppu::UnoType<embed::XStateChangeListener>::get());
         if ( pContainer != NULL )
         {

extensions/source/propctrlr/formgeometryhandler.cxx
@@ -194,7 +194,7 @@ namespace pcr
 
     private:
         ::cppu::OWeakObject&                m_rParent;
-        ::cppu::OInterfaceContainerHelper   m_aPropertyChangeListeners;
+        ::comphelper::OInterfaceContainerHelper2   m_aPropertyChangeListeners;
         Reference< XShape >                 m_xShape;
     };
 

sc/source/ui/inc/DrawModelBroadcaster.hxx
@@ -21,7 +21,7 @@
 #define INCLUDED_SC_SOURCE_UI_INC_DRAWMODELBROADCASTER_HXX
 
 #include <svl/lstner.hxx>
-#include <cppuhelper/interfacecontainer.h>
+#include <comphelper/interfacecontainer2.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <com/sun/star/document/XEventBroadcaster.hpp>
 
@@ -31,7 +31,7 @@ class ScDrawModelBroadcaster : public SfxListener,
     public ::cppu::WeakImplHelper< css::document::XEventBroadcaster >
 {
     mutable ::osl::Mutex maListenerMutex;
-    ::cppu::OInterfaceContainerHelper maEventListeners;
+    ::comphelper::OInterfaceContainerHelper2 maEventListeners;
     SdrModel *mpDrawModel;
 
 public:

sw/inc/unochart.hxx
@@ -42,7 +42,7 @@
 #include <com/sun/star/chart/ChartDataRowSource.hpp>
 #include <com/sun/star/table/XCell.hpp>
 
-#include <cppuhelper/interfacecontainer.h>
+#include <comphelper/interfacecontainer2.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <cppuhelper/weakref.hxx>
 
@@ -135,9 +135,9 @@ class SwChartDataProvider :
     // all tables of the document.
     mutable Map_Set_DataSequenceRef_t       aDataSequences;
 
-    ::cppu::OInterfaceContainerHelper       aEvtListeners;
+    ::comphelper::OInterfaceContainerHelper2      aEvtListeners;
     const SwDoc *                           pDoc;
-    bool                                bDisposed;
+    bool                                    bDisposed;
 
     SwChartDataProvider( const SwChartDataProvider & ) = delete;
     SwChartDataProvider & operator = ( const SwChartDataProvider & ) = delete;
@@ -251,8 +251,8 @@ class SwChartDataSequence :
     public SwChartDataSequenceBaseClass,
     public SwClient
 {
-    ::cppu::OInterfaceContainerHelper          m_aEvtListeners;
-    ::cppu::OInterfaceContainerHelper          m_aModifyListeners;
+    ::comphelper::OInterfaceContainerHelper2          m_aEvtListeners;
+    ::comphelper::OInterfaceContainerHelper2          m_aModifyListeners;
     css::chart2::data::DataSequenceRole        m_aRole;
 
     OUString  m_aRowLabelText;
@@ -362,8 +362,8 @@ SwChartLabeledDataSequenceBaseClass;
 class SwChartLabeledDataSequence :
     public SwChartLabeledDataSequenceBaseClass
 {
-    ::cppu::OInterfaceContainerHelper                           aEvtListeners;
-    ::cppu::OInterfaceContainerHelper                           aModifyListeners;
+    ::comphelper::OInterfaceContainerHelper2                           aEvtListeners;
+    ::comphelper::OInterfaceContainerHelper2                           aModifyListeners;
 
     css::uno::Reference< css::chart2::data::XDataSequence >     xData;
     css::uno::Reference< css::chart2::data::XDataSequence >     xLabels;