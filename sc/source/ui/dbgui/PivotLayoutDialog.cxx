LibreOffice/core

commit 2ce227a7642647267c982909a8b3175d22c58b74

coverity#1351755 Dereference null return value
Change-Id: I144a2c498763534b8aff604bdb3440180ac0f2ff

sc/source/ui/dbgui/PivotLayoutDialog.cxx
@@ -517,12 +517,16 @@ void ScPivotLayoutDialog::ApplyChanges()
         {
             const ScRange& rOldRange = pOldDPObj->GetOutRange();
 
+            ScDPObject *pDPObj = nullptr;
             // FIXME: if the new range overlaps with the old one, the table actually doesn't move
             // and shouldn't therefore be deleted
             if ( ( ( rOldRange != aDestinationRange ) && !rOldRange.In( aDestinationRange ) )
                  || bToNewSheet )
             {
-                ScDPObject *pDPObj = mpDocument->GetDPAtCursor( maPivotParameters.nCol, maPivotParameters.nRow, maPivotParameters.nTab);
+                mpDocument->GetDPAtCursor( maPivotParameters.nCol, maPivotParameters.nRow, maPivotParameters.nTab);
+            }
+            if (pDPObj)
+            {
                 ScDBDocFunc aFunc( *(mpViewData->GetDocShell() ));
                 aFunc.RemovePivotTable( *pDPObj, true, false);
                 mpViewData->GetView()->CursorPosChanged();

zhirshya/lo-tblr

commit 3e7dfa7e9038d4413eb4e0e4c02ceb121fbff696

coverity#1351877 'Constant' variable guards dead code
and

coverity#1351882 Useless call

sigh, silly me

regression from

commit 2ce227a7642647267c982909a8b3175d22c58b74
Author: Caolán McNamara <caolanm@redhat.com>
Date:   Thu Feb 4 09:17:42 2016 +0000

    coverity#1351755 Dereference null return value

Change-Id: I6d6841d04f9bba68ec2077d874dcd24edbf4ab6c

sc/source/ui/dbgui/PivotLayoutDialog.cxx
@@ -523,7 +523,7 @@ void ScPivotLayoutDialog::ApplyChanges()
             if ( ( ( rOldRange != aDestinationRange ) && !rOldRange.In( aDestinationRange ) )
                  || bToNewSheet )
             {
-                mpDocument->GetDPAtCursor( maPivotParameters.nCol, maPivotParameters.nRow, maPivotParameters.nTab);
+                pDPObj = mpDocument->GetDPAtCursor( maPivotParameters.nCol, maPivotParameters.nRow, maPivotParameters.nTab);
             }
             if (pDPObj)
             {