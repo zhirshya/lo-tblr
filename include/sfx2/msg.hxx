LibreOffice/core

commit bdc1824ea7acfa2fe9d71cdbe57882acce155577

SwPaM::Find: search in shapes anchored to the range
Change-Id: I028f95029deaebddcacf2730d429ad53abbc79fa

sw/source/core/crsr/findtxt.cxx
@@ -37,6 +37,7 @@
 #include <doc.hxx>
 #include <IDocumentUndoRedo.hxx>
 #include <IDocumentState.hxx>
+#include <IDocumentDrawModelAccess.hxx>
 #include <pamtyp.hxx>
 #include <ndtxt.hxx>
 #include <swundo.hxx>
@@ -244,7 +245,8 @@ bool SwPaM::Find( const SearchOptions& rSearchOpt, bool bSearchInNotes , utl::Te
     {
         if( pNode->IsTxtNode() )
         {
-            sal_Int32 nTxtLen = pNode->GetTxtNode()->GetTxt().getLength();
+            SwTxtNode& rTxtNode = *pNode->GetTxtNode();
+            sal_Int32 nTxtLen = rTxtNode.GetTxt().getLength();
             sal_Int32 nEnd;
             if( rNdIdx == pPam->GetMark()->nNode )
                 nEnd = pPam->GetMark()->nContent.GetIndex();
@@ -256,7 +258,7 @@ bool SwPaM::Find( const SearchOptions& rSearchOpt, bool bSearchInNotes , utl::Te
             // if there are SwPostItFields inside our current node text, we
             // split the text into separate pieces and search for text inside
             // the pieces as well as inside the fields
-            const SwpHints *pHts = pNode->GetTxtNode()->GetpSwpHints();
+            const SwpHints *pHts = rTxtNode.GetpSwpHints();
 
             // count PostItFields by looping over all fields
             sal_Int32 aNumberPostits = 0;
@@ -295,14 +297,15 @@ bool SwPaM::Find( const SearchOptions& rSearchOpt, bool bSearchInNotes , utl::Te
             SwViewShell *const pWrtShell = (pDocShell) ? pDocShell->GetEditShell() : 0;
             SwPostItMgr *const pPostItMgr = (pWrtShell) ? pWrtShell->GetPostItMgr() : 0;
 
+            SvxSearchItem aSearchItem(SID_SEARCH_ITEM);
+            aSearchItem.SetSearchOptions(rSearchOpt);
+            aSearchItem.SetBackward(!bSrchForward);
+
             // If there is an active text edit, then search there.
             if (SdrView* pSdrView = pWrtShell->GetDrawView())
             {
                 if (pSdrView->GetTextEditObject())
                 {
-                    SvxSearchItem aSearchItem(SID_SEARCH_ITEM);
-                    aSearchItem.SetSearchOptions(rSearchOpt);
-                    aSearchItem.SetBackward(!bSrchForward);
                     sal_uInt16 nResult = pSdrView->GetTextEditOutlinerView()->StartSearchAndReplace(aSearchItem);
                     if (!nResult)
                     {
@@ -316,6 +319,19 @@ bool SwPaM::Find( const SearchOptions& rSearchOpt, bool bSearchInNotes , utl::Te
                         break;
                     }
                 }
+            }
+
+            // If there are any shapes anchored to this node, search there.
+            SwPaM aPaM(pNode->GetDoc()->GetNodes().GetEndOfContent());
+            aPaM.GetPoint()->nNode = rTxtNode;
+            aPaM.GetPoint()->nContent.Assign(aPaM.GetPoint()->nNode.GetNode().GetTxtNode(), nStart);
+            aPaM.SetMark();
+            aPaM.GetMark()->nNode = rTxtNode.GetIndex() + 1;
+            aPaM.GetMark()->nContent.Assign(aPaM.GetMark()->nNode.GetNode().GetTxtNode(), 0);
+            if (pNode->GetDoc()->getIDocumentDrawModelAccess().Search(aPaM, aSearchItem))
+            {
+                bFound = true;
+                break;
             }
 
             sal_Int32 aStart = 0;

zhirshya/lo-tblr

commit 63cdbfb396a1858538911a9174e6ce5e47e61d86

tdf#98458 sw: make search in shape text optional
Instead of forcing it silently on everyone, add a new search option and
enable it only in case it's requested, as shape search is expensive for
large documents.

(Regression from commit bdc1824ea7acfa2fe9d71cdbe57882acce155577.)

Change-Id: Ibb6e76ad5c870dfd63b36429e16ef6d572f5f1fd

include/sfx2/msg.hxx
@@ -146,7 +146,7 @@ SFX_DECL_TYPE(13); // for SwAddPrinterItem, Sd...
 SFX_DECL_TYPE(14);
 SFX_DECL_TYPE(16); // for SwDocDisplayItem
 SFX_DECL_TYPE(17); // for SvxAddressItem
-SFX_DECL_TYPE(23); // for SvxSearchItem
+SFX_DECL_TYPE(24); // for SvxSearchItem
 
 // all SfxTypes must be in this header
 #undef SFX_DECL_TYPE

include/svl/memberid.hrc
@@ -61,6 +61,7 @@
 #define MID_SEARCH_STARTPOINTY          21
 #define MID_SEARCH_SEARCHFORMATTED      22
 #define MID_SEARCH_ALGORITHMTYPE2       23
+#define MID_SEARCH_INSHAPES             24
 
 #endif
 

include/svl/srchitem.hxx
@@ -90,6 +90,9 @@ class SVL_DLLPUBLIC SvxSearchItem :
     sal_Int32       m_nStartPointX;
     sal_Int32       m_nStartPointY;
 
+    /// Writer-specific: search in shape text.
+    bool            m_bSearchInShapes;
+
     virtual void    ImplCommit() override;
 
 public:
@@ -198,6 +201,7 @@ public:
     sal_Int32 GetStartPointY() const;
     /// Either x or y start point is set.
     bool HasStartPoint() const;
+    bool IsSearchInShapes() const;
 };
 
 const OUString& SvxSearchItem::GetSearchString() const

sfx2/sdi/sfxitems.sdi
@@ -91,6 +91,7 @@
         INT32       SearchStartPointY MID_SEARCH_STARTPOINTY;
         BOOL        SearchFormatted MID_SEARCH_SEARCHFORMATTED;
         INT16       AlgorithmType2  MID_SEARCH_ALGORITHMTYPE2;
+        BOOL        SearchInShapes  MID_SEARCH_INSHAPES;
     };
     item SvxSearch SvxSearchItem;
 

svl/source/items/srchitem.cxx
@@ -127,7 +127,8 @@ SvxSearchItem::SvxSearchItem( const sal_uInt16 nId ) :
     m_bContent        ( false ),
     m_bAsianOptions   ( false ),
     m_nStartPointX(0),
-    m_nStartPointY(0)
+    m_nStartPointY(0),
+    m_bSearchInShapes(false)
 {
     EnableNotification( lcl_GetNotifyNames() );
 
@@ -226,7 +227,8 @@ SvxSearchItem::SvxSearchItem( const SvxSearchItem& rItem ) :
     m_bContent        ( rItem.m_bContent ),
     m_bAsianOptions   ( rItem.m_bAsianOptions ),
     m_nStartPointX(rItem.m_nStartPointX),
-    m_nStartPointY(rItem.m_nStartPointY)
+    m_nStartPointY(rItem.m_nStartPointY),
+    m_bSearchInShapes(rItem.m_bSearchInShapes)
 {
     EnableNotification( lcl_GetNotifyNames() );
 }
@@ -670,6 +672,11 @@ bool SvxSearchItem::PutValue( const css::uno::Any& rVal, sal_uInt8 nMemberId )
             bRet = (rVal >>= m_nStartPointY);
             break;
         }
+        case MID_SEARCH_INSHAPES:
+        {
+            bRet = (rVal >>= m_bSearchInShapes);
+            break;
+        }
         default:
             OSL_FAIL( "Unknown MemberId" );
     }
@@ -690,6 +697,11 @@ sal_Int32 SvxSearchItem::GetStartPointY() const
 bool SvxSearchItem::HasStartPoint() const
 {
     return m_nStartPointX > 0 || m_nStartPointY > 0;
+}
+
+bool SvxSearchItem::IsSearchInShapes() const
+{
+    return m_bSearchInShapes;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/crsr/findtxt.cxx
@@ -301,6 +301,10 @@ bool SwPaM::Find( const SearchOptions2& rSearchOpt, bool bSearchInNotes , utl::T
             SwWrtShell *const pWrtShell = (pDocShell) ? pDocShell->GetWrtShell() : nullptr;
             SwPostItMgr *const pPostItMgr = (pWrtShell) ? pWrtShell->GetPostItMgr() : nullptr;
 
+            SvxSearchItem* pSearchItem = SwView::GetSearchItem();
+            if (pSearchItem && pSearchItem->IsSearchInShapes())
+            {
+            // Search in shapes only if explicitly requested.
             SvxSearchItem aSearchItem(SID_SEARCH_ITEM);
             aSearchItem.SetSearchOptions(rSearchOpt);
             aSearchItem.SetBackward(!bSrchForward);
@@ -344,7 +348,6 @@ bool SwPaM::Find( const SearchOptions2& rSearchOpt, bool bSearchInNotes , utl::T
             }
 
             // Writer and editeng selections are not supported in parallel.
-            SvxSearchItem* pSearchItem = SwView::GetSearchItem();
             // If we just finished search in shape text, don't attempt to do that again.
             if (!bEndedTextEdit && !(pSearchItem && pSearchItem->GetCommand() == SvxSearchCmd::FIND_ALL))
             {
@@ -374,6 +377,7 @@ bool SwPaM::Find( const SearchOptions2& rSearchOpt, bool bSearchInNotes , utl::T
                         }
                     }
                 }
+            }
             }
 
             sal_Int32 aStart = 0;

sw/qa/extras/tiledrendering/tiledrendering.cxx
@@ -328,7 +328,8 @@ void lcl_search(bool bBackward)
     uno::Sequence<beans::PropertyValue> aPropertyValues(comphelper::InitPropertySequence(
     {
         {"SearchItem.SearchString", uno::makeAny(OUString("shape"))},
-        {"SearchItem.Backward", uno::makeAny(bBackward)}
+        {"SearchItem.Backward", uno::makeAny(bBackward)},
+        {"SearchItem.SearchInShapes", uno::makeAny(true)}
     }));
     comphelper::dispatchCommand(".uno:ExecuteSearch", aPropertyValues);
 }