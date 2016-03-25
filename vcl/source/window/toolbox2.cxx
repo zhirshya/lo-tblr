LibreOffice/core

commit 27d94c482e82ea5d4f202109614c6fb3578fb0a4

color bar doesn't fill tool item
since

commit b8f21298288890d52c06fef6c84441634c56c986
Author: Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
Date:   Mon Mar 2 20:57:32 2015 +0900

the color bar is always drawn under the icon and doesn't
get drawn to the right and fill all the empty space of
the tool item, e.g. in impress see the new toolbar color
selector

Change-Id: Ib70fec9c933b9d743e67475899938a5d9f405496

vcl/source/window/toolbox2.cxx
@@ -935,6 +935,18 @@ sal_uInt16 ToolBox::GetItemId( const Point& rPos ) const
     return 0;
 }
 
+Size ToolBox::GetItemContentSize( sal_uInt16 nItemId ) const
+{
+    if ( mbCalc || mbFormat )
+        ((ToolBox*)this)->ImplFormat();
+
+    sal_uInt16 nPos = GetItemPos( nItemId );
+    if ( nPos < mpData->m_aItems.size() )
+        return mpData->m_aItems[nPos].maContentSize;
+    else
+        return Size();
+}
+
 sal_uInt16 ToolBox::GetItemId(const OUString &rCommand) const
 {
     if (!mpData)

include/vcl/toolbox.hxx
@@ -379,6 +379,9 @@ public:
     Rectangle           GetItemRect( sal_uInt16 nItemId ) const;
     Rectangle           GetItemPosRect( sal_uInt16 nPos ) const;
 
+    /// Returns size of the bitmap / text that is inside this toolbox item.
+    Size                GetItemContentSize( sal_uInt16 nItemId ) const;
+
     /// Retrieves the optimal position to place a popup window for this item (subtoolbar or dropdown)
     Point               GetItemPopupPosition( sal_uInt16 nItemId, const Size& rSize ) const;
 

svx/source/tbxctrls/tbxcolorupdate.cxx
@@ -79,8 +79,7 @@ namespace svx
     void ToolboxButtonColorUpdater::Update(const Color& rColor)
     {
         Image aImage(mpTbx->GetItemImageOriginal(mnBtnId));
-        BitmapEx aSource(aImage.GetBitmapEx());
-        Size aItemSize = aSource.GetSizePixel();
+        Size aItemSize(mpTbx->GetItemContentSize(mnBtnId));
 
         const bool bSizeChanged = (maBmpSize != aItemSize);
         const bool bDisplayModeChanged = (mbWasHiContrastMode != mpTbx->GetSettings().GetStyleSettings().GetHighContrastMode());
@@ -101,6 +100,7 @@ namespace svx
             sal_uInt8 nAlpha = 255;
             BitmapEx aBmpEx(Bitmap(aItemSize, 24), AlphaMask(aItemSize, &nAlpha));
 
+            BitmapEx aSource(aImage.GetBitmapEx());
             long nWidth = std::min(aItemSize.Width(), aSource.GetSizePixel().Width());
             long nHeight = std::min(aItemSize.Height(), aSource.GetSizePixel().Height());
 

zhirshya/lo-tblr

commit 2c3d04c75bf22c6538429fc4de9254dc15e8f9e8

tdf#94734: fix icon in sidebar disappears when changing font colour
- properly initalize maImageOriginal along with maImage in various
  places
- this is a regression from 27d94c482e82ea5d4f202109614c6fb3578fb0a4
- that commit is mostly innocent in itself though, only exposing
  omissions from b8f21298288890d52c06fef6c84441634c56c986
- having both maImage and maImageOriginal is extremely fragile as-is --
  likely I'd be helpful to have them private in ImplToolItem and then a
  SetImage accessor with a tristate eOriginal/eCached/eBoth enum
  parameter to prevent oversights like this in the future ...

Change-Id: I294d7ed8e07be5cadec0ae251cc08c8d089bd059

vcl/source/window/toolbox2.cxx
@@ -106,7 +106,8 @@ ImplToolItem::ImplToolItem()
 
 ImplToolItem::ImplToolItem( sal_uInt16 nItemId, const Image& rImage,
                             ToolBoxItemBits nItemBits ) :
-    maImage( rImage )
+    maImage( rImage ),
+    maImageOriginal( rImage )
 {
     init(nItemId, nItemBits, false);
 }
@@ -121,6 +122,7 @@ ImplToolItem::ImplToolItem( sal_uInt16 nItemId, const OUString& rText,
 ImplToolItem::ImplToolItem( sal_uInt16 nItemId, const Image& rImage,
                             const OUString& rText, ToolBoxItemBits nItemBits ) :
     maImage( rImage ),
+    maImageOriginal( rImage ),
     maText( rText )
 {
     init(nItemId, nItemBits, false);
@@ -130,6 +132,7 @@ ImplToolItem::ImplToolItem( const ImplToolItem& rItem ) :
         mpWindow                ( rItem.mpWindow ),
         mpUserData              ( rItem.mpUserData ),
         maImage                 ( rItem.maImage ),
+        maImageOriginal         ( rItem.maImageOriginal ),
         mnImageAngle            ( rItem.mnImageAngle ),
         mbMirrorMode            ( rItem.mbMirrorMode ),
         maText                  ( rItem.maText ),
@@ -168,6 +171,7 @@ ImplToolItem& ImplToolItem::operator=( const ImplToolItem& rItem )
     mpWindow                = rItem.mpWindow;
     mpUserData              = rItem.mpUserData;
     maImage                 = rItem.maImage;
+    maImageOriginal         = rItem.maImageOriginal;
     mnImageAngle            = rItem.mnImageAngle;
     mbMirrorMode            = rItem.mbMirrorMode;
     maText                  = rItem.maText;
@@ -484,6 +488,7 @@ void ToolBox::InsertItem( const ResId& rResId )
         Bitmap aBmp = Bitmap( ResId( static_cast<RSHEADER_TYPE*>(GetClassRes()), *rResId.GetResMgr() ) );
         IncrementRes( GetObjSizeRes( static_cast<RSHEADER_TYPE*>(GetClassRes()) ) );
         aItem.maImage = Image( aBmp, IMAGE_STDBTN_COLOR );
+        aItem.maImageOriginal = aItem.maImage;
         bImage = true;
     }
     if ( nObjMask & RSC_TOOLBOXITEM_IMAGE )
@@ -506,7 +511,10 @@ void ToolBox::InsertItem( const ResId& rResId )
 
     // if no image is loaded, try to load one from the image list
     if ( !bImage && aItem.mnId )
+    {
         aItem.maImage = maImageList.GetImage( aItem.mnId );
+        aItem.maImageOriginal = aItem.maImage;
+    }
 
     // if this is a ButtonItem, check ID
     bool bNewCalc;