LibreOffice/core

commit 73f4a06c0bce51c7c8b9ae9adfdc7ffac27d06b4

sw table styles: Insert column, split table, delete row/column.
Make these operations applying the table style too.

Change-Id: Iee2808a5a5c5f84538474b8fbc6bf53d1fab19b7

sw/inc/fesh.hxx
@@ -724,7 +724,8 @@ public:
     bool SetTableStyle(const SwTableAutoFormat& rNew);
 
     /// Update the direct formatting according to the current table style.
-    bool UpdateTableStyleFormatting();
+    /// @param pTableNode Table node to update.  When nullptr, current cursor position is used.
+    bool UpdateTableStyleFormatting(SwTableNode *pTableNode = nullptr);
 
     bool GetTableAutoFormat( SwTableAutoFormat& rGet );
 

sw/source/core/docnode/ndtbl.cxx
@@ -2104,8 +2104,12 @@ bool SwDoc::DeleteRowCol( const SwSelBoxes& rBoxes, bool bColumn )
             pTableNd->DelFrms();
             getIDocumentContentOperations().DeleteSection( pTableNd );
         }
+
+        GetDocShell()->GetFEShell()->UpdateTableStyleFormatting();
+
         getIDocumentState().SetModified();
         getIDocumentFieldsAccess().SetFieldsDirty( true, NULL, 0 );
+
         return true;
     }
 
@@ -2135,6 +2139,8 @@ bool SwDoc::DeleteRowCol( const SwSelBoxes& rBoxes, bool bColumn )
         bRet = rTable.DeleteSel( this, aSelBoxes, 0, pUndo, true, true );
         if (bRet)
         {
+            GetDocShell()->GetFEShell()->UpdateTableStyleFormatting();
+
             getIDocumentState().SetModified();
             getIDocumentFieldsAccess().SetFieldsDirty( true, NULL, 0 );
         }
@@ -2203,6 +2209,8 @@ bool SwDoc::SplitTable( const SwSelBoxes& rBoxes, bool bVert, sal_uInt16 nCnt,
 
         if (bRet)
         {
+            GetDocShell()->GetFEShell()->UpdateTableStyleFormatting();
+
             getIDocumentState().SetModified();
             getIDocumentFieldsAccess().SetFieldsDirty( true, NULL, 0 );
         }
@@ -2310,6 +2318,7 @@ sal_uInt16 SwDoc::MergeTable( SwPaM& rPam )
         if( pTableNd->GetTable().Merge( this, aBoxes, aMerged, pMergeBox, pUndo ))
         {
             nRet = TBLMERGE_OK;
+
             getIDocumentState().SetModified();
             getIDocumentFieldsAccess().SetFieldsDirty( true, NULL, 0 );
             if( pUndo )
@@ -3257,6 +3266,10 @@ bool SwDoc::SplitTable( const SwPosition& rPos, sal_uInt16 eHdlnMode,
     // TL_CHART2: need to inform chart of probably changed cell names
     UpdateCharts( rTable.GetFrameFormat()->GetName() );
 
+    // update table style formatting of both the tables
+    GetDocShell()->GetFEShell()->UpdateTableStyleFormatting(pTNd);
+    GetDocShell()->GetFEShell()->UpdateTableStyleFormatting(pNew);
+
     getIDocumentFieldsAccess().SetFieldsDirty( true, NULL, 0 );
 
     return 0 != pNew;
@@ -3457,6 +3470,8 @@ SwTableNode* SwNodes::SplitTable( const SwNodeIndex& rPos, bool bAfter,
         *pNewTableFormat = *pOldTableFormat;
         pNewTableNd->GetTable().RegisterToFormat( *pNewTableFormat );
 
+        pNewTableNd->GetTable().SetTableStyleName(rTable.GetTableStyleName());
+
         // Calculate a new Size?
         // lcl_ChgTableSize: Only execute the second call if the first call was
         // successful, thus has an absolute Size
@@ -3527,6 +3542,8 @@ bool SwDoc::MergeTable( const SwPosition& rPos, bool bWithPrev, sal_uInt16 nMode
     }
     if( bRet )
     {
+        GetDocShell()->GetFEShell()->UpdateTableStyleFormatting();
+
         getIDocumentState().SetModified();
         getIDocumentFieldsAccess().SetFieldsDirty( true, NULL, 0 );
     }

sw/source/core/frmedt/fetab.cxx
@@ -1181,22 +1181,25 @@ bool SwFEShell::SetTableStyle(const SwTableAutoFormat& rStyle)
     // make sure SwDoc has the style
     GetDoc()->GetTableStyles().AddAutoFormat(rStyle);
 
-    SwTableNode *pTableNd = const_cast<SwTableNode*>(IsCrsrInTable());
-    if (!pTableNd)
+    SwTableNode *pTableNode = const_cast<SwTableNode*>(IsCrsrInTable());
+    if (!pTableNode)
         return false;
 
     // set the name & update
-    pTableNd->GetTable().SetTableStyleName(rStyle.GetName());
-    return UpdateTableStyleFormatting();
+    pTableNode->GetTable().SetTableStyleName(rStyle.GetName());
+    return UpdateTableStyleFormatting(pTableNode);
 }
 
-bool SwFEShell::UpdateTableStyleFormatting()
+bool SwFEShell::UpdateTableStyleFormatting(SwTableNode *pTableNode)
 {
-    SwTableNode *pTableNd = const_cast<SwTableNode*>(IsCrsrInTable());
-    if( !pTableNd || pTableNd->GetTable().IsTableComplex() )
-        return false;
+    if (!pTableNode)
+    {
+        pTableNode = const_cast<SwTableNode*>(IsCrsrInTable());
+        if (!pTableNode || pTableNode->GetTable().IsTableComplex())
+            return false;
+    }
 
-    OUString aTableStyleName(pTableNd->GetTable().GetTableStyleName());
+    OUString aTableStyleName(pTableNode->GetTable().GetTableStyleName());
     SwTableAutoFormat* pTableStyle = GetDoc()->GetTableStyles().FindAutoFormat(aTableStyleName);
     if (!pTableStyle)
         return false;
@@ -1211,7 +1214,7 @@ bool SwFEShell::UpdateTableStyleFormatting()
         ::GetTableSelCrs( *this, aBoxes );
     else
     {
-        const SwTableSortBoxes& rTBoxes = pTableNd->GetTable().GetTabSortBoxes();
+        const SwTableSortBoxes& rTBoxes = pTableNode->GetTable().GetTabSortBoxes();
         for (size_t n = 0; n < rTBoxes.size(); ++n)
         {
             SwTableBox* pBox = rTBoxes[ n ];

sw/source/core/doc/tblrwcl.cxx
@@ -550,6 +550,8 @@ bool SwTable::InsertCol( SwDoc* pDoc, const SwSelBoxes& rBoxes, sal_uInt16 nCnt,
         pPCD->AddRowCols( *this, rBoxes, nCnt, bBehind );
     pDoc->UpdateCharts( GetFrameFormat()->GetName() );
 
+    pDoc->GetDocShell()->GetFEShell()->UpdateTableStyleFormatting();
+
     return bRes;
 }
 
@@ -2094,6 +2096,8 @@ bool SwTable::MakeCopy( SwDoc* pInsDoc, const SwPosition& rPos,
     OSL_ENSURE( pTableNd, "Where is the TableNode now?" );
 
     pTableNd->GetTable().SetRowsToRepeat( GetRowsToRepeat() );
+
+    pNewTable->SetTableStyleName(pTableNd->GetTable().GetTableStyleName());
 
     if( IS_TYPE( SwDDETable, this ))
     {

sw/source/core/table/swtable.cxx
@@ -264,6 +264,7 @@ SwTable::SwTable( const SwTable& rTable )
     eTableChgMode( rTable.eTableChgMode ),
     nGrfsThatResize( 0 ),
     nRowsToRepeat( rTable.GetRowsToRepeat() ),
+    maTableStyleName(rTable.maTableStyleName),
     bModifyLocked( false ),
     bNewModel( rTable.bNewModel )
 {

zhirshya/lo-tblr

commit 57bec2cc27d1deca667bcb056c06d32193f32d07

tdf#98226: fix undo of table AutoFormat
The new call to SwTable::SetTableStyleName() was not recorded in
SwUndoTableAutoFormat and hence persisted even after Undo.

(regression from 73f4a06c0bce51c7c8b9ae9adfdc7ffac27d06b4)

Change-Id: Ia7f769dafa62f02ff8e4b0596b48266190c7a69b

sw/inc/doc.hxx
@@ -1246,7 +1246,7 @@ public:
 
     /// AutoFormat for table/table selection.
     /// @param bResetDirect Reset direct formatting that might be applied to the cells.
-    bool SetTableAutoFormat(const SwSelBoxes& rBoxes, const SwTableAutoFormat& rNew, bool bResetDirect = false);
+    bool SetTableAutoFormat(const SwSelBoxes& rBoxes, const SwTableAutoFormat& rNew, bool bResetDirect = false, bool isSetStyleName = false);
 
     // Query attributes.
     bool GetTableAutoFormat( const SwSelBoxes& rBoxes, SwTableAutoFormat& rGet );

sw/inc/fesh.hxx
@@ -727,7 +727,8 @@ public:
     /// Update the direct formatting according to the current table style.
     /// @param pTableNode Table node to update.  When nullptr, current cursor position is used.
     /// @param bResetDirect Reset direct formatting that might be applied to the cells.
-    bool UpdateTableStyleFormatting(SwTableNode *pTableNode = nullptr, bool bResetDirect = false);
+    /// @param pStyleName new style to apply
+    bool UpdateTableStyleFormatting(SwTableNode *pTableNode = nullptr, bool bResetDirect = false, OUString const* pStyleName = nullptr);
 
     bool GetTableAutoFormat( SwTableAutoFormat& rGet );
 

sw/source/core/docnode/ndtbl.cxx
@@ -3742,7 +3742,7 @@ static bool lcl_SetAFormatBox(FndBox_ & rBox, SetAFormatTabPara *pSetPara, bool 
     return true;
 }
 
-bool SwDoc::SetTableAutoFormat(const SwSelBoxes& rBoxes, const SwTableAutoFormat& rNew, bool bResetDirect)
+bool SwDoc::SetTableAutoFormat(const SwSelBoxes& rBoxes, const SwTableAutoFormat& rNew, bool bResetDirect, bool const isSetStyleName)
 {
     OSL_ENSURE( !rBoxes.empty(), "No valid Box list" );
     SwTableNode* pTableNd = const_cast<SwTableNode*>(rBoxes[0]->GetSttNd()->FindTableNode());
@@ -3779,6 +3779,11 @@ bool SwDoc::SetTableAutoFormat(const SwSelBoxes& rBoxes, const SwTableAutoFormat
         pUndo = new SwUndoTableAutoFormat( *pTableNd, rNew );
         GetIDocumentUndoRedo().AppendUndo(pUndo);
         GetIDocumentUndoRedo().DoUndo(false);
+    }
+
+    if (isSetStyleName)
+    {   // tdf#98226 do this here where undo can record it
+        pTableNd->GetTable().SetTableStyleName(rNew.GetName());
     }
 
     rNew.RestoreTableProperties(table);

sw/source/core/frmedt/fetab.cxx
@@ -1186,11 +1186,11 @@ bool SwFEShell::SetTableStyle(const SwTableAutoFormat& rStyle)
         return false;
 
     // set the name & update
-    pTableNode->GetTable().SetTableStyleName(rStyle.GetName());
-    return UpdateTableStyleFormatting(pTableNode);
+    return UpdateTableStyleFormatting(pTableNode, false, &rStyle.GetName());
 }
 
-bool SwFEShell::UpdateTableStyleFormatting(SwTableNode *pTableNode, bool bResetDirect)
+bool SwFEShell::UpdateTableStyleFormatting(SwTableNode *pTableNode,
+        bool bResetDirect, OUString const*const pStyleName)
 {
     if (!pTableNode)
     {
@@ -1199,7 +1199,9 @@ bool SwFEShell::UpdateTableStyleFormatting(SwTableNode *pTableNode, bool bResetD
             return false;
     }
 
-    OUString aTableStyleName(pTableNode->GetTable().GetTableStyleName());
+    OUString const aTableStyleName((pStyleName)
+            ? *pStyleName
+            : pTableNode->GetTable().GetTableStyleName());
     SwTableAutoFormat* pTableStyle = GetDoc()->GetTableStyles().FindAutoFormat(aTableStyleName);
     if (!pTableStyle)
         return false;
@@ -1227,7 +1229,8 @@ bool SwFEShell::UpdateTableStyleFormatting(SwTableNode *pTableNode, bool bResetD
     {
         SET_CURR_SHELL( this );
         StartAllAction();
-        bRet = GetDoc()->SetTableAutoFormat(aBoxes, *pTableStyle, bResetDirect);
+        bRet = GetDoc()->SetTableAutoFormat(
+                aBoxes, *pTableStyle, bResetDirect, pStyleName != nullptr);
         DELETEZ( pLastCols );
         DELETEZ( pLastRows );
         EndAllActionAndCall();

sw/source/core/inc/UndoTable.hxx
@@ -142,6 +142,7 @@ class SwUndoTableNumFormat;
 
 class SwUndoTableAutoFormat : public SwUndo
 {
+    OUString m_TableStyleName;
     sal_uLong nSttNode;
     SaveTable* pSaveTable;
     std::vector< std::shared_ptr<SwUndoTableNumFormat> > m_Undos;

sw/source/core/undo/untbl.cxx
@@ -1409,9 +1409,10 @@ void SwUndoAttrTable::RedoImpl(::sw::UndoRedoContext & rContext)
 // UndoObject for AutoFormat on Table
 SwUndoTableAutoFormat::SwUndoTableAutoFormat( const SwTableNode& rTableNd,
                                     const SwTableAutoFormat& rAFormat )
-    : SwUndo( UNDO_TABLE_AUTOFMT ),
-    nSttNode( rTableNd.GetIndex() ),
-    bSaveContentAttr( false )
+    : SwUndo( UNDO_TABLE_AUTOFMT )
+    , m_TableStyleName(rTableNd.GetTable().GetTableStyleName())
+    , nSttNode( rTableNd.GetIndex() )
+    , bSaveContentAttr( false )
     , m_nRepeatHeading(rTableNd.GetTable().GetRowsToRepeat())
 {
     pSaveTable = new SaveTable( rTableNd.GetTable() );
@@ -1444,6 +1445,12 @@ SwUndoTableAutoFormat::UndoRedo(bool const bUndo, ::sw::UndoRedoContext & rConte
     OSL_ENSURE( pTableNd, "no TableNode" );
 
     SwTable& table = pTableNd->GetTable();
+    if (table.GetTableStyleName() != m_TableStyleName)
+    {
+        OUString const temp(table.GetTableStyleName());
+        table.SetTableStyleName(m_TableStyleName);
+        m_TableStyleName = temp;
+    }
     SaveTable* pOrig = new SaveTable( table );
     // than go also over the ContentNodes of the EndBoxes and collect
     // all paragraph attributes