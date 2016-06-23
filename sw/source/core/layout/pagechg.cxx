LibreOffice/core

commit b415494bf0468b74318b61f114e2ff4ae68c00ee

clang-tidy modernize-loop-convert in sw
Change-Id: I1f4a0ad6658bd3154c48940296aa8edc1ea1612c
Reviewed-on: https://gerrit.libreoffice.org/24876
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

sw/source/core/layout/pagechg.cxx
@@ -236,9 +236,8 @@ void SwPageFrame::DestroyImpl()
     {
         // Objects can be anchored at pages that are before their anchors (why ever...).
         // In such cases, we would access already freed memory.
-        for ( size_t i = 0; i < m_pSortedObjs->size(); ++i )
+        for (SwAnchoredObject* pAnchoredObj : *m_pSortedObjs)
         {
-            SwAnchoredObject* pAnchoredObj = (*m_pSortedObjs)[i];
             pAnchoredObj->SetPageFrame( nullptr );
         }
         delete m_pSortedObjs;
@@ -910,10 +909,9 @@ static void lcl_PrepFlyInCntRegister( SwContentFrame *pFrame )
     pFrame->Prepare( PREP_REGISTER );
     if( pFrame->GetDrawObjs() )
     {
-        for( size_t i = 0; i < pFrame->GetDrawObjs()->size(); ++i )
+        for(SwAnchoredObject* pAnchoredObj : *pFrame->GetDrawObjs())
         {
             // #i28701#
-            SwAnchoredObject* pAnchoredObj = (*pFrame->GetDrawObjs())[i];
             if ( dynamic_cast< const SwFlyInContentFrame *>( pAnchoredObj ) !=  nullptr )
             {
                 SwFlyFrame* pFly = static_cast<SwFlyInContentFrame*>(pAnchoredObj);
@@ -940,10 +938,9 @@ void SwPageFrame::PrepareRegisterChg()
     }
     if( GetSortedObjs() )
     {
-        for( size_t i = 0; i < GetSortedObjs()->size(); ++i )
+        for(SwAnchoredObject* pAnchoredObj : *GetSortedObjs())
         {
             // #i28701#
-            SwAnchoredObject* pAnchoredObj = (*GetSortedObjs())[i];
             if ( dynamic_cast< const SwFlyFrame *>( pAnchoredObj ) !=  nullptr )
             {
                 SwFlyFrame *pFly = static_cast<SwFlyFrame*>(pAnchoredObj);
@@ -1767,9 +1764,8 @@ static void lcl_MoveAllLowerObjs( SwFrame* pFrame, const Point& rOffset )
 
     // note: pSortedObj elements may be removed and inserted from
     // MoveObjectIfActive(), invalidating iterators
-    for (size_t i = 0; i < pSortedObj->size(); ++i)
+    for (SwAnchoredObject* pAnchoredObj : *pSortedObj)
     {
-        SwAnchoredObject *const pAnchoredObj = (*pSortedObj)[i];
         const SwFrameFormat& rObjFormat = pAnchoredObj->GetFrameFormat();
         const SwFormatAnchor& rAnchor = rObjFormat.GetAnchor();
 

zhirshya/lo-tblr

commit 05f1d49a8de53248f494bb1d7a7d10481cb6b060

sw: fix STL asserts in lcl_MoveAllLowerObjs *again*
Comment added in cf91483690291272f48ff95c1aebd165da8ae4f0 evidently
insufficient.

(regression from b415494bf0468b74318b61f114e2ff4ae68c00ee)

Change-Id: I9a017aefc3a77b0760fcdc3b8e43ff7c6ac516ac

sw/source/core/layout/pagechg.cxx
@@ -1763,8 +1763,10 @@ static void lcl_MoveAllLowerObjs( SwFrame* pFrame, const Point& rOffset )
 
     // note: pSortedObj elements may be removed and inserted from
     // MoveObjectIfActive(), invalidating iterators
-    for (SwAnchoredObject* pAnchoredObj : *pSortedObj)
+    // DO NOT CONVERT THIS TO A C++11 FOR LOOP, IT DID NOT WORK THE LAST 2 TIMES
+    for (size_t i = 0; i < pSortedObj->size(); ++i)
     {
+        SwAnchoredObject *const pAnchoredObj = (*pSortedObj)[i];
         const SwFrameFormat& rObjFormat = pAnchoredObj->GetFrameFormat();
         const SwFormatAnchor& rAnchor = rObjFormat.GetAnchor();
 