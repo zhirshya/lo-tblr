LibreOffice/core

commit c42be58e4c006ab3e1f262b644789cba8659d0ab

Show the certificate use also in the View Certificate dialog
Change-Id: I25ba55456be62307348eec529bfd434f8a90bac4

xmlsecurity/source/dialogs/certificateviewer.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <xmlsecurity/certificatechooser.hxx>
 #include <xmlsecurity/certificateviewer.hxx>
 #include <com/sun/star/security/XCertificate.hpp>
 
@@ -277,6 +278,11 @@ CertificateViewerDetailsTP::CertificateViewerDetailsTP( vcl::Window* _pParent, C
 
     aLBEntry = aDetails = xCert->getSignatureAlgorithm();
     InsertElement( XMLSEC_RES( STR_SIGNATURE_ALGO ), aLBEntry, aDetails );
+
+    CertificateChooser* pChooser = dynamic_cast<CertificateChooser*>(mpDlg->GetParent());
+    assert(pChooser);
+    aLBEntry = pChooser->UsageInClearText( mpDlg->mxCert->getCertificateUsage() );
+    InsertElement( XMLSEC_RES( STR_USE ), aLBEntry, aLBEntry );
 
     aSeq = xCert->getSHA1Thumbprint();
     aLBEntry = XmlSec::GetHexString( aSeq, pHexSep );

xmlsecurity/inc/xmlsecurity/certificatechooser.hxx
@@ -64,7 +64,6 @@ private:
     void ImplInitialize();
 
     void HandleOneUsageBit(OUString& string, int& bits, int bit, const char *name);
-    OUString UsageInClearText(int bits);
 
 public:
     CertificateChooser(vcl::Window* pParent, css::uno::Reference< css::uno::XComponentContext>& rxCtx, css::uno::Reference< css::xml::crypto::XSecurityEnvironment >& rxSecurityEnvironment);
@@ -77,6 +76,7 @@ public:
     /// Gets the description string provided when selecting the certificate.
     OUString GetDescription();
 
+    OUString UsageInClearText(int bits);
 };
 
 #endif // INCLUDED_XMLSECURITY_INC_XMLSECURITY_CERTIFICATECHOOSER_HXX

xmlsecurity/source/dialogs/certificateviewer.src
@@ -86,6 +86,11 @@ String STR_SIGNATURE_ALGO
     Text [ en-US ] = "Signature Algorithm";
 };
 
+String STR_USE
+{
+    Text [ en-US ] = "Certificate Use";
+};
+
 String STR_THUMBPRINT_SHA1
 {
     Text [ en-US ] = "Thumbprint SHA1";

xmlsecurity/source/dialogs/dialogs.hrc
@@ -38,6 +38,7 @@
 #define STR_SIGNATURE_ALGO              266
 #define STR_THUMBPRINT_SHA1             267
 #define STR_THUMBPRINT_MD5              268
+#define STR_USE                         271
 
 // --------- general metrics ---------
 #define DLGS_WIDTH                      287

zhirshya/lo-tblr

commit 526ead6272c21f8165cc16a1e0db11df2e943f52

Certificate viewer dialog can have various kinds of parents
...like the "Security Warning: Domain Name Mismatch" WarnDialog (cf.
TITLE_UUI_SSLWARN_DOMAINMISMATCH in uui), not only a CertificateChooser.

Regression introduced with c42be58e4c006ab3e1f262b644789cba8659d0ab "Show the
certificate use also in the View Certificate dialog", for which this fix is
hopefully fine.

Change-Id: I6efced1ebaad07c314fe7a5dd1cdc8e91d34555e

xmlsecurity/source/dialogs/certificateviewer.cxx
@@ -280,9 +280,11 @@ CertificateViewerDetailsTP::CertificateViewerDetailsTP( vcl::Window* _pParent, C
     InsertElement( XMLSEC_RES( STR_SIGNATURE_ALGO ), aLBEntry, aDetails );
 
     CertificateChooser* pChooser = dynamic_cast<CertificateChooser*>(mpDlg->GetParent());
-    assert(pChooser);
-    aLBEntry = pChooser->UsageInClearText( mpDlg->mxCert->getCertificateUsage() );
-    InsertElement( XMLSEC_RES( STR_USE ), aLBEntry, aLBEntry );
+    if (pChooser)
+    {
+        aLBEntry = pChooser->UsageInClearText( mpDlg->mxCert->getCertificateUsage() );
+        InsertElement( XMLSEC_RES( STR_USE ), aLBEntry, aLBEntry );
+    }
 
     aSeq = xCert->getSHA1Thumbprint();
     aLBEntry = XmlSec::GetHexString( aSeq, pHexSep );