LibreOffice/core

commit c9ed6e832213129cc579c1c309ce9dbd3593c57a

tdf#95339 Extend test to distinguish minute/month
Detect minute in user number format such as SSMM or MMHH

Change-Id: Iadb29dabba167b8f59903ff05e7367857378738a
Reviewed-on: https://gerrit.libreoffice.org/25222
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

svl/source/numbers/zforscan.cxx
@@ -1152,6 +1152,10 @@ sal_Int32 ImpSvNumberformatScan::ScanType()
                 cChar = PreviousChar(i);
                 if (nIndexPre == NF_KEY_H   ||      // H
                     nIndexPre == NF_KEY_HH  ||      // HH
+                    nIndexPre == NF_KEY_S   ||      // S before M tdf#95339
+                    nIndexPre == NF_KEY_SS  ||      // SS
+                    nIndexNex == NF_KEY_H   ||      // H after M tdf#95339
+                    nIndexNex == NF_KEY_HH  ||      // HH
                     nIndexNex == NF_KEY_S   ||      // S
                     nIndexNex == NF_KEY_SS  ||      // SS
                     cChar == '['  )                 // [M

zhirshya/lo-tblr

commit 396a95edf78bdf0059badd4ad13fe6b364aedc0b

Resolves: tdf#101096 remove the "H after M" rule
wrongly introduced with c9ed6e832213129cc579c1c309ce9dbd3593c57a

Change-Id: Ic6c9c404ad0af11eb5d638f453150ee0da60e633

svl/source/numbers/zforscan.cxx
@@ -1146,7 +1146,13 @@ sal_Int32 ImpSvNumberformatScan::ScanType()
                 break;
             case NF_KEY_M:                          // M
             case NF_KEY_MM:                         // MM
-                // minute or month
+                /* Minute or month.
+                   Minute if one of:
+                   * preceded by time keyword H or S (ignoring separators)
+                   * followed by time keyword S (ignoring separators)
+                   * preceded by '[' amount bracket
+                   Else month.
+                */
                 nIndexPre = PreviousKeyword(i);
                 nIndexNex = NextKeyword(i);
                 cChar = PreviousChar(i);
@@ -1154,8 +1160,6 @@ sal_Int32 ImpSvNumberformatScan::ScanType()
                     nIndexPre == NF_KEY_HH  ||      // HH
                     nIndexPre == NF_KEY_S   ||      // S before M tdf#95339
                     nIndexPre == NF_KEY_SS  ||      // SS
-                    nIndexNex == NF_KEY_H   ||      // H after M tdf#95339
-                    nIndexNex == NF_KEY_HH  ||      // HH
                     nIndexNex == NF_KEY_S   ||      // S
                     nIndexNex == NF_KEY_SS  ||      // SS
                     cChar == '['  )                 // [M