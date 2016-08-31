LibreOffice/core

commit bcb41235deaf4b7ca90522bda3ba21a686819e6e

convert SfxStyleFamily to scoped enum
and update the RSC compiler to accept such

In the process fix some confusion in SD where it was confusing
SfxStyleFamily and the index of the relevant family
(which other parts of the code in SVL use)

Change-Id: I1efc9f85fbed8ab76eafe8f6e1ada411753ae5f9

sc/source/ui/view/formatsh.cxx
@@ -196,7 +196,7 @@ void ScFormatShell::GetStyleState( SfxItemSet& rSet )
                 SCTAB           nCurTab     = GetViewData()->GetTabNo();
                 OUString        aPageStyle  = pDoc->GetPageStyle( nCurTab );
                 SfxStyleSheet*  pStyleSheet = pStylePool ? static_cast<SfxStyleSheet*>(pStylePool->
-                                    Find( aPageStyle, SFX_STYLE_FAMILY_PAGE )) : nullptr;
+                                    Find( aPageStyle, SfxStyleFamily::Page )) : nullptr;
 
                 if ( pStyleSheet )
                     rSet.Put( SfxTemplateItem( nSlotId, aPageStyle ) );
@@ -217,7 +217,7 @@ void ScFormatShell::GetStyleState( SfxItemSet& rSet )
                 pTabViewShell->GetViewFrame()->GetBindings().QueryState(SID_STYLE_FAMILY, pItem);
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
 
-                bool bPage = pFamilyItem && SFX_STYLE_FAMILY_PAGE == SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue());
+                bool bPage = pFamilyItem && SfxStyleFamily::Page == SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue());
 
                 if ( bProtected || bPage )
                     rSet.DisableItem( nSlotId );
@@ -232,7 +232,7 @@ void ScFormatShell::GetStyleState( SfxItemSet& rSet )
                 std::unique_ptr<SfxPoolItem> pItem;
                 pTabViewShell->GetViewFrame()->GetBindings().QueryState(SID_STYLE_FAMILY, pItem);
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
-                bool bPage = pFamilyItem && SFX_STYLE_FAMILY_PAGE == SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue());
+                bool bPage = pFamilyItem && SfxStyleFamily::Page == SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue());
 
                 if ( bProtected && !bPage )
                     rSet.DisableItem( nSlotId );
@@ -274,7 +274,7 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
     {
         if (nSlotId == SID_STYLE_PREVIEW)
         {
-            SfxStyleFamily eFamily = SFX_STYLE_FAMILY_PARA;
+            SfxStyleFamily eFamily = SfxStyleFamily::Para;
             const SfxPoolItem* pFamItem;
             if ( pArgs && SfxItemState::SET == pArgs->GetItemState( SID_STYLE_FAMILY, true, &pFamItem ) )
                 eFamily = (SfxStyleFamily) static_cast<const SfxUInt16Item*>(pFamItem)->GetValue();
@@ -282,7 +282,7 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
             OUString aStyleName;
             if (pArgs && SfxItemState::SET == pArgs->GetItemState( nSlotId, true, &pNameItem ))
                 aStyleName = static_cast<const SfxStringItem*>(pNameItem)->GetValue();
-            if ( eFamily == SFX_STYLE_FAMILY_PARA ) // CellStyles
+            if ( eFamily == SfxStyleFamily::Para ) // CellStyles
             {
                 ScMarkData aFuncMark( pViewData->GetMarkData() );
                 ScViewUtil::UnmarkFiltered( aFuncMark, &rDoc );
@@ -354,7 +354,7 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
         ScStyleSaveData aOldData;       // for undo/redo
         ScStyleSaveData aNewData;
 
-        SfxStyleFamily eFamily = SFX_STYLE_FAMILY_PARA;
+        SfxStyleFamily eFamily = SfxStyleFamily::Para;
         const SfxPoolItem* pFamItem;
         if ( pArgs && SfxItemState::SET == pArgs->GetItemState( SID_STYLE_FAMILY, true, &pFamItem ) )
             eFamily = (SfxStyleFamily) static_cast<const SfxUInt16Item*>(pFamItem)->GetValue();
@@ -362,9 +362,9 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
         {
             OUString sFamily = static_cast<const SfxStringItem*>(pFamItem)->GetValue();
             if (sFamily == "CellStyles")
-                eFamily = SFX_STYLE_FAMILY_PARA;
+                eFamily = SfxStyleFamily::Para;
             else if (sFamily == "PageStyles")
-                eFamily = SFX_STYLE_FAMILY_PAGE;
+                eFamily = SfxStyleFamily::Page;
         }
 
         OUString                aStyleName;
@@ -495,7 +495,7 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
 
         switch ( eFamily )
         {
-            case SFX_STYLE_FAMILY_PARA:
+            case SfxStyleFamily::Para:
             {
                 switch ( nSlotId )
                 {
@@ -652,10 +652,10 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
                     default:
                         break;
                 }
-            } // case SFX_STYLE_FAMILY_PARA:
+            } // case SfxStyleFamily::Para:
             break;
 
-            case SFX_STYLE_FAMILY_PAGE:
+            case SfxStyleFamily::Page:
             {
                 switch ( nSlotId )
                 {
@@ -764,7 +764,7 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
                     default:
                         break;
                 } // switch ( nSlotId )
-            } // case SFX_STYLE_FAMILY_PAGE:
+            } // case SfxStyleFamily::Page:
             break;
 
             default:
@@ -786,11 +786,11 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
 
                 switch ( eFam )
                 {
-                    case SFX_STYLE_FAMILY_PAGE:
+                    case SfxStyleFamily::Page:
                         nRsc = RID_SCDLG_STYLES_PAGE;
                         break;
 
-                    case SFX_STYLE_FAMILY_PARA:
+                    case SfxStyleFamily::Para:
                     default:
                         {
                             SfxItemSet& rSet = pStyleSheet->GetItemSet();
@@ -876,7 +876,7 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
 
                         // Attribute comparisons (earlier in ModifyStyleSheet) now here
                         // with the old values (the style is already changed)
-                        if ( SFX_STYLE_FAMILY_PARA == eFam )
+                        if ( SfxStyleFamily::Para == eFam )
                         {
                             SfxItemSet& rNewSet = pStyleSheet->GetItemSet();
                             bool bNumFormatChanged;
@@ -923,7 +923,7 @@ void ScFormatShell::ExecuteStyle( SfxRequest& rReq )
 
                         pDocSh->SetDocumentModified();
 
-                        if ( SFX_STYLE_FAMILY_PARA == eFam )
+                        if ( SfxStyleFamily::Para == eFam )
                         {
                             ScTabViewShell::UpdateNumberFormatter(
                                 static_cast<const SvxNumberInfoItem&>(

sd/source/ui/view/outlnvsh.cxx
@@ -417,7 +417,7 @@ void OutlineViewShell::GetCtrlState(SfxItemSet &rSet)
 void OutlineViewShell::FuSupport(SfxRequest &rReq)
 {
     if( rReq.GetSlot() == SID_STYLE_FAMILY && rReq.GetArgs())
-        GetDocSh()->SetStyleFamily(static_cast<const SfxUInt16Item&>(rReq.GetArgs()->Get( SID_STYLE_FAMILY )).GetValue());
+        GetDocSh()->SetStyleFamily((SfxStyleFamily) static_cast<const SfxUInt16Item&>(rReq.GetArgs()->Get( SID_STYLE_FAMILY )).GetValue());
 
     bool bPreviewState = false;
     sal_uLong nSlot = rReq.GetSlot();

sd/source/ui/view/viewshe3.cxx
@@ -103,9 +103,7 @@ void  ViewShell::GetMenuState( SfxItemSet &rSet )
 
                 if( pStyleSheet )
                 {
-                    SfxStyleFamily eFamily = pStyleSheet->GetFamily();
-                    nFamily = SfxTemplate::SfxFamilyIdToNId(eFamily);
-                    GetDocSh()->SetStyleFamily(nFamily);
+                    GetDocSh()->SetStyleFamily(pStyleSheet->GetFamily());
                 }
             }
         }

sfx2/source/dialog/templdlg.cxx
@@ -236,7 +236,7 @@ sal_Int8 DropListBox_Impl::AcceptDrop( const AcceptDropEvent& rEvt )
     {
         // special case: page styles are allowed to create new styles by example
         // but not allowed to be created by drag and drop
-        if (pDialog->GetActualFamily() == SFX_STYLE_FAMILY_PAGE ||
+        if (pDialog->GetActualFamily() == SfxStyleFamily::Page ||
                 pDialog->bNewByExampleDisabled)
             return DND_ACTION_NONE;
         else
@@ -744,17 +744,17 @@ void SfxCommonTemplateDialog_Impl::ReadResource()
     for (i = 0; i < nCount; ++i)
     {
         sal_uInt16 nSlot = 0;
-        switch( (sal_uInt16)pStyleFamilies->at( i )->GetFamily() )
+        switch( pStyleFamilies->at( i )->GetFamily() )
         {
-            case SFX_STYLE_FAMILY_CHAR:
+            case SfxStyleFamily::Char:
                 nSlot = SID_STYLE_FAMILY1; break;
-            case SFX_STYLE_FAMILY_PARA:
+            case SfxStyleFamily::Para:
                 nSlot = SID_STYLE_FAMILY2; break;
-            case SFX_STYLE_FAMILY_FRAME:
+            case SfxStyleFamily::Frame:
                 nSlot = SID_STYLE_FAMILY3; break;
-            case SFX_STYLE_FAMILY_PAGE:
+            case SfxStyleFamily::Page:
                 nSlot = SID_STYLE_FAMILY4; break;
-            case SFX_STYLE_FAMILY_PSEUDO:
+            case SfxStyleFamily::Pseudo:
                 nSlot = SID_STYLE_FAMILY5; break;
             default: OSL_FAIL("unknown StyleFamily"); break;
         }
@@ -890,11 +890,11 @@ namespace SfxTemplate
     {
         switch ( nFamily )
         {
-            case SFX_STYLE_FAMILY_CHAR:   return 1;
-            case SFX_STYLE_FAMILY_PARA:   return 2;
-            case SFX_STYLE_FAMILY_FRAME:  return 3;
-            case SFX_STYLE_FAMILY_PAGE:   return 4;
-            case SFX_STYLE_FAMILY_PSEUDO: return 5;
+            case SfxStyleFamily::Char:   return 1;
+            case SfxStyleFamily::Para:   return 2;
+            case SfxStyleFamily::Frame:  return 3;
+            case SfxStyleFamily::Page:   return 4;
+            case SfxStyleFamily::Pseudo: return 5;
             default:                      return 0;
         }
     }
@@ -903,12 +903,12 @@ namespace SfxTemplate
     {
         switch (nId)
         {
-            case 1: return SFX_STYLE_FAMILY_CHAR;
-            case 2: return SFX_STYLE_FAMILY_PARA;
-            case 3: return SFX_STYLE_FAMILY_FRAME;
-            case 4: return SFX_STYLE_FAMILY_PAGE;
-            case 5: return SFX_STYLE_FAMILY_PSEUDO;
-            default: return SFX_STYLE_FAMILY_ALL;
+            case 1: return SfxStyleFamily::Char;
+            case 2: return SfxStyleFamily::Para;
+            case 3: return SfxStyleFamily::Frame;
+            case 4: return SfxStyleFamily::Page;
+            case 5: return SfxStyleFamily::Pseudo;
+            default: return SfxStyleFamily::All;
         }
     }
 }
@@ -2157,7 +2157,7 @@ SfxStyleFamily SfxCommonTemplateDialog_Impl::GetActualFamily() const
 {
     const SfxStyleFamilyItem *pFamilyItem = GetFamilyItem_Impl();
     if( !pFamilyItem || nActFamily == 0xffff )
-        return SFX_STYLE_FAMILY_PARA;
+        return SfxStyleFamily::Para;
     else
         return pFamilyItem->GetFamily();
 }
@@ -2277,13 +2277,13 @@ void SfxTemplateDialog_Impl::EnableFamilyItem( sal_uInt16 nId, bool bEnable )
 void SfxTemplateDialog_Impl::InsertFamilyItem(sal_uInt16 nId,const SfxStyleFamilyItem *pItem)
 {
     OString sHelpId;
-    switch( (sal_uInt16) pItem->GetFamily() )
+    switch( pItem->GetFamily() )
     {
-        case SFX_STYLE_FAMILY_CHAR:     sHelpId = ".uno:CharStyle"; break;
-        case SFX_STYLE_FAMILY_PARA:     sHelpId = ".uno:ParaStyle"; break;
-        case SFX_STYLE_FAMILY_FRAME:    sHelpId = ".uno:FrameStyle"; break;
-        case SFX_STYLE_FAMILY_PAGE:     sHelpId = ".uno:PageStyle"; break;
-        case SFX_STYLE_FAMILY_PSEUDO:   sHelpId = ".uno:ListStyle"; break;
+        case SfxStyleFamily::Char:     sHelpId = ".uno:CharStyle"; break;
+        case SfxStyleFamily::Para:     sHelpId = ".uno:ParaStyle"; break;
+        case SfxStyleFamily::Frame:    sHelpId = ".uno:FrameStyle"; break;
+        case SfxStyleFamily::Page:     sHelpId = ".uno:PageStyle"; break;
+        case SfxStyleFamily::Pseudo:   sHelpId = ".uno:ListStyle"; break;
         default: OSL_FAIL("unknown StyleFamily"); break;
     }
     m_aActionTbL->InsertItem( nId, pItem->GetImage(), pItem->GetText(), ToolBoxItemBits::NONE, 0);
@@ -2624,7 +2624,7 @@ sal_Int8 DropToolBox_Impl::AcceptDrop( const AcceptDropEvent& rEvt )
     }
     // special case: page styles are allowed to create new styles by example
     // but not allowed to be created by drag and drop
-    if ( nItemId != SfxTemplate::SfxFamilyIdToNId( SFX_STYLE_FAMILY_PAGE )&&
+    if ( nItemId != SfxTemplate::SfxFamilyIdToNId( SfxStyleFamily::Page )&&
         IsDropFormatSupported( SotClipboardFormatId::OBJECTDESCRIPTOR ) &&
         !rParent.bNewByExampleDisabled )
     {

zhirshya/lo-tblr

commit 0ef1efe31b47f2d49f4a2c16f2554ca189b98728

let's make Impress crash less - SfxStyleFamily edition
Commit bcb41235deaf4b7ca90522bda3ba21a686819e6e - in addition to
introducing the enum SfxStyleFamily - inconsistently converted one
call of DrawDocShell::SetStyleFamily to use enum SfxStyleFamily
instead of a mysterious "5", but did not adapt all of the code that
extracts the SfxUInt16Item that subsequently extracts this value and
expects a number 0-5.

Since it's clearly inexcusably stupid to have 2 different public sets
of integers identifying styles, make the usage of the second "array
index" integers private to templdlg.cxx; the SfxUInt16 item now always
contains enum SfxStyleFamily.

(regression from bcb41235deaf4b7ca90522bda3ba21a686819e6e)

Change-Id: I333575c504277c2046f8f5a6b36ae3f86b3b3201

include/sfx2/templdlg.hxx
@@ -28,14 +28,6 @@
 #include <sfx2/childwin.hxx>
 #include <sfx2/basedlgs.hxx>
 
-namespace SfxTemplate
-{
-    // converts from SFX_STYLE_FAMILY Ids to 1-5
-    sal_uInt16 SFX2_DLLPUBLIC SfxFamilyIdToNId(SfxStyleFamily nFamily);
-    // converts from 1-5 to SFX_STYLE_FAMILY Ids
-    SfxStyleFamily SFX2_DLLPUBLIC NIdToSfxFamilyId(sal_uInt16 nId);
-}
-
 class SfxTemplateDialog_Impl;
 
 class SFX2_DLLPUBLIC SfxTemplatePanelControl : public vcl::Window

sc/source/ui/view/formatsh.cxx
@@ -217,7 +217,7 @@ void ScFormatShell::GetStyleState( SfxItemSet& rSet )
                 pTabViewShell->GetViewFrame()->GetBindings().QueryState(SID_STYLE_FAMILY, pItem);
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
 
-                bool bPage = pFamilyItem && SfxStyleFamily::Page == SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue());
+                bool bPage = pFamilyItem && SfxStyleFamily::Page == static_cast<SfxStyleFamily>(pFamilyItem->GetValue());
 
                 if ( bProtected || bPage )
                     rSet.DisableItem( nSlotId );
@@ -232,7 +232,7 @@ void ScFormatShell::GetStyleState( SfxItemSet& rSet )
                 std::unique_ptr<SfxPoolItem> pItem;
                 pTabViewShell->GetViewFrame()->GetBindings().QueryState(SID_STYLE_FAMILY, pItem);
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
-                bool bPage = pFamilyItem && SfxStyleFamily::Page == SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue());
+                bool bPage = pFamilyItem && SfxStyleFamily::Page == static_cast<SfxStyleFamily>(pFamilyItem->GetValue());
 
                 if ( bProtected && !bPage )
                     rSet.DisableItem( nSlotId );

sd/source/ui/view/drviewsf.cxx
@@ -491,7 +491,7 @@ void DrawViewShell::GetAttrState( SfxItemSet& rSet )
                 std::unique_ptr<SfxPoolItem> pItem;
                 GetViewFrame()->GetBindings().QueryState(SID_STYLE_FAMILY, pItem);
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
-                if (pFamilyItem && SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
+                if (pFamilyItem && static_cast<SfxStyleFamily>(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
                     rSet.Put(SfxBoolItem(nWhich,false));
                 else
                 {
@@ -506,7 +506,7 @@ void DrawViewShell::GetAttrState( SfxItemSet& rSet )
                 std::unique_ptr<SfxPoolItem> pItem;
                 GetViewFrame()->GetBindings().QueryState(SID_STYLE_FAMILY, pItem);
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
-                if (pFamilyItem && SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
+                if (pFamilyItem && static_cast<SfxStyleFamily>(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
                 {
                     rSet.DisableItem(nWhich);
                 }
@@ -518,7 +518,7 @@ void DrawViewShell::GetAttrState( SfxItemSet& rSet )
                 std::unique_ptr<SfxPoolItem> pItem;
                 GetViewFrame()->GetBindings().QueryState(SID_STYLE_FAMILY, pItem);
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
-                if (pFamilyItem && SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
+                if (pFamilyItem && static_cast<SfxStyleFamily>(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
                     rSet.DisableItem(nWhich);
             }
             break;
@@ -533,11 +533,11 @@ void DrawViewShell::GetAttrState( SfxItemSet& rSet )
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
                 if (pFamilyItem)
                 {
-                    if (SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
+                    if (static_cast<SfxStyleFamily>(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
                     {
                         rSet.DisableItem(nWhich);
                     }
-                    else if (SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_GRAPHICS)
+                    else if (static_cast<SfxStyleFamily>(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_GRAPHICS)
                     {
                         if (!mpDrawView->AreObjectsMarked())
                         {

sd/source/ui/view/outlnvsh.cxx
@@ -1475,7 +1475,7 @@ void OutlineViewShell::GetAttrState( SfxItemSet& rSet )
                 std::unique_ptr<SfxPoolItem> pItem;
                 GetViewFrame()->GetBindings().QueryState(SID_STYLE_FAMILY, pItem);
                 SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
-                if (pFamilyItem && SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
+                if (pFamilyItem && static_cast<SfxStyleFamily>(pFamilyItem->GetValue()) == SD_STYLE_FAMILY_PSEUDO)
                 {
                     SfxItemSet aSet(*rSet.GetPool(), SID_STATUS_LAYOUT, SID_STATUS_LAYOUT);
                     GetStatusBarState(aSet);

sd/source/ui/view/viewshe3.cxx
@@ -90,7 +90,7 @@ void  ViewShell::GetMenuState( SfxItemSet &rSet )
 {
     if( SfxItemState::DEFAULT == rSet.GetItemState( SID_STYLE_FAMILY ) )
     {
-        sal_uInt16 nFamily = (sal_uInt16)GetDocSh()->GetStyleFamily();
+        SfxStyleFamily const nFamily = GetDocSh()->GetStyleFamily();
 
         SdrView* pDrView = GetDrawView();
 
@@ -109,7 +109,7 @@ void  ViewShell::GetMenuState( SfxItemSet &rSet )
             }
         }
 
-        rSet.Put(SfxUInt16Item(SID_STYLE_FAMILY, nFamily ));
+        rSet.Put(SfxUInt16Item(SID_STYLE_FAMILY, static_cast<sal_uInt16>(nFamily)));
     }
 
     if(SfxItemState::DEFAULT == rSet.GetItemState(SID_GETUNDOSTRINGS))

sfx2/source/dialog/templdlg.cxx
@@ -639,6 +639,40 @@ SvTreeListEntry* FillBox_Impl(SvTreeListBox* pBox,
     return pTreeListEntry;
 }
 
+
+namespace SfxTemplate
+{
+    // converts from SFX_STYLE_FAMILY Ids to 1-6
+    sal_uInt16 SfxFamilyIdToNId(SfxStyleFamily nFamily)
+    {
+        switch ( nFamily )
+        {
+            case SfxStyleFamily::Char:   return 1;
+            case SfxStyleFamily::Para:   return 2;
+            case SfxStyleFamily::Frame:  return 3;
+            case SfxStyleFamily::Page:   return 4;
+            case SfxStyleFamily::Pseudo: return 5;
+            case SfxStyleFamily::Table:  return 6;
+            default:                      return 0;
+        }
+    }
+
+    // converts from 1-6 to SFX_STYLE_FAMILY Ids
+    SfxStyleFamily NIdToSfxFamilyId(sal_uInt16 nId)
+    {
+        switch (nId)
+        {
+            case 1: return SfxStyleFamily::Char;
+            case 2: return SfxStyleFamily::Para;
+            case 3: return SfxStyleFamily::Frame;
+            case 4: return SfxStyleFamily::Page;
+            case 5: return SfxStyleFamily::Pseudo;
+            case 6: return SfxStyleFamily::Table;
+            default: return SfxStyleFamily::All;
+        }
+    }
+}
+
 // Constructor
 
 SfxCommonTemplateDialog_Impl::SfxCommonTemplateDialog_Impl( SfxBindings* pB, vcl::Window* pW, bool )
@@ -885,37 +919,6 @@ SfxCommonTemplateDialog_Impl::~SfxCommonTemplateDialog_Impl()
     aFmtLb.disposeAndClear();
     aFilterLb.disposeAndClear();
     aPreviewCheckbox.disposeAndClear();
-}
-
-namespace SfxTemplate
-{
-    sal_uInt16 SfxFamilyIdToNId(SfxStyleFamily nFamily)
-    {
-        switch ( nFamily )
-        {
-            case SfxStyleFamily::Char:   return 1;
-            case SfxStyleFamily::Para:   return 2;
-            case SfxStyleFamily::Frame:  return 3;
-            case SfxStyleFamily::Page:   return 4;
-            case SfxStyleFamily::Pseudo: return 5;
-            case SfxStyleFamily::Table:  return 6;
-            default:                      return 0;
-        }
-    }
-
-    SfxStyleFamily NIdToSfxFamilyId(sal_uInt16 nId)
-    {
-        switch (nId)
-        {
-            case 1: return SfxStyleFamily::Char;
-            case 2: return SfxStyleFamily::Para;
-            case 3: return SfxStyleFamily::Frame;
-            case 4: return SfxStyleFamily::Page;
-            case 5: return SfxStyleFamily::Pseudo;
-            case 6: return SfxStyleFamily::Table;
-            default: return SfxStyleFamily::All;
-        }
-    }
 }
 
 // Helper function: Access to the current family item
@@ -1701,12 +1704,14 @@ IMPL_LINK_TYPED( SfxCommonTemplateDialog_Impl, FilterSelectHdl, ListBox&, rBox, 
 // Select-Handler for the Toolbox
 void SfxCommonTemplateDialog_Impl::FamilySelect(sal_uInt16 nEntry, bool bPreviewRefresh)
 {
+    assert((0 < nEntry && nEntry <= MAX_FAMILIES) || 0xffff == nEntry);
     if( nEntry != nActFamily || bPreviewRefresh )
     {
         CheckItem( nActFamily, false );
         nActFamily = nEntry;
         SfxDispatcher* pDispat = pBindings->GetDispatcher_Impl();
-        SfxUInt16Item aItem( SID_STYLE_FAMILY, nEntry );
+        SfxUInt16Item const aItem(SID_STYLE_FAMILY,
+                static_cast<sal_uInt16>(SfxTemplate::NIdToSfxFamilyId(nEntry)));
         pDispat->ExecuteList(SID_STYLE_FAMILY, SfxCallMode::SYNCHRON, { &aItem });
         pBindings->Invalidate( SID_STYLE_FAMILY );
         pBindings->Update( SID_STYLE_FAMILY );
@@ -2563,8 +2568,10 @@ IMPL_LINK_TYPED( SfxTemplateDialog_Impl, MenuSelectHdl, Menu*, pMenu, bool)
     return false;
 }
 
-void SfxCommonTemplateDialog_Impl::SetFamily( sal_uInt16 nId )
+void SfxCommonTemplateDialog_Impl::SetFamily(SfxStyleFamily const nFamily)
 {
+    sal_uInt16 const nId(SfxTemplate::SfxFamilyIdToNId(nFamily));
+    assert((0 < nId && nId <= MAX_FAMILIES) || 0xffff == nId);
     if ( nId != nActFamily )
     {
         if ( nActFamily != 0xFFFF )

sfx2/source/dialog/tplcitem.cxx
@@ -148,7 +148,9 @@ void SfxTemplateControllerItem::StateChanged( sal_uInt16 nSID, SfxItemState eSta
         {
             const SfxUInt16Item *pStateItem = dynamic_cast< const SfxUInt16Item* >(pItem);
             if (pStateItem)
-                rTemplateDlg.SetFamily( pStateItem->GetValue() );
+            {
+                rTemplateDlg.SetFamily(static_cast<SfxStyleFamily>(pStateItem->GetValue()));
+            }
             break;
         }
     }

sfx2/source/inc/templdgi.hxx
@@ -274,7 +274,7 @@ protected:
     void Notify( SfxBroadcaster& rBC, const SfxHint& rHint ) override;
 
     void FamilySelect( sal_uInt16 nId, bool bPreviewRefresh = false );
-    void SetFamily( sal_uInt16 nId );
+    void SetFamily(SfxStyleFamily nFamily);
     void ActionSelect( sal_uInt16 nId );
 
     sal_Int32 LoadFactoryStyleFilter( SfxObjectShell* i_pObjSh );

sw/source/uibase/app/docst.cxx
@@ -114,7 +114,7 @@ void  SwDocShell::StateStyleSheet(SfxItemSet& rSet, SwWrtShell* pSh)
         SfxUInt16Item* pFamilyItem = dynamic_cast<SfxUInt16Item*>(pItem.get());
         if (pFamilyItem)
         {
-            nActualFamily = SfxTemplate::NIdToSfxFamilyId(pFamilyItem->GetValue());
+            nActualFamily = static_cast<SfxStyleFamily>(pFamilyItem->GetValue());
         }
     }
 