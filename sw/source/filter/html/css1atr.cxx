LibreOffice/core

commit 535971f3a166da6c0e44a6b7211c7f8c298f388f

Refactored IDocumentStylePoolAccess out of SwDoc.
Added non const SwDoc::GetDfltTxtFmtColl and SwDoc::GetTxtFmtColls
and SwDoc::GetCharFmts.
Had to make the manager friend of SwTxtFmtColl and SwConditionTxtFmtColl
Had to make SwDoc::_MakeCharFmt and SwDoc::_MakeFrmFmt public.

Change-Id: I19a36d91f92b3781a6c4313ec12eb8f7d4dfacc3

sw/source/filter/html/css1atr.cxx
@@ -564,7 +564,7 @@ void SwHTMLWriter::OutStyleSheet( const SwPageDesc& rPageDesc, bool bUsed )
         sal_uInt16 nPoolId = pColl->GetPoolFmtId();
         if( !bUsed || nPoolId == RES_POOLCOLL_TEXT ||
             pDoc->IsUsed( *pColl ) )
-            OutCSS1_SwFmt( *this, *pColl, pDoc, pTemplate );
+            OutCSS1_SwFmt( *this, *pColl, &pDoc->getIDocumentStylePoolAccess(), pTemplate );
     }
 
     // the Default-TextStyle is not also exported !!
@@ -576,7 +576,7 @@ void SwHTMLWriter::OutStyleSheet( const SwPageDesc& rPageDesc, bool bUsed )
         if( !bUsed || nPoolId == RES_POOLCHR_INET_NORMAL ||
             nPoolId == RES_POOLCHR_INET_VISIT ||
             pDoc->IsUsed( *pCFmt ) )
-            OutCSS1_SwFmt( *this, *pCFmt, pDoc, pTemplate );
+            OutCSS1_SwFmt( *this, *pCFmt, &pDoc->getIDocumentStylePoolAccess(), pTemplate );
     }
 
     const SwFtnIdxs& rIdxs = pDoc->GetFtnIdxs();
@@ -1512,16 +1512,16 @@ static Writer& OutCSS1_SwFmt( Writer& rWrt, const SwFmt& rFmt,
     switch( nDeep )
     {
     case CSS1_FMT_ISTAG:
-        pRefFmt = SwHTMLWriter::GetTemplateFmt( nRefPoolId, pTemplate );
+        pRefFmt = SwHTMLWriter::GetTemplateFmt( nRefPoolId, &pTemplate->getIDocumentStylePoolAccess() );
         break;
     case CSS1_FMT_CMPREF:
         pRefFmt = SwHTMLWriter::GetTemplateFmt( nRefPoolId, pDoc );
-        pRefFmtScript = SwHTMLWriter::GetTemplateFmt( nRefPoolId, pTemplate );
+        pRefFmtScript = SwHTMLWriter::GetTemplateFmt( nRefPoolId, &pTemplate->getIDocumentStylePoolAccess() );
         bClearSame = false;
         break;
     default:
         pRefFmt = SwHTMLWriter::GetParentFmt( rFmt, nDeep );
-        pRefFmtScript = SwHTMLWriter::GetTemplateFmt( nRefPoolId, pTemplate );
+        pRefFmtScript = SwHTMLWriter::GetTemplateFmt( nRefPoolId, &pTemplate->getIDocumentStylePoolAccess() );
         bSetDefaults = false;
         break;
     }
@@ -1639,7 +1639,7 @@ static Writer& OutCSS1_SwPageDesc( Writer& rWrt, const SwPageDesc& rPageDesc,
     if( !bExtRef )
         pRefPageDesc = pDoc->GetPageDescFromPool( nRefPoolId, false );
     else if( pTemplate )
-        pRefPageDesc = pTemplate->GetPageDescFromPool( nRefPoolId, false );
+        pRefPageDesc = pTemplate->getIDocumentStylePoolAccess().GetPageDescFromPool( nRefPoolId, false );
 
     OUString aSelector = "@" + OStringToOUString( sCSS1_page, RTL_TEXTENCODING_ASCII_US );
 
@@ -1764,7 +1764,7 @@ static Writer& OutCSS1_SwFtnInfo( Writer& rWrt, const SwEndNoteInfo& rInfo,
         // footnote and endnote template.
         if( nNotes == 0 && rHTMLWrt.pTemplate )
         {
-            SwFmt *pRefFmt = rHTMLWrt.pTemplate->GetCharFmtFromPool(
+            SwFmt *pRefFmt = rHTMLWrt.pTemplate->getIDocumentStylePoolAccess().GetCharFmtFromPool(
                         static_cast< sal_uInt16 >(bEndNote ? RES_POOLCHR_ENDNOTE : RES_POOLCHR_FOOTNOTE) );
             if( pRefFmt )
                 SwHTMLWriter::SubtractItemSet( aItemSet, pRefFmt->GetAttrSet(),

zhirshya/lo-tblr

commit 0ecf58fe26759c43a4741d244e3130c8a4e56512

pTemplate can probably be null here
Observed with "make clean && make CppunitTest_sw_filters_test" (which misses a
dependency, a bug to be fixed next, so happens to not set up m_pTemplate in
SwHTMLWriter::WriteStream, so dereferences a null pTemplate here.  Don't know
whether there's also legitimate scenarios that can lead to a null pTemplate
here, but at least the code in SwHTMLWriter::WriteStream setting up m_pTemplate
takes into account that it can be set up as null, and the code in the called
SwHTMLWriter::GetTemplateFormat takes into account that the passed in pTemplate
(which used to be the same object as the outer pTemplate/pDoc, before breaking
IDocumentStylePoolAccess out of SwDoc, see below) can be null.

Regression introduced with 535971f3a166da6c0e44a6b7211c7f8c298f388f "Refactored
IDocumentStylePoolAccess out of SwDoc."

Change-Id: Iabb4ae2ca018ce3a19756d62b9002b245cb409cd

sw/source/filter/html/css1atr.cxx
@@ -1561,16 +1561,16 @@ static Writer& OutCSS1_SwFormat( Writer& rWrt, const SwFormat& rFormat,
     switch( nDeep )
     {
     case CSS1_FMT_ISTAG:
-        pRefFormat = SwHTMLWriter::GetTemplateFormat( nRefPoolId, &pTemplate->getIDocumentStylePoolAccess() );
+        pRefFormat = SwHTMLWriter::GetTemplateFormat( nRefPoolId, pTemplate == nullptr ? nullptr : &pTemplate->getIDocumentStylePoolAccess() );
         break;
     case CSS1_FMT_CMPREF:
         pRefFormat = SwHTMLWriter::GetTemplateFormat( nRefPoolId, pDoc );
-        pRefFormatScript = SwHTMLWriter::GetTemplateFormat( nRefPoolId, &pTemplate->getIDocumentStylePoolAccess() );
+        pRefFormatScript = SwHTMLWriter::GetTemplateFormat( nRefPoolId, pTemplate == nullptr ? nullptr : &pTemplate->getIDocumentStylePoolAccess() );
         bClearSame = false;
         break;
     default:
         pRefFormat = SwHTMLWriter::GetParentFormat( rFormat, nDeep );
-        pRefFormatScript = SwHTMLWriter::GetTemplateFormat( nRefPoolId, &pTemplate->getIDocumentStylePoolAccess() );
+        pRefFormatScript = SwHTMLWriter::GetTemplateFormat( nRefPoolId, pTemplate == nullptr ? nullptr : &pTemplate->getIDocumentStylePoolAccess() );
         bSetDefaults = false;
         break;
     }