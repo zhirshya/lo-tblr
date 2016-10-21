LibreOffice/core

commit d7748842e96536daeb0c638c6f8d01d99b9a3e08

hide grid and axes options in chart elements if not supported, tdf#94297
Change-Id: I358340bd1b17f0e2eb5a119a60b623e489044dd3

chart2/source/controller/sidebar/ChartElementsPanel.cxx
@@ -22,6 +22,8 @@
 #include <sfx2/sidebar/ControlFactory.hxx>
 #include <com/sun/star/chart2/LegendPosition.hpp>
 #include <com/sun/star/chart/ChartLegendExpansion.hpp>
+#include <com/sun/star/chart2/XChartTypeContainer.hpp>
+#include <com/sun/star/chart2/XCoordinateSystemContainer.hpp>
 
 #include "ChartElementsPanel.hxx"
 #include "ChartController.hxx"
@@ -40,6 +42,7 @@
 #include "ChartModelHelper.hxx"
 #include "AxisHelper.hxx"
 #include "DiagramHelper.hxx"
+#include "ChartTypeHelper.hxx"
 
 #include "ChartModel.hxx"
 
@@ -297,6 +300,9 @@ ChartElementsPanel::ChartElementsPanel(
     get(mpCBGridVerticalMinor,  "checkbutton_gridline_vertical_minor");
     get(mpCBGridHorizontalMinor,  "checkbutton_gridline_horizontal_minor");
 
+    get(mpLBAxis, "label_axes");
+    get(mpLBGrid, "label_gri");
+
     get(mpLBLegendPosition, "comboboxtext_legend");
     get(mpBoxLegend, "box_legend");
 
@@ -316,7 +322,6 @@ ChartElementsPanel::~ChartElementsPanel()
 
 void ChartElementsPanel::dispose()
 {
-
     css::uno::Reference<css::util::XModifyBroadcaster> xBroadcaster(mxModel, css::uno::UNO_QUERY_THROW);
     xBroadcaster->removeModifyListener(mxListener);
     mpCBTitle.clear();
@@ -339,6 +344,9 @@ void ChartElementsPanel::dispose()
 
     mpLBLegendPosition.clear();
     mpBoxLegend.clear();
+
+    mpLBAxis.clear();
+    mpLBGrid.clear();
 
     mpTextTitle.clear();
     mpTextSubTitle.clear();
@@ -374,6 +382,26 @@ void ChartElementsPanel::Initialize()
     mpLBLegendPosition->SetSelectHdl(LINK(this, ChartElementsPanel, LegendPosHdl));
 }
 
+namespace {
+
+css::uno::Reference<css::chart2::XChartType> getChartType(css::uno::Reference<css::frame::XModel> xModel)
+{
+    css::uno::Reference<css::chart2::XChartDocument> xChartDoc(xModel, css::uno::UNO_QUERY_THROW);
+    css::uno::Reference<chart2::XDiagram > xDiagram = xChartDoc->getFirstDiagram();
+
+    css::uno::Reference<css::chart2::XCoordinateSystemContainer > xCooSysContainer( xDiagram, css::uno::UNO_QUERY_THROW );
+
+    css::uno::Sequence<css::uno::Reference<css::chart2::XCoordinateSystem>> xCooSysSequence(xCooSysContainer->getCoordinateSystems());
+
+    css::uno::Reference<css::chart2::XChartTypeContainer> xChartTypeContainer(xCooSysSequence[0], css::uno::UNO_QUERY_THROW);
+
+    css::uno::Sequence<css::uno::Reference<css::chart2::XChartType>> xChartTypeSequence(xChartTypeContainer->getChartTypes());
+
+    return xChartTypeSequence[0];
+}
+
+}
+
 void ChartElementsPanel::updateData()
 {
     if (!mbModelValid)
@@ -400,6 +428,40 @@ void ChartElementsPanel::updateData()
     mpCBZAxis->Check(isAxisVisible(mxModel, AxisType::Z_MAIN));
     mpCB2ndXAxis->Check(isAxisVisible(mxModel, AxisType::X_SECOND));
     mpCB2ndYAxis->Check(isAxisVisible(mxModel, AxisType::Y_SECOND));
+
+
+    bool bSupportsMainAxis = ChartTypeHelper::isSupportingMainAxis(
+            getChartType(mxModel), 0, 0);
+    if (bSupportsMainAxis)
+    {
+        mpCBXAxis->Show();
+        mpCBYAxis->Show();
+        mpCBZAxis->Show();
+        mpCBXAxisTitle->Show();
+        mpCBYAxisTitle->Show();
+        mpCBZAxisTitle->Show();
+        mpCBGridVerticalMajor->Show();
+        mpCBGridVerticalMinor->Show();
+        mpCBGridHorizontalMajor->Show();
+        mpCBGridHorizontalMinor->Show();
+        mpLBAxis->Show();
+        mpLBGrid->Show();
+    }
+    else
+    {
+        mpCBXAxis->Hide();
+        mpCBYAxis->Hide();
+        mpCBZAxis->Hide();
+        mpCBXAxisTitle->Hide();
+        mpCBYAxisTitle->Hide();
+        mpCBZAxisTitle->Hide();
+        mpCBGridVerticalMajor->Hide();
+        mpCBGridVerticalMinor->Hide();
+        mpCBGridHorizontalMajor->Hide();
+        mpCBGridHorizontalMinor->Hide();
+        mpLBAxis->Hide();
+        mpLBGrid->Hide();
+    }
 
     if (nDimension == 3)
     {

chart2/source/controller/sidebar/ChartElementsPanel.hxx
@@ -92,6 +92,8 @@ private:
     VclPtr<CheckBox> mpCBGridHorizontalMinor;
     VclPtr<FixedText> mpTextTitle;
     VclPtr<FixedText> mpTextSubTitle;
+    VclPtr<FixedText> mpLBAxis;
+    VclPtr<FixedText> mpLBGrid;
 
     VclPtr<ListBox> mpLBLegendPosition;
     VclPtr<VclHBox> mpBoxLegend;

zhirshya/lo-tblr

commit 1c6ff66ad2bcf4553142fa4858ccff2183fc7daa

Avoid further crash with empty Chart document
following up on 8b5f8c26f007c8e67bc46b1b09340cae660d0b8b "Avoid crash with empty
Chart document", when you then close the Writer doc (discarding the changes), it
crashes in a chart::sidebar::ChartSidebarModifyListener::disposing call whose
mpParent (that was a ChartElementsPanel) is already destroyed.  The problem is
that the ChartElementsPanel ctor creates an mxListener pointing to this, then
calls Initialize, which calls xBroadcaster->addModifyListener(mxListener) and
then, during updateData, throws a RuntimeException when xDiagram is null in
getChartType, causing the ChartElementsPanel object to immediately be destroyed,
but the listener still registered.  This apparently needs fixing independently
of the immediate problem.  (The immediate problem is a regression introduced
with d7748842e96536daeb0c638c6f8d01d99b9a3e08 "hide grid and axes options in
chart elements if not supported, tdf#94297".)

Change-Id: I2fe4f2a59600664f6210456c65e033f9453f4cc9

chart2/source/controller/sidebar/ChartElementsPanel.cxx
@@ -391,6 +391,9 @@ css::uno::Reference<css::chart2::XChartType> getChartType(const css::uno::Refere
 {
     css::uno::Reference<css::chart2::XChartDocument> xChartDoc(xModel, css::uno::UNO_QUERY_THROW);
     css::uno::Reference<chart2::XDiagram > xDiagram = xChartDoc->getFirstDiagram();
+    if (!xDiagram.is()) {
+        return css::uno::Reference<css::chart2::XChartType>();
+    }
 
     css::uno::Reference<css::chart2::XCoordinateSystemContainer > xCooSysContainer( xDiagram, css::uno::UNO_QUERY_THROW );
 