LibreOffice/core

commit 83d51e5e52688c4c9bc0ad70a511458bb06a242d

fdo#80748: Fix for file corruption due to framePr
 -For framePr Unit test is there in ooxmlexport.cxx
    -http://opengrok.libreoffice.org/xref/core/sw/qa/extras/ooxmlexport/ooxmlexport.cxx#3578

Change-Id: Ic1fd4c7a863d8ab57c6495e20bff6863eb941688
Reviewed-on: https://gerrit.libreoffice.org/10045
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

include/sax/fshelper.hxx
@@ -62,7 +62,7 @@ const sal_Int32 FSEND_internal = -1; // same as XML_TOKEN_INVALID
 
 namespace sax_fastparser {
 
-enum MergeMarksEnum { MERGE_MARKS_APPEND = 0, MERGE_MARKS_PREPEND = 1, MERGE_MARKS_POSTPONE = 2 };
+enum MergeMarksEnum { MERGE_MARKS_APPEND = 0, MERGE_MARKS_PREPEND = 1, MERGE_MARKS_POSTPONE = 2, MERGE_MARKS_IGNORE = 3 };
 
 typedef ::com::sun::star::uno::Reference< ::com::sun::star::xml::sax::XFastAttributeList > XFastAttributeListRef;
 

sax/source/tools/fastserializer.cxx
@@ -291,7 +291,7 @@ namespace sax_fastparser {
         if ( maMarkStack.empty() )
             return;
 
-        if ( maMarkStack.size() == 1 )
+        if ( maMarkStack.size() == 1  && eMergeType != MERGE_MARKS_IGNORE)
         {
             mxOutputStream->writeBytes( maMarkStack.top()->getData() );
             maMarkStack.pop();
@@ -306,6 +306,8 @@ namespace sax_fastparser {
             case MERGE_MARKS_APPEND:   maMarkStack.top()->append( aMerge );   break;
             case MERGE_MARKS_PREPEND:  maMarkStack.top()->prepend( aMerge );  break;
             case MERGE_MARKS_POSTPONE: maMarkStack.top()->postpone( aMerge ); break;
+            case MERGE_MARKS_IGNORE  :  break;
+
         }
     }
 

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -329,11 +329,9 @@ void DocxAttributeOutput::PopulateFrameProperties(const SwFrmFmt* pFrmFmt, const
             relativeFromV = "page";
             break;
         case text::RelOrientation::FRAME:
-            relativeFromV = "paragraph";
-            break;
         case text::RelOrientation::TEXT_LINE:
         default:
-            relativeFromV = "line";
+            relativeFromV = "text";
             break;
     }
 
@@ -346,19 +344,31 @@ void DocxAttributeOutput::PopulateFrameProperties(const SwFrmFmt* pFrmFmt, const
             relativeFromH = "page";
             break;
         case text::RelOrientation::CHAR:
-            relativeFromH = "character";
-            break;
         case text::RelOrientation::PAGE_RIGHT:
-            relativeFromH = "page";
-            break;
         case text::RelOrientation::FRAME:
         default:
-            relativeFromH = "column";
+            relativeFromH = "text";
             break;
+    }
+
+    switch (pFrmFmt->GetSurround().GetValue())
+    {
+    case SURROUND_NONE:
+        attrList->add( FSNS( XML_w, XML_wrap), "none");
+        break;
+    case SURROUND_THROUGHT:
+        attrList->add( FSNS( XML_w, XML_wrap), "through");
+        break;
+    case SURROUND_PARALLEL:
+        attrList->add( FSNS( XML_w, XML_wrap), "notBeside");
+        break;
+    case SURROUND_IDEAL:
+    default:
+        attrList->add( FSNS( XML_w, XML_wrap), "auto");
+        break;
     }
     attrList->add( FSNS( XML_w, XML_vAnchor), relativeFromV);
     attrList->add( FSNS( XML_w, XML_hAnchor), relativeFromH);
-    attrList->add( FSNS( XML_w, XML_wrap), "notBeside");
     attrList->add( FSNS( XML_w, XML_hRule), "exact");
 
     sax_fastparser::XFastAttributeListRef xAttrList(attrList);
@@ -399,7 +409,7 @@ void DocxAttributeOutput::EndParagraph( ww8::WW8TableNodeInfoInner::Pointer_t pT
 {
     // write the paragraph properties + the run, already in the correct order
     m_pSerializer->mergeTopMarks();
-
+    std::vector<  boost::shared_ptr <sw::Frame> > aFramePrTextbox;
     // Write the anchored frame if any
     // Word can't handle nested text boxes, so write them on the same level.
     ++m_nTextFrameLevel;
@@ -411,12 +421,11 @@ void DocxAttributeOutput::EndParagraph( ww8::WW8TableNodeInfoInner::Pointer_t pT
         {
             m_bParagraphFrameOpen = true;
             sw::Frame aFrame = m_aFramesOfParagraph[nIndex];
-            m_pSerializer->startElementNS( XML_w, XML_r, FSEND );
-
             const SwFrmFmt& rFrmFmt = aFrame.GetFrmFmt();
 
             if (!TextBoxIsFramePr(rFrmFmt) || m_bWritingHeaderFooter)
             {
+                m_pSerializer->startElementNS( XML_w, XML_r, FSEND );
                 m_pSerializer->startElementNS(XML_mc, XML_AlternateContent, FSEND);
                 m_pSerializer->startElementNS(XML_mc, XML_Choice,
                         XML_Requires, "wps",
@@ -465,17 +474,15 @@ void DocxAttributeOutput::EndParagraph( ww8::WW8TableNodeInfoInner::Pointer_t pT
 
                 m_pSerializer->endElementNS(XML_mc, XML_Fallback);
                 m_pSerializer->endElementNS(XML_mc, XML_AlternateContent);
+                m_pSerializer->endElementNS( XML_w, XML_r );
+                m_bParagraphFrameOpen = false;
             }
             else
             {
-                ww8::WW8TableInfo::Pointer_t pOldTableInfo = m_rExport.mpTableInfo;
-                //Reset the table infos after saving.
-                m_rExport.mpTableInfo = ww8::WW8TableInfo::Pointer_t(new ww8::WW8TableInfo());
-                m_rExport.SdrExporter().writeOnlyTextOfFrame(&aFrame);
-                m_rExport.mpTableInfo = pOldTableInfo;
+                ::boost::shared_ptr<sw::Frame>  pFramePr;
+                pFramePr.reset(new sw::Frame(aFrame));
+                aFramePrTextbox.push_back(pFramePr);
             }
-            m_pSerializer->endElementNS( XML_w, XML_r );
-            m_bParagraphFrameOpen = false;
         }
         if (!m_postponedCustomShape->empty())
         {
@@ -515,8 +522,25 @@ void DocxAttributeOutput::EndParagraph( ww8::WW8TableNodeInfoInner::Pointer_t pT
     //sdtcontent is written so Set m_bParagraphHasDrawing to false
     m_rExport.SdrExporter().setParagraphHasDrawing( false );
     m_bRunTextIsOn = false;
-    m_pSerializer->mergeTopMarks();
+    if(aFramePrTextbox.empty())
+        m_pSerializer->mergeTopMarks();
+    else
+        m_pSerializer->mergeTopMarks(sax_fastparser::MERGE_MARKS_IGNORE );
 
+    // Write framePr
+    if(!aFramePrTextbox.empty())
+    {
+            ww8::WW8TableInfo::Pointer_t pOldTableInfo = m_rExport.mpTableInfo;
+            for (std::vector< boost::shared_ptr<sw::Frame> > ::iterator it = aFramePrTextbox.begin() ; it != aFramePrTextbox.end(); ++it)
+            {
+                m_rExport.mpTableInfo = ww8::WW8TableInfo::Pointer_t(new ww8::WW8TableInfo());
+                m_pCurrentFrame = it->get();
+                m_rExport.SdrExporter().writeOnlyTextOfFrame(it->get());
+                m_pCurrentFrame = NULL;
+            }
+            m_rExport.mpTableInfo = pOldTableInfo;
+            aFramePrTextbox.clear();
+    }
     // Check for end of cell, rows, tables here
     FinishTableRowCell( pTextNodeInfoInner );
 
@@ -818,20 +842,16 @@ void DocxAttributeOutput::EndParagraphProperties( const SfxItemSet* pParagraphMa
         m_pSerializer->endElementNS( XML_w, XML_rPr );
     }
 
-    if (!m_bWritingHeaderFooter)
+    if (!m_bWritingHeaderFooter && m_pCurrentFrame)
     {
-        //Check whether we have Frame for paragraph
-        for (size_t nIndex = 0; nIndex < m_aFramesOfParagraph.size(); ++nIndex)
+        const SwFrmFmt& rFrmFmt = m_pCurrentFrame->GetFrmFmt();
+        if (TextBoxIsFramePr(rFrmFmt))
         {
-            sw::Frame aFrame = m_aFramesOfParagraph[nIndex];
-            const SwFrmFmt& rFrmFmt = aFrame.GetFrmFmt();
-            if (TextBoxIsFramePr(rFrmFmt))
-            {
-                const Size aSize = aFrame.GetSize();
-                PopulateFrameProperties(&rFrmFmt, aSize);
-            }
+            const Size aSize = m_pCurrentFrame->GetSize();
+            PopulateFrameProperties(&rFrmFmt, aSize);
         }
     }
+
     m_pSerializer->endElementNS( XML_w, XML_pPr );
 
     if ( m_nColBreakStatus == COLBRK_WRITE )
@@ -7756,6 +7776,7 @@ DocxAttributeOutput::DocxAttributeOutput( DocxExport &rExport, FSHelperPtr pSeri
       m_nNextBookmarkId( 0 ),
       m_nNextAnnotationMarkId( 0 ),
       m_pTableWrt( NULL ),
+      m_pCurrentFrame( NULL ),
       m_bParagraphOpened( false ),
       m_bParagraphFrameOpen( false ),
       m_bIsFirstParagraph( true ),

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -770,6 +770,8 @@ private:
     /// The current table helper
     SwWriteTable *m_pTableWrt;
 
+    sw::Frame* m_pCurrentFrame;
+
     bool m_bParagraphOpened;
     bool m_bParagraphFrameOpen;
     bool m_bIsFirstParagraph;

zhirshya/lo-tblr

commit 5030aa2f8b6e3785805fb27a5f3a2a3594f4dcca

tdf#103544: DOCX exp.: Image loss when have a frame anchored to the same para.
Regression from:
83d51e5e52688c4c9bc0ad70a511458bb06a242d

Partly revert the commit causes this regression.
I checked the related bugs (tdf#78590,tdf#80748)
intended to be fixed by this commit and reverting
this part does not bring back the corruption.
I guess something changed in frames' and text boxes'
import in the meantime, because this MergeMarks::IGNORE
is useless now.

Change-Id: If17776e8628561961c7ce2a2994e3fc609f75639
Reviewed-on: https://gerrit.libreoffice.org/30351
Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>

include/sax/fshelper.hxx
@@ -60,7 +60,7 @@ const sal_Int32 FSEND_internal = -1; // same as XML_TOKEN_INVALID
 
 namespace sax_fastparser {
 
-enum class MergeMarks { APPEND = 0, PREPEND = 1, POSTPONE = 2, IGNORE = 3 };
+enum class MergeMarks { APPEND = 0, PREPEND = 1, POSTPONE = 2};
 
 typedef css::uno::Reference< css::xml::sax::XFastAttributeList > XFastAttributeListRef;
 

sax/source/tools/fastserializer.cxx
@@ -405,7 +405,7 @@ namespace sax_fastparser {
         // flush, so that we get everything in getData()
         maCachedOutputStream.flush();
 
-        if (maMarkStack.size() == 1 && eMergeType != MergeMarks::IGNORE)
+        if (maMarkStack.size() == 1)
         {
 #ifdef DBG_UTIL
             while (!maMarkStack.top()->m_DebugEndedElements.empty())
@@ -469,8 +469,6 @@ namespace sax_fastparser {
                     topDebugEndedElements,
                     topDebugStartedElements);
                 break;
-            case MergeMarks::IGNORE:
-                break;
         }
 #endif
         if (maMarkStack.empty())
@@ -488,8 +486,6 @@ namespace sax_fastparser {
             case MergeMarks::APPEND:   maMarkStack.top()->append( aMerge );   break;
             case MergeMarks::PREPEND:  maMarkStack.top()->prepend( aMerge );  break;
             case MergeMarks::POSTPONE: maMarkStack.top()->postpone( aMerge ); break;
-            case MergeMarks::IGNORE:   break;
-
         }
     }
 

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -579,10 +579,7 @@ void DocxAttributeOutput::EndParagraph( ww8::WW8TableNodeInfoInner::Pointer_t pT
     //sdtcontent is written so Set m_bParagraphHasDrawing to false
     m_rExport.SdrExporter().setParagraphHasDrawing( false );
     m_bRunTextIsOn = false;
-    if(aFramePrTextbox.empty())
-        m_pSerializer->mergeTopMarks(Tag_StartParagraph_1);
-    else
-        m_pSerializer->mergeTopMarks(Tag_StartParagraph_1, sax_fastparser::MergeMarks::IGNORE);
+    m_pSerializer->mergeTopMarks(Tag_StartParagraph_1);
 
     // Write framePr
     if(!aFramePrTextbox.empty())

sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -17,6 +17,7 @@
 #include <com/sun/star/text/XTextFramesSupplier.hpp>
 #include <com/sun/star/text/XTextEmbeddedObjectsSupplier.hpp>
 #include <com/sun/star/text/XTextViewCursorSupplier.hpp>
+#include <com/sun/star/graphic/XGraphic.hpp>
 
 class Test : public SwModelTestBase
 {
@@ -68,6 +69,20 @@ DECLARE_OOXMLEXPORT_TEST(testTdf84678, "tdf84678.docx")
     // This was 0, left margin inside a shape+text wasn't imported from DOCX.
     // 360000 EMU, but layout uses twips.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(567), parseDump("/root/page/body/txt/anchored/fly/infos/prtBounds", "left").toInt32());
+
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf103544, "tdf103544.docx")
+{
+    // We have two shapes: a frame and an image
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2), xDrawPage->getCount());
+
+    // Image was lost because of the frame export
+    uno::Reference<beans::XPropertySet> xImage(getShape(1), uno::UNO_QUERY);
+    auto xGraphic = getProperty<uno::Reference<graphic::XGraphic> >(xImage, "Graphic");
+    CPPUNIT_ASSERT(xGraphic.is());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/qa/extras/ooxmlexport/ooxmlfieldexport.cxx
@@ -498,8 +498,8 @@ DECLARE_OOXMLEXPORT_TEST(testFDO78590, "FDO78590.docx")
         return;
 
     // This is to ensure that the fld starts and ends inside a hyperlink...
-    assertXPath ( pXmlDoc, "/w:document/w:body/w:p[1]/w:pPr/w:framePr", "w", "9851" );
-    assertXPath ( pXmlDoc, "/w:document/w:body/w:p[1]/w:pPr/w:framePr", "h", "1669" );
+    assertXPath ( pXmlDoc, "/w:document/w:body/w:p[2]/w:pPr/w:framePr", "w", "9851" );
+    assertXPath ( pXmlDoc, "/w:document/w:body/w:p[2]/w:pPr/w:framePr", "h", "1669" );
 }
 
 DECLARE_OOXMLEXPORT_TEST(testSdtCitationRun, "sdt-citation-run.docx")