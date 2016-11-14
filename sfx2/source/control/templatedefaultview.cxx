LibreOffice/core

commit 96e9ffa64706f523d67659a20a0ccce6fbd0cb91

loplogin:singlevalfields in include/
Change-Id: I27842162fcf82120ecb811ee8e89e187430931fc
Reviewed-on: https://gerrit.libreoffice.org/28931
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/control/templatedefaultview.cxx
@@ -26,7 +26,6 @@ VCL_BUILDER_FACTORY(TemplateDefaultView)
 TemplateDefaultView::TemplateDefaultView( Window* pParent)
     : TemplateLocalView(pParent)
     , mnTextHeight(30)
-    , mnItemPadding(5)//TODO:: Change padding to 10. It looks really crowded and occupied.
 {
     Rectangle aScreen = Application::GetScreenPosSizePixel(Application::GetDisplayBuiltInScreen());
     mnItemMaxSize = std::min(aScreen.GetWidth(),aScreen.GetHeight()) > 800 ? 256 : 192;

compilerplugins/clang/singlevalfields.py
@@ -34,6 +34,12 @@ with io.open("loplugin.singlevalfields.log", "rb", buffering=1024*1024) as txt:
 
 tmp1list = list()
 for fieldInfo, assignValues in fieldAssignDict.iteritems():
+    v0 = fieldInfo[0] + " " + fieldInfo[1]
+    v1 = (",".join(assignValues))
+    v2 = ""
+    if fieldInfo not in definitionToSourceLocationMap:
+        continue
+    v2 = definitionToSourceLocationMap[fieldInfo]
     if len(assignValues) != 1:
         continue
     if "?" in assignValues:
@@ -46,19 +52,16 @@ for fieldInfo, assignValues in fieldAssignDict.iteritems():
         continue
     # ignore things which are representations of on-disk structures
     if containingClass in ["SEPr", "WW8Dop", "BmpInfoHeader", "BmpFileHeader", "Exif::ExifIFD",
-            "sw::WW8FFData", "FFDataHeader", "INetURLHistory_Impl::head_entry"]:
+            "sw::WW8FFData", "FFDataHeader", "INetURLHistory_Impl::head_entry", "ImplPPTParaPropSet", "SvxSwAutoFormatFlags"]:
         continue
     # Windows-only
-    if containingClass in ["SfxAppData_Impl", "sfx2::ImplDdeItem"]:
+    if containingClass in ["SfxAppData_Impl", "sfx2::ImplDdeItem", "SvFileStream", "DdeService", "DdeTopic", "DdeItem", "DdeConnection", "connectivity::sdbcx::OUser", "connectivity::sdbcx::OGroup", "connectivity::sdbcx::OCatalog"]:
+        continue
+    if v2.startswith("include/svl/svdde.hxx"):
         continue
     # Some of our supported compilers don't do constexpr, which means o3tl::typed_flags can't be 'static const'
     if containingClass in ["WaitWindow_Impl"]:
         continue
-    v0 = fieldInfo[0] + " " + fieldInfo[1]
-    v1 = (",".join(assignValues))
-    v2 = ""
-    if fieldInfo in definitionToSourceLocationMap:
-        v2 = definitionToSourceLocationMap[fieldInfo]
     tmp1list.append((v0,v1,v2))
 
 # sort results by filename:lineno

drawinglayer/source/primitive3d/sdrextrudeprimitive3d.cxx
@@ -78,7 +78,7 @@ namespace drawinglayer
                 // create geometry
                 ::std::vector< basegfx::B3DPolyPolygon > aFill;
                 extractPlanesFromSlice(aFill, rSliceVector,
-                    bCreateNormals, getSmoothHorizontalNormals(), getSmoothNormals(), getSmoothLids(), false,
+                    bCreateNormals, true/*smoothHorizontalNormals*/, getSmoothNormals(), getSmoothLids(), false,
                     0.5, 0.6, bCreateTextureCoordinatesX || bCreateTextureCoordinatesY, aTexTransform);
 
                 // get full range
@@ -406,7 +406,6 @@ namespace drawinglayer
             mfBackScale(fBackScale),
             mpLastRLGViewInformation(nullptr),
             mbSmoothNormals(bSmoothNormals),
-            mbSmoothHorizontalNormals(true),
             mbSmoothLids(bSmoothLids),
             mbCharacterMode(bCharacterMode),
             mbCloseFront(bCloseFront),
@@ -460,7 +459,6 @@ namespace drawinglayer
                     && getDiagonal() == rCompare.getDiagonal()
                     && getBackScale() == rCompare.getBackScale()
                     && getSmoothNormals() == rCompare.getSmoothNormals()
-                    && getSmoothHorizontalNormals() == rCompare.getSmoothHorizontalNormals()
                     && getSmoothLids() == rCompare.getSmoothLids()
                     && getCharacterMode() == rCompare.getCharacterMode()
                     && getCloseFront() == rCompare.getCloseFront()

drawinglayer/source/primitive3d/sdrlatheprimitive3d.cxx
@@ -73,7 +73,7 @@ namespace drawinglayer
                 // create geometry
                 ::std::vector< basegfx::B3DPolyPolygon > aFill;
                 extractPlanesFromSlice(aFill, rSliceVector,
-                    bCreateNormals, getSmoothHorizontalNormals(), getSmoothNormals(), getSmoothLids(), bClosedRotation,
+                    bCreateNormals, true/*smoothHorizontalNormals*/, getSmoothNormals(), getSmoothLids(), bClosedRotation,
                     0.85, 0.6, bCreateTextureCoordinatesX || bCreateTextureCoordinatesY, aTexTransform);
 
                 // get full range
@@ -266,7 +266,6 @@ namespace drawinglayer
             mfRotation(fRotation),
             mpLastRLGViewInformation(nullptr),
             mbSmoothNormals(bSmoothNormals),
-            mbSmoothHorizontalNormals(true),
             mbSmoothLids(bSmoothLids),
             mbCharacterMode(bCharacterMode),
             mbCloseFront(bCloseFront),
@@ -322,7 +321,6 @@ namespace drawinglayer
                     && getBackScale() == rCompare.getBackScale()
                     && getRotation() == rCompare.getRotation()
                     && getSmoothNormals() == rCompare.getSmoothNormals()
-                    && getSmoothHorizontalNormals() == rCompare.getSmoothHorizontalNormals()
                     && getSmoothLids() == rCompare.getSmoothLids()
                     && getCharacterMode() == rCompare.getCharacterMode()
                     && getCloseFront() == rCompare.getCloseFront()

filter/source/msfilter/msdffimp.cxx
@@ -7280,7 +7280,6 @@ SvxMSDffImportRec::SvxMSDffImportRec()
       pYRelTo( nullptr ), //   relative to paragraph
       nLayoutInTableCell( 0 ), // element is laid out in table cell
       nFlags( 0 ),
-      nTextRotationAngle( 0 ),
       nDxTextLeft( 144 ),
       nDyTextTop( 72 ),
       nDxTextRight( 144 ),
@@ -7319,7 +7318,6 @@ SvxMSDffImportRec::SvxMSDffImportRec(const SvxMSDffImportRec& rCopy)
       pYRelTo( nullptr ),
       nLayoutInTableCell( rCopy.nLayoutInTableCell ),
       nFlags( rCopy.nFlags ),
-      nTextRotationAngle( rCopy.nTextRotationAngle ),
       nDxTextLeft( rCopy.nDxTextLeft    ),
       nDyTextTop( rCopy.nDyTextTop ),
       nDxTextRight( rCopy.nDxTextRight ),

filter/source/msfilter/svdfppt.cxx
@@ -1464,7 +1464,6 @@ SdrPowerPointImport::SdrPowerPointImport( PowerPointImportParam& rParam, const O
                 ReadFontCollection();
 
             // reading TxPF, TxSI
-            PPTTextCharacterStyleAtomInterpreter    aTxCFStyle; // SJ: TODO, this atom needs to be interpreted, it contains character default styles for standard objects (instance4)
             PPTTextParagraphStyleAtomInterpreter    aTxPFStyle;
             PPTTextSpecInfoAtomInterpreter          aTxSIStyle; // styles (default language setting ... )
 
@@ -1607,7 +1606,7 @@ SdrPowerPointImport::SdrPowerPointImport( PowerPointImportParam& rParam, const O
                                 if ( aTxSIStyle.bValid && !aTxSIStyle.aList.empty() )
                                     aTxSI = *( aTxSIStyle.aList[ 0 ] );
 
-                                rE2.pStyleSheet = new PPTStyleSheet( aSlideHd, rStCtrl, *this, aTxCFStyle, aTxPFStyle, aTxSI );
+                                rE2.pStyleSheet = new PPTStyleSheet( aSlideHd, rStCtrl, *this, aTxPFStyle, aTxSI );
                                 pDefaultSheet = rE2.pStyleSheet;
                             }
                             if ( SeekToRec( rStCtrl, PPT_PST_ColorSchemeAtom, aSlideHd.GetRecEndFilePos() ) )
@@ -4026,8 +4025,8 @@ void PPTParaSheet::UpdateBulletRelSize(  sal_uInt32 nLevel, sal_uInt16 nFontHeig
 }
 
 PPTStyleSheet::PPTStyleSheet( const DffRecordHeader& rSlideHd, SvStream& rIn, SdrPowerPointImport& rManager,
-                                const PPTTextCharacterStyleAtomInterpreter& /*rTxCFStyle*/, const PPTTextParagraphStyleAtomInterpreter& rTxPFStyle,
-                                    const PPTTextSpecInfo& rTextSpecInfo ) :
+                              const PPTTextParagraphStyleAtomInterpreter& rTxPFStyle,
+                              const PPTTextSpecInfo& rTextSpecInfo ) :
 
     PPTNumberFormatCreator  ( new PPTExtParaProv( rManager, rIn, &rSlideHd ) ),
     maTxSI                  ( rTextSpecInfo )
@@ -4636,21 +4635,6 @@ PPTTextRulerInterpreter::~PPTTextRulerInterpreter()
 {
     if ( ! ( --mpImplRuler->nRefCount ) )
         delete mpImplRuler;
-}
-
-PPTTextCharacterStyleAtomInterpreter::PPTTextCharacterStyleAtomInterpreter()
-    : nFlags1(0)
-    , nFlags2(0)
-    , nFlags3(0)
-    , n1(0)
-    , nFontHeight(0)
-    , nFontColor(0)
-{
-}
-
-
-PPTTextCharacterStyleAtomInterpreter::~PPTTextCharacterStyleAtomInterpreter()
-{
 }
 
 PPTTextParagraphStyleAtomInterpreter::PPTTextParagraphStyleAtomInterpreter() :

formula/source/ui/dlg/formula.cxx
@@ -828,8 +828,8 @@ void FormulaDlg_Impl::FillListboxes()
     }
     else if ( pData )
     {
-        pFuncPage->SetCategory( pData->GetCatSel() );
-        pFuncPage->SetFunction( pData->GetFuncSel() );
+        pFuncPage->SetCategory( 1 );
+        pFuncPage->SetFunction( LISTBOX_ENTRY_NOTFOUND );
     }
     FuncSelHdl(*pFuncPage);
 
@@ -1902,8 +1902,6 @@ void FormEditData::Reset()
     pParent = nullptr;
     nMode = 0;
     nFStart = 0;
-    nCatSel = 1;        //! oder 0 (zuletzt benutzte)
-    nFuncSel = LISTBOX_ENTRY_NOTFOUND;
     nOffset = 0;
     nEdFocus = 0;
     bMatrix = false;
@@ -1918,8 +1916,6 @@ FormEditData& FormEditData::operator=( const FormEditData& r )
     pParent         = r.pParent;
     nMode           = r.nMode;
     nFStart         = r.nFStart;
-    nCatSel         = r.nCatSel;
-    nFuncSel        = r.nFuncSel;
     nOffset         = r.nOffset;
     nEdFocus        = r.nEdFocus;
     aUndoStr        = r.aUndoStr;

include/drawinglayer/primitive3d/sdrextrudeprimitive3d.hxx
@@ -58,7 +58,6 @@ namespace drawinglayer
             geometry::ViewInformation3D*                mpLastRLGViewInformation;
 
             bool                                        mbSmoothNormals : 1; // Plane self
-            bool                                        mbSmoothHorizontalNormals : 1; // always
             bool                                        mbSmoothLids : 1; // Front/back
             bool                                        mbCharacterMode : 1;
             bool                                        mbCloseFront : 1;
@@ -98,7 +97,6 @@ namespace drawinglayer
             double getDiagonal() const { return mfDiagonal; }
             double getBackScale() const { return mfBackScale; }
             bool getSmoothNormals() const { return mbSmoothNormals; }
-            bool getSmoothHorizontalNormals() const { return mbSmoothHorizontalNormals; }
             bool getSmoothLids() const { return mbSmoothLids; }
             bool getCharacterMode() const { return mbCharacterMode; }
             bool getCloseFront() const { return mbCloseFront; }

zhirshya/lo-tblr

commit 2e6290e347ff73cccfc194656084c2dd1f1b547b

tdf#103897 No space between template previews from Start Center
regression from
	commit	96e9ffa64706f523d67659a20a0ccce6fbd0cb91 (patch)
	loplogin:singlevalfields in include/

I did not notice that the mnItemPadding field was shadowing a
declaration in a superclass

Change-Id: I52cf9945da43fa3d1049b624a6b24bc6d974d445

sfx2/source/control/templatedefaultview.cxx
@@ -23,13 +23,15 @@
 
 VCL_BUILDER_FACTORY(TemplateDefaultView)
 
+static const int gnItemPadding(5); //TODO:: Change padding to 10. It looks really crowded and occupied.
+
 TemplateDefaultView::TemplateDefaultView( Window* pParent)
     : TemplateLocalView(pParent)
     , mnTextHeight(30)
 {
     Rectangle aScreen = Application::GetScreenPosSizePixel(Application::GetDisplayBuiltInScreen());
     mnItemMaxSize = std::min(aScreen.GetWidth(),aScreen.GetHeight()) > 800 ? 256 : 192;
-    ThumbnailView::setItemDimensions( mnItemMaxSize, mnItemMaxSize, mnTextHeight, mnItemPadding );
+    ThumbnailView::setItemDimensions( mnItemMaxSize, mnItemMaxSize, mnTextHeight, gnItemPadding );
     updateThumbnailDimensions(mnItemMaxSize);
 
     // startcenter specific settings
@@ -44,7 +46,7 @@ void TemplateDefaultView::reload()
 {
     TemplateLocalView::reload();
     // Set preferred width
-    set_width_request(mnTextHeight + mnItemMaxSize + 2*mnItemPadding);
+    set_width_request(mnTextHeight + mnItemMaxSize + 2*gnItemPadding);
 }
 
 void TemplateDefaultView::showAllTemplates()