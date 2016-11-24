LibreOffice/core

commit 329742e6c9da7cd7848d92a6846e3d1249d8d9b4

fdo#85886 don't redraw the Navigator content tree if nothing changed
This fixes the flickering of the scrollbar on re-draw once a second.
Perhaps it helps for the performance issue too.

Change-Id: I2ec8f0a8a241b128113bfa3d47fb09ba472b4a7e

sw/source/uibase/inc/conttree.hxx
@@ -19,6 +19,7 @@
 #ifndef INCLUDED_SW_SOURCE_UIBASE_INC_CONTTREE_HXX
 #define INCLUDED_SW_SOURCE_UIBASE_INC_CONTTREE_HXX
 
+#include <svl/lstner.hxx>
 #include <svtools/treelistbox.hxx>
 #include <svtools/svlbitm.hxx>
 #include "swcont.hxx"
@@ -43,7 +44,9 @@ class SdrObject;
 #define EDIT_MODE_DELETE        4
 #define EDIT_MODE_RENAME        5
 
-class SwContentTree : public SvTreeListBox
+class SwContentTree
+    : public SvTreeListBox
+    , public SfxListener
 {
     ImageList           aEntryImages;
     OUString            sSpace;
@@ -85,6 +88,7 @@ class SwContentTree : public SvTreeListBox
     bool                bIsOutlineMoveable  :1;
     bool                bViewHasChanged     :1;
     bool                bIsImageListInitialized : 1;
+    bool                m_bActiveDocModified :1;
 
     static bool         bIsInDrag;
 
@@ -196,6 +200,10 @@ public:
 
     virtual bool    Select( SvTreeListEntry* pEntry, bool bSelect=true ) SAL_OVERRIDE;
     virtual sal_Int32  GetEntryRealChildrenNum( SvTreeListEntry* pEntry ) const;
+
+    using Control::Notify; // FIXME why do we have 2 of these
+    virtual void Notify(SfxBroadcaster& rBC, SfxHint const& rHint) SAL_OVERRIDE;
+
 };
 
 // TreeListBox for global documents

sw/source/uibase/utlui/content.cxx
@@ -1756,6 +1756,7 @@ void SwContentTree::Display( bool bActive )
         ScrollOutputArea( (short)nDelta );
     }
 
+    m_bActiveDocModified = false;
 }
 
 // In the Clear the content types have to be deleted, also.
@@ -2208,6 +2209,8 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
     bool bClear = pActiveShell != pSh;
     if(bIsActive && bClear)
     {
+        if (pActiveShell)
+            EndListening(*pActiveShell->GetView().GetDocShell());
         pActiveShell = pSh;
         FindActiveTypeAndRemoveUserData();
         Clear();
@@ -2216,6 +2219,7 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
     {
         if(!lcl_FindShell(pActiveShell))
         {
+            EndListening(*pActiveShell->GetView().GetDocShell());
             pActiveShell = pSh;
             bIsActive = true;
             bIsConstant = false;
@@ -2226,6 +2230,7 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
     // the screen filled new.
     if(bIsActive && bClear)
     {
+        StartListening(*pActiveShell->GetView().GetDocShell());
         FindActiveTypeAndRemoveUserData();
         for(sal_uInt16 i=0; i < CONTENT_TYPE_MAX; i++)
         {
@@ -2239,15 +2244,33 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
 
 void SwContentTree::SetConstantShell(SwWrtShell* pSh)
 {
+    if (pActiveShell)
+        EndListening(*pActiveShell->GetView().GetDocShell());
     pActiveShell = pSh;
     bIsActive       = false;
     bIsConstant     = true;
+    StartListening(*pActiveShell->GetView().GetDocShell());
     FindActiveTypeAndRemoveUserData();
     for(sal_uInt16 i=0; i < CONTENT_TYPE_MAX; i++)
     {
         DELETEZ(aActiveContentArr[i]);
     }
     Display(true);
+}
+
+
+
+void SwContentTree::Notify(SfxBroadcaster & rBC, SfxHint const& rHint)
+{
+    SfxSimpleHint const*const pHint(dynamic_cast<SfxSimpleHint const*>(&rHint));
+    if (pHint && SFX_HINT_DOCCHANGED == pHint->GetId())
+    {
+        m_bActiveDocModified = true;
+    }
+    else
+    {
+        SfxListener::Notify(rBC, rHint);
+    }
 }
 
 // Execute commands of the Navigator
@@ -2436,18 +2459,25 @@ IMPL_LINK_NOARG(SwContentTree, TimerUpdate)
         }
 
         if(bIsActive && pActShell != GetWrtShell())
+        {
             SetActiveShell(pActShell);
+        }
         else if( (bIsActive || (bIsConstant && pActShell == GetWrtShell())) &&
                     HasContentChanged())
         {
-            FindActiveTypeAndRemoveUserData();
-            Display(true);
+            if (!bIsActive || m_bActiveDocModified)
+            {   // don't burn cpu and redraw and flicker if not modified
+                FindActiveTypeAndRemoveUserData();
+                Display(true);
+            }
         }
     }
     else if(!pView && bIsActive && !bIsIdleClear)
     {
         if(pActiveShell)
+        {
             SetActiveShell(0);
+        }
         Clear();
         bIsIdleClear = true;
     }

zhirshya/lo-tblr

commit 358ad67558ec3461c20a442aee5ae98368ec4d09

tdf#103788 sw: fix use-after-free in navigator dialog
The problem is that if SwContentTree::HasContentChanged() returns true,
it may have deleted the SwTypeNumber instances that are referenced in
SvTreeListEntry::pUserData, but it has not reset pUserData so those
pointers are now used to acceess deleted objects.

Also it looks like the HasContentChanged() detects additional conditions
that would not cause a modified event from the document but should still
cause a repaint, such as when the user moves the cursor between
headings.

Revert the optimization, it was a stupid idea.

(regression from 329742e6c9da7cd7848d92a6846e3d1249d8d9b4)

Change-Id: Idb5207e896b0638324fc41b7c214536be4ba864b

sw/source/uibase/inc/conttree.hxx
@@ -96,7 +96,6 @@ class SwContentTree
     bool                m_bIsOutlineMoveable  :1;
     bool                m_bViewHasChanged     :1;
     bool                m_bIsImageListInitialized : 1;
-    bool                m_bActiveDocModified :1;
 
     static bool         bIsInDrag;
 

sw/source/uibase/utlui/content.cxx
@@ -800,7 +800,6 @@ SwContentTree::SwContentTree(vcl::Window* pParent, SwNavigationPI* pDialog)
     , m_bIsOutlineMoveable(true)
     , m_bViewHasChanged(false)
     , m_bIsImageListInitialized(false)
-    , m_bActiveDocModified(false)
     , m_bIsKeySpace(false)
 {
     SetHelpId(HID_NAVIGATOR_TREELIST);
@@ -1726,8 +1725,6 @@ void SwContentTree::Display( bool bActive )
         sal_Int32 nDelta = pVScroll->GetThumbPos() - nOldScrollPos;
         ScrollOutputArea( (short)nDelta );
     }
-
-    m_bActiveDocModified = false;
 }
 
 void SwContentTree::Clear()
@@ -2227,12 +2224,6 @@ void SwContentTree::SetConstantShell(SwWrtShell* pSh)
 
 void SwContentTree::Notify(SfxBroadcaster & rBC, SfxHint const& rHint)
 {
-    if (SFX_HINT_DOCCHANGED == rHint.GetId())
-    {
-        m_bActiveDocModified = true;
-        return;
-    }
-
     SfxViewEventHint const*const pVEHint(dynamic_cast<SfxViewEventHint const*>(&rHint));
     SwXTextView* pDyingShell = nullptr;
     if (m_pActiveShell && pVEHint && pVEHint->GetEventName() == "OnViewClosed")
@@ -2447,11 +2438,8 @@ IMPL_LINK_NOARG(SwContentTree, TimerUpdate, Timer *, void)
         else if( (m_bIsActive || (m_bIsConstant && pActShell == GetWrtShell())) &&
                     HasContentChanged())
         {
-            if (!m_bIsActive || m_bActiveDocModified)
-            {   // don't burn cpu and redraw and flicker if not modified
-                FindActiveTypeAndRemoveUserData();
-                Display(true);
-            }
+            FindActiveTypeAndRemoveUserData();
+            Display(true);
         }
     }
     else if(!pView && m_bIsActive && !m_bIsIdleClear)