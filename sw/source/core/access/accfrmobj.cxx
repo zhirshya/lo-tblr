LibreOffice/core

commit ae06f4be6bbdebc10a88c386064d548848e57f80

tdf#101464 tdf#91260 sw: a11y for partially off-page drawing object
The problem is that the drawing object in the bugdoc is not disposed
when the document view is closed at a scroll position that does not show
the page with the anchor frame of the drawing object, but does show the
top of the following page, because it will only be disposed via its anchor
page.

SwAccessibleChild::GetBox() must take into account that the
model-determined bounding box of the SdrObject can be partially outside
the page where the object is anchored since commit
a4dee94afed9ade6ac50237c8d99a6e49d3bebc1.

Change-Id: I4276ee636d054320977e2a0ed0f69e53bbc778a3

sw/source/core/access/accfrmobj.cxx
@@ -258,6 +258,13 @@ SwRect SwAccessibleChild::GetBox( const SwAccessibleMap& rAccMap ) const
     else if( mpDrawObj )
     {
         aBox = SwRect( mpDrawObj->GetCurrentBoundRect() );
+        SwDrawContact const*const pContact(dynamic_cast<SwDrawContact const*>(::GetUserCall(mpDrawObj)));
+        // assume that a) the SwVirt* objects that don't have this are handled
+        // by the mpFrame case above b) for genuine SdrObject this must be set
+        // if it's connected to layout
+        assert(pContact);
+        // tdf#91260 drawing object may be partially off-page
+        aBox.Intersection(pContact->GetPageFrame()->Frame());
     }
     else if ( mpWindow )
     {

zhirshya/lo-tblr

commit a3230994060fcff884738e2833947fbb2a6c185d

tdf#101464 tdf#91260 tdf#104620 sw: fix null SwPageFrame crash
It turns out that draw objects in header/footer often don't have
AnchoredObject::mpPageFrame set, so use FindPageFrameOfAnchor()
instead.

(regression from ae06f4be6bbdebc10a88c386064d548848e57f80)

Change-Id: I6d54baa3acffa6775d5b57a1517c77a964065dd0

sw/source/core/access/accfrmobj.cxx
@@ -257,14 +257,19 @@ SwRect SwAccessibleChild::GetBox( const SwAccessibleMap& rAccMap ) const
     }
     else if( mpDrawObj )
     {
-        aBox = SwRect( mpDrawObj->GetCurrentBoundRect() );
         SwDrawContact const*const pContact(dynamic_cast<SwDrawContact const*>(::GetUserCall(mpDrawObj)));
         // assume that a) the SwVirt* objects that don't have this are handled
         // by the mpFrame case above b) for genuine SdrObject this must be set
         // if it's connected to layout
         assert(pContact);
-        // tdf#91260 drawing object may be partially off-page
-        aBox.Intersection(pContact->GetPageFrame()->Frame());
+        SwPageFrame const*const pPage(const_cast<SwAnchoredObject *>(
+            pContact->GetAnchoredObj(mpDrawObj))->FindPageFrameOfAnchor());
+        if (pPage) // may end up here with partial layout -> not visible
+        {
+            aBox = SwRect( mpDrawObj->GetCurrentBoundRect() );
+            // tdf#91260 drawing object may be partially off-page
+            aBox.Intersection(pPage->Frame());
+        }
     }
     else if ( mpWindow )
     {