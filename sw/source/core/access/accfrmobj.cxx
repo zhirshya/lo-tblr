LibreOffice/core

commit 76c549eb01dcb7b5bf28a271ce00e386f3d388ba

Integrate branch of IAccessible2
Conflicts:
	everything

Change-Id: I9619634ee1e60d449025c006803da29c1e9d14b3

sw/source/core/access/accfrmobj.cxx
@@ -33,6 +33,8 @@
 #include <fmtanchr.hxx>
 #include <dcontact.hxx>
 
+#include <pam.hxx>
+
 #include <vcl/window.hxx>
 
 namespace sw { namespace access {
@@ -162,6 +164,23 @@ bool SwAccessibleChild::IsBoundAsChar() const
     }
 
     return bRet;
+}
+
+sal_uInt32 SwAccessibleChild::GetAnchorPosition() const
+{
+    if( mpDrawObj )
+    {
+            const SwFrmFmt *pFrmFmt = ::FindFrmFmt( mpDrawObj );
+            if ( pFrmFmt )
+            {
+        const SwPosition *pPos = pFrmFmt->GetAnchor().GetCntntAnchor();
+                if ( pPos )
+                {
+            return pPos->nContent.GetIndex();
+                }
+            }
+    }
+    return 0;
 }
 
 SwAccessibleChild::SwAccessibleChild( const SwAccessibleChild& r )

zhirshya/lo-tblr

commit 654475862fca56d67f35e127f07b430b927f8798

tdf#105009 sw: fix a11y crash when removing drawing object
SwFrame::RemoveDrawObj() calls SwAnchoredObject::ChgAnchorFrame(0)
so the SwAnchoredDrawObj has no anchor frame later when
SwAccessibleMap::InvalidateCursorPosition() asks for the parent.

(regression from 76c549eb01dcb7b5bf28a271ce00e386f3d388ba)

Change-Id: Id55cb5fc41a4e37e863498265d1565e1621d508e

sw/source/core/access/accfrmobj.cxx
@@ -379,10 +379,14 @@ const SwFrame* SwAccessibleChild::GetParent( const bool bInPagePreview ) const
             else
             {
                 // In any other case the parent is the root frm
-                if( bInPagePreview )
-                    pParent = pContact->GetAnchorFrame()->FindPageFrame();
-                else
-                    pParent = pContact->GetAnchorFrame()->getRootFrame();
+                SwFrame const*const pAnchor(pContact->GetAnchorFrame());
+                if (pAnchor) // null if object removed from layout
+                {
+                    if (bInPagePreview)
+                        pParent = pAnchor->FindPageFrame();
+                    else
+                        pParent = pAnchor->getRootFrame();
+                }
             }
         }
     }