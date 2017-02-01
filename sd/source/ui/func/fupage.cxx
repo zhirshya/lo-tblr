LibreOffice/core

commit f3a90d13056ca23708cc4fd9562c99d0b41a4b6f

new loplugin: useuniqueptr: sd
Change-Id: I2fc8a2fa57cc00edf2edab9e3722c824e75cb7e5
Reviewed-on: https://gerrit.libreoffice.org/33204
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/ui/func/fupage.cxx
@@ -159,7 +159,6 @@ void FuPage::DoExecute( SfxRequest& )
 
 FuPage::~FuPage()
 {
-    delete mpBackgroundObjUndoAction;
 }
 
 void FuPage::Activate()
@@ -386,8 +385,8 @@ const SfxItemSet* FuPage::ExecuteDialog( vcl::Window* pParent )
 
                 if( mbPageBckgrdDeleted )
                 {
-                    mpBackgroundObjUndoAction = new SdBackgroundObjUndoAction(
-                        *mpDoc, *mpPage, mpPage->getSdrPageProperties().GetItemSet());
+                    mpBackgroundObjUndoAction.reset( new SdBackgroundObjUndoAction(
+                        *mpDoc, *mpPage, mpPage->getSdrPageProperties().GetItemSet()) );
 
                     if(!mpPage->IsMasterPage())
                     {
@@ -609,9 +608,8 @@ void FuPage::ApplyItemSet( const SfxItemSet* pArgs )
         if( !mbMasterPage && !mbPageBckgrdDeleted )
         {
             // Only this page
-            delete mpBackgroundObjUndoAction;
-            mpBackgroundObjUndoAction = new SdBackgroundObjUndoAction(
-                *mpDoc, *mpPage, mpPage->getSdrPageProperties().GetItemSet());
+            mpBackgroundObjUndoAction.reset( new SdBackgroundObjUndoAction(
+                *mpDoc, *mpPage, mpPage->getSdrPageProperties().GetItemSet()) );
             SfxItemSet aSet( *pArgs );
             sdr::properties::CleanupFillProperties(aSet);
             mpPage->getSdrPageProperties().ClearItem();
@@ -623,7 +621,7 @@ void FuPage::ApplyItemSet( const SfxItemSet* pArgs )
     if( mpBackgroundObjUndoAction )
     {
         // set merge flag, because a SdUndoGroupAction could have been inserted before
-        mpDocSh->GetUndoManager()->AddUndoAction( mpBackgroundObjUndoAction, true );
+        mpDocSh->GetUndoManager()->AddUndoAction( mpBackgroundObjUndoAction.get(), true );
         mpBackgroundObjUndoAction = nullptr;
     }
 

sd/inc/undo/undoobjects.hxx
@@ -92,7 +92,7 @@ public:
     virtual void Redo() override;
 
 private:
-    SfxUndoAction* mpUndoAnimation;
+    std::unique_ptr<SfxUndoAction> mpUndoAnimation;
     bool            mbNewEmptyPresObj;
     SdrObjectWeakRef mxSdrObject;
 };

sd/source/core/undo/undoobjects.cxx
@@ -186,14 +186,13 @@ UndoObjectSetText::UndoObjectSetText( SdrObject& rObject, sal_Int32 nText )
         css::uno::Reference< css::drawing::XShape > xShape( rObject.getUnoShape(), css::uno::UNO_QUERY );
         if( pPage->getMainSequence()->hasEffect( xShape ) )
         {
-            mpUndoAnimation = new UndoAnimation( static_cast< SdDrawDocument* >( pPage->GetModel() ), pPage );
+            mpUndoAnimation.reset( new UndoAnimation( static_cast< SdDrawDocument* >( pPage->GetModel() ), pPage ) );
         }
     }
 }
 
 UndoObjectSetText::~UndoObjectSetText()
 {
-    delete mpUndoAnimation;
 }
 
 void UndoObjectSetText::Undo()

sd/source/filter/eppt/eppt.hxx
@@ -135,12 +135,7 @@ struct PPTExOleObjEntry
 
 struct TextRuleEntry
 {
-    SvMemoryStream*     pOut;
-
-    explicit TextRuleEntry() :
-        pOut ( nullptr ){};
-
-    ~TextRuleEntry() { delete pOut; };
+    std::unique_ptr<SvMemoryStream>  pOut;
 };
 
 class TextObjBinary : public TextObj

sd/source/filter/eppt/epptbase.hxx
@@ -120,7 +120,8 @@ class PPTExBulletProvider
         SvMemoryStream          aBuExOutlineStream;
         SvMemoryStream          aBuExMasterStream;
 
-        EscherGraphicProvider*  pGraphicProv;
+        std::unique_ptr<EscherGraphicProvider>
+                                pGraphicProv;
 
     public:
 

sd/source/filter/eppt/epptso.cxx
@@ -97,13 +97,12 @@ using namespace ::com::sun::star;
 #define FIXED_PITCH             0x01
 
 PPTExBulletProvider::PPTExBulletProvider()
+    : pGraphicProv( new EscherGraphicProvider( EscherGraphicProviderFlags::UseInstances ) )
 {
-    pGraphicProv = new EscherGraphicProvider( EscherGraphicProviderFlags::UseInstances );
 }
 
 PPTExBulletProvider::~PPTExBulletProvider()
 {
-    delete pGraphicProv;
 }
 
 sal_uInt16 PPTExBulletProvider::GetId( const OString& rUniqueId, Size& rGraphicSize )
@@ -1266,7 +1265,10 @@ void PPTWriter::ImplWriteTextStyleAtom( SvStream& rOut, int nTextInstance, sal_u
             {
                 SvStream* pRuleOut = &rOut;
                 if ( pTextRule )
-                    pRuleOut = pTextRule->pOut = new SvMemoryStream( 0x100, 0x100 );
+                {
+                    pTextRule->pOut.reset( new SvMemoryStream( 0x100, 0x100 ) );
+                    pRuleOut = pTextRule->pOut.get();
+                }
 
                 sal_uInt32 nRulePos = pRuleOut->Tell();
                 pRuleOut->WriteUInt32( EPP_TextRulerAtom << 16 ).WriteUInt32( 0 );
@@ -2763,12 +2765,11 @@ void PPTWriter::ImplWritePage( const PHLayout& rLayout, EscherSolverContainer& a
                         ImplGetText();
                         ImplWriteTextStyleAtom( *pClientTextBox, nTextType, nPObjects, &aTextRule, aExtBu, nullptr );
                         ImplWriteExtParaHeader( aExtBu, nPObjects++, nTextType, nPageNumber + 0x100 );
-                        SvMemoryStream* pOut = aTextRule.pOut;
+                        SvMemoryStream* pOut = aTextRule.pOut.get();
                         if ( pOut )
                         {
                             pClientTextBox->WriteBytes(pOut->GetData(), pOut->Tell());
-                            delete pOut;
-                            aTextRule.pOut = nullptr;
+                            aTextRule.pOut.reset();
                         }
                         if ( aExtBu.Tell() )
                         {

sd/source/filter/ppt/pptin.cxx
@@ -155,7 +155,7 @@ SdPPTImport::SdPPTImport( SdDrawDocument* pDocument, SvStream& rDocStream, SotSt
         }
     }
 
-    pFilter = new ImplSdPPTImport( pDocument, rStorage, rMedium, aParam );
+    pFilter.reset( new ImplSdPPTImport( pDocument, rStorage, rMedium, aParam ) );
 }
 
 bool SdPPTImport::Import()
@@ -165,7 +165,6 @@ bool SdPPTImport::Import()
 
 SdPPTImport::~SdPPTImport()
 {
-    delete pFilter;
 }
 
 ImplSdPPTImport::ImplSdPPTImport( SdDrawDocument* pDocument, SotStorage& rStorage_, SfxMedium& rMedium, PowerPointImportParam& rParam )

sd/source/filter/ppt/pptin.hxx
@@ -82,7 +82,7 @@ public:
 
 class SdPPTImport
 {
-    ImplSdPPTImport* pFilter;
+    std::unique_ptr<ImplSdPPTImport> pFilter;
 
     public:
 

zhirshya/lo-tblr

commit 16cefed98cd0c06da9428ece2289be7b32f969a9

tdf#105677: sd: fix crash in FuPage::ApplyItemSet
(regression from f3a90d13056ca23708cc4fd9562c99d0b41a4b6f)

Change-Id: I54e3f06a74b2ac715061b5da54ac4db2b0a1010a

sd/source/ui/func/fupage.cxx
@@ -621,8 +621,7 @@ void FuPage::ApplyItemSet( const SfxItemSet* pArgs )
     if( mpBackgroundObjUndoAction )
     {
         // set merge flag, because a SdUndoGroupAction could have been inserted before
-        mpDocSh->GetUndoManager()->AddUndoAction( mpBackgroundObjUndoAction.get(), true );
-        mpBackgroundObjUndoAction = nullptr;
+        mpDocSh->GetUndoManager()->AddUndoAction( mpBackgroundObjUndoAction.release(), true );
     }
 
     // Objects can not be bigger than ViewSize