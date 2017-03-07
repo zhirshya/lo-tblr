LibreOffice/core

commit f4609c4c294a62023b6cd6baeb6a73d44992dec3

sw33bf04: #i101009#: doccomp.cxx: avoid creating redlines including EndOfContent

sw/source/core/doc/doccomp.cxx
@@ -1480,6 +1480,17 @@ void SwCompareData::SetRedlinesToDoc( BOOL bUseDocInfo )
                 pTmp->GetPoint()->nNode++;
                 pTmp->GetPoint()->nContent.Assign( pTmp->GetCntntNode(), 0 );
             }
+            // --> mst 2010-05-17 #i101009#
+            // prevent redlines that end on structural end node
+            if (& rDoc.GetNodes().GetEndOfContent() ==
+                & pTmp->GetPoint()->nNode.GetNode())
+            {
+                pTmp->GetPoint()->nNode--;
+                SwCntntNode *const pContentNode( pTmp->GetCntntNode() );
+                pTmp->GetPoint()->nContent.Assign( pContentNode,
+                        (pContentNode) ? pContentNode->Len() : 0 );
+            }
+            // <--
 
             rDoc.DeleteRedline( *pTmp, false, USHRT_MAX );
 
@@ -1499,6 +1510,17 @@ void SwCompareData::SetRedlinesToDoc( BOOL bUseDocInfo )
                 pTmp->GetPoint()->nNode++;
                 pTmp->GetPoint()->nContent.Assign( pTmp->GetCntntNode(), 0 );
             }
+            // --> mst 2010-05-17 #i101009#
+            // prevent redlines that end on structural end node
+            if (& rDoc.GetNodes().GetEndOfContent() ==
+                & pTmp->GetPoint()->nNode.GetNode())
+            {
+                pTmp->GetPoint()->nNode--;
+                SwCntntNode *const pContentNode( pTmp->GetCntntNode() );
+                pTmp->GetPoint()->nContent.Assign( pContentNode,
+                        (pContentNode) ? pContentNode->Len() : 0 );
+            }
+            // <--
         } while( pInsRing != ( pTmp = (SwPaM*)pTmp->GetNext() ));
         SwRedlineData aRedlnData( nsRedlineType_t::REDLINE_INSERT, nAuthor, aTimeStamp,
                                     aEmptyStr, 0, 0 );

zhirshya/lo-tblr

commit 8e14576d95ce101a81d4bfe28deb2ea0f3611060

tdf#106218 sw: compare document: tweak special handling of EndOfContent
In case the last paragraph in the document is inserted or deleted, the
redline must not be positioned on the EndOfContent node (i#101009).

But then rejecting the redline will have an additional paragraph as
compared to the original document.  Try to avoid that by tweaking the
start position of the redline to the end of the previous paragraph.

(regression from f4609c4c294a62023b6cd6baeb6a73d44992dec3)

Change-Id: Iae75042a3124d0f3bca955fb856c94a6f478b986

sw/source/core/doc/doccomp.cxx
@@ -1680,6 +1680,17 @@ void CompareData::SetRedlinesToDoc( bool bUseDocInfo )
                 SwContentNode *const pContentNode( pTmp->GetContentNode() );
                 pTmp->GetPoint()->nContent.Assign( pContentNode,
                         (pContentNode) ? pContentNode->Len() : 0 );
+                // tdf#106218 try to avoid losing a paragraph break here:
+                if (pTmp->GetMark()->nContent == 0)
+                {
+                    SwNodeIndex const prev(pTmp->GetMark()->nNode, -1);
+                    if (prev.GetNode().IsTextNode())
+                    {
+                        *pTmp->GetMark() = SwPosition(
+                            *prev.GetNode().GetTextNode(),
+                            prev.GetNode().GetTextNode()->Len());
+                    }
+                }
             }
 
             rDoc.getIDocumentRedlineAccess().DeleteRedline( *pTmp, false, USHRT_MAX );
@@ -1712,6 +1723,17 @@ void CompareData::SetRedlinesToDoc( bool bUseDocInfo )
                 SwContentNode *const pContentNode( pTmp->GetContentNode() );
                 pTmp->GetPoint()->nContent.Assign( pContentNode,
                         (pContentNode) ? pContentNode->Len() : 0 );
+                // tdf#106218 try to avoid losing a paragraph break here:
+                if (pTmp->GetMark()->nContent == 0)
+                {
+                    SwNodeIndex const prev(pTmp->GetMark()->nNode, -1);
+                    if (prev.GetNode().IsTextNode())
+                    {
+                        *pTmp->GetMark() = SwPosition(
+                            *prev.GetNode().GetTextNode(),
+                            prev.GetNode().GetTextNode()->Len());
+                    }
+                }
             }
         } while( pInsRing != ( pTmp = pTmp->GetNext()) );
         SwRedlineData aRedlnData( nsRedlineType_t::REDLINE_INSERT, nAuthor, aTimeStamp,