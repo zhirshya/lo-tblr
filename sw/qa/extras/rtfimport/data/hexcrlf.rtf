LibreOffice/core

commit 10e733908038407791f9c14af2a86417cc4a653c

writerfilter: RTF import: hex-escaped \r and \n create paragraph break
... in Word 2010, while the spec doesn't say what they do.
So just handle \'0d and \'0a like \par.

This fixes an assert failure on importing lp556169-2.rtf, where
insertTextPortion was called with a string containing "\r", which split
the paragraph and that messed up the SwPaM.

Change-Id: Iee8b5b47e15d18232de841adfbc9c6498727c384

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1120,7 +1120,19 @@ RTFError RTFDocumentImpl::resolveChars(char ch)
     if (m_aStates.top().nInternalState == RTFInternalState::HEX && m_aStates.top().eDestination != Destination::LEVELNUMBERS)
     {
         if (!bSkipped)
-            m_aHexBuffer.append(ch);
+        {
+            // note: apparently \'0d\'0a is interpreted as 2 breaks, not 1
+            if (m_aStates.top().eDestination != Destination::DOCCOMM
+                && (ch == '\r' || ch == '\n'))
+            {
+                checkUnicode(/*bUnicode =*/ false, /*bHex =*/ true);
+                dispatchSymbol(RTF_PAR);
+            }
+            else
+            {
+                m_aHexBuffer.append(ch);
+            }
+        }
         return RTFError::OK;
     }
 

sw/qa/extras/rtfimport/data/hexcrlf.rtf
@@ -1,0 +1,4 @@
+{\rtf1
+foo\'0dba
+r\'0abaz\'0d\'0aquux
+\par }

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -1101,6 +1101,16 @@ DECLARE_RTFIMPORT_TEST(testFdo59419, "fdo59419.rtf")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
 }
 
+DECLARE_RTFIMPORT_TEST(testHexCRLF, "hexcrlf.rtf")
+{
+    // hex-escaped \r and \n should create a paragraph break
+    getParagraph(1, "foo");
+    getParagraph(2, "bar");
+    getParagraph(3, "baz");
+    getParagraph(4, "");
+    getParagraph(5, "quux");
+}
+
 DECLARE_RTFIMPORT_TEST(testFdo58076_2, "fdo58076-2.rtf")
 {
     // Position of the picture wasn't correct.

zhirshya/lo-tblr

commit 033bbb130813130e6fc0e0524282837af3da652d

tdf#106692 writerfilter: RTF import: fix \'0d in \leveltext
It's not a newline but yet another one of those bizarre RTF-encodings.

(regression from 10e733908038407791f9c14af2a86417cc4a653c)

Change-Id: I568050b031b95ac0b6ebfa1a0c39107e62f68bed

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1123,8 +1123,10 @@ RTFError RTFDocumentImpl::resolveChars(char ch)
         if (!bSkipped)
         {
             // note: apparently \'0d\'0a is interpreted as 2 breaks, not 1
-            if (m_aStates.top().eDestination != Destination::DOCCOMM
-                    && (ch == '\r' || ch == '\n'))
+            if ((ch == '\r' || ch == '\n')
+                    && m_aStates.top().eDestination != Destination::DOCCOMM
+                    && m_aStates.top().eDestination != Destination::LEVELNUMBERS
+                    && m_aStates.top().eDestination != Destination::LEVELTEXT)
             {
                 checkUnicode(/*bUnicode =*/ false, /*bHex =*/ true);
                 dispatchSymbol(RTF_PAR);

sw/qa/extras/rtfimport/data/hexcrlf.rtf
@@ -1,4 +1,10 @@
 {\rtf1
+{\*\listtable
+{\list\listtemplateid1
+{\listlevel{\leveltext \'0d\'00.\'01.\'02.\'03.\'04.\'05.\'06}{\levelnumbers \'01\'03\'05\'07\'09\'0b\'0d}}
+{\listname Heading;}\listid1199164
+}}
+
 foo\'0dba
 r\'0abaz\'0d\'0aquux
 \par }