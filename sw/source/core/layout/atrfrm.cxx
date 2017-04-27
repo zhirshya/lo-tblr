LibreOffice/core

commit d4267231754c1e6b03c7723a6fecc46750e7c780

use SwIterator<>
Change-Id: I464b22b2a418d20a4b230ebe10d36642ace84b35

sw/source/core/layout/atrfrm.cxx
@@ -118,13 +118,10 @@ void DelHFFormat( SwClient *pToRemove, SwFrmFmt *pFmt )
     {
         // nested scope because DTOR of SwClientIter resets the flag bTreeChg.
         // It's suboptimal if the format is deleted beforehand.
-        SwClientIter aIter( *pFmt );        // TODO
-        SwClient *pLast = aIter.GoStart();
-        if( pLast )
-            do {
-                bDel = pLast->IsA( TYPE(SwFrm) )
-                    || SwXHeadFootText::IsXHeadFootText(pLast);
-            } while( bDel && 0 != ( pLast = ++aIter ));
+        SwIterator<SwClient,SwFrmFmt> aIter(*pFmt);
+        for(SwClient* pLast = aIter.First(); bDel && pLast; pLast = aIter.Next())
+            if(!pLast->IsA(TYPE(SwFrm)) || !SwXHeadFootText::IsXHeadFootText(pLast))
+                bDel = false;
     }
 
     if ( bDel )

zhirshya/lo-tblr

commit 31e66946e16fa777385db28a97fb1adc76c180f7

sw: fix always-true condition in lcl_DelHFFormat ...
... that prevents headers from being deleted if they have a client.

(regression from d4267231754c1e6b03c7723a6fecc46750e7c780)

Change-Id: I71f52f8806e59c97b81aa14144c700c14c5527b0

sw/source/core/layout/atrfrm.cxx
@@ -120,7 +120,7 @@ static void lcl_DelHFFormat( SwClient *pToRemove, SwFrameFormat *pFormat )
         // It's suboptimal if the format is deleted beforehand.
         SwIterator<SwClient,SwFrameFormat> aIter(*pFormat);
         for(SwClient* pLast = aIter.First(); bDel && pLast; pLast = aIter.Next())
-            if(dynamic_cast<const SwFrame*>( pLast ) ==  nullptr || !SwXHeadFootText::IsXHeadFootText(pLast))
+            if (dynamic_cast<const SwFrame*>(pLast) == nullptr && !SwXHeadFootText::IsXHeadFootText(pLast))
                 bDel = false;
     }
 