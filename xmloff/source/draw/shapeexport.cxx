LibreOffice/core

commit 9835a5823e0f559aabbc0e15ea126c82229c4bc7

sw textboxes: reimplement ODF import/export
Turns out that for normal TextFrames and rectangular drawinglayer
shapes, the engine is written in ODF implicitly. Use the same trick to
describe if the shape content should be rendered by SW or editeng.

This reverts 9d310ecfce3c2fc481b125e1493a534e2107a68e (sw textboxes:
implement ODF import/export, 2014-06-02).

Change-Id: Id8afa6368bedcd293e578244abb58e18949bec8d

xmloff/source/draw/shapeexport.cxx
@@ -76,6 +76,7 @@
 #include <com/sun/star/presentation/AnimationSpeed.hpp>
 #include <com/sun/star/presentation/ClickAction.hpp>
 #include <com/sun/star/style/XStyle.hpp>
+#include <com/sun/star/style/XStyleFamiliesSupplier.hpp>
 #include <com/sun/star/table/XColumnRowRange.hpp>
 #include <com/sun/star/text/XText.hpp>
 #include <com/sun/star/document/XStorageBasedDocument.hpp>
@@ -352,6 +353,18 @@ void XMLShapeExport::collectShapeAutoStyles(const uno::Reference< drawing::XShap
 
                 aParentName += xStyle->getName();
             }
+        }
+
+        if (aParentName.isEmpty() && xPropertySetInfo->hasPropertyByName("TextBox") && xPropSet->getPropertyValue("TextBox").get<bool>())
+        {
+            // Shapes with a Writer TextBox always have a parent style.
+            // If there would be none, then just assign the first available.
+            uno::Reference<style::XStyleFamiliesSupplier> xStyleFamiliesSupplier(GetExport().GetModel(), uno::UNO_QUERY);
+            uno::Reference<container::XNameAccess> xStyleFamilies = xStyleFamiliesSupplier->getStyleFamilies();
+            uno::Reference<container::XNameAccess> xFrameStyles = xStyleFamilies->getByName("FrameStyles").get< uno::Reference<container::XNameAccess> >();
+            uno::Sequence<OUString> aFrameStyles = xFrameStyles->getElementNames();
+            if (aFrameStyles.hasElements())
+                aParentName = aFrameStyles[0];
         }
 
         // filter propset

sw/source/core/doc/textboxhelper.cxx
@@ -97,6 +97,7 @@ void SwTextBoxHelper::create(SwFrmFmt* pShape)
         syncProperty(pShape, RES_HORI_ORIENT, MID_HORIORIENT_POSITION, xShapePropertySet->getPropertyValue(UNO_NAME_HORI_ORIENT_POSITION));
         syncProperty(pShape, RES_VERT_ORIENT, MID_VERTORIENT_POSITION, xShapePropertySet->getPropertyValue(UNO_NAME_VERT_ORIENT_POSITION));
         syncProperty(pShape, RES_FRM_SIZE, MID_FRMSIZE_IS_AUTO_HEIGHT, xShapePropertySet->getPropertyValue(UNO_NAME_TEXT_AUTOGROWHEIGHT));
+        syncProperty(pShape, RES_TEXT_VERT_ADJUST, 0, xShapePropertySet->getPropertyValue(UNO_NAME_TEXT_VERT_ADJUST));
     }
 }
 

xmloff/source/draw/sdpropls.cxx
@@ -80,8 +80,6 @@ using namespace ::xmloff::token;
 #define TMAP(name,prefix,token,type,context) _MAP(name,prefix,token,type|XML_TYPE_PROP_TEXT,context)
 #define PMAP(name,prefix,token,type,context) _MAP(name,prefix,token,type|XML_TYPE_PROP_PARAGRAPH,context)
 #define MAP_END() { 0L, 0, 0, XML_EMPTY, 0 ,0, SvtSaveOptions::ODFVER_010, false }
-// extensions import/export
-#define MAP_EXT(name,prefix,token,type,context)  _MAPV(name,prefix,token,type,context,SvtSaveOptions::ODFVER_012_EXT_COMPAT)
 
 // entry list for graphic properties
 
@@ -289,8 +287,6 @@ const XMLPropertyMapEntry aXMLSDProperties[] =
     // misc object properties
     GMAP( "MoveProtect",                    XML_NAMESPACE_STYLE, XML_PROTECT,               XML_SD_TYPE_MOVE_PROTECT|MID_FLAG_MULTI_PROPERTY|MID_FLAG_MERGE_ATTRIBUTE, CTF_SD_MOVE_PROTECT ),
     GMAP( "SizeProtect",                    XML_NAMESPACE_STYLE, XML_PROTECT,               XML_SD_TYPE_SIZE_PROTECT|MID_FLAG_MULTI_PROPERTY|MID_FLAG_MERGE_ATTRIBUTE, CTF_SD_SIZE_PROTECT ),
-
-    MAP_EXT("TextBox", XML_NAMESPACE_LO_EXT, XML_TEXT_BOX, XML_TYPE_BOOL|XML_TYPE_PROP_GRAPHIC, 0),
 
     MAP_END()
 };

zhirshya/lo-tblr

commit 4f50568492e2597168aef31ea845560ed318728c

tdf#108231 tdf#108109 xmloff: SvxUnoDrawingModel doesn't implement...
... XStyleFamiliesSupplier, so try to do without.  This is called from
sw in case SWTRANSFER_OBJECTTYPE_DRAWMODEL.

(regression from 9835a5823e0f559aabbc0e15ea126c82229c4bc7)

Change-Id: Ic99ad4bf0c8e6e24fc10da173ec480c9d29c0363

xmloff/source/draw/shapeexport.cxx
@@ -389,11 +389,16 @@ void XMLShapeExport::collectShapeAutoStyles(const uno::Reference< drawing::XShap
             // Shapes with a Writer TextBox always have a parent style.
             // If there would be none, then just assign the first available.
             uno::Reference<style::XStyleFamiliesSupplier> xStyleFamiliesSupplier(GetExport().GetModel(), uno::UNO_QUERY);
-            uno::Reference<container::XNameAccess> xStyleFamilies = xStyleFamiliesSupplier->getStyleFamilies();
-            uno::Reference<container::XNameAccess> xFrameStyles = xStyleFamilies->getByName("FrameStyles").get< uno::Reference<container::XNameAccess> >();
-            uno::Sequence<OUString> aFrameStyles = xFrameStyles->getElementNames();
-            if (aFrameStyles.hasElements())
-                aParentName = aFrameStyles[0];
+            if (xStyleFamiliesSupplier.is()) // tdf#108231
+            {
+                uno::Reference<container::XNameAccess> xStyleFamilies = xStyleFamiliesSupplier->getStyleFamilies();
+                uno::Reference<container::XNameAccess> xFrameStyles = xStyleFamilies->getByName("FrameStyles").get< uno::Reference<container::XNameAccess> >();
+                uno::Sequence<OUString> aFrameStyles = xFrameStyles->getElementNames();
+                if (aFrameStyles.hasElements())
+                {
+                    aParentName = aFrameStyles[0];
+                }
+            }
         }
 
         // filter propset