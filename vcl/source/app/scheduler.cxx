gllona/core

Correctly account invoked task
Don't account the to-be invoked task before invoking it.
If it happens to be restarted, account it and eventually lower
the system timer timeout.

Change-Id: I567e3b92c6c2999ce51aecb31f858e51cab6c999

vcl/source/app/scheduler.cxx
@@ -270,14 +270,16 @@ bool Scheduler::ProcessTaskScheduling()
         if ( !pSchedulerData->mpTask->IsActive() )
             goto next_entry;
 
-        UpdateMinPeriod( pSchedulerData, nTime, nMinPeriod );
-
         // skip ready tasks with lower priority than the most urgent (numerical lower is higher)
         if ( pSchedulerData->mpTask->ReadyForSchedule( nTime ) &&
              (!pMostUrgent || (pSchedulerData->mpTask->GetPriority() < pMostUrgent->mpTask->GetPriority())) )
         {
+            if ( pMostUrgent )
+                UpdateMinPeriod( pMostUrgent, nTime, nMinPeriod );
             pMostUrgent = pSchedulerData;
         }
+        else
+            UpdateMinPeriod( pSchedulerData, nTime, nMinPeriod );
 
 next_entry:
         pPrevSchedulerData = pSchedulerData;
@@ -317,7 +319,11 @@ next_entry:
                                tools::Time::GetSystemTicks() );
         }
         else if ( pMostUrgent->mpTask && !pMostUrgent->mbDelete )
+        {
             pMostUrgent->mnUpdateTime = tools::Time::GetSystemTicks();
+            UpdateMinPeriod( pMostUrgent, nTime, nMinPeriod );
+            UpdateSystemTimer( pSVData, nMinPeriod, false, nTime );
+        }
     }
 
     return !!pMostUrgent;

zhirshya/lo-tblr

vcl/source/app/scheduler.cxx
@@ -294,14 +294,16 @@ bool Scheduler::ProcessTaskScheduling()
         if ( !pSchedulerData->mpTask->IsActive() )
             goto next_entry;
 
-        UpdateMinPeriod( pSchedulerData, nTime, nMinPeriod );
-
         // skip ready tasks with lower priority than the most urgent (numerical lower is higher)
         if ( pSchedulerData->mpTask->ReadyForSchedule( nTime ) &&
              (!pMostUrgent || (pSchedulerData->mpTask->GetPriority() < pMostUrgent->mpTask->GetPriority())) )
         {
+            if ( pMostUrgent )
+                UpdateMinPeriod( pMostUrgent, nTime, nMinPeriod );
             pMostUrgent = pSchedulerData;
         }
+        else
+            UpdateMinPeriod( pSchedulerData, nTime, nMinPeriod );
 
 next_entry:
         pPrevSchedulerData = pSchedulerData;
@@ -343,7 +345,11 @@ next_entry:
                                tools::Time::GetSystemTicks() );
         }
         else if ( pMostUrgent->mpTask && !pMostUrgent->mbDelete )
+        {
             pMostUrgent->mnUpdateTime = tools::Time::GetSystemTicks();
+            UpdateMinPeriod( pMostUrgent, nTime, nMinPeriod );
+            UpdateSystemTimer( rSchedCtx, nMinPeriod, false, nTime );
+        }
     }
 
     return !!pMostUrgent;