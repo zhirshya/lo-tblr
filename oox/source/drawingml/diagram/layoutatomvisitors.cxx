LibreOffice/core

SmartArt: more layout work
* basic layout algorithms implementation
* change layouting order to "from top"

Change-Id: I8ef397fa0e39bb6d8cda2d1387b663980f134a59

oox/source/drawingml/diagram/layoutatomvisitors.cxx
@@ -98,7 +98,9 @@ void ShapeCreationVisitor::visit(LayoutNode& rAtom)
 
     if (rAtom.getExistingShape())
     {
-        rAtom.setupShape(rAtom.getExistingShape(), mrDgm, mnCurrIdx);
+        // reuse existing shape
+        if (rAtom.setupShape(rAtom.getExistingShape(), mrDgm, mnCurrIdx))
+            rAtom.getNodeShapes().push_back(rAtom.getExistingShape());
     }
     else if( pCurrShape )
     {
@@ -117,6 +119,7 @@ void ShapeCreationVisitor::visit(LayoutNode& rAtom)
         {
             pCurrParent->addChild(pClonedShape);
             pCurrParent = pClonedShape;
+            rAtom.getNodeShapes().push_back(pClonedShape);
         }
     }
     else
@@ -134,10 +137,11 @@ void ShapeCreationVisitor::visit(LayoutNode& rAtom)
     // restore parent
     mpParentShape=pPreviousParent;
 
-    // layout shapes - now all child shapes are created
-    ShapeLayoutingVisitor aLayoutingVisitor(pCurrParent,
-                                            rAtom.getName());
-    aLayoutingVisitor.defaultVisit(rAtom);
+    // remove unneeded empty group shapes
+    pCurrParent->getChildren().erase(
+        std::remove_if(pCurrParent->getChildren().begin(), pCurrParent->getChildren().end(),
+            [] (const ShapePtr & aChild) { return aChild->getServiceName() == "com.sun.star.drawing.GroupShape" && aChild->getChildren().empty(); }),
+        pCurrParent->getChildren().end());
 }
 
 void ShapeLayoutingVisitor::defaultVisit(LayoutAtom& rAtom)
@@ -154,12 +158,16 @@ void ShapeLayoutingVisitor::visit(ConstraintAtom& /*rAtom*/)
 
 void ShapeLayoutingVisitor::visit(AlgAtom& rAtom)
 {
-    rAtom.layoutShape(mpParentShape, maName);
+    if (mbLookForAlg && mpCurrentLayoutNode)
+    {
+        for (const auto& pShape : mpCurrentLayoutNode->getNodeShapes())
+            rAtom.layoutShape(pShape, mpCurrentLayoutNode->getName());
+    }
 }
 
-void ShapeLayoutingVisitor::visit(ForEachAtom& /*rAtom*/)
+void ShapeLayoutingVisitor::visit(ForEachAtom& rAtom)
 {
-    // stop processing
+    defaultVisit(rAtom);
 }
 
 void ShapeLayoutingVisitor::visit(ConditionAtom& rAtom)
@@ -172,9 +180,21 @@ void ShapeLayoutingVisitor::visit(ChooseAtom& rAtom)
     defaultVisit(rAtom);
 }
 
-void ShapeLayoutingVisitor::visit(LayoutNode& /*rAtom*/)
+void ShapeLayoutingVisitor::visit(LayoutNode& rAtom)
 {
-    // stop processing - only traverse Condition/Choose atoms
+    if (mbLookForAlg)
+        return;
+
+    LayoutNode* pPreviousLayoutNode = mpCurrentLayoutNode;
+    mpCurrentLayoutNode = &rAtom;
+
+    // process alg atoms first, nested layout nodes afterwards
+    mbLookForAlg = true;
+    defaultVisit(rAtom);
+    mbLookForAlg = false;
+    defaultVisit(rAtom);
+
+    mpCurrentLayoutNode = pPreviousLayoutNode;
 }
 
 void ShallowPresNameVisitor::defaultVisit(LayoutAtom& rAtom)

zhirshya/lo-tblr

oox/source/drawingml/diagram/layoutatomvisitors.cxx
@@ -97,7 +97,9 @@ void ShapeCreationVisitor::visit(LayoutNode& rAtom)
 
     if (rAtom.getExistingShape())
     {
-        rAtom.setupShape(rAtom.getExistingShape(), mrDgm, mnCurrIdx);
+        // reuse existing shape
+        if (rAtom.setupShape(rAtom.getExistingShape(), mrDgm, mnCurrIdx))
+            rAtom.getNodeShapes().push_back(rAtom.getExistingShape());
     }
     else
     {
@@ -117,6 +119,7 @@ void ShapeCreationVisitor::visit(LayoutNode& rAtom)
             {
                 pCurrParent->addChild(pShape);
                 pCurrParent = pShape;
+                rAtom.getNodeShapes().push_back(pShape);
             }
         }
         else
@@ -135,10 +138,11 @@ void ShapeCreationVisitor::visit(LayoutNode& rAtom)
     // restore parent
     mpParentShape=pPreviousParent;
 
-    // layout shapes - now all child shapes are created
-    ShapeLayoutingVisitor aLayoutingVisitor(pCurrParent,
-                                            rAtom.getName());
-    aLayoutingVisitor.defaultVisit(rAtom);
+    // remove unneeded empty group shapes
+    pCurrParent->getChildren().erase(
+        std::remove_if(pCurrParent->getChildren().begin(), pCurrParent->getChildren().end(),
+            [] (const ShapePtr & aChild) { return aChild->getServiceName() == "com.sun.star.drawing.GroupShape" && aChild->getChildren().empty(); }),
+        pCurrParent->getChildren().end());
 }
 
 void ShapeCreationVisitor::visit(ShapeAtom& /*rAtom*/)
@@ -212,12 +216,16 @@ void ShapeLayoutingVisitor::visit(ConstraintAtom& /*rAtom*/)
 
 void ShapeLayoutingVisitor::visit(AlgAtom& rAtom)
 {
-    rAtom.layoutShape(mpParentShape, maName);
+    if (mbLookForAlg && mpCurrentLayoutNode)
+    {
+        for (const auto& pShape : mpCurrentLayoutNode->getNodeShapes())
+            rAtom.layoutShape(pShape, mpCurrentLayoutNode->getName());
+    }
 }
 
-void ShapeLayoutingVisitor::visit(ForEachAtom& /*rAtom*/)
+void ShapeLayoutingVisitor::visit(ForEachAtom& rAtom)
 {
-    // stop processing
+    defaultVisit(rAtom);
 }
 
 void ShapeLayoutingVisitor::visit(ConditionAtom& rAtom)
@@ -230,9 +238,21 @@ void ShapeLayoutingVisitor::visit(ChooseAtom& rAtom)
     defaultVisit(rAtom);
 }
 
-void ShapeLayoutingVisitor::visit(LayoutNode& /*rAtom*/)
+void ShapeLayoutingVisitor::visit(LayoutNode& rAtom)
 {
-    // stop processing - only traverse Condition/Choose atoms
+    if (mbLookForAlg)
+        return;
+
+    LayoutNode* pPreviousLayoutNode = mpCurrentLayoutNode;
+    mpCurrentLayoutNode = &rAtom;
+
+    // process alg atoms first, nested layout nodes afterwards
+    mbLookForAlg = true;
+    defaultVisit(rAtom);
+    mbLookForAlg = false;
+    defaultVisit(rAtom);
+
+    mpCurrentLayoutNode = pPreviousLayoutNode;
 }
 
 void ShapeLayoutingVisitor::visit(ShapeAtom& /*rAtom*/)