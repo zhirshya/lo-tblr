LibreOffice/core

commit 6a5dbe73537642b06bcde782118a34b4593d17eb

do not iterate over all bookmarks in SwTxtNode::Update()
3f9872185e introduced new API for fast finding of marks to a specific text node,
so use that (code itself based on cb46aaf2d7).
This makes mailmerge faster (since it can create a huge document, and especially
with the change to use UNO bookmarks to mark starts of MM documents inside
the larger single document there can be a large number of marks).

Change-Id: I30ec69acf423e9a62fae5f5492ed8744cb727a56

sw/source/core/txtnode/ndtxt.cxx
@@ -1067,12 +1067,19 @@ void SwTxtNode::Update(
         {
             bool bAtLeastOneBookmarkMoved = false;
             bool bAtLeastOneExpandedBookmarkAtInsertionPosition = false;
-            const IDocumentMarkAccess* const pMarkAccess = getIDocumentMarkAccess();
-            for ( IDocumentMarkAccess::const_iterator_t ppMark = pMarkAccess->getAllMarksBegin();
-                ppMark != pMarkAccess->getAllMarksEnd();
-                ++ppMark )
+            // A text node already knows its marks via its SwIndexes.
+            std::set<const sw::mark::IMark*> aSeenMarks;
+            const SwIndex* next;
+            for (const SwIndex* pIndex = GetFirstIndex(); pIndex; pIndex = next )
             {
-                const ::sw::mark::IMark* const pMark = ppMark->get();
+                next = pIndex->GetNext();
+                const sw::mark::IMark* pMark = pIndex->GetMark();
+                if (!pMark)
+                    continue;
+                // Only handle bookmarks once, if they start and end at this node as well.
+                if (aSeenMarks.find(pMark) != aSeenMarks.end())
+                    continue;
+                aSeenMarks.insert(pMark);
                 const SwPosition* pEnd = &pMark->GetMarkEnd();
                 SwIndex & rEndIdx = const_cast<SwIndex&>(pEnd->nContent);
                 if( this == &pEnd->nNode.GetNode() &&

zhirshya/lo-tblr

commit 88662396f7c20b68f9bebaf2f096892adf636ea9

tdf#105705 sw: fix another case of bookmarks becoming un-sorted
The loop in SwTextNode::Update() that reassigns bookmark end positions
away from the node to a temporary SwIndexReg can stop prematurely in a
particular situation where there is a bookmark that is expanded and with
both mark positions at the critical "rPos" insertion position, and also
both their SwIndexes consecutive in the SwIndexReg::m_pFirst
linked-list.

What happens then is that the iteration gets to the first of the 2
consecutive positions of the bookmark, then calls GetMarkEnd() on the
mark, which happens to return the other position, which is already
stored in the "next" local variable.

That other position is then moved to aTmpIdxReg, and in the next loop
iteration GetNext() is null as it is the last one in aTmpIdxReg and the
loop terminates.

Thus various bookmark end positions don't get preserved, or the
bAtLeastOneExpandedBookmarkAtInsertionPosition doesn't get set, either
of which can cause the bookmark array to lose its sort order.

This was found playing around with Zotero 4.0.29.10, while switching
between different citation styles.

(regression from 6a5dbe73537642b06bcde782118a34b4593d17eb)

Change-Id: Ia35ce0656bcb2d6af7ea189458af3942ff83e4da

sw/source/core/txtnode/ndtxt.cxx
@@ -1087,6 +1087,10 @@ void SwTextNode::Update(
                 if( this == &pEnd->nNode.GetNode() &&
                     rPos.GetIndex() == rEndIdx.GetIndex() )
                 {
+                    if (&rEndIdx == next) // nasty corner case:
+                    {   // don't switch to iterating aTmpIdxReg!
+                        next = rEndIdx.GetNext();
+                    }
                     rEndIdx.Assign( &aTmpIdxReg, rEndIdx.GetIndex() );
                     bAtLeastOneBookmarkMoved = true;
                 }