LibreOffice/core

commit 850795942b3e168cab8ce88b4f2b421945ff29ca

tdf#99692 sw: fix bookmark positions in tables at start of redlines
The code assumes that if it can move the cursor backward in line 2038,
that move can be "inverted" by moving the cursor forward after the
content has been moved - but if the cursor moved back a node, and the
moved content does not start with a SwTextNode, the cursor will move
forward skipping over the non-text nodes, so offsets in the aSaveBkmks
(and aSaveRedl, presumably) are going to be wrong.

Just don't use Move() if it leaves the current node.

Change-Id: I95278a10c14aeba9f76558486bb2712f6726dbcb

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -2033,12 +2033,19 @@ bool DocumentContentOperationsManager::MoveRange( SwPaM& rPaM, SwPosition& rPos,
 
     // Put back the Pam by one "content"; so that it's always outside of
     // the manipulated range.
-    // If there's no content anymore, set it to the StartNode (that's
-    // always there).
-    const bool bNullContent = !aSavePam.Move( fnMoveBackward, GoInContent );
+    // tdf#99692 don't Move() back if that would end up in another node
+    // because moving backward is not necessarily the inverse of forward then.
+    const bool bNullContent = aSavePam.GetPoint()->nContent == 0;
     if( bNullContent )
     {
         aSavePam.GetPoint()->nNode--;
+        aSavePam.GetPoint()->nContent.Assign(aSavePam.GetContentNode(), 0);
+    }
+    else
+    {
+        bool const success(aSavePam.Move(fnMoveBackward, GoInContent));
+        assert(success);
+        (void) success;
     }
 
     // Copy all Bookmarks that are within the Move range into an array,

zhirshya/lo-tblr

commit 3501b0a90361980540711a71befe3e2c9e5c9db2

tdf#99692 sw: fix the fix: don't set bNullContent if node was split
When showing the redlines in rhbz908615-13.odt, the following assertion
happens:

Assertion `IDocumentMarkAccess::IsLegalPaMForCrossRefHeadingBookmark(rPaM) && "<CrossRefBookmark::CrossRefBookmark(..)>" "- creation of cross-reference bookmark with an illegal PaM that does not expand over exactly one whole paragraph."' failed.

This is because in DocumentContentOperationsManager::MoveRange() the
flag bNullContent is set after the node has been split; in this case the
nContent is of course always 0.

Later the function then restores aSavePam to the index 0 of the next
node, when it actually shouldn't do anything because the JoinNext()
already positioned it correctly at the merge-index of the re-joined node.

(regression from 850795942b3e168cab8ce88b4f2b421945ff29ca)

Change-Id: I64d50e70b19e2fd81537a9771fa8706898b17642

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -2034,7 +2034,8 @@ bool DocumentContentOperationsManager::MoveRange( SwPaM& rPaM, SwPosition& rPos,
     // the manipulated range.
     // tdf#99692 don't Move() back if that would end up in another node
     // because moving backward is not necessarily the inverse of forward then.
-    const bool bNullContent = aSavePam.GetPoint()->nContent == 0;
+    // (but do Move() back if we have split the node)
+    const bool bNullContent = !bSplit && aSavePam.GetPoint()->nContent == 0;
     if( bNullContent )
     {
         aSavePam.GetPoint()->nNode--;