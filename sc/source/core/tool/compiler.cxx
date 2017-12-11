LibreOffice/core

commit 73c7e0921d752df53004ed55735f3e8888cc592f

sc-perf: tdf#79023 for ODFF do not call SvNumberFormatter to determine numeric
Speedup of this particular function and callees by factor 33 ...

1000 calls for =12345.6789 and =123.456 alternating:

Before:
Ir       Irpc  Callee
9859177  9859  ScCompiler::IsValue
6246858  6246  SvNumberFormatter::IsNumberFormat
3496261  3496  SvNumberFormatter::GetStandardIndex

After:
 298000   298  ScCompiler::IsValue
 248000   248  rtl_math_uStringToDouble

Change-Id: I36eac8c5fe1b1cbf34dfb480c9e7ca6607769364

sc/source/core/tool/compiler.cxx
@@ -2885,8 +2885,51 @@ bool ScCompiler::IsOpCode2( const OUString& rName )
     return bFound;
 }
 
+static bool lcl_ParenthesisFollows( const sal_Unicode* p )
+{
+    while (*p == ' ')
+        p++;
+    return *p == '(';
+}
+
 bool ScCompiler::IsValue( const OUString& rSym )
 {
+    if (FormulaGrammar::isODFF( GetGrammar()))
+    {
+        // Speedup things for ODFF, only well-formed numbers, not locale
+        // dependent nor user input.
+        rtl_math_ConversionStatus eStatus;
+        sal_Int32 nParseEnd;
+        double fVal = rtl::math::stringToDouble( rSym, '.', 0, &eStatus, &nParseEnd);
+        if (nParseEnd != rSym.getLength())
+        {
+            // Not (only) a number.
+
+            if (nParseEnd > 0)
+                return false;   // partially a number => no such thing
+
+            if (lcl_ParenthesisFollows( aFormula.getStr() + nSrcPos))
+                return false;   // some function name, not a constant
+
+            // Could be TRUE or FALSE constant.
+            if (rSym.equalsIgnoreAsciiCase("TRUE"))
+            {
+                maRawToken.SetDouble( 1.0 );
+                return true;
+            }
+            if (rSym.equalsIgnoreAsciiCase("FALSE"))
+            {
+                maRawToken.SetDouble( 0.0 );
+                return true;
+            }
+            return false;
+        }
+        if (eStatus == rtl_math_ConversionStatus_OutOfRange)
+            SetError( errIllegalArgument );
+        maRawToken.SetDouble( fVal );
+        return true;
+    }
+
     double fVal;
     sal_uInt32 nIndex = mxSymbols->isEnglish() ? mpFormatter->GetStandardIndex(LANGUAGE_ENGLISH_US) : 0;
 
@@ -2905,10 +2948,7 @@ bool ScCompiler::IsValue( const OUString& rSym )
 
     if (nType == css::util::NumberFormat::LOGICAL)
     {
-        const sal_Unicode* p = aFormula.getStr() + nSrcPos;
-        while( *p == ' ' )
-            p++;
-        if (*p == '(')
+        if (lcl_ParenthesisFollows( aFormula.getStr() + nSrcPos))
             return false;   // Boolean function instead.
     }
 

zhirshya/lo-tblr

commit ae614bd293f3917d116cf7e04d20cb4541c43a80

Resolves: tdf#114406 treat % as the operator that it is
Regression from

    commit 73c7e0921d752df53004ed55735f3e8888cc592f
    Date:   Tue Sep 20 21:39:10 2016 +0200

	sc-perf: tdf#79023 for ODFF do not call SvNumberFormatter to determine numeric

for ODFF, and for OOXML

    commit a8a8ff59c5749bbe1f2f58ea8fd42d66e6ae2a81
    Date:   Tue Sep 20 22:37:59 2016 +0200

	sc-perf: tdf#79023 do not call SvNumberFormatter also for numbers in OOXML

Numbers followed by a percent operator like 100% were always
(wrongly) treated as one entity (by '%' having ScCharFlags::Value)
and incidentally handled by the number formatter's parser, which
so far "worked" in the sense that the correct constant number was
produced (i.e. 1 here), but the expression detail was lost.

The commits above for performance reasons when reading ODF or
OOXML files don't let a symbol pass through the number formatter's
parser anymore and the "100%" does not represent a strict number,
so lead to #NAME? error at the end.

Set the proper flags in the compiler's character table to treat
'%' as operator in all circumstances.

Change-Id: I266beb74a313c779370e5fac42f45d0fb5cdba0c

sc/source/core/tool/compiler.cxx
@@ -323,7 +323,7 @@ ScCompiler::Convention::Convention( FormulaGrammar::AddressConvention eConv )
 /* $ */     t[36] = ScCharFlags::CharWord | ScCharFlags::Word | ScCharFlags::CharIdent | ScCharFlags::Ident;
             if (FormulaGrammar::CONV_ODF == meConv)
 /* $ */         t[36] |= ScCharFlags::OdfNameMarker;
-/* % */     t[37] = ScCharFlags::Value;
+/* % */     t[37] = ScCharFlags::Char | ScCharFlags::WordSep | ScCharFlags::ValueSep;
 /* & */     t[38] = ScCharFlags::Char | ScCharFlags::WordSep | ScCharFlags::ValueSep;
 /* ' */     t[39] = ScCharFlags::NameSep;
 /* ( */     t[40] = ScCharFlags::Char | ScCharFlags::WordSep | ScCharFlags::ValueSep;