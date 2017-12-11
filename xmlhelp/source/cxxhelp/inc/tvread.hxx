LibreOffice/core

commit 4b69497e36b941d4db62ae8d5bad863d032fdc50

loplugin:useuniqueptr in TVDom
Change-Id: I885c202c64f43b89372d079659dd9b5b12677fbd
Reviewed-on: https://gerrit.libreoffice.org/44861
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

xmlhelp/source/treeview/tvread.cxx
@@ -37,6 +37,7 @@
 #include <com/sun/star/uri/XVndSunStarExpandUrl.hpp>
 #include <i18nlangtag/languagetag.hxx>
 #include <unotools/pathoptions.hxx>
+#include <memory>
 
 namespace treeview {
 
@@ -54,23 +55,17 @@ namespace treeview {
         {
         }
 
-        ~TVDom()
-        {
-            for(TVDom* p : children)
-                delete p;
-        }
-
         TVDom* newChild()
         {
-            children.push_back( new TVDom( this ) );
-            return children.back();
+            children.emplace_back( new TVDom( this ) );
+            return children.back().get();
         }
 
         TVDom* newChild(TVDom* p)
         {
-            children.push_back( p );
+            children.emplace_back( p );
             p->parent = this;
-            return children.back();
+            return children.back().get();
         }
 
         TVDom* getParent() const
@@ -158,7 +153,7 @@ namespace treeview {
         OUString  targetURL;
 
         TVDom *parent;
-        std::vector< TVDom* > children;
+        std::vector< std::unique_ptr<TVDom> > children;
     };
 
 }
@@ -382,7 +377,7 @@ TVChildTarget::TVChildTarget( const ConfigData& configData,TVDom* tvDom )
 {
     Elements.resize( tvDom->children.size() );
     for( size_t i = 0; i < Elements.size(); ++i )
-        Elements[i] = new TVRead( configData,tvDom->children[i] );
+        Elements[i] = new TVRead( configData,tvDom->children[i].get() );
 }
 
 TVChildTarget::TVChildTarget( const Reference< XComponentContext >& xContext )
@@ -428,7 +423,7 @@ TVChildTarget::TVChildTarget( const Reference< XComponentContext >& xContext )
 
     Elements.resize( tvDom.children.size() );
     for( size_t i = 0; i < Elements.size(); ++i )
-        Elements[i] = new TVRead( configData,tvDom.children[i] );
+        Elements[i] = new TVRead( configData,tvDom.children[i].get() );
 }
 
 TVChildTarget::~TVChildTarget()
@@ -450,11 +445,11 @@ void TVChildTarget::Check(TVDom* tvDom)
             if (((tvDom->children[i])->application == (tvDom->children[tvDom->children.size()-1])->application) &&
                 ((tvDom->children[i])->id == (tvDom->children[tvDom->children.size()-1])->id))
             {
-                TVDom* p = tvDom->children[tvDom->children.size()-1];
+                TVDom* p = tvDom->children.back().get();
 
-                for(TVDom* k : p->children)
-                    if (!SearchAndInsert(k, tvDom->children[i]))
-                        tvDom->children[i]->newChild(k);
+                for(auto & k : p->children)
+                    if (!SearchAndInsert(k.get(), tvDom->children[i].get()))
+                        tvDom->children[i]->newChild(k.get());
 
                 tvDom->children.pop_back();
                 h = true;
@@ -470,7 +465,7 @@ bool TVChildTarget::SearchAndInsert(TVDom* p, TVDom* tvDom)
     bool h = false;
     sal_Int32 max = 0;
 
-    std::vector< TVDom* >::iterator max_It, i;
+    std::vector< std::unique_ptr<TVDom> >::iterator max_It, i;
     max_It = tvDom->children.begin();
 
     sal_Int32 c_int;
@@ -486,7 +481,7 @@ bool TVChildTarget::SearchAndInsert(TVDom* p, TVDom* tvDom)
 
             if (p_int==c_int)
             {
-                (*(tvDom->children.insert(i+1, p)))->parent = tvDom;
+                (*(tvDom->children.insert(i+1, std::unique_ptr<TVDom>(p))))->parent = tvDom;
                 return true;
             }
             else if(c_int>max && c_int < p_int)
@@ -495,13 +490,14 @@ bool TVChildTarget::SearchAndInsert(TVDom* p, TVDom* tvDom)
                 max_It = i+1;
             }
         }
-    if (h) (*(tvDom->children.insert(max_It, p)))->parent = tvDom;
+    if (h)
+        (*(tvDom->children.insert(max_It, std::unique_ptr<TVDom>(p))))->parent = tvDom;
     else
     {
         i = tvDom->children.begin();
         while ((i!=tvDom->children.end()) && (!h))
         {
-            h = SearchAndInsert(p, *i);
+            h = SearchAndInsert(p, i->get());
             ++i;
         }
     }

zhirshya/lo-tblr

commit 1b89fb01b66a6b28475a0b155a00e25cbca09d9f

tdf#114242 xmlhelp: fix crash in TVChildTarget::Check()
The ... idiomatic C++ code in TVChildTarget::Check() and
SearchAndInsert() handles ownership in non-obvious ways,
so it's not surprising that it took offense at the recent
conversion to std::unique_ptr.  Let's at least try to
prevent it from crashing so quickly.

(regression from 4b69497e36b941d4db62ae8d5bad863d032fdc50)

Change-Id: I0981707a61aee1733e727b1c00346d8ec524362e

xmlhelp/source/cxxhelp/inc/tvread.hxx
@@ -227,7 +227,7 @@ namespace treeview {
 
         static void subst( OUString& instpath );
 
-        bool SearchAndInsert(TVDom* p, TVDom* tvDom);
+        std::unique_ptr<TVDom> SearchAndInsert(std::unique_ptr<TVDom> p, TVDom* tvDom);
 
         void Check(TVDom* tvDom);
 

xmlhelp/source/treeview/tvread.cxx
@@ -61,11 +61,10 @@ namespace treeview {
             return children.back().get();
         }
 
-        TVDom* newChild(TVDom* p)
+        void newChild(std::unique_ptr<TVDom> p)
         {
-            children.emplace_back( p );
-            p->parent = this;
-            return children.back().get();
+            children.emplace_back(std::move(p));
+            children.back()->parent = this;
         }
 
         TVDom* getParent() const
@@ -448,8 +447,13 @@ void TVChildTarget::Check(TVDom* tvDom)
                 TVDom* p = tvDom->children.back().get();
 
                 for(auto & k : p->children)
-                    if (!SearchAndInsert(k.get(), tvDom->children[i].get()))
-                        tvDom->children[i]->newChild(k.get());
+                {
+                    std::unique_ptr<TVDom> tmp(SearchAndInsert(std::move(k), tvDom->children[i].get()));
+                    if (tmp)
+                    {
+                        tvDom->children[i]->newChild(std::move(tmp));
+                    }
+                }
 
                 tvDom->children.pop_back();
                 h = true;
@@ -458,9 +462,10 @@ void TVChildTarget::Check(TVDom* tvDom)
         }
 }
 
-bool TVChildTarget::SearchAndInsert(TVDom* p, TVDom* tvDom)
+std::unique_ptr<TVDom>
+TVChildTarget::SearchAndInsert(std::unique_ptr<TVDom> p, TVDom* tvDom)
 {
-    if (p->isLeaf()) return false;
+    if (p->isLeaf()) return p;
 
     bool h = false;
     sal_Int32 max = 0;
@@ -481,8 +486,8 @@ bool TVChildTarget::SearchAndInsert(TVDom* p, TVDom* tvDom)
 
             if (p_int==c_int)
             {
-                (*(tvDom->children.insert(i+1, std::unique_ptr<TVDom>(p))))->parent = tvDom;
-                return true;
+                (*(tvDom->children.insert(i+1, std::move(p))))->parent = tvDom;
+                return nullptr;
             }
             else if(c_int>max && c_int < p_int)
             {
@@ -491,17 +496,20 @@ bool TVChildTarget::SearchAndInsert(TVDom* p, TVDom* tvDom)
             }
         }
     if (h)
-        (*(tvDom->children.insert(max_It, std::unique_ptr<TVDom>(p))))->parent = tvDom;
+    {
+        (*(tvDom->children.insert(max_It, std::move(p))))->parent = tvDom;
+        return nullptr;
+    }
     else
     {
         i = tvDom->children.begin();
-        while ((i!=tvDom->children.end()) && (!h))
+        while ((i!=tvDom->children.end()) && (p != nullptr))
         {
-            h = SearchAndInsert(p, i->get());
+            p = SearchAndInsert(std::move(p), i->get());
             ++i;
         }
+        return p;
     }
-    return h;
 }
 
 Any SAL_CALL