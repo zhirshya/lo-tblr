LibreOffice/core

commit 334601603aa04ea968e8a850f4e7f6cf52f7735b

sw: move some globals in fetab.cxx into SwFEShell
It looks like the main point of these being globals is so that they can
be cleared from various places when tables or table frames are destroyed.

Add a SwDoc parameter to ClearFEShellTabCols() and just iterate
over all shells.

Change-Id: I75ad6b695ee1bfa76b9a05c606b07a3574c70ac4

sw/source/core/frmedt/fetab.cxx
@@ -29,7 +29,6 @@
 #include <editeng/brushitem.hxx>
 #include <editeng/frmdiritem.hxx>
 #include <svtools/ruler.hxx>
-#include <fetab.hxx>
 #include <swwait.hxx>
 #include <fmtfsize.hxx>
 #include <fmtornt.hxx>
@@ -72,18 +71,6 @@ using namespace ::com::sun::star;
 #define COLFUZZY 20L
 
 inline bool IsSame( long nA, long nB ) { return  std::abs(nA-nB) <= COLFUZZY; }
-
-// table column cache
-static SwTabCols *pLastCols   = nullptr;
-const SwTable   *g_pColumnCacheLastTable  = nullptr;
-const SwTabFrame  *g_pColumnCacheLastTabFrame = nullptr;
-const SwFrame     *g_pColumnCacheLastCellFrame = nullptr;
-
-// table row cache
-static SwTabCols *pLastRows   = nullptr;
-const SwTable   *g_pRowCacheLastTable  = nullptr;
-const SwTabFrame  *g_pRowCacheLastTabFrame = nullptr;
-const SwFrame     *g_pRowCacheLastCellFrame = nullptr;
 
 class TableWait
 {
@@ -511,8 +498,7 @@ bool SwFEShell::SplitTab( bool bVert, sal_uInt16 nCnt, bool bSameHeight )
         // now delete the columns
         bRet = GetDoc()->SplitTable( aBoxes, bVert, nCnt, bSameHeight );
 
-        DELETEZ( pLastCols );
-        DELETEZ( pLastRows );
+        ClearFEShellTabCols(*GetDoc(), nullptr);
     }
     else
         bRet = false;
@@ -520,13 +506,13 @@ bool SwFEShell::SplitTab( bool bVert, sal_uInt16 nCnt, bool bSameHeight )
     return bRet;
 }
 
-void SwFEShell::GetTabCols_( SwTabCols &rToFill, const SwFrame *pBox )
+void SwFEShell::GetTabCols_(SwTabCols &rToFill, const SwFrame *pBox) const
 {
     const SwTabFrame *pTab = pBox->FindTabFrame();
-    if ( pLastCols )
+    if (m_pColumnCache)
     {
         bool bDel = true;
-        if (g_pColumnCacheLastTable == pTab->GetTable())
+        if (m_pColumnCache->pLastTable == pTab->GetTable())
         {
             bDel = false;
             SwRectFnSet aRectFnSet(pTab);
@@ -537,60 +523,61 @@ void SwFEShell::GetTabCols_( SwTabCols &rToFill, const SwFrame *pBox )
             const sal_uLong nRightMax = aRectFnSet.GetRight(pTab->getFrameArea()) -
                                     aRectFnSet.GetLeft(pPage->getFrameArea());
 
-            if (g_pColumnCacheLastTabFrame != pTab)
+            if (m_pColumnCache->pLastTabFrame != pTab)
             {
                 // if TabFrame was changed, we only shift a little bit
                 // as the width is the same
-                SwRectFnSet fnRectX(g_pColumnCacheLastTabFrame);
-                if (fnRectX.GetWidth(g_pColumnCacheLastTabFrame->getFrameArea()) ==
+                SwRectFnSet fnRectX(m_pColumnCache->pLastTabFrame);
+                if (fnRectX.GetWidth(m_pColumnCache->pLastTabFrame->getFrameArea()) ==
                     aRectFnSet.GetWidth(pTab->getFrameArea()) )
                 {
-                    pLastCols->SetLeftMin( nLeftMin );
+                    m_pColumnCache->pLastCols->SetLeftMin( nLeftMin );
 
-                    g_pColumnCacheLastTabFrame = pTab;
+                    m_pColumnCache->pLastTabFrame = pTab;
                 }
                 else
                     bDel = true;
             }
 
             if ( !bDel &&
-                 pLastCols->GetLeftMin () == (sal_uInt16)nLeftMin &&
-                 pLastCols->GetLeft    () == (sal_uInt16)aRectFnSet.GetLeft(pTab->getFramePrintArea()) &&
-                 pLastCols->GetRight   () == (sal_uInt16)aRectFnSet.GetRight(pTab->getFramePrintArea())&&
-                 pLastCols->GetRightMax() == (sal_uInt16)nRightMax - pLastCols->GetLeftMin() )
+                 m_pColumnCache->pLastCols->GetLeftMin () == (sal_uInt16)nLeftMin &&
+                 m_pColumnCache->pLastCols->GetLeft    () == (sal_uInt16)aRectFnSet.GetLeft(pTab->getFramePrintArea()) &&
+                 m_pColumnCache->pLastCols->GetRight   () == (sal_uInt16)aRectFnSet.GetRight(pTab->getFramePrintArea())&&
+                 m_pColumnCache->pLastCols->GetRightMax() == (sal_uInt16)nRightMax - m_pColumnCache->pLastCols->GetLeftMin() )
             {
-                if (g_pColumnCacheLastCellFrame != pBox)
+                if (m_pColumnCache->pLastCellFrame != pBox)
                 {
-                    pTab->GetTable()->GetTabCols( *pLastCols,
+                    pTab->GetTable()->GetTabCols( *m_pColumnCache->pLastCols,
                                         static_cast<const SwCellFrame*>(pBox)->GetTabBox(), true);
-                    g_pColumnCacheLastCellFrame = pBox;
+                    m_pColumnCache->pLastCellFrame = pBox;
                 }
-                rToFill = *pLastCols;
+                rToFill = *m_pColumnCache->pLastCols;
             }
             else
                 bDel = true;
         }
         if ( bDel )
-            DELETEZ(pLastCols);
+            m_pColumnCache.reset();
     }
-    if ( !pLastCols )
+    if (!m_pColumnCache)
     {
         SwDoc::GetTabCols( rToFill, static_cast<const SwCellFrame*>(pBox) );
 
-        pLastCols   = new SwTabCols( rToFill );
-        g_pColumnCacheLastTable  = pTab->GetTable();
-        g_pColumnCacheLastTabFrame = pTab;
-        g_pColumnCacheLastCellFrame= pBox;
+        m_pColumnCache.reset(new SwColCache);
+        m_pColumnCache->pLastCols.reset(new SwTabCols(rToFill));
+        m_pColumnCache->pLastTable = pTab->GetTable();
+        m_pColumnCache->pLastTabFrame = pTab;
+        m_pColumnCache->pLastCellFrame = pBox;
     }
 }
 
-void SwFEShell::GetTabRows_( SwTabCols &rToFill, const SwFrame *pBox )
+void SwFEShell::GetTabRows_(SwTabCols &rToFill, const SwFrame *pBox) const
 {
     const SwTabFrame *pTab = pBox->FindTabFrame();
-    if ( pLastRows )
+    if (m_pRowCache)
     {
         bool bDel = true;
-        if (g_pRowCacheLastTable == pTab->GetTable())
+        if (m_pRowCache->pLastTable == pTab->GetTable())
         {
             bDel = false;
             SwRectFnSet aRectFnSet(pTab);
@@ -602,31 +589,32 @@ void SwFEShell::GetTabRows_( SwTabCols &rToFill, const SwFrame *pBox )
             const long nRight    = aRectFnSet.GetHeight(pTab->getFramePrintArea());
             const long nRightMax = aRectFnSet.IsVert() ? nRight : LONG_MAX;
 
-            if (g_pRowCacheLastTabFrame != pTab || g_pRowCacheLastCellFrame != pBox)
+            if (m_pRowCache->pLastTabFrame != pTab || m_pRowCache->pLastCellFrame != pBox)
                 bDel = true;
 
             if ( !bDel &&
-                 pLastRows->GetLeftMin () == nLeftMin &&
-                 pLastRows->GetLeft    () == nLeft &&
-                 pLastRows->GetRight   () == nRight &&
-                 pLastRows->GetRightMax() == nRightMax )
+                 m_pRowCache->pLastCols->GetLeftMin () == nLeftMin &&
+                 m_pRowCache->pLastCols->GetLeft    () == nLeft &&
+                 m_pRowCache->pLastCols->GetRight   () == nRight &&
+                 m_pRowCache->pLastCols->GetRightMax() == nRightMax )
             {
-                rToFill = *pLastRows;
+                rToFill = *m_pRowCache->pLastCols;
             }
             else
                 bDel = true;
         }
         if ( bDel )
-            DELETEZ(pLastRows);
+            m_pRowCache.reset();
     }
-    if ( !pLastRows )
+    if (!m_pRowCache)
     {
         SwDoc::GetTabRows( rToFill, static_cast<const SwCellFrame*>(pBox) );
 
-        pLastRows   = new SwTabCols( rToFill );
-        g_pRowCacheLastTable = pTab->GetTable();
-        g_pRowCacheLastTabFrame = pTab;
-        g_pRowCacheLastCellFrame = pBox;
+        m_pRowCache.reset(new SwColCache);
+        m_pRowCache->pLastCols.reset(new SwTabCols(rToFill));
+        m_pRowCache->pLastTable = pTab->GetTable();
+        m_pRowCache->pLastTabFrame = pTab;
+        m_pRowCache->pLastCellFrame = pBox;
     }
 }
 
@@ -1250,8 +1238,7 @@ bool SwFEShell::UpdateTableStyleFormatting(SwTableNode *pTableNode,
         StartAllAction();
         bRet = GetDoc()->SetTableAutoFormat(
                 aBoxes, *pTableStyle, bResetDirect, pStyleName != nullptr);
-        DELETEZ( pLastCols );
-        DELETEZ( pLastRows );
+        ClearFEShellTabCols(*GetDoc(), nullptr);
         EndAllActionAndCall();
     }
     else
@@ -1320,8 +1307,7 @@ bool SwFEShell::DeleteTableSel()
 
         bRet = GetDoc()->DeleteRowCol( aBoxes );
 
-        DELETEZ( pLastCols );
-        DELETEZ( pLastRows );
+        ClearFEShellTabCols(*GetDoc(), nullptr);
     }
     else
         bRet = false;
@@ -2097,10 +2083,33 @@ size_t SwFEShell::GetCurMouseTabColNum( const Point &rPt ) const
     return nRet;
 }
 
-void ClearFEShellTabCols()
+void ClearFEShellTabCols(SwDoc & rDoc, SwTabFrame const*const pFrame)
 {
-    DELETEZ( pLastCols );
-    DELETEZ( pLastRows );
+    for (SwViewShell& rCurrentShell : rDoc.getIDocumentLayoutAccess().GetCurrentViewShell()->GetRingContainer())
+    {
+        if (auto pFE = dynamic_cast<SwFEShell *>(&rCurrentShell))
+        {
+            pFE->ClearColumnRowCache(pFrame);
+        }
+    }
+}
+
+void SwFEShell::ClearColumnRowCache(SwTabFrame const*const pFrame)
+{
+    if (m_pColumnCache)
+    {
+        if (pFrame == nullptr || pFrame == m_pColumnCache->pLastTabFrame)
+        {
+            m_pColumnCache.reset();
+        }
+    }
+    if (m_pRowCache)
+    {
+        if (pFrame == nullptr || pFrame == m_pRowCache->pLastTabFrame)
+        {
+            m_pRowCache.reset();
+        }
+    }
 }
 
 void SwFEShell::GetTableAttr( SfxItemSet &rSet ) const
@@ -2225,8 +2234,7 @@ bool SwFEShell::SetColRowWidthHeight( TableChgWidthHeightType eType, sal_uInt16 
                     *const_cast<SwTableBox*>(static_cast<SwCellFrame*>(pFrame)->GetTabBox()),
                     eType, nDiff, nLogDiff );
 
-    delete pLastCols;
-    pLastCols = nullptr;
+    ClearFEShellTabCols(*GetDoc(), nullptr);
     EndAllActionAndCall();
 
     if( bRet && (eType & (TableChgWidthHeightType::BiggerMode | TableChgWidthHeightType::InsertDeleteMode)) == TableChgWidthHeightType::InsertDeleteMode )

compilerplugins/clang/badstatics.cxx
@@ -168,10 +168,6 @@ public:
                 || name == "s_pVout" // FrameFinit()
                 || name == "s_pPaintQueue" // SwPaintQueue::Remove()
                 || name == "gProp" // only owned (VclPtr) member cleared again
-                || name == "g_pColumnCacheLastTabFrame" // not owning
-                || name == "g_pColumnCacheLastCellFrame" // not owning
-                || name == "g_pRowCacheLastTabFrame" // not owning
-                || name == "g_pRowCacheLastCellFrame" // not owning
                 || name == "g_OszCtrl" // SwCrsrOszControl::Exit()
                 || name == "g_pSpellIter" // SwEditShell::SpellEnd()
                 || name == "g_pConvIter" // SwEditShell::SpellEnd()

solenv/clang-format/blacklist
@@ -15567,7 +15567,6 @@ sw/source/core/inc/dumpfilter.hxx
 sw/source/core/inc/dview.hxx
 sw/source/core/inc/environmentofanchoredobject.hxx
 sw/source/core/inc/fefly.hxx
-sw/source/core/inc/fetab.hxx
 sw/source/core/inc/fieldhint.hxx
 sw/source/core/inc/flowfrm.hxx
 sw/source/core/inc/flyfrm.hxx

sw/inc/fesh.hxx
@@ -198,10 +198,13 @@ enum class SwTab
 };
 
 class SdrDropMarkerOverlay;
+struct SwColCache;
 
 class SW_DLLPUBLIC SwFEShell : public SwEditShell
 {
 private:
+    mutable std::unique_ptr<SwColCache> m_pColumnCache;
+    mutable std::unique_ptr<SwColCache> m_pRowCache;
     std::unique_ptr<SdrDropMarkerOverlay> m_pChainTo;
     std::unique_ptr<SdrDropMarkerOverlay> m_pChainFrom;
     bool m_bCheckForOLEInCaption;
@@ -223,8 +226,8 @@ private:
     SAL_DLLPRIVATE static sal_uInt16 GetCurColNum_( const SwFrame *pFrame,
                           SwGetCurColNumPara* pPara );
 
-    SAL_DLLPRIVATE static void GetTabCols_( SwTabCols &rToFill, const SwFrame *pBox );
-    SAL_DLLPRIVATE static void GetTabRows_( SwTabCols &rToFill, const SwFrame *pBox );
+    SAL_DLLPRIVATE void GetTabCols_(SwTabCols &rToFill, const SwFrame *pBox) const;
+    SAL_DLLPRIVATE void GetTabRows_(SwTabCols &rToFill, const SwFrame *pBox) const;
 
     SAL_DLLPRIVATE bool ImpEndCreate();
 
@@ -809,9 +812,11 @@ public:
 
     void ToggleHeaderFooterEdit( );
     static void SetLineEnds(SfxItemSet& rAttr, SdrObject const * pObj, sal_uInt16 nSlotId);
+
+    SAL_DLLPRIVATE void ClearColumnRowCache(SwTabFrame const*);
 };
 
-void ClearFEShellTabCols();
+void ClearFEShellTabCols(SwDoc & rDoc, SwTabFrame const*const pFrame);
 
 #endif
 

sw/inc/tabcol.hxx
@@ -22,6 +22,8 @@
 #include <tools/solar.h>
 
 #include <vector>
+#include <memory>
+
 #include "swdllapi.h"
 
 struct SwTabColsEntry
@@ -85,6 +87,17 @@ public:
 
     bool IsLastRowAllowedToChange() const { return bLastRowAllowedToChange; }
     void SetLastRowAllowedToChange( bool bNew ) { bLastRowAllowedToChange = bNew; }
+};
+
+class SwTable;
+class SwTabFrame;
+class SwFrame;
+
+struct SwColCache {
+    std::unique_ptr<SwTabCols> pLastCols;
+    SwTable const* pLastTable  = nullptr;
+    SwTabFrame const* pLastTabFrame = nullptr;
+    SwFrame const* pLastCellFrame = nullptr;
 };
 
 #endif // INCLUDED_SW_INC_TABCOL_HXX

sw/source/core/bastyp/init.cxx
@@ -735,8 +735,6 @@ void FinitCore()
         delete pHt;
     }
 
-    ::ClearFEShellTabCols();
-
     for (SvGlobalName* p : *pGlobalOLEExcludeList)
         delete p;
     delete pGlobalOLEExcludeList;

sw/source/core/docnode/ndtbl.cxx
@@ -1740,7 +1740,7 @@ bool SwDoc::InsertCol( const SwSelBoxes& rBoxes, sal_uInt16 nCnt, bool bBehind )
         if (bRet)
         {
             getIDocumentState().SetModified();
-            ::ClearFEShellTabCols();
+            ::ClearFEShellTabCols(*this, nullptr);
             getIDocumentFieldsAccess().SetFieldsDirty( true, nullptr, 0 );
         }
     }
@@ -1802,7 +1802,7 @@ bool SwDoc::InsertRow( const SwSelBoxes& rBoxes, sal_uInt16 nCnt, bool bBehind )
         if (bRet)
         {
             getIDocumentState().SetModified();
-            ::ClearFEShellTabCols();
+            ::ClearFEShellTabCols(*this, nullptr);
             getIDocumentFieldsAccess().SetFieldsDirty( true, nullptr, 0 );
         }
     }
@@ -1965,7 +1965,7 @@ bool SwDoc::DeleteRowCol( const SwSelBoxes& rBoxes, bool bColumn )
     if( dynamic_cast<const SwDDETable*>( &pTableNd->GetTable() ) !=  nullptr)
         return false;
 
-    ::ClearFEShellTabCols();
+    ::ClearFEShellTabCols(*this, nullptr);
     SwSelBoxes aSelBoxes( rBoxes );
     SwTable &rTable = pTableNd->GetTable();
     long nMin = 0;
@@ -2333,7 +2333,7 @@ TableMergeErr SwDoc::MergeTable( SwPaM& rPam )
         rPam.GetPoint()->nNode = *pMergeBox->GetSttNd();
         rPam.Move();
 
-        ::ClearFEShellTabCols();
+        ::ClearFEShellTabCols(*this, nullptr);
         getIDocumentRedlineAccess().SetRedlineFlags_intern( eOld );
     }
     GetIDocumentUndoRedo().EndUndo( SwUndoId::TABLE_MERGE, nullptr );
@@ -2864,7 +2864,7 @@ void SwDoc::SetTabRows( const SwTabCols &rNew, bool bCurColOnly,
 
     GetIDocumentUndoRedo().EndUndo( SwUndoId::TABLE_ATTR, nullptr );
 
-    ::ClearFEShellTabCols();
+    ::ClearFEShellTabCols(*this, nullptr);
 }
 
 /**
@@ -2879,7 +2879,7 @@ void SwDoc::SetTabCols(SwTable& rTab, const SwTabCols &rNew, const SwTabCols &rO
             new SwUndoAttrTable( *rTab.GetTableNode(), true ));
     }
     rTab.SetTabCols( rNew, rOld, pStart, bCurRowOnly );
-      ::ClearFEShellTabCols();
+      ::ClearFEShellTabCols(*this, nullptr);
     getIDocumentState().SetModified();
 }
 

sw/source/core/docnode/ndtbl1.cxx
@@ -808,9 +808,8 @@ void SwDoc::SetTabBorders( const SwCursor& rCursor, const SfxItemSet& rSet )
             pTableLayout->GetBrowseWidthByTabFrame( *pTabFrame ) );
     }
     SwTableFormatCmp::Delete( aFormatCmp );
-    ::ClearFEShellTabCols();
+    ::ClearFEShellTabCols(*this, nullptr);
     getIDocumentState().SetModified();
-
 }
 
 static void lcl_SetLineStyle( SvxBorderLine *pToSet,
@@ -902,7 +901,7 @@ void SwDoc::SetTabLineStyle( const SwCursor& rCursor,
             pTableLayout->BordersChanged(
                 pTableLayout->GetBrowseWidthByTabFrame( *pTabFrame ) );
         }
-        ::ClearFEShellTabCols();
+        ::ClearFEShellTabCols(*this, nullptr);
         getIDocumentState().SetModified();
     }
 }

zhirshya/lo-tblr

commit 1d087c52ebcede00368c506b0d4e8a481aac4e9c

sw: fix null-pointer crash in ClearFEShellTabCols()
(regression from 334601603aa04ea968e8a850f4e7f6cf52f7735b)

Change-Id: I6e257f07a44740f77450ca6278aa2c0fa8ce2551

sw/source/core/frmedt/fetab.cxx
@@ -2085,11 +2085,15 @@ size_t SwFEShell::GetCurMouseTabColNum( const Point &rPt ) const
 
 void ClearFEShellTabCols(SwDoc & rDoc, SwTabFrame const*const pFrame)
 {
-    for (SwViewShell& rCurrentShell : rDoc.getIDocumentLayoutAccess().GetCurrentViewShell()->GetRingContainer())
+    auto const pShell(rDoc.getIDocumentLayoutAccess().GetCurrentViewShell());
+    if (pShell)
     {
-        if (auto pFE = dynamic_cast<SwFEShell *>(&rCurrentShell))
+        for (SwViewShell& rCurrentShell : pShell->GetRingContainer())
         {
-            pFE->ClearColumnRowCache(pFrame);
+            if (auto const pFE = dynamic_cast<SwFEShell *>(&rCurrentShell))
+            {
+                pFE->ClearColumnRowCache(pFrame);
+            }
         }
     }
 }