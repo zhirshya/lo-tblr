LibreOffice/core

commit 86e3f95cff4d3dad48d64e50e0c4b05034fa7064

tdf#114523 Make inline tooltips for changes optional
Tooltips on tracked changes are not shown when Show track changes
is switched off; additional property introduced at Tools > Option >
Writer > Changes to disable the inline tooltip completely

Change-Id: I5b54895bb22d167a383b06627c69326c3c2f5b02
Reviewed-on: https://gerrit.libreoffice.org/46971
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Heiko Tietze <tietze.heiko@gmail.com>

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -2717,6 +2717,16 @@ void DocumentRedlineManager::checkRedlining(RedlineFlags& _rReadlineMode)
     }
 }
 
+bool DocumentRedlineManager::IsHideInlineTooltips()
+{
+    return bHideInlineTooltips;
+}
+
+void DocumentRedlineManager::SetHideInlineTooltips(bool bSet)
+{
+    bHideInlineTooltips = bSet;
+}
+
 DocumentRedlineManager::~DocumentRedlineManager()
 {
 }

sw/source/core/inc/DocumentRedlineManager.hxx
@@ -30,6 +30,8 @@ namespace sw
 
 class DocumentRedlineManager : public IDocumentRedlineAccess
 {
+    bool bHideInlineTooltips : 1;
+
 public:
     DocumentRedlineManager( SwDoc& i_rSwdoc );
 
@@ -111,6 +113,8 @@ public:
     virtual void SetRedlinePassword(
         /*[in]*/const css::uno::Sequence <sal_Int8>& rNewPassword) override;
 
+    virtual bool IsHideInlineTooltips() override;
+    virtual void SetHideInlineTooltips(bool bSet) override;
 
     //Non Interface methods;
 

officecfg/registry/schema/org/openoffice/Office/Writer.xcs
@@ -2650,6 +2650,14 @@
           <value>0</value>
         </prop>
       </group>
+      <prop oor:name="ShowInlineTooltip" oor:type="xs:boolean" oor:nillable="false">
+        <!-- UIHints: Tools - Options - Text document - Changes - Show tooltips on changes -->
+        <info>
+          <desc>Specifies whether tooltips for track changes are shown in the document.</desc>
+          <label>Show _tooltips on changes</label>
+        </info>
+        <value>true</value>
+      </prop>
     </group>
     <group oor:name="Comparison">
       <info>

sw/inc/IDocumentRedlineAccess.hxx
@@ -239,6 +239,9 @@ public:
     virtual void SetRedlinePassword(
         /*[in]*/const css::uno::Sequence <sal_Int8>& rNewPassword) = 0;
 
+    virtual bool IsHideInlineTooltips() = 0;
+    virtual void SetHideInlineTooltips(bool bSet) = 0;
+
 protected:
      virtual ~IDocumentRedlineAccess() {};
 };

sw/inc/modcfg.hxx
@@ -65,14 +65,15 @@ class SwRevisionConfig : public utl::ConfigItem
 {
     friend class SwModuleOptions;
 
-    AuthorCharAttr  aInsertAttr;    //Revision/TextDisplay/Insert/Attribute  // Redlining: author character attributes
-                                    //Revision/TextDisplay/Insert/Color
-    AuthorCharAttr  aDeletedAttr;   //Revision/TextDisplay/Delete/Attribute
-                                    //Revision/TextDisplay/Delete/Color
-    AuthorCharAttr  aFormatAttr;    //Revision/TextDisplay/ChangeAttribute/Attribute
-                                    //Revision/TextDisplay/ChangeAttribute/Color
-    sal_uInt16          nMarkAlign;     //Revision/LinesChanged/Mark
-    Color           aMarkColor;     //Revision/LinesChanged/Color
+    AuthorCharAttr  aInsertAttr;        //Revision/TextDisplay/Insert/Attribute  // Redlining: author character attributes
+                                        //Revision/TextDisplay/Insert/Color
+    AuthorCharAttr  aDeletedAttr;       //Revision/TextDisplay/Delete/Attribute
+                                        //Revision/TextDisplay/Delete/Color
+    AuthorCharAttr  aFormatAttr;        //Revision/TextDisplay/ChangeAttribute/Attribute
+                                        //Revision/TextDisplay/ChangeAttribute/Color
+    sal_uInt16      nMarkAlign;         //Revision/LinesChanged/Mark
+    Color           aMarkColor;         //Revision/LinesChanged/Color
+    bool            bShowInlineTooltip; //ShowInlineTooltip
 
     static const css::uno::Sequence<OUString>& GetPropertyNames();
 
@@ -253,6 +254,10 @@ public:
     const AuthorCharAttr    &GetFormatAuthorAttr() const { return aRevisionConfig.aFormatAttr; }
     void        SetFormatAuthorAttr( AuthorCharAttr const &rAttr ) {  aRevisionConfig.aFormatAttr = rAttr;
                                                                       aRevisionConfig.SetModified();}
+
+    bool        IsShowInlineTooltip() const { return aRevisionConfig.bShowInlineTooltip; }
+    void        SetShowInlineTooltip( bool bSet ) {  aRevisionConfig.bShowInlineTooltip = bSet;
+                                                      aRevisionConfig.SetModified(); }
 
     sal_uInt16          GetMarkAlignMode()  const               { return aRevisionConfig.nMarkAlign; }
     void            SetMarkAlignMode(sal_uInt16 nMode)          { aRevisionConfig.nMarkAlign = nMode;

sw/source/core/crsr/crstrvl.cxx
@@ -1116,6 +1116,11 @@ bool SwCursorShell::GetContentAtPos( const Point& rPt,
     SET_CURR_SHELL( this );
     bool bRet = false;
 
+    const bool bHideInlineTooltips = GetDoc()->getIDocumentRedlineAccess().IsHideInlineTooltips();
+    const bool bShowTrackChanges = IDocumentRedlineAccess::IsShowChanges( GetDoc()->getIDocumentRedlineAccess().GetRedlineFlags() );
+
+    if (bHideInlineTooltips || !bShowTrackChanges) return bRet;
+
     if( !IsTableMode() )
     {
         Point aPt( rPt );

sw/source/core/edit/edredln.cxx
@@ -25,6 +25,7 @@
 #include <editsh.hxx>
 #include <edimp.hxx>
 #include <frmtool.hxx>
+#include <officecfg/Office/Writer.hxx>
 
 RedlineFlags SwEditShell::GetRedlineFlags() const
 {
@@ -130,7 +131,15 @@ void SwEditShell::UpdateRedlineAttr()
         SET_CURR_SHELL( this );
         StartAllAction();
 
-        GetDoc()->getIDocumentRedlineAccess().UpdateRedlineAttr();
+        // issue when the changes the inline option when IsShow is off
+        SwDoc& rDoc = *GetDoc();
+        bool bShowInlineTooltip = officecfg::Office::Writer::Revision::ShowInlineTooltip::get();
+        if (bShowInlineTooltip != rDoc.getIDocumentRedlineAccess().IsHideInlineTooltips() )
+        {
+           rDoc.getIDocumentRedlineAccess().SetHideInlineTooltips( bShowInlineTooltip );
+        }
+
+        rDoc.getIDocumentRedlineAccess().UpdateRedlineAttr();
 
         EndAllAction();
     }

sw/source/ui/config/optpage.cxx
@@ -32,6 +32,8 @@
 #include <docsh.hxx>
 #include <IDocumentDeviceAccess.hxx>
 #include <IDocumentSettingAccess.hxx>
+#include <IDocumentRedlineAccess.hxx>
+
 #include <swmodule.hxx>
 #include <wrtsh.hxx>
 #include <uitool.hxx>
@@ -1752,6 +1754,7 @@ SwRedlineOptionsTabPage::SwRedlineOptionsTabPage( vcl::Window* pParent,
     get(m_pMarkPosLB,"markpos");
     get(m_pMarkColorLB,"markcolor");
     get(m_pMarkPreviewWN,"markpreview");
+    get(m_pShowChangesTooltip,"changestooltip");
 
     m_pInsertedPreviewWN->set_height_request(aPreviewSize.Height());
     m_pDeletedPreviewWN->set_height_request(aPreviewSize.Height());
@@ -1810,6 +1813,7 @@ void SwRedlineOptionsTabPage::dispose()
     m_pMarkPosLB.clear();
     m_pMarkColorLB.clear();
     m_pMarkPreviewWN.clear();
+    m_pShowChangesTooltip.clear();
     SfxTabPage::dispose();
 }
 
@@ -1830,6 +1834,8 @@ bool SwRedlineOptionsTabPage::FillItemSet( SfxItemSet* )
     AuthorCharAttr aOldInsertAttr(pOpt->GetInsertAuthorAttr());
     AuthorCharAttr aOldDeletedAttr(pOpt->GetDeletedAuthorAttr());
     AuthorCharAttr aOldChangedAttr(pOpt->GetFormatAuthorAttr());
+
+    const bool bOldShowInlineTooltips = pOpt->IsShowInlineTooltip();
 
     ColorData nOldMarkColor = pOpt->GetMarkAlignColor().GetColor();
     sal_uInt16 nOldMarkMode = pOpt->GetMarkAlignMode();
@@ -1874,14 +1880,15 @@ bool SwRedlineOptionsTabPage::FillItemSet( SfxItemSet* )
         case 4: nPos = text::HoriOrientation::INSIDE;     break;
     }
     pOpt->SetMarkAlignMode(nPos);
-
     pOpt->SetMarkAlignColor(m_pMarkColorLB->GetSelectEntryColor());
+    pOpt->SetShowInlineTooltip( m_pShowChangesTooltip->IsChecked() );
 
     if (!(aInsertedAttr == aOldInsertAttr) ||
         !(aDeletedAttr == aOldDeletedAttr) ||
         !(aChangedAttr == aOldChangedAttr) ||
        nOldMarkColor != pOpt->GetMarkAlignColor().GetColor() ||
-       nOldMarkMode != pOpt->GetMarkAlignMode())
+       nOldMarkMode != pOpt->GetMarkAlignMode() ||
+       bOldShowInlineTooltips != pOpt->IsShowInlineTooltip() )
     {
         // update all documents
         SwDocShell* pDocShell = static_cast<SwDocShell*>(SfxObjectShell::GetFirst(checkSfxObjectShell<SwDocShell>));
@@ -1919,6 +1926,8 @@ void SwRedlineOptionsTabPage::Reset( const SfxItemSet*  )
     m_pChangedColorLB->SelectEntry(Color(nColor));
 
     m_pMarkColorLB->SelectEntry(pOpt->GetMarkAlignColor());
+
+    m_pShowChangesTooltip->Check( pOpt->IsShowInlineTooltip() );
 
     m_pInsertLB->SelectEntryPos(0);
     m_pDeletedLB->SelectEntryPos(0);

zhirshya/lo-tblr

commit 0f10eb804a2358dc876122f6fdafe19ad19afdd5

sw: uninitialised DocumentRedlineManager::bHideLinineTooltips
(regression from 86e3f95cff4d3dad48d64e50e0c4b05034fa7064)

Change-Id: I48db6f070d7cf5ef0071952ad34822f75d472a21

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -606,14 +606,16 @@ namespace
 namespace sw
 {
 
-DocumentRedlineManager::DocumentRedlineManager( SwDoc& i_rSwdoc ) : m_rDoc( i_rSwdoc ),
-                                                                    meRedlineFlags(RedlineFlags::ShowInsert | RedlineFlags::ShowDelete),
-                                                                    mpRedlineTable( new SwRedlineTable ),
-                                                                    mpExtraRedlineTable ( new SwExtraRedlineTable ),
-                                                                    mpAutoFormatRedlnComment( nullptr ),
-                                                                    mbIsRedlineMove(false),
-                                                                    mbReadlineChecked(false),
-                                                                    mnAutoFormatRedlnCommentNo( 0 )
+DocumentRedlineManager::DocumentRedlineManager(SwDoc& i_rSwdoc)
+    : m_rDoc(i_rSwdoc)
+    , meRedlineFlags(RedlineFlags::ShowInsert | RedlineFlags::ShowDelete)
+    , mpRedlineTable(new SwRedlineTable)
+    , mpExtraRedlineTable(new SwExtraRedlineTable)
+    , mpAutoFormatRedlnComment(nullptr)
+    , mbIsRedlineMove(false)
+    , mbReadlineChecked(false)
+    , mnAutoFormatRedlnCommentNo(0)
+    , m_bHideInlineTooltips(false)
 {
 }
 
@@ -2719,12 +2721,12 @@ void DocumentRedlineManager::checkRedlining(RedlineFlags& _rReadlineMode)
 
 bool DocumentRedlineManager::IsHideInlineTooltips()
 {
-    return bHideInlineTooltips;
+    return m_bHideInlineTooltips;
 }
 
 void DocumentRedlineManager::SetHideInlineTooltips(bool bSet)
 {
-    bHideInlineTooltips = bSet;
+    m_bHideInlineTooltips = bSet;
 }
 
 DocumentRedlineManager::~DocumentRedlineManager()

sw/source/core/inc/DocumentRedlineManager.hxx
@@ -30,8 +30,6 @@ namespace sw
 
 class DocumentRedlineManager : public IDocumentRedlineAccess
 {
-    bool bHideInlineTooltips : 1;
-
 public:
     DocumentRedlineManager( SwDoc& i_rSwdoc );
 
@@ -144,6 +142,7 @@ private:
     sal_uInt16 mnAutoFormatRedlnCommentNo;  /**< SeqNo for conjoining of AutoFormat-Redlines.
                                          by the UI. Managed by SwAutoFormat! */
     css::uno::Sequence <sal_Int8 > maRedlinePasswd;
+    bool m_bHideInlineTooltips : 1;
 };
 
 }