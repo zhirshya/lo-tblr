LibreOffice/core

commit 41af2a9e26f3ee6b505b343120d5df3617ba5c49

tdf#91106 sfx2: Stylist: sort top-level styles in hierarchical mode
(regression from 8f04b172c5c78fa80c3692a729f0191425195869)

Change-Id: I111d12a622390afc02eb73a42b1d3c714f046bd2

sfx2/source/dialog/templdlg.cxx
@@ -558,6 +558,12 @@ StyleTreeArr_Impl& MakeTree_Impl(StyleTreeArr_Impl& rArr)
     // Only keep tree roots in rArr, child elements can be accessed through the hierarchy
     rArr.erase(std::remove_if(rArr.begin(), rArr.end(), [](StyleTree_Impl* pEntry) { return pEntry->HasParent(); }), rArr.end());
 
+    // tdf#91106 sort top level styles
+    std::sort(rArr.begin(), rArr.end(),
+        [&aSorter](StyleTree_Impl* pEntry1, StyleTree_Impl* pEntry2) {
+            return aSorter.compare(pEntry1->getName(), pEntry2->getName()) < 0;
+        });
+
     return rArr;
 }
 

zhirshya/lo-tblr

commit d0cb6a112c4b6e52db68984c56858f50bfc3e45a

tdf#91106 sfx2: Stylist hierarchical: sort the Default Style first
(regression from 41af2a9e26f3ee6b505b343120d5df3617ba5c49)

Change-Id: I47377447dd1e04a44033dea27ce7307a65821737

sfx2/source/dialog/templdlg.cxx
@@ -555,6 +555,10 @@ void MakeTree_Impl(StyleTreeArr_Impl& rArr)
     // tdf#91106 sort top level styles
     std::sort(rArr.begin(), rArr.end(),
         [&aSorter](std::unique_ptr<StyleTree_Impl> const & pEntry1, std::unique_ptr<StyleTree_Impl> const & pEntry2) {
+            if (pEntry2->getName() == "Default Style")
+                return false;
+            if (pEntry1->getName() == "Default Style")
+                return true; // default always first
             return aSorter.compare(pEntry1->getName(), pEntry2->getName()) < 0;
         });
 }